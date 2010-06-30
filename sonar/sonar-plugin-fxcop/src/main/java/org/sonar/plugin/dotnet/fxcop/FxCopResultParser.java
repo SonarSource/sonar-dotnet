08bd62a78d6d3dbea01fa2b97ba71bbc6f46241e
Alexandre Victoor <alexvictoor@gmail.com>
2010-06-30 21:40:43 +0000

    more robust fxcop plugin, no failure when cs files are not referenced in the csproj files

--- sonar/sonar-plugin-fxcop/src/main/java/org/sonar/plugin/dotnet/fxcop/FxCopResultParser.java
+++ sonar/sonar-plugin-fxcop/src/main/java/org/sonar/plugin/dotnet/fxcop/FxCopResultParser.java
@@ -29,6 +29,8 @@ import java.text.ParseException;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.SensorContext;
 import org.sonar.api.profiles.RulesProfile;
 import org.sonar.api.resources.Project;
@@ -40,6 +42,7 @@ import org.sonar.api.rules.Violation;
 import org.sonar.api.utils.ParsingUtils;
 import org.sonar.plugin.dotnet.core.AbstractXmlParser;
 import org.sonar.plugin.dotnet.core.resource.CSharpFile;
+import org.sonar.plugin.dotnet.core.resource.InvalidResourceException;
 import org.w3c.dom.Element;
 
 /**
@@ -47,98 +50,103 @@ import org.w3c.dom.Element;
  * 
  * @author Jose CHILLAN Sep 24, 2009
  */
-public class FxCopResultParser extends AbstractXmlParser
-{
-  private Project       project;
-  private SensorContext context;
-  private RulesManager  rulesManager;
-  private RulesProfile  profile;
+public class FxCopResultParser extends AbstractXmlParser {
 
-  /**
-   * Constructs a @link{FxCopResultParser}.
-   * 
-   * @param project
-   * @param context
-   * @param rulesManager
-   * @param profile
-   */
-  public FxCopResultParser(Project project, SensorContext context, RulesManager rulesManager, RulesProfile profile)
-  {
-    super();
-    this.project = project;
-    this.context = context;
-    this.rulesManager = rulesManager;
-    this.profile = profile;
-  }
+	private final static Logger log = LoggerFactory
+	    .getLogger(FxCopResultParser.class);
 
-  /**
-   * Parses a processed violation file.
-   * 
-   * @param stream
-   */
-  public void parse(URL url)
-  {
-    List<Element> issues = extractElements(url, "//issue");
-    // We add each issue
-    for (Element issueElement : issues)
-    {
-      String path = getNodeContent(issueElement, "path");
-      String fileName = getNodeContent(issueElement, "name");
-      String key = getNodeContent(issueElement, "key");
-      String message = getNodeContent(issueElement, "message");
-      String lineNumber = getNodeContent(issueElement, "line");
-      Resource<?> resource = getResource(path, fileName);
-      Integer line = getIntValue(lineNumber);
-      Rule rule = rulesManager.getPluginRule(FxCopPlugin.KEY, key);
-      if (rule == null)
-      {
-        // We skip the rules that were not registered
-        continue;
-      }
-      ActiveRule activeRule = profile.getActiveRule(FxCopPlugin.KEY, key);
-      Violation violation = new Violation(rule, resource);
-      violation.setLineId(line);
-      violation.setMessage(message);
-      if (activeRule != null)
-      {
-        violation.setPriority(activeRule.getPriority());
-      }
+	private Project project;
+	private SensorContext context;
+	private RulesManager rulesManager;
+	private RulesProfile profile;
 
-      // We store the violation
-      context.saveViolation(violation);
-    }
-  }
+	/**
+	 * Constructs a @link{FxCopResultParser}.
+	 * 
+	 * @param project
+	 * @param context
+	 * @param rulesManager
+	 * @param profile
+	 */
+	public FxCopResultParser(Project project, SensorContext context,
+	    RulesManager rulesManager, RulesProfile profile) {
+		super();
+		this.project = project;
+		this.context = context;
+		this.rulesManager = rulesManager;
+		this.profile = profile;
+	}
 
-  public Resource<?> getResource(String path, String fileName)
-  {
-    if (StringUtils.isBlank(fileName) || StringUtils.isBlank(path))
-    {
-      return null;
-    }
-    File file = new File(path, fileName);
-    CSharpFile fileResource = CSharpFile.from(project, file, false);
-    return fileResource;
-  }
+	/**
+	 * Parses a processed violation file.
+	 * 
+	 * @param stream
+	 */
+	public void parse(URL url) {
+		List<Element> issues = extractElements(url, "//issue");
+		// We add each issue
+		for (Element issueElement : issues) {
+			String path = getNodeContent(issueElement, "path");
+			String fileName = getNodeContent(issueElement, "name");
+			String key = getNodeContent(issueElement, "key");
+			String message = getNodeContent(issueElement, "message");
+			String lineNumber = getNodeContent(issueElement, "line");
+			Resource<?> resource = getResource(path, fileName);
+			Integer line = getIntValue(lineNumber);
+			Rule rule = rulesManager.getPluginRule(FxCopPlugin.KEY, key);
+			if (rule == null) {
+				// We skip the rules that were not registered
+				continue;
+			}
+			ActiveRule activeRule = profile.getActiveRule(FxCopPlugin.KEY, key);
+			Violation violation = new Violation(rule, resource);
+			violation.setLineId(line);
+			violation.setMessage(message);
+			if (activeRule != null) {
+				violation.setPriority(activeRule.getPriority());
+			}
 
-  /**
-   * Extracts the line number.
-   * 
-   * @param lineStr
-   * @return
-   */
-  protected Integer getIntValue(String lineStr)
-  {
-    if (StringUtils.isBlank(lineStr))
-    {
-      return null;
-    }
-    try
-    {
-      return (int) ParsingUtils.parseNumber(lineStr);
-    }
-    catch (ParseException ignore)
-    {
-      return null;
-    }
-  }
+			// We store the violation
+			context.saveViolation(violation);
+		}
+	}
+
+	public Resource<?> getResource(String path, String fileName) {
+		if (StringUtils.isBlank(fileName) || StringUtils.isBlank(path)) {
+			return null;
+		}
+		File file = new File(path, fileName);
+
+		CSharpFile fileResource;
+		if (file.exists()) {
+			try {
+				fileResource = CSharpFile.from(project, file, false);
+			} catch (InvalidResourceException ex) {
+				log.warn("resource error", ex);
+				fileResource = null;
+			}
+		} else {
+			log.error("Unable to ge resource for path " + file);
+			fileResource = null;
+		}
+
+		return fileResource;
+	}
+
+	/**
+	 * Extracts the line number.
+	 * 
+	 * @param lineStr
+	 * @return
+	 */
+	protected Integer getIntValue(String lineStr) {
+		if (StringUtils.isBlank(lineStr)) {
+			return null;
+		}
+		try {
+			return (int) ParsingUtils.parseNumber(lineStr);
+		} catch (ParseException ignore) {
+			return null;
+		}
+	}
 }
