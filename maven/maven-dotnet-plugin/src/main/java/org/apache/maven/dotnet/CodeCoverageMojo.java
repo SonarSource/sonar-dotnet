74a135ef5e1911bc7436b780128d4c4e252d9d6d
Alexandre Victoor <alexvictoor@gmail.com>
2010-07-25 06:01:22 +0000

    [SONARPLUGINS-576] ncover support

--- maven/maven-dotnet-plugin/src/main/java/org/apache/maven/dotnet/CodeCoverageMojo.java
+++ maven/maven-dotnet-plugin/src/main/java/org/apache/maven/dotnet/CodeCoverageMojo.java
@@ -36,226 +36,238 @@ import org.apache.maven.plugin.logging.Log;
 import org.codehaus.plexus.util.cli.Commandline;
 
 /**
- * Generates a coverage report on a .Net project or solution using the unit tests and PartCover
+ * Generates a coverage report on a .Net project or solution using the unit
+ * tests and PartCover
+ * 
  * @goal coverage
  * @phase site
- * @description generates a report on code coverage of a .Net project or solution
+ * @description generates a report on code coverage of a .Net project or
+ *              solution
  * @author Jose CHILLAN Apr 16, 2009
  */
-public class CodeCoverageMojo
-  extends GallioMojo
-{
-  /**
-   * Name of the resource folder that contains the Partcover exe
-   */
-  public static final String PART_RESOURCE_DIR = "partcover";
+public class CodeCoverageMojo extends GallioMojo {
+	private static final String GALLIO_RUNNER_4_N_COVER = "NCover3";
 
-  /**
-   * Name of the folder that will contain the extracted PartCover
-   */
-  public static final String PART_EXPORT_PATH  = "partcover-runtime";
+	/**
+	 * Name of the resource folder that contains the Partcover exe
+	 */
+	public static final String PART_RESOURCE_DIR = "partcover";
 
-  /**
-   * Location of the PartCover installation.
-   * 
-   * @parameter expression="${partcover.directory}"
-   */
-  protected File             partCoverDirectory;
+	/**
+	 * Name of the folder that will contain the extracted PartCover
+	 */
+	public static final String PART_EXPORT_PATH = "partcover-runtime";
 
-  /**
-   * Name of the PartCover executable.
-   * 
-   * @parameter expression="${partcover.executable}" default-value="PartCover.exe"
-   */
-  protected String           partCoverExecutable;
+	/**
+	 * Location of the PartCover installation.
+	 * 
+	 * @parameter expression="${partcover.directory}"
+	 */
+	protected File partCoverDirectory;
 
-  private File               executable;
-  /**
-   * Namespaces and assemblies excluded from the code coverage. The format is the PartCover format: "[assembly]namespace"
-   * 
-   * @parameter alias="coverageExcludes"
-   */
-  protected String[]         coverageExcludes;
+	/**
+	 * Name of the PartCover executable.
+	 * 
+	 * @parameter expression="${partcover.executable}"
+	 *            default-value="PartCover.exe"
+	 */
+	protected String partCoverExecutable;
 
-  /**
-   * Name of the coverage report XML file.
-   * 
-   * @parameter alias="coverageReportName" default-value="coverage-report.xml";
-   */
-  protected String           coverageReportName;
+	private File executable;
+	/**
+	 * Namespaces and assemblies excluded from the code coverage. The format is
+	 * the PartCover format: "[assembly]namespace"
+	 * 
+	 * @parameter alias="coverageExcludes"
+	 */
+	protected String[] coverageExcludes;
 
-  /**
-   * The output file for test.
-   * 
-   * @parameter alias="coverageReportFileName" default-value="coverage-test-report.xml"
-   */
-  protected String           coverageReportFileName;
+	/**
+	 * Name of the coverage report XML file.
+	 * 
+	 * @parameter alias="coverageReportName" default-value="coverage-report.xml";
+	 */
+	protected String coverageReportName;
 
-  public CodeCoverageMojo()
-  {
-  }
+	/**
+	 * The output file for test.
+	 * 
+	 * @parameter alias="coverageReportFileName"
+	 *            default-value="coverage-test-report.xml"
+	 */
+	protected String coverageReportFileName;
 
-  /**
-   * 
-   * Executes for a project (not supported for coverage)
-   * 
-   * @param visualProject
-   * @throws MojoExecutionException
-   * @throws MojoFailureException
-   */
-  @Override
-  protected void executeProject(VisualStudioProject visualProject) throws MojoExecutionException, MojoFailureException
-  {
-    getLog().info("Cannot launch code coverage on a single project");
-  }
+	/**
+	 * Set this true to use NCover3 instead of PartCover
+	 * 
+	 * @parameter expression="${useNCover}"
+	 */
+	protected boolean useNCover;
 
-  @Override
-  protected void executeSolution(VisualStudioSolution solution) throws MojoExecutionException, MojoFailureException
-  {
-    // For code coverage, we don't care of failures
-    this.testFailureIgnore = true;
-    super.executeSolution(solution);
-    getLog().info("Finished the code coverage");
-  }
+	public CodeCoverageMojo() {
+	}
 
-  /**
-   * @return
-   */
-  @Override
-  protected String getCommandName()
-  {
-    return "Code coverage";
-  }
+	/**
+	 * 
+	 * Executes for a project (not supported for coverage)
+	 * 
+	 * @param visualProject
+	 * @throws MojoExecutionException
+	 * @throws MojoFailureException
+	 */
+	@Override
+	protected void executeProject(VisualStudioProject visualProject)
+	    throws MojoExecutionException, MojoFailureException {
+		getLog().info("Cannot launch code coverage on a single project");
+	}
 
-  /**
-   * Generates a part cover specific test command.
-   * 
-   * @param solution
-   * @param testedAssemblies
-   * @return the command to run
-   * @throws MojoExecutionException
-   * @throws MojoFailureException
-   */
-  @Override
-  protected Commandline generateTestCommand(VisualStudioSolution solution, List<File> testedAssemblies)
-                                                                                                       throws MojoExecutionException,
-                                                                                                       MojoFailureException
-  {
-    List<VisualStudioProject> projects = solution.getProjects();
-    List<String> coveredAssemblyNames = new ArrayList<String>();
-    // We build the list to of covered assemblies
-    for (VisualStudioProject visualProject : projects)
-    {
-      if (!visualProject.isTest())
-      {
-        String assemblyName = visualProject.getAssemblyName();
-        coveredAssemblyNames.add(assemblyName);
-      }
-    }
+	@Override
+	protected void executeSolution(VisualStudioSolution solution)
+	    throws MojoExecutionException, MojoFailureException {
+		// For code coverage, we don't care of failures
+		this.testFailureIgnore = true;
+		super.executeSolution(solution);
+		getLog().info("Finished the code coverage");
+	}
 
-    List<String> testCommandArgs = generateGallioArgs(testedAssemblies);
-    // Extracts PartCover if necessary
-    extractPartCover();
+	/**
+	 * @return
+	 */
+	@Override
+	protected String getCommandName() {
+		return "Code coverage";
+	}
 
-    // Builds the launch data
-    File workDir = getReportDirectory();
-    File reportFile = getReportFile(coverageReportName);
-    File gallioExe = getGallioExe();
-    Log log = getLog();
-    if (log.isDebugEnabled())
-    {
-      log.debug("Parameters of the PartCover execution");
-      log.debug(" - PartCover          : " + executable);
-      log.debug(" - Gallio             : " + gallioExe);
-      log.debug(" - Covered assemblies : " + coveredAssemblyNames);
-      log.debug(" - Exclusions         : " + Arrays.toString(coverageExcludes));
-      log.debug(" - Work directory     : " + workDir);
-      log.debug(" - Report file        : " + reportFile);
-    }
+	/**
+	 * Generates a part cover specific test command.
+	 * 
+	 * @param solution
+	 * @param testedAssemblies
+	 * @return the command to run
+	 * @throws MojoExecutionException
+	 * @throws MojoFailureException
+	 */
+	@Override
+	protected Commandline generateTestCommand(VisualStudioSolution solution,
+	    List<File> testedAssemblies) throws MojoExecutionException,
+	    MojoFailureException {
+		List<VisualStudioProject> projects = solution.getProjects();
+		List<String> coveredAssemblyNames = new ArrayList<String>();
+		// We build the list to of covered assemblies
+		for (VisualStudioProject visualProject : projects) {
+			if (!visualProject.isTest()) {
+				String assemblyName = visualProject.getAssemblyName();
+				coveredAssemblyNames.add(assemblyName);
+			}
+		}
 
-    List<String> arguments = new ArrayList<String>();
-    arguments.add("--target");
-    arguments.add(toCommandPath(gallioExe));
-    arguments.add("--target-work-dir");
-    arguments.add(toCommandPath(workDir));
-    // We add all the tested assemblies
+		// Builds the launch data
+		File workDir = getReportDirectory();
+		File reportFile = getReportFile(coverageReportName);
+		File gallioExe = getGallioExe();
+		Log log = getLog();
+		if (log.isDebugEnabled()) {
+			log.debug("Parameters of the Coverage execution");
+			log.debug(" - Use NCover ?       : " + useNCover);
+			log.debug(" - PartCover          : " + executable);
+			log.debug(" - Gallio             : " + gallioExe);
+			log.debug(" - Covered assemblies : " + coveredAssemblyNames);
+			log.debug(" - Exclusions         : " + Arrays.toString(coverageExcludes));
+			log.debug(" - Work directory     : " + workDir);
+			log.debug(" - Report file        : " + reportFile);
+		}
 
-    // Generates the test arguments all in one, in which the quotes needs to be escaped
-    StringBuilder targetArgsBuilder = new StringBuilder();
-    targetArgsBuilder.append('"');
-    boolean isFirst = true;
-    for (String currentArg : testCommandArgs)
-    {
-      if (isFirst)
-      {
-        isFirst = false;
-      }
-      else
-      {
-        targetArgsBuilder.append(' ');
-      }
-      String escapedArg = escapeQuotes(currentArg);
-      targetArgsBuilder.append(escapedArg);
-    }
-    targetArgsBuilder.append('"');
-    arguments.add("--target-args");
-    arguments.add(targetArgsBuilder.toString());
+		final List<String> arguments;
 
-    // We add all the covered assemblies
-    for (String assemblyName : coveredAssemblyNames)
-    {
-      arguments.add("--include");
-      arguments.add("[" + assemblyName + "]*");
-    }
+		if (useNCover) {
+			gallioRunner = GALLIO_RUNNER_4_N_COVER;
+			arguments = generateGallioArgs(testedAssemblies);
+			arguments.add("/runner-property:NCoverCoverageFile="+reportFile);				
+			//arguments.add("/runner-property:NCoverArguments=//ias Example.Core");
 
-    // We add all the configured exclusions
-    if (coverageExcludes != null)
-    {
-      for (String exclusion : coverageExcludes)
-      {
-        arguments.add("--exclude");
-        arguments.add(exclusion);
-      }
-    }
-    arguments.add("--output");
-    arguments.add(toCommandPath(reportFile));
-    Commandline commandLine = generateCommandLine(executable, arguments);
-    return commandLine;
-  }
+			
+			executable = getGallioExe();
+		} else {
+			List<String> testCommandArgs = generateGallioArgs(testedAssemblies);
+			// Extracts PartCover if necessary
+			extractPartCover();
 
-  /**
-   * Escapes the quotes of a string.
-   * 
-   * @param input
-   * @return
-   */
-  public String escapeQuotes(String input)
-  {
-    StringBuilder result = new StringBuilder();
-    for (int idxChar = 0; idxChar < input.length(); idxChar++)
-    {
-      char currentChar = input.charAt(idxChar);
-      if (currentChar == '"')
-      {
-        result.append('\\');
-      }
-      result.append(currentChar);
-    }
-    return result.toString();
-  }
+			arguments = new ArrayList<String>();
+			arguments.add("--target");
+			arguments.add(toCommandPath(gallioExe));
+			arguments.add("--target-work-dir");
+			arguments.add(toCommandPath(workDir));
+			// We add all the tested assemblies
 
-  /**
-   * Extracts the part cover runtime if necessary.
-   * 
-   * @throws MojoExecutionException
-   * @throws MojoFailureException
-   */
-  private void extractPartCover() throws MojoExecutionException, MojoFailureException
-  {
-    if (partCoverDirectory == null)
-    {
-      partCoverDirectory = extractFolder(PART_RESOURCE_DIR, PART_EXPORT_PATH, "PartCover");
-    }
-    executable = new File(partCoverDirectory, partCoverExecutable);
-  }
+			// Generates the test arguments all in one, in which the quotes needs to
+			// be escaped
+			StringBuilder targetArgsBuilder = new StringBuilder();
+			targetArgsBuilder.append('"');
+			boolean isFirst = true;
+			for (String currentArg : testCommandArgs) {
+				if (isFirst) {
+					isFirst = false;
+				} else {
+					targetArgsBuilder.append(' ');
+				}
+				String escapedArg = escapeQuotes(currentArg);
+				targetArgsBuilder.append(escapedArg);
+			}
+			targetArgsBuilder.append('"');
+			arguments.add("--target-args");
+			arguments.add(targetArgsBuilder.toString());
+
+			// We add all the covered assemblies
+			for (String assemblyName : coveredAssemblyNames) {
+				arguments.add("--include");
+				arguments.add("[" + assemblyName + "]*");
+			}
+
+			// We add all the configured exclusions
+			if (coverageExcludes != null) {
+				for (String exclusion : coverageExcludes) {
+					arguments.add("--exclude");
+					arguments.add(exclusion);
+				}
+			}
+			arguments.add("--output");
+			arguments.add(toCommandPath(reportFile));
+		}
+
+		Commandline commandLine = generateCommandLine(executable, arguments);
+		return commandLine;
+	}
+
+	/**
+	 * Escapes the quotes of a string.
+	 * 
+	 * @param input
+	 * @return
+	 */
+	public String escapeQuotes(String input) {
+		StringBuilder result = new StringBuilder();
+		for (int idxChar = 0; idxChar < input.length(); idxChar++) {
+			char currentChar = input.charAt(idxChar);
+			if (currentChar == '"') {
+				result.append('\\');
+			}
+			result.append(currentChar);
+		}
+		return result.toString();
+	}
+
+	/**
+	 * Extracts the part cover runtime if necessary.
+	 * 
+	 * @throws MojoExecutionException
+	 * @throws MojoFailureException
+	 */
+	private void extractPartCover() throws MojoExecutionException,
+	    MojoFailureException {
+		if (partCoverDirectory == null) {
+			partCoverDirectory = extractFolder(PART_RESOURCE_DIR, PART_EXPORT_PATH,
+			    "PartCover");
+		}
+		executable = new File(partCoverDirectory, partCoverExecutable);
+	}
 }
