e4191e5c3478fb3e7d7d70e53d6746783e694854
Alexandre Victoor <alexvictoor@gmail.com>
2010-09-27 21:51:13 +0000

    tiny parsing improvement

--- sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeResultParser.java
+++ sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeResultParser.java
@@ -81,7 +81,7 @@ public class GendarmeResultParser extends AbstractXmlParser {
    * @param stream
    */
   public void parse(URL url) {
-    List<Element> issues = extractElements(url, "//issue");
+    List<Element> issues = extractElements(url, "/issues/issue");
     // We add each issue
     for (Element issueElement : issues) {
 
