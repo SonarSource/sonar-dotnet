532b57cd2f574f8efcb1fe43f5fc803b0635c39c
Alexandre Victoor <alexvictoor@gmail.com>
2010-11-08 22:56:33 +0000

    [SONARPLUGINS-788] fix fxcop tests

--- sonar/sonar-plugin-fxcop/src/test/java/org/sonar/plugin/dotnet/fxcop/FxCopResultParserTest.java
+++ sonar/sonar-plugin-fxcop/src/test/java/org/sonar/plugin/dotnet/fxcop/FxCopResultParserTest.java
@@ -27,6 +27,7 @@ import static org.mockito.Mockito.*;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import org.apache.commons.configuration.Configuration;
 import org.apache.maven.dotnet.commons.project.VisualStudioUtils;
 import org.apache.maven.project.MavenProject;
 import org.junit.Before;
@@ -68,6 +69,9 @@ public class FxCopResultParserTest {
     // set up sonar project
     Project project = mock(Project.class);
     when(project.getPom()).thenReturn(mvnProject);
+    Configuration configuration = mock(Configuration.class);
+    when(project.getConfiguration()).thenReturn(configuration);
+    
 
     parser = new FxCopResultParser(project, context, rulesManager, profile);
   }
