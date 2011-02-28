06bd0f556bca79555b88f5459073d7d14c9e2171
Alexandre Victoor <alexvictoor@gmail.com>
2011-02-28 22:18:58 +0000

    fix indentation issues

--- sonar/sonar-plugin-dotnet-core/src/main/java/org/sonar/plugin/dotnet/core/colorizer/CSharpColorizerFormat.java
+++ sonar/sonar-plugin-dotnet-core/src/main/java/org/sonar/plugin/dotnet/core/colorizer/CSharpColorizerFormat.java
@@ -40,21 +40,14 @@ public class CSharpColorizerFormat extends CodeColorizerFormat {
 
   static {
     keywords = new HashSet<String>();
-    keywords.addAll(Arrays.asList(new String[] { "abstract", "add", "alias",
-        "as", "ascending", "base", "bool", "break", "by", "byte", "case",
-        "catch", "char", "checked", "class", "const", "continue", "decimal",
-        "default", "delegate", "descending", "do", "double", "dynamic", "else",
-        "enum", "equals", "event", "explicit", "extern", "false", "finally",
-        "fixed", "float", "for", "foreach", "from", "get", "global", "goto",
-        "group", "if", "implicit", "in", "int", "interface", "internal",
-        "into", "is", "join", "let", "lock", "long", "namespace", "new",
-        "null", "object", "on", "operator", "orderby", "out", "override",
-        "params", "partial", "private", "protected", "public", "readonly",
-        "ref", "remove", "return", "sbyte", "sealed", "select", "set", "short",
-        "sizeof", "stackalloc", "static", "string", "struct", "switch", "this",
-        "throw", "true", "try", "typeof", "uint", "ulong", "unchecked",
-        "unsafe", "ushort", "using", "value", "var", "virtual", "void",
-        "volatile", "where", "while", "yield" }));
+    keywords.addAll(Arrays.asList(new String[] { "abstract", "add", "alias", "as", "ascending", "base", "bool", "break", "by", "byte",
+        "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "descending", "do", "double",
+        "dynamic", "else", "enum", "equals", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "from",
+        "get", "global", "goto", "group", "if", "implicit", "in", "int", "interface", "internal", "into", "is", "join", "let", "lock",
+        "long", "namespace", "new", "null", "object", "on", "operator", "orderby", "out", "override", "params", "partial", "private",
+        "protected", "public", "readonly", "ref", "remove", "return", "sbyte", "sealed", "select", "set", "short", "sizeof", "stackalloc",
+        "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort",
+        "using", "value", "var", "virtual", "void", "volatile", "where", "while", "yield" }));
   }
 
   public CSharpColorizerFormat() {
@@ -63,12 +56,9 @@ public class CSharpColorizerFormat extends CodeColorizerFormat {
 
   @Override
   public List<Tokenizer> getTokenizers() {
-    return Collections.unmodifiableList(Arrays.asList(new StringTokenizer(
-        "<span class=\"s\">", "</span>"), new XmlDocTokenizer(
-        "<span class=\"cppd\">", "</span>"), new CDocTokenizer(
-        "<span class=\"cd\">", "</span>"), new CppDocTokenizer(
-        "<span class=\"cppd\">", "</span>"), new KeywordsTokenizer(
-        "<span class=\"k\">", "</span>", keywords)));
+    return Collections.unmodifiableList(Arrays.asList(new StringTokenizer("<span class=\"s\">", "</span>"), new XmlDocTokenizer(
+        "<span class=\"cppd\">", "</span>"), new CDocTokenizer("<span class=\"cd\">", "</span>"), new CppDocTokenizer(
+        "<span class=\"cppd\">", "</span>"), new KeywordsTokenizer("<span class=\"k\">", "</span>", keywords)));
   }
 
 }
