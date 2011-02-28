7e2e83954cbfed1226114b29cfe4a92c5cd92b2d
Alexandre Victoor <alexvictoor@gmail.com>
2011-02-28 22:16:33 +0000

    fix indentation issues

--- sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeRule.java
+++ sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeRule.java
@@ -27,66 +27,63 @@ import org.sonar.api.rules.RulePriority;
 
 public class GendarmeRule {
 
-	private RulePriority priority = RulePriority.MAJOR;
-	
-	private String id;
-	
-	private List<RuleProperty> properties = new ArrayList<RuleProperty>();
-	
-	public RulePriority getPriority() {
-  	return priority;
+  private RulePriority priority = RulePriority.MAJOR;
+
+  private String id;
+
+  private List<RuleProperty> properties = new ArrayList<RuleProperty>();
+
+  public RulePriority getPriority() {
+    return priority;
   }
 
   public void setPriority(RulePriority priority) {
-  	this.priority = priority;
+    this.priority = priority;
   }
 
-	public String getId() {
-  	return id;
+  public String getId() {
+    return id;
   }
 
-	public void setId(String id) {
-  	this.id = id;
+  public void setId(String id) {
+    this.id = id;
   }
 
-	public List<RuleProperty> getProperties() {
+  public List<RuleProperty> getProperties() {
     return properties;
   }
 
   public void setProperties(List<RuleProperty> properties) {
     this.properties = properties;
   }
-  
+
   public void addProperty(RuleProperty property) {
     properties.add(property);
   }
-  
 
   @Override
   public int hashCode() {
-	  final int prime = 31;
-	  int result = 1;
-	  result = prime * result + ((id == null) ? 0 : id.hashCode());
-	  return result;
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
   }
 
-	@Override
+  @Override
   public boolean equals(Object obj) {
-	  if (this == obj)
-		  return true;
-	  if (obj == null)
-		  return false;
-	  if (getClass() != obj.getClass())
-		  return false;
-	  GendarmeRule other = (GendarmeRule) obj;
-	  if (id == null) {
-		  if (other.id != null)
-			  return false;
-	  } else if (!id.equals(other.id))
-		  return false;
-	  return true;
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    GendarmeRule other = (GendarmeRule) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if ( !id.equals(other.id))
+      return false;
+    return true;
   }
 
-	
-	
 }
--- sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeRuleParserImpl.java
+++ sonar/sonar-plugin-gendarme/src/main/java/org/sonar/plugin/dotnet/gendarme/GendarmeRuleParserImpl.java
@@ -34,52 +34,51 @@ import org.w3c.dom.Element;
 
 public class GendarmeRuleParserImpl extends AbstractXmlParser implements GendarmeRuleParser {
 
-	
-	/* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.sonar.plugin.dotnet.gendarme.GendarmeRuleParser#parseRuleConfiguration(java.lang.String)
    */
-	public Collection<GendarmeRule> parseRuleConfiguration(String rawConfiguration) {
-		Set<GendarmeRule> rules = parseRuleConfiguration(rawConfiguration, "default", RulePriority.MAJOR);
-		
-		RulePriority[] priorities = RulePriority.values();
-		for (RulePriority rulePriority : priorities) {
-			Set<GendarmeRule> rules4priority = parseRuleConfiguration(rawConfiguration, rulePriority.name().toLowerCase(), rulePriority);
-			rules.removeAll(rules4priority);
-			rules.addAll(rules4priority);
+  public Collection<GendarmeRule> parseRuleConfiguration(String rawConfiguration) {
+    Set<GendarmeRule> rules = parseRuleConfiguration(rawConfiguration, "default", RulePriority.MAJOR);
+
+    RulePriority[] priorities = RulePriority.values();
+    for (RulePriority rulePriority : priorities) {
+      Set<GendarmeRule> rules4priority = parseRuleConfiguration(rawConfiguration, rulePriority.name().toLowerCase(), rulePriority);
+      rules.removeAll(rules4priority);
+      rules.addAll(rules4priority);
     }
-		
-		return rules;
-	}
-	
-	
+
+    return rules;
+  }
+
   private Set<GendarmeRule> parseRuleConfiguration(String rawConfiguration, String ruleSet, RulePriority rulePriority) {
-		
-		Reader reader = new StringReader(rawConfiguration);
-		Set<GendarmeRule> result = new HashSet<GendarmeRule>();
-	  List<Element> ruleElements = extractElements(reader, "/gendarme/ruleset[@name=\""+ruleSet+"\"]/rules");
-	  for (Element element : ruleElements) {
-	    String assemblyName = element.getAttribute("from");
-	    String rawRuleArray = element.getAttribute("include");
-			String[] ruleArray = StringUtils.split(rawRuleArray, '|');
-			for (String ruleName : ruleArray) {
-	      String ruleId = StringUtils.trim(ruleName)+'@'+assemblyName;
-	      GendarmeRule rule = new GendarmeRule();
-	      rule.setId(ruleId);
-	      rule.setPriority(rulePriority);
-	      
-	      List<Element> propElements = 
-	        extractElements(element, "parameter[@rule=\""+StringUtils.trim(ruleName)+"\"]");
-	      
-	      for (Element propElement : propElements) {
+
+    Reader reader = new StringReader(rawConfiguration);
+    Set<GendarmeRule> result = new HashSet<GendarmeRule>();
+    List<Element> ruleElements = extractElements(reader, "/gendarme/ruleset[@name=\"" + ruleSet + "\"]/rules");
+    for (Element element : ruleElements) {
+      String assemblyName = element.getAttribute("from");
+      String rawRuleArray = element.getAttribute("include");
+      String[] ruleArray = StringUtils.split(rawRuleArray, '|');
+      for (String ruleName : ruleArray) {
+        String ruleId = StringUtils.trim(ruleName) + '@' + assemblyName;
+        GendarmeRule rule = new GendarmeRule();
+        rule.setId(ruleId);
+        rule.setPriority(rulePriority);
+
+        List<Element> propElements = extractElements(element, "parameter[@rule=\"" + StringUtils.trim(ruleName) + "\"]");
+
+        for (Element propElement : propElements) {
           String name = evaluateAttribute(propElement, "@property");
           String value = evaluateAttribute(propElement, "@value");
           rule.addProperty(new RuleProperty(name, value));
         }
-	      
-	      result.add(rule);
+
+        result.add(rule);
       }
     }
-		return result;
-	}
-	
+    return result;
+  }
+
 }
