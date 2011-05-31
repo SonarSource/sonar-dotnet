e10e7b3782f28cc9270558f6bb4f805d3b965c54
Alexandre Victoor <alexvictoor@gmail.com>
2011-05-31 20:40:25 +0000

    test clean up

--- maven/dotnet-commons/src/main/java/org/apache/maven/dotnet/commons/project/VisualStudioUtils.java
+++ maven/dotnet-commons/src/main/java/org/apache/maven/dotnet/commons/project/VisualStudioUtils.java
@@ -29,7 +29,6 @@ import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
