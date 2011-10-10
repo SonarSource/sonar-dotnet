27af567aef927c4aed51e60b58012d7b4579e2a8
Alexandre Victoor <alexvictoor@gmail.com>
2011-10-10 21:54:52 +0000

    [SONARPLUGINS-1348] exclusion of WPF generated files

--- sonar/sonar-csharp-core-plugin/src/main/java/org/sonar/plugins/csharp/api/CSharpConstants.java
+++ sonar/sonar-csharp-core-plugin/src/main/java/org/sonar/plugins/csharp/api/CSharpConstants.java
@@ -42,7 +42,7 @@ public final class CSharpConstants {
 
   public static final String CSHARP_CORE_EXECUTED = "C# Core executed";
 
-  public static final String[] DEFAULT_FILES_TO_EXCLUDE = new String[] { "**/Reference.cs", "**/*.designer.cs", "**/*.Designer.cs" };
+  public static final String[] DEFAULT_FILES_TO_EXCLUDE = new String[] { "**/Reference.cs", "**/*.designer.cs", "**/*.Designer.cs", "**/*.g.cs" };
 
   // ----------- Plugin Configuration Properties ----------- //
 
