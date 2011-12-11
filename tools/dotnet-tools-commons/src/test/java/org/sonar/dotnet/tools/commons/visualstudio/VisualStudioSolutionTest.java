fc08324d563d5044ce092d0d984175301b60c49b
Alexandre Victoor <alexvictoor@gmail.com>
2011-12-11 21:43:07 +0000

    [SONARPLUGINS-1459] More tests on VisualStudio model

--- tools/dotnet-tools-commons/src/test/java/org/sonar/dotnet/tools/commons/visualstudio/VisualStudioSolutionTest.java
+++ tools/dotnet-tools-commons/src/test/java/org/sonar/dotnet/tools/commons/visualstudio/VisualStudioSolutionTest.java
@@ -1,11 +1,32 @@
+/*
+ * .NET tools :: Commons
+ * Copyright (C) 2010 Jose Chillan, Alexandre Victoor and SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
 package org.sonar.dotnet.tools.commons.visualstudio;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.util.List;
 
 import org.junit.Test;
+import org.sonar.api.resources.Project;
 import org.sonar.test.TestUtils;
 
 
@@ -77,6 +98,22 @@ public class VisualStudioSolutionTest {
     assertTrue(solution.isSilverlightUsed());
   }
   
+  @Test
+  public void testGetProjectFromSonarProject() throws Exception {
+    VisualStudioSolution solution = ModelFactory.getSolution(TestUtils.getResource(SOLUTION_PATH));
+    Project project = mock(Project.class);
+    when(project.getName()).thenReturn("Example.Application");
+    VisualStudioProject vsProject = solution.getProjectFromSonarProject(project);
+    assertEquals("Example.Application", vsProject.getName());
+  }
   
-
+  @Test
+  public void testGetProjectFromSonarProjectWithBranch() throws Exception {
+    VisualStudioSolution solution = ModelFactory.getSolution(TestUtils.getResource(SOLUTION_PATH));
+    Project project = mock(Project.class);
+    when(project.getName()).thenReturn("Example.Application MyBranch");
+    when(project.getBranch()).thenReturn("MyBranch");
+    VisualStudioProject vsProject = solution.getProjectFromSonarProject(project);
+    assertEquals("Example.Application", vsProject.getName());
+  }
 }
