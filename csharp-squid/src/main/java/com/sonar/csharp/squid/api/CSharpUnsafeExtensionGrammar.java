fc46bfe364f2be5c3caa07c565560942dc1fc26e
Dinesh Bolkensteyn <dinesh.bolkensteyn@sonarsource.com>
2012-02-13 09:00:06 +0000

    C# Reduce the dependency on left-recursion to a minimum on unsafe grammar rules

--- csharp-squid/src/main/java/com/sonar/csharp/squid/api/CSharpUnsafeExtensionGrammar.java
+++ csharp-squid/src/main/java/com/sonar/csharp/squid/api/CSharpUnsafeExtensionGrammar.java
@@ -6,26 +6,27 @@
 package com.sonar.csharp.squid.api;
 
 import com.sonar.sslr.api.LeftRecursiveRule;
+import com.sonar.sslr.api.Rule;
 
 /**
  * Listing of the syntactic elements of the C# grammar extension for unsafe code
  */
 public class CSharpUnsafeExtensionGrammar {
 
-  public LeftRecursiveRule destructorDeclaration; // OK
-  public LeftRecursiveRule unsafeStatement; // OK
+  public Rule destructorDeclaration; // OK
+  public Rule unsafeStatement; // OK
   public LeftRecursiveRule pointerType; // OK
-  public LeftRecursiveRule pointerIndirectionExpression; // OK
-  public LeftRecursiveRule pointerMemberAccess; // OK
-  public LeftRecursiveRule pointerElementAccess; // OK
-  public LeftRecursiveRule addressOfExpression; // OK
-  public LeftRecursiveRule sizeOfExpression; // OK
-  public LeftRecursiveRule fixedStatement; // OK
-  public LeftRecursiveRule fixedPointerDeclarator; // OK
-  public LeftRecursiveRule fixedPointerInitializer; // OK
-  public LeftRecursiveRule fixedSizeBufferDeclaration; // OK
-  public LeftRecursiveRule fixedSizeBufferModifier; // OK
-  public LeftRecursiveRule fixedSizeBufferDeclarator; // OK
-  public LeftRecursiveRule stackallocInitializer; // OK
+  public Rule pointerIndirectionExpression; // OK
+  public Rule pointerMemberAccess; // OK
+  public Rule pointerElementAccess; // OK
+  public Rule addressOfExpression; // OK
+  public Rule sizeOfExpression; // OK
+  public Rule fixedStatement; // OK
+  public Rule fixedPointerDeclarator; // OK
+  public Rule fixedPointerInitializer; // OK
+  public Rule fixedSizeBufferDeclaration; // OK
+  public Rule fixedSizeBufferModifier; // OK
+  public Rule fixedSizeBufferDeclarator; // OK
+  public Rule stackallocInitializer; // OK
 
 }
