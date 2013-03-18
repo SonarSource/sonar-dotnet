d6c4489acc7d17296af0e14f285a5892604307ab
Dinesh Bolkensteyn <dinesh.bolkensteyn@sonarsource.com>
2013-03-18 17:08:03 +0100

    SONARDOTNT-322 Toolkit: Remove the libraries property which was mistakenly copy/pasted from C++

--- sonar/csharp/sonar-csharp-squid/sslr-csharp-toolkit/src/main/java/com/sonar/csharp/toolkit/CSharpConfigurationModel.java
+++ sonar/csharp/sonar-csharp-squid/sslr-csharp-toolkit/src/main/java/com/sonar/csharp/toolkit/CSharpConfigurationModel.java
@@ -20,8 +20,6 @@
 package com.sonar.csharp.toolkit;
 
 import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.sonar.csharp.squid.CSharpConfiguration;
 import com.sonar.csharp.squid.parser.CSharpParser;
@@ -32,10 +30,8 @@ import org.slf4j.LoggerFactory;
 import org.sonar.colorizer.Tokenizer;
 import org.sonar.sslr.toolkit.AbstractConfigurationModel;
 import org.sonar.sslr.toolkit.ConfigurationProperty;
-import org.sonar.sslr.toolkit.ValidationCallback;
 import org.sonar.sslr.toolkit.Validators;
 
-import java.io.File;
 import java.nio.charset.Charset;
 import java.util.List;
 
@@ -44,27 +40,11 @@ public class CSharpConfigurationModel extends AbstractConfigurationModel {
   private static final Logger LOG = LoggerFactory.getLogger(CSharpConfigurationModel.class);
 
   private static final String CHARSET_PROPERTY_KEY = "sonar.sourceEncoding";
-  private static final String LIB_DIRECTORIES = "sonar.cpp.library.directories";
 
   @VisibleForTesting
   ConfigurationProperty charsetProperty = new ConfigurationProperty("Charset", CHARSET_PROPERTY_KEY,
       getPropertyOrDefaultValue(CHARSET_PROPERTY_KEY, "UTF-8"),
       Validators.charsetValidator());
-  @VisibleForTesting
-  ConfigurationProperty libDirectoriesProperty = new ConfigurationProperty("Library directories", LIB_DIRECTORIES,
-      getPropertyOrDefaultValue(LIB_DIRECTORIES, ""),
-      new ValidationCallback() {
-
-        public String validate(String newValueCandidate) {
-          try {
-            toFiles(newValueCandidate);
-            return "";
-          } catch (Exception e) {
-            return e.getMessage();
-          }
-        }
-
-      });
 
   // FIXME SSLR 1.18
   /*
@@ -75,13 +55,7 @@ public class CSharpConfigurationModel extends AbstractConfigurationModel {
    */
 
   public List<ConfigurationProperty> getProperties() {
-    ImmutableList.Builder<ConfigurationProperty> builder = ImmutableList.builder();
-
-    builder.add(
-        charsetProperty,
-        libDirectoriesProperty);
-
-    return builder.build();
+    return ImmutableList.of(charsetProperty);
   }
 
   @Override
@@ -110,19 +84,6 @@ public class CSharpConfigurationModel extends AbstractConfigurationModel {
       LOG.info("The property \"" + propertyKey + "\" is set, using its value \"" + propertyValue + "\".");
       return propertyValue;
     }
-  }
-
-  private List<File> toFiles(String value) {
-    ImmutableList.Builder<File> builder = ImmutableList.builder();
-
-    for (String path : Splitter.on(',').omitEmptyStrings().trimResults().split(value)) {
-      File file = new File(path);
-      Preconditions.checkArgument(file.exists(), "Not exists: " + path);
-      Preconditions.checkArgument(file.isDirectory(), "Not a folder: " + path);
-      builder.add(file);
-    }
-
-    return builder.build();
   }
 
 }
