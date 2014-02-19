399f698d9456d2186d0e228ef63f14f18c08a95b
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-02-19 14:11:23 +0100

    Improve log message

--- sonar/dotnet/sonar-dotnet-plugin/src/main/java/org/sonar/plugins/dotnet/api/microsoft/VisualStudioProject.java
+++ sonar/dotnet/sonar-dotnet-plugin/src/main/java/org/sonar/plugins/dotnet/api/microsoft/VisualStudioProject.java
@@ -201,7 +201,7 @@ public class VisualStudioProject {
     if (assembly.exists()) {
       result.add(assembly);
     } else {
-      LOG.info("Skipping the non generated assembly \"{}\" of project : {}", assembly.getAbsolutePath(), name);
+      LOG.info("Unable to find the assembly \"{}\" of project : {}", assembly.getAbsolutePath(), name);
     }
     return result;
   }
