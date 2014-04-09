1d34a31b79f8d91ac2ad3e12db65da3b38e86a53
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-04-09 20:09:47 +0200

    SONARCS-434 Update description

--- sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/core/CSharpCorePlugin.java
+++ sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/core/CSharpCorePlugin.java
@@ -17,7 +17,6 @@
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
-
 package org.sonar.plugins.csharp.core;
 
 import com.google.common.collect.ImmutableList;
