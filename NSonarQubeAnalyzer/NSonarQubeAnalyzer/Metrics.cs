e663fb40da55ab73dbef92ff20f314336dba384e
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-11-07 16:45:08 +0100

    Compute the comments metric

--- NSonarQubeAnalyzer/NSonarQubeAnalyzer/Metrics.cs
+++ NSonarQubeAnalyzer/NSonarQubeAnalyzer/Metrics.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace NSonarQubeAnalyzer
 {
@@ -20,6 +23,37 @@ namespace NSonarQubeAnalyzer
         public int Lines()
         {
             return tree.GetLineSpan(TextSpan.FromBounds(tree.Length, tree.Length)).EndLinePosition.Line + 1;
+        }
+
+        public ISet<int> Comments()
+        {
+            var builder = ImmutableHashSet.CreateBuilder<int>();
+            foreach (SyntaxTrivia trivia in tree.GetRoot().DescendantTrivia())
+            {
+                if (IsComment(trivia))
+                {
+                    int startLine = tree.GetLineSpan(trivia.FullSpan).StartLinePosition.Line + 1;
+                    int endLine = tree.GetLineSpan(trivia.FullSpan).EndLinePosition.Line + 1;
+                    for (int line = startLine; line <= endLine; line++)
+                    {
+                        builder.Add(line);
+                    }
+                }
+            }
+            return builder.ToImmutable();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="trivia"></param>
+        /// <returns></returns>
+        private static bool IsComment(SyntaxTrivia trivia)
+        {
+            return trivia.IsKind(SyntaxKind.SingleLineCommentTrivia) ||
+                trivia.IsKind(SyntaxKind.MultiLineCommentTrivia) ||
+                trivia.IsKind(SyntaxKind.SingleLineDocumentationCommentTrivia) ||
+                trivia.IsKind(SyntaxKind.MultiLineDocumentationCommentTrivia);
         }
     }
 }
--- NSonarQubeAnalyzer/Tests/MetricsTest.cs
+++ NSonarQubeAnalyzer/Tests/MetricsTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Immutable;
+using FluentAssertions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -18,6 +20,20 @@ namespace Tests
             Assert.AreEqual(2, MetricsFor("\r\n").Lines());
             Assert.AreEqual(2, MetricsFor("\n").Lines());
             Assert.AreEqual(3, MetricsFor("using System;\r\n/*hello\r\nworld*/").Lines());
+        }
+
+        [TestMethod]
+        public void Comments()
+        {
+            MetricsFor("").Comments().Should().BeEmpty();
+            MetricsFor("#ifdef DEBUG\nfoo\n#endif").Comments().Should().BeEmpty();
+            MetricsFor("// l1").Comments().Should().BeEquivalentTo(1);
+            MetricsFor("// l1\n// l2").Comments().Should().BeEquivalentTo(1, 2);
+            MetricsFor("/* l1 */").Comments().Should().BeEquivalentTo(1);
+            MetricsFor("/* l1 \n l2 */").Comments().Should().BeEquivalentTo(1, 2);
+            MetricsFor("/* l1 \n l2 */").Comments().Should().BeEquivalentTo(1, 2);
+            MetricsFor("/// foo").Comments().Should().BeEquivalentTo(1);
+            MetricsFor("/** */").Comments().Should().BeEquivalentTo(1);
         }
 
         private static Metrics MetricsFor(string text)
