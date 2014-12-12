618f6fe4f762e26b66dd45c740bd5527f6ea38b5
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2014-12-12 17:05:44 +0100

    Mark migrated rules

--- csharp-checks/src/main/java/com/sonar/csharp/checks/CheckList.java
+++ csharp-checks/src/main/java/com/sonar/csharp/checks/CheckList.java
@@ -57,8 +57,8 @@ public final class CheckList {
       EmptyNestedBlockCheck.class,
       ParameterAssignedToCheck.class,
       UnusedLocalVariableCheck.class,
-
       CommentedCodeCheck.class,
+
       FunctionComplexityCheck.class,
       CommentRegularExpressionCheck.class);
   }
