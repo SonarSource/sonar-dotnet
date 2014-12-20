dadc1343a5c2ee43483f6c092b70e572e50b3440
Jorge Costa <jorge.costa@tekla.com>
2014-12-20 14:09:32 +0200

    not needed project

--- NSonarQubeAnalyzer/Tests/Diagnostics/FileLinesTest.cs
+++ NSonarQubeAnalyzer/Tests/Diagnostics/FileLinesTest.cs
@@ -14,13 +14,5 @@
             Verifier.Verify(@"TestCases\FileLines12.cs", diagnostic);
             Verifier.Verify(@"TestCases\FileLines13.cs", diagnostic);
         }
-
-        [TestMethod]
-        public void FileLinesWithDefault()
-        {
-            var diagnostic = new FileLines();
-            Verifier.Verify(@"TestCases\FileLines12.cs", diagnostic);
-            Verifier.Verify(@"TestCases\FileLines13.cs", diagnostic);
-        }
     }
 }
