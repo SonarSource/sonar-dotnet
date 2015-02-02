373b68a72462cbc4c3d4ce719df277a1a3b2827e
Dinesh Bolkensteyn <dinesh@dinsoft.net>
2015-02-02 09:11:13 +0100

    Fix copyright of the new C# plugin

--- sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/CSharpPlugin.java
+++ sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/CSharpPlugin.java
@@ -1,6 +1,6 @@
 /*
  * Sonar C# Plugin :: Core
- * Copyright (C) 2010 Jose Chillan, Alexandre Victoor and SonarSource
+ * Copyright (C) 2014 SonarSource
  * dev@sonar.codehaus.org
  *
  * This program is free software; you can redistribute it and/or
