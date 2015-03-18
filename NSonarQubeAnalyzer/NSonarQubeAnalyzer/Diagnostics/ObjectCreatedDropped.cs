439a443cade8d1a59d11dadbc78e26846635121d
tvsonar <tamas.vajk@sonarsource.com>
2015-03-18 11:08:04 +0100

    SONARCS-478 Fixed the error message to contain the related class name

--- NSonarQubeAnalyzer/NSonarQubeAnalyzer/Diagnostics/ObjectCreatedDropped.cs
+++ NSonarQubeAnalyzer/NSonarQubeAnalyzer/Diagnostics/ObjectCreatedDropped.cs
@@ -11,7 +11,7 @@ namespace NSonarQubeAnalyzer.Diagnostics
     {
         internal const string DiagnosticId = "S1848";
         internal const string Description = "Objects should not be created to be dropped immediately without being used";
-        internal const string MessageFormat = "Either remove this useless object instantiation of the class or use it";
+        internal const string MessageFormat = @"Either remove this useless object instantiation of class ""{0}"" or use it";
         internal const string Category = "SonarQube";
         internal const DiagnosticSeverity Severity = DiagnosticSeverity.Warning;
 
@@ -24,10 +24,11 @@ namespace NSonarQubeAnalyzer.Diagnostics
             context.RegisterSyntaxNodeAction(
                 c =>
                 {
+                    var objectCreation = c.Node as ObjectCreationExpressionSyntax;
                     var parent = c.Node.Parent as ExpressionStatementSyntax;
-                    if (parent != null)
+                    if (objectCreation!= null && parent != null)
                     {
-                        c.ReportDiagnostic(Diagnostic.Create(Rule, c.Node.GetLocation()));
+                        c.ReportDiagnostic(Diagnostic.Create(Rule, c.Node.GetLocation(), objectCreation.Type));
                     }
                 },
                 SyntaxKind.ObjectCreationExpression);
