dc406e2ad305323c02d695557c9945357aff1251
tvsonar <tamas.vajk@sonarsource.com>
2015-03-19 12:45:30 +0100

    SONARCS-474 Added test for 0 analyzer case

---
+++ NSonarQubeAnalyzer/Tests/DiagnosticRunnerTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using NSonarQubeAnalyzer;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Tests
+{
+    [TestClass]
+    public class DiagnosticRunnerTest
+    {
+        [TestMethod]
+        public void DiagnosticRunnerTest_NoAnalyzer()
+        {
+            var runner = new DiagnosticsRunner(ImmutableArray.Create<DiagnosticAnalyzer>());
+            var syntaxTree = CSharpSyntaxTree.ParseText("");
+
+            var diagnosticsResult = runner.GetDiagnostics(syntaxTree);
+
+            diagnosticsResult.Count().ShouldBeEquivalentTo(0);
+        }
+    }
+}
