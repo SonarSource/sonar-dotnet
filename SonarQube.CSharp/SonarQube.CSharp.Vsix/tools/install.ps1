e3bf2e9bb5ad7a6412f11b5702d42fc857be346a
unknown <tamas.vajk@sonarsource.com>
2015-04-02 18:17:15 +0200

    SONARCS-498 Added nuget package building to post-build action

---
+++ SonarQube.CSharp/SonarQube.CSharp.Vsix/tools/install.ps1
@@ -0,0 +1,18 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$analyzersPath = join-path $toolsPath "analyzers"
+
+# Install the language agnostic analyzers.
+foreach ($analyzerFilePath in Get-ChildItem $analyzersPath)
+{
+    $project.Object.AnalyzerReferences.Add($analyzerFilePath.FullName)
+}
+
+# Install language specific analyzers.
+# $project.Type gives the language name like (C# or VB.NET)
+$languageAnalyzersPath = join-path $analyzersPath $project.Type
+
+foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath)
+{
+    $project.Object.AnalyzerReferences.Add($analyzerFilePath.FullName)
+}
---
+++ SonarQube.CSharp/SonarQube.CSharp.Vsix/tools/uninstall.ps1
@@ -0,0 +1,18 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+# Uninstall the language agnostic analyzers.
+$analyzersPath = join-path $toolsPath "analyzers"
+
+foreach ($analyzerFilePath in Get-ChildItem $analyzersPath)
+{
+    $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+}
+
+# Uninstall language specific analyzers.
+# $project.Type gives the language name like (C# or VB.NET)
+$languageAnalyzersPath = join-path $analyzersPath $project.Type
+
+foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath)
+{
+    $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+}
