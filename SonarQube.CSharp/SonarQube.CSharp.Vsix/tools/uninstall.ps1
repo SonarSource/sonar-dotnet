887475567073c8f3a11bd8f1da8cd9186bc467aa
unknown <tamas.vajk@sonarsource.com>
2015-04-08 10:31:31 +0200

    SONARCS-498 Fixed Nuget install script and added resources for the extension

--- SonarQube.CSharp/SonarQube.CSharp.Vsix/tools/uninstall.ps1
+++ SonarQube.CSharp/SonarQube.CSharp.Vsix/tools/uninstall.ps1
@@ -2,17 +2,5 @@
 
 # Uninstall the language agnostic analyzers.
 $analyzersPath = join-path $toolsPath "analyzers"
-
-foreach ($analyzerFilePath in Get-ChildItem $analyzersPath)
-{
-    $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
-}
-
-# Uninstall language specific analyzers.
-# $project.Type gives the language name like (C# or VB.NET)
-$languageAnalyzersPath = join-path $analyzersPath $project.Type
-
-foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath)
-{
-    $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
-}
+$analyzerFilePath = join-path $analyzersPath "SonarQube.Analyzers.dll"
+$project.Object.AnalyzerReferences.Remove($analyzerFilePath)
