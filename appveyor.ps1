4304fe77408c6bab1b945995b17211de93cf7883
Julien HENRY <julien.henry@sonarsource.com>
2016-10-12 12:40:07 +0200

    Use Appveyor for Dory analysis on deployment on repox + QA

--- appveyor.ps1
+++ appveyor.ps1
@@ -1,62 +1,23 @@
 $ErrorActionPreference = "Stop"
 
-function FetchAndUnzip
-{
-	param ([string]$Url, [string]$Out)
-
-	$tmp = [System.IO.Path]::GetTempFileName()
-	[System.Reflection.Assembly]::LoadWithPartialName('System.Net.Http') | Out-Null
-	$client = (New-Object System.Net.Http.HttpClient)
-	try
-	{
-		if (-not([string]::IsNullOrEmpty($env:GITHUB_TOKEN)))
-		{
-			$credentials = [string]::Format([System.Globalization.CultureInfo]::InvariantCulture, "{0}:", $env:GITHUB_TOKEN);
-			$credentials = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($credentials));
-			$client.DefaultRequestHeaders.Authorization = (New-Object System.Net.Http.Headers.AuthenticationHeaderValue("Basic", $credentials));
-		}
-		$contents = $client.GetByteArrayAsync($url).Result;
-		[System.IO.File]::WriteAllBytes($tmp, $contents);
-	}
-	finally
-	{
-		$client.Dispose()
-	}
-
-	if (-not(Test-Path $Out))
-	{
-		mkdir $Out | Out-Null
-	}
-	[System.Reflection.Assembly]::LoadWithPartialName('System.IO.Compression.FileSystem') | Out-Null
-	[System.IO.Compression.ZipFile]::ExtractToDirectory($tmp, $Out)
-}
+$PSVersionTable.PSVersion
 
 function InstallAppveyorTools
 {
-	$travisUtilsVersion = "28"
+	$travisUtilsVersion = "33"
 	$localPath = "$env:USERPROFILE\.local"
-	$travisUtilsPath = "$localPath\travis-utils-$travisUtilsVersion"
-	if (Test-Path $travisUtilsPath)
-	{
-		echo "Reusing the Travis Utils version $travisUtilsVersion already downloaded under $travisUtilsPath"
-	}
-	else
-	{
-		$url = "https://github.com/SonarSource/travis-utils/archive/v$travisUtilsVersion.zip"
-		echo "Downloading Travis Utils version $travisUtilsVersion from $url into $localPath"
-		FetchAndUnzip $url $localPath
-	}
-
+	$tmp = [IO.Path]::GetTempFileName() | Rename-Item -NewName { [IO.Path]::ChangeExtension($_,".tmp.zip") } -PassThru
+	wget "https://github.com/SonarSource/travis-utils/archive/v$travisUtilsVersion.zip" -OutFile $tmp
+	
+	Expand-Archive -Force -Path $tmp -DestinationPath $localPath
+    
 	$mavenLocal = "$env:USERPROFILE\.m2"
 	if (-not(Test-Path $mavenLocal))
 	{
 		mkdir $mavenLocal | Out-Null
 	}
 	echo "Installating Travis Utils public Maven settings.xml into $mavenLocal"
-	Copy-Item "$travisUtilsPath\m2\settings-public.xml" "$mavenLocal\settings.xml"
-
-	$env:ORCHESTRATOR_CONFIG_URL = ""
-	$env:TRAVIS = "ORCH-332"
+	Copy-Item "$localPath\travis-utils-$travisUtilsVersion\m2\settings-public.xml" "$mavenLocal\settings.xml"
 }
 
 function CheckLastExitCode
@@ -75,33 +36,115 @@ CALLSTACK:$(Get-PSCallStack | Out-String)
 
 InstallAppveyorTools
 
-switch ($env:TEST)
+function maven_expression
 {
-	"CI"
-	{
-		mvn verify "--batch-mode" "-B" "-e" "-V"
-		CheckLastExitCode
-	}
+  param ([string]$exp)
+  
+  $out = mvn help:evaluate -B -Dexpression="$exp"
+  CheckLastExitCode
+  $m = ($out | Select-String -NotMatch -Pattern '^\[|Download\w\+\:')
+  $m.Line
+}
 
-	"PLUGIN"
-	{
-		mvn package "-PnoVersionNumberInJar" "--batch-mode" "-Dsource.skip=true" "-Denforcer.skip=true" "-Danimal.sniffer.skip=true" "-Dmaven.test.skip=true"
-		CheckLastExitCode
+function set_maven_build_version
+{
+  param ([string]$build_id)
+  $CURRENT_VERSION=maven_expression "project.version"
+  $RELEASE_VERSION=$CURRENT_VERSION.Split("-").Get(0)
 
-		pushd its/plugin
-		try
-		{
-			mvn test "--batch-mode" "-DcsharpVersion=""DEV""" "-Dsonar.runtimeVersion=""$env:SQ_VERSION""" "-Dmaven.test.redirectTestOutputToFile=false" "-Dsonar.jdbc.dialect=embedded" "-Dorchestrator.updateCenterUrl=http://update.sonarsource.org/update-center-dev.properties" "-Dmaven.localRepository=$env:USERPROFILE\.m2\repository"
-			CheckLastExitCode
-		}
-		finally
-		{
-			popd
-		}
-	}
+# In case of 2 digits, we need to add the 3rd digit (0 obviously)
+# Mandatory in order to compare versions (patch VS non patch)
+  $DIGIT_COUNT=$RELEASE_VERSION.Split(".").Count
+  if ( $DIGIT_COUNT -lt 3 )
+  {
+    $RELEASE_VERSION="$RELEASE_VERSION.0"
+  }
+  $NEW_VERSION="$RELEASE_VERSION.$build_id"
 
-	default
-	{
-		throw "Unexpected TEST mode: $env:TEST"
-	}
+  echo "Replacing version $CURRENT_VERSION with $NEW_VERSION"
+
+  mvn org.codehaus.mojo:versions-maven-plugin:2.2:set "-DnewVersion=$NEW_VERSION" -DgenerateBackupPoms=false -B -e
+  CheckLastExitCode
+
+  $env:PROJECT_VERSION=$NEW_VERSION
+}
+
+$env:DEPLOY_PULL_REQUEST="true"
+
+# Waiting for a fix in parent pom:
+$env:BUILD_ID=$env:APPVEYOR_BUILD_NUMBER
+$env:CI_BUILD_URL="https://ci.appveyor.com/project/SonarSource/sonar-csharp/build/$env:APPVEYOR_BUILD_VERSION"
+$env:GIT_BRANCH=$env:APPVEYOR_REPO_BRANCH
+$env:GIT_COMMIT=$env:APPVEYOR_REPO_COMMIT
+
+if ($env:APPVEYOR_REPO_BRANCH -eq 'master' -and $env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
+{
+  echo "======= Build, deploy and analyze master"
+  
+  $CURRENT_VERSION=maven_expression "project.version"
+  set_maven_build_version $env:APPVEYOR_BUILD_NUMBER
+  
+  $env:MAVEN_OPTS="-Xmx1536m -Xms128m"
+  
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar `
+      "-Pcoverage-per-test,deploy-sonarsource,release" `
+      "-Dmaven.test.redirectTestOutputToFile=false" `
+      "-Dsonar.host.url=$env:SONAR_HOST_URL" `
+      "-Dsonar.login=$env:SONAR_TOKEN" `
+      "-Dsonar.projectVersion=$CURRENT_VERSION" `
+      -B -e -V
+  CheckLastExitCode
+}
+elseif ($env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null -and $env:GITHUB_TOKEN -ne $null)
+{
+  echo '======= Build and analyze pull request'
+  
+  # Do not deploy a SNAPSHOT version but the release version related to this build and PR
+  set_maven_build_version $env:APPVEYOR_BUILD_NUMBER
+
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  $env:MAVEN_OPTS="-Xmx1G -Xms128m"
+  if ($env:DEPLOY_PULL_REQUEST -eq "true")
+  {
+    echo '======= with deploy'
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar `
+      -Pdeploy-sonarsource `
+      "-Dmaven.test.redirectTestOutputToFile=false" `
+      "-Dsonar.analysis.mode=issues" `
+      "-Dsonar.github.pullRequest=$env:APPVEYOR_PULL_REQUEST_NUMBER" `
+      "-Dsonar.github.repository=$env:APPVEYOR_REPO_NAME" `
+      "-Dsonar.github.oauth=$env:GITHUB_TOKEN" `
+      "-Dsonar.host.url=$env:SONAR_HOST_URL" `
+      "-Dsonar.login=$env:SONAR_TOKEN" `
+      -B -e -V
+    CheckLastExitCode
+  }
+  else
+  {
+    echo '======= no deploy'
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify sonar:sonar `
+      "-Dmaven.test.redirectTestOutputToFile=false" `
+      "-Dsonar.analysis.mode=issues" `
+      "-Dsonar.github.pullRequest=$env:APPVEYOR_PULL_REQUEST_NUMBER" `
+      "-Dsonar.github.repository=$env:APPVEYOR_REPO_NAME" `
+      "-Dsonar.github.oauth=$env:GITHUB_TOKEN" `
+      "-Dsonar.host.url=$env:SONAR_HOST_URL" `
+      "-Dsonar.login=$env:SONAR_TOKEN" `
+      -B -e -V
+    CheckLastExitCode
+  }
+}
+else
+{
+  echo '======= Build, no analysis, no deploy'
+
+  # No need for Maven phase "install" as the generated JAR files do not need to be installed
+  # in Maven local repository. Phase "verify" is enough.
+
+  mvn verify `
+      "-Dmaven.test.redirectTestOutputToFile=false" `
+      -B -e -V
+  CheckLastExitCode
 }
