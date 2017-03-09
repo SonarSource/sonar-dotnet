3dbc0566e13c7455b11d6270257a6713e7a5eaa2
Tom <thomas.verin@sonarsource.com>
2017-03-09 10:37:19 +0100

    update license headers

--- sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/protobuf/EncodingImporterTest.java
+++ sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/protobuf/EncodingImporterTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarSource :: .NET :: Shared library
- * Copyright (C) 2016-2017 SonarSource SA
+ * Copyright (C) 2014-2017 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
