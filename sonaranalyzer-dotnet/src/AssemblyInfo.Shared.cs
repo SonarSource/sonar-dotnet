5586da4290adb353b6489ebae7a5196b1fdf20e4
Amaury Levé <amaury.leve@sonarsource.com>
2017-03-16 15:21:17 +0100

    Update sonaranalyzer version to 5.9

--- sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
+++ sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
@@ -21,9 +21,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.24.0")]
-[assembly: AssemblyFileVersion("1.24.0.0")]
-[assembly: AssemblyInformationalVersion("Version:1.24.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("5.9.0")]
+[assembly: AssemblyFileVersion("5.9.0.0")]
+[assembly: AssemblyInformationalVersion("Version:5.9.0.0 Branch:not-set Sha1:not-set")]
 
 [assembly: AssemblyConfiguration("")]
 
