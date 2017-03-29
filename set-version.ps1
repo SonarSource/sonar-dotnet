8ff3dd81e484d36d9512bff7db3287be66f2cc4a
Amaury Levé <amaury.leve@sonarsource.com>
2017-03-29 11:10:21 +0200

    Adds a version script to increase java and .net files

---
+++ set-version.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True, Position=1)]
+    [string]$version
+)
+
+function UpdateJavaFiles {
+    Write-Output "Update version in Java files"
+
+    $fixedVersion = $version
+    If ($fixedVersion.Split('.').Count -eq 3 -and $fixedVersion.EndsWith(".0")) {
+        $fixedVersion = $version.Substring(0, $version.Length - 2)
+    }
+    mvn org.codehaus.mojo:versions-maven-plugin:2.2:set "-DnewVersion=${fixedVersion}-SNAPSHOT" -DgenerateBackupPoms=false -B -e
+}
+
+function UpdateDotNetFiles {
+    Write-Output "Update version in .Net files"
+
+    Push-Location ".\sonaranalyzer-dotnet\build"
+    $versionPropsFile = "Version.props"
+    $xml = [xml](Get-Content $versionPropsFile)
+    $xml.Project.PropertyGroup.MainVersion = ${version}
+    $xml.Save($versionPropsFile)
+
+    msbuild "ChangeVersion.proj"
+    Pop-Location
+}
+
+function ValidateVersionFormat {
+    If ($version -notmatch "\d{1,3}\.\d{1,3}(\.\d{1,3})?") {
+        Write-Error "Invalid version format."
+    }
+}
+
+ValidateVersionFormat
+UpdateJavaFiles
+UpdateDotNetFiles
