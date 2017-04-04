d9c749831add612c98528f7bc486287e47a47289
Valeri Hristov <valeri.hristov@sonarsource.com>
2017-04-04 15:53:03 +0200

    Replaced build scripts with reusable implementation, added unit test execution & code coverage generation

--- build.ps1
+++ build.ps1
@@ -46,7 +46,17 @@ $env:DEPLOY_PULL_REQUEST="true"
 
 #build sonaranalyzer
 cd sonaranalyzer-dotnet
-& .\build\build.ps1
+& .\build\Build.ps1 `
+  -analyze `
+  -test `
+  -package `
+  -githubRepo $env:GITHUB_REPO `
+  -githubToken $env:GITHUB_TOKEN `
+  -githubPullRequest $env:PULL_REQUEST `
+  -isPullRequest $env:IS_PULLREQUEST `
+  -sonarQubeUrl $env:SONAR_HOST_URL `
+  -sonarQubeToken $env:SONAR_TOKEN `
+  -certificatePath $env:CERT_PATH
 testExitCode
 cd ..
 
