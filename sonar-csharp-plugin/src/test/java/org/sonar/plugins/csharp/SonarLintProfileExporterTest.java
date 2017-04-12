cb7f1ad625788c92645dc31caa79526bfc10adf4
Amaury Levé <amaury.leve@sonarsource.com>
2017-04-12 15:31:23 +0200

    Undo edit of a file not linked to the commit

--- sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/SonarLintProfileExporterTest.java
+++ sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/SonarLintProfileExporterTest.java
@@ -19,8 +19,10 @@
  */
 package org.sonar.plugins.csharp;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.io.StringWriter;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import org.junit.Test;
