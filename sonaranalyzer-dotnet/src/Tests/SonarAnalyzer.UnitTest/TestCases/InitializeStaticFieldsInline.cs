92ace9a855f74f8ccb4e233b0d76742b76f24de6
Amaury Levé <amaury.leve@sonarsource.com>
2017-04-18 16:25:59 +0200

    Rule S3963: 'static fields' should be initialized inline

---
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/InitializeStaticFieldsInline.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Resources;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        static int i;
+        static string s;
+
+        static Program() // Noncompliant
+        {
+            i = 3;
+            ResourceManager sm = new ResourceManager("strings", Assembly.GetExecutingAssembly());
+            s = sm.GetString("mystring");
+        }
+    }
+}
