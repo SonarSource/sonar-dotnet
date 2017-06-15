9a1d74d559d6846d9fca17613d9764144e1f7dd8
Amaury Levé <amaury.leve@sonarsource.com>
2017-06-15 14:30:46 +0200

    Bump version to 6.1

--- sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
+++ sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
@@ -21,9 +21,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("6.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
-[assembly: AssemblyInformationalVersion("Version:6.0.0.0 Branch:not-set Sha1:not-set")]
+[assembly: AssemblyVersion("6.1")]
+[assembly: AssemblyFileVersion("6.1.0")]
+[assembly: AssemblyInformationalVersion("Version:6.1.0 Branch:not-set Sha1:not-set")]
 
 [assembly: AssemblyConfiguration("")]
 
