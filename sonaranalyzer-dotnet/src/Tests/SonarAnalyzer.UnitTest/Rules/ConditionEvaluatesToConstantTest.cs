c719fd5b0a7497c6c656bacda933f05ded919d9b
Amaury Levé <amaury.leve@sonarsource.com>
2017-06-28 10:34:15 +0200

    Extract nullable test cases into unit tests

--- sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/Rules/ConditionEvaluatesToConstantTest.cs
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/Rules/ConditionEvaluatesToConstantTest.cs
@@ -34,5 +34,219 @@ namespace SonarAnalyzer.UnitTest.Rules
             Verifier.VerifyAnalyzer(@"TestCases\ConditionEvaluatesToConstant.cs", new ConditionEvaluatesToConstant(),
                 new CSharpParseOptions(LanguageVersion.CSharp6));
         }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case1()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case1()
+    {
+        bool? b = true;
+        if (b == true) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+        {
+
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case2()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case2()
+    {
+        bool? b = true;
+        if (b == false) // Noncompliant {{Change this condition so that it does not always evaluate to 'false'; some subsequent code is never executed.}}
+        { // Secondary
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case3()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case3(bool? b)
+    {
+        if (b == null)
+        {
+            if (null == b) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+            {
+                b.ToString();
+            }
+        }
+        else
+        {
+            if (b != null) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+            {
+                b.ToString();
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case4()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case4(bool? b)
+    {
+        if (b == true)
+        {
+            if (true == b) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+            {
+                b.ToString();
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case5()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case5(bool? b)
+    {
+        if (b == true)
+        {
+        }
+        else if (b == false)
+        {
+        }
+        else // Compliant
+        {
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case6()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case6(bool? b)
+    {
+        if (b == null)
+        {
+        }
+        else if (b == true)
+        {
+        }
+        else
+        {
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case7()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case7(bool? b)
+    {
+        if (b == null)
+        {
+            if (b ?? false) // Noncompliant {{Change this condition so that it does not always evaluate to 'false'; some subsequent code is never executed.}}
+            { // Secondary
+
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case8()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case8(bool? b)
+    {
+        if (b != null)
+        {
+            if (b.HasValue) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+            {
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case9()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case9(bool? b)
+    {
+        if (b == true)
+        {
+            var x = b.Value;
+            if (x == true) // Noncompliant {{Change this condition so that it does not always evaluate to 'true'.}}
+            {
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void Case10()
+        {
+            Verifier.VerifyCSharpAnalyzer(@"class Foo
+{
+    void Case10(int? i)
+    {
+        if (i == null)
+        {
+            if (i.HasValue) // Noncompliant {{Change this condition so that it does not always evaluate to 'false'; some subsequent code is never executed.}}
+            { // Secondary
+            }
+        }
+    }
+}",
+                new ConditionEvaluatesToConstant(),
+                new CSharpParseOptions(LanguageVersion.CSharp6));
+        }
     }
 }
