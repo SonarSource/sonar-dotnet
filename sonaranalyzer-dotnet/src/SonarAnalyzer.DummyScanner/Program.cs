7a955204c6a8d02a0cb34ac2b1facb7da38b0996
Michal Barczyk <michal.barczyk@sonarsource.com>
2017-06-29 16:11:35 +0200

    Copyright header + fix for sonar warning.

--- sonaranalyzer-dotnet/src/SonarAnalyzer.DummyScanner/Program.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.DummyScanner/Program.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2017 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
 
 namespace DummyScanner
 {
-    class Program
+    public static class Program
     {
-        static int Main(string[] args)
+        public static int Main(string[] args)
         {
             Console.Error.WriteLine("This version of scanner is deprecated. Upgrade to more recent version or downgrade SonarC# to pre-6.0 version.");
             return 1;
