651d0fe7bbe950a434560eb2a42561ca025e616c
Michal Barczyk <michal.barczyk@sonarsource.com>
2017-06-29 15:58:23 +0200

    Provide better error message when using old version of Scanner for MSBuild

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.DummyScanner/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DummyScanner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Error.WriteLine("This version of scanner is deprecated. Upgrade to more recent version or downgrade SonarC# to pre-6.0 version.");
+            return 1;
+        }
+    }
+}
