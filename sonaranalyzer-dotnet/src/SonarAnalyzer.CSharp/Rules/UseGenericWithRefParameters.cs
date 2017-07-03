bd263573091a44e0764d5803bb70c13fe3c74053
Michal Barczyk <michal.barczyk@sonarsource.com>
2017-07-03 10:11:04 +0200

    Rule S4047: Generics should be used when appropriate

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/UseGenericWithRefParameters.cs
@@ -0,0 +1,79 @@
+/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2017 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class UseGenericWithRefParameters : SonarDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S4047";
+        private const string MessageFormat = "Make this method generic and replace the 'object' parameter with a type parameter.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(c =>
+            {
+                var methodDeclaration = (MethodDeclarationSyntax)c.Node;
+                var methodSymbol = c.SemanticModel.GetDeclaredSymbol(methodDeclaration);
+                var methodIdentifierLocation = methodDeclaration.FindIdentifierLocation();
+
+                if (methodSymbol == null ||
+                    methodIdentifierLocation == null)
+                {
+                    return;
+                }
+
+                var refObjectParameters = methodSymbol
+                    .GetParameters()
+                    .Where(IsRefObject)
+                    .ToList();
+
+                if (refObjectParameters.Count > 0)
+                {
+                    var parameterLocations = refObjectParameters.Select(p => p.Locations.FirstOrDefault()).WhereNotNull();
+                    c.ReportDiagnostic(Diagnostic.Create(rule, methodIdentifierLocation,
+                        additionalLocations: parameterLocations));
+                }
+
+            },
+            SyntaxKind.MethodDeclaration);
+        }
+
+        private static bool IsRefObject(IParameterSymbol parameter)
+        {
+            return parameter.RefKind == RefKind.Ref &&
+                   parameter.Type.Is(KnownType.System_Object);
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/UseGenericWithRefParameters.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Tests.Diagnostics
+{
+    public class Foo
+    {
+        public void Bar(ref object o1, ref object o2)
+//                  ^^^ Noncompliant {{Make this method generic and replace the 'object' parameter with a type parameter.}}
+//                                 ^^ Secondary@-1
+//                                                ^^ Secondary@-2
+        {
+        }
+
+        public void Bar2<T>(ref T ref1, ref T ref2)
+        {
+        }
+
+        public void Bar3(out object o1,
+                         ref Foo o2,
+                         ref object[] o3)
+        {
+        }
+
+        public void (ref object o1)
+        {
+        }
+    }
+}
