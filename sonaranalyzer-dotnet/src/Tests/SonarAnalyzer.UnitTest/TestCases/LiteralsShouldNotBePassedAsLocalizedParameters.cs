a76f47ff9b466ce29b55fea45344a26729677dc2
Valeri Hristov <valeri.hristov@sonarsource.com>
2017-07-07 16:40:53 +0200

    Rule S4055: Literals should not be passed as localized parameters

---
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/LiteralsShouldNotBePassedAsLocalizedParameters.cs
@@ -0,0 +1,66 @@
+using System;
+using System.ComponentModel;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        [Localizable(true)]
+        public string Property1 { get; set; }
+        [Localizable(false)]
+        public string Property2 { get; set; }
+        public string Message { get; set; }
+        public string Text { get; set; }
+        public string Caption { get; set; }
+        public string OtherText { get; set; }
+
+        public void Foo()
+        {
+            Bar("param1", // Noncompliant {{Replace this string literal with a string retrieved through an instance of the 'ResourceManager' class.}}
+                "param2", // Noncompliant
+                "param3", // Noncompliant
+                "param4"); // Noncompliant
+
+            Baz("param1",
+                "param2",
+                "param3",
+                "param4");
+
+            Console.WriteLine("some text"); // Noncompliant
+            Console.Write("some more text"); // Noncompliant
+            Console.WriteLine("the format", "a"); // Noncompliant
+            Console.Write("the format", "a"); // Noncompliant
+
+            Property1 = "some text"; // Noncompliant
+            Property2 = "moar text";
+            Message = "message text"; // Noncompliant
+            Text = "textual text"; // Noncompliant
+            Caption = "caption text"; // Noncompliant
+            OtherText = "other text";
+        }
+
+        public void Bar([Localizable(true)]string param1, string message, string text, string caption)
+        {
+        }
+
+        public void Baz([Localizable(false)]string param1, string myMessage, string otherText, string captionString)
+        {
+        }
+    }
+
+    class InvalidProgram
+    {
+        public void Foo()
+        {
+            Bar("some string", "other string", "third string"); // Compliant, this cannot be compiled
+            Bar("some string"); // Compliant, this cannot be compiled
+            Bar(); // Compliant
+            Console.Write(); // Compliant
+            Console.WriteLine(); // Compliant
+        }
+
+        public void Bar(string text, string message)
+        {
+        }
+    }
+}
