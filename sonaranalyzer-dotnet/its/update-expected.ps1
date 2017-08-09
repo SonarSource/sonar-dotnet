338ccf5a435826063078ffc8c95bf52c6b4b41bd
Amaury Levé <amaury.leve@sonarsource.com>
2017-08-09 17:50:21 +0200

    Make update-expected.ps1 work from other directories

--- sonaranalyzer-dotnet/its/update-expected.ps1
+++ sonaranalyzer-dotnet/its/update-expected.ps1
@@ -1,3 +1,15 @@
-Remove-Item .\expected -Recurse -Force
-Rename-Item .\actual .\expected
-Remove-Item .\expected\.git -Recurse -Force
+Set-StrictMode -version 2.0
+$ErrorActionPreference = "Stop"
+
+. (Join-Path $PSScriptRoot "..\..\scripts\build\build-utils.ps1")
+
+try {
+    Push-Location $PSScriptRoot
+
+    Remove-Item .\expected -Recurse -Force
+    Rename-Item .\actual .\expected
+    Remove-Item .\expected\.git -Recurse -Force
+}
+finally {
+    Pop-Location
+}
