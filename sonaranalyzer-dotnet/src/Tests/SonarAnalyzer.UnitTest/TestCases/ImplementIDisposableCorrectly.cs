a4bbadef5cd8678500fad216510eeff38de8a907
michalb-sonar <michal.barczyk@sonarsource.com>
2017-08-24 15:58:18 +0200

    Update S3881: Correct documentation and add more unit tests (#714)

--- sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/ImplementIDisposableCorrectly.cs
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/ImplementIDisposableCorrectly.cs
@@ -155,3 +155,104 @@ namespace Tests.Diagnostics
     {
     }
 }
+
+namespace Rspec_Compliant_Samples
+{
+    // Sealed class
+    public sealed class Foo1 : IDisposable
+    {
+        public void Dispose()
+        {
+            // Cleanup
+        }
+    }
+
+    // Simple implementation
+    public class Foo2 : IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+        }
+    }
+
+    // Implementation with a finalizer
+    public class Foo3 : IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+        }
+
+        ~Foo3()
+        {
+            Dispose(false);
+        }
+    }
+
+    // Base disposable class
+    public class Foo4 : DisposableBase
+    {
+        protected override void Dispose(bool disposing)
+        {
+            // Cleanup
+            // Do not forget to call base
+            base.Dispose(disposing);
+        }
+    }
+}
+
+namespace VS_Generated_Implementation
+{
+    // NOTE: this is not compliant
+    public class Foo3 : IDisposable // Noncompliant
+    {
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Foo3() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose() // Secondary
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
