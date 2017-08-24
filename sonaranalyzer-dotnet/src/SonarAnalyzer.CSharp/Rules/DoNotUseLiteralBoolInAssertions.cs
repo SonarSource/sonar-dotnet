1ebbe53fcce5583005e374a601c6d0ec072cbb9b
Michal Barczyk <michal.barczyk@sonarsource.com>
2017-08-24 11:03:35 +0200

    Rule S2701: Literal boolean values should not be used in assertions

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/DoNotUseLiteralBoolInAssertions.cs
@@ -0,0 +1,121 @@
+/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2017 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class DoNotUseLiteralBoolInAssertions : SonarDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S2701";
+        private const string MessageFormat = "Remove or correct this assertion.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        private static Dictionary<KnownType, HashSet<string>> trackedTypeAndMethods =
+            new Dictionary<KnownType, HashSet<string>>
+            {
+                [KnownType.Xunit_Assert] = new HashSet<string>
+                {
+                    "Equal", "Equals", "False", "NotEqual", "Same", "StrictEqual", "NotSame"
+                },
+
+                [KnownType.Microsoft_VisualStudio_TestTools_UnitTesting_Assert] = new HashSet<string>
+                {
+                    "AreEqual", "AreNotEqual", "AreSame", "IsFalse", "IsTrue"
+                },
+
+                [KnownType.NUnit_Framework_Assert] = new HashSet<string>
+                {
+                    "AreEqual", "AreNotEqual", "AreNotSame", "AreSame", "False",
+                    "IsFalse", "IsTrue", "That", "True"
+                },
+
+                [KnownType.System_Diagnostics_Debug] = new HashSet<string>
+                {
+                    "Assert"
+                }
+            };
+
+        private static List<SyntaxKind> boolLiterals =
+            new List<SyntaxKind>
+            {
+                SyntaxKind.TrueLiteralExpression,
+                SyntaxKind.FalseLiteralExpression
+            };
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
+                {
+                    var invocation = (InvocationExpressionSyntax)c.Node;
+
+                    var symbolInfo = c.SemanticModel.GetSymbolInfo(invocation);
+                    var methodSymbol = symbolInfo.Symbol ?? symbolInfo.CandidateSymbols.FirstOrDefault();
+
+                    if (methodSymbol != null &&
+                        invocation.ArgumentList != null &&
+                        IsTrackedMethod(methodSymbol) &&
+                        IsFirstOrSecondArgumentABoolLiteral(invocation.ArgumentList.Arguments))
+                    {
+                        c.ReportDiagnostic(Diagnostic.Create(rule, invocation.GetLocation()));
+                    }
+                },
+                SyntaxKind.InvocationExpression);
+        }
+
+        private static bool IsFirstOrSecondArgumentABoolLiteral(
+            SeparatedSyntaxList<ArgumentSyntax> arguments)
+        {
+            switch (arguments.Count)
+            {
+                case 0:  return false;
+                case 1:  return IsBooleanLiteral(arguments[0]);
+                default: return IsBooleanLiteral(arguments[0]) || IsBooleanLiteral(arguments[1]);
+            }
+        }
+
+        private static bool IsBooleanLiteral(ArgumentSyntax argument)
+        {
+            return argument.Expression is LiteralExpressionSyntax &&
+                   argument.Expression.IsAnyKind(boolLiterals);
+        }
+
+        private static bool IsTrackedMethod(ISymbol methodSymbol)
+        {
+            return trackedTypeAndMethods
+                .Where(kvp => methodSymbol.ContainingType.Is(kvp.Key))
+                .Any(kvp => kvp.Value.Contains(methodSymbol.Name));
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/DoNotUseLiteralBoolInAssertions.cs
@@ -0,0 +1,57 @@
+using System;
+
+namespace Tests.Diagnostics
+{
+    class Foo
+    {
+        public void Test()
+        {
+            bool b;
+
+            System.Diagnostics.Debug.Assert(false); // Noncompliant
+            System.Diagnostics.Debug.Assert(true); // Noncompliant {{Remove or correct this assertion.}}
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+            NUnit.Framework.Assert.AreEqual(true, b); // Noncompliant
+            NUnit.Framework.Assert.AreNotEqual(true, b); // Noncompliant
+            NUnit.Framework.Assert.AreNotSame(true, b); // Noncompliant
+            NUnit.Framework.Assert.AreSame(true, b); // Noncompliant
+            NUnit.Framework.Assert.False(true, b); // Noncompliant
+            NUnit.Framework.Assert.IsFalse(true, b); // Noncompliant
+            NUnit.Framework.Assert.IsTrue(true, b); // Noncompliant
+            NUnit.Framework.Assert.That(true, b); // Noncompliant
+            NUnit.Framework.Assert.True(true, b); // Noncompliant
+            NUnit.Framework.Assert.AreEqual(false, b); // Noncompliant
+            NUnit.Framework.Assert.AreEqual(b, false); // Noncompliant
+            NUnit.Framework.Assert.AreEqual(true, false); // Noncompliant
+
+            Xunit.Assert.Equal(true, b); // Noncompliant
+            Xunit.Assert.Equals(true, b); // Noncompliant
+            Xunit.Assert.False(true, b); // Noncompliant
+            Xunit.Assert.NotEqual(true, b); // Noncompliant
+            Xunit.Assert.Same(true, b); // Noncompliant
+            Xunit.Assert.StrictEqual(true, b); // Noncompliant
+            Xunit.Assert.NotSame(true, b); // Noncompliant
+            Xunit.Assert.Equal(false, b); // Noncompliant
+            Xunit.Assert.Equal(b, false); // Noncompliant
+            Xunit.Assert.Equal(true, false); // Noncompliant
+
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(true, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreNotEqual(true, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreSame(true, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(true, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(true, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(false, b); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(b, false); // Noncompliant
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(true, false); // Noncompliant
+
+            System.Diagnostics.Debug.Assert(b);
+            NUnit.Framework.Assert.AreEqual(b, b);
+            Xunit.Assert.Equal(b, b);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(b, b);
+            Xunit.Assert.True(true, b); // There is no Assert.Fail in Xunit. Assert.True(false) is way to simulate it.
+            Xunit.Assert.True(b, false);
+        }
+    }
+}
