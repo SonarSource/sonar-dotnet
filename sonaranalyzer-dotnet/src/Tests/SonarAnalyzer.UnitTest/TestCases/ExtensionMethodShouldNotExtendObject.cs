955b2e2e24626e88125d95f5e18c3a5471bf6adc
Amaury Levé <amaury.leve@sonarsource.com>
2017-09-11 13:45:58 +0200

    Address review comment

--- sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/ExtensionMethodShouldNotExtendObject.cs
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/ExtensionMethodShouldNotExtendObject.cs
@@ -4,7 +4,7 @@ namespace Tests.Diagnostics
 {
     static class Program
     {
-        static void Foo(this object obj) // Noncompliant
+        static void Foo(this object obj) // Noncompliant {{Refactor this extension to extend a more concrete type.}}
 //                  ^^^
         {
         }
