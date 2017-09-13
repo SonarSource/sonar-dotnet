d572ff3da3d8b5cabc7b542dd2a7ac000379a090
michalb-sonar <michal.barczyk@sonarsource.com>
2017-09-13 10:24:07 +0200

    Fix S4056: Rule should not recommend to use obsolete methods (#772)

--- sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/SpecifyIFormatProviderOrCultureInfo.cs
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/SpecifyIFormatProviderOrCultureInfo.cs
@@ -4,13 +4,31 @@ using System.Resources;
 
 namespace Tests.Diagnostics
 {
+    static class Methods
+    {
+        public static int DoStuff(string foo) => 1;
+
+        [Obsolete]
+        public static int DoStuff(string foo, IFormatProvider format) => 1;
+
+
+        public static int DoStuff2(string foo) => 1;
+
+        [Obsolete]
+        public static int DoStuff2(string foo, IFormatProvider format) => 1;
+        public static int DoStuff2(string foo, CultureInfo foo2) => 1;
+    }
+
     class Program
     {
+        enum Colors { Red, Green, Blue, Yellow = 12 };
+
         void InvalidCases()
         {
             42.ToString(); // Noncompliant {{Use the overload that takes a 'CultureInfo' or 'IFormatProvider' parameter.}}
 //          ^^^^^^^^^^^^^
             string.Format("bla"); // Noncompliant
+            Methods.DoStuff2("foo"); // Noncompliant
         }
 
         void ValidCases()
@@ -23,6 +41,11 @@ namespace Tests.Diagnostics
             var resourceManager = new ResourceManager(typeof(Program));
             resourceManager.GetObject("a"); // Compliant - excluded
             resourceManager.GetString("a"); // Compliant - excluded
+
+            Console.WriteLine("Colors.Red = {0}", Colors.Red.ToString("d"));
+            Colors myColor = Colors.Yellow;
+            Console.WriteLine("Colors.Red = {0}", myColor.ToString("d"));
+            Methods.DoStuff("foo");
         }
     }
 }
