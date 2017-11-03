383854c39e7167582f9673e6bed7b794e936aad5
Amaury Levé <amaury.leve@sonarsource.com>
2017-11-03 17:27:52 +0100

    Small powershell cosmetic refactoring (#896)

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Vsix/tools-cs/uninstall.ps1
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Vsix/tools-cs/uninstall.ps1
@@ -1,46 +1,37 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-if($project.Type -ne "C#")
-{
+if ($project.Type -ne "C#") {
     return
 }
 
-if ('14.0', '15.0' -notcontains $project.DTE.Version)
-{
+if ('14.0', '15.0' -notcontains $project.DTE.Version) {
     return
 }
 
-if ($project.Object.AnalyzerReferences -eq $null)
-{
+if ($project.Object.AnalyzerReferences -eq $null) {
     return
 }
 
-$analyzersPath = split-path -path $toolsPath -parent
-$analyzersPath = join-path $analyzersPath "analyzers"
+$analyzersPath = Split-Path -Path $toolsPath -Parent
+$analyzersPath = Join-Path $analyzersPath "analyzers"
 
-$analyzerFilePath = join-path $analyzersPath "Google.Protobuf.dll"
-try
-{
+$analyzerFilePath = Join-Path $analyzersPath "Google.Protobuf.dll"
+try {
     $project.Object.AnalyzerReferences.Remove($analyzerFilePath)
 }
-catch
-{
+catch {
 }
 
-$analyzerFilePath = join-path $analyzersPath "SonarAnalyzer.dll"
-try
-{
+$analyzerFilePath = Join-Path $analyzersPath "SonarAnalyzer.dll"
+try {
     $project.Object.AnalyzerReferences.Remove($analyzerFilePath)
 }
-catch
-{
+catch {
 }
 
-$analyzerFilePath = join-path $analyzersPath "SonarAnalyzer.CSharp.dll"
-try
-{
+$analyzerFilePath = Join-Path $analyzersPath "SonarAnalyzer.CSharp.dll"
+try {
     $project.Object.AnalyzerReferences.Remove($analyzerFilePath)
 }
-catch
-{
+catch {
 }
--- sonaranalyzer-dotnet/src/SonarAnalyzer.Vsix/tools-vbnet/install.ps1
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Vsix/tools-vbnet/install.ps1
@@ -1,28 +1,25 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-if ('14.0', '15.0' -notcontains $project.DTE.Version)
-{
+if ('14.0', '15.0' -notcontains $project.DTE.Version) {
     throw 'This package can only be installed on Visual Studio 2015  or Visual Studio 2017.'
 }
 
-if ($project.Object.AnalyzerReferences -eq $null)
-{
+if ($project.Object.AnalyzerReferences -eq $null) {
     throw 'This package cannot be installed without an analyzer reference.'
 }
 
-if($project.Type -ne "VB.NET")
-{
+if ($project.Type -ne "VB.NET") {
     throw 'This package can only be installed on VB.NET projects.'
 }
 
-$analyzersPath = split-path -path $toolsPath -parent
-$analyzersPath = join-path $analyzersPath "analyzers"
+$analyzersPath = Split-Path -Path $toolsPath -Parent
+$analyzersPath = Join-Path $analyzersPath "analyzers"
 
-$analyzerFilePath = join-path $analyzersPath "Google.Protobuf.dll"
+$analyzerFilePath = Join-Path $analyzersPath "Google.Protobuf.dll"
 $project.Object.AnalyzerReferences.Add($analyzerFilePath)
 
-$analyzerFilePath = join-path $analyzersPath "SonarAnalyzer.dll"
+$analyzerFilePath = Join-Path $analyzersPath "SonarAnalyzer.dll"
 $project.Object.AnalyzerReferences.Add($analyzerFilePath)
 
-$analyzerFilePath = join-path $analyzersPath "SonarAnalyzer.VisualBasic.dll"
+$analyzerFilePath = Join-Path $analyzersPath "SonarAnalyzer.VisualBasic.dll"
 $project.Object.AnalyzerReferences.Add($analyzerFilePath)
