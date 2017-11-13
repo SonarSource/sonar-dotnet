949ded8e87fed60e60d8ca9ff4f4761f574745a3
Amaury Levé <amaury.leve@sonarsource.com>
2017-11-13 10:02:48 +0100

    Update solution to VS2017 (#918)

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Protobuf/build.bat
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Protobuf/build.bat
@@ -1,1 +1,1 @@
-"..\..\..\packages\Google.Protobuf.Tools.3.0.0\tools\windows_x64\protoc.exe" -I=. --csharp_out=. ./AnalyzerReport.proto
+"..\..\..\packages\Google.Protobuf.Tools.3.4.0\tools\windows_x64\protoc.exe" -I=. --csharp_out=. ./AnalyzerReport.proto
