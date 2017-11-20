9da0b4d702d754bc3a5d53f4c43041453328c235
Amaury Levé <amaury.leve@sonarsource.com>
2017-11-20 17:59:50 +0100

    Rule S113: Files should contain an empty newline at the end

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/FileShouldEndWithEmptyNewLine.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2017 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class FileShouldEndWithEmptyNewLine : SonarDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S113";
+        private const string MessageFormat = "Add a new line at the end of the file '{0}'.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxTreeActionInNonGenerated(stac =>
+            {
+                var lastToken = stac.Tree.GetRoot().GetLastToken();
+
+                if (lastToken == default(SyntaxToken))
+                {
+                    return;
+                }
+
+                var isFileEndingWithNewLine = lastToken.TrailingTrivia.LastOrDefault().IsKind(SyntaxKind.EndOfLineTrivia);
+                if (!isFileEndingWithNewLine)
+                {
+                    stac.ReportDiagnosticWhenActive(Diagnostic.Create(rule, lastToken.GetLocation(),
+                        Path.GetFileName(stac.Tree.FilePath)));
+                }
+            });
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/src/Tests/SonarAnalyzer.UnitTest/TestCases/FileShouldEndWithEmptyNewLine_NoEmptyLine.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+    }
+} // Noncompliant {{Add a new line at the end of the file 'FileShouldEndWithEmptyNewLine_NoEmptyLine.cs'.}}
