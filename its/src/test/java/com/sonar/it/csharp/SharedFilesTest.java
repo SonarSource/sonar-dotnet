4e1cc44c9e430b2a0f4ccb1adc2f771dc373cac3
Duarte Meneses <duarte.meneses@sonarsource.com>
2017-11-22 16:47:29 +0100

    Exclude repeated issues on shared files (#656)

--- its/src/test/java/com/sonar/it/csharp/SharedFilesTest.java
+++ its/src/test/java/com/sonar/it/csharp/SharedFilesTest.java
@@ -1,3 +1,22 @@
+/*
+ * SonarSource :: C# :: ITs :: Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package com.sonar.it.csharp;
 
 import com.sonar.orchestrator.Orchestrator;
@@ -53,6 +72,6 @@ public class SharedFilesTest {
     assertThat(getMeasureAsInt("SharedFilesTest:Class1.cs", "ncloc")).isEqualTo(7);
     
     List<Issue> issues = getIssues("SharedFilesTest:Class1.cs");
-    assertThat(issues).hasSize(2);
+    assertThat(issues).hasSize(1);
   }
 }
