092e19f4d49b71494902477c6afa53158353dff4
Amaury Levé <amaury.leve@sonarsource.com>
2017-11-23 17:39:19 +0100

    Add a new ITs for a multi-target solution

---
+++ its/projects/MultiTargetConsoleApp/MultiTargetConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MultiTargetConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int unusedVar = 42;
+
+            Console.WriteLine("Hello World!");
+#if NETCOREAPP2_0
+            Console.WriteLine("\tfrom .Net Core 2.0"); // FIXME: .Net Core 2
+#elif NET46
+            Console.WriteLine("\tfrom .Net Fwk 4.6"); // FIXME: .Net fwk 4.6
+#else
+            Console.WriteLine("\tfrom other"); // FIXME: Other
+#endif
+
+            Console.ReadKey();
+        }
+    }
+}
