d63ccadc714db8fac0fa38b7f790b1c76029fa35
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-01-23 10:54:37 +0100

    add test for local functions

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotCallExitMethods.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotCallExitMethods.cs
@@ -8,8 +8,10 @@ namespace Tests.Diagnostics
     {
         public void Foo()
         {
-            Environment.Exit(0); // Noncompliant
-            System.Windows.Forms.Application.Exit(); // Noncompliant
+            Environment.Exit(0); // Noncompliant {{Remove this call to 'Environment.Exit' or ensure it is really required.}}
+//                      ^^^^
+            System.Windows.Forms.Application.Exit(); // Noncompliant {{Remove this call to 'Application.Exit' or ensure it is really required.}}
+//                                           ^^^^
             Application.Exit();
         }
 
@@ -24,6 +26,30 @@ namespace Tests.Diagnostics
         public static async Task<int> Main(string[] args)
         {
             Environment.Exit(0); // Compliant - inside Main
+        }
+
+        public static void Main()
+        {
+            Foo();
+
+            int x = 1;
+
+            void Foo()
+            {
+                Environment.Exit(0); // Compliant - inside Main
+            }
+        }
+
+        public static void Bar()
+        {
+            Baz();
+
+            int x = 1;
+
+            void Baz()
+            {
+                Environment.Exit(0); // Noncompliant
+            }
         }
     }
 
