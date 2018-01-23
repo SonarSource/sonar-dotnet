35db92063b878ecf53018e69ae058c32226cac53
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-01-23 10:41:57 +0100

    review comments

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotInstantiateSharedClasses.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotInstantiateSharedClasses.cs
@@ -5,11 +5,20 @@ namespace Tests.Diagnostics
     [PartCreationPolicy(CreationPolicy.Shared)]
     class SharedClass {}
 
+    [System.ComponentModel.Composition.PartCreationPolicy(System.ComponentModel.Composition.CreationPolicy.Shared)]
+    class SharedClassFullNamespace { }
+
     [PartCreationPolicy(CreationPolicy.NonShared)]
     class NonSharedClass { }
 
     [PartCreationPolicy(CreationPolicy.Any)]
     class AnyClass { }
+
+    [PartCreationPolicy(Foo)]
+    class InvalidAttrParameter { }
+
+    [PartCreationPolicy()]
+    class NoAttrParameter { }
 
     class NoAttr { }
 
@@ -19,8 +28,11 @@ namespace Tests.Diagnostics
         {
             new SharedClass(); // Noncompliant {{Refactor this code so that it doesn't invoke the constructor of this class.}}
 //          ^^^^^^^^^^^^^^^^^
+            new SharedClassFullNamespace(); // Noncompliant
             new NonSharedClass();
             new AnyClass();
+            new InvalidAttrParameter();
+            new NoAttrParameter();
             new NoAttr();
         }
     }
