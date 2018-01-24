ea122bfb43519050fa4eee2ee76292b8683c6f84
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-01-24 11:41:56 +0100

    Rule S3060: "is" should not be used with "this"

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotTestThisWithIsOperator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public Program(object o)
+        {
+            if (this is IDisposable) // Noncompliant {{Offload the code that's conditional on this 'is' test to the appropriate subclass and remove the test.}}
+//              ^^^^^^^^^^^^^^^^^^^
+            {
+            }
+
+            if (o is IDisposable)
+            {
+            }
+        }
+    }
+}
