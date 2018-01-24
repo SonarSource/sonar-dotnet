b3e8ae61bc99e3bed021fcf463175549b8b9b7fa
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-01-24 12:29:27 +0100

    Rule S138: Functions should not have too many lines of code

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/MethodsShouldNotHaveTooManyLines.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class MethodsShouldNotHaveTooManyLines : ParameterLoadingDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S138";
+        private const string MessageFormat = "This {0} has {1} lines, which is greater than the {2} lines authorized. Split it into smaller methods.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        private const int DefaultMaxMethodLines = 80;
+        [RuleParameter("max", PropertyType.Integer, "Maximum authorized lines of code in a method",
+            DefaultMaxMethodLines)]
+
+        public int Max { get; set; } = DefaultMaxMethodLines;
+        
+        protected override void Initialize(ParameterLoadingAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(c =>
+                {
+                    if (Max < 2)
+                    {
+                        throw new InvalidOperationException($"Invalid maximum number of lines: {Max}");
+                    }
+
+                    if (c.IsTest())
+                    {
+                        return;
+                    }
+
+                    var baseMethodSyntax = (BaseMethodDeclarationSyntax)c.Node;
+
+                    var identifierLocation = baseMethodSyntax?.FindIdentifierLocation();
+                    var bodyStatements = baseMethodSyntax?.Body?.Statements;
+                    if (bodyStatements == null || identifierLocation == null)
+                    {
+                        return;
+                    }
+
+                    var linesCount = bodyStatements.Value
+                        .SelectMany(s => s.DescendantTokens())
+                        .Select(token => token.GetLineNumber())
+                        .Distinct()
+                        .LongCount();
+
+                    if (linesCount > Max)
+                    {
+                        c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, identifierLocation,
+                            GetDescription(baseMethodSyntax), linesCount, Max));
+                    }
+                },
+                SyntaxKind.MethodDeclaration,
+                SyntaxKind.ConstructorDeclaration,
+                SyntaxKind.DestructorDeclaration);
+        }
+
+        private static string GetDescription(BaseMethodDeclarationSyntax baseMethodDeclaration)
+        {
+            var identifierName = baseMethodDeclaration.GetIdentifierOrDefault()?.ValueText;
+            if (identifierName == null)
+            {
+                return "method";
+            }
+
+            if (baseMethodDeclaration is ConstructorDeclarationSyntax)
+            {
+                return $"constructor '{identifierName}'";
+            }
+
+            if (baseMethodDeclaration is DestructorDeclarationSyntax)
+            {
+                return $"finalizer '~{identifierName}'";
+            }
+
+            if (baseMethodDeclaration is MethodDeclarationSyntax methodDeclaration)
+            {
+                return $"method '{identifierName}'";
+            }
+
+            return "method";
+        }
+
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MethodsShouldNotHaveTooManyLines_CustomValues.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public Program() // Noncompliant {{This constructor 'Program' has 4 lines, which is greater than the 2 lines authorized. Split it into smaller methods.}}
+//             ^^^^^^^
+        {
+            int i = 1; i++;
+            i++;
+            i++;
+            i++;
+        }
+
+        public ~Program() // Noncompliant {{This finalizer '~Program' has 4 lines, which is greater than the 2 lines authorized. Split it into smaller methods.}}
+        {
+            int i = 1; i++;
+            i++;
+            i++;
+            i++;
+        }
+
+
+        public void Method_01() // Noncompliant {{This method 'Method_01' has 4 lines, which is greater than the 2 lines authorized. Split it into smaller methods.}}
+//                  ^^^^^^^^^
+        {
+            int i = 1; i++;
+            i++;
+            i++;
+            i++;
+        }
+
+        public int Method_02() // Noncompliant {{This method 'Method_02' has 13 lines, which is greater than the 2 lines authorized. Split it into smaller methods.}}
+        {
+            int i = 1;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+            i++;
+
+            return 1;
+        }
+
+        public abstract int Method_04();
+
+        extern int Method_05();
+
+        public int Method_06() // Noncompliant {{This method 'Method_06' has 6 lines, which is greater than the 2 lines authorized. Split it into smaller methods.}}
+        {
+
+            void InnerFunction_06()
+            {
+                int i = 0;
+                i++;
+                i++;
+            }
+        }
+
+        // Compliant. Properties are not checked
+        public int Property_01
+        {
+            get
+            {
+                int i = 0;
+                i++;
+
+                return 1;
+            }
+
+            set
+            {
+                int i = 0;
+                i++;
+                i++;
+            }
+        }
+
+    }
+}
