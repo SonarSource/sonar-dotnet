106783afb8760c599787be35931eaf19610ab6f6
Michal Barczyk <michal.barczyk@sonarsource.com>
2018-01-26 17:25:09 +0100

    Add 'extern alias csharp' to C# test template

--- scripts/rspec/rspec-templates/CSharpTestTemplate.cs
+++ scripts/rspec/rspec-templates/CSharpTestTemplate.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+extern alias csharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using csharp::SonarAnalyzer.Rules.CSharp;
 
