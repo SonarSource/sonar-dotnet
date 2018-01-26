72dff3048905f3af96102d05a09e6f42313b4457
michalb-sonar <michal.barczyk@sonarsource.com>
2018-01-26 16:14:00 +0100

    Rule S3358: Ternary operators should not be nested (#1114)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotNestTernaryOperators.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public bool Foo()
+        {
+            var b = System.Environment?.NewLine;
+            return true ? FooImpl(true, false) : true;
+        }
+
+        public bool FooImpl(bool isMale, bool isMarried)
+        {
+            var x = isMale ? "Mr. " : isMarried ? "Mrs. " : "Miss ";  // Noncompliant {{Extract this nested ternary operation into an independent statement.}}
+//                                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+            var x2 = isMale ? "Mr. " : isMarried ? "Mrs. " : true ? "Miss " : "what? ";
+//                                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Noncompliant
+//                                                           ^^^^^^^^^^^^^^^^^^^^^^^^^ Noncompliant@-1
+
+            var x3 = isMale ? "Mr. " :
+                 isMarried // Noncompliant
+                ? "Mrs. "
+                : "Miss ";
+        }
+    }
+}
