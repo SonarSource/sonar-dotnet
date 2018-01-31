d1dc35b6c40baee42b8b02bffd9843d3fba55943
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-01-31 11:14:35 +0100

    Rule S4200: Native methods should be wrapped

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/NativeMethodsShouldBeWrapped.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class NativeMethodsShouldBeWrapped : SonarDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S4200";
+        private const string MessageFormat = "{0}";
+        private const string MakeThisMethodPrivateMessage = "Make this native method private and provide a wrapper.";
+        private const string MakeThisWrapperLessTrivialMessage = "Make this wrapper for native method '{0}' less trivial.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(ReportPublicExternalMethods, SyntaxKind.MethodDeclaration);
+            context.RegisterSyntaxNodeAction(ReportTrivialWrappers, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void ReportPublicExternalMethods(SyntaxNodeAnalysisContext c)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)c.Node;
+
+            if (methodDeclaration.Modifiers.Any(IsExternal) &&
+                methodDeclaration.Modifiers.Any(IsPublicOrProtected))
+            {
+                c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, methodDeclaration.Identifier.GetLocation(),
+                    MakeThisMethodPrivateMessage));
+            }
+        }
+
+        private static void ReportTrivialWrappers(SyntaxNodeAnalysisContext c)
+        {
+            var methodDeclaration = (MethodDeclarationSyntax)c.Node;
+            var methodSymbol = c.SemanticModel.GetDeclaredSymbol(methodDeclaration);
+
+            if (methodDeclaration.Modifiers.Any(IsExternal) ||
+                methodDeclaration.ParameterList?.Parameters.Count == 0 ||
+                HasTwoOrMoreStatements(methodDeclaration))
+            {
+                return;
+            }
+
+            var invocations = GetBodyDescendants(methodDeclaration)
+                .OfType<InvocationExpressionSyntax>();
+
+            if (invocations.Take(2).Count() == 2) // Two or more invocations
+            {
+                return;
+            }
+
+            bool ParametersMatchCotnainingMethodDeclaration(InvocationExpressionSyntax invocation) =>
+                invocation.ArgumentList.Arguments
+                    .All(a => a.Expression is LiteralExpressionSyntax ||
+                        a.Expression is IdentifierNameSyntax i
+                        && methodDeclaration.ParameterList.Parameters.Any(p => p.Identifier.Text == i.Identifier.Text));
+
+            var externalMethodSymbols = GetExternalMethods(methodSymbol);
+
+            var invokedExternalMethods = invocations
+                .Where(ParametersMatchCotnainingMethodDeclaration)
+                .Select(i => c.SemanticModel.GetSymbolInfo(i).Symbol)
+                .OfType<IMethodSymbol>()
+                .Where(externalMethodSymbols.Contains);
+
+            foreach (var invocation in invokedExternalMethods)
+            {
+                c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, methodDeclaration.Identifier.GetLocation(),
+                  string.Format(MakeThisWrapperLessTrivialMessage, invocation.Name)));
+            }
+        }
+
+        private static ISet<IMethodSymbol> GetExternalMethods(IMethodSymbol methodSymbol) =>
+            methodSymbol.ContainingType.GetMembers()
+                .OfType<IMethodSymbol>()
+                .Where(IsExternal)
+                .ToHashSet();
+
+        private static bool HasTwoOrMoreStatements(MethodDeclarationSyntax methodDeclaration) =>
+            GetBodyDescendants(methodDeclaration)
+                .OfType<StatementSyntax>()
+                .Take(2)
+                .Count() == 2;
+
+        private static IEnumerable<SyntaxNode> GetBodyDescendants(MethodDeclarationSyntax methodDeclaration) =>
+            methodDeclaration.Body?.DescendantNodes()
+            ?? methodDeclaration.ExpressionBody?.DescendantNodes()
+            ?? Enumerable.Empty<SyntaxNode>();
+
+        private static bool IsPublicOrProtected(SyntaxToken token) =>
+            token.IsKind(SyntaxKind.PublicKeyword) || token.IsKind(SyntaxKind.ProtectedKeyword);
+
+        private static bool IsExternal(SyntaxToken token) =>
+            token.IsKind(SyntaxKind.ExternKeyword);
+
+        private static bool IsExternal(IMethodSymbol symbol) =>
+            symbol.IsExtern;
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        private static T DoSomething<T>(T x) => x;
+
+        extern private static void Extern0(); // Compliant
+
+        extern private static void Extern1(string s, int x); // Compliant
+
+        extern public static void Extern2(string s, int x); // Noncompliant {{Make this native method private and provide a wrapper.}}
+//                                ^^^^^^^
+
+        extern private static int Extern3(string s); // Compliant
+
+        public static void Wrapper1(string s, int x) // Noncompliant {{Make this wrapper for native method 'Extern1' less trivial.}}
+        {
+            Extern1(s, x);
+        }
+
+        public static void Wrapper2() // Compliant, no arguments
+        {
+            Extern0();
+        }
+
+        public static int Wrapper3() // Compliant, no arguments
+        {
+            return DoSomething(false) // simulate some check
+                ? Extern0()
+                : 5;
+        }
+
+        public static void Wrapper4(string s, int x) // Compliant, more than one statement
+        {
+            Extern1(s, x);
+            Extern1(s, x);
+        }
+
+        public static void Wrapper5(string s, int x) // Compliant, more than one statement
+        {
+            if (string.IsNullOrEmpty(s) || x < 0)
+            {
+                return;
+            }
+            Extern1(s, x);
+        }
+
+        public static void Wrapper6(string s, int x) // Compliant, parameters are not directly passed
+        {
+            Extern1(s != null ? s : string.Empty, x > 0 ? x : 100);
+        }
+
+        public static void Wrapper7(string s, int x) // Compliant, parameters are not directly passed
+        {
+            Extern1(DoSomething(s), DoSomething(x));
+        }
+
+        public static void Wrapper8(string s, int x) => // Noncompliant {{Make this wrapper for native method 'Extern1' less trivial.}}
+            Extern1(s, x);
+
+        public static void Wrapper9(string s, int x) => // Compliant, parameters are not directly passed
+            Extern1(s.Length > 0 ? s : string.Empty, x > 0 ? x : 100);
+
+        public static void Wrapper10(string s, int x) => // Compliant, parameters are not directly passed
+            Extern1(DoSomething(s), DoSomething(x));
+
+        public static int Wrapper11() => // Compliant, no arguments
+            DoSomething(false) ? Extern0() : 5; // simulate some check
+    }
+}
