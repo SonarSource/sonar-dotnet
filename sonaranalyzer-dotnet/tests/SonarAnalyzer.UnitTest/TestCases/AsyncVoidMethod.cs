86eb70579b495fd1c6a2fe25e1ac98a255d7ebe9
Amaury Levé <amaury.leve@sonarsource.com>
2018-02-22 18:00:02 +0100

    Fix S3168: Rule should not be too strict on first parameter name

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/AsyncVoidMethod.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/AsyncVoidMethod.cs
@@ -5,18 +5,26 @@ using System;
 
 namespace Tests.Diagnostics
 {
-    public class AsyncVoidMethod
+    public class Foo : EventArgs { }
+
+    public class EventHandlerCases
     {
         async void MyMethod() { } // Noncompliant {{Return 'Task' instead.}}
 //            ^^^^
-        async void MyMethod(object sender, EventArgs args) { } // Compliant
+        async void MyMethod(object sender, EventArgs args) { }
 
+        async void MyMethod1(object o, EventArgs e) { }
 
+        async void MyMethod2(object o, Foo e) { }
+    }
+
+    public class UwpCases
+    {
         // A lot of classes/interfaces in UWP do not inherit from EventArgs so we had to change the detection mechanism
         // See issue https://github.com/SonarSource/sonar-csharp/issues/704
         private interface ISuspendingEventArgs { }
 
-        async void MyOtherMethod1(object o, ISuspendingEventArgs args) { } // Noncompliant
-        private async void OnSuspending(object sender, ISuspendingEventArgs e) { } // Compliant - ends with EventArgs and 1st param is object sender
+        async void MyOtherMethod1(object o, ISuspendingEventArgs args) { }
+        private async void OnSuspending(object sender, ISuspendingEventArgs e) { }
     }
 }
