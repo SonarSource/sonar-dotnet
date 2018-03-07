a1d748832f6fba1a940288450c436a50f63e1088
Karol Deland <karoldeland@gmail.com>
2018-03-06 20:32:24 -0500

    Adding custom VB.NET Plugin

---
+++ sonar-vbnet-plugin/src/main/java/org/sonar/plugins/vbnet/VBNetPropertyDefinitions.java
@@ -0,0 +1,29 @@
+/*
+ * SonarVB
+ * Copyright (C) 2014-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.vbnet;
+
+import org.sonarsource.dotnet.shared.plugins.AbstractPropertyDefinitions;
+
+public class VBNetPropertyDefinitions extends AbstractPropertyDefinitions {
+
+  public VBNetPropertyDefinitions() {
+    super(VBNetPlugin.LANGUAGE_KEY, VBNetPlugin.FILE_SUFFIXES_DEFVALUE);
+  }
+}
