f4df640971988d19bb708de83c3e787a378c6d32
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-03-15 15:24:34 +0100

    Fix S3264: Rule should not raise when 'BeginInvoke' or 'EndInvoke' are called

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UninvokedEventDeclaration.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UninvokedEventDeclaration.cs
@@ -126,4 +126,83 @@ namespace Tests.Diagnostics
             throw new NotImplementedException();
         }
     }
+
+    // Shows all different kinds of event invocations
+    public class EventInvocations
+    {
+        public event EventHandler MyEvent1;
+        public event EventHandler MyEvent2;
+        public event EventHandler MyEvent3;
+        public event EventHandler MyEvent4;
+        public event EventHandler MyEvent5;
+        public event EventHandler MyEvent6;
+
+        public event Action<object, EventArgs> MyAction1;
+        public event Action<object, EventArgs> MyAction2;
+        public event Action<object, EventArgs> MyAction3;
+        public event Action<object, EventArgs> MyAction4;
+        public event Action<object, EventArgs> MyAction5;
+        public event Action<object, EventArgs> MyAction6;
+
+        public void InvokeAll()
+        {
+            MyEvent1(this, EventArgs.Empty);
+            MyEvent2.Invoke(this, EventArgs.Empty);
+            MyEvent3.DynamicInvoke(this, EventArgs.Empty);
+            MyEvent4.BeginInvoke(this, EventArgs.Empty, null, null);
+            this.MyEvent5(this, EventArgs.Empty);
+            MyEvent6?.Invoke(this, EventArgs.Empty);
+
+            MyAction1(this, EventArgs.Empty);
+            MyAction2.Invoke(this, EventArgs.Empty);
+            MyAction3.DynamicInvoke(this, EventArgs.Empty);
+            MyAction4.BeginInvoke(this, EventArgs.Empty, null, null);
+            this.MyAction5(this, EventArgs.Empty);
+            MyAction6?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    // Shows all different kinds of event invocations from an inner class
+    public class EventInvocationsFromInnerClass
+    {
+        public event EventHandler MyEvent1;
+        public event EventHandler MyEvent2;
+        public event EventHandler MyEvent3;
+        public event EventHandler MyEvent4;
+        public event EventHandler MyEvent5;
+        public event EventHandler MyEvent6;
+
+        public event Action<object, EventArgs> MyAction1;
+        public event Action<object, EventArgs> MyAction2;
+        public event Action<object, EventArgs> MyAction3;
+        public event Action<object, EventArgs> MyAction4;
+        public event Action<object, EventArgs> MyAction5;
+        public event Action<object, EventArgs> MyAction6;
+
+        public class Inner
+        {
+            readonly EventInvocationsFromInnerClass outer;
+            public Inner(EventInvocationsFromInnerClass outer)
+            {
+                this.outer = outer;
+            }
+
+            public void InvokeAll()
+            {
+                outer.MyEvent1(this, EventArgs.Empty);
+                outer.MyEvent2.Invoke(this, EventArgs.Empty);
+                outer.MyEvent3.DynamicInvoke(this, EventArgs.Empty);
+                outer.MyEvent4.BeginInvoke(this, EventArgs.Empty, null, null);
+                outer.MyEvent5(this, EventArgs.Empty);
+                outer.MyEvent6?.Invoke(this, EventArgs.Empty);
+
+                outer.MyAction1(this, EventArgs.Empty);
+                outer.MyAction2.Invoke(this, EventArgs.Empty);
+                outer.MyAction3.DynamicInvoke(this, EventArgs.Empty);
+                outer.MyAction4.BeginInvoke(this, EventArgs.Empty, null, null);
+                outer.MyAction5(this, EventArgs.Empty);
+                outer.MyAction6?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
 }
