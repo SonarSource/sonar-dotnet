732715db9c90c727a504f95be36174001333eafa
Amaury Levé <amaury.leve@sonarsource.com>
2018-04-04 18:21:33 +0200

    Update CI build script to fix error on versioning with Next (#1307)

--- scripts/build/ci-build.ps1
+++ scripts/build/ci-build.ps1
@@ -258,7 +258,7 @@ function Get-MavenExpression([string]$exp) {
     $out = Exec { mvn help:evaluate -B -Dexpression="${exp}" }
     Test-ExitCode "ERROR: Evaluation of expression ${exp} FAILED."
 
-    return ($out | Select-String -NotMatch -Pattern '^\[|Download\w\+\:').Line
+    return $out | Select-String -NotMatch -Pattern '^\[|Download\w\+\:' | Select-Object -First 1 -ExpandProperty Line
 }
 
 function Set-MavenBuildVersion() {
