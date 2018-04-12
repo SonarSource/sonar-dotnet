00d8678491a4414f5efd51c9f6f786b61a45cb86
Amaury Levé <amaury.leve@sonarsource.com>
2018-04-12 17:34:22 +0200

    Address review comments

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/HttpPostControllerActionShouldValidateInput.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/HttpPostControllerActionShouldValidateInput.cs
@@ -6,7 +6,8 @@ namespace Tests.Diagnostics
     public class FooBarController1 : Controller
     {
         [HttpPost]
-        [ValidateInput(false)] // Noncompliant
+        [ValidateInput(false)] // Noncompliant {{Enable validation on this 'ValidateInput' attribute.}}
+//       ^^^^^^^^^^^^^^^^^^^^
         public ActionResult Purchase1(string input)
         {
             return Foo(input);
@@ -26,18 +27,39 @@ namespace Tests.Diagnostics
             return Foo(input);
         }
 
-        [ValidateInput(false)] // Noncompliant - no HttpPost
+        [ValidateInput(false)] // Noncompliant - even when there is no HttpPost
         public ActionResult Purchase4(string input)
         {
             return Foo(input);
         }
     }
 
-    public class FooBarController1
+    public class FooBarController2
     {
         [HttpPost]
-        [ValidateInput(false)] // Noncompliant - class doesn't inherit from Controller
+        [ValidateInput(false)] // Noncompliant - even when class doesn't inherit from Controller
         public ActionResult Purchase1(string input)
+        {
+            return Foo(input);
+        }
+
+        [HttpPost]
+        [ValidateInput("foo")] // Compliant - unrecognized argument
+        public ActionResult Purchase2(string input)
+        {
+            return Foo(input);
+        }
+
+        [HttpPost]
+        [ValidateInput()] // Compliant - no argument
+        public ActionResult Purchase3(string input)
+        {
+            return Foo(input);
+        }
+
+        [HttpPost]
+        [ValidateInput(false, "foo")] // Compliant - more than 1 argument
+        public ActionResult Purchase3(string input)
         {
             return Foo(input);
         }
