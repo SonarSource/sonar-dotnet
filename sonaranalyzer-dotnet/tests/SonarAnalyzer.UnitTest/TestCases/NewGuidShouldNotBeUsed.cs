865a48c0439fea46e4ef3ca2402a745a0d00d138
Amaury Levé <amaury.leve@sonarsource.com>
2018-04-16 15:28:57 +0200

    Rule S4581: 'new Guid()' should not be used

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/NewGuidShouldNotBeUsed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    public class Program
+    {
+        public void Foo()
+        {
+            var result = new Guid(); // Noncompliant {{Please review this use of 'new Guid()'.}}
+//                       ^^^^^^^^^^
+
+            result = Guid.Empty;
+            result = Guid.NewGuid();
+            result = new Guid(new byte[0]);
+        }
+    }
+}
