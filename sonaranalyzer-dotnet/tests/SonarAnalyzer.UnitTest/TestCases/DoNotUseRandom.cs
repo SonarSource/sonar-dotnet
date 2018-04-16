c4c16438338af1139aff1e58db019c0d27fc086d
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-04-16 16:44:16 +0200

    Rule S2245: Pseudorandom number generators (PRNGs) should not be used in secure contexts

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotUseRandom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public void Main()
+        {
+            new Random(); // Noncompliant {{Use a cryptographically strong random number generator (RNG) like 'System.Security.Cryptography.RandomNumberGenerator'.}}
+//          ^^^^^^^^^^^^
+            new Random(1); // Noncompliant
+
+            new EventArgs(); // Compliant, not Random
+
+            RandomNumberGenerator.Create(); // Compliant, using cryptographically strong RNG
+        }
+    }
+}
