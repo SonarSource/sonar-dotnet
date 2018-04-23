a8ecd4dc282f8ad5a78797b72acdb2676664cb98
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-04-23 11:05:54 +0200

    Rule S4432: AES encryption algorithm should be used with secured mode

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/AesManagedShouldBeWithSecureMode.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class AesManagedShouldBeWithSecureMode : ObjectShouldBeInitializedCorrectlyBase
+    {
+        internal const string DiagnosticId = "S4432";
+        private const string MessageFormat = "Use a certified third party library implementing Galois/Counter Mode (GCM) instead.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        protected override DiagnosticDescriptor Rule => rule;
+
+        internal override KnownType TrackedType => KnownType.System_Security_Cryptography_AesManaged;
+
+        protected override string TrackedPropertyName => "Mode";
+
+        protected override bool IsAllowedValue(object constantValue) =>
+            false;
+    }
+}
