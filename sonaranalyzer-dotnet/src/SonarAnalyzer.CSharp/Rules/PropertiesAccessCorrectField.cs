1808b6b3e3ad0375d65a90f13e20e8f65d123c53
duncanp <duncan.pocklington@sonarsource.com>
2018-06-12 14:53:35 +0100

    Added S4275  Getters and setters should access the expected fields
    https://jira.sonarsource.com/browse/RSPEC-4275

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/PropertiesAccessCorrectField.cs
@@ -0,0 +1,228 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class PropertiesAccessCorrectField : SonarDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S4275";
+        private const string MessageFormat = "Refactor this {0} so that it actually refers to the field '{1}'";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            // We want to check the fields read and assigned in all properties in this class
+            // so this is a symbol-level rule (also means the callback is called only once
+            // for partial classes)
+            context.RegisterSymbolAction(c => CheckType(c),
+                SymbolKind.NamedType);
+        }
+
+        private void CheckType(SymbolAnalysisContext context)
+        {
+            var symbol = (INamedTypeSymbol)context.Symbol;
+            if (symbol.TypeKind != TypeKind.Class &&
+                symbol.TypeKind != TypeKind.Structure)
+            {
+                return;
+            }
+
+            var fields = symbol.GetMembers().Where(m => m.Kind == SymbolKind.Field).OfType<IFieldSymbol>();
+
+            if (!fields.Any())
+            {
+                return;
+            }
+
+            var properties = symbol.GetMembers()
+                .Where(m => m.Kind == SymbolKind.Property)
+                .OfType<IPropertySymbol>()
+                .Where(p => !p.IsImplicitlyDeclared);
+            if (!properties.Any())
+            {
+                return;
+            }
+
+            var fieldToStandardNameMap = new Dictionary<IFieldSymbol, string>();
+            foreach (var field in fields)
+            {
+                fieldToStandardNameMap[field] = GetCanonicalFieldName(field.Name);
+            }
+
+            var allPropertyData = CollectPropertyData(properties, context.Compilation);
+
+            // Check that if there is a single matching field name it is used by the property
+            foreach (var data in allPropertyData)
+            {
+                var matchingFields = fieldToStandardNameMap.Keys.Where(k => AreCanonicalNamesEqual(fieldToStandardNameMap[k], data.CanonicalName));
+                if (matchingFields.Count() == 1)
+                {
+                    var field = matchingFields.Single();
+
+                    if (data.FieldReturned.HasValue && data.FieldReturned.Value.Field != field)
+                    {
+                        context.ReportDiagnosticWhenActive(Diagnostic.Create(
+                            rule,
+                            data.FieldReturned.Value.Location,
+                            "getter",
+                            field.Name
+                            ));
+                    }
+
+                    if (data.FieldUpdated.HasValue && data.FieldUpdated.Value.Field != field)
+                    {
+                        context.ReportDiagnosticWhenActive(Diagnostic.Create(
+                            rule,
+                            data.FieldUpdated.Value.Location,
+                            "setter",
+                            field.Name
+                            ));
+                    }
+                }
+            }
+        }
+
+        private static string GetCanonicalFieldName(string name)
+            => name.Replace("_", string.Empty);
+
+        private static bool AreCanonicalNamesEqual(string name1, string name2)
+            => name1.Equals(name2, System.StringComparison.OrdinalIgnoreCase);
+
+        private static IList<PropertyData> CollectPropertyData(IEnumerable<IPropertySymbol> properties, Compilation compilation)
+        {
+            IList<PropertyData> allPropertyData = new List<PropertyData>();
+
+            // Collect the list of fields read/written by each property
+            foreach (var property in properties)
+            {
+                var returned = FindReturnedField(property, compilation);
+                var updated = FindFieldAssignment(property, compilation);
+                var data = new PropertyData(property, returned, updated);
+                allPropertyData.Add(data);
+            }
+            return allPropertyData;
+        }
+
+        private static FieldData? FindFieldAssignment(IPropertySymbol property, Compilation compilation)
+        {
+            if (property.SetMethod?.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is AccessorDeclarationSyntax accessor
+                //// We assume that if there are multiple field assignments in a property
+                //// then they are all to the same field
+                && accessor.DescendantNodes().FirstOrDefault(n => n is ExpressionStatementSyntax) is ExpressionStatementSyntax expression
+                && expression.Expression is AssignmentExpressionSyntax assignment
+                && assignment.IsKind(SyntaxKind.SimpleAssignmentExpression))
+            {
+                return ExtractFieldFromExpression(assignment.Left, compilation);
+            }
+
+            return null;
+        }
+
+        private static FieldData? FindReturnedField(IPropertySymbol property, Compilation compilation)
+        {
+            // We don't handle properties with multiple returns that return different fields
+            if (property.GetMethod?.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is AccessorDeclarationSyntax accessor
+                && accessor.DescendantNodes().FirstOrDefault(n => n is ReturnStatementSyntax) is ReturnStatementSyntax returnStatement
+                && returnStatement.Expression != null)
+            {
+                return ExtractFieldFromExpression(returnStatement.Expression, compilation);
+            }
+            return null;
+        }
+
+        private static FieldData? ExtractFieldFromExpression(ExpressionSyntax expression,
+            Compilation compilation)
+        {
+            var semanticModel = compilation.GetSemanticModel(expression.SyntaxTree);
+            if (semanticModel == null)
+            {
+                return null;
+            }
+
+            var strippedExpression = expression.RemoveParentheses();
+
+            // Check for direct field access: "foo"
+            if (strippedExpression is IdentifierNameSyntax &&
+                semanticModel.GetSymbolInfo(strippedExpression).Symbol is IFieldSymbol field)
+            {
+                return new FieldData(field, strippedExpression.GetLocation());
+            }
+            else
+            {
+                // Check for "this.foo"
+                if (strippedExpression is MemberAccessExpressionSyntax member
+                  && member.Expression is ThisExpressionSyntax thisExpression
+                  && semanticModel.GetSymbolInfo(expression).Symbol is IFieldSymbol field2)
+                {
+                    return new FieldData(field2, member.Name.GetLocation());
+                }
+            }
+
+            return null;
+        }
+
+        private struct PropertyData
+        {
+            public PropertyData(IPropertySymbol propertySymbol, FieldData? returned, FieldData? updated)
+            {
+                this.PropertySymbol = propertySymbol;
+                this.CanonicalName = GetCanonicalFieldName(propertySymbol.Name);
+                this.FieldReturned = returned;
+                this.FieldUpdated = updated;
+            }
+
+            public IPropertySymbol PropertySymbol { get; }
+
+            public string CanonicalName { get; }
+
+            public FieldData? FieldReturned { get; }
+
+            public FieldData? FieldUpdated { get; }
+        }
+
+        private struct FieldData
+        {
+            public FieldData(IFieldSymbol field, Location location)
+            {
+                this.Field = field;
+                this.Location = location;
+            }
+
+            public IFieldSymbol Field { get; }
+
+            public Location Location { get; }
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PropertiesAccessCorrectField.cs
@@ -0,0 +1,256 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class NonCompliantClass_FromRspec
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return x; }  // Noncompliant {{Refactor this getter so that it actually refers to the field 'y'}}
+//                       ^
+            set { x = value; } // Noncompliant {{Refactor this setter so that it actually refers to the field 'y'}}
+//                ^
+        }
+    }
+
+    class NonCompliantStruct_FromRspec
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return x; }  // Noncompliant: field 'y' is not used in the return value
+            set { x = value; } // Noncompliant: field 'y' is not updated
+        }
+    }
+
+
+    class NonCompliant_UnderscoresInNamesAndCasing
+    {
+        private int yyy;
+        private int __x__X; // test that underscores and casing in names are ignored
+        public int XX
+        {
+            get { return yyy; }   // Noncompliant {{Refactor this getter so that it actually refers to the field '__x__X'}}
+//                       ^^^
+            set { yyy = value; }  // Noncompliant {{Refactor this setter so that it actually refers to the field '__x__X'}}
+//                ^^^
+        }
+
+        public string _Y___Y_Y_
+        {
+            get { return __x__X; } // Noncompliant
+//                       ^^^^^^
+        }
+    }
+
+    class NonCompliant_FieldTypeIsIgnored
+    {
+        private int aaa;
+        private string aString;
+
+        public string AAA
+        {
+            get { return aString; } // Noncompliant - field called 'aaa' exists, even though type is different
+//                       ^^^^^^^
+            set { aString = value; } // Noncompliant
+//                ^^^^^^^
+        }
+    }
+
+    class NonCompliant_AssigningToExpression
+    {
+        private int aaa;
+        private string aString;
+
+        public string AAA
+        {
+            set { aString = "foo" + value; } // Noncompliant
+//                ^^^^^^^
+        }
+    }
+
+    partial class NonCompliant_PartialClass
+    {
+        private object myProperty;
+    }
+    partial class NonCompliant_PartialClass
+    {
+        private object anotherObject;
+    }
+    partial class NonCompliant_PartialClass
+    {
+        public object MyProperty
+        {
+            get { return this.anotherObject; } // Noncompliant
+            set { this.anotherObject = value; } // Noncompliant
+        }
+    }
+
+
+    class NonCompliant_ComplexProperty
+    {
+        private int field1;
+        private int field2;
+        private bool initialized;
+        private bool isDisposed;
+
+        public int Field1
+        {
+            get
+            {
+                if (!this.initialized)
+                {
+                    throw new InvalidOperationException();
+                }
+                if (this.isDisposed)
+                {
+                    throw new ObjectDisposedException("object name");
+                }
+
+                return this.field2;  // Noncompliant
+//                          ^^^^^^
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value));
+                }
+                this.field2 = value; // Noncompliant
+//                   ^^^^^^
+            }
+        }
+    }
+
+    class NonCompliant_Parentheses
+    {
+        private int field1;
+        private int field2;
+
+        public int Field2
+        {
+            get { return (((this.field1))); } // Noncompliant
+//                               ^^^^^^
+        }
+    }
+
+
+    class NonCompliant_OuterClass
+    {
+        private string fielda;
+        private string fieldb;
+
+        struct NonCompliant_NestedClass
+        {
+            int fielda;
+            int fieldb;
+
+            public int FieldA
+            {
+                get { return this.fieldb; }     // Noncompliant
+                set { this.fieldb = value ; }   // Noncompliant
+            }
+        }
+    }
+
+
+    struct Compliant_Indexer
+    {
+        // Declare an array to store the data elements.
+        private readonly int[] arr = new int[100];
+
+        // Define the indexer to allow client code to use [] notation.
+        public int this[int i]
+        {
+            get { return arr[i]; }  // Compliant - we don't know which field to check against
+            set { arr[i] = value; }
+        }
+    }
+
+    class CompliantClass
+    {
+        private int xxx;
+
+        public int XXX
+        {
+            get { return xxx; }
+            set { xxx = value; }
+        }
+
+        public int UUU
+        {
+            get { return xxx; }     // Compliant - no matching field name
+            set { xxx = value; }
+        }
+
+        private string _a_b_c;
+        private string abc;
+        private string yyy;
+
+        public string Abc
+        {
+            get { return yyy; }     // Compliant - multiple possible matching field names, so don't raise
+            set { yyy = value; }
+        }
+    }
+
+    class Compliant_ImplicitProperties
+    {
+        private string firstName;
+        private string secondName;
+
+        public string FirstName { get; set; } = "Jane";
+        public string SecondName { get; set; }
+    }
+
+
+    class WrappedClass
+    {
+        internal int field1;
+        internal int field2;
+    }
+    class Compliant_WrappedObject
+    {
+        private WrappedClass wrapped;
+
+        public int Field2
+        {
+            get { return wrapped.field1; }
+            get { wrapped.field1 = value; }
+        }
+    }
+
+    class BaseClass
+    {
+        protected int field1;
+    }
+
+    class ChildClass : BaseClass
+    {
+        private int field2;
+
+        public int Field1
+        {
+            get { return field2; }      // Compliant - aren't checking inherited fields
+            set { field2 = value; }     // Compliant
+        }
+    }
+}
