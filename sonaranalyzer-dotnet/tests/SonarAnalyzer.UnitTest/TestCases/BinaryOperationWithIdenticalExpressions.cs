4efd3c2d0dd8c6aa14fdfaa821b9011151c4c484
Amaury Levé <amaury.leve@sonarsource.com>
2018-08-13 15:42:00 +0200

    Update S1764: update rule metadata (#1674)
    Fix #1673

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/BinaryOperationWithIdenticalExpressions.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/BinaryOperationWithIdenticalExpressions.cs
@@ -32,16 +32,13 @@ namespace Tests.TestCases
             // Secondary@-1
             int k = 5 - 5; //Noncompliant
             // Secondary@-1
-            int l = 5 * 5;
 
             l = 5 | 5; // Noncompliant
             // Secondary@-1
             l |= (l); // Noncompliant
             // Secondary@-1
 
-            int i = 1 << 1;
-            i = 1 << 0x1;
-            i = 2 << 2; // Compliant
+            int i = 1;
 
             object.Equals(i, i);
 //                        ^ {{Change one instance of 'i' to a different value; comparing 'i' to itself always returns true.}}
@@ -60,6 +57,17 @@ namespace Tests.TestCases
             f.Equals(f); // Noncompliant
             // Secondary@-1
 
+        }
+
+        public void CompliantCases()
+        {
+            int i = 1 << 1;
+            i = 1 << 0x1;
+            i = 2 << 2;
+            i = 2 + 2;
+            i = 2 * 2;
+
+            i = i;
         }
     }
 }
