44d20af778cd44fa4d7ec5c4b91fa05b672f94b9
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-08-17 14:05:35 +0100

    Fixed #1486 - exception for classes that contain assembly-level setup/cleanup methods (#1713)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestClassShouldHaveTestMethod.MsTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestClassShouldHaveTestMethod.MsTest.cs
@@ -68,3 +68,44 @@ namespace Tests.Diagnostics
 
     }
 }
+
+namespace TestSetupAndCleanupAttributes
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    // Regression tests for Bug 1486: https://github.com/SonarSource/sonar-csharp/issues/1486
+    // Don't raise for classes that test setup or cleanup attributes
+
+    [TestClass]
+    public class SetupAttributes1
+    {
+        [AssemblyInitialize]
+        public static void BeforeTests(TestContext context)
+        {
+        }
+    }
+
+    [TestClass]
+    public static class SetupAttributes2
+    {
+        [AssemblyCleanup]
+        public static void AfterTests()
+        {
+        }
+    }
+
+
+    [TestClass]
+    public static class SetupAttributes3
+    {
+        [AssemblyInitialize]
+        public static void BeforeTests(TestContext context)
+        {
+        }
+
+        [AssemblyCleanup]
+        public static void AfterTests()
+        {
+        }
+    }
+}
