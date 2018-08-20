9b36aaab21a74261331162615152792176d656f4
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-08-20 13:56:12 +0100

    Fix #1705: support recognized test attributes (#1720)
    Added support for DataTestMethod and NUnit Theory tests.
    Added support for NUnit Ignore
    Removed FPs for xUnit
    
    Fixed #1705

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestMethodShouldNotBeIgnored.Xunit.v1.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestMethodShouldNotBeIgnored.Xunit.v1.cs
@@ -1,5 +1,8 @@
 ﻿using MSTest = Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
+using Xunit.Extensions;
+
+// Legacy xUnit (v1.9.1)
 
 // Regression test for #1705: https://github.com/SonarSource/sonar-csharp/issues/1705
 // The rule should not be applied to xUnit tests. Previously, the rule was raising if
