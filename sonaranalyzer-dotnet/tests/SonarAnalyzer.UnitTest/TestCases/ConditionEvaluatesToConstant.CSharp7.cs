7f324fbd449d82e36c1591a81a5bc4c081a8d88e
Amaury Levé <amaury.leve@sonarsource.com>
2018-08-21 09:22:25 +0200

    Update Symbolic Execution Engine: Run rules on ExpressionBody

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ConditionEvaluatesToConstant.CSharp7.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests.Diagnostics
+{
+    public class ExpressionBodyTest
+    {
+        private const bool TRUE = true;
+        private bool myBool;
+
+        public bool Method(bool b) => TRUE || b; // Noncompliant
+                                                 // Secondary@-1
+
+        public bool Prop => TRUE || myBool; // Noncompliant
+                                            // Secondary@-1
+
+        public bool OtherProp
+        {
+            get => TRUE || myBool; // Noncompliant
+                                   // Secondary@-1
+        }
+    }
+}
