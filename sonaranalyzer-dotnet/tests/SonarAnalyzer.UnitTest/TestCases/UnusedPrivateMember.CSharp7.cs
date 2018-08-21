108ce41a238c6a88d0079b95ace4f51842c864c4
Valeri Hristov <valhristov@users.noreply.github.com>
2018-08-21 11:28:53 +0200

    Update S1144: Rule should handle ExpressionBody (#1759)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp7.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Tests.Diagnostics
+{
+    public class ExpressionBodyProperties
+    {
+        private int field;
+
+        private int Property01
+        {
+            get => field;
+            set => field = value; // Noncompliant
+        }
+
+        private int Property02
+        {
+            get => field; // Noncompliant
+            set => field = value;
+        }
+
+        public void Method()
+        {
+            int x;
+
+            x = Property01;
+            Property02 = x;
+        }
+    }
+
+    public class EmptyCtor
+    {
+        // That's invalid syntax, but it is still empty ctor and we should not raise for it, even if it is not used
+        public EmptyCtor() =>
+    }
+}
