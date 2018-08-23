af673312a52ef0f7c7efd2ab23481b0f1a5f5bb1
Amaury Levé <amaury.leve@sonarsource.com>
2018-08-23 14:52:10 +0200

    Rule S2148: Underscores should be used to make large numbers readable

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseNumericLiteralSeparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        void Invalid()
+        {
+            int i = 10000000;  // Noncompliant; is this 10 million or 100 million?
+            int j = 0b01101001010011011110010101011110;  // Noncompliant {{Add underscores to this numeric value for readability.}}
+//                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            long l = 0x7fffffffffffffffL;  // Noncompliant
+        }
+
+        void Valid()
+        {
+            int i = 10_000_000;
+            int j = 0b01101001_01001101_11100101_01011110;
+            long k = 0x7fff_ffff_ffff_ffffL;
+            var l = 1000;
+            var m = 0b001;
+            var n = 0x000;
+        }
+    }
+}
