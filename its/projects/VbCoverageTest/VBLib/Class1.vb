a073f2349bcc8cf7ec0a2289e9890cdd2412a0e6
Amaury Levé <amaury.leve@sonarsource.com>
2018-09-04 16:00:33 +0200

    Add ITs for SonarVB

---
+++ its/projects/VbCoverageTest/VBLib/Class1.vb
@@ -0,0 +1,15 @@
+Public Class Class1
+
+    Public Sub Method()
+        Console.WriteLine("Hello, ")
+
+        If Condition() Then
+            Console.WriteLine("world!")
+        End If
+    End Sub
+
+    Public Function Condition() As Boolean
+        Return False
+    End Function
+
+End Class
---
+++ its/projects/VbNoSonarTest/Class1.vb
@@ -0,0 +1,18 @@
+﻿Public Class IFoo ' NOSONAR
+
+    Public Sub Method()
+        Console.WriteLine("Hello, ")
+
+        If Condition() Then
+            Console.WriteLine("world!")
+        End If
+    End Sub
+
+    Public Function Condition() As Boolean
+        Return False
+    End Function
+
+End Class
+
+Public Class ABCDEFGHIJKLMNOPQRSTUVWXYZ
+End Class
