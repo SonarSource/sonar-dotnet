6092d6c38822c98c3909f72dd9cc03b56eae1bdf
Valeri Hristov <valhristov@users.noreply.github.com>
2018-09-18 15:09:30 +0200

    Switched to msbuild-only protobuf generation (removed build.cmd, move… (#1859)
    * Switched to msbuild-only protobuf generation (removed build.cmd, moved the Google.Protobuf.Tools dependency to Common)
    * adding xplat code

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Protobuf/build.bat
+++
@@ -1,2 +0,0 @@
-"..\..\..\packages\Google.Protobuf.Tools.3.4.0\tools\windows_x64\protoc.exe" -I=. --csharp_out=. ./AnalyzerReport.proto
-"..\..\..\packages\Google.Protobuf.Tools.3.4.0\tools\windows_x64\protoc.exe" -I=./../../../../sonar-ucfg/src/main/protobuf/ --csharp_out=. ./../../../../sonar-ucfg/src/main/protobuf/ucfg.proto
