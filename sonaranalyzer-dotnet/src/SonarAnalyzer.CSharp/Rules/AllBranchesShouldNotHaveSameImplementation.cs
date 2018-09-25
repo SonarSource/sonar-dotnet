360156ea9862180478a150b8e3fe391048e18526
Valeri Hristov <valhristov@users.noreply.github.com>
2018-09-25 16:37:54 +0200

    Rule S3923 : All branches in a conditional structure should not have exactly the same implementation (#1869)

--- sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/AllBranchesShouldNotHaveSameImplementation.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/AllBranchesShouldNotHaveSameImplementation.cs
@@ -20,7 +20,6 @@
 
 using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -32,81 +31,76 @@ namespace SonarAnalyzer.Rules.CSharp
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     [Rule(DiagnosticId)]
-    public sealed class AllBranchesShouldNotHaveSameImplementation : SonarDiagnosticAnalyzer
+    public sealed class AllBranchesShouldNotHaveSameImplementation : AllBranchesShouldNotHaveSameImplementationBase
     {
-        internal const string DiagnosticId = "S3923";
-        private const string MessageFormat = "{0}";
-        private const string SwitchMessage = "Remove this switch or edit its sections so that they are not all the same.";
-        private const string IfMessage = "Remove this if or edit its blocks so that they are not all the same.";
-        private const string TernaryMessage = "Remove this ternary operator or edit it so that when true and when false blocks are not the same.";
-
         private static readonly DiagnosticDescriptor rule =
             DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(rule);
 
         protected override void Initialize(SonarAnalysisContext context)
         {
             context.RegisterSyntaxNodeActionInNonGenerated(
-                c =>
-                {
-                    var switchStatement = (SwitchStatementSyntax)c.Node;
-
-                    if (switchStatement.Sections.Count >= 2 &&
-                        switchStatement.HasDefaultLabel() &&
-                        switchStatement.Sections.Skip(1).All(section =>
-                            SyntaxFactory.AreEquivalent(section.Statements, switchStatement.Sections[0].Statements, false)))
-                    {
-                        c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, switchStatement.GetLocation(), SwitchMessage));
-                    }
-                },
+                new SwitchStatementAnalyzer().GetAnalysisAction(rule, "switch"),
                 SyntaxKind.SwitchStatement);
 
             context.RegisterSyntaxNodeActionInNonGenerated(
-                c =>
-                {
-                    var elseClause = (ElseClauseSyntax)c.Node;
-                    if (elseClause.Statement is IfStatementSyntax)
-                    {
-                        return;
-                    }
-
-                    var elseBlock = elseClause.Statement;
-
-                    var allBlocks = new List<StatementSyntax>();
-                    var currentIfStatement = elseClause.Parent as IfStatementSyntax;
-                    var topLevelIfStatement = currentIfStatement;
-
-                    while (currentIfStatement != null)
-                    {
-                        topLevelIfStatement = currentIfStatement;
-                        allBlocks.Add(currentIfStatement.Statement);
-
-                        // This trick avoids to keep walking up the tree when if is children of another if.
-                        // For example, with the code below Parent.Parent would be IfStatementSyntax causing the code to produce
-                        // wrong results: if (a) { if (b) { if (c) {} } } else { }
-                        currentIfStatement = (currentIfStatement.Parent as ElseClauseSyntax)?.Parent as IfStatementSyntax;
-                    }
-
-                    if (topLevelIfStatement != null && // Should not happen but let's go defensive
-                        allBlocks.All(block => block.IsEquivalentTo(elseBlock, false)))
-                    {
-                        c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, topLevelIfStatement.GetLocation(), IfMessage));
-                    }
-                },
-                SyntaxKind.ElseClause);
+                new TernaryStatementAnalyzer().GetAnalysisAction(rule),
+                SyntaxKind.ConditionalExpression);
 
             context.RegisterSyntaxNodeActionInNonGenerated(
-                c =>
-                {
-                    var ternaryStatement = (ConditionalExpressionSyntax)c.Node;
+                new IfStatementAnalyzer().GetAnalysisAction(rule, "if"),
+                SyntaxKind.ElseClause);
+        }
 
-                    if (ternaryStatement.WhenTrue.RemoveParentheses().IsEquivalentTo(
-                            ternaryStatement.WhenFalse.RemoveParentheses(), false))
-                    {
-                        c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, ternaryStatement.GetLocation(), TernaryMessage));
-                    }
-                },
-                SyntaxKind.ConditionalExpression);
+        private class IfStatementAnalyzer : IfStatementAnalyzerBase<ElseClauseSyntax, IfStatementSyntax>
+        {
+            protected override bool IsLastElseInChain(ElseClauseSyntax elseSyntax) =>
+                !(elseSyntax.Statement is IfStatementSyntax);
+
+            protected override IEnumerable<SyntaxNode> GetStatements(ElseClauseSyntax elseSyntax) =>
+                new[] { elseSyntax.Statement };
+
+            protected override IEnumerable<IEnumerable<SyntaxNode>> GetIfBlocksStatements(ElseClauseSyntax elseSyntax,
+                out IfStatementSyntax topLevelIf)
+            {
+                var allStatements = new List<IEnumerable<SyntaxNode>>();
+
+                var currentElse = elseSyntax;
+
+                topLevelIf = null;
+
+                while (currentElse?.Parent is IfStatementSyntax currentIf)
+                {
+                    topLevelIf = currentIf;
+                    allStatements.Add(new[] { currentIf.Statement });
+                    currentElse = currentIf.Parent as ElseClauseSyntax;
+                }
+
+                return allStatements;
+            }
+        }
+
+        private class TernaryStatementAnalyzer : TernaryStatementAnalyzerBase<ConditionalExpressionSyntax>
+        {
+            protected override SyntaxNode GetWhenFalse(ConditionalExpressionSyntax ternaryStatement) =>
+                ternaryStatement.WhenFalse.RemoveParentheses();
+
+            protected override SyntaxNode GetWhenTrue(ConditionalExpressionSyntax ternaryStatement) =>
+                ternaryStatement.WhenTrue.RemoveParentheses();
+        }
+
+        private class SwitchStatementAnalyzer : SwitchStatementAnalyzerBase<SwitchStatementSyntax, SwitchSectionSyntax>
+        {
+            protected override bool AreEquivalent(SwitchSectionSyntax section1, SwitchSectionSyntax section2) =>
+                SyntaxFactory.AreEquivalent(section1.Statements, section2.Statements);
+
+            protected override IEnumerable<SwitchSectionSyntax> GetSections(SwitchStatementSyntax switchStatement) =>
+                switchStatement.Sections;
+
+            protected override bool HasDefaultLabel(SwitchStatementSyntax switchStatement) =>
+                switchStatement.HasDefaultLabel();
         }
     }
 }
---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/AllBranchesShouldNotHaveSameImplementation.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Helpers.VisualBasic;
+
+namespace SonarAnalyzer.Rules.VisualBasic
+{
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [Rule(DiagnosticId)]
+    public sealed class AllBranchesShouldNotHaveSameImplementation : AllBranchesShouldNotHaveSameImplementationBase
+    {
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                new SelectCaseStatementAnalyzer().GetAnalysisAction(rule, "Select Case"),
+                SyntaxKind.SelectBlock);
+
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                new TernaryStatementAnalyzer().GetAnalysisAction(rule),
+                SyntaxKind.TernaryConditionalExpression);
+
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                new IfStatementAnalyzer().GetAnalysisAction(rule, "If"),
+                SyntaxKind.ElseBlock);
+
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                new SingleLineIfStatementAnalyzer().GetAnalysisAction(rule, "If"),
+                SyntaxKind.SingleLineElseClause);
+        }
+
+        private class IfStatementAnalyzer : IfStatementAnalyzerBase<ElseBlockSyntax, MultiLineIfBlockSyntax>
+        {
+            protected override bool IsLastElseInChain(ElseBlockSyntax elseSyntax) => true;
+
+            protected override IEnumerable<SyntaxNode> GetStatements(ElseBlockSyntax elseSyntax) =>
+                elseSyntax.Statements;
+
+            protected override IEnumerable<IEnumerable<SyntaxNode>> GetIfBlocksStatements(ElseBlockSyntax elseSyntax,
+                out MultiLineIfBlockSyntax topLevelIf)
+            {
+                topLevelIf = (MultiLineIfBlockSyntax)elseSyntax.Parent;
+                return topLevelIf.ElseIfBlocks
+                    .Select(elseif => elseif.Statements.Cast<SyntaxNode>())
+                    .Concat(new[] { topLevelIf.Statements.Cast<SyntaxNode>() });
+            }
+        }
+
+        private class TernaryStatementAnalyzer : TernaryStatementAnalyzerBase<TernaryConditionalExpressionSyntax>
+        {
+            protected override SyntaxNode GetWhenFalse(TernaryConditionalExpressionSyntax ternaryStatement) =>
+                ternaryStatement.WhenFalse.RemoveParentheses();
+
+            protected override SyntaxNode GetWhenTrue(TernaryConditionalExpressionSyntax ternaryStatement) =>
+                ternaryStatement.WhenTrue.RemoveParentheses();
+        }
+
+        private class SingleLineIfStatementAnalyzer : IfStatementAnalyzerBase<SingleLineElseClauseSyntax, SingleLineIfStatementSyntax>
+        {
+            protected override IEnumerable<IEnumerable<SyntaxNode>> GetIfBlocksStatements(SingleLineElseClauseSyntax elseSyntax,
+                out SingleLineIfStatementSyntax topLevelIf)
+            {
+                topLevelIf = (SingleLineIfStatementSyntax)elseSyntax.Parent;
+                return new[] { topLevelIf.Statements.Cast<SyntaxNode>() };
+            }
+
+            protected override IEnumerable<SyntaxNode> GetStatements(SingleLineElseClauseSyntax elseSyntax) =>
+                elseSyntax.Statements;
+
+            protected override bool IsLastElseInChain(SingleLineElseClauseSyntax elseSyntax) => true;
+        }
+
+        private class SelectCaseStatementAnalyzer : SwitchStatementAnalyzerBase<SelectBlockSyntax, CaseBlockSyntax>
+        {
+            protected override bool AreEquivalent(CaseBlockSyntax section1, CaseBlockSyntax section2) =>
+                SyntaxFactory.AreEquivalent(section1.Statements, section2.Statements);
+
+            protected override IEnumerable<CaseBlockSyntax> GetSections(SelectBlockSyntax switchStatement) =>
+                switchStatement.CaseBlocks;
+
+            protected override bool HasDefaultLabel(SelectBlockSyntax switchStatement) =>
+                switchStatement.CaseBlocks.Any(section => section.IsKind(SyntaxKind.CaseElseBlock));
+        }
+    }
+}
