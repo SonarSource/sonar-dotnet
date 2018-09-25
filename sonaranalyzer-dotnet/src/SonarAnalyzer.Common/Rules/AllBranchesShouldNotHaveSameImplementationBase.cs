09886fa875103f80f3a7683c5a36e9e66c71a606
Valeri Hristov <valhristov@users.noreply.github.com>
2018-09-25 16:37:54 +0200

    Rule S3923 : All branches in a conditional structure should not have exactly the same implementation (#1869)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/AllBranchesShouldNotHaveSameImplementationBase.cs
@@ -0,0 +1,129 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class AllBranchesShouldNotHaveSameImplementationBase : SonarDiagnosticAnalyzer
+    {
+        protected const string SelectMessage =
+            "Remove this '{0}' or edit its sections so that they are not all the same.";
+
+        protected const string TernaryMessage =
+            "Remove this ternary operator or edit it so that when true and when false blocks are not the same.";
+
+        protected const string IfMessage =
+            "Remove this '{0}' or edit its blocks so that they are not all the same.";
+
+        internal const string DiagnosticId = "S3923";
+        internal const string MessageFormat = "{0}";
+
+        protected abstract class IfStatementAnalyzerBase<TElseSyntax, TIfSyntax>
+            where TElseSyntax : SyntaxNode
+            where TIfSyntax : SyntaxNode
+        {
+            protected abstract IEnumerable<SyntaxNode> GetStatements(TElseSyntax elseSyntax);
+
+            protected abstract IEnumerable<IEnumerable<SyntaxNode>> GetIfBlocksStatements(TElseSyntax elseSyntax,
+                out TIfSyntax topLevelIf);
+
+            protected abstract bool IsLastElseInChain(TElseSyntax elseSyntax);
+
+            public Action<SyntaxNodeAnalysisContext> GetAnalysisAction(DiagnosticDescriptor rule, params object[] messageArgs) =>
+                context =>
+                {
+                    var elseSyntax = (TElseSyntax)context.Node;
+
+                    if (!IsLastElseInChain(elseSyntax))
+                    {
+                        return;
+                    }
+
+                    var ifBlocksStatements = GetIfBlocksStatements(elseSyntax, out var topLevelIf);
+
+                    var elseStatements = GetStatements(elseSyntax);
+
+                    if (ifBlocksStatements.All(ifStatements => AreEquivalent(ifStatements, elseStatements)))
+                    {
+                        var message = string.Format(IfMessage, messageArgs);
+                        context.ReportDiagnosticWhenActive(Diagnostic.Create(rule, topLevelIf.GetLocation(), message));
+                    }
+                };
+
+            private static bool AreEquivalent(IEnumerable<SyntaxNode> nodes1, IEnumerable<SyntaxNode> nodes2) =>
+                nodes1.Equals(nodes2, (x, y) => x.IsEquivalentTo(y, topLevel: false));
+        }
+
+        protected abstract class TernaryStatementAnalyzerBase<TTernaryStatement>
+            where TTernaryStatement : SyntaxNode
+        {
+            protected abstract SyntaxNode GetWhenTrue(TTernaryStatement ternaryStatement);
+
+            protected abstract SyntaxNode GetWhenFalse(TTernaryStatement ternaryStatement);
+
+            public Action<SyntaxNodeAnalysisContext> GetAnalysisAction(DiagnosticDescriptor rule) =>
+                context =>
+                {
+                    var ternaryStatement = (TTernaryStatement)context.Node;
+
+                    var whenTrue = GetWhenTrue(ternaryStatement);
+                    var whenFalse = GetWhenFalse(ternaryStatement);
+
+                    if (whenTrue.IsEquivalentTo(whenFalse, topLevel: false))
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(rule, ternaryStatement.GetLocation(), TernaryMessage));
+                    }
+                };
+        }
+
+        protected abstract class SwitchStatementAnalyzerBase<TSwitchStatement, TSwitchSection>
+            where TSwitchStatement : SyntaxNode
+            where TSwitchSection : SyntaxNode
+        {
+            protected abstract IEnumerable<TSwitchSection> GetSections(TSwitchStatement switchStatement);
+
+            protected abstract bool HasDefaultLabel(TSwitchStatement switchStatement);
+
+            protected abstract bool AreEquivalent(TSwitchSection section1, TSwitchSection section2);
+
+            public Action<SyntaxNodeAnalysisContext> GetAnalysisAction(DiagnosticDescriptor rule, params object[] messageArgs) =>
+                context =>
+                {
+                    var switchStatement = (TSwitchStatement)context.Node;
+
+                    var sections = GetSections(switchStatement).ToList();
+
+                    if (sections.Count >= 2 &&
+                        HasDefaultLabel(switchStatement) &&
+                        sections.Skip(1).All(section => AreEquivalent(section, sections[0])))
+                    {
+                        var message = string.Format(SelectMessage, messageArgs);
+                        context.ReportDiagnostic(Diagnostic.Create(rule, switchStatement.GetLocation(), message));
+                    }
+                };
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/AllBranchesShouldNotHaveSameImplementation.vb
@@ -0,0 +1,105 @@
+﻿Imports System
+
+Namespace Tests.Diagnostics
+    Public Class Program
+        Public Sub IfElseCases()
+            If b = 0 Then 'Noncompliant
+                DoSomething()
+            ElseIf b = 1 Then
+                DoSomething()
+            Else
+                DoSomething()
+            End If
+
+            If b = 0 Then 'Noncompliant
+                If c = 1 Then 'Noncompliant
+                    DoSomething()
+                Else
+                    DoSomething()
+                End If
+            Else
+                If c = 1 Then 'Noncompliant
+                    DoSomething()
+                Else
+                    DoSomething()
+                End If
+            End If
+
+            If b = 0 Then
+                DoSomething()
+            Else
+                DoSomethingElse()
+            End If
+
+            If b = 0 Then 'Compliant, no else
+                DoSomething()
+            ElseIf b = 1 Then
+                DoSomething()
+            End If
+        End Sub
+
+        Public Sub SwitchCases()
+            Select Case i ' Noncompliant {{Remove this 'Select Case' or edit its sections so that they are not all the same.}}
+                Case 1
+                    DoSomething()
+                Case 2
+                    DoSomething()
+                Case 3
+                    DoSomething()
+                Case Else
+                    DoSomething()
+            End Select
+
+            Select Case i ' Noncompliant
+                Case 1
+                    DoSomething()
+                    Exit Select
+                Case 2
+                    DoSomething()
+                    Exit Select
+                Case 3
+                    DoSomething()
+                    Exit Select
+                Case Else
+                    DoSomething()
+                    Exit Select
+            End Select
+
+            Select Case i
+                Case 1
+                    DoSomething()
+                Case Else
+                    DoSomethingElse()
+            End Select
+
+            Select Case i
+                Case 1
+                    DoSomething()
+                Case 2
+                    DoSomething()
+                Case 3
+                    DoSomething()
+            End Select
+        End Sub
+
+        Public Sub TernaryCases(ByVal c As Boolean)
+            Dim b As Integer = If(a > 12, 4, 4) 'Noncompliant {{Remove this ternary operator or edit it so that when true and when false blocks are not the same.}}
+            Dim x = If(1 > 18, True, True) 'Noncompliant
+            Dim y = If(1 > 18, True, False) 
+            y = If(1 > 18, (True), True) 'Noncompliant
+            TernaryCases(If(1 > 18, (True), True)) 'Noncompliant
+        End Sub
+
+        Public Sub SingleLineIfCases(ByVal c As Boolean)
+            Dim x As Integer
+            If c Then x = 4 else x = 4 'Noncompliant {{Remove this 'If' or edit its blocks so that they are not all the same.}}
+            If c Then x = 0 else x = 1
+        End Sub
+
+        Private Sub DoSomething()
+        End Sub
+
+        Private Sub DoSomethingElse()
+        End Sub
+    End Class
+End Namespace
