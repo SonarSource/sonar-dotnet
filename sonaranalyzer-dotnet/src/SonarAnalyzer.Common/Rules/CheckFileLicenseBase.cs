b3ecfdb607f2cdf43551b9c093b7c88c219ceb0f
Loïc Joly <loic.joly@sonarsource.com>
2018-09-26 17:03:00 +0200

    RSPEC-1451 Track lack of copyright and license headers (for VB)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/CheckFileLicenseBase.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using System;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class CheckFileLicenseBase : ParameterLoadingDiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "S1451";
+        protected const string MessageFormat = "Add or update the header of this file.";
+
+        internal const string HeaderFormatRuleParameterKey = "headerFormat";
+        internal const string HeaderFormatPropertyKey = nameof(HeaderFormat);
+        public abstract string HeaderFormat { get; set; }
+
+        internal const string IsRegularExpressionRuleParameterKey = "isRegularExpression";
+        internal const string IsRegularExpressionPropertyKey = nameof(IsRegularExpression);
+        internal const string IsRegularExpressionDefaultValue = "false";
+        [RuleParameter(IsRegularExpressionRuleParameterKey, PropertyType.Boolean,
+            "Whether the headerFormat is a regular expression.", IsRegularExpressionDefaultValue)]
+        public bool IsRegularExpression { get; set; } = bool.Parse(IsRegularExpressionDefaultValue);
+
+        protected static bool IsRegexPatternValid(string pattern)
+        {
+            try
+            {
+                Regex.Match(string.Empty, pattern);
+                return true;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+        }
+
+        protected bool HasValidLicenseHeader(SyntaxNode node)
+        {
+            if (node == null || !node.HasLeadingTrivia)
+            {
+                return false;
+            }
+
+            var trivias = node.GetLeadingTrivia();
+            if (IsEndOfLine(trivias.Last()))
+            {
+                trivias = trivias.RemoveAt(trivias.Count - 1);
+            }
+
+            var header = trivias.ToString();
+            return header != null && AreHeadersEqual(header);
+        }
+
+        protected abstract bool IsEndOfLine(SyntaxTrivia trivia);
+
+        protected bool AreHeadersEqual(string currentHeader)
+        {
+            var unixEndingHeader = currentHeader.Replace("\r\n", "\n");
+            var unixEndingHeaderFormat = HeaderFormat.Replace("\r\n", "\n").Replace("\\r\\n", "\n");
+
+            return IsRegularExpression
+                ? Regex.IsMatch(unixEndingHeader, unixEndingHeaderFormat)
+                : unixEndingHeader.Equals(unixEndingHeaderFormat, StringComparison.Ordinal);
+        }
+
+        protected ImmutableDictionary<string, string> CreateDiagnosticProperties()
+        {
+            return ImmutableDictionary<string, string>.Empty
+                .Add(HeaderFormatPropertyKey, HeaderFormat)
+                .Add(IsRegularExpressionPropertyKey, IsRegularExpression.ToString());
+        }
+    }
+}
