82234fc0e75e4cfb93b5d7d33cfdd6c2aa6ae50a
Loïc Joly <37904001+loic-joly-sonarsource@users.noreply.github.com>
2018-09-26 12:01:02 +0200

    Rule S1301:  "switch" statements should have at least 3 "case" clauses (#1882)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SwitchCasesMinimumThree.vb
@@ -0,0 +1,22 @@
+﻿Namespace Tests.Diagnostics
+    Public Class SwitchCasesMinimumThree
+        Public Sub New(ByVal n As Integer)
+            Select Case n ' Noncompliant {{Replace this 'Select' statement with 'If' statements to increase readability.}}
+'           ^^^^^^
+                Case 0
+                Case Else
+            End Select
+
+            Select Case n ' Noncompliant {{Replace this 'Select' statement with 'If' statements to increase readability.}}
+'           ^^^^^^
+            End Select
+
+            Select Case n 'Compliant, 3 cases
+                Case 0, 1
+                Case Else
+                    Dim x = 5
+            End Select
+        End Sub
+    End Class
+End Namespace
+
