31c4c0a5d7fd431d21d32dda84d119ba3f796d39
Loïc Joly <37904001+loic-joly-sonarsource@users.noreply.github.com>
2018-09-26 12:18:38 +0200

    Ruls S3358: Ternary operators should not be nested (VB) (#1889)

--- sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/DoNotNestTernaryOperators.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/DoNotNestTernaryOperators.cs
@@ -18,37 +18,39 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using SonarAnalyzer.Common;
 using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Rules.Common;
 
-namespace SonarAnalyzer.Rules.CSharp
+namespace SonarAnalyzer.Rules.VisualBasic
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
     [Rule(DiagnosticId)]
     public sealed class DoNotNestTernaryOperators : DoNotNestTernaryOperatorsBase
     {
-        private const string MessageFormat = "Extract this nested ternary operation into an independent statement.";
-
+        private const string MessageFormat = "Extract this nested If operator into independent If...Then...Else statements.";
         private static readonly DiagnosticDescriptor rule =
             DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
 
         protected override void Initialize(SonarAnalysisContext context)
         {
-            context.RegisterSyntaxNodeActionInNonGenerated(c =>
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
                 {
-                    if (c.Node.Ancestors().OfType<ConditionalExpressionSyntax>().Any())
+                    if (c.Node.Ancestors().OfType<TernaryConditionalExpressionSyntax>().Any())
                     {
                         c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, c.Node.GetLocation()));
                     }
                 },
-                SyntaxKind.ConditionalExpression);
+                SyntaxKind.TernaryConditionalExpression);
         }
     }
 }
