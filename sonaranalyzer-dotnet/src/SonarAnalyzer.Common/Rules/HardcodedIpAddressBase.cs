9301bc4164fe1a56b162cc69bcad48f036004159
Valeri Hristov <valhristov@users.noreply.github.com>
2018-09-27 16:55:44 +0200

    Rule S1313: IP addresses should not be hardcoded (#1902)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/HardcodedIpAddressBase.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class HardcodedIpAddressBase<TLiteralExpression> : HotspotDiagnosticAnalyzer
+        where TLiteralExpression : SyntaxNode
+    {
+        protected const string DiagnosticId = "S1313";
+        protected const string MessageFormat = "Make sure using this hardcoded IP address '{0}' is safe here.";
+
+        private static readonly ISet<string> IgnoredVariableNames =
+            new HashSet<string>
+            {
+                "VERSION",
+                "ASSEMBLY",
+            };
+
+        protected abstract string GetAssignedVariableName(TLiteralExpression stringLiteral);
+        protected abstract string GetValueText(TLiteralExpression literalExpression);
+        protected abstract bool HasAttributes(TLiteralExpression literalExpression);
+
+        protected HardcodedIpAddressBase(IAnalyzerConfiguration analysisConfiguration) : base(analysisConfiguration)
+        {
+        }
+
+        protected Action<SyntaxNodeAnalysisContext> GetAnalysisAction(DiagnosticDescriptor rule) =>
+            c =>
+            {
+                var stringLiteral = (TLiteralExpression)c.Node;
+                var literalValue = GetValueText(stringLiteral);
+
+                if (literalValue == "::" ||
+                    literalValue == "127.0.0.1" ||
+                    !IPAddress.TryParse(literalValue, out var address))
+                {
+                    return;
+                }
+
+                if (address.AddressFamily == AddressFamily.InterNetwork &&
+                    literalValue.Split('.').Length != 4)
+                {
+                    return;
+                }
+
+                var variableName = GetAssignedVariableName(stringLiteral);
+                if (variableName != null &&
+                    IgnoredVariableNames.Any(variableName.Contains))
+                {
+                    return;
+                }
+
+                if (HasAttributes(stringLiteral))
+                {
+                    return;
+                }
+
+                c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, stringLiteral.GetLocation(), literalValue));
+            };
+    }
+}
---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/HardcodedIpAddress.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Helpers.VisualBasic;
+
+namespace SonarAnalyzer.Rules.VisualBasic
+{
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [Rule(DiagnosticId)]
+    public sealed class HardcodedIpAddress : HardcodedIpAddressBase<LiteralExpressionSyntax>
+    {
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        public HardcodedIpAddress()
+            : base(new DefaultAnalyzerConfiguration())
+        {
+        }
+
+        public HardcodedIpAddress(IAnalyzerConfiguration analysisConfiguration)
+            : base(analysisConfiguration)
+        {
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+            ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterCompilationStartAction(
+                ccc =>
+                {
+                    if (!IsEnabled(ccc.Options))
+                    {
+                        return;
+                    }
+
+                    context.RegisterSyntaxNodeActionInNonGenerated(
+                        GetAnalysisAction(rule),
+                        SyntaxKind.StringLiteralExpression);
+                });
+        }
+
+        protected override string GetValueText(LiteralExpressionSyntax literalExpression) =>
+            literalExpression.Token.ValueText;
+
+        protected override bool HasAttributes(LiteralExpressionSyntax literalExpression) =>
+            literalExpression.Ancestors().AnyOfKind(SyntaxKind.Attribute);
+
+        protected override string GetAssignedVariableName(LiteralExpressionSyntax stringLiteral) =>
+            stringLiteral.FirstAncestorOrSelf<SyntaxNode>(IsVariableIdentifier)?.ToString().ToUpperInvariant();
+
+        private static bool IsVariableIdentifier(SyntaxNode syntaxNode) =>
+            syntaxNode is StatementSyntax ||
+            syntaxNode is VariableDeclaratorSyntax ||
+            syntaxNode is ParameterSyntax;
+    }
+}
