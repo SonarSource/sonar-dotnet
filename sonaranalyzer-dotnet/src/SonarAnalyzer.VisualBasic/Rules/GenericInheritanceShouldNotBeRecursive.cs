a70f2fd97c5cbcd54be50c8e3db93f90b5f6bf67
Amaury Levé <amaury.leve@sonarsource.com>
2018-09-28 14:41:30 +0200

    Fix indentation and re-order/remove usings

--- sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/GenericInheritanceShouldNotBeRecursive.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/GenericInheritanceShouldNotBeRecursive.cs
@@ -18,16 +18,13 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.VisualBasic;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using SonarAnalyzer.Common;
 using SonarAnalyzer.Helpers;
-using SonarAnalyzer.Rules.Common;
 
 namespace SonarAnalyzer.Rules.VisualBasic
 {
@@ -49,7 +46,7 @@ namespace SonarAnalyzer.Rules.VisualBasic
                     {
                         c.ReportDiagnosticWhenActive(
                             Diagnostic.Create(rule, typeDeclaration.BlockStatement.Identifier.GetLocation(),
-                            typeDeclaration.BlockStatement.DeclarationKeyword));
+                                typeDeclaration.BlockStatement.DeclarationKeyword));
                     }
                 },
                 SyntaxKind.ClassBlock,
