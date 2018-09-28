821eeb6c5dafd1d2d555254b1d3b40d521050caa
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2018-09-28 17:05:33 +0200

    SONARVB-260 Rule S4143: do not overwrite collection items (#1907)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/DoNotOverwriteCollectionElements.cs
@@ -0,0 +1,188 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.VisualBasic
+{
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [Rule(DiagnosticId)]
+    public sealed class DoNotOverwriteCollectionElements
+        : DoNotOverwriteCollectionElementsBase<InvocationExpressionSyntax, IdentifierNameSyntax, StatementSyntax,
+            MemberAccessExpressionSyntax, MeExpressionSyntax, MyBaseExpressionSyntax, ExpressionStatementSyntax>
+    {
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                new AssignmentVerifier(this).GetAnalysisAction(rule),
+                SyntaxKind.SimpleAssignmentStatement);
+            context.RegisterSyntaxNodeActionInNonGenerated(
+               new InvocationVerifier(this).GetAnalysisAction(rule),
+               SyntaxKind.InvocationExpression);
+        }
+
+        protected override SyntaxNode GetExpression(InvocationExpressionSyntax invocation) => invocation.Expression;
+
+        protected override SyntaxNode GetExpression(MemberAccessExpressionSyntax memberAccess) => memberAccess.Expression;
+
+        protected override SyntaxNode GetName(MemberAccessExpressionSyntax memberAccess) => memberAccess.Name;
+
+        protected override SyntaxToken GetIdentifierToken(IdentifierNameSyntax invocation) => invocation.Identifier;
+
+        protected override SyntaxToken GetIdentifierToken(MemberAccessExpressionSyntax memberAccess) => memberAccess.Name.Identifier;
+
+        protected override SyntaxToken GetIdentifierToken(MeExpressionSyntax thisAccess) => thisAccess.Keyword;
+
+        protected override SyntaxToken GetIdentifierToken(MyBaseExpressionSyntax baseAccess) => baseAccess.Keyword;
+
+        private class InvocationVerifier : InvocationVerifierBase
+        {
+            private readonly DoNotOverwriteCollectionElements analyzer;
+
+            public InvocationVerifier(DoNotOverwriteCollectionElements analyzer)
+                : base(analyzer.GetInvokedOnName, analyzer.GetPreviousStatements)
+            {
+                this.analyzer = analyzer;
+            }
+
+            internal override KnownType DictionaryType => KnownType.System_Collections_Generic_IDictionary_TKey_TValue_VB;
+
+            protected override bool HasNumberOfArguments(InvocationExpressionSyntax invocation, int number) =>
+                invocation.ArgumentList != null && invocation.ArgumentList.Arguments.Count == number;
+
+            protected override SyntaxNode GetFirstArgument(InvocationExpressionSyntax invocation) =>
+                invocation.ArgumentList != null && invocation.ArgumentList.Arguments.Count >= 1
+                    ? invocation.ArgumentList.Arguments[0]
+                    : null;
+
+            protected override SyntaxNode GetExpression(ExpressionStatementSyntax expression) =>
+                expression.Expression;
+
+            protected override InvocationExpressionSyntax GetInvocation(SyntaxNode node) =>
+                (InvocationExpressionSyntax) node;
+
+            protected override SyntaxNode GetExpression(MemberAccessExpressionSyntax memberAccess) =>
+                analyzer.GetExpression(memberAccess);
+
+            protected override SyntaxNode GetExpression(InvocationExpressionSyntax memberAccess) =>
+                analyzer.GetExpression(memberAccess);
+
+            protected override SyntaxToken GetIdentifierToken(MemberAccessExpressionSyntax memberAccess) =>
+                analyzer.GetIdentifierToken(memberAccess);
+
+            protected override SyntaxNode GetName(MemberAccessExpressionSyntax memberAccess) =>
+                analyzer.GetName(memberAccess);
+        }
+
+        private class AssignmentVerifier : AssignmentVerifierBase<AssignmentStatementSyntax, InvocationExpressionSyntax>
+        {
+            private readonly DoNotOverwriteCollectionElements analyzer;
+            public AssignmentVerifier(DoNotOverwriteCollectionElements analyzer)
+            {
+                this.analyzer = analyzer;
+            }
+
+            protected override void Report(SyntaxNodeAnalysisContext c, DiagnosticDescriptor rule,
+                InvocationExpressionSyntax elementAccess, InvocationExpressionSyntax previousAssignmentOfVariable)
+            {
+                bool IsCollectionOrProperty(InvocationExpressionSyntax invocation) =>
+                    (c.SemanticModel.GetSymbolInfo(invocation).Symbol is ISymbol symbol) &&
+                    (IsCollection(symbol) || symbol.Kind == SymbolKind.Property);
+                bool IsArray(ExpressionSyntax expression) =>
+                    (c.SemanticModel.GetSymbolInfo(expression).Symbol is ISymbol symbol) &&
+                    symbol.GetSymbolType().DerivesOrImplements(KnownType.System_Array);
+
+                if (IsCollectionOrProperty(elementAccess) && IsCollectionOrProperty(previousAssignmentOfVariable))
+                {
+                    c.ReportDiagnosticWhenActive(
+                        Diagnostic.Create(rule, elementAccess.GetLocation(),
+                            additionalLocations: new[] { previousAssignmentOfVariable.GetLocation() }));
+                    return;
+                }
+                if (IsArray(elementAccess.Expression) && IsArray(previousAssignmentOfVariable.Expression))
+                {
+                    c.ReportDiagnosticWhenActive(
+                        Diagnostic.Create(rule, elementAccess.GetLocation(),
+                            additionalLocations: new[] { previousAssignmentOfVariable.GetLocation() }));
+                }
+            }
+
+            protected override InvocationExpressionSyntax GetElementAccess(AssignmentStatementSyntax assignment)
+            {
+                if (assignment.Left is InvocationExpressionSyntax elementAccess &&
+                    elementAccess.ArgumentList != null &&
+                    elementAccess.ArgumentList.Arguments.Count != 0)
+                {
+                    return elementAccess;
+                }
+                return null;
+            }
+
+            protected override string GetCollectionIdentifier(InvocationExpressionSyntax elementAccess) =>
+                analyzer.GetInvokedOnName(elementAccess);
+
+            protected override InvocationExpressionSyntax GetPreviousAssignmentOfVariable(InvocationExpressionSyntax elementAccess,
+                    string collectionIdentifier, IEnumerable<string> arguments) =>
+                analyzer.GetPreviousStatements(elementAccess)
+                .OfType<AssignmentStatementSyntax>()
+                .TakeWhile(e => IsElementAccessAssignmentOnSameItem(e, collectionIdentifier))
+                .Select(aes => aes.Left)
+                .Cast<InvocationExpressionSyntax>()
+                .WhereNotNull()
+                .FirstOrDefault(eaes => arguments.SequenceEqual(
+                    eaes.ArgumentList.Arguments.Select(a => a.ToString())));
+
+            private bool IsElementAccessAssignmentOnSameItem(SyntaxNode expression, string accessedOn) =>
+                expression is AssignmentStatementSyntax aes &&
+                aes.Left is InvocationExpressionSyntax currentElementAccess &&
+                analyzer.GetInvokedOnName(currentElementAccess) is string currentAccessedOn &&
+                currentElementAccess.ArgumentList != null &&
+                (currentElementAccess.Expression.IsKind(SyntaxKind.SimpleMemberAccessExpression) ||
+                currentElementAccess.Expression.IsKind(SyntaxKind.IdentifierName)) &&
+                accessedOn != null &&
+                currentAccessedOn == accessedOn;
+
+            protected override AssignmentStatementSyntax GetAssignment(SyntaxNode node) =>
+                (AssignmentStatementSyntax) node;
+
+            protected override IEnumerable<string> GetArguments(InvocationExpressionSyntax elementAccess) =>
+                elementAccess.ArgumentList.Arguments.Select(a => a.ToString());
+
+            private bool IsCollection(ISymbol symbol) =>
+                symbol.OriginalDefinition != null && symbol.OriginalDefinition.ContainingType != null &&
+                symbol.OriginalDefinition.ContainingType
+                    .DerivesOrImplements(KnownType.System_Collections_Generic_ICollection_T_VB);
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DoNotOverwriteCollectionElements.vb
@@ -0,0 +1,182 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Namespace Tests.TestCases
+  Class TestCases
+
+    Public Shared dictionaryField As IDictionary(Of Integer, Integer)
+    Public Shared dictionaryField2 As IDictionary(Of Integer, Integer)
+    Public fooList() As Foo 
+
+    Private Sub SameIndexOnDictionaryItem(ByVal dict As Dictionary(Of Integer, Integer))
+      dict.Item(0) = 0 ' Secondary
+'     ^^^^^^^^^^^^
+      dict.Item(0) = 1 ' Noncompliant {{Verify this is the index/key that was intended; a value has already been set for it.}}
+'     ^^^^^^^^^^^^
+    End Sub
+
+    Private Sub MeUsage
+      Me.dictionaryField.Item(0) = 1 ' Secondary
+      Me.dictionaryField.Item(0) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub MyClassUsage
+      MyClass.dictionaryField.Item(0) = 1 ' Secondary
+      MyClass.dictionaryField.Item(0) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexOnArray1(ByVal array() As Integer)
+      array(0) = 0 ' Secondary
+      array(0) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexOnArray2(ByVal array() As Integer)
+      array(0) = 1 ' Secondary
+      array(0) = (array(0) + 1) ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexOnArray3(ByVal obj As CustomIndexerOneArg)
+      obj("foo") = 0 ' Secondary
+      obj("foo") = 1 ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexOnArray4(ByVal obj As CustomIndexerMultiArg)
+      obj("s", 1, 1) = 0 ' Secondary
+      obj("s", 1, 1) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexOnList(ByVal list As List(Of Integer))
+      list(0) = 0 ' Secondary
+      list(0) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub SameIndexSpacedOut(ByVal names() As String)
+      names("a") = "a" ' Secondary
+      names("b") = "b"
+      names("a") = "c" ' Noncompliant
+    End Sub
+
+    Private Sub NonSequentialAccessOnSameIndex(ByVal values() As Integer)
+      Dim index As Integer = 0
+      values(0) = 1
+      index = (index + 1)
+      values(0) = 2 ' FN - We only take consecutive element access
+    End Sub
+
+    Private Sub NonConstantConsecutiveIndexAccess(ByVal values() As Integer)
+      Dim index As Integer = 0
+      values(index) = 1 ' Secondary
+      values(index) = 2 ' Noncompliant
+    End Sub
+
+    Private Sub IDictionaryAdd(ByVal dict As IDictionary(Of Integer, Integer))
+      dict.Add(0, 0) ' Secondary
+'     ^^^^^^^^^^^^^^
+      dict.Add(0, 1) ' Noncompliant
+'     ^^^^^^^^^^^^^^
+    End Sub
+
+    Private Sub IDictionaryAddLowercase(ByVal dict As IDictionary(Of Integer, Integer))
+      dict.add(0, 0) ' Secondary
+'     ^^^^^^^^^^^^^^
+      dict.add(0, 1) ' Noncompliant
+'     ^^^^^^^^^^^^^^
+    End Sub
+
+    Private Sub IDictionaryAddMixedCase(ByVal dict As IDictionary(Of Integer, Integer))
+      dict.add(0, 0) ' Secondary
+'     ^^^^^^^^^^^^^^
+      dict.ADD(0, 1) ' Noncompliant
+'     ^^^^^^^^^^^^^^
+    End Sub
+
+    Private Sub DictionaryAdd(ByVal dict As Dictionary(Of Integer, Integer))
+      dict.Add(0, 0) ' Secondary
+      dict.Add(0, 1) ' Noncompliant
+    End Sub
+
+    Private Sub IDictionaryAddOnMultiMemberAccess(ByVal c As TestCases)
+      ' The below code will throw an ArgumentException at runtime
+      c.dictionaryField.Add(0, 0) ' Secondary
+      c.dictionaryField.Add(0, 1) ' Noncompliant
+    End Sub
+
+    Private Sub DoNotReportOnNonDictionaryAdd(ByVal c As CustomAddItem)
+      c.Add(0, 1)
+      c.Add(0, 2) ' Compliant this is not on a dictionary
+    End Sub
+
+
+    Private Sub MeWithDifferent()
+      Me.dictionaryField(0) = 1 ' Compliant
+      Me.dictionaryField2(0) = 1 ' Compliant
+    End Sub
+
+    Private Sub MyClassWithDifferent()
+      MyClass.dictionaryField(0) = 1 ' Compliant
+      MyClass.dictionaryField2(0) = 1 ' Compliant
+    End Sub
+
+    Private Sub ArrayFields()
+      fooList(0).FooField = 1
+      fooList(0).BarField = 1
+      AddHandler fooList(0).Ev1, AddressOf Handler
+      AddHandler fooList(0).Ev2, AddressOf Handler
+    End Sub
+
+    Private Sub Handler()
+    End Sub
+
+  End Class
+
+  Class InheritTestCases
+    Inherits TestCases
+
+    Private Sub BaseSame()
+      MyBase.dictionaryField(0) = 1 ' Secondary
+      MyBase.dictionaryField(0) = 1 ' Noncompliant
+    End Sub
+
+    Private Sub BaseDifferent()
+      MyBase.dictionaryField(0) = 1 ' Compliant
+      MyBase.dictionaryField2(0) = 1 ' Compliant
+    End Sub
+
+  End Class
+
+  Class Foo
+    Public FooField
+    Public BarField
+    Public Event Ev1()
+    Public Event Ev2()
+  End Class
+
+  Class CustomIndexerOneArg
+
+    Default Property Item(ByVal key As String) As Integer
+      Get
+        Return 1
+      End Get
+      Set
+
+      End Set
+    End Property
+  End Class
+  Class CustomIndexerMultiArg
+
+    Default Property Item(ByVal s As String, ByVal i As Integer, ByVal d As Double) As Integer
+      Get
+        Return 1
+      End Get
+      Set
+
+      End Set
+    End Property
+  End Class
+  Class CustomAddItem
+    Public Sub Add(ByVal a As Integer, ByVal b As Integer)
+    End Sub
+  End Class
+End Namespace
