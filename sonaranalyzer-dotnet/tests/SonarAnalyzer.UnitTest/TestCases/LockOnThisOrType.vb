a7cd1847bab42557ffde059992a3d423bf25af34
Loïc Joly <37904001+loic-joly-sonarsource@users.noreply.github.com>
2018-10-02 15:31:15 +0200

    RSPEC-2551 Types and "Me" should not be used for locking (for VB) (#1933)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/LockOnThisOrType.vb
@@ -0,0 +1,25 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Namespace Tests.Diagnostics
+    Public Class LockOnThisOrType
+        Public Sub MyLockingMethod()
+            SyncLock Me ' Noncompliant {{Lock on a new 'object' instead.}}
+'                    ^^
+            End SyncLock
+
+            SyncLock lockObj
+            End SyncLock
+
+            SyncLock GetType(LockOnThisOrType) ' Noncompliant
+'                    ^^^^^^^^^^^^^^^^^^^^^^^^^
+            End SyncLock
+
+            SyncLock (New LockOnThisOrType()).[GetType]() ' Noncompliant
+'                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            End SyncLock
+        End Sub
+
+        Dim lockObj As New Object()
+    End Class
+End Namespace
