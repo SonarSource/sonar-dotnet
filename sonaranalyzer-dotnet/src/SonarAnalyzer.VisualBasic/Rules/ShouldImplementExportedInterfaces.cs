f09c32a23df29f28b83adfc053932fc7f7b7dd15
Amaury Levé <amaury.leve@sonarsource.com>
2018-10-02 15:11:04 +0200

    SONARVB-331: Rule S4159: Classes should implement their 'ExportAttribute' interfaces

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/ShouldImplementExportedInterfaces.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Rules.Common;
+
+namespace SonarAnalyzer.Rules.VisualBasic
+{
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [Rule(DiagnosticId)]
+    public sealed class ShouldImplementExportedInterfaces : ShouldImplementExportedInterfacesBase<ArgumentSyntax, ExpressionSyntax, ClassStatementSyntax>
+    {
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
+                {
+                    var attributeSyntax = (AttributeSyntax)c.Node;
+
+                    if (!(c.SemanticModel.GetSymbolInfo(attributeSyntax.Name).Symbol is IMethodSymbol   attributeCtorSymbol) ||
+                        !attributeCtorSymbol.ContainingType.IsAny(ExportAttributes))
+                    {
+                        return;
+                    }
+
+                    var exportedType = GetExportedTypeSymbol(attributeSyntax.ArgumentList?.Arguments, c.SemanticModel);
+                    var attributeTargetType = GetAttributeTargetSymbol(attributeSyntax, c.SemanticModel);
+
+                    if (exportedType != null &&
+                        attributeTargetType != null &&
+                        !IsOfExportType(attributeTargetType, exportedType))
+                    {
+                        var action = exportedType.IsInterface()
+                            ? ActionForInterface
+                            : ActionForClass;
+
+                        c.ReportDiagnosticWhenActive(Diagnostic.Create(rule, attributeSyntax.GetLocation(), action,
+                            exportedType.Name, attributeTargetType.Name));
+                    }
+                },
+                SyntaxKind.Attribute);
+        }
+
+        protected override string GetIdentifier(ArgumentSyntax argumentSyntax) =>
+            (argumentSyntax as SimpleArgumentSyntax)?.NameColonEquals?.Name.Identifier.ValueText;
+        protected override ExpressionSyntax GetExpression(ArgumentSyntax argumentSyntax) =>
+            argumentSyntax?.GetExpression();
+        protected override SyntaxNode GetExportedTypeSyntax(ExpressionSyntax expressionSyntax) =>
+            (expressionSyntax as GetTypeExpressionSyntax)?.Type;
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ShouldImplementExportedInterfaces.vb
@@ -0,0 +1,115 @@
+﻿Imports System
+Imports System.ComponentModel.Composition
+
+Namespace Classes
+    Module Constants
+        Public Const ContractName As String = "asdasd"
+    End Module
+
+    Interface MyInterface
+    End Interface
+
+    <Export(GetType(MyInterface))> ' Noncompliant {{Implement 'MyInterface' on 'NotExported' or remove this export attribute.}}
+    <Export(GetType(Exported))> ' Noncompliant {{Derive from 'Exported' on 'NotExported' or remove this export attribute.}}
+    Class NotExported
+'    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ @-2
+'    ^^^^^^^^^^^^^^^^^^^^^^^^^ @-2
+    End Class
+
+    <Export(contractType:=GetType(IComparable), contractName:="asdasd")> ' Noncompliant
+    <Export(contractType:=GetType(MyInterface), contractName:="asdasd")>
+    class NotExported_NamedArgs_ReverseOrder
+        Implements MyInterface
+    End Class
+
+    <Export("something", GetType(MyInterface))> ' Noncompliant
+    <Export(Constants.ContractName, GetType(IDisposable))> ' Noncompliant
+    class NotExported_MultipleArgs
+    End Class
+
+    <Export(GetType(MyInterface)), Export(GetType(IComparable)), Export(GetType(IDisposable))>
+    Class NotExported_Multiple
+'    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ @-1 {{Implement 'MyInterface' on 'NotExported_Multiple' or remove this export attribute.}}
+'                                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ @-2 {{Implement 'IComparable' on 'NotExported_Multiple' or remove this export attribute.}}
+        Implements IDisposable
+        Public Sub Dispose()
+        End Sub
+    End Class
+
+    <ExportAttribute(GetType(MyInterface))> ' Noncompliant
+    Class NotExported_FullAttributeName
+    End Class
+
+    <Export(GetType(MyInterface))>
+    <Export(GetType(Descendant))> ' Noncompliant
+    Class Exported
+        Implements MyInterface
+    End Class
+
+    <Export>
+    <Export("something")> ' Exposing ourselves
+    <Export(GetType(Exporting_Ourselves))>
+    Class Exporting_Ourselves
+    End Class
+
+    <Export(1)>
+    <Export(1, GetType(IComparable))>
+    <Export(GetType(ASDASD))>
+    <Export(GetType(MyInterface), GetType(IComparable))>
+    Class InvalidSyntax
+    End Class
+
+    <Import(GetType(MyInterface))>
+    <InheritedExport(GetType(MyInterface))> ' Noncompliant
+    <InheritedExport(GetType(OtherAttributes))>
+    Class OtherAttributes
+    End Class
+
+    <Export(GetType(MyInterface))>
+    <Export(GetType(Exported))>
+    class Descendant
+        Inherits Exported
+    End Class
+
+    Class ExportingMembers_Are_Ignored
+        <Export(GetType(MyInterface))>
+        <Export(GetType(Exported))>
+        Public Property MyProperty As NotExported
+
+        <Export(GetType(MyInterface))>
+        <Export(GetType(Exported))>
+        Public MyField As NotExported
+
+        <Export(GetType(MyInterface))>
+        <Export(GetType(Exported))>
+        Public Function MyMethod() As NotExported
+        End Function
+    End Class
+
+    Interface ISomething(Of T)
+    End Interface
+
+    Public Class BaseThing
+    End Class
+    Public class BaseThing2
+    End Class
+
+    <Export(GetType(ISomething(Of BaseThing)))>
+    Public Class BaseSomethingImplementation
+        Implements ISomething(Of BaseThing)
+    End Class
+
+    <Export(GetType(ISomething(Of BaseThing)))> ' Noncompliant
+    Public Class Something(Of BaseThing)
+    End Class
+
+    <Export(GetType(ISomething(Of BaseThing)))> ' Noncompliant
+    public Class SomethingImplementation
+        Implements ISomething(Of BaseThing2)
+    End Class
+
+    <Export(GetType(ISomething(Of)))> ' Noncompliant
+    Public Class Something(Of T)
+        Implements ISomething(Of T)
+    End Class
+End Namespace
