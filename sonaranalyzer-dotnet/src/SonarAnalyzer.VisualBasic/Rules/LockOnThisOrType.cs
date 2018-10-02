c66b4e483e20b1def5223ddefd08974344b5fb95
Loïc Joly <loic.joly@sonarsource.com>
2018-10-02 10:55:03 +0200

    Rule registration

--- sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/LockOnThisOrType.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/LockOnThisOrType.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * SonarAnalyzer for .NET
  * Copyright (C) 2015-2018 SonarSource SA
  * mailto: contact AT sonarsource DOT com
@@ -18,31 +18,25 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-extern alias csharp;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using csharp::SonarAnalyzer.Rules.CSharp;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Rules.Common;
 
-namespace SonarAnalyzer.UnitTest.Rules
+namespace SonarAnalyzer.Rules.VisualBasic
 {
-    [TestClass]
-    public class LockOnThisOrTypeTest
+    [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
+    [Rule(DiagnosticId)]
+    public sealed class LockOnThisOrType : LockOnThisOrTypeBase
     {
-        [TestMethod]
-        [TestCategory("Rule")]
-        public void LockOnThisOrType_CS()
-        {
-            Verifier.VerifyAnalyzer(@"TestCases\LockOnThisOrType.cs", new LockOnThisOrType());
-        }
-
-        [TestMethod]
-        [TestCategory("Rule")]
-        public void LockOnThisOrType_VB()
-        {
-            Verifier.VerifyAnalyzer(@"TestCases\LockOnThisOrType.vb",
-                new SonarAnalyzer.Rules.VisualBasic.LockOnThisOrType());
-        }
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
     }
 }
-
-
-
