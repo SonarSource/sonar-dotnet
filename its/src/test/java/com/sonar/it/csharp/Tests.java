eae30e24bee019ccf9cfbc0ac9fc6bdebb2ecf72
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-10-16 16:44:26 +0200

    Removed all UCFG-related classes and files

--- its/src/test/java/com/sonar/it/csharp/Tests.java
+++ its/src/test/java/com/sonar/it/csharp/Tests.java
@@ -55,7 +55,6 @@ import static org.assertj.core.api.Assertions.assertThat;
   MultiTargetAppTest.class,
   NoSonarTest.class,
   SharedFilesTest.class,
-  UCFGDeserializationTest.class,
   UnitTestResultsTest.class
 })
 public class Tests {
