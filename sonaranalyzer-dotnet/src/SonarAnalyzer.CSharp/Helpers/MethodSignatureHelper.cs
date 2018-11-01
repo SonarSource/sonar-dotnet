1b82284529feade961451fb4919a101073b1540d
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-01 09:51:43 +0100

    Adding base classes for hotspot rules (#2032)
    * Adding base classes for hotspot rules

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Helpers/MethodSignatureHelper.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SonarAnalyzer.Helpers
+{
+    public static class MethodSignatureHelper
+    {
+        public static Func<MethodSignature, bool> IsSame<TSymbolType>(SimpleNameSyntax identifierName, SemanticModel semanticModel)
+            where TSymbolType : class, ISymbol
+        {
+            var identifierText = identifierName.Identifier.ValueText;
+            var identifierSymbol = new Lazy<TSymbolType>(() => semanticModel.GetSymbolInfo(identifierName).Symbol as TSymbolType);
+            // This function will be called multiple times for each tracked MethodSignature,
+            // hence we cache as much as possible before.
+            return (methodSignature) =>
+                identifierText == methodSignature.Name &&
+                identifierSymbol.Value != null &&
+                identifierSymbol.Value.ContainingType.Is(methodSignature.ContainingType);
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/HotspotHelper.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+
+namespace SonarAnalyzer.Helpers
+{
+    public abstract class HotspotHelper
+    {
+        private readonly IAnalyzerConfiguration analysisConfiguration;
+        private readonly ImmutableArray<DiagnosticDescriptor> supportedDiagnostics;
+
+        protected HotspotHelper(IAnalyzerConfiguration analysisConfiguration,
+            ImmutableArray<DiagnosticDescriptor> supportedDiagnostics)
+        {
+            this.analysisConfiguration = analysisConfiguration;
+            this.supportedDiagnostics = supportedDiagnostics;
+        }
+
+        protected bool IsEnabled(AnalyzerOptions options)
+        {
+            if (analysisConfiguration.EnabledRules == null)
+            {
+                analysisConfiguration.Read(options);
+            }
+
+            if (analysisConfiguration.EnabledRules == null)
+            {
+                return false;
+            }
+
+            return supportedDiagnostics.Any(d => analysisConfiguration.IsEnabled(d.Id));
+        }
+
+        public abstract void TrackMethodInvocations(SonarAnalysisContext context, DiagnosticDescriptor rule,
+            params MethodSignature[] trackedMethods);
+
+        public abstract void TrackPropertyAccess(SonarAnalysisContext context, DiagnosticDescriptor rule,
+            params MethodSignature[] trackedProperties);
+    }
+}
