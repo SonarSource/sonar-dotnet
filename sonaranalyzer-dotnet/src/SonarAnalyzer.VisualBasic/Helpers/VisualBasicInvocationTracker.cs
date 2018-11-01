ca01e63d8e7457004544e0620918fc19d0d6499a
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-01 17:42:39 +0100

    Refactor hotspot helpers (#2035)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Helpers/VisualBasicInvocationTracker.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers.VisualBasic;
+
+namespace SonarAnalyzer.Helpers
+{
+    public class VisualBasicInvocationTracker : InvocationTracker<SyntaxKind>
+    {
+        public VisualBasicInvocationTracker(IAnalyzerConfiguration analysisConfiguration, DiagnosticDescriptor rule)
+            : base(analysisConfiguration, rule)
+        {
+        }
+
+        protected override SyntaxKind[] TrackedSyntaxKinds { get; } =
+            new[] { SyntaxKind.InvocationExpression };
+
+        protected override GeneratedCodeRecognizer GeneratedCodeRecognizer { get; } =
+            VisualBasic.GeneratedCodeRecognizer.Instance;
+
+        protected override SyntaxNode GetIdentifier(SyntaxNode invocationExpression) =>
+            ((InvocationExpressionSyntax)invocationExpression).Expression.GetIdentifier();
+
+        protected override Func<MethodSignature, bool> IsSame<TSymbolType>(SyntaxNode identifier, SemanticModel semanticModel) =>
+            MethodSignatureHelper.IsSame<TSymbolType>((SimpleNameSyntax)identifier, semanticModel);
+    }
+}
