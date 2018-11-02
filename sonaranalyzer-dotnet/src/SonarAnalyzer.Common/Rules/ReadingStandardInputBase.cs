5e5f6ad093340bc23194187ec9d7ce49423aca20
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-11-02 13:20:03 +0100

    Rule S4829: Reading the Standard Input is security-sensitive

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/ReadingStandardInputBase.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class ReadingStandardInputBase<TSyntaxKind> : SonarDiagnosticAnalyzer
+        where TSyntaxKind : struct
+    {
+        protected const string DiagnosticId = "S4829";
+        protected const string MessageFormat = "Make sure that reading the standard input is safe here.";
+
+        protected InvocationTracker<TSyntaxKind> InvocationTracker { get; set; }
+
+        protected PropertyAccessTracker<TSyntaxKind> PropertyAccessTracker { get; set; }
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            InvocationTracker.Track(context,
+                InvocationTracker.MatchSimpleNames(
+                    new MethodSignature(KnownType.System_Console, nameof(Console.OpenStandardInput))));
+
+            InvocationTracker.Track(context,
+                WhenResultIsNotIgnored, // This is syntax-only check and we can execute it first
+                InvocationTracker.MatchSimpleNames(
+                    new MethodSignature(KnownType.System_Console, nameof(Console.Read)),
+                    new MethodSignature(KnownType.System_Console, nameof(Console.ReadKey)),
+                    new MethodSignature(KnownType.System_Console, nameof(Console.ReadLine))));
+
+            PropertyAccessTracker.Track(context,
+                new MethodSignature(KnownType.System_Console, nameof(Console.In)));
+        }
+
+        protected abstract bool WhenResultIsNotIgnored(InvocationContext context);
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ReadingStandardInput.vb
@@ -0,0 +1,52 @@
+﻿Imports System
+Imports Con = System.Console
+
+Namespace Tests.Diagnostics
+    Class Program
+        Public Sub Method()
+            Dim value As String
+            Dim code As Integer
+            Dim key As ConsoleKeyInfo
+
+            code = System.Console.Read() ' Noncompliant
+            code = Con.Read() ' Noncompliant
+
+            value = Console.ReadLine() ' Noncompliant
+            code = Console.Read() ' Noncompliant
+            key = Console.ReadKey() ' Noncompliant
+            key = Console.ReadKey(True) ' Noncompliant
+
+            Console.Read() ' Compliant, value is ignored
+            Console.ReadLine() ' Compliant, value is ignored
+            Console.ReadKey() ' Compliant, value is ignored
+            Console.ReadKey(True) ' Compliant, value is ignored
+
+            Console.OpenStandardInput() ' Noncompliant
+            Console.OpenStandardInput(100) ' Noncompliant
+
+            Dim x = System.Console.[In] ' Noncompliant
+            x = Console.[In] ' Noncompliant
+            x = Con.[In] ' Noncompliant
+            Console.[In].Read() ' Noncompliant
+
+            ' Other Console methods
+            Console.Write(1)
+            Console.WriteLine(1)
+            ' Other classes
+            MyConsole.Read()
+            MyConsole.[In].Read()
+        End Sub
+    End Class
+
+    Module MyConsole
+        Function Read() As Integer
+            Return 1
+        End Function
+
+        Function ReadKey() As Integer
+            Return 1
+        End Function
+
+        Public Shared ReadOnly Property [In] As System.IO.TextReader
+    End Module
+End Namespace
