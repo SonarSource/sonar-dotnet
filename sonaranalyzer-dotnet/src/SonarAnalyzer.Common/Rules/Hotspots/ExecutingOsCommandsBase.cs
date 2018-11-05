fdd7ac0200313cb824805931119406e4651ca8bb
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-11-05 12:57:13 +0100

    Rule S4721: Executing OS commands is security-sensitive

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/Hotspots/ExecutingOsCommandsBase.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class ExecutingOsCommandsBase<TSyntaxKind> : SonarDiagnosticAnalyzer
+        where TSyntaxKind : struct
+    {
+        protected const string DiagnosticId = "S4721";
+        protected const string MessageFormat = "Make sure that executing this OS command is safe here.";
+
+        protected InvocationTracker<TSyntaxKind> InvocationTracker { get; set; }
+
+        protected PropertyAccessTracker<TSyntaxKind> PropertyAccessTracker { get; set; }
+
+        protected ObjectCreationTracker<TSyntaxKind> ObjectCreationTracker { get; set; }
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            InvocationTracker.Track(context,
+                InvocationTracker.MatchSimpleNames(
+                    new MethodSignature(KnownType.System_Diagnostics_Process, "Start")),
+                WhenFirstArgumentIsNot(KnownType.System_Diagnostics_ProcessStartInfo),
+                WhenThereAreArguments());
+
+            PropertyAccessTracker.Track(context,
+                PropertyAccessTracker.MatchSimpleNames(
+                    new MethodSignature(KnownType.System_Diagnostics_ProcessStartInfo, "FileName")),
+                PropertyAccessTracker.MatchSet());
+
+            ObjectCreationTracker.Track(context,
+                ObjectCreationTracker.MatchConstructors(
+                    KnownType.System_Diagnostics_ProcessStartInfo),
+                WhenFirstArgumentIs(KnownType.System_String));
+        }
+
+        private InvocationCondition WhenThereAreArguments() =>
+            (context) =>
+                context.InvokedMethodSymbol.Value != null &&
+                context.InvokedMethodSymbol.Value.Parameters.Length > 0;
+
+        private ObjectCreationCondition WhenFirstArgumentIs(KnownType type) =>
+            (context) =>
+                context.InvokedConstructorSymbol.Value != null &&
+                context.InvokedConstructorSymbol.Value.ArgumentAtIndexIs(0, type);
+
+        private InvocationCondition WhenFirstArgumentIsNot(KnownType type) =>
+            (context) =>
+                context.InvokedMethodSymbol.Value != null &&
+                !context.InvokedMethodSymbol.Value.ArgumentAtIndexIs(0, type);
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExecutingOsCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security;
+using System.Diagnostics;
+using P = System.Diagnostics.Process;
+using PSI = System.Diagnostics.ProcessStartInfo;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public void Foo(string fileName, ProcessStartInfo startInfo, Process process)
+        {
+            process.Start(); // Compliant, FileName is not passed here
+            Process.Start(fileName); // Noncompliant
+            Process.Start(fileName, null); // Noncompliant
+            Process.Start(fileName, null, null, null); // Noncompliant
+            Process.Start(fileName, null, null, null, null); // Noncompliant
+
+            Process.Start(startInfo); // Compliant, the ProcessStartInfo's FileName has already been highlighted elsewhere
+
+            startInfo.FileName = fileName; // Noncompliant
+            process.StartInfo.FileName = fileName; // Noncompliant, setting ProcessStartInfo.FileName here.
+
+            fileName = startInfo.FileName; // Compliant, the FileName is not set here
+            fileName = process.StartInfo.FileName; // Compliant, the FileName is not set here
+
+            new ProcessStartInfo(); // Compliant, the FileName is set elsewhere
+            new ProcessStartInfo(fileName); // Noncompliant
+            new ProcessStartInfo(fileName, null); // Noncompliant
+
+            // Different ways to specify the types
+            System.Diagnostics.Process.Start(fileName); // Noncompliant
+            P.Start(fileName); // Noncompliant
+            new System.Diagnostics.ProcessStartInfo(fileName); // Noncompliant
+            new PSI(fileName); // Noncompliant
+        }
+    }
+}
