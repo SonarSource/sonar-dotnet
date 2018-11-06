42e4d0b7471355c3a3ac7da5fb2858da09710a5a
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-06 11:48:16 +0100

    Rule S4829: Reading the Standard Input is security-sensitive (#2071)
    * Rule S4829: Reading the Standard Input is security-sensitive

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ReadingStandardInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Con = System.Console;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public void Method()
+        {
+            string value;
+            int code;
+            ConsoleKeyInfo key;
+
+            code = System.Console.Read(); // Noncompliant {{Make sure that reading the standard input is safe here.}}
+//                 ^^^^^^^^^^^^^^^^^^^^^
+            code = Con.Read(); // Noncompliant
+
+            value = Console.ReadLine(); // Noncompliant
+            code = Console.Read(); // Noncompliant
+            key = Console.ReadKey(); // Noncompliant
+            key = Console.ReadKey(true); // Noncompliant
+
+            Console.Read(); // Compliant, value is ignored
+            Console.ReadLine(); // Compliant, value is ignored
+            Console.ReadKey(); // Compliant, value is ignored
+            Console.ReadKey(true); // Compliant, value is ignored
+
+            Console.OpenStandardInput(); // Noncompliant
+            Console.OpenStandardInput(100); // Noncompliant
+
+            var x = System.Console.In; // Noncompliant
+//                  ^^^^^^^^^^^^^^^^^
+            x = Console.In; // Noncompliant
+            x = Con.In; // Noncompliant
+            Console.In.Read(); // Noncompliant
+
+            // Other Console methods
+            Console.Write(1);
+            Console.WriteLine(1);
+            // Other classes
+            MyConsole.Read();
+            MyConsole.In.Read();
+        }
+    }
+
+    public static class MyConsole
+    {
+        public static int Read() => 1;
+        public static int ReadKey() => 1;
+        public static System.IO.TextReader In { get; }
+    }
+}
