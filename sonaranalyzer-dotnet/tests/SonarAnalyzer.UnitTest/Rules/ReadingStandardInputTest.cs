b78c5c449bf37cb7044316757d1428554b304436
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-06 11:48:16 +0100

    Rule S4829: Reading the Standard Input is security-sensitive (#2071)
    * Rule S4829: Reading the Standard Input is security-sensitive

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/ReadingStandardInputTest.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+extern alias csharp;
+extern alias vbnet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp = csharp::SonarAnalyzer.Rules.CSharp;
+using VisualBasic = vbnet::SonarAnalyzer.Rules.VisualBasic;
+
+namespace SonarAnalyzer.UnitTest.Rules
+{
+    [TestClass]
+    public class ReadingStandardInputTest
+    {
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ReadingStandardInput_CS()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ReadingStandardInput.cs",
+                new CSharp.ReadingStandardInput(new TestAnalyzerConfiguration(null, "S4829")));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ReadingStandardInput_VB()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ReadingStandardInput.vb",
+                new VisualBasic.ReadingStandardInput(new TestAnalyzerConfiguration(null, "S4829")));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ReadingStandardInput_CS_Disabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\ReadingStandardInput.cs",
+                new CSharp.ReadingStandardInput()); // Since there is no SonarLint.xml the rule is disabled
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ReadingStandardInput_VB_Disabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\ReadingStandardInput.vb",
+                new VisualBasic.ReadingStandardInput()); // Since there is no SonarLint.xml the rule is disabled
+        }
+    }
+}
