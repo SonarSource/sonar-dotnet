3c4a9f2ef2e3b674543fbad481f6675e27d5ba23
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-11-07 17:20:03 +0100

    Rule S4823: Using command line arguments is security-sensitive

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UsingCommandLineArguments.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+
+Namespace Tests.Diagnostics
+    Class Program1
+        Public Shared Sub Main(ParamArray args As String()) ' Noncompliant {{Make sure that command line arguments are used safely here.}}
+'                         ^^^^
+            Console.WriteLine(args)
+        End Sub
+    End Class
+
+    Class Program2
+        Public Shared Sub Main(ParamArray args As String()) ' Compliant, args is not used
+        End Sub
+
+        Public Shared Sub Main(ByVal arg As String) ' Compliant, not a Main method
+            Console.WriteLine(arg)
+        End Sub
+
+        Public Shared Sub Main(ByVal x As Integer, ParamArray args As String()) ' Compliant, not a Main method
+            Console.WriteLine(args)
+        End Sub
+    End Class
+
+    Class Program3
+        Private Shared args As String()
+
+        Public Shared Sub Main(ParamArray args As String()) ' Compliant, args is not used
+            Console.WriteLine(Program3.args)
+        End Sub
+    End Class
+
+    Class Program4
+        Public Shared Function Main(ParamArray args As String()) As String ' Compliant, not a Main method
+            Console.WriteLine(args)
+            Return Nothing
+        End Function
+    End Class
+End Namespace
