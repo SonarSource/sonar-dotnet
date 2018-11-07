42b6d37906b77f4ec4dbe144af7c25a0a51a967f
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-11-07 16:17:01 +0100

    Adding MethodDeclarationTracker

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/MethodDeclarationContext.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis;
+
+namespace SonarAnalyzer.Helpers
+{
+    /// <summary>
+    /// Syntax and semantic information about a single method declaration
+    /// </summary>
+    public class MethodDeclarationContext
+    {
+        private readonly Compilation compilation;
+
+        public IMethodSymbol MethodSymbol { get; }
+
+        public SemanticModel GetSemanticModel(SyntaxNode syntaxNode) =>
+            compilation.GetSemanticModel(syntaxNode.SyntaxTree);
+
+        public MethodDeclarationContext(IMethodSymbol methodSymbol, Compilation compilation)
+        {
+            MethodSymbol = methodSymbol;
+            this.compilation = compilation;
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Helpers/VisualBasicMethodDeclarationTracker.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using SonarAnalyzer.Common;
+
+namespace SonarAnalyzer.Helpers
+{
+    public class VisualBasicMethodDeclarationTracker : MethodDeclarationTracker<SyntaxKind>
+    {
+        public VisualBasicMethodDeclarationTracker(IAnalyzerConfiguration analysisConfiguration, DiagnosticDescriptor rule)
+            : base(analysisConfiguration, rule)
+        {
+        }
+
+        protected override SyntaxKind[] TrackedSyntaxKinds =>
+            throw new NotSupportedException("MethodDeclarationTracker uses symbols, not syntax");
+
+        protected override GeneratedCodeRecognizer GeneratedCodeRecognizer { get; } =
+            VisualBasic.GeneratedCodeRecognizer.Instance;
+
+        public override MethodDeclarationCondition ParameterAtIndexIsUsed(int index) =>
+            (context) =>
+            {
+                var parameterSymbol = context.MethodSymbol.Parameters.ElementAtOrDefault(0);
+                if (parameterSymbol == null)
+                {
+                    return false;
+                }
+
+                var methodDeclaration = context.MethodSymbol.DeclaringSyntaxReferences
+                    .Select(r => (MethodBlockSyntax)r.GetSyntax().Parent)
+                    .FirstOrDefault(HasImplementation);
+
+                if (methodDeclaration == null)
+                {
+                    return false;
+                }
+
+                var semanticModel = context.GetSemanticModel(methodDeclaration);
+
+                var descendantNodes = methodDeclaration.Statements
+                    .SelectMany(statement => statement.DescendantNodes());
+
+                return descendantNodes.Any(
+                    node =>
+                    {
+                        return node.IsKind(SyntaxKind.IdentifierName) &&
+                            ((IdentifierNameSyntax)node).Identifier.ValueText == parameterSymbol.Name &&
+                            parameterSymbol == semanticModel.GetSymbolInfo(node).Symbol;
+                    });
+            };
+
+        private static bool HasImplementation(MethodBlockSyntax methodBlock) =>
+            methodBlock.Statements.Count > 0;
+
+        protected override SyntaxToken GetMethodIdentifier(SyntaxNode methodDeclaration) =>
+            ((MethodStatementSyntax)methodDeclaration).Identifier;
+    }
+}
