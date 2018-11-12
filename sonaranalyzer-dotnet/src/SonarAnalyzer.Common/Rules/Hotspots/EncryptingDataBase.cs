29b84cdcdcf9aecb2cf5eb4ae355bdb9c64e5571
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-11-12 09:42:11 +0000

    Rule S4787: encrypting data is security-sensitive (#2089)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/Hotspots/EncryptingDataBase.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class EncryptingDataBase<TSyntaxKind> : SonarDiagnosticAnalyzer
+        where TSyntaxKind : struct
+    {
+        protected const string DiagnosticId = "S4787";
+        protected const string MessageFormat = "Make sure that encrypting data is safe here.";
+
+        private static readonly ImmutableArray<KnownType> forbiddenBaseTypes =
+            ImmutableArray.Create(
+                KnownType.System_Security_Cryptography_AsymmetricAlgorithm,
+                KnownType.System_Security_Cryptography_SymmetricAlgorithm
+            );
+
+        protected InvocationTracker<TSyntaxKind> InvocationTracker { get; set; }
+
+        protected BaseTypeTracker<TSyntaxKind> BaseTypeTracker { get; set; }
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            InvocationTracker.Track(context,
+                InvocationTracker.MatchSimpleNames(
+                    // "RSA" is the base class for all RSA algorithm implementations
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "Encrypt"),
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "EncryptValue"),
+
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "Decrypt"),
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "DecryptValue"),
+
+                    // RSA methods added in NET Core 2.1
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "TryEncrypt"),
+                    new MethodSignature(KnownType.System_Security_Cryptography_RSA, "TryDecrypt"),
+
+                    new MethodSignature(KnownType.System_Security_Cryptography_SymmetricAlgorithm, "CreateEncryptor"),
+                    new MethodSignature(KnownType.System_Security_Cryptography_SymmetricAlgorithm, "CreateDecryptor"))
+                    );
+
+            BaseTypeTracker.Track(context,
+                BaseTypeTracker.WhenDerivesFrom(forbiddenBaseTypes));
+        }
+    }
+}
+
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/EncryptingDataTest.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+extern alias csharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using csharp::SonarAnalyzer.Rules.CSharp;
+
+namespace SonarAnalyzer.UnitTest.Rules
+{
+    [TestClass]
+    public class EncryptingDataTest
+    {
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void EncryptingData_CS()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\EncryptingData.cs",
+                new EncryptingData(new TestAnalyzerConfiguration(null, "S4787")));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void EncryptingData_VB()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\EncryptingData.vb",
+                new SonarAnalyzer.Rules.VisualBasic.EncryptingData(new TestAnalyzerConfiguration(null, "S4787")));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void EncryptingData_CS_RuleDisabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\EncryptingData.cs",
+                new EncryptingData());
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void EncryptingData_VB_RuleDisabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\EncryptingData.vb",
+                new SonarAnalyzer.Rules.VisualBasic.EncryptingData());
+        }
+    }
+}
