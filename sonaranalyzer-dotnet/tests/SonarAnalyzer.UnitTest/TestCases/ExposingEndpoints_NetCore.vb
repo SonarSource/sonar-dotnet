b0e1ace1ee05973000be6b7f33484e22dcc8444b
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-12 16:41:59 +0100

    Rule S4529: Exposing HTTP endpoints is security-sensitive (#2097)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExposingEndpoints_NetCore.vb
@@ -0,0 +1,39 @@
+﻿Imports Microsoft.AspNetCore.Mvc
+
+Public Class SomeController
+    Inherits ControllerBase
+
+    Public Sub PublicFoo() ' Noncompliant {{Make sure that exposing this HTTP endpoint is safe here.}}
+'              ^^^^^^^^^
+    End Sub
+
+    Protected Sub ProtectedFoo()
+    End Sub
+
+    Friend Sub InternalFoo()
+    End Sub
+
+    Private Sub PrivateFoo()
+    End Sub
+
+    Private Class Bar
+        Inherits ControllerBase
+
+        Public Sub InnerFoo()
+        End Sub
+    End Class
+End Class
+
+<Controller>
+Public Class MyController
+    Public Sub PublicFoo() ' Noncompliant
+    End Sub
+End Class
+
+<NonController>
+Public Class MyNoncontroller
+    Inherits ControllerBase
+
+    Public Sub PublicFoo() ' Compliant, not a controller
+    End Sub
+End Class
