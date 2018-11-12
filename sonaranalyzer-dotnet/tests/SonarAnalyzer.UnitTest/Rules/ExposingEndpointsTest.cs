61fcf4f295a166cba45db683c349a838ea7f6b45
Valeri Hristov <valhristov@users.noreply.github.com>
2018-11-12 16:41:59 +0100

    Rule S4529: Exposing HTTP endpoints is security-sensitive (#2097)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/ExposingEndpointsTest.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+extern alias csharp;
+extern alias vbnet;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharp = csharp::SonarAnalyzer.Rules.CSharp;
+using VisualBasic = vbnet::SonarAnalyzer.Rules.VisualBasic;
+
+namespace SonarAnalyzer.UnitTest.Rules
+{
+    [TestClass]
+    public class ExposingEndpointsTest
+    {
+        public static IEnumerable<MetadataReference> GetAdditionalReferences_Net46(string mvcVersion) =>
+            NuGetMetadataReference.MicrosoftAspNetMvc(mvcVersion);
+
+        public static IEnumerable<MetadataReference> GetAdditionalReferences_NetCore(string aspnetCoreVersion) =>
+            FrameworkMetadataReference.Netstandard
+                .Concat(NuGetMetadataReference.MicrosoftAspNetCoreMvcCore(aspnetCoreVersion));
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_CS_Net46()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ExposingEndpoints_Net46.cs",
+                new CSharp.ExposingEndpoints(new TestAnalyzerConfiguration(null, "S4529")),
+                additionalReferences: GetAdditionalReferences_Net46(Constants.NuGetLatestVersion));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_CS_NetCore()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ExposingEndpoints_NetCore.cs",
+                new CSharp.ExposingEndpoints(new TestAnalyzerConfiguration(null, "S4529")),
+                additionalReferences: GetAdditionalReferences_NetCore(Constants.NuGetLatestVersion));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_CS_Disabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\ExposingEndpoints_Net46.cs",
+                new CSharp.ExposingEndpoints(),
+                additionalReferences: GetAdditionalReferences_Net46(Constants.NuGetLatestVersion));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_VB_Net46()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ExposingEndpoints_Net46.vb",
+                new VisualBasic.ExposingEndpoints(new TestAnalyzerConfiguration(null, "S4529")),
+                additionalReferences: GetAdditionalReferences_Net46(Constants.NuGetLatestVersion));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_VB_NetCore()
+        {
+            Verifier.VerifyAnalyzer(@"TestCases\ExposingEndpoints_NetCore.vb",
+                new VisualBasic.ExposingEndpoints(new TestAnalyzerConfiguration(null, "S4529")),
+                additionalReferences: GetAdditionalReferences_NetCore(Constants.NuGetLatestVersion));
+        }
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ExposingEndpoints_VB_Disabled()
+        {
+            Verifier.VerifyNoIssueReported(@"TestCases\ExposingEndpoints_Net46.vb",
+                new VisualBasic.ExposingEndpoints(),
+                additionalReferences: GetAdditionalReferences_Net46(Constants.NuGetLatestVersion));
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExposingEndpoints_Net46.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+public class Foo : Controller
+{
+    public void PublicFoo() { } // Noncompliant {{Make sure that exposing this HTTP endpoint is safe here.}}
+//              ^^^^^^^^^
+
+    protected void ProtectedFoo() { }
+
+    internal void InternalFoo() { }
+
+    private void PrivateFoo() { }
+
+    private class Bar : Controller
+    {
+        public void InnerFoo() { }
+    }
+}
