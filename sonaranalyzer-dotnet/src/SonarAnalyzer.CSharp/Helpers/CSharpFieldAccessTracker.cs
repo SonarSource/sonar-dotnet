e7270fd1c1cbabc187630d77a4b23bdc347064b9
duncanp-sonar <duncan.pocklington@sonarsource.com>
2018-11-14 09:02:39 +0000

    Rule S3011: bypassing accessibility is security-sensitive (#2098)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Helpers/CSharpFieldAccessTracker.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2018 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SonarAnalyzer.Common;
+
+namespace SonarAnalyzer.Helpers
+{
+    public class CSharpFieldAccessTracker : FieldAccessTracker<SyntaxKind>
+    {
+        public CSharpFieldAccessTracker(IAnalyzerConfiguration analysisConfiguration, DiagnosticDescriptor rule)
+            : base(analysisConfiguration, rule)
+        {
+        }
+
+        protected override SyntaxKind[] TrackedSyntaxKinds { get; } =
+            new[]
+            {
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxKind.MemberBindingExpression,
+                SyntaxKind.IdentifierName
+            };
+
+        protected override GeneratedCodeRecognizer GeneratedCodeRecognizer { get; } =
+            CSharp.GeneratedCodeRecognizer.Instance;
+
+        protected override SyntaxNode GetIdentifier(SyntaxNode expression) =>
+            ((ExpressionSyntax)expression).GetIdentifier();
+
+        protected override bool IsIdentifierWithinMemberAccess(SyntaxNode expression) =>
+            expression.IsKind(SyntaxKind.IdentifierName) &&
+            expression.Parent.IsKind(SyntaxKind.SimpleMemberAccessExpression);
+
+        #region Syntax-level checking methods
+
+        public override FieldAccessCondition MatchGet() =>
+            (context) => !((ExpressionSyntax)context.Expression).IsLeftSideOfAssignment();
+
+        public override FieldAccessCondition MatchSet() =>
+            (context) => ((ExpressionSyntax)context.Expression).IsLeftSideOfAssignment();
+
+        public override FieldAccessCondition MatchSimpleNames(params MethodSignature[] methods) =>
+            (context) =>
+                MethodSignatureHelper.IsMatch(context.Identifier as SimpleNameSyntax, context.Model,
+                    context.InvokedFieldSymbol, false, methods);
+
+        public override FieldAccessCondition AssignedValueIsConstant() =>
+            (context) =>
+            {
+                var assignment = (AssignmentExpressionSyntax)context.Expression.Ancestors()
+                    .FirstOrDefault(ancestor => ancestor.IsKind(SyntaxKind.SimpleAssignmentExpression));
+
+                return assignment != null &&
+                    assignment.Right.IsConstant(context.Model);
+            };
+
+        #endregion
+
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/BypassingAccessibility.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.Reflection
+
+Namespace Tests.TestCases
+    Class Foo
+        Public Sub Test()
+
+            ' RSPEC: https:'jira.sonarsource.com/browse/RSPEC-3011
+            Dim dynClass = Type.GetType("MyInternalClass")
+            'Questionable.Using BindingFlags.NonPublic will return non-public members
+            Dim bindingAttr As BindingFlags = BindingFlags.NonPublic Or BindingFlags.Static
+'                                             ^^^^^^^^^^^^^^^^^^^^^^   {{Make sure that this accessibility bypass is safe here.}}
+
+            Dim dynMethod As MethodInfo = dynClass.GetMethod("mymethod", bindingAttr)
+            Dim result = dynMethod.Invoke(dynClass, Nothing)
+
+        End Sub
+
+        Public Function AdditionalChecks(t As System.Type) As BindingFlags
+
+            ' Using other binding attributes should be ok
+            Dim bindingAttr = BindingFlags.Static Or BindingFlags.CreateInstance Or BindingFlags.DeclaredOnly Or
+                BindingFlags.ExactBinding Or BindingFlags.GetField Or BindingFlags.InvokeMethod ' et cetera...
+            Dim dynMeth = t.GetMember("mymethod", bindingAttr)
+
+            ' We don't detect casts to the forbidden value
+            Dim nonPublic As BindingFlags = DirectCast(32, BindingFlags)
+            nonPublic = CType(32, BindingFlags)
+            dynMeth = t.GetMember("mymethod", bindingAttr)
+
+            System.Enum.TryParse(Of BindingFlags)("NonPublic", nonPublic)
+            dynMeth = t.GetMember("mymethod", bindingAttr)
+
+
+            bindingAttr = (((BindingFlags.NonPublic)) Or BindingFlags.Static)
+'                            ^^^^^^^^^^^^^^^^^^^^^^
+
+            dynMeth = t.GetMember("mymethod", (BindingFlags.NonPublic))
+'                                              ^^^^^^^^^^^^^^^^^^^^^^
+
+            Dim val As Integer = CType(BindingFlags.NonPublic, Integer) ' Noncompliant
+            Return BindingFlags.NonPublic ' Noncompliant
+        End Function
+
+
+        Public ReadOnly DefaultAccess As BindingFlags = BindingFlags.OptionalParamBinding Or BindingFlags.NonPublic
+'                                                                                            ^^^^^^^^^^^^^^^^^^^^^^
+        Private ReadOnly access1 = BindingFlags.NonPublic     ' Noncompliant
+
+    End Class
+End Namespace
