203874a1f6be8e148c8380d43c1377938811a8ae
Valeri Hristov <valeri.hristov@sonarsource.com>
2018-11-20 13:20:18 +0100

    Rule S5042: Expanding archive files is security-sensitive

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExpandingArchives.vb
@@ -0,0 +1,44 @@
+﻿Imports System
+Imports System.IO
+Imports System.IO.Compression
+Imports System.Linq
+
+Namespace ClassLibrary1
+    Public Class Class1
+        Public Sub ExtractArchive(ByVal archive As ZipArchive)
+            For Each entry As ZipArchiveEntry In archive.Entries
+                entry.ExtractToFile("") ' Noncompliant
+'               ^^^^^^^^^^^^^^^^^^^^^^^
+            Next
+
+            For i As Integer = 0 To archive.Entries.Count - 1
+                archive.Entries(i).ExtractToFile("") ' Noncompliant
+            Next
+
+            archive.Entries.ToList().ForEach(Sub(e) e.ExtractToFile("")) ' Noncompliant
+'                                                   ^^^^^^^^^^^^^^^^^^^
+        End Sub
+
+        Public Sub ExtractEntry(ByVal entry As ZipArchiveEntry)
+            Dim fullName As String
+            Dim stream As Stream
+
+            entry.ExtractToFile("") ' Noncompliant
+            entry.ExtractToFile("", True) ' Noncompliant
+
+            ZipFileExtensions.ExtractToFile(entry, "") ' Noncompliant
+            ZipFileExtensions.ExtractToFile(entry, "", True) ' Noncompliant
+
+            stream = entry.Open() ' Noncompliant
+
+            entry.Delete() ' Compliant, method is not tracked
+
+            fullName = entry.FullName ' Compliant, properties are not tracked
+
+            ExtractToFile(entry) ' Compliant, method is not tracked
+        End Sub
+
+        Public Sub ExtractToFile(ByVal entry As ZipArchiveEntry)
+        End Sub
+    End Class
+End Namespace
