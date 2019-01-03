93b195456343f3c191c4d4ea3960fbf0392a64f4
Valeri Hristov <valeri.hristov@sonarsource.com>
2019-01-03 18:07:16 +0100

    Addressing review comments

--- sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/InterfaceMethodsShouldBeCallableByChildTypes.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/InterfaceMethodsShouldBeCallableByChildTypes.cs
@@ -109,7 +109,7 @@ namespace SonarAnalyzer.Rules.CSharp
             MethodDeclarationSyntax targetedMethod)
         {
             return currentMethod != targetedMethod &&
-                currentMethod.Modifiers.Any(IsPublicOrProtected) &&
+                currentMethod.Modifiers.Any(modifier => IsPublicOrProtected(modifier)) &&
                 (currentMethod.Identifier.ValueText == targetedMethod.Identifier.ValueText ||
                     (targetedMethod.Identifier.ValueText == nameof(IDisposable.Dispose) &&
                     currentMethod.Identifier.ValueText == "Close")); // Allows to replace IDisposable.Dispose() with Close()
@@ -120,7 +120,7 @@ namespace SonarAnalyzer.Rules.CSharp
         {
             return currentProperty != targetedProperty &&
                 currentProperty.Identifier.ValueText == targetedProperty.Identifier.ValueText &&
-                currentProperty.Modifiers.Any(IsPublicOrProtected);
+                currentProperty.Modifiers.Any(modifier => IsPublicOrProtected(modifier));
         }
 
         private static bool AreEventsEquivalent(EventDeclarationSyntax currentEvent,
@@ -128,7 +128,7 @@ namespace SonarAnalyzer.Rules.CSharp
         {
             return currentEvent != targetedEvent &&
                 currentEvent.Identifier.ValueText == targetedEvent.Identifier.ValueText &&
-                currentEvent.Modifiers.Any(IsPublicOrProtected);
+                currentEvent.Modifiers.Any(modifier => IsPublicOrProtected(modifier));
         }
 
         private static bool IsPublicOrProtected(SyntaxToken modifier) =>
