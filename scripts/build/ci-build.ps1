6ebf783f30afaa38d68fd6b6a14fe521abbe8b15
duncanp-sonar <duncan.pocklington@sonarsource.com>
2019-02-26 11:00:32 +0000

    Improved error reporting in script

--- scripts/build/ci-build.ps1
+++ scripts/build/ci-build.ps1
@@ -102,24 +102,34 @@ function Get-ScannerMsBuildPath() {
     $scannerMsbuild = Join-Path $currentDir "SonarScanner.MSBuild.exe"
 
     if (-Not (Test-Path $scannerMsbuild)) {
-        Write-Debug "Scanner for MSBuild not found, downloading it"
+        Write-Host "Scanner for MSBuild not found, downloading it"
+
+        if ($env:ARTIFACTORY_URL)
+        {
+            Write-Host "Environment variable ARTIFACTORY_URL = $env:ARTIFACTORY_URL"
+        }
+        else
+        {
+            # We want this to be a terminating error so we throw
+            Throw "Environment variable ARTIFACTORY_URL is not set"
+        }
 
         # This links always redirect to the latest released scanner
         $downloadLink = "$env:ARTIFACTORY_URL/sonarsource-public-releases/org/sonarsource/scanner/msbuild/" +
             "sonar-scanner-msbuild/%5BRELEASE%5D/sonar-scanner-msbuild-%5BRELEASE%5D-net46.zip"
         $scannerMsbuildZip = Join-Path $currentDir "\SonarScanner.MSBuild.zip"
 
-        Write-Debug "Downloading scanner from '${downloadLink}' at '${currentDir}'"
+        Write-Host "Scanner for MSBuild not found, downloading it" "Downloading scanner from '${downloadLink}' at '${currentDir}'"
         (New-Object System.Net.WebClient).DownloadFile($downloadLink, $scannerMsbuildZip)
 
         # perhaps we could use other folder, not the repository root
         Expand-ZIPFile $scannerMsbuildZip $currentDir
 
-        Write-Debug "Deleting downloaded zip"
+        Write-Host "Deleting downloaded zip"
         Remove-Item $scannerMsbuildZip -Force
     }
 
-    Write-Debug "Scanner for MSBuild found at '$scannerMsbuild'"
+    Write-Host "Scanner for MSBuild found at '$scannerMsbuild'"
     return $scannerMsbuild
 }
 
