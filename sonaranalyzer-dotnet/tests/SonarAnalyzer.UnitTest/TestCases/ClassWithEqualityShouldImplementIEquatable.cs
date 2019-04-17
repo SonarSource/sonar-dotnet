c06b3ce1042801810fe5f7b36189d7dd2a3da20d
Andrei Epure <andrei.epure@sonarsource.com>
2019-04-17 10:26:43 +0200

    Add FP test for S3897

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ClassWithEqualityShouldImplementIEquatable.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ClassWithEqualityShouldImplementIEquatable.cs
@@ -27,4 +27,22 @@ namespace Tests.Diagnostics
             return false;
         }
     }
+
+    public sealed class Bar : MyEquatable<Bar> // Noncompliant FP - issue #1960
+    {
+        public Bar(string x)
+        {
+            X = x;
+        }
+
+        public string X { get; }
+
+        public new bool Equals(Bar other) => string.Equals(X, other.X, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public abstract class MyEquatable<T> : IEquatable<T>
+            where T : MyEquatable<T>
+    {
+        public bool Equals(T other) => true;
+    }
 }
