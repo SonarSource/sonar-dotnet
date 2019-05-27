764a811f440f7d4a53427789c75923a55cb46aca
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2019-05-27 14:03:17 +0200

    S4275: Add UT for cross-procedural FP (#2434)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PropertiesAccessCorrectField.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PropertiesAccessCorrectField.cs
@@ -522,4 +522,18 @@ namespace Tests.Diagnostics
             set => throw new System.InvalidOperationException(""); // Compliant
         }
     }
+
+    public class CrossProcedural
+    {
+        private int myValue;
+        public int MyValue
+        {
+            // below there are two False Positives
+            get => this.GetMyValue(); // Noncompliant {{Refactor this getter so that it actually refers to the field 'myValue'.}}
+            set => this.SetMyValue(value); // Noncompliant {{Refactor this setter so that it actually refers to the field 'myValue'.}}
+        }
+
+        private int GetMyValue() => this.myValue;
+        private void SetMyValue(int value) => this.myValue = value;
+    }
 }
