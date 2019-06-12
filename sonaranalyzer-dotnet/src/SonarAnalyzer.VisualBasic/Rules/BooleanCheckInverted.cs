6b20d467224f1da203d42212e1404351299e89f0
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2019-06-12 11:19:31 +0200

    Remove not needed using clause

--- sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/BooleanCheckInverted.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.VisualBasic/Rules/BooleanCheckInverted.cs
@@ -18,7 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Microsoft.CodeAnalysis;
