fb84bce3342dca83150f294e03f7cc25ee7069d7
Amélie Renard <44666826+amelie-renard-sonarsource@users.noreply.github.com>
2019-07-24 02:53:47 -0700

    MLIR : comparison operators + simple assignments

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/MlirExportTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/MlirExportTest.cs
@@ -1,11 +1,6 @@
 ﻿extern alias csharp;
 
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using csharp::SonarAnalyzer.ControlFlowGraph.CSharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarAnalyzer.ControlFlowGraph;
