e1ae8c43e78a8300eaeabb6bdcf85b84bdb817d5
Amélie Renard <44666826+amelie-renard-sonarsource@users.noreply.github.com>
2019-07-26 00:24:06 -0700

    Add locations in MLIR

--- sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/MLIRExporter.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/MLIRExporter.cs
@@ -32,7 +32,7 @@ namespace SonarAnalyzer
             var returnType = HasNoReturn(method) ?
                 "()" :
                 "i32";
-            writer.WriteLine($"func @{method.Identifier.ValueText}{GetAnonymousArgumentsString(method)} -> {returnType} {{");
+            writer.WriteLine($"func @{method.Identifier.ValueText}{GetAnonymousArgumentsString(method)} -> {returnType} {GetLocation(method)} {{");
             CreateEntryBlock(method);
 
             var cfg = CSharpControlFlowGraph.Create(method.Body, semanticModel);
@@ -58,8 +58,8 @@ namespace SonarAnalyzer
             foreach (var param in method.ParameterList.Parameters)
             {
                 var id = OpId(param);
-                writer.WriteLine($"%{id} = cbde.alloca i32 : () -> memref<i32>");
-                writer.WriteLine($"cbde.store %{param.Identifier.ValueText}, %{id} : memref<i32>");
+                writer.WriteLine($"%{id} = cbde.alloca i32 : () -> memref<i32> {GetLocation(param)}");
+                writer.WriteLine($"cbde.store %{param.Identifier.ValueText}, %{id} : memref<i32> {GetLocation(param)}");
             }
             writer.WriteLine("br ^0");
             writer.WriteLine();
@@ -74,7 +74,7 @@ namespace SonarAnalyzer
         {
             if (block is ExitBlock && !HasNoReturn(parentMethod))
             {
-                // If the method returns, it will have an explicit return, no need for this spurious block
+               // If the method returns, it will have an explicit return, no need for this spurious block
                 return;
             }
             else
@@ -94,13 +94,13 @@ namespace SonarAnalyzer
                     switch (jb.JumpNode)
                     {
                         case ReturnStatementSyntax ret:
-                            writer.WriteLine($"return %{OpId(ret.Expression)} : i32");
+                            writer.WriteLine($"return %{OpId(ret.Expression)} : i32 {GetLocation(ret)}");
                             break;
-                        case BreakStatementSyntax _:
-                            writer.WriteLine($"br ^{BlockId(jb.SuccessorBlock)} // break");
+                        case BreakStatementSyntax breakStmt:
+                            writer.WriteLine($"br ^{BlockId(jb.SuccessorBlock)} {GetLocation(breakStmt)} // break");
                             break;
-                        case ContinueStatementSyntax _:
-                            writer.WriteLine($"br ^{BlockId(jb.SuccessorBlock)} // continue");
+                        case ContinueStatementSyntax continueStmt:
+                            writer.WriteLine($"br ^{BlockId(jb.SuccessorBlock)} {GetLocation(continueStmt)} // continue");
                             break;
                         default:
                             Debug.Assert(false, "Unknown kind of JumpBlock");
@@ -113,7 +113,7 @@ namespace SonarAnalyzer
                     // For a for, bbb.BranchingNode represents the for. Since for is a statement, not an expression, if we
                     // see a for, we know it's at the top level of the expression tree, so it cannot be a for inside of a if condition
 
-                    writer.WriteLine($"cond_br %{OpId(cond)}, ^{BlockId(bbb.TrueSuccessorBlock)}, ^{BlockId(bbb.FalseSuccessorBlock)}");
+                    writer.WriteLine($"cond_br %{OpId(cond)}, ^{BlockId(bbb.TrueSuccessorBlock)}, ^{BlockId(bbb.FalseSuccessorBlock)} {GetLocation(cond)}");
                     /*
                      * Up to now, we do exactly the same for all cases that may have created a BinaryBranchBlock
                      * maybe later, depending on the reason (if vs for?) we'll do something different
@@ -195,37 +195,37 @@ namespace SonarAnalyzer
                 case SyntaxKind.AddExpression:
                     {
                         var binExpr = op as BinaryExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = addi %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+                        writer.WriteLine($"%{OpId(op)} = addi %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.SubtractExpression:
                     {
                         var binExpr = op as BinaryExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = subi %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+                        writer.WriteLine($"%{OpId(op)} = subi %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.MultiplyExpression:
                     {
                         var binExpr = op as BinaryExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = muli %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+                        writer.WriteLine($"%{OpId(op)} = muli %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.DivideExpression:
                     {
                         var binExpr = op as BinaryExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = divis %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+                        writer.WriteLine($"%{OpId(op)} = divis %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.ModuloExpression:
                     {
                         var binExpr = op as BinaryExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = remis %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+                        writer.WriteLine($"%{OpId(op)} = remis %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.NumericLiteralExpression:
                     {
                         var lit = op as LiteralExpressionSyntax;
-                        writer.WriteLine($"%{OpId(op)} = constant {lit.Token.ValueText} : i32");
+                        writer.WriteLine($"%{OpId(op)} = constant {lit.Token.ValueText} : i32 {GetLocation(op)}");
                         break;
                     }
                 case SyntaxKind.EqualsExpression:
@@ -250,17 +250,17 @@ namespace SonarAnalyzer
                     {
                         var id = op as IdentifierNameSyntax;
                         var decl = semanticModel.GetSymbolInfo(id).Symbol.DeclaringSyntaxReferences[0].GetSyntax();
-                        writer.WriteLine($"%{OpId(op)} = cbde.load %{OpId(decl)} : memref<i32>");
+                        writer.WriteLine($"%{OpId(op)} = cbde.load %{OpId(decl)} : memref<i32> {GetLocation(op)}");
                     }
                     break;
                 case SyntaxKind.VariableDeclarator:
                     {
                         var decl = op as VariableDeclaratorSyntax;
                         var id = OpId(decl);
-                        writer.WriteLine($"%{id} = cbde.alloca i32 : () -> memref<i32> // {decl.Identifier.ValueText}");
+                        writer.WriteLine($"%{id} = cbde.alloca i32 : () -> memref<i32> {GetLocation(decl)} // {decl.Identifier.ValueText}");
                         if (decl.Initializer != null)
                         {
-                            writer.WriteLine($"cbde.store %{OpId(decl.Initializer.Value)}, %{id} : memref<i32>");
+                            writer.WriteLine($"cbde.store %{OpId(decl.Initializer.Value)}, %{id} : memref<i32> {GetLocation(decl)}");
                         }
                     }
                     break;
@@ -268,11 +268,11 @@ namespace SonarAnalyzer
                     {
                         var assign = op as AssignmentExpressionSyntax;
                         var lhs = semanticModel.GetSymbolInfo(assign.Left).Symbol.DeclaringSyntaxReferences[0].GetSyntax();
-                        writer.WriteLine($"cbde.store %{OpId(assign.Right)}, %{OpId(lhs)} : memref<i32>");
+                        writer.WriteLine($"cbde.store %{OpId(assign.Right)}, %{OpId(lhs)} : memref<i32> {GetLocation(op)}");
                         break;
                     }
                 default:
-                    writer.WriteLine($"%{OpId(op)} = constant {OpId(op)} : i32 // {op.ToFullString()} ({op.Kind()})");
+                    writer.WriteLine($"%{OpId(op)} = constant {OpId(op)} : i32 {GetLocation(op)} // {op.ToFullString()} ({op.Kind()})");
                     break;
             }
         }
@@ -280,8 +280,18 @@ namespace SonarAnalyzer
         {
             var binExpr = op as BinaryExpressionSyntax;
             // The type is the type of the operands, not of the result, which is always i1
-            writer.WriteLine($"%{OpId(op)} = cmpi \"{compName}\", %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32");
+            writer.WriteLine($"%{OpId(op)} = cmpi \"{compName}\", %{OpId(binExpr.Left)}, %{OpId(binExpr.Right)} : i32 {GetLocation(op)}");
+        }
 
+        private string GetLocation(SyntaxNode node)
+        {
+            // TODO: We should decide which of GetLineSpan or GetMappedLineSpan is better to use
+            var loc = node.GetLocation().GetLineSpan();
+            var location = $"loc(\"{loc.Path}\"" +
+                $" :{loc.StartLinePosition.Line}" +
+                $" :{loc.StartLinePosition.Character})";
+
+            return location;
         }
 
         private readonly TextWriter writer;
