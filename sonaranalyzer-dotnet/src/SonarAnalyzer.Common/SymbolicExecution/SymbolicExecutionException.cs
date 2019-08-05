75e488a5d69a42bc1a031e26fcb0f831b8f5ffa6
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2019-08-05 18:46:46 +0200

    Log exception details during symbolic execution (#2543)

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/SymbolicExecution/SymbolicExecutionException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SonarAnalyzer.SymbolicExecution
+{
+    public sealed class SymbolicExecutionException : Exception
+    {
+        public SymbolicExecutionException()
+        {
+        }
+
+        public SymbolicExecutionException(string message)
+            : base(message)
+        {
+        }
+
+        public SymbolicExecutionException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
