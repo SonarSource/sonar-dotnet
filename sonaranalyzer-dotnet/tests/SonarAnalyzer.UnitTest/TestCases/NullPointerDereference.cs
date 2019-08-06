SonarSource/sonar-dotnet

commit 987246d88d9a6544de803fdd95323a107856b525

Fix S1854 FP: `try-catch` inside of loop and with return (#2511)

sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/ControlFlowGraph/CSharpControlFlowGraphBuilder.cs
@@ -553,25 +553,49 @@ namespace SonarAnalyzer.ControlFlowGraph.CSharp
                 tryEndStatementConnections.Add(this.ExitTarget.Peek());
             }
 
-            var tryBody = BuildBlock(tryStatement.Block,
-                CreateBranchBlock(tryStatement, tryEndStatementConnections.Distinct()));
-
-            var tryStartStatementConnections = catchBlocks.ToList();
-            tryStartStatementConnections.Add(tryBody); // try body
-
-            if (!areAllExceptionsCaught) // unexpected exception thrown, go to exit (through finally if present)
+            Block tryBody;
+            if (tryStatement.Block.Statements.Any(s=>s.IsKind(SyntaxKind.ReturnStatement)))
             {
-                tryStartStatementConnections.Add(this.ExitTarget.Peek());
+                // there is a return inside the `try`, thus a JumpBlock directly to exit will be created
+                var returnBlock = BuildBlock(tryStatement.Block, currentBlock);
+                var connections = new List<Block>();
+                connections.Add(returnBlock);
+                // if an exception is thrown, it will reach the `catch` blocks
+                connections.AddRange(catchBlocks);
+                // if there is a finally, add it; otherwise, the `return` will jump to exit
+                if (catchSuccessor != currentBlock)
+                {
+                    connections.Add(catchSuccessor);
+                }
+                tryBody = CreateBranchBlock(tryStatement, connections);
+            }
+            else
+            {
+                tryBody = BuildBlock(tryStatement.Block, CreateBranchBlock(tryStatement,
+                    tryEndStatementConnections.Distinct()));
             }
 
-            var tryStartBlock = CreateBranchBlock(tryStatement, tryStartStatementConnections.Distinct());
+            // if this try is inside another try, the `beforeTryBlock` must have edges to the outer catch & finally blocks
+            Block beforeTryBlock;
+            if (currentBlock is BranchBlock possibleOuterTry &&
+                possibleOuterTry.BranchingNode.IsKind(SyntaxKind.TryStatement))
+            {
+                var beforeTryConnections = possibleOuterTry.SuccessorBlocks.ToList();
+                beforeTryConnections.Add(tryBody);
+                beforeTryBlock = CreateBranchBlock(tryStatement, beforeTryConnections.Distinct());
+            }
+            else
+            {
+                // otherwise, what happens before the try is not handled by any catch or finally
+                beforeTryBlock = CreateBlock(tryBody);
+            }
 
             if (hasFinally)
             {
                 this.ExitTarget.Pop();
             }
 
-            return tryStartBlock;
+            return beforeTryBlock;
         }
 
         private Block BuildGotoDefaultStatement(GotoStatementSyntax statement, Block currentBlock)
@@ -743,6 +767,15 @@ namespace SonarAnalyzer.ControlFlowGraph.CSharp
             }
 
             var target = this.BreakTarget.Peek();
+            if (currentBlock is BranchBlock possibleTryBlock &&
+                possibleTryBlock.BranchingNode.IsKind(SyntaxKind.TryStatement))
+            {
+                var newSuccessors = possibleTryBlock.SuccessorBlocks.ToList();
+                newSuccessors.Add(target);
+                var branchBlock = CreateBranchBlock(possibleTryBlock.BranchingNode, newSuccessors);
+                branchBlock.ReversedInstructions.Add(breakStatement);
+                return branchBlock;
+            }
             return CreateJumpBlock(breakStatement, target, currentBlock);
         }
 

sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/DeadStores.cs
@@ -113,6 +113,21 @@ namespace SonarAnalyzer.Rules.CSharp
 
             foreach (var block in cfg.Blocks)
             {
+                // For the `finally` instruction, 2 different blocks are created:
+                // - one block for the "uncaught exception flow", which will always point to EXIT
+                // - one block for the "normal flow", which points to the block after the `try-finally` (possibly EXIT)
+                if (block is SimpleBlock simpleBlock &&
+                    !(block is JumpBlock) &&
+                    simpleBlock.SuccessorBlock is ExitBlock &&
+                    block.PredecessorBlocks.Count == 1 &&
+                    block.PredecessorBlocks.ElementAt(0) is BranchBlock possibleTry &&
+                    possibleTry.BranchingNode.IsKind(SyntaxKind.TryStatement))
+                {
+                    // we ignore the "uncaught exception flow" block because it gives FPs when inside a loop
+                    // this leads to FNs because we cannot tell what block is actually a `finally` block
+                    // so any block which follows a Try statement and points to EXIT will be ignored
+                    continue;
+                }
                 CheckCfgBlockForDeadStores(block, lva.GetLiveOut(block), lva.CapturedVariables, node, declaration, context);
             }
         }

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DeadStores.cs
@@ -416,6 +416,70 @@ namespace Tests.Diagnostics
         }
     }
 
+    public class AkkaSnippet
+    {
+        internal void OnlyFinally(object actor)
+        {
+            try
+            {
+                Foo(actor);
+            }
+            finally
+            {
+                // the root cause of this situation is the fact that:
+                // - for each finally clause, to separate blocks are generated (see comments in DeadStore class)
+                // - when analyzing a block that follow a try statement, we cannot tell whether it's a catch,
+                //   a finally or just the code which follows the try-catch-finally statement
+                //  this FN can be fixed by adding finally clause in the instructions of the finally block to distinguish catch from finally
+                actor = null; // FN
+            }
+        }
+
+        internal void Finally_Followed_By_Deadstore(object actor)
+        {
+            try
+            {
+                Foo(actor);
+            }
+            finally
+            {
+                actor = null; // Noncompliant
+            }
+            actor = null; // Noncompliant
+        }
+
+        internal void SnippetTwo(object actor)
+        {
+            try
+            {
+                Foo(actor);
+            }
+            catch (Exception)
+            {
+                actor = null; // Noncompliant
+            }
+            Foo(null);
+        }
+
+        internal void SnippetThree(object actor, int i)
+        {
+            try
+            {
+                Foo(actor);
+            }
+            catch (Exception) when(i == 1)
+            {
+                actor = null; // Noncompliant
+            }
+            finally
+            {
+                actor = null; // Noncompliant
+            }
+        }
+
+        private void Foo(object obj) { }
+    }
+
     public class ReproGithubIssue2311
     {
         private void SwitchCaseWithWhenAndLocalScope(object sender, KeyEventArgs e)
@@ -516,8 +580,8 @@ namespace Tests.Diagnostics
             {
                 try
                 {
-                    attempts++; // Noncompliant FP
-                    // do stuff
+                    attempts++;
+                    DoNothing();
                     break;
                 }
                 catch (Exception ex)
@@ -528,6 +592,37 @@ namespace Tests.Diagnostics
             } while (true);
         }
 
+        public static void ComplexRetryOnException(int retries)
+        {
+            var attempts = 0;
+            try
+            {
+                do
+                {
+                    if (retries > 0)
+                    {
+                        DoNothing();
+                        try
+                        {
+                            attempts++;
+                            DoNothing();
+                            break;
+                        }
+                        catch (ArgumentException ex)
+                        {
+                            DoNothing();
+                        }
+                    }
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+                if (attempts > retries)
+                    throw;
+                DoNothing();
+            }
+        }
+
         public void Bar()
         {
             bool isFirst = true;
@@ -535,20 +630,16 @@ namespace Tests.Diagnostics
             {
                 try
                 {
-                    if (isFirst)
-                    {
-                        Console.WriteLine("First");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Not first");
-                    }
+                    DoNothing(isFirst);
                 }
                 finally
                 {
-                    isFirst = false; // Noncompliant FP
+                    isFirst = false; // is used in DoNothing, after loop
                 }
             }
         }
+
+        private static void DoNothing() { }
+        private static void DoNothing(bool b) { }
     }
 }

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MemberInitializerRedundant.Fixed.cs
@@ -152,12 +152,12 @@ namespace Tests.Diagnostics
 
     class Person9
     {
-        int year = 1980;
+        int year; // Fixed
         public Person9()
         {
             try
             {
-                year = 400; // FN - the CFG connects the beginning of the try block with the catch, hence we have a path where "year" is not rewritten
+                year = 400; // the CFG connects the beginning of the try block with the catch, hence we have a path where "year" is not rewritten
             }
             catch (Exception)
             {

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MemberInitializerRedundant.cs
@@ -154,12 +154,12 @@ namespace Tests.Diagnostics
 
     class Person9
     {
-        int year = 1980;
+        int year = 1980; // Noncompliant
         public Person9()
         {
             try
             {
-                year = 400; // FN - the CFG connects the beginning of the try block with the catch, hence we have a path where "year" is not rewritten
+                year = 400; // the CFG connects the beginning of the try block with the catch, hence we have a path where "year" is not rewritten
             }
             catch (Exception)
             {

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/NullPointerDereferenceCSharp6.cs
@@ -27,7 +27,7 @@ namespace Tests.Diagnostics
             {
                 o = new object();
             }
-            return o.ToString(); // Noncompliant, when e.Message is null o will be null
+            return o.ToString(); // If e.Message is null, the exception won't be caught and this is not reachable
         }
 
         // https://github.com/SonarSource/sonar-csharp/issues/1324

commit 1a27fac4bfc41fa3d924cbb66fed555ae421a7ed

Fix regression (Symbolic Execution exception which breaks cirru… (#2544)
* Fix regression introduced in #2511

Note: this was making many rules fail: NullPointerDereference, ObjectsShouldNotBeDisposedMoreThanOnce, EmptyNullableValueAccess, ConditionEvaluatesToConstant, InvalidCastToInterface, EmptyCollectionsShouldNotBeEnumerated

I have added just some partial UTs that go over the exploded graph

sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/ControlFlowGraph/CSharpControlFlowGraphBuilder.cs
@@ -773,7 +773,6 @@ namespace SonarAnalyzer.ControlFlowGraph.CSharp
                 var newSuccessors = possibleTryBlock.SuccessorBlocks.ToList();
                 newSuccessors.Add(target);
                 var branchBlock = CreateBranchBlock(possibleTryBlock.BranchingNode, newSuccessors);
-                branchBlock.ReversedInstructions.Add(breakStatement);
                 return branchBlock;
             }
             return CreateJumpBlock(breakStatement, target, currentBlock);

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/ControlFlowGraph/ControlFlowGraphTest.cs
@@ -2759,7 +2759,7 @@ namespace NS
             VerifyAllInstructions(doBlock, "cw0", "cw0()");
             doBlock.SuccessorBlocks.Should().OnlyContainInOrder(tryBody);
 
-            VerifyAllInstructions(tryBody, "attempts", "attempts++", "cw1", "cw1()", "break;");
+            VerifyAllInstructions(tryBody, "attempts", "attempts++", "cw1", "cw1()");
             // this is wrong, the tryBody should not have a connection with whileStmt, it can lead to FNs
             tryBody.SuccessorBlocks.Should().OnlyContainInOrder(catchBlock, whileStmt, afterDoWhile);
 
@@ -2826,7 +2826,7 @@ namespace NS
             VerifyAllInstructions(doBeforeTry, "cw0", "cw0()");
             doBeforeTry.SuccessorBlocks.Should().OnlyContainInOrder(tryStatement);
 
-            VerifyAllInstructions(tryStatement, "cw1", "cw1()", "break;");
+            VerifyAllInstructions(tryStatement, "cw1", "cw1()");
             tryStatement.SuccessorBlocks.Should().OnlyContainInOrder(catchBody, finallyBody_NormalFlow, finallyBlock_Exit, afterDoWhile);
 
             tryBody.SuccessorBlocks.Should().OnlyContainInOrder(catchBody, finallyBody_NormalFlow, finallyBlock_Exit);
@@ -2983,7 +2983,7 @@ namespace NS
             VerifyAllInstructions(insideDoBeforeTry, "cw0", "cw0()");
             insideDoBeforeTry.SuccessorBlocks.Should().OnlyContainInOrder(insideTry);
 
-            VerifyAllInstructions(insideTry, "attempts", "attempts++", "cw1", "cw1()", "break;");
+            VerifyAllInstructions(insideTry, "attempts", "attempts++", "cw1", "cw1()");
             insideTry.SuccessorBlocks.Should().OnlyContainInOrder(catchBodyWithIf, whileStmt, afterDoWhile);
 
             temporaryStrayBlock.ReversedInstructions.Should().BeEmpty();

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EmptyNullableValueAccess.cs
@@ -117,4 +117,28 @@ namespace Tests.Diagnostics
             }
         }
     }
+
+    class TestLoopWithBreak
+    {
+        public static void LoopWithBreak(System.Collections.Generic.IEnumerable<string> list, bool condition)
+        {
+            int? i1 = null;
+            foreach (string x in list)
+            {
+                try
+                {
+                    if (condition)
+                    {
+                        Console.WriteLine(i1.Value); // Noncompliant
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+
 }

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/NullPointerDereference.cs
@@ -663,4 +663,46 @@ namespace Tests.Diagnostics
             s.ToString(); // Noncompliant
         }
     }
+
+    class TestLoopWithBreak
+    {
+        public static void LoopWithBreak(IEnumerable<string> list)
+        {
+            foreach (string x in list)
+            {
+                try
+                {
+                    if (x == null)
+                    {
+                        x.ToString(); // Noncompliant
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+
+        public static IEnumerable<string> LoopWithYeildBreak(IEnumerable<string> list)
+        {
+            foreach (string x in list)
+            {
+                try
+                {
+                    if (x == null)
+                    {
+                        yield return x.ToString(); // Noncompliant
+                    }
+                    yield break;
+                }
+                finally
+                {
+                    // do stuff
+                }
+            }
+        }
+
+    }
 }

sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ObjectsShouldNotBeDisposedMoreThanOnce.cs
@@ -182,4 +182,27 @@ namespace Tests.Diagnostics
 
         }
     }
+
+    class TestLoopWithBreak
+    {
+        public static void LoopWithBreak(System.Collections.Generic.IEnumerable<string> list, bool condition, IInterface1 instance1)
+        {
+            foreach (string x in list)
+            {
+                try
+                {
+                    if (condition)
+                    {
+                        instance1.Dispose(); // Noncompliant
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+            }
+        }
+    }
+
 }