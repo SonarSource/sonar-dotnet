8ca72789c206725683baa46fd5ab53a56755a990
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2019-08-12 15:36:30 +0200

    Add code cov (#2581)

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Common/SymbolicExecution/SymbolicExecutionException.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/SymbolicExecution/SymbolicExecutionException.cs
@@ -1,21 +1,16 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
 
 namespace SonarAnalyzer.SymbolicExecution
 {
+    [Serializable]
     public sealed class SymbolicExecutionException : Exception
     {
-        public SymbolicExecutionException()
-        {
-        }
+        public SymbolicExecutionException() { }
 
-        public SymbolicExecutionException(string message)
-            : base(message)
-        {
-        }
+        public SymbolicExecutionException(string message) : base(message) { }
 
-        public SymbolicExecutionException(string message, Exception inner)
-            : base(message, inner)
-        {
-        }
+        public SymbolicExecutionException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
