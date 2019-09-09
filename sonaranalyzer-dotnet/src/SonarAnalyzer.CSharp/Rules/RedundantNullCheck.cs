f79b4ca20942b2060c10fa2ba6dedc3fba63671e
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2019-09-09 14:28:04 +0200

    [C#] Rule S4201: Null checks should not be redundant

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/RedundantNullCheck.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2019 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.Helpers.CSharp;
+using SonarAnalyzer.ShimLayer.CSharp;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class RedundantNullCheck : RedundantNullCheckBase<BinaryExpressionSyntax>
+    {
+        private const string MessageFormat = "Remove this unnecessary null check; 'is' returns false for nulls.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                CheckAndExpression,
+                SyntaxKind.LogicalAndExpression);
+
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                CheckOrExpression,
+                SyntaxKind.LogicalOrExpression);
+        }
+
+        protected override SyntaxNode GetLeftNode(BinaryExpressionSyntax binaryExpression) => binaryExpression.Left.RemoveParentheses();
+
+        protected override SyntaxNode GetRightNode(BinaryExpressionSyntax binaryExpression) => binaryExpression.Right.RemoveParentheses();
+
+        private SyntaxNode GetNullCheckVariableForKind(SyntaxNode node, SyntaxKind kind) {
+            if (RemoveParentheses(node) is BinaryExpressionSyntax binaryExpression && binaryExpression.IsKind(kind)) {
+                var leftNode = GetLeftNode(binaryExpression);
+                var rightNode = GetRightNode(binaryExpression);
+
+                if (leftNode.IsKind(SyntaxKind.NullLiteralExpression)) {
+                    return rightNode;
+                }
+                if (rightNode.IsKind(SyntaxKind.NullLiteralExpression))
+                {
+                    return leftNode;
+                }
+            }
+            return null;
+        }
+
+        protected override SyntaxNode GetNullCheckVariable(SyntaxNode node) => GetNullCheckVariableForKind(node, SyntaxKind.EqualsExpression);
+
+        protected override SyntaxNode GetNonNullCheckVariable(SyntaxNode node) => GetNullCheckVariableForKind(node, SyntaxKind.NotEqualsExpression);
+
+        protected override SyntaxNode GetIsOperatorCheckVariable(SyntaxNode node)
+        {
+            var innerExpression = RemoveParentheses(node);
+            if (innerExpression is BinaryExpressionSyntax binaryExpression && binaryExpression.IsKind(SyntaxKind.IsExpression))
+            {
+                return GetLeftNode(binaryExpression);
+            } else if (innerExpression.IsKind(SyntaxKindEx.IsPatternExpression))
+            {
+                var isPatternExpression = (IsPatternExpressionSyntaxWrapper)innerExpression;
+                return isPatternExpression.Expression.RemoveParentheses();
+            }
+            return null;
+        }
+
+        protected override SyntaxNode GetInvertedIsOperatorCheckVariable(SyntaxNode node)
+        {
+            if (RemoveParentheses(node) is PrefixUnaryExpressionSyntax prefixUnary && prefixUnary.IsKind(SyntaxKind.LogicalNotExpression))
+            {
+                return GetIsOperatorCheckVariable(prefixUnary.Operand);
+            }
+            return null;
+        }
+
+        protected override bool AreEquivalent(SyntaxNode node1, SyntaxNode node2) => CSharpEquivalenceChecker.AreEquivalent(node1, node2);
+
+        private SyntaxNode RemoveParentheses(SyntaxNode syntaxNode) => syntaxNode.RemoveParentheses();
+    }
+}
---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/RedundantNullCheckBase.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2019 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class RedundantNullCheckBase<TBinaryExpression> : SonarDiagnosticAnalyzer
+        where TBinaryExpression : SyntaxNode
+    {
+        internal const string DiagnosticId = "S4201";
+
+        protected abstract SyntaxNode GetLeftNode(TBinaryExpression binaryExpression);
+
+        protected abstract SyntaxNode GetRightNode(TBinaryExpression binaryExpression);
+
+        protected abstract SyntaxNode GetNullCheckVariable(SyntaxNode node);
+
+        protected abstract SyntaxNode GetNonNullCheckVariable(SyntaxNode node);
+
+        protected abstract SyntaxNode GetIsOperatorCheckVariable(SyntaxNode node);
+
+        protected abstract SyntaxNode GetInvertedIsOperatorCheckVariable(SyntaxNode node);
+
+        protected abstract bool AreEquivalent(SyntaxNode node1, SyntaxNode node2);
+
+        // LogicalAnd (C#) / AndAlso (VB)
+        protected void CheckAndExpression(SyntaxNodeAnalysisContext context)
+        {
+            var binaryExpression = (TBinaryExpression)context.Node;
+            var binaryExpressionLeft = GetLeftNode(binaryExpression);
+            var binaryExpressionRight = GetRightNode(binaryExpression);
+
+            if (GetNonNullCheckVariable(binaryExpressionLeft) is SyntaxNode node1
+                && GetIsOperatorCheckVariable(binaryExpressionRight) is  SyntaxNode node2
+                && AreEquivalent(node1, node2))
+            {
+                context.ReportDiagnosticWhenActive(Diagnostic.Create(SupportedDiagnostics[0], binaryExpressionLeft.GetLocation()));
+            }
+            if (GetNonNullCheckVariable(binaryExpressionRight) is SyntaxNode node3
+                && GetIsOperatorCheckVariable(binaryExpressionLeft) is SyntaxNode node4
+                && AreEquivalent(node3, node4))
+            {
+                context.ReportDiagnosticWhenActive(Diagnostic.Create(SupportedDiagnostics[0], binaryExpressionRight.GetLocation()));
+            }
+        }
+
+        // LogicalOr (C#) / OrElse (VB)
+        protected void CheckOrExpression(SyntaxNodeAnalysisContext context)
+        {
+            var binaryExpression = (TBinaryExpression)context.Node;
+            var binaryExpressionLeft = GetLeftNode(binaryExpression);
+            var binaryExpressionRight = GetRightNode(binaryExpression);
+
+            if (GetNullCheckVariable(binaryExpressionLeft) is SyntaxNode node1
+                && GetInvertedIsOperatorCheckVariable(binaryExpressionRight) is SyntaxNode node2
+                && AreEquivalent(node1, node2))
+            {
+                context.ReportDiagnosticWhenActive(Diagnostic.Create(SupportedDiagnostics[0], binaryExpressionLeft.GetLocation()));
+            }
+            if (GetNullCheckVariable(binaryExpressionRight) is SyntaxNode node3
+                && GetInvertedIsOperatorCheckVariable(binaryExpressionLeft) is SyntaxNode node4
+                && AreEquivalent(node3, node4))
+            {
+                context.ReportDiagnosticWhenActive(Diagnostic.Create(SupportedDiagnostics[0], binaryExpressionRight.GetLocation()));
+            }
+        }
+    }
+}
