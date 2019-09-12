ebd657882db892020cca8f1b7bba81d5db046bd6
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2019-09-12 10:24:48 +0200

    [C#] Rule S1128: Useless usings should be removed

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.CSharp/Rules/UnnecessaryImports.cs
@@ -0,0 +1,207 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2019 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+using SonarAnalyzer.Helpers.CSharp;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    [Rule(DiagnosticId)]
+    public sealed class UnnecessaryImports : SonarDiagnosticAnalyzer
+    {
+
+        internal const string DiagnosticId = "S1128";
+        private const string MessageFormat = "Remove this {0} 'using'.";
+
+        private static readonly DiagnosticDescriptor rule =
+            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(rule);
+
+        protected override void Initialize(SonarAnalysisContext context)
+        {
+            context.RegisterSyntaxNodeActionInNonGenerated(
+                c =>
+                {
+                    var compilationUnit = (CompilationUnitSyntax)c.Node;
+                    var simpleNamespaces = compilationUnit.Usings.Where(usingDirective => usingDirective.Alias == null);
+                    var globalUsingDirectives = new HashSet<EquivalentNameSyntax>();
+                    globalUsingDirectives.UnionWith(simpleNamespaces.Select(x => new EquivalentNameSyntax(x.Name)));
+
+                    var visitor = new CSharpRemovableUsingWalker(c, globalUsingDirectives, null);
+                    foreach (var member in compilationUnit.Members)
+                    {
+                        visitor.SafeVisit(member);
+                    }
+                    foreach (var attribute in compilationUnit.AttributeLists)
+                    {
+                        visitor.SafeVisit(attribute);
+                    }
+
+                    CheckDuplicateUsings(c, new HashSet<EquivalentNameSyntax>(), simpleNamespaces);
+                    CheckUnnecessaryUsings(c, simpleNamespaces, visitor.usedNamespaces);
+                },
+                SyntaxKind.CompilationUnit);
+
+        }
+
+        private static void CheckDuplicateUsings(SyntaxNodeAnalysisContext context, HashSet<EquivalentNameSyntax> ancestorsUsingDirectives, IEnumerable<UsingDirectiveSyntax> usingDirectives)
+        {
+            var groupingDirectives = usingDirectives
+                .GroupBy(usingDirective => new EquivalentNameSyntax(usingDirective.Name))
+                .ToList();
+
+            foreach (var potentialDuplicate in groupingDirectives)
+            {
+                var duplicates = potentialDuplicate.Skip(1);
+                foreach (var duplicate in duplicates)
+                {
+                    context.ReportDiagnosticWhenActive(Diagnostic.Create(rule, duplicate.GetLocation(), "duplicate"));
+                }
+
+                if (ancestorsUsingDirectives.Contains(potentialDuplicate.Key))
+                {
+                    context.ReportDiagnosticWhenActive(Diagnostic.Create(rule, potentialDuplicate.First().GetLocation(), "duplicate"));
+                }
+            }
+        }
+
+        private static void CheckUnnecessaryUsings(SyntaxNodeAnalysisContext context, IEnumerable<UsingDirectiveSyntax> usingDirectives, HashSet<INamespaceSymbol> usedNamespaces)
+        {
+            foreach (var usingDirective in usingDirectives)
+            {
+                if (context.SemanticModel.GetSymbolInfo(usingDirective.Name).Symbol is INamespaceSymbol namespaceSymbol
+                && !usedNamespaces.Any(usedNamespace => IsSameNamespace(usedNamespace, namespaceSymbol)))
+                {
+                    context.ReportDiagnosticWhenActive(Diagnostic.Create(rule, usingDirective.GetLocation(), "unnecessary"));
+                }
+            }
+        }
+
+        private static bool IsSameNamespace(INamespaceSymbol namespace1, INamespaceSymbol namespace2)
+        {
+            return namespace1.IsGlobalNamespace && namespace2.IsGlobalNamespace
+                || (namespace1.Name.Equals(namespace2.Name)
+                && namespace1.ContainingNamespace != null
+                && namespace2.ContainingNamespace != null
+                && IsSameNamespace(namespace1.ContainingNamespace, namespace2.ContainingNamespace));
+        }
+
+        private class CSharpRemovableUsingWalker : CSharpSyntaxWalker
+        {
+            private readonly SyntaxNodeAnalysisContext context;
+            private readonly HashSet<EquivalentNameSyntax> usingDirectivesFromParent;
+            private readonly INamespaceSymbol currentNamespace;
+            public readonly HashSet<INamespaceSymbol> usedNamespaces;
+
+            public CSharpRemovableUsingWalker(SyntaxNodeAnalysisContext context, HashSet<EquivalentNameSyntax> usingDirectives, INamespaceSymbol currentNamespace)
+            {
+                this.context = context;
+                this.usingDirectivesFromParent = usingDirectives;
+                this.usedNamespaces = new HashSet<INamespaceSymbol>();
+                this.currentNamespace = currentNamespace;
+            }
+
+            public override void VisitNamespaceDeclaration(NamespaceDeclarationSyntax node)
+            {
+                var simpleNamespaces = node.Usings.Where(usingDirective => usingDirective.Alias == null);
+                var newUsingDirectives = new HashSet<EquivalentNameSyntax>();
+                newUsingDirectives.UnionWith(usingDirectivesFromParent);
+                newUsingDirectives.UnionWith(simpleNamespaces.Select(x => new EquivalentNameSyntax(x.Name)));
+
+                var visitingNamespace = context.SemanticModel.GetSymbolInfo(node.Name).Symbol as INamespaceSymbol;
+                var visitor = new CSharpRemovableUsingWalker(context, newUsingDirectives, visitingNamespace);
+                foreach (var member in node.Members)
+                {
+                    visitor.SafeVisit(member);
+                }
+
+                CheckDuplicateUsings(context, usingDirectivesFromParent, simpleNamespaces);
+                CheckUnnecessaryUsings(context, simpleNamespaces, visitor.usedNamespaces);
+
+                usedNamespaces.UnionWith(visitor.usedNamespaces);
+            }
+
+            public override void VisitIdentifierName(IdentifierNameSyntax node)
+            {
+                VisitNameNode(node);
+            }
+
+            public override void VisitGenericName(GenericNameSyntax node)
+            {
+                VisitNameNode(node);
+                base.VisitGenericName(node);
+            }
+
+            private void VisitNameNode(SimpleNameSyntax node)
+            {
+                if (context.SemanticModel.GetSymbolInfo(node).Symbol is ISymbol symbol
+                    && symbol.ContainingNamespace is INamespaceSymbol namespaceSymbol
+                    && (currentNamespace == null || !IsSelfOrAncestorNamespace(namespaceSymbol, currentNamespace)))
+                {
+                    usedNamespaces.Add(namespaceSymbol);
+                }
+            }
+
+
+            private bool IsSelfOrAncestorNamespace(INamespaceSymbol namespaceSymbol, INamespaceSymbol currentNamespace)
+            {
+                return IsSameNamespace(namespaceSymbol, currentNamespace)
+                    || (currentNamespace.ContainingNamespace != null && IsSelfOrAncestorNamespace(namespaceSymbol, currentNamespace.ContainingNamespace));
+            }
+        }
+    }
+
+    internal sealed class EquivalentNameSyntax : IEquatable<EquivalentNameSyntax>
+    {
+        private readonly NameSyntax name;
+
+        public EquivalentNameSyntax(NameSyntax name)
+        {
+            this.name = name;
+        }
+
+        public override int GetHashCode()
+        {
+            return name.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            return other is EquivalentNameSyntax equivalentName
+                && Equals(equivalentName);
+        }
+
+        public bool Equals(EquivalentNameSyntax other)
+        {
+            return other != null && CSharpEquivalenceChecker.AreEquivalent(this.name, other.name);
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnnecessaryImports.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Concurrent; // Noncompliant {{Remove this unnecessary 'using'.}}
+using System.IO;
+using System.IO; // Noncompliant {{Remove this duplicate 'using'.}}
+using static System.Console;
+using static System.DateTime; // FN - System.DateTime is not a namespace symbol
+using MySysAlias = System;
+using MyOtherAlias = System.Collections; // FN - aliases not yet supported
+using MyNamespace1; // Compliant - used inside MyNamspace2 to access Ns1_1
+using MyNamespace3; // Noncompliant {{Remove this unnecessary 'using'.}}
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyNamespace0
+{
+    class Ns0_0 { }
+}
+
+namespace MyNamespace1
+{
+    class Ns1_0 { }
+}
+
+namespace MyNamespace2
+{
+    class Ns2_0
+    {
+        Ns1_0 ns11;
+    }
+}
+
+namespace MyNamespace2.Level1
+{
+    using MyNamespace0;
+    using MyNamespace0; // Noncompliant {{Remove this duplicate 'using'.}}
+    using MyNamespace0; // Noncompliant {{Remove this duplicate 'using'.}}
+    using MyNamespace1; // Noncompliant {{Remove this duplicate 'using'.}}
+    using System.Linq; // Noncompliant {{Remove this unnecessary 'using'.}}
+    using MyNamespace2.Level1; // Noncompliant {{Remove this unnecessary 'using'.}}
+    using MyNamespace2; // Noncompliant {{Remove this unnecessary 'using'.}}
+
+    class Ns2_1
+    {
+        Ns0_0 ns00;
+        Ns2_0 ns20;
+        Ns1_0 ns11;
+        Ns2_1 ns21;
+    }
+
+    namespace Level2
+    {
+        using MyNamespace1; // Noncompliant {{Remove this duplicate 'using'.}}
+        using System.IO; // Noncompliant {{Remove this duplicate 'using'.}}
+
+        class Ns2_2
+        {
+            Ns1_0 ns11;
+
+            void M(IEnumerable<DateTimeStyles> myEnumerable)
+            {
+                File.ReadAllLines("");
+                WriteLine("");
+                MySysAlias.Console.WriteLine("");
+            }
+        }
+    }
+}
+
+namespace MyNamespace2.Level1.Level2
+{
+    using MyNamespace0;
+    using MyNamespace2.Level1; // Noncompliant {{Remove this unnecessary 'using'.}}
+
+    class Ns2_3
+    {
+        Ns0_0 ns00;
+        Ns2_1 ns21;
+    }
+}
+
+namespace MyNamespace3
+{
+    class Ns3_0 { }
+}
