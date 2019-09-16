168a414283dd2e43a8e58da3f2abb77163302c4d
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2019-09-16 13:28:15 +0200

    [C#] Rule S4136: Method overloads should be grouped together

---
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Rules/MethodOverloadsShouldBeGroupedBase.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2019 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using SonarAnalyzer.Common;
+using SonarAnalyzer.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarAnalyzer.Rules
+{
+    public abstract class MethodOverloadsShouldBeGroupedBase<TMemberDeclarationSyntax> : SonarDiagnosticAnalyzer
+        where TMemberDeclarationSyntax : SyntaxNode
+    {
+        internal const string DiagnosticId = "S4136";
+        protected const string MessageFormat = "All '{0}' signatures should be adjacent";
+
+        protected abstract DiagnosticDescriptor Rule { get; }
+
+        protected abstract bool IsCaseSensitive { get; }
+
+        protected abstract SyntaxToken? GetNameSyntaxNode(TMemberDeclarationSyntax member);
+
+        protected string GetMethodName(TMemberDeclarationSyntax member)
+        {
+            var nameSyntaxNode = GetNameSyntaxNode(member);
+            if (IsCaseSensitive)
+            {
+                return nameSyntaxNode?.ValueText;
+            }
+            else
+            {
+                return nameSyntaxNode?.ValueText?.ToLowerInvariant();
+            }
+        }
+
+        protected Location GetLocation(TMemberDeclarationSyntax member) => GetNameSyntaxNode(member)?.GetLocation();
+
+        protected StringComparison CaseSensitivity
+        {
+            get => IsCaseSensitive ? StringComparison.InvariantCulture : StringComparison.InvariantCultureIgnoreCase;
+        }
+
+        protected void CheckMembers(SyntaxNodeAnalysisContext c, IEnumerable<TMemberDeclarationSyntax> members)
+        {
+            var misplacedOverloadsMapping = GetMisplacedOverloads(members);
+            foreach (var misplacedOverloadsEntry in misplacedOverloadsMapping)
+            {
+                var misplacedOverloads = misplacedOverloadsEntry.Value;
+                if (misplacedOverloads.Count > 1)
+                {
+                    var mainLocation = misplacedOverloads.First();
+                    var secondaryLocations = misplacedOverloads.Skip(1).Select(location => new SecondaryLocation(location, "Non-adjacent overload"));
+                    c.ReportDiagnosticWhenActive(
+                        Diagnostic.Create(
+                            descriptor: Rule,
+                            location: mainLocation,
+                            additionalLocations: secondaryLocations.ToAdditionalLocations(),
+                            properties: secondaryLocations.ToProperties(),
+                            messageArgs: misplacedOverloadsEntry.Key));
+
+                }
+            }
+        }
+
+        protected IDictionary<string, List<Location>> GetMisplacedOverloads(IEnumerable<TMemberDeclarationSyntax> members)
+        {
+            var misplacedOverloads = new Dictionary<string, List<Location>>();
+            string previousMemberName = null;
+            foreach (var member in members)
+            {
+                if (GetMethodName(member) is string methodName
+                    && GetLocation(member) is Location location)
+                {
+                    if (misplacedOverloads.TryGetValue(methodName, out var currentList))
+                    {
+                        if (!methodName.Equals(previousMemberName, CaseSensitivity))
+                        {
+                            currentList.Add(location);
+                        }
+                    }
+                    else
+                    {
+                        misplacedOverloads.Add(methodName, new List<Location> { location });
+                    }
+                    previousMemberName = methodName;
+                }
+                else
+                {
+                    previousMemberName = null;
+                }
+            }
+            return misplacedOverloads;
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MethodOverloadsShouldBeGrouped.cs
@@ -0,0 +1,107 @@
+﻿namespace Tests.Diagnostics
+{
+    class A
+    {
+        void a() { } // Noncompliant {{All 'a' signatures should be adjacent}}
+//           ^
+
+        void a(int a, char b) { }
+
+        class B { }
+
+        void a(string a) { } // Secondary
+//           ^
+
+        interface C { }
+
+        void a(int a) { } // Secondary {{Non-adjacent overload}}
+//           ^
+
+        enum D { }
+
+        void a(double a) { } // Secondary {{Non-adjacent overload}}
+//           ^
+
+        int myField;
+
+        void a(char a) { } // Secondary {{Non-adjacent overload}}
+//           ^
+
+        int MyProperty
+        {
+            get;
+        }
+
+        void a(char a, int b) { } // Secondary {{Non-adjacent overload}}
+//           ^
+    }
+
+    class B
+    {
+        B() { } // Noncompliant {{All 'B' signatures should be adjacent}}
+
+        B(int a) { }
+
+        B(char a) { }
+
+        ~B() { }
+
+        B(double a) { } // Secondary {{Non-adjacent overload}}
+    }
+
+    class C
+    {
+        public static C operator -(C c1) // Compliant - this one is unary -, while the other is binary -
+        {
+            return null;
+        }
+
+        void a() { }
+
+        public static implicit operator B(C d) => null; // Compliant - this operator and the B() method are not related
+
+        void B() { }
+
+        public static C operator -(C c1, C c2)
+        {
+            return null;
+        }
+    }
+
+    interface D
+    {
+        void DoSomething(double b);
+
+        void DoSomething();
+
+        void DoSomething(int a);
+    }
+
+    interface E
+    {
+        void DoSomething(double b); // Noncompliant {{All 'DoSomething' signatures should be adjacent}}
+//           ^^^^^^^^^^^
+
+        void DoSomething();
+
+        void DoSomethingElse();
+
+        void DoSomething(int a); // Secondary {{Non-adjacent overload}}
+//           ^^^^^^^^^^^
+    }
+
+    struct F
+    {
+        F(char a) { }
+
+        F(int a) { }
+
+        void MyStructMethod() { } // Noncompliant {{All 'MyStructMethod' signatures should be adjacent}}
+
+        void MyStructMethod(int a) { }
+
+        void MyStructMethod2() { }
+
+        void MyStructMethod(char a) { } // Secondary {{Non-adjacent overload}}
+    }
+}
