431d73938461b289cfd8f88e9d7b8926d1bce57d
Amélie Renard <44666826+amelie-renard-sonarsource@users.noreply.github.com>
2019-09-17 10:42:04 +0200

    Handle preincrement, postincrement, predecrement and postdecrement expressions

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/CBDE/CheckMLIRWithReference.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/CBDE/CheckMLIRWithReference.cs
@@ -53,5 +53,80 @@ func @_$invalid$global$code$.Func$int$(%arg0: i32) {
             MlirTestUtilities.ValidateWithReference(code, expected, TestContext.TestName);
 
         }
+
+        [TestMethod]
+        public void PrePostIncrementDecrement()
+        {
+            var code = @"
+class A
+{
+    private long p;
+    private readonly int[] indices;
+
+    private long f(int i)
+    {
+        int result;
+        result = ++i;
+        result = i++;
+        result = --i;
+        result = i--;
+
+        return ++p; // Not handled, but should not crash
+    }
+
+    private void g(int dim)
+    {
+         _indices[dimension]++; // Not handled, but should not crash
+    }
+}
+";
+            var expected = @"
+func @_A.f$int$(%arg0: i32) -> none {
+  %0 = cbde.alloca i32
+  cbde.store %arg0, %0 : memref<i32>
+  br ^bb1
+^bb1:	// pred: ^bb0
+  %1 = cbde.alloca i32
+  %2 = cbde.load %0 : memref<i32>
+  %c1_i32 = constant 1 : i32
+  %3 = addi %2, %c1_i32 : i32
+  cbde.store %3, %0 : memref<i32>
+  cbde.store %3, %1 : memref<i32>
+  %4 = cbde.load %0 : memref<i32>
+  %c1_i32_0 = constant 1 : i32
+  %5 = addi %4, %c1_i32_0 : i32
+  cbde.store %5, %0 : memref<i32>
+  cbde.store %4, %1 : memref<i32>
+  %6 = cbde.load %0 : memref<i32>
+  %c1_i32_1 = constant 1 : i32
+  %7 = subi %6, %c1_i32_1 : i32
+  cbde.store %7, %0 : memref<i32>
+  cbde.store %7, %1 : memref<i32>
+  %8 = cbde.load %0 : memref<i32>
+  %c1_i32_2 = constant 1 : i32
+  %9 = subi %8, %c1_i32_2 : i32
+  cbde.store %9, %0 : memref<i32>
+  cbde.store %8, %1 : memref<i32>
+  %10 = cbde.unknown : none
+  %11 = cbde.unknown : none
+  return %11 : none
+^bb2:	// no predecessors
+  cbde.unreachable
+}
+
+func @_A.g$int$(%arg0: i32) {
+  %0 = cbde.alloca i32
+  cbde.store %arg0, %0 : memref<i32>
+  br ^bb1
+^bb1:	// pred: ^bb0
+  %1 = cbde.unknown : none
+  br ^bb2
+^bb2:	// pred: ^bb1
+  return
+}
+
+";
+            MlirTestUtilities.ValidateWithReference(code, expected, TestContext.TestName);
+        }
     }
 }
