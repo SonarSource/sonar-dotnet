7b773527425b14fec7fcea070ac183ebb9153766
Pavel Mikula <pavel.mikula@sonarsource.com>
2019-11-11 18:01:06 +0100

    Added function() invocation inspection

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/CertificateValidationCheckMini.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/CertificateValidationCheckMini.cs
@@ -7,23 +7,61 @@ using System.Security.Cryptography.X509Certificates;
 namespace Tests.Diagnostics
 {
 
-    class RootType
+    class CertificateValidationChecks   //FIXME: Vyhodit
     {
-        class MiddleType
+
+
+        void Mini()
         {
+            //False Negative - not implemented case
+            ServicePointManager.ServerCertificateValidationCallback += FindValidator(false);    //Noncompliant
+            ServicePointManager.ServerCertificateValidationCallback += FindValidator();         //Noncompliant
+            ServicePointManager.ServerCertificateValidationCallback += LambdaValidator();       //Noncompliant
+            ServicePointManager.ServerCertificateValidationCallback += Compliant(true);     
+        }
 
-            class CertificateValidationChecks   //FIXME: Vyhodit
+        static RemoteCertificateValidationCallback FindValidator()
+        {
+            return NoncompilantValidation;                                      //Secondary
+        }
+
+        static bool NoncompilantValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;                                                        //Secondary
+                                                                                //Secondary@-1
+        }
+
+        static RemoteCertificateValidationCallback LambdaValidator()
+        {
+            return (sender, certificate, chain, SslPolicyErrors) => true;        //Secondary
+        }
+
+        static RemoteCertificateValidationCallback FindValidator(bool useDelegate)
+        {
+            if (useDelegate)
             {
-
-
-                void Mini()
-                {
-
-                }
-                
-
+                return NoncompilantValidation;                                  //Secondary
+            }
+            else
+            {
+                return (sender, certificate, chain, SslPolicyErrors) => true;   //Secondary
             }
         }
 
+
+        static RemoteCertificateValidationCallback Compliant(bool Compliant)
+        {
+            if (Compliant)
+            {
+                return null;
+            }
+            else
+            {
+                return (sender, certificate, chain, SslPolicyErrors) => true;
+            }
+        }
+
+
     }
 }
+
