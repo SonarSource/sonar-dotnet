009a05d4fcf5ddf6e351d1bc775890be7b6a6dd9
Pavel Mikula <pavel.mikula@sonarsource.com>
2019-11-12 15:38:44 +0100

    Resistance against recursion

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/CertificateValidationCheckMini.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/CertificateValidationCheckMini.cs
@@ -18,97 +18,15 @@ namespace Tests.Diagnostics
 
         void Mini()
         {
-
-        }
-
-
-        void DelegateReturnedByFunction()
-        {
-            CreateRQ().ServerCertificateValidationCallback += FindNoncompliant(false);      //Noncompliant
-            CreateRQ().ServerCertificateValidationCallback += FindNoncompliant();           //Noncompliant
-            CreateRQ().ServerCertificateValidationCallback += FindLambdaValidator();        //Noncompliant
-            CreateRQ().ServerCertificateValidationCallback += FindCompliant(true);
-            CreateRQ().ServerCertificateValidationCallback += FindCompliantRecursive(3);
-            CreateRQ().ServerCertificateValidationCallback += FindNoncompliantRecursive(3); //Noncompliant
+            
         }
 
 
 
-        #region Find Validators
 
-
-        static RemoteCertificateValidationCallback FindNoncompliant()
-        {
-            return NoncompilantValidation;                                      //Secondary
-        }
-
-        static RemoteCertificateValidationCallback FindLambdaValidator()
-        {
-            return (sender, certificate, chain, SslPolicyErrors) => true;        //Secondary
-        }
-
-        static RemoteCertificateValidationCallback FindNoncompliant(bool useDelegate)   //All paths return noncompliant
-        {
-            if (useDelegate)
-            {
-                return NoncompilantValidation;                                  //Secondary
-            }
-            else
-            {
-                return (sender, certificate, chain, SslPolicyErrors) => true;   //Secondary
-            }
-        }
-
-
-        static RemoteCertificateValidationCallback FindCompliant(bool Compliant)    //At least one path returns compliant => there is a logic and it is considered compliant
-        {
-            if (Compliant)
-            {
-                return null;
-            }
-            else
-            {
-                return (sender, certificate, chain, SslPolicyErrors) => true;
-            }
-        }
-
-        static RemoteCertificateValidationCallback FindCompliantRecursive(int Index)
-        {
-            if (Index <= 0)
-            {
-                return CompilantValidation;
-            }
-            else
-            {
-                return FindCompliantRecursive(Index - 1);
-            }
-        }
-
-        static RemoteCertificateValidationCallback FindNoncompliantRecursive(int Index)
-        {
-            if (Index <= 0)
-            {
-                return NoncompilantValidation;                                  //Secondary
-            }
-            else
-            {
-                return FindNoncompliantRecursive(Index - 1);
-            }
-        }
-
-        #endregion
-
-
-        static bool NoncompilantValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        static bool InvalidValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
         {
             return true;    //Secondary
-                            //Secondary@-1
-
-        }
-
-        static bool CompilantValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-        {
-            return false; //Compliant
         }
 
     }
