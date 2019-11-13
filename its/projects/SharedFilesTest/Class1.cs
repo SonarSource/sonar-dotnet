c36f113b4b4d676c463dbde0794e3730f4f6a975
Pavel Mikula <pavel.mikula@sonarsource.com>
2019-11-13 11:36:27 +0100

    Revert of SharedFilesText\Class1.cs accidental update

--- its/projects/SharedFilesTest/Class1.cs
+++ its/projects/SharedFilesTest/Class1.cs
@@ -1,26 +1,6 @@
 ﻿public class Class1
 {
-
-    public Class1()
-    {
-        Mini();
-    }
-
-    void Mini()
-    {
-        var rq = (HttpWebRequest)System.Net.HttpWebRequest.Create("http://localhost");
-        var httpHandler = new System.Net.Http.HttpClientHandler();
-
-        // Secondary@+1
-        rq.ServerCertificateValidationCallback += (sender, certificate, chain, SslPolicyErrors) => true;    // Noncompliant
-        ServicePointManager.ServerCertificateValidationCallback += NoncompilantValidation;                  // Noncompliant
-        httpHandler.ServerCertificateCustomValidationCallback += NoncompilantValidation;                    // Noncompliant
-    }
-
-    bool NoncompilantValidation(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
-    {
-        // Secondary@+1
-        return true; // Secondary
-    }
-
+	public Class1()
+	{
+	}
 }
