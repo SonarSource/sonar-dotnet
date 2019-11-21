49f281dc0304b2636c558ae78ba0130b4516987e
Pavel Mikula <pavel.mikula@sonarsource.com>
2019-11-21 15:11:58 +0100

    ITS S1451 FP workaround

--- sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssuesVB/IntentionalFindingsVB/S4830.vb
+++ sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssuesVB/IntentionalFindingsVB/S4830.vb
@@ -2,18 +2,18 @@
 ' Copyright (C) 2008-2013 SonarSource
 ' mailto:contact AT sonarsource DOT com
 '
-' SonarQube Is free software; you can redistribute it And/Or
+' SonarQube is free software; you can redistribute it and/or
 ' modify it under the terms of the GNU Lesser General Public
 ' License as published by the Free Software Foundation; either
-' version 3 of the License, Or (at your option) any later version.
+' version 3 of the License, or (at your option) any later version.
 '
-' SonarQube Is distributed in the hope that it will be useful,
+' SonarQube is distributed in the hope that it will be useful,
 ' but WITHOUT ANY WARRANTY; without even the implied warranty of
-' MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ' Lesser General Public License for more details.
 '
 ' You should have received a copy of the GNU Lesser General Public License
-' along with this program; if Not, write to the Free Software Foundation,
+' along with this program; if not, write to the Free Software Foundation,
 ' Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Public Module S4830
