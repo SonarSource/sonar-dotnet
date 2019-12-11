1e22a80a3e57e48f13dc24802a145ea6c48e0e6a
Costin Zaharia <costin.zaharia@sonarsource.com>
2019-12-11 16:37:53 +0100

    Migrate project to .Net 4.8

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/EnumerableExtensions.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/EnumerableExtensions.cs
@@ -26,20 +26,6 @@ namespace SonarAnalyzer.Helpers
 {
     public static class EnumerableExtensions
     {
-        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> enumerable, IEqualityComparer<T> equalityComparer = null)
-        {
-            if (enumerable == null)
-            {
-                return equalityComparer != null
-                    ? new HashSet<T>(equalityComparer)
-                    : new HashSet<T>();
-            }
-
-            return equalityComparer != null
-                ? new HashSet<T>(enumerable, equalityComparer)
-                : new HashSet<T>(enumerable);
-        }
-
         /// <summary>
         /// Compares each element between two collections. The elements needs in the same order to be considered equal.
         /// </summary>
