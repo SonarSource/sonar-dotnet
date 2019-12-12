ba81d6b39316f4e17103db218b441bffe96ebba4
Loïc Joly <loic.joly@sonarsource.com>
2019-12-12 14:30:34 +0100

    Remove dead code

--- scripts/unzip.ps1
+++
@@ -1,8 +0,0 @@
-param(
-  [string]$zipfile,
-  [string]$outpath
-)
-
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
-
