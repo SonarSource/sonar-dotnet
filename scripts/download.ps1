328ae15a499da06d5f60af96be9225570399ff79
Loïc Joly <loic.joly@sonarsource.com>
2019-12-12 15:24:07 +0100

    Use modern MsBuild features and remove mention of Linux
    This is made possible by moving from Cix to AzurePipelines.

--- scripts/download.ps1
+++
@@ -1,9 +0,0 @@
-param(
-  [string]$url,
-  [string]$outfolder,
-  [string]$outfile
-)
-
-
-New-Item -ItemType Directory -Force -Path $outfolder | Out-Null
-(New-Object System.Net.WebClient).DownloadFile($url, $outfolder + $outfile)
