c4f8ab928fc13f1c0340fc67f01a3e6018145e17
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2019-12-12 18:18:35 +0100

    Fix vsixmanifest versions (#2889)

--- sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
+++ sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
@@ -24,9 +24,9 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("8.2.0")]
-[assembly: AssemblyFileVersion("8.2.0.")]
+[assembly: AssemblyFileVersion("8.2.0.0")]
 // The value should look like "Version:X.X.X.X Branch:not-set Sha1:not-set"
-[assembly: AssemblyInformationalVersion("Version:8.2.0. Branch: Sha1:")]
+[assembly: AssemblyInformationalVersion("Version:8.2.0.0 Branch: Sha1:")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyCopyright("Copyright © 2015-2019 SonarSource SA")]
