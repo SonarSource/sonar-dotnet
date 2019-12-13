8b37b30454513b456cf0a695e922a4bb2dcd01b4
Geoffray Adde <geoffray.adde@sonarsource.com>
2019-12-13 17:44:05 +0100

    update expected errors for CBDE it

--- sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssues/IntentionalFindings/S3949.cs
+++ sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssues/IntentionalFindings/S3949.cs
@@ -1,16 +1,11 @@
-﻿using System;
-
-namespace Tests.Diagnostics
+﻿public class IfConditionalAlwaysTrueOrFalse
 {
-    public class IfConditionalAlwaysTrueOrFalse
-    {
-        public void PositiveOverflow() {
-            int i = 2147483600;
-            i +=100; // Noncompliant {{There is a path on which this operation always overflows}}
-        }
-        public void NegativeOverflow() {
-            int i = -2147483600;
-            i -=100; // Noncompliant {{There is a path on which this operation always overflows}}
-        }
+    public void PositiveOverflow() {
+        int i = 2147483600;
+        i +=100; // Noncompliant {{There is a path on which this operation always overflows}}
+    }
+    public void NegativeOverflow() {
+        int i = -2147483600;
+        i -=100; // Noncompliant {{There is a path on which this operation always overflows}}
     }
 }
