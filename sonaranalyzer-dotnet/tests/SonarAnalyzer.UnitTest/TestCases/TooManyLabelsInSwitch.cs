d295cfd9e5341de6a7fdc642a42a90c39ec3505c
Costin Zaharia <costin.zaharia@sonarsource.com>
2019-12-18 09:38:38 +0100

    Add test with switch expression S1479

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TooManyLabelsInSwitch.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TooManyLabelsInSwitch.cs
@@ -55,5 +55,22 @@
                     break;
             }
         }
+
+        public int Test(string type)
+        {
+            return type switch // Compliant - FN
+            {
+                "a" => 1,
+                "b" => 2,
+                "c" => 3,
+                "d" => 4,
+                "e" => 5,
+                "f" => 6,
+                "g" => 7,
+                "h" => 8,
+                "i" => 9,
+                _ => 10
+            };
+        }
     }
 }
