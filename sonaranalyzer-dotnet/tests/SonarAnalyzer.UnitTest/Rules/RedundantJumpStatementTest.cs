f617a8bc49c34bb7fd6d167a15e211cdd652e452
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2019-12-18 17:14:32 +0100

    Rule S3626: Add C# 8 unit tests for Non-SE CFG rule RedundantJumpStatement (#2915)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/RedundantJumpStatementTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/RedundantJumpStatementTest.cs
@@ -21,6 +21,7 @@
 extern alias csharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using csharp::SonarAnalyzer.Rules.CSharp;
+using SonarAnalyzer.UnitTest.TestFramework;
 
 namespace SonarAnalyzer.UnitTest.Rules
 {
@@ -31,7 +32,10 @@ namespace SonarAnalyzer.UnitTest.Rules
         [TestCategory("Rule")]
         public void RedundantJumpStatement()
         {
-            Verifier.VerifyAnalyzer(@"TestCases\RedundantJumpStatement.cs", new RedundantJumpStatement());
+            Verifier.VerifyAnalyzer(@"TestCases\RedundantJumpStatement.cs",
+                new RedundantJumpStatement(),
+                ParseOptionsHelper.FromCSharp8,
+                additionalReferences: NuGetMetadataReference.NETStandardV2_1_0);
         }
     }
 }
