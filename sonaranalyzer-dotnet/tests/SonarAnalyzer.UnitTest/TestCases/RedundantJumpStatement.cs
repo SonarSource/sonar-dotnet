ce9185583669245d9d84c2be45b9ad0dcdd94600
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2019-12-18 17:14:32 +0100

    Rule S3626: Add C# 8 unit tests for Non-SE CFG rule RedundantJumpStatement (#2915)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantJumpStatement.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantJumpStatement.cs
@@ -168,5 +168,48 @@ namespace Tests.Diagnostics
                 Console.WriteLine();
             }
         }
+
+        void CSharp8_StaticLocalFunctions()
+        {
+            static void Compute(int a, out int b)
+            {
+                b = a;
+                return;     // Noncompliant
+            }
+            static void EnsurePositive(int a, out int b)
+            {
+                b = 0;
+                if (a <= 0)
+                {
+                    return;
+                }
+                b = a;
+            }
+
+        }
+    }
+
+    public interface IWithDefaultImplementation
+    {
+        decimal Count { get; set; }
+        decimal Price { get; set; }
+
+        //Default interface methods
+        void Reset()
+        {
+            Price = 0;
+            Count = 0;
+            return;     // Noncompliant
+        }
+
+        void ResetIfZero()
+        {
+            if (Count == 0)
+            {
+                return;
+            }
+            Price = 0;
+        }
+
     }
 }
