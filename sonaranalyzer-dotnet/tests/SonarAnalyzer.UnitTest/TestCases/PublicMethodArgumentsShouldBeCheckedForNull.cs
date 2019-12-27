22c4f54febb64cf582ef2e4fc667b35cb8abd671
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2019-12-27 15:34:25 +0100

    Rule S3900: Update SE to the new CFG and add C# 8 unit tests (#2925)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PublicMethodArgumentsShouldBeCheckedForNull.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PublicMethodArgumentsShouldBeCheckedForNull.cs
@@ -28,7 +28,12 @@ namespace Tests.Diagnostics
             o.ToString(); // Compliant, not public
         }
 
-        private void CompliantCases_Internal(object o)
+        protected internal void NotCompliantCases_ProtectedInternal(object o)
+        {
+            o.ToString(); // Noncompliant
+        }
+
+        internal void CompliantCases_Internal(object o)
         {
             o.ToString(); // Compliant, not public
         }
@@ -307,6 +312,220 @@ namespace Tests.Diagnostics
                 // do stuff
             }
             return equals;
+        }
+    }
+
+    public class ReproWithIsNullOrEmpty
+    {
+        public static string SanitiseIMDbId(string imdbId)
+        {
+            if (string.IsNullOrEmpty(imdbId))
+                return string.Empty;
+
+            if (imdbId.StartsWith(" "))
+                imdbId = string.Concat("tt", imdbId);
+
+            if (imdbId.Length != 9) // Noncompliant - FP
+                imdbId = string.Empty;
+
+            return imdbId;
+        }
+    }
+}
+
+namespace CSharp8
+{
+    public class NullCoalescenceAssignment
+    {
+        public void NullCoalescenceAssignment_NotNull(string s1)
+        {
+            s1 ??= "N/A";
+            s1.ToString(); // Compliant
+        }
+
+        public void NullCoalescenceAssignment_Null(string s1)
+        {
+            s1 ??= null;
+            s1.ToString(); // Noncompliant
+        }
+
+        public void InsideIf(string str)
+        {
+            if (str != null)
+            {
+                str ??= null;
+                str.ToString(); // Compliant, we know str is not null
+            }
+
+            if (str == null)
+            {
+                str ??= "foo";
+                str.ToString(); // Compliant, assigned foo
+            }
+        }
+    }
+
+    public interface IWithDefaultMembers
+    {
+        decimal Count { get; set; }
+        decimal Price { get; set; }
+
+        void Reset(string s)
+        {
+            s.ToString(); // Noncompliant
+        }
+    }
+
+    public class LocalStaticFunctions
+    {
+        public void Method(object arg)
+        {
+            string LocalFunction(object o)
+            {
+                return o.ToString(); // Compliant - FN: local functions are not supported by the CFG
+            }
+
+            static string LocalStaticFunction(object o)
+            {
+                return o.ToString(); // Compliant - FN: local functions are not supported by the CFG
+            }
+        }
+    }
+
+    public class Address
+    {
+        public string Name { get; }
+
+        public string State { get; }
+
+        public void Deconstruct(out string name, out string state) =>
+            (name, state) = (Name, State);
+    }
+
+    public class Person
+    {
+        public string Name { get; }
+
+        public Address Address { get; }
+
+        public void Deconstruct(out string name, out Address address) =>
+            (name, address) = (Name, Address);
+    }
+
+    public class SwitchExpressions
+    {
+        public void OnlyDiscardBranch_Noncompliant(string s, bool b)
+        {
+            var result = b switch
+            {
+                _ => s.ToString() // Noncompliant
+            };
+        }
+
+        public void MultipleBranches_Noncompliant(string s, int val)
+        {
+            var result = val switch
+            {
+                1 => "a",
+                2 => s.ToString(), // Noncompliant
+                _ => "b"
+            };
+        }
+
+        public void Nested_Noncompliant(string s, int val, bool condition)
+        {
+            var result = val switch
+            {
+                1 => "a",
+                2 => condition switch
+                {
+                    _ => s.ToString() // Noncompliant
+                },
+                _ => "b"
+            };
+        }
+
+        public void MultipleBranches_HandleNull(string s, int val)
+        {
+            var result = s switch
+            {
+                null => s.ToString(), // Noncompliant
+                _ => s.ToString() // Compliant as null was already handled
+            };
+        }
+
+        public void MultipleBranches_Compliant(string s, int val)
+        {
+            var result = val switch
+            {
+                1 => "a",
+                2 => s == null ? string.Empty : s.ToString(),
+                _ => "b"
+            };
+        }
+
+        public string MultipleBranches_PropertyPattern(Address address, string s)
+        {
+            return address switch
+            {
+                {State: "WA" } addr => s.ToString(), // Noncompliant
+                _ => string.Empty
+            };
+        }
+
+        public string MultipleBranches_PropertyPattern_FP(string s)
+        {
+            return s switch
+            {
+                { Length: 5 } => s.ToString(), // Noncompliant - FP we know that the length is 5 so the string cannot be null
+                _ => string.Empty
+            };
+        }
+
+        public string MultipleBranches_RecursivePattern(Person person, string s)
+        {
+            return person switch
+            {
+                { Address: {State: "WA" } } pers => s.ToString(), // Noncompliant
+                _ => string.Empty
+            };
+        }
+
+        public string MultipleBranches_TuplePattern(Address address, string s)
+         {
+             return address switch
+             {
+                 var (name, state) => s.ToString(), // Compliant - FN
+                 _ => string.Empty
+             };
+         }
+
+        public string MultipleBranches_WhenClause(Address address, string s)
+        {
+            return address switch
+            {
+                Address addr when addr.Name.Length > 0 => s.ToString(), // Noncompliant
+                Address addr when addr.Name.Length == s.Length => string.Empty, // Noncompliant
+                _ => string.Empty
+            };
+        }
+
+        public string MultipleBranches_VarDeclaration(Address address, string s)
+        {
+            return address switch
+            {
+                Address addr => s.ToString(), // Noncompliant
+                _ => string.Empty
+            };
+        }
+
+        public string TwoBranches_NoDefault(bool condition, string s)
+        {
+            return condition switch
+            {
+                true => s.ToString(), // Noncompliant
+                false => s.ToString() // Noncompliant
+            };
         }
     }
 }
