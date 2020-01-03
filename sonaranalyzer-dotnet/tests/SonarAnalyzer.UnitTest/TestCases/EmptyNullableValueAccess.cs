ee35e6ad4fed448df7c08efe0152547900102a32
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-01-03 10:48:44 +0100

    More switch expressions tests

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EmptyNullableValueAccess.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EmptyNullableValueAccess.cs
@@ -129,6 +129,21 @@ namespace Tests.Diagnostics
             return i.Value;
         }
 
+        public int CSharp8_SwitchExpressions3(int? value)
+        {
+            return value.HasValue switch { true => value.Value, false => 0};
+        }
+
+        public int CSharp8_SwitchExpressions4(int? value)
+        {
+            return value.HasValue switch { true => 0, false => value.Value };   // FN - switch expressions are not constrained
+        }
+
+        public int CSharp8_SwitchExpressions5(int? value, bool flag)
+        {
+            return flag switch { true => value.Value, false => 0 }; // FN - switch expressions are not constrained
+        }
+
         public int CSharp8_StaticLocalFunctions(int? param)
         {
             static int ExtractValue(int? intOrNull)
