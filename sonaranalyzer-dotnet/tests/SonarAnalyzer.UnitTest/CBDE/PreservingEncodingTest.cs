d7b4e2f037c78fe6b6121499cfb65c314b779ff1
Loïc Joly <loic.joly@sonarsource.com>
2020-01-08 16:22:47 +0100

    Add unit tests for PreservingEncodingBuffer/PreservingEncodingFallback

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/CBDE/PreservingEncodingTest.cs
@@ -0,0 +1,45 @@
+﻿extern alias csharp;
+
+using System;
+using System.Text;
+using csharp::SonarAnalyzer.CBDE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SonarAnalyzer.UnitTest.CBDE
+{
+    [TestClass]
+    public class PreservingEncodingTest
+    {
+        private readonly Encoding encoder = System.Text.Encoding.GetEncoding("ASCII", new PreservingEncodingFallback(), DecoderFallback.ExceptionFallback);
+
+        public string Encode(string name)
+        {
+            Byte[] encodedBytes = encoder.GetBytes(name);
+            return encoder.GetString(encodedBytes);
+
+        }
+
+        public void checkEncoding(string source, string encoded)
+        {
+            Assert.AreEqual(encoded, Encode(source));
+        }
+        [TestMethod]
+        public void SimpleCharsAreUnchanged()
+        {
+            checkEncoding("ABCDEF abcdef", "ABCDEF abcdef");
+        }
+
+        [TestMethod]
+        public void AccentedCharacterAreChanged()
+        {
+            checkEncoding("àÅéÈïİøÒùÛçµ", ".E0.C5.E9.C8.EF.130.F8.D2.F9.DB.E7.B5");
+        }
+
+        [TestMethod]
+        public void CharactersWithLongEncodingAreChanged()
+        {
+            checkEncoding("𤭢𐐷", ".D852DF62.D801DC37");
+        }
+
+    }
+}
