a9feb3578fcb0be41217f06afdd7b4ead5bd2053
Loïc Joly <loic.joly@sonarsource.com>
2020-01-08 17:04:30 +0100

    Apply review comments

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/CBDE/PreservingEncodingTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/CBDE/PreservingEncodingTest.cs
@@ -1,4 +1,24 @@
-﻿extern alias csharp;
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2019 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+ extern alias csharp;
 
 using System;
 using System.Text;
@@ -16,13 +36,13 @@ namespace SonarAnalyzer.UnitTest.CBDE
         {
             Byte[] encodedBytes = encoder.GetBytes(name);
             return encoder.GetString(encodedBytes);
-
         }
 
         public void checkEncoding(string source, string encoded)
         {
             Assert.AreEqual(encoded, Encode(source));
         }
+
         [TestMethod]
         public void SimpleCharsAreUnchanged()
         {
@@ -39,6 +59,13 @@ namespace SonarAnalyzer.UnitTest.CBDE
         public void CharactersWithLongEncodingAreChanged()
         {
             checkEncoding("𤭢𐐷", ".D852DF62.D801DC37");
+        }
+
+        [TestMethod]
+        public void StringWithPointsAreNoLongerInjective()
+        {
+            checkEncoding(".E0", ".E0"); // If the input string contains a point, we are no longer injective
+            checkEncoding("à", ".E0");
         }
 
     }
