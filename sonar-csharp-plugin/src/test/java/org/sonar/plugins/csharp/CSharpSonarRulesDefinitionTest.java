1b9669dcb767eff1b05f3905ca0c8586afa600a0
Andrei Epure <andrei.epure@sonarsource.com>
2020-01-09 10:47:50 +0100

    Update license headers for java code

--- sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarRulesDefinitionTest.java
+++ sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarRulesDefinitionTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarC#
- * Copyright (C) 2014-2019 SonarSource SA
+ * Copyright (C) 2014-2020 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
