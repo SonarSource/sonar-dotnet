c68bf537ee16df5073b0b30c0f70779747248770
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-01-10 14:12:30 +0100

    Other files

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/FileLines9.vb
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/FileLines9.vb
@@ -1,22 +1,4 @@
-﻿'SonarAnalyzer for .NET
-'Copyright(C) 2015-2019 SonarSource SA
-'mailto: contact AT sonarsource DOT com
-
-'This program Is free software; you can redistribute it And/Or
-'modify it under the terms Of the GNU Lesser General Public
-'License as published by the Free Software Foundation; either
-'version 3 Of the License, Or (at your option) any later version.
-
-'This program Is distributed In the hope that it will be useful,
-'but WITHOUT ANY WARRANTY; without even the implied warranty Of
-'MERCHANTABILITY Or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-'Lesser General Public License For more details.
-
-'You should have received a copy Of the GNU Lesser General Public License
-'along with this program; if Not, write To the Free Software Foundation,
-'Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-Namespace Tests.Diagnostics
+﻿Namespace Tests.Diagnostics
     Class Foo
         Sub New() ' TEST
             ' HELLO
