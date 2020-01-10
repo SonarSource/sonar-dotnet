ff9c6eef3c738468bab43b6e7dbaab94b25a760c
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-01-10 14:03:19 +0100

    2020 *.cs

--- sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/DiagnosticAnalyzerContextHelper.cs
+++ sonaranalyzer-dotnet/src/SonarAnalyzer.Common/Helpers/DiagnosticAnalyzerContextHelper.cs
@@ -1,6 +1,6 @@
 /*
  * SonarAnalyzer for .NET
- * Copyright (C) 2015-2019 SonarSource SA
+ * Copyright (C) 2015-2020 SonarSource SA
  * mailto:contact@sonarsource.com
  *
  * This program is free software; you can redistribute it and/or
