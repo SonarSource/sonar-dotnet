484769842fd36b2057dcf4be9601a4fecf404c4a
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2020-01-14 11:39:16 +0100

    S4022 FP reproduction of issue 3026 (#3030)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EnumStorageNeedsToBeInt32.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EnumStorageNeedsToBeInt32.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace MyLibrary
 {
@@ -15,4 +15,33 @@ namespace MyLibrary
 
     public enum Test_04 : long { } // Noncompliant
     public enum Test_05 : ulong { } // Noncompliant
+
+}
+
+// https://github.com/SonarSource/sonar-dotnet/issues/3026
+namespace Issue_3026
+{
+    public enum Long : long // Noncompliant FP, it has to be long due to enum values
+    {
+        Visible = 0,
+        Invisible = 3147483647,
+    }
+
+    public enum LongWithNegative : long // Noncompliant FP, it has to be long due to enum values
+    {
+        Visible = 0,
+        Invisible = Int64.MinValue,
+    }
+
+    public enum ULong : ulong // Noncompliant FP, it has to be ulong due to enum values
+    {
+        Visible = 0,
+        Invisible = 18446744073709551615,
+    }
+
+    public enum UInt : uint // Noncompliant FP, it has to be uint due to enum values
+    {
+        Visible = 0,
+        Invisible = 3147483647,
+    }
 }
