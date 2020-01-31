c646b2cbcbe3a6f47ec8d757366b9023c2dd3aa4
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2020-01-31 09:05:53 +0100

    Add reproducer for S3241 'AD0001' error (#3087)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnusedReturnValue.part1.cs
@@ -0,0 +1,15 @@
+﻿namespace SonarCheck
+{
+    public partial class Partial
+    {
+        private int GetValue1()
+        {
+            return 1;
+        }
+
+        private int GetValue2() // Noncompliant
+        {
+            return 1;
+        }
+    }
+}
