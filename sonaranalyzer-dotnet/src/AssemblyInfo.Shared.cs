634eb96e975c417bc914472bf0639757d84fc6d7
Andrei Epure <andrei.epure@sonarsource.com>
2020-02-21 13:47:45 +0100

    Bump version to 8.5
    Prepare the next development iteration

--- sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
+++ sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
@@ -23,10 +23,10 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("8.4.0")]
-[assembly: AssemblyFileVersion("8.4.0.0")]
+[assembly: AssemblyVersion("8.5.0")]
+[assembly: AssemblyFileVersion("8.5.0.0")]
 // The value should look like "Version:X.X.X.X Branch:not-set Sha1:not-set"
-[assembly: AssemblyInformationalVersion("Version:8.4.0.0 Branch: Sha1:")]
+[assembly: AssemblyInformationalVersion("Version:8.5.0.0 Branch: Sha1:")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyCopyright("Copyright © 2015-2020 SonarSource SA")]
