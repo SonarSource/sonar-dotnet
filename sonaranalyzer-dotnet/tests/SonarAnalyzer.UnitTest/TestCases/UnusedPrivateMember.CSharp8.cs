4223b1dcbef03e4ece96c0dee80c7a9c2a3fff2f
Christophe Zürn <36889251+christophe-zurn-sonarsource@users.noreply.github.com>
2020-03-03 13:32:17 +0100

    UnusedPrivateMember throws 'NullReferenceException' in SonarLint (#3171)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp8.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests.Diagnostics
+{
+    public interface MyInterface1
+    {
+        public void Method1() { }
+    }
+
+    public class Class1
+    {
+        private interface MyInterface2 // Noncompliant
+        {
+            public void Method1() { }
+        }
+    }
+}
