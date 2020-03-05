4011e689224a26313a12e2087d3a6cd52ff1ff01
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-03-05 09:01:29 +0100

    Sort members

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/NuGetMetadataReference.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/NuGetMetadataReference.cs
@@ -56,9 +56,6 @@ namespace SonarAnalyzer.UnitTest
         public static IEnumerable<MetadataReference> FluentAssertions(string packageVersion) =>
             NugetMetadataFactory.Create("FluentAssertions", packageVersion);
 
-        public static IEnumerable<MetadataReference> NSubstitute(string packageVersion) =>
-            NugetMetadataFactory.Create("NSubstitute", packageVersion);
-
         public static IEnumerable<MetadataReference> Log4Net(string packageVersion, string targetFramework) =>
             NugetMetadataFactory.Create("log4net", packageVersion, targetFramework);
 
@@ -162,6 +159,9 @@ namespace SonarAnalyzer.UnitTest
 
         public static IEnumerable<MetadataReference> NHibernate(string packageVersion = "5.2.2") =>
             NugetMetadataFactory.Create("NHibernate", packageVersion);
+
+        public static IEnumerable<MetadataReference> NSubstitute(string packageVersion) =>
+            NugetMetadataFactory.Create("NSubstitute", packageVersion);
 
         public static IEnumerable<MetadataReference> NUnit(string packageVersion) =>
             NugetMetadataFactory.Create("NUnit", packageVersion);
