271d95c444525a24a54fa81eeecb99f634345432
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-03-05 14:38:43 +0100

    More tests

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using JWT;
 using JWT.Algorithms;
 using JWT.Builder;
@@ -10,14 +11,12 @@ namespace Tests.Diagnostics
         const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
         const string invalidToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJmYWtlYmFyIiwiaWF0IjoxNTc1NjQ0NTc3fQ.pcX_7snpSGf01uBfaM8XPkbgdhs1gq9JcYRCQvZrJyk";
 
-        private IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+        private JwtParts invalidParts;
 
         // Encoding with JWT.NET is safe
 
-        void DecodingWithDecoder()
+        void DecodingWithDecoder(JwtDecoder decoder)
         {
-            var decoder = new JwtDecoder(null, null, null, algorithm);
-
             var decoded1 = decoder.Decode(invalidToken, secret, true);// Compliant
             var decoded2 = decoder.Decode(invalidToken, secret, false); // Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
 
@@ -34,6 +33,40 @@ namespace Tests.Diagnostics
             var decoded10 = decoder.Decode(invalidToken, verify: false, key: new byte[] { 42 }); // Noncompliant
 
             var decoded11 = decoder.Decode(invalidToken); // Noncompliant
+            var decoded12 = decoder.Decode(invalidParts); // Noncompliant
+
+            var decoded21 = decoder.DecodeToObject(invalidToken, secret, true); // Compliant
+            var decoded22 = decoder.DecodeToObject(invalidToken, secret, false); // Noncompliant
+
+            var decoded31 = decoder.DecodeToObject<UserInfo>(invalidToken, secret, true); // Compliant
+            var decoded32 = decoder.DecodeToObject<UserInfo>(invalidToken, secret, false); // Noncompliant
+        }
+
+        void DecodingWithCustomDecoder(CustomDecoder decoder)
+        {
+            var decoded1 = decoder.Decode(invalidToken, secret, true);// Compliant
+            var decoded2 = decoder.Decode(invalidToken, secret, false); // Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
+
+            var decoded3 = decoder.Decode(invalidToken, secret, verify: true);// Compliant
+            var decoded4 = decoder.Decode(invalidToken, secret, verify: false); // Noncompliant
+
+            var decoded5 = decoder.Decode(invalidToken, secret, verify: true);// Compliant
+            var decoded6 = decoder.Decode(invalidToken, secret, verify: false); // Noncompliant
+
+            var decoded7 = decoder.Decode(invalidToken, verify: true, key: secret);// Compliant
+            var decoded8 = decoder.Decode(invalidToken, verify: false, key: secret); // Noncompliant
+
+            var decoded9 = decoder.Decode(invalidToken, verify: true, key: new byte[] { 42 });// Compliant
+            var decoded10 = decoder.Decode(invalidToken, verify: false, key: new byte[] { 42 }); // Noncompliant
+
+            var decoded11 = decoder.Decode(invalidToken); // Noncompliant
+            var decoded12 = decoder.Decode(invalidParts); // Noncompliant
+
+            var decoded21 = decoder.DecodeToObject(invalidToken, secret, true); // Compliant
+            var decoded22 = decoder.DecodeToObject(invalidToken, secret, false); // Noncompliant
+
+            var decoded31 = decoder.DecodeToObject<UserInfo>(invalidToken, secret, true); // Compliant
+            var decoded32 = decoder.DecodeToObject<UserInfo>(invalidToken, secret, false); // Noncompliant
         }
 
         void DecodingWithBuilder()
@@ -55,6 +88,122 @@ namespace Tests.Diagnostics
 
             var builder3 = new JwtBuilder().WithSecret(secret).MustVerifySignature();
             builder3.Decode(invalidToken); // Compliant
+
+            var decoded11 = new JwtBuilder()
+                .WithSecret(secret)
+                .WithVerifySignature(true)
+                .MustVerifySignature()
+                .DoNotVerifySignature()
+                .Decode(invalidToken); // Noncompliant
+
+            var Decoded12 = new JwtBuilder()
+                .WithSecret(secret)
+                .WithVerifySignature(false)
+                .DoNotVerifySignature()
+                .MustVerifySignature()
+                .Decode(invalidToken); // Compliant
+
+            var Decoded21 = new JwtBuilder()
+                .WithSecret(secret)
+                .DoNotVerifySignature()
+                .WithVerifySignature(false)
+                .WithVerifySignature(true)
+                .Decode(invalidToken); // Compliant
+
+            var Decoded31 = new JwtBuilder()
+                .WithSecret(secret)
+                .MustVerifySignature()
+                .WithVerifySignature(true)
+                .WithVerifySignature(false)
+                .Decode(invalidToken); // Noncompliant
+        }
+    }
+
+    class UserInfo
+    {
+        string Name { get; set; }
+    }
+
+    class CustomDecoder : IJwtDecoder
+    {
+        public string Decode(JwtParts jwt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string token, string key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string token, string[] keys, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string token, byte[] key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decode(string token, byte[][] keys, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, object> DecodeToObject(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, object> DecodeToObject(string token, string key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, object> DecodeToObject(string token, string[] keys, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, object> DecodeToObject(string token, byte[] key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionary<string, object> DecodeToObject(string token, byte[][] keys, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DecodeToObject<T>(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DecodeToObject<T>(string token, string key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DecodeToObject<T>(string token, string[] keys, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DecodeToObject<T>(string token, byte[] key, bool verify)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DecodeToObject<T>(string token, byte[][] keys, bool verify)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.vb
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.vb
@@ -2,6 +2,7 @@
 Imports JWT
 Imports JWT.Algorithms
 Imports JWT.Builder
+Imports System.Collections.Generic
 
 Namespace Tests.Diagnostics
 
@@ -10,37 +11,68 @@ Namespace Tests.Diagnostics
         Protected Const Secret As String = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk"
         Protected Const InvalidToken As String = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJmYWtlYmFyIiwiaWF0IjoxNTc1NjQ0NTc3fQ.pcX_7snpSGf01uBfaM8XPkbgdhs1gq9JcYRCQvZrJyk"
 
-        Private Algorithm As New HMACSHA256Algorithm()
+        Private InvalidParts As JwtParts
 
         ' Encoding with JWT.NET Is safe
 
-        Sub DecodingWithDecoder()
-            Dim Decoder As New JwtDecoder(Nothing, Nothing, Nothing, Algorithm)
-
+        Sub DecodingWithDecoder(Decoder As JwtDecoder)
             Dim Decoded1 As String = Decoder.Decode(InvalidToken, Secret, True) ' Compliant
-            Dim decoded2 As String = Decoder.Decode(InvalidToken, Secret, False) ' Noncompliant {{Use only strong cipher algorithms When verifying the signature Of this JWT.}}
+            Dim Decoded2 As String = Decoder.Decode(InvalidToken, Secret, False) ' Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
 
-            Dim decoded3 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
-            Dim decoded4 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
+            Dim Decoded3 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
+            Dim Decoded4 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
 
-            Dim decoded5 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
-            Dim decoded6 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
+            Dim Decoded5 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
+            Dim Decoded6 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
 
-            Dim decoded7 As String = Decoder.Decode(InvalidToken, verify:=True, key:=Secret) ' Compliant
-            Dim decoded8 As String = Decoder.Decode(InvalidToken, verify:=False, key:=Secret) ' Noncompliant
+            Dim Decoded7 As String = Decoder.Decode(InvalidToken, verify:=True, key:=Secret) ' Compliant
+            Dim Decoded8 As String = Decoder.Decode(InvalidToken, verify:=False, key:=Secret) ' Noncompliant
 
-            Dim decoded9 As String = Decoder.Decode(InvalidToken, verify:=True, key:={42}) ' Compliant
-            Dim decoded10 As String = Decoder.Decode(InvalidToken, verify:=False, key:={42}) ' Noncompliant
+            Dim Decoded9 As String = Decoder.Decode(InvalidToken, verify:=True, key:={42}) ' Compliant
+            Dim Decoded10 As String = Decoder.Decode(InvalidToken, verify:=False, key:={42}) ' Noncompliant
 
-            Dim decoded11 As String = Decoder.Decode(InvalidToken) ' Noncompliant
+            Dim Decoded11 As String = Decoder.Decode(InvalidToken) ' Noncompliant
+            Dim Decoded12 As String = Decoder.Decode(InvalidParts) ' Noncompliant
+
+            Dim Decoded21 As Object = Decoder.DecodeToObject(InvalidToken, Secret, True) ' Compliant
+            Dim Decoded22 As Object = Decoder.DecodeToObject(InvalidToken, Secret, False) ' Noncompliant
+
+            Dim Decoded31 As UserInfo = Decoder.DecodeToObject(Of UserInfo)(InvalidToken, Secret, True) ' Compliant
+            Dim Decoded32 As UserInfo = Decoder.DecodeToObject(Of UserInfo)(InvalidToken, Secret, False) ' Noncompliant
+        End Sub
+
+        Sub DecodingWithCustomDecoder(Decoder As CustomDecoder)
+            Dim Decoded1 As String = Decoder.Decode(InvalidToken, Secret, True) ' Compliant
+            Dim Decoded2 As String = Decoder.Decode(InvalidToken, Secret, False) ' Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
+
+            Dim Decoded3 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
+            Dim Decoded4 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
+
+            Dim Decoded5 As String = Decoder.Decode(InvalidToken, Secret, verify:=True) ' Compliant
+            Dim Decoded6 As String = Decoder.Decode(InvalidToken, Secret, verify:=False) ' Noncompliant
+
+            Dim Decoded7 As String = Decoder.Decode(InvalidToken, verify:=True, key:=Secret) ' Compliant
+            Dim Decoded8 As String = Decoder.Decode(InvalidToken, verify:=False, key:=Secret) ' Noncompliant
+
+            Dim Decoded9 As String = Decoder.Decode(InvalidToken, verify:=True, key:={42}) ' Compliant
+            Dim Decoded10 As String = Decoder.Decode(InvalidToken, verify:=False, key:={42}) ' Noncompliant
+
+            Dim Decoded11 As String = Decoder.Decode(InvalidToken) ' Noncompliant
+            Dim Decoded12 As String = Decoder.Decode(InvalidParts) ' Noncompliant
+
+            Dim Decoded21 As Object = Decoder.DecodeToObject(InvalidToken, Secret, True) ' Compliant
+            Dim Decoded22 As Object = Decoder.DecodeToObject(InvalidToken, Secret, False) ' Noncompliant
+
+            Dim Decoded31 As UserInfo = Decoder.DecodeToObject(Of UserInfo)(InvalidToken, Secret, True) ' Compliant
+            Dim Decoded32 As UserInfo = Decoder.DecodeToObject(Of UserInfo)(InvalidToken, Secret, False) ' Noncompliant
         End Sub
 
         Sub DecodingWithBuilder()
-            Dim decoded1 As String = New JwtBuilder().
+            Dim Decoded1 As String = New JwtBuilder().
                 WithSecret(Secret).
-                Decode(InvalidToken) ' Noncompliant {{Use only strong cipher algorithms When verifying the signature Of this JWT.}}
+                Decode(InvalidToken) ' Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
 
-            Dim decoded2 As String = New JwtBuilder().
+            Dim Decoded2 As String = New JwtBuilder().
                 WithSecret(Secret).
                 MustVerifySignature().
                 Decode(InvalidToken) ' Compliant
@@ -53,7 +85,94 @@ Namespace Tests.Diagnostics
 
             Dim builder3 As JwtBuilder = New JwtBuilder().WithSecret(Secret).MustVerifySignature()
             builder3.Decode(InvalidToken) ' Compliant
+
+            Dim Decoded11 As String = New JwtBuilder().
+                WithSecret(Secret).
+                WithVerifySignature(True).
+                MustVerifySignature().
+                DoNotVerifySignature().
+                Decode(InvalidToken) ' Noncompliant
+
+            Dim Decoded12 As String = New JwtBuilder().
+                WithSecret(Secret).
+                WithVerifySignature(False).
+                DoNotVerifySignature().
+                MustVerifySignature().
+                Decode(InvalidToken) ' Compliant
+
+            Dim Decoded21 As String = New JwtBuilder().
+                WithSecret(Secret).
+                DoNotVerifySignature().
+                WithVerifySignature(False).
+                WithVerifySignature(True).
+                Decode(InvalidToken) ' Compliant
+
+            Dim Decoded31 As String = New JwtBuilder().
+                WithSecret(Secret).
+                MustVerifySignature().
+                WithVerifySignature(True).
+                WithVerifySignature(False).
+                Decode(InvalidToken) ' Noncompliant
         End Sub
+
+    End Class
+
+    Public Class UserInfo
+
+        Public Property Name As String
+
+    End Class
+
+    Public Class CustomDecoder
+        Implements IJwtDecoder
+
+        Public Function Decode(jwt As JwtParts) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function Decode(token As String) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function Decode(token As String, key As String, verify As Boolean) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function Decode(token As String, keys() As String, verify As Boolean) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function Decode(token As String, key() As Byte, verify As Boolean) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function Decode(token As String, keys As Byte()(), verify As Boolean) As String Implements IJwtDecoder.Decode
+        End Function
+
+        Public Function DecodeToObject(token As String) As IDictionary(Of String, Object) Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(token As String, key As String, verify As Boolean) As IDictionary(Of String, Object) Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(token As String, keys() As String, verify As Boolean) As IDictionary(Of String, Object) Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(token As String, key() As Byte, verify As Boolean) As IDictionary(Of String, Object) Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(token As String, keys As Byte()(), verify As Boolean) As IDictionary(Of String, Object) Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(Of T)(token As String) As T Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(Of T)(token As String, key As String, verify As Boolean) As T Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(Of T)(token As String, keys() As String, verify As Boolean) As T Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(Of T)(token As String, key() As Byte, verify As Boolean) As T Implements IJwtDecoder.DecodeToObject
+        End Function
+
+        Public Function DecodeToObject(Of T)(token As String, keys As Byte()(), verify As Boolean) As T Implements IJwtDecoder.DecodeToObject
+        End Function
 
     End Class
 
