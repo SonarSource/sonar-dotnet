cb3e9f850671eb224c43ce3980c101c6a3ee64cc
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-03-10 11:33:08 +0100

    Add integration tests

---
+++ sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssues/NetFramework35/XPathDocumentTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+using System.Xml.XPath;
+
+namespace NetFramework35
+{
+    public class XPathDocumentTest
+    {
+        public void UnsafeByDefault()
+        {
+            new XPathDocument("doc.xml"); // Noncompliant (S2755) {{Disable access to external entities in XML parsing.}}
+        }
+
+        public void UnsafeXmlReader()
+        {
+            var xmlReader = XmlReader.Create("uri", new XmlReaderSettings {ProhibitDtd = false}); // Noncompliant
+            new XPathDocument(xmlReader);
+        }
+
+        public void SafeXmlReader()
+        {
+            var xmlReader = XmlReader.Create("uri", new XmlReaderSettings {ProhibitDtd = true});
+            new XPathDocument(xmlReader);
+        }
+
+        public void XmlReaderAsParameter(XmlReader xmlReader)
+        {
+            new XPathDocument(xmlReader);
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/its/sources/ManuallyAddedNoncompliantIssues/NetFramework45/XPathDocumentTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml;
+using System.Xml.XPath;
+
+namespace NetFramework45
+{
+    public class XPathDocumentTest
+    {
+        public void UnsafeByDefault()
+        {
+            _ = new XPathDocument("doc.xml"); // Noncompliant (S2755) {{Disable access to external entities in XML parsing.}}
+        }
+
+        public void UnsafeXmlReader()
+        {
+            var xmlReader = XmlReader.Create("uri", new XmlReaderSettings {DtdProcessing = DtdProcessing.Parse}); // Noncompliant
+            _ = new XPathDocument(xmlReader);
+        }
+
+        public void SafeXmlReader()
+        {
+            var xmlReader = XmlReader.Create("uri", new XmlReaderSettings {DtdProcessing = DtdProcessing.Ignore});
+            _ = new XPathDocument(xmlReader);
+        }
+
+        public void XmlReaderAsParameter(XmlReader xmlReader)
+        {
+            _ = new XPathDocument(xmlReader);
+        }
+    }
+}
