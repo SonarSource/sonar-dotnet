6f29504d27fe47c33545d452ae2686aef17db4df
Christophe Zurn <christophe.zurn@sonarsource.com>
2020-03-24 16:53:35 +0100

    Add repro for bug: S2755 crashes on direct external parameter use

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/XmlExternalEntityShouldNotBeParsed_XmlReader_ExternalParameter.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+namespace SonarAnalyzer.UnitTest.TestCases
+{
+    public class XmlReaderExternalParameter
+    {
+        public void XmlReader_ExternalParameter()
+        {
+            XmlReader.Create("uri", XmlReaderParameterProvider.Settings); // Ok - semantic model cannot resolve symbol info
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/XmlExternalEntityShouldNotBeParsed_XmlReader_ParameterProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Xml;
+
+namespace SonarAnalyzer.UnitTest.TestCases
+{
+    public class XmlReaderParameterProvider
+    {
+        internal static readonly XmlReaderSettings Settings = new XmlReaderSettings
+        {
+            Async = false,
+        };
+    }
+}
