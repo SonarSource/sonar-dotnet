d5a29440250ca7c999a4d369e8a97f360dd71714
Andrei Epure <andrei.epure@sonarsource.com>
2020-03-31 17:01:39 +0200

    Add repro

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantCast.CSharp8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Diagnostics
+{
+    public class CastOnNullable
+    {
+        public static IEnumerable<string> UsefulCast()
+        {
+            var nullableStrings = new string?[] { "one", "two", null, "three" };
+            return nullableStrings.OfType<string>(); // Noncompliant FP - filters out the null
+        }
+     }
+}
