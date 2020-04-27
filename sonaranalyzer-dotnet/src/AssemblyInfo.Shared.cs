4d242670a6809b5319503ef83d1a1ca83ccf9880
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-04-27 18:02:41 +0200

    Set version to 8.6.1

--- sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
+++ sonaranalyzer-dotnet/src/AssemblyInfo.Shared.cs
@@ -23,10 +23,10 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("8.7.0")]
-[assembly: AssemblyFileVersion("8.7.0.0")]
+[assembly: AssemblyVersion("8.6.1")]
+[assembly: AssemblyFileVersion("8.6.1.0")]
 // The value should look like "Version:X.X.X.X Branch:not-set Sha1:not-set"
-[assembly: AssemblyInformationalVersion("Version:8.7.0.0 Branch: Sha1:")]
+[assembly: AssemblyInformationalVersion("Version:8.6.1.0 Branch: Sha1:")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyCopyright("Copyright © 2015-2020 SonarSource SA")]
