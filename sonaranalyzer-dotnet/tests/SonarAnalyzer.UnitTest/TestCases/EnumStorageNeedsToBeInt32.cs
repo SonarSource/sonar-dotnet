841d279aafaf5e88ba5e0f024548f199ce682aa5
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-05-07 17:09:45 +0200

    Allow also uint, long and ULong
    Fixes #3026

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EnumStorageNeedsToBeInt32.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/EnumStorageNeedsToBeInt32.cs
@@ -9,39 +9,14 @@ namespace MyLibrary
         Invisible = 1,
     }
 
-    public enum Test_01 { } // Compliant
-    public enum Test_02 : int { } // Compliant
-    public enum Test_03 : System.Int32 { } // Compliant
+    public enum Byte : byte { }  // Noncompliant
+    public enum Short : short { } // Noncompliant
+    public enum UShort : ushort { }  // Noncompliant
 
-    public enum Test_04 : long { } // Noncompliant
-    public enum Test_05 : ulong { } // Noncompliant
-
-}
-
-// https://github.com/SonarSource/sonar-dotnet/issues/3026
-namespace Issue_3026
-{
-    public enum Long : long // Noncompliant FP, it has to be long due to enum values
-    {
-        Visible = 0,
-        Invisible = 3147483647,
-    }
-
-    public enum LongWithNegative : long // Noncompliant FP, it has to be long due to enum values
-    {
-        Visible = 0,
-        Invisible = Int64.MinValue,
-    }
-
-    public enum ULong : ulong // Noncompliant FP, it has to be ulong due to enum values
-    {
-        Visible = 0,
-        Invisible = 18446744073709551615,
-    }
-
-    public enum UInt : uint // Noncompliant FP, it has to be uint due to enum values
-    {
-        Visible = 0,
-        Invisible = 3147483647,
-    }
+    public enum Default { } // Compliant
+    public enum IntAlias : int { } // Compliant
+    public enum Int : System.Int32 { } // Compliant
+    public enum Long : long { } // Compliant
+    public enum ULong : ulong { } // Compliant
+    public enum UInt : uint { }  // Compliant
 }
