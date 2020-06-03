44a8439ed810fef33d043d74c65ff63659e81c64
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-06-03 10:51:23 +0200

    Add additional cases

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecutionRules.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecutionRules.cs
@@ -8,10 +8,26 @@ namespace SonarAnalyzer.UnitTest.TestCases
         {
             public static void Main()
             {
-                using var resource = GetNullResource();
+                using var resource1 = GetNullResource();
 
-                if (resource == null)
-                { throw new Exception(); }
+                if (resource1 == null) // Compliant - "resource1" can be null
+                {
+                    throw new Exception();
+                }
+
+                var resource2 = GetNullResource();
+                if (resource2 == null) // Compliant - "resource2" can be null
+                {
+                    throw new Exception();
+                }
+
+                using (var resource3 = GetNullResource())
+                {
+                    if (resource3 == null) // Compliant - "resource3" can be null
+                    {
+                        throw new Exception();
+                    }
+                }
             }
 
             private static IDisposable GetNullResource() => null;
