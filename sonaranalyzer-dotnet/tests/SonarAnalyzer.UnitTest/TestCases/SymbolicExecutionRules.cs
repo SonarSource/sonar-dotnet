451aeaf8e2816a8dd76b6091a51f54f234329c67
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2020-06-03 12:59:41 +0200

    S2583: Fix FP when using the new "using statement" from C# 8 (#3375)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecutionRules.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SonarAnalyzer.UnitTest.TestCases
+{
+    public class SymbolicExecutionRules
+    {
+        internal static class Program
+        {
+            public static void Main()
+            {
+                using var resource1 = GetNullResource();
+
+                if (resource1 == null) // Compliant - "resource1" can be null
+                {
+                    throw new Exception();
+                }
+
+                var resource2 = GetNullResource();
+                if (resource2 == null) // Compliant - "resource2" can be null
+                {
+                    throw new Exception();
+                }
+
+                using (var resource3 = GetNullResource())
+                {
+                    if (resource3 == null) // Compliant - "resource3" can be null
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+
+            private static IDisposable GetNullResource() => null;
+        }
+    }
+}
