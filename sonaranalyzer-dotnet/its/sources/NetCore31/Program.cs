0240fe0dc2b4c6b10e39ee887d0e72112d448cc1
Andrei Epure <andrei.epure@sonarsource.com>
2020-07-13 17:12:39 +0200

    Add NetCore31 project

---
+++ sonaranalyzer-dotnet/its/sources/NetCore31/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NetCore31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (args[0] == null)
+            {
+                Console.WriteLine(args[0].ToString());
+            }
+        }
+    }
+}
