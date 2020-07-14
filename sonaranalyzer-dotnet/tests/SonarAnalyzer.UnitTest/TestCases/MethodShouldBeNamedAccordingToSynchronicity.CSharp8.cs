a9e7863e2b7a62c96919741112bdfa487185a0e1
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-07-14 09:19:23 +0200

    S4261: Add repro for 3433

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MethodShouldBeNamedAccordingToSynchronicity.CSharp8.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    public abstract class IEnumerableReproducer
+    {
+        protected abstract IAsyncEnumerable<int> GetFilesAsync(string folderName); // Noncompliant - FP, async method (https://github.com/SonarSource/sonar-dotnet/issues/3433)
+    }
+}
