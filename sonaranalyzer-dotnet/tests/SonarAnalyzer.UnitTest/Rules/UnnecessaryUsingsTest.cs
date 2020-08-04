07dba522d792abf660e5d4d924e489dd1b6bf8b5
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-08-04 11:52:22 +0200

    Move repro case back in separate file

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/UnnecessaryUsingsTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/UnnecessaryUsingsTest.cs
@@ -40,34 +40,9 @@ namespace SonarAnalyzer.UnitTest.Rules
 
         [TestMethod]
         [TestCategory("Rule")]
-        public void UnnecessaryUsings_TupleDeconstruct()
-        {
-            const string code = @"
-namespace Repro_3408_Consumer
-{
-    using System; // Noncompliant - FP, See: https://github.com/SonarSource/sonar-dotnet/issues/3408
-
-    public class Repro3408
-    {
-        public void Consumer()
-        {
-            var (_, y) = Repro_3408_Provider.ServiceReturningTuples.GetPair();
-        }
-    }
-}
-
-namespace Repro_3408_Provider
-{
-    using System;
-
-    public static class ServiceReturningTuples
-    {
-        public static Tuple<string, int> GetPair() => Tuple.Create(""a"", 1);
-        }
-    }
-";
-            Verifier.VerifyCSharpAnalyzer(code, new CS.UnnecessaryUsings());
-        }
+        public void UnnecessaryUsings_TupleDeconstruct() =>
+            Verifier.VerifyAnalyzer(@"TestCases\UnnecessaryUsings.TupleDeconstruct.cs",
+                                    new CS.UnnecessaryUsings());
 
         [TestMethod]
         [TestCategory("CodeFix")]
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnnecessaryUsings.TupleDeconstruct.cs
@@ -0,0 +1,22 @@
+﻿namespace Repro_3408_Consumer
+{
+    using System; // Noncompliant - FP, See: https://github.com/SonarSource/sonar-dotnet/issues/3408
+
+    public class Repro3408
+    {
+        public void Consumer()
+        {
+            var (_, y) = Repro_3408_Provider.ServiceReturningTuples.GetPair();
+        }
+    }
+}
+
+namespace Repro_3408_Provider
+{
+    using System;
+
+    public static class ServiceReturningTuples
+    {
+        public static Tuple<string, int> GetPair() => Tuple.Create("a", 1);
+    }
+}
