10d55042188224020f52789edff11123e7bfc151
Andrei Epure <andrei.epure@sonarsource.com>
2020-08-12 17:13:50 +0200

    Update ScannerSide in rules definition

--- sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/CSharpSonarRulesDefinition.java
+++ sonar-csharp-plugin/src/main/java/org/sonar/plugins/csharp/CSharpSonarRulesDefinition.java
@@ -19,7 +19,7 @@
  */
 package org.sonar.plugins.csharp;
 
-import org.sonar.api.batch.ScannerSide;
+import org.sonar.api.scanner.ScannerSide;
 import org.sonarsource.dotnet.shared.plugins.AbstractRulesDefinition;
 
 import static org.sonar.plugins.csharp.CSharpPlugin.REPOSITORY_KEY;
