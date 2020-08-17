bffd5568dcdbac6a9fb7f0abae228a9890db5549
Andrei Epure <andrei.epure@sonarsource.com>
2020-08-17 12:19:45 +0200

    Add the MultipleProjects solution

---
+++ its/projects/MultipleProjects/FirstProject/FirstClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq; // unused
+
+namespace FirstProject
+{
+    public class FirstClass
+    {
+        public int CoveredGet { get; set; } public int UncoveredProperty { get; set; } public int CoveredSet { get; set; }
+
+        public int CoveredGetOnSecondLine { get; set; }
+
+        public int CoveredProperty { get; set; }
+
+        public int ArrowMethod(bool condition) => condition ? CoveredGetOnSecondLine : UncoveredProperty;
+
+        public int BodyMethod()
+        {
+            var x = CoveredProperty; CoveredProperty = 1; goto label; UncoveredProperty = 1;
+
+            UncoveredProperty = 1;
+
+            label:
+            CoveredSet = 1;
+
+            return CoveredGet;
+        }
+    }
+}
