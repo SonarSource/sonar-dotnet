063b7c05fd7d0b76ccff9eaff80f2d58376a0d36
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2020-08-25 14:04:13 +0200

    S2387: Add FP repro for #3393 (#3550)

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/FieldShadowsParentField.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/FieldShadowsParentField.cs
@@ -15,3 +15,20 @@ namespace Tests.TestCases
         protected static int leafs; // Compliant, static is ignored
     }
 }
+
+// https://github.com/SonarSource/sonar-dotnet/issues/3393
+namespace Repro_3393
+{
+    public class Animal { }
+    public class Dog : Animal { }
+
+    public class AnimalContainer
+    {
+        protected readonly Animal animal;
+    }
+
+    public class DogContainer : AnimalContainer
+    {
+        private new readonly Dog animal; // Noncompliant FP, modifier "new" is used to explicitly declare the intention
+    }
+}
