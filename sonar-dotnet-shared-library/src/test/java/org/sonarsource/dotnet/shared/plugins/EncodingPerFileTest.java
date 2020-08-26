44fc10b7828a620e50c2096e04a13f6ff6e4efb5
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-08-26 13:43:16 +0200

    Review update 1

--- sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/EncodingPerFileTest.java
+++ sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/EncodingPerFileTest.java
@@ -61,7 +61,7 @@ public class EncodingPerFileTest {
   public void should_treat_as_match_and_warn_when_roslyn_encoding_missing() throws IOException {
     assertEncodingMatch(null, fileUri, null, true);
 
-    assertThat(logTester.logs(LoggerLevel.WARN)).containsOnly(String.format("Roslyn encoding was not found for '%s', using default instead.", fileUri.toString()));
+    assertThat(logTester.logs(LoggerLevel.WARN)).containsOnly(String.format("Roslyn encoding was not detected for '%s', using default instead.", fileUri.toString()));
   }
 
   @Test
