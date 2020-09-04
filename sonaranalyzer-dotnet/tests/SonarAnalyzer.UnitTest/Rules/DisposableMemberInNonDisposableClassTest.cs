03df12f556ac3f65882239ddf59368b012c4edbb
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-09-04 14:45:21 +0200

    Add check for IAsyncDisposable

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/DisposableMemberInNonDisposableClassTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/DisposableMemberInNonDisposableClassTest.cs
@@ -40,7 +40,7 @@ namespace SonarAnalyzer.UnitTest.Rules
         [TestMethod]
         public void DisposableMemberInNonDisposableClass_IAsyncDisposable() =>
             Verifier.VerifyCSharpAnalyzer(@"
-namespace AsyncDisposable
+namespace Namespace
 {
     using System;
     using System.Threading;
@@ -61,6 +61,27 @@ namespace AsyncDisposable
             this.cancellationTokenSource?.Dispose();
             return new ValueTask();
         }
+    }
+
+    public class C1 // Noncompliant, needs to implement IDisposable or IAsyncDisposable
+    {
+        private IAsyncDisposable disposable;
+
+        public void Init() => disposable = new AsyncDisposable();
+    }
+
+    public class C2 : IAsyncDisposable // Implements IAsyncDisposable
+    {
+        private IAsyncDisposable disposable;
+
+        public void Init() => disposable = new AsyncDisposable();
+
+        public ValueTask DisposeAsync() => disposable.DisposeAsync();
+    }
+
+    public class AsyncDisposable : IAsyncDisposable
+    {
+        public ValueTask DisposeAsync() => new ValueTask();
     }
 }", new DisposableMemberInNonDisposableClass(), additionalReferences: NetStandardMetadataReference.Netstandard);
 #endif
