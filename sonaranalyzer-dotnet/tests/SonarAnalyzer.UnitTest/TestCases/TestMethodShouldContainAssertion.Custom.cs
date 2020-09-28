3a78518bac8c3933cbeda23aa09e01ed3be44f00
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-09-28 17:27:55 +0200

    S2699: Add FP repro for #2699

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestMethodShouldContainAssertion.Custom.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomTests
+{
+    using TestFramework;
+
+    [TestClass]
+    public class Program
+    {
+        [TestMethod]
+        public void TestMethod1() // Noncompliant {{Add at least one assertion to this test case.}}
+//                  ^^^^^^^^^^^
+        {
+            var x = 42;
+        }
+
+        [TestMethod]
+        public void TestMethod2() // Noncompliant FP
+        {
+            Validator.StaticWithAttribute();
+        }
+
+        [TestMethod]
+        public void TestMethod3() // Noncompliant
+        {
+            Validator.StaticWrongAttribute();
+        }
+
+        [TestMethod]
+        public void TestMethod4() // Noncompliant
+        {
+            Validator.StaticNoAttribute();
+        }
+
+        [TestMethod]
+        public void TestMethod5() // Noncompliant FP
+        {
+            var validator = new Validator();
+            validator.InstanceWithAttribute();
+        }
+
+        [TestMethod]
+        public void TestMethod6() => // Noncompliant FP
+            new Validator().InstanceWithAttributeAndArg(null);
+    }
+}
+
+namespace TestFramework
+{
+    using TestFramework.Attributes;
+
+    public class Validator
+    {
+        [AssertionMethodAttribute]
+        public static void StaticWithAttribute() { }
+
+        [NotAssertionMethodAttribute]
+        public static void StaticWrongAttribute() { }
+
+        public static void StaticNoAttribute() { }
+
+        [AssertionMethod]
+        public bool InstanceWithAttribute() => true;
+
+        [AssertionMethod]
+        public bool InstanceWithAttributeAndArg(object arg) => true;
+    }
+
+    [AssertionMethod] // Missused attribute
+    public static class AttributedType
+    {
+        public static void AttributeOnType() { } // Not an assertion method
+    }
+}
+
+namespace TestFramework.Attributes
+{
+    public class AssertionMethodAttribute : Attribute { }
+    public class NotAssertionMethodAttribute : Attribute { } // AssertionMethodAttribute doesn't count as an assertion method attribute
+}
