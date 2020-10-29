3e0f293f88a94c4a56067e30757f12e112ce45bc
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-10-29 17:47:11 +0100

    Add C# 9 test cases for S2123

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp9.cs
@@ -0,0 +1,9 @@
+﻿nint i = 0;
+i = i++; // Noncompliant; i is still zero
+i = ++i; // Compliant
+
+nuint u = 0;
+u = u++; // Noncompliant; u is still zero
+u = ++u; // Compliant
+
+return (int)i++; // Compliant FN
