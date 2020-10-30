1ad484b74edbfa27c4c9722cbeba8e35a9f3b8ff
Andrei Epure <andrei.epure@sonarsource.com>
2020-10-30 10:51:29 +0100

    Add C# 9 tests for CheckArgumentException

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/CheckArgumentException.TopLevelStatements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+if (args == null)
+{
+    throw new ArgumentException(); // Noncompliant {{Use a constructor overloads that allows a more meaningful exception message to be provided.}}
+    throw new ArgumentNullException("foo"); // Noncompliant {{The parameter name 'foo' is not declared in the argument list.}}
+    throw new ArgumentException("args", "message"); // Noncompliant {{The parameter name 'message' is not declared in the argument list.}}
+    throw new ArgumentException("message", "args"); // Noncompliant FP
+    throw new ArgumentNullException("args"); // Noncompliant FP
+}
+
+if (args.Length > 0)
+{
+    foreach (var arg in args)
+    {
+        Console.WriteLine(arg);
+    }
+}
