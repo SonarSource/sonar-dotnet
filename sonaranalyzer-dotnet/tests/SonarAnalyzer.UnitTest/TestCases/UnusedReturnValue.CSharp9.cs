a0e586193fa899c3727e7a2fe046a301f2630f68
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-10-30 14:53:58 +0100

    Add C# 9 test cases for S3241

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UnusedReturnValue.CSharp9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int MyMethod() { return 42; } // Compliant FN
+int MyMethod2() { return 42; }
+
+MyMethod();
+var i = MyMethod2();
+Action<int> a = (x) => MyMethod();
+SomeGenericMethod<object>();
+var o = SomeGenericMethod2<object>();
+
+T SomeGenericMethod2<T>() where T : class { return null; }
+T SomeGenericMethod<T>() where T : class { return null; } // Compliant
+
+new R().MyMethod();
+i = new R().MyMethod2();
+
+int NeverUsed(int neverUsedVal) // FN - the returned value is not used
+{
+    return neverUsedVal;
+}
+
+record R
+{
+    public int MyMethod() { return 42; } // Compliant
+    public int MyMethod2() { return 42; }
+}
