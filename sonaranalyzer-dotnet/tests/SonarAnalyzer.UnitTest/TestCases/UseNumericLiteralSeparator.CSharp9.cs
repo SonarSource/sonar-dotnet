9e20cfbd62cf6ffe696b1ffab4612e39e4847bdf
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-10-30 13:49:04 +0100

    Add C# 9 test cases for S2148

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseNumericLiteralSeparator.CSharp9.cs
@@ -0,0 +1,5 @@
+﻿nint i1 = 10000000;  // Noncompliant; is this 10 million or 100 million?
+int i2 = 10_000_000;
+
+nuint j1 = 0b01101001010011011110010101011110;  // Noncompliant {{Add underscores to this numeric value for readability.}}
+nuint j2 = 0b01101001_01001101_11100101_01011110;
