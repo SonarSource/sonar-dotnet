71f2954f54362e7e4be9189f655f482530193417
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-02 15:18:30 +0100

    Add C# 9 test cases for S3366

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ThisShouldNotBeExposedFromConstructors.CSharp9.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+record Record
+{
+    Record()
+    {
+        OtherClass.StaticMethod(this); // Noncompliant
+        OtherClass.StaticList.Add(this); // Noncompliant
+        OtherClass.StaticProperty = this; // Noncompliant
+
+        InstanceProperty = this;
+        InstanceMethod(this);
+    }
+
+    public void InstanceMethod(Record r) { }
+
+    public Record InstanceProperty { get; set; }
+}
+
+static class OtherClass
+{
+    public static List<Record> StaticList = new List<Record>();
+
+    public static void StaticMethod(Record r) { }
+
+    public static Record StaticProperty { get; set; }
+}
