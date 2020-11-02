eae8cb2beb5b7dae836c0b334529f2798a8d55a2
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-02 16:46:58 +0100

    Add C# 9 test cases for S3433

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TestMethodShouldHaveCorrectSignature.CSharp9.cs
@@ -0,0 +1,52 @@
+﻿namespace MicrosoftTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    class TestSuite
+    {
+        public void Method()
+        {
+            [TestMethod]
+            void NestedTest() { } // Compliant - FN, test is not run by the test runner
+
+            [DataTestMethod]
+            void NestedDataTest() { } // Compliant - FN, test is not run by the test runner
+        }
+    }
+}
+
+namespace NUnitTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class TestSuite
+    {
+        public void Method()
+        {
+            [Test]
+            void NestedTest() { } // Compliant - FN, test is not run by the test runner
+
+            [TestCase(42)]
+            void NestedTestCase() { } // Compliant - FN, test is not run by the test runner
+        }
+    }
+}
+
+namespace XUnitTests
+{
+    using Xunit;
+
+    class TestSuite
+    {
+        public void Method()
+        {
+            [Fact]
+            void NestedFact() { } // Compliant - FN, test is not run by the test runner
+
+            [Theory]
+            void NestedTheory() { } // Compliant - FN, test is not run by the test runner
+        }
+    }
+}
