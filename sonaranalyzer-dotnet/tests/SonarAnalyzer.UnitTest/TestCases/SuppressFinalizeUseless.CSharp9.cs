f064cbbe1ad79b8c3755a49ece2a3412d91a87aa
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 10:07:11 +0100

    Add C# 9 test cases for S3234

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SuppressFinalizeUseless.CSharp9.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+GC.SuppressFinalize(new object()); // Compliant - class is static. Should we raise if `this` is not passed as parameter?
+
+static void Foo()
+{
+    // There is also CA1816
+    GC.SuppressFinalize(new object()); // Compliant - class is static
+}
+
+sealed record Noncompliant
+{
+    public void M() => GC.SuppressFinalize(this); // Noncompliant
+}
+
+record Compliant
+{
+    public void M() => GC.SuppressFinalize(this);
+
+    ~Compliant() { }
+}
