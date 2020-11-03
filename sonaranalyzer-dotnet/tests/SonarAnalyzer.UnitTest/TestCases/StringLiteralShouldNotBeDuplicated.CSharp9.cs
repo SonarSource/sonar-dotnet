24e1d6806f789f07ac19ebed4a2fa43debaf55d3
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 10:36:30 +0100

    Add C# 9 test cases for S1192

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/StringLiteralShouldNotBeDuplicated.CSharp9.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+string compliant = "compliant";
+string noncompliant = "foobar";
+
+record Record
+{
+    private string name = "foobar"; // Compliant - FN
+
+    public static readonly string NameReadonly = "foobar"; // Compliant - FN
+
+    string Name { get; } = "foobar"; // Compliant - FN
+
+    void Method()
+    {
+        var x = "foobar"; // Compliant - FN
+
+        void NestedMethod()
+        {
+            var y = "foobar"; // Compliant - FN
+        }
+    }
+
+    [DebuggerDisplay("foobar", Name = "foobar", TargetTypeName = "foobar")] // Compliant - in attribute -> ignored
+    record InnerRecord
+    {
+        private string name = "foobar"; // Compliant - FN
+
+        public static readonly string NameReadonly = "foobar"; // Compliant - FN
+
+        string Name { get; } = "foobar"; // Compliant - FN
+
+        void Method()
+        {
+            var x = "foobar"; // Compliant - FN
+
+            [Conditional("DEBUG")] // Compliant - in attribute -> ignored
+            static void NestedMethod()
+            {
+                var y = "foobar"; // Compliant - FN
+            }
+        }
+    }
+}
