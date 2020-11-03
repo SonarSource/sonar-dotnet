7ad228d9e86dcd76b04e1f01a6d0f7734b68d2e1
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 10:19:06 +0100

    Add C# 9 test cases for S3876

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/StringOrIntegralTypesForIndexers.CSharp9.cs
@@ -0,0 +1,18 @@
+﻿record Foo
+{
+    public int this[Class index] { get { return 0; } }
+//                  ^^^^^ Noncompliant {{Use string or an integral type here, or refactor this indexer into a method.}}
+
+    public int this[Record index] { get { return 0; } }
+
+    public int this[int index] { get { return 0; } }
+
+    public int this[string index] { get { return 0; } }
+
+    public int this[nint index] { get { return 0; } }
+
+    public int this[nuint index] { get { return 0; } }
+}
+
+class Class { }
+record Record { }
