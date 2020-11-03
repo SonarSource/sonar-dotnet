1c4d7c52c87e2055c9ceae687b86bb9af8676b5f
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 12:00:11 +0100

    Add C# 9 test cases for S2223

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/StaticFieldVisibleTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/StaticFieldVisibleTest.cs
@@ -30,9 +30,13 @@ namespace SonarAnalyzer.UnitTest.Rules
     {
         [TestMethod]
         [TestCategory("Rule")]
-        public void StaticFieldVisible()
-        {
-            Verifier.VerifyAnalyzer(@"TestCases\StaticFieldVisible.cs", new StaticFieldVisible());
-        }
+        public void StaticFieldVisible() => Verifier.VerifyAnalyzer(@"TestCases\StaticFieldVisible.cs", new StaticFieldVisible());
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void StaticFieldVisible_CSharp9() =>
+            Verifier.VerifyAnalyzer(@"TestCases\StaticFieldVisible.CSharp9.cs",
+                                    new StaticFieldVisible(),
+                                    ParseOptionsHelper.FromCSharp9);
     }
 }
