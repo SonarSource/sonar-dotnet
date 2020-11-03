4408b2cc218eec96ad25421d1f0bae87906ba5e9
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 15:31:14 +0100

    Add C# 9 test cases for S2437

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/SillyBitwiseOperation.CSharp9.cs
@@ -0,0 +1,37 @@
+﻿nint resultNint;
+nint bitMaskNint = 0x010F;
+nint nOne = 1;
+nint nZero = 0;
+
+resultNint = -1 & bitMaskNint; // Noncompliant
+resultNint = bitMaskNint | nOne; // Compliant - FN
+resultNint = bitMaskNint ^ nZero; // Compliant - FN
+resultNint = bitMaskNint ^ nZero; // Compliant - FN
+resultNint &= -nOne; // Compliant - FN
+resultNint |= nZero; // Compliant - FN
+resultNint ^= nZero; // Compliant - FN
+var result2 = resultNint ^= nZero; // Compliant - FN
+
+resultNint = bitMaskNint & 1; // Compliant
+resultNint = bitMaskNint | 1; // compliant
+resultNint = bitMaskNint ^ 1; // Compliant
+resultNint &= 1; // Compliant
+resultNint |= 1; // compliant
+resultNint ^= 1; // Compliant
+
+nuint bitMaskNuint = 0x010F;
+nuint resultNuint;
+nuint nuOne = 1;
+nuint nuZero = 0;
+
+resultNuint = bitMaskNuint & + + +nuOne; // Compliant - FN
+resultNuint = bitMaskNuint & nuZero; // Compliant
+resultNuint = bitMaskNuint | 0; // Noncompliant
+resultNuint = bitMaskNuint | 0x0; // Noncompliant
+resultNuint = bitMaskNuint & returnNuint(); // Compliant
+resultNuint = bitMaskNuint & 0x0F; // Compliant
+
+MyMethod(1 | 0x00000); // Noncompliant
+
+static void MyMethod(nuint u) { }
+static nuint returnNuint() => 1;
