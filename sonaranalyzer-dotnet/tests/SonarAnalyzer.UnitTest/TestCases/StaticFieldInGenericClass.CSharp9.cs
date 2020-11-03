2ef7613784356f3c242fe2f7a63e5084bd9f1328
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-03 14:03:27 +0100

    Add C# 9 test cases for S2743

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/StaticFieldInGenericClass.CSharp9.cs
@@ -0,0 +1,11 @@
+﻿record StaticFieldInGenericClass<T, U>
+    where T : class
+{
+    internal static string field; //Noncompliant
+
+    public static string Prop1 { get; set; } //Noncompliant
+
+    public string Prop2 { get; set; }
+
+    public static T Prop3 { get; set; }
+}
