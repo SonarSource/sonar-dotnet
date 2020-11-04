864a089376d7eb76c467d76ee3c67c732131a66e
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-04 13:52:11 +0100

    Add C# 9 test cases for S3626

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantJumpStatement.CSharp9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+var a = new Action(() =>
+{
+    return; // Noncompliant
+//  ^^^^^^^
+});
+
+goto A; // Compliant - FN
+A:
+return; // Compliant - FN
+
+Action<int> b = static i =>
+{
+    return;  // Noncompliant
+};
+
+static void LocalMethod()
+{
+    return; // Noncompliant
+};
+
+record Record
+{
+    int Prop
+    {
+        init
+        {
+            goto A; // Compliant - FN
+A:
+            return; // Compliant - FN
+        }
+    }
+}
+
+// See https://github.com/dotnet/roslyn/issues/45510
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
