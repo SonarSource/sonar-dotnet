4b16c0f9f6a41104e1e3694167b62a8727f57117
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-04 15:27:55 +0100

    Add C# 9 test cases for S3257

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantDeclaration.CSharp9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+Record r1 = new Record(); // Compliant - FN
+Record r2 = null;
+r2 = new Record(); // Compliant - FN
+
+LocalStatic(new Record()); // Compliant - FN
+Action<int, int> a = static (parameter1, parameter2) => { }; // Compliant - FN, variables are not used and they should be replaced by discard (_, _)
+
+Record Local() => new Record(); // Compliant - FN
+static Record LocalStatic(Record r) => r;
+
+record Record
+{
+    object field;
+
+    object Property
+    {
+        get { return new object(); } // Compliant - FN
+        init { field = new object(); } // Compliant - FN
+    }
+}
+
+// See https://github.com/dotnet/roslyn/issues/45510
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
