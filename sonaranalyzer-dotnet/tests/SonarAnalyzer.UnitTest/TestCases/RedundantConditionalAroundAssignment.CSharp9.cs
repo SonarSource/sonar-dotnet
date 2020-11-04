f30f0aac8872013800681894158ec52c2ebd77ab
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-04 15:44:20 +0100

    Add C# 9 test cases for S3440

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantConditionalAroundAssignment.CSharp9.cs
@@ -0,0 +1,39 @@
+﻿int x = 5;
+int y = 6;
+
+y = y switch
+{
+    not 5 => 5 // Compliant - FN
+};
+
+y = y switch
+{
+    5 => 5 // Compliant - FN
+};
+
+record Record
+{
+    string x;
+
+    string NoncompliantProperty
+    {
+        init
+        {
+            if (x != value) // Noncompliant
+            {
+                x = value;
+            }
+        }
+    }
+
+    string CompliantProperty
+    {
+        init { x = value; }
+    }
+}
+
+// See https://github.com/dotnet/roslyn/issues/45510
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
