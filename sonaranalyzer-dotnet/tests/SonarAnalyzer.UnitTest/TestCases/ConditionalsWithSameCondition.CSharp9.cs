75d65b0eb44743a89a512c060a07ac0696e103cb
Andrei Epure <andrei.epure@sonarsource.com>
2020-11-04 12:19:09 +0100

    Add C# 9 tests for S2760

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ConditionalsWithSameCondition.CSharp9.cs
@@ -0,0 +1,53 @@
+﻿string c = null;
+if (c is null)
+{
+    doTheThing(c);
+}
+if (c is null) // FN
+{
+    doTheThing(c);
+}
+if (c is not null) // Compliant
+{
+    doTheThing(c);
+}
+
+if (c is "banana") // Compliant, c might be updated in the previous if
+{
+    c += "a";
+}
+if (c is "banana") // Compliant, c might be updated in the previous if
+{
+    c = "";
+}
+if (c is "banana") // Compliant, c might be updated in the previous if
+{
+    c = "";
+}
+
+int i = 0;
+if (i is > 0 and < 100)
+{
+    doTheThing(i);
+}
+if (i is > 0 and < 100) // FN
+{
+    doTheThing(i);
+}
+
+Fruit f = null;
+bool cond = false;
+if (f is Apple)
+{
+    f = cond ? new Apple() : new Orange();
+}
+if (f is Apple) // Compliant as f may change
+{
+    f = cond ? new Apple() : new Orange();
+}
+
+void doTheThing(object o) { }
+
+abstract class Fruit { }
+class Apple : Fruit { }
+class Orange : Fruit { }
