1c45c55acb68499f0c59791c2c9a6440ddc28af8
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-05 11:11:32 +0100

    Add C# 9 test cases for  S3603

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PureAttributeOnVoidMethod.CSharp9.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Contracts;
+
+[Pure]
+int Compliant(int age) => age;
+
+[Pure] // Compliant - FN
+void Noncompliant()
+{
+}
+
+record Record
+{
+    Record()
+    {
+        [Pure] // Compliant - FN
+        void LocalFunction()
+        {
+            [Pure] // Compliant - FN
+            void NestedLocalFunction()
+            {
+            }
+        }
+    }
+}
