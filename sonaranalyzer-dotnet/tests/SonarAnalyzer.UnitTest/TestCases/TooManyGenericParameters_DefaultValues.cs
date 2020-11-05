620f6bf3af1b88b1c9475338a0701f58a9482ac2
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-05 10:20:41 +0100

    Fix syntax to be compatible with C# 7

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TooManyGenericParameters_DefaultValues.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/TooManyGenericParameters_DefaultValues.cs
@@ -66,10 +66,10 @@ namespace Tests.Diagnostics
     interface IFoo<T1, T2, T3> // Noncompliant {{Reduce the number of generic parameters in the 'IFoo' interface to no more than the 2 authorized.}}
 //            ^^^^
     {
-        public void Foo<T1, T2, T3>();
+        void Foo<T1, T2, T3>();
 
-        public void Foo<T1, T2, T3, T4>(); // Noncompliant
-//                  ^^^
+        void Foo<T1, T2, T3, T4>(); // Noncompliant
+//           ^^^
     }
 }
 
