0e92abbe250c7b3eeb1fe0a659704e22cb57c5fd
Andrei Epure <andrei.epure@sonarsource.com>
2020-11-05 15:52:09 +0100

    Add C# 9 test for S4524

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/DefaultSectionShouldBeFirstOrLast.CSharp9.cs
@@ -0,0 +1,27 @@
+﻿switch (args[0])
+{
+    case "a":
+        break;
+    default: // Noncompliant {{Move this 'default:' case to the beginning or end of this 'switch' statement.}}
+//  ^^^^^^^^
+        break;
+    case "b":
+        break;
+}
+
+switch (args[0])
+{
+    default: // Compliant - first section
+        break;
+
+    case "a":
+        break;
+}
+
+switch (args[0])
+{
+    case "a":
+        break;
+    default: // Compliant - last section
+        break;
+}
