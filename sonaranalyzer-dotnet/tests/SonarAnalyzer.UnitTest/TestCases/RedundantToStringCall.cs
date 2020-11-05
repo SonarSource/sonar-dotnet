5e700a3d4dbdc5a6cf3a64ad242c212a060b24d7
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-05 12:29:04 +0100

    S1858: Add FP repro for #3665

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantToStringCall.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundantToStringCall.cs
@@ -1,4 +1,6 @@
-﻿namespace Tests.Diagnostics
+﻿using System;
+
+namespace Tests.Diagnostics
 {
     public class MyClass
     {
@@ -39,6 +41,22 @@
             s = m.ToString() + "";
 
             s = base.ToString() + "";
+        }
+    }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/3665
+    public class Repro_3665
+    {
+        public void Repro(Uri a, Uri b, Uri c, Uri d)
+        {
+            var two = a.ToString() + b.ToString();  // First occurance is False Positive, second is True Positive
+//                     ^^^^^^^^^^^
+//                                    ^^^^^^^^^^^@-1
+
+            var four = a.ToString() + b.ToString() + c.ToString() + d.ToString();   // Noncompliant, a.ToString() is a False Positive
+            // Noncompliant@-1 // b is TP
+            // Noncompliant@-2 // c is TP
+            // Noncompliant@-3 // d is TP
         }
     }
 }
