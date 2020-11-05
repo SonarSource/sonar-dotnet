b1987bbf6804b152aae9cfa20775202d5f839218
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-05 10:44:17 +0100

    Add C# 9 test cases for S3253

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/RedundancyInConstructorDestructorDeclaration.CSharp9.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+record RecordBase
+{
+    public RecordBase() { }
+    public RecordBase(int i) { }
+}
+
+record Record : RecordBase
+{
+    public Record()
+        : base() { } // Noncompliant
+
+    public Record(string s)
+        : base() // Noncompliant
+    {
+    }
+
+    public Record(int i)
+        : base(i)
+    {
+    }
+}
+
+record StaticCtor
+{
+    static StaticCtor() { } // Noncompliant
+}
+
+record Foo
+{
+    public Foo() { } // Compliant - FN
+
+    ~Foo() { } // Noncompliant {{Remove this redundant destructor.}}
+}
+
+record Bar
+{
+    public Bar()
+    {
+        Console.WriteLine("Hi!");
+    }
+}
