3350af03da080f0183f8e24008413959f36ac443
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2020-11-06 09:55:18 +0100

    Add C# 9 tests for S3254, S4829, S3603, S3253, S2339, S3926 and S2376 (#3715)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ProvideDeserializationMethodsForOptionalFields.CSharp9.cs
@@ -0,0 +1,59 @@
+﻿using System;
+// Noncompliant@-1 // FP, the location is wrong for records
+// Noncompliant@-2
+// Noncompliant@-3
+
+using System.Runtime.Serialization;
+
+[Serializable]
+public record Compliant 
+{
+    [OptionalField]
+    int optionalField = 5;
+
+    [OnDeserializing]
+    void OnDeserializing(StreamingContext context)
+    {
+        optionalField = 5;
+    }
+
+    [OnDeserialized]
+    void OnDeserialized(StreamingContext context)
+    {
+        // Set optionalField if dependent on other deserialized values.
+    }
+}
+
+public record NoEventHandlerMethods // Compliant - FN
+{
+    [OptionalField]
+    int optionalField = 5;
+}
+
+public record OnlyOnDeserializingEventHandlerMethod // Compliant - FN
+{
+    [OptionalField]
+    int optionalField = 5;
+
+    [OnDeserializing]
+    void OnDeserializing(StreamingContext context) => optionalField = 5;
+}
+
+[Serializable]
+public record Record // Compliant - FN, attributes are added on local methods
+{
+    [OptionalField]
+    int optionalField = 5;
+
+    public Record()
+    {
+        [OnDeserializing]
+        void OnDeserializing(StreamingContext context) => optionalField = 5;
+
+        [OnDeserialized]
+        void OnDeserialized(StreamingContext context)
+        {
+            // Set optionalField if dependent on other deserialized values.
+        }
+    }
+}
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ReadingStandardInput.CSharp9.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+string value;
+int code;
+ConsoleKeyInfo key;
+
+code = Console.Read(); // Noncompliant {{Make sure that reading the standard input is safe here.}}
+value = Console.ReadLine(); // Noncompliant
+code = Console.Read(); // Noncompliant
+key = Console.ReadKey(); // Noncompliant
+key = Console.ReadKey(true); // Noncompliant
+
+Console.OpenStandardInput(); // Noncompliant
+Console.OpenStandardInput(100); // Noncompliant
+
+var x = Console.In; // Noncompliant
+Console.In.Read(); // Noncompliant
+
+Console.Read(); // Compliant, value is ignored
+Console.ReadLine(); // Compliant, value is ignored
+Console.ReadKey(); // Compliant, value is ignored
+Console.ReadKey(true); // Compliant, value is ignored
+
+Console.Write(1);
+Console.WriteLine(1);
+
+record Record
+{
+    public Record()
+    {
+        string line = Console.ReadLine(); // Noncompliant
+    }
+}
