2f4af6ff153fba4c633582b3525a64d8d66039be
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-11 16:02:35 +0100

    Add UTs S2278, S5547

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/InsecureEncryptionAlgorithm.CSharp9.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography;
+
+namespace Tests.Diagnostics
+{
+    public class MyTripleDESCryptoServiceProvider : TripleDES
+    {
+        public override ICryptoTransform CreateDecryptor(byte[] rgbKey, byte[] rgbIV) =>
+            throw new System.NotImplementedException();
+
+        public override ICryptoTransform CreateEncryptor(byte[] rgbKey, byte[] rgbIV) =>
+            throw new System.NotImplementedException();
+
+        public override void GenerateIV() =>
+            throw new System.NotImplementedException();
+
+        public override void GenerateKey() =>
+            throw new System.NotImplementedException();
+    }
+
+    public class InsecureEncryptionAlgorithm
+    {
+        // Rule will raise an issue for both S2278 and S5547 as they are activated by default in unit tests
+        public InsecureEncryptionAlgorithm()
+        {
+            using (MyTripleDESCryptoServiceProvider tripleDES = new()) // FN
+            {
+            }
+
+            using (DESCryptoServiceProvider des = new())    // FN
+            {
+            }
+
+            using (RC2CryptoServiceProvider rc21 = new())   // FN
+            {
+            }
+
+            using (AesCryptoServiceProvider aes = new())    // Compliant
+            {
+            }
+
+        }
+    }
+}
