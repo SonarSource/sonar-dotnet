ba059ffd302c9611551b6980e95c910b635de71a
Corniel Nobel <mail@corniel.nl>
2020-11-11 14:02:05 +0100

    Re-enable tests.

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.cs
@@ -1,13 +1,20 @@
-﻿using System;
-
-namespace Tests.Diagnostics
+﻿namespace Tests.Diagnostics
 {
-    class Program
+    public class UseArrayEmpty
     {
-        public void Test()
+        public UseArrayEmpty()
         {
+            var array = new int[0]; // Noncompliant {{Declare this empty array using Array.Empty<T>.}}
+            //          ^^^^^^^^^^
 
+            var dbl = new int[00]; // Noncompliant
+            var other = new int[] { }; // Noncompliant
+            var comment = new int[] { /* comment */ }; // Noncompliant
+            var dynamic = new dynamic[] { };// Noncompliant
+
+            var array_1 = new int[1]; // Compliant
+            var other_1 = new[] { 17 }; // Compliant
+            var multi = new int[0, 0]; // Compliant
         }
-
     }
 }
--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.vb
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.vb
@@ -1,13 +1,23 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
+﻿Option Strict Off
 
-Namespace Tests.TestCases
+Namespace Tests.Diagnostics
 
-    Class Program
+    Public Class UseArrayEmpty
 
-        Public Sub Test()
+        Public Sub Arrays()
+
+            Dim eplicit(-1) As Integer ' Noncompliant {{Declare this empty array using Array.Empty(Of T).}}
+            '   ^^^^^^^^^^^^^^^^^^^^^^
+
+            Dim implicit1 As Integer() = New Integer() {} ' Noncompliant
+            Dim implicit2 As Integer() = {} ' // Noncompliant
+            Dim implicit3() As Integer = {} ' Noncompliant
+
+
+            Dim dynamic = {} ' Noncompliant
+
+            Dim array_1(0) As Integer ' Compliant
+            Dim other_1() As Integer = {1} ' Compliant
 
         End Sub
 
