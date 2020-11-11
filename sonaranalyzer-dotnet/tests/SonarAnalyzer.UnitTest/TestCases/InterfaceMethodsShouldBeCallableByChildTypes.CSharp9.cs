cbe84c13061f0c3ac22dedee393ffc517b8a0a6d
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-11 17:44:43 +0100

    Add UTs S4039

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/InterfaceMethodsShouldBeCallableByChildTypes.CSharp9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    public interface IFoo
+    {
+        void Method();
+        int Property { get; set; }
+
+        event EventHandler Event;
+    }
+
+    public record Foo : IFoo
+    {
+        void IFoo.Method() // FN {{Make 'Foo' sealed, change to a non-explicit declaration or provide a new method exposing the functionality of 'IFoo.Method'.}}
+        {
+        }
+
+        void Method() { }
+
+        int IFoo.Property // FN {{Make 'Foo' sealed, change to a non-explicit declaration or provide a new method exposing the functionality of 'IFoo.Property'.}}
+        { get; set; }
+
+        int Property { get; set; }
+
+        event EventHandler IFoo.Event // Should be FN after Roslyn fix
+        { add { } remove { } }
+
+        // We cannot add this line yet, it crashes Roslyn with StackOverflowException https://github.com/dotnet/roslyn/issues/49286
+        //event EventHandler Event {  add { } remove { } }
+    }
+}
