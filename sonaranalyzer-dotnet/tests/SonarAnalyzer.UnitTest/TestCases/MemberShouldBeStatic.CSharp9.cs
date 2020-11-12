18ec60d82131f14f4a8a88b2698a105872d28852
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-12 18:36:45 +0100

    Add UTs S2325

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MemberShouldBeStatic.CSharp9.cs
@@ -0,0 +1,23 @@
+﻿
+int TopLevelMethod() => 0;              // FN
+
+var localVariable = 42;
+int LocalFunction() => localVariable;   // Compliant
+
+public record Methods
+{
+    private int instanceMember = 0;
+    private static int staticMember = 0;
+
+    public int Method1() => 0;                              // Noncompliant {{Make 'Method1' a static method.}}
+    public int Method2() => instanceMember;
+    public int Method3() => this.instanceMember;
+    public int Method4() => staticMember;                   // Noncompliant
+    public int Method5() => Methods.staticMember;           // Noncompliant
+    public int Method6() => new Methods().instanceMember;   // Noncompliant
+    public int Method7(Methods arg) => arg.instanceMember;  // Noncompliant
+
+    public static int StaticMethod1() => 0;
+    public static int StaticMethod2() => staticMember;
+}
+
