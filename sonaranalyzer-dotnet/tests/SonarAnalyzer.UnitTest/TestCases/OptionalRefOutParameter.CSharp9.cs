4925df5c156e97614c8b84dde151b59c3c8825bc
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-12 14:11:40 +0100

    Add C# 9 tests for S3447

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/OptionalRefOutParameter.CSharp9.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+static void WithRefParam([Optional] ref int i) { } // Noncompliant
+//                        ^^^^^^^^
+
+static void WithOutParam([Optional] out int i) // Noncompliant {{Remove the 'Optional' attribute, it cannot be used with 'out'.}}
+{
+    i = 23;
+}
+
+static void Compliant([Optional] int i) { }
+
+public record OptionalRefOutParameter
+{
+    public void WithRefParam([Optional] ref int i) { } // Noncompliant
+//                            ^^^^^^^^
+
+    public void WithOutParam([Optional] out int i) // Noncompliant {{Remove the 'Optional' attribute, it cannot be used with 'out'.}}
+    {
+        i = 23;
+    }
+
+    public void Compliant([Optional] int i) { }
+}
