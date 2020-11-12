7b2c7d39b3b8b30ff6cfd33d77474c785a122232
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-12 14:30:47 +0100

    Add C# 9 tests for S4050

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/OperatorsShouldBeOverloadedConsistently.CSharp9.cs
@@ -0,0 +1,15 @@
+﻿public record Compliant
+{
+    public static object operator +(Compliant a, Compliant b) => new object();
+    public static object operator -(Compliant a, Compliant b) => new object();
+}
+
+public record OnlyPlus // Compliant - FN, should implement `-` too
+{
+    public static object operator +(OnlyPlus a, OnlyPlus b) => new object();
+}
+
+public record OnlyMinus // Compliant - FN, should implement `+` too
+{
+    public static object operator -(OnlyMinus a, OnlyMinus b) => new object();
+}
