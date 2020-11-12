3783549deabfcf0f965caab27669ed24c2452ea2
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-12 11:25:04 +0100

    Fix build error and remove redundant case

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PropertyToAutoProperty.CSharp9.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/PropertyToAutoProperty.CSharp9.cs
@@ -4,20 +4,12 @@ public class MyAttribute : Attribute { }
 
 public record Record
 {
-    private int field1;
+    private string field1;
     private string field2;
-    private string field3;
 
     [My()]
     private int fieldWithAttribute;
-
     public int Compliant
-    {
-        get { return field1; }
-        set { field1 = value; }
-    }
-
-    public int Compliant2 //Compliant
     {
         get { return fieldWithAttribute; }
         set { fieldWithAttribute = value; }
@@ -25,15 +17,15 @@ public record Record
 
     public string PropWithGetAndSet // Noncompliant
     {
-        get { return field2; }
-        set { field2 = value; }
+        get { return field1; }
+        set { field1 = value; }
     }
 
     public string PropWithGetAndInit // Compliant - FN
     {
-        get { return field3; }
-        init { field3 = value; }
+        get { return field2; }
+        init { field2 = value; }
     }
 
-    public string PropWithGetAndInit { get; init; } // Compliant
+    public string AutoPropWithGetAndInit { get; init; } // Compliant
 }
