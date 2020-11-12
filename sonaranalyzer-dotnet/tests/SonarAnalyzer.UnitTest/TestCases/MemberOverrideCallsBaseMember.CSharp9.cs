996450cae0a73063b6390f43732b54accbc24ef4
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-12 18:12:31 +0100

    Add UTs S1185

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/MemberOverrideCallsBaseMember.CSharp9.cs
@@ -0,0 +1,16 @@
+﻿
+class Base
+{
+    public virtual int MyProperty1 { get; init; }
+    public virtual int MyProperty2 { get; init; }
+    public virtual int MyProperty3 { get; init; }
+}
+
+class Derived : Base
+{
+    bool isInitialized;
+
+    public override int MyProperty1 { get => base.MyProperty1; init => base.MyProperty1 = value; }      //Noncompliant
+    public override int MyProperty2 { get => base.MyProperty2; init => base.MyProperty2 = value + 1; }  //Noncompliant, FP
+    public override int MyProperty3 { get => base.MyProperty3; init { base.MyProperty2 = value; isInitialized = true; } } //Noncompliant, FP
+}
