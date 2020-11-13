9de083d2cffd2203162185fcbcacc7104f055c66
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-13 13:04:38 +0100

    Add UTs S2387, S4025

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/FieldShadowsParentField.CSharp9.cs
@@ -0,0 +1,11 @@
+﻿public record Fruit
+{
+    protected int ripe;
+    protected static int leafs;
+}
+
+public record Raspberry : Fruit
+{
+    private bool ripe;  // Noncompliant {{'ripe' is the name of a field in 'Fruit'.}}
+    protected static int leafs; // Compliant, static is ignored
+}
