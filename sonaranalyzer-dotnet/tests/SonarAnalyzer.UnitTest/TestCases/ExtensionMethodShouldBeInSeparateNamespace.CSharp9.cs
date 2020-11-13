a5fc6d2b3135feaee398c9d1909b11dd3ac6e7b5
Andrei Epure <andrei.epure@sonarsource.com>
2020-11-13 19:09:48 +0100

    add test for S4226

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExtensionMethodShouldBeInSeparateNamespace.CSharp9.cs
@@ -0,0 +1,24 @@
+﻿var x = 1;
+
+public record GlobalRecord { }
+
+namespace MyLibrary
+{
+    public record Foo { }
+}
+namespace Helpers
+{
+    public static class MyExtensions
+    {
+        public static void Bar(this MyLibrary.Foo foo) { }
+        public static void Bar(this GlobalRecord foo) { }
+    }
+}
+namespace Same
+{
+    public record R { }
+    public static class Extensions
+    {
+        public static void Bar(this R r) {  } // Noncompliant
+    }
+}
