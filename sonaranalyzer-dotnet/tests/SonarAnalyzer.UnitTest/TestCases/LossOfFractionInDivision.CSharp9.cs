5670bfb1edd769c8ba3406b5a1a6993e35590bd9
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-13 10:07:48 +0100

    Review update

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/LossOfFractionInDivision.CSharp9.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/LossOfFractionInDivision.CSharp9.cs
@@ -4,9 +4,16 @@ Method(3 / 2);          // Noncompliant
 dec = (decimal)3 / 2;
 Method(3.0F / 2);
 
-decimal ndec = (nint)3 / 2; // FN
-ndec = (nuint)3 / 2;        // FN
-Method((nint)3 / 2);        // FN
+nint a = 3;
+nint b = 2;
+decimal both = a / b;           // FN
+decimal left = (nint)3 / 2;     // FN
+decimal right = 3 / (nint)2;    // FN
+left = (nuint)3 / 2;            // FN
+Method((nint)3 / 2);            // FN
+
+right = (decimal)3 / b;         // Compliant
+Method(3.0F / b);               // Compliant
 
 void Method(float f) { }
 
