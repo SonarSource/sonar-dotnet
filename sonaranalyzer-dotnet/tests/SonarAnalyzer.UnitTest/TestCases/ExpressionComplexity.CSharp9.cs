204f23b90d8c3e70c1921f01df3877989f8ff5f5
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2020-11-16 13:10:33 +0100

    Add C# 9 tests for S3877 , S1067 , S4226  (#3750)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ExpressionComplexity.CSharp9.cs
@@ -0,0 +1,11 @@
+﻿var d1 = true && false && true && false && true && true; // Noncompliant
+
+object x = null;
+
+if (x is true or true or true or true or true) { } // FN
+
+if (x is true and true and true and true and true) { } // FN
+
+if (x is 10 or 20 or 40 or 50 or 60 or 70) { } // FN
+
+if (x is < 10 or < 20 or < 30 and (40 or 50 or 60)) { } // FN
