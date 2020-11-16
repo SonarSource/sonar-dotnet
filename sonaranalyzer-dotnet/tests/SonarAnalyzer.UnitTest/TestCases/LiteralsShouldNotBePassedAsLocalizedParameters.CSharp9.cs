c285b295296539e23ba8c6dce27f6c1031bd56c9
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2020-11-16 12:07:13 +0100

    Add C# 9 unit tests for S4055 (#3752)

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/LiteralsShouldNotBePassedAsLocalizedParameters.CSharp9.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+TopLevelLocalFunction("literal", "literal"); //Noncompliant
+                                             //Noncompliant@-1
+
+void TopLevelLocalFunction([Localizable(true)] string param1, string message)
+{
+}
+
+record Record
+{
+    [Localizable(true)]
+    public string Property1 { get; set; }
+    public string Property2 { get; set; }
+
+    public void Method()
+    {
+        Property1 = "lorem"; // Noncompliant
+        Property2 = "ipsum";
+
+        LocalFunctionWithAttribute("literal"); //Noncompliant
+
+        void LocalFunctionWithAttribute([Localizable(true)] string param1)
+        {
+        }
+    }
+}
