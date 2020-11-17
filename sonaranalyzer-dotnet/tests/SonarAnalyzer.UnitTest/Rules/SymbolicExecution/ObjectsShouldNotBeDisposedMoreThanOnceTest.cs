a4272aed501646a978c4f2cdbfea9ad867c63cc9
Pavel Mikula <pavel.mikula@sonarsource.com>
2020-11-17 17:39:03 +0100

    Add UTs S3966 ObjectsShouldNotBeDisposedMoreThanOnce

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ObjectsShouldNotBeDisposedMoreThanOnceTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ObjectsShouldNotBeDisposedMoreThanOnceTest.cs
@@ -39,5 +39,10 @@ namespace SonarAnalyzer.UnitTest.Rules
                 additionalReferences: NuGetMetadataReference.NETStandardV2_1_0,
 #endif
                 options: ParseOptionsHelper.FromCSharp8);
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_CSharp9() =>
+            Verifier.VerifyAnalyzerFromCSharp9Console(@"TestCases\ObjectsShouldNotBeDisposedMoreThanOnce.CSharp9.cs", new SymbolicExecutionRunner(new ObjectsShouldNotBeDisposedMoreThanOnce()));
     }
 }
---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ObjectsShouldNotBeDisposedMoreThanOnce.CSharp9.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+
+var topLevel = new MemoryStream();
+topLevel.Dispose();
+topLevel.Dispose(); //FN
+
+using var top = new MemoryStream(); // Compliant
+
+void TopLevelLocalFunction()
+{
+    var local = new MemoryStream();
+    local.Dispose();
+    local.Dispose();    // FN
+}
+
+public class Sample
+{
+    public void TargetTypedNew()
+    {
+        MemoryStream ms = new();
+        ms.Dispose();
+        ms.Dispose();   // FN, can't build CFG for this method
+    }
+
+    public void StaticLambda()
+    {
+        Action a = static () =>
+        {
+            var ms = new MemoryStream();
+            ms.Dispose();
+            ms.Dispose();    // Noncompliant
+        };
+        a();
+    }
+
+    public int Property
+    {
+        get => 42;
+        init
+        {
+            var ms = new MemoryStream();
+            ms.Dispose();
+            ms.Dispose();    // FN
+        }
+    }
+}
+
+public record Record : IDisposable
+{
+    public void Method()
+    {
+        var ms = new MemoryStream();
+        ms.Dispose();
+        ms.Dispose();    // Noncompliant
+    }
+
+    public void Dispose() => throw new NotImplementedException();
+
+    public static void DisposeRecord()
+    {
+        var r = new Record();
+        r.Dispose();
+        r.Dispose();    // Noncompliant
+    }
+}
+
+public partial class Partial
+{
+    public partial void Method();
+}
+
+public partial class Partial
+{
+    public partial void Method()
+    {
+        var ms = new MemoryStream();
+        ms.Dispose();
+        ms.Dispose();    // Noncompliant
+    }
+}
+
+namespace TartetTypedConditional
+{
+    public interface ISomething { }
+    public class First : ISomething, IDisposable { public void Dispose() { } }
+    public class Second : ISomething, IDisposable { public void Dispose() { } }
+
+    public class Sample
+    {
+        public void Go(bool condition)
+        {
+            var a = new First();
+            var b = new Second();
+            a.Dispose();
+            IDisposable result = condition ? a : b;
+            result.Dispose();  // Noncompliant
+        }
+    }
+}
+
