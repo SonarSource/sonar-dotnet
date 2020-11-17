c80ae10858aced550cda88e8457b4441da52a2ba
Costin Zaharia <costin.zaharia@sonarsource.com>
2020-11-17 09:57:36 +0100

    Remove netcoreapp3.1 target framework from tests

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/DeliveringDebugFeaturesInProductionTest.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/Rules/DeliveringDebugFeaturesInProductionTest.cs
@@ -63,7 +63,7 @@ namespace SonarAnalyzer.UnitTest.Rules
                 new VisualBasic.DeliveringDebugFeaturesInProduction(),
                 additionalReferences: AdditionalReferencesNetCore2);
 
-#if NETCOREAPP
+#if NET5_0
 
         [TestMethod]
         [TestCategory("Rule")]
