78b468e4737881355a5522927d811140656dd2e1
Corniel Nobel <mail@corniel.nl>
2020-11-26 13:33:57 +0100

    Added test scenario's.

--- sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.cs
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/UseArrayEmpty.cs
@@ -2,6 +2,8 @@
 {
     public class UseArrayEmpty
     {
+        public const int Zero = 0;
+
         public UseArrayEmpty()
         {
             var array = new int[0]; // Noncompliant {{Declare this empty array using Array.Empty<T>.}}
@@ -11,10 +13,18 @@
             var other = new int[] { }; // Noncompliant
             var comment = new int[] { /* comment */ }; // Noncompliant
             var dynamic = new dynamic[] { };// Noncompliant
+            var cnst = new int[Zero]; // Noncompliant
 
             var array_1 = new int[1]; // Compliant
             var other_1 = new[] { 17 }; // Compliant
             var multi = new int[0, 0]; // Compliant
+
+            Arguments(new int[42] { }); // Compliant
+            Arguments(new int[0]); // Noncompliant
+        }
+
+        public void Arguments(int[] arguments)
+        {
         }
     }
 }
