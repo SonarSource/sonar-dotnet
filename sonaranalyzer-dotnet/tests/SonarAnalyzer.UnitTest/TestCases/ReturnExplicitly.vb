a28095f5fd2d84f8d29ffba948371290256c1a26
Corniel Nobel <mail@corniel.nl>
2020-11-30 10:43:04 +0100

    Implementation of rule 5944.

---
+++ sonaranalyzer-dotnet/tests/SonarAnalyzer.UnitTest/TestCases/ReturnExplicitly.vb
@@ -0,0 +1,23 @@
+﻿Public Class UseReturnStatement
+
+    Public Function FunctionName() As Integer
+        FunctionName = 42 ' Noncompliant {{Use a 'Return' statement; assigning returned values to function names is obsolete.}}
+'       ^^^^^^^^^^^^
+    End Function
+
+    Public Function ReturnStatement() As Integer
+        Return 42 ' Compliant
+    End Function
+
+    Public Function ReturnWithVariable() As Integer
+        Dim value As Integer = 42
+        Return value  ' Compliant
+    End Function
+
+
+    Public Function ReturnWithFunctionName() As Integer
+        ReturnWithFunctionName = 42 ' Noncompliant
+        Return ReturnWithFunctionName  ' Noncompliant
+    End Function
+
+End Class
