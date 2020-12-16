8c2f69e7a85c64c8894381c83a9cfee32eae7022
Corniel Nobel <mail@corniel.nl>
2020-12-16 19:36:04 +0100

    Added compliant recursion test.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseReturnStatement.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseReturnStatement.vb
@@ -3,21 +3,28 @@
     Public Function FunctionName() As Integer
     FunctionName = 42 ' Noncompliant {{Use a 'Return' statement; assigning returned values to function names is obsolete.}}
 '       ^^^^^^^^^^^^
-End Function
+    End Function
 
-Public Function ReturnStatement() As Integer
-    Return 42 ' Compliant
-End Function
+    Public Function ReturnStatement() As Integer
+        Return 42 ' Compliant
+    End Function
 
-Public Function ReturnWithVariable() As Integer
-    Dim value As Integer = 42
-    Return value  ' Compliant
-End Function
+    Public Function ReturnWithVariable() As Integer
+        Dim value As Integer = 42
+        Return value  ' Compliant
+    End Function
 
+    Public Function ReturnWithFunctionName() As Integer
+        ReturnWithFunctionName = 42 ' Noncompliant
+        Return ReturnWithFunctionName  ' Noncompliant
+    End Function
 
-Public Function ReturnWithFunctionName() As Integer
-    ReturnWithFunctionName = 42 ' Noncompliant
-    Return ReturnWithFunctionName  ' Noncompliant
-End Function
+    Public Function WithRecursion(number As Integer) As Integer
+        If number = 42 Then
+            Return number
+        Else
+            Return WithRecursion(42) ' Compliant, method call
+        End If
+    End Function
 
 End Class
