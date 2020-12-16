fe7eb5050ace2732eb315a0f2b66cc3b5a66608b
Corniel Nobel <mail@corniel.nl>
2020-12-16 21:50:45 +0100

    stash

---
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/CollectionEmptinessCheckingFixProvider.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2020 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.Rules.CSharp
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp)]
+    public sealed class CollectionEmptinessCheckingFixProvider : SonarCodeFixProvider
+    {
+        internal const string Title = "Use Any() instead";
+        public override ImmutableArray<string> FixableDiagnosticIds =>
+            ImmutableArray.Create(CollectionEmptinessChecking.DiagnosticId);
+
+        protected override Task RegisterCodeFixesAsync(SyntaxNode root, CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            //context.RegisterCodeFix(
+            //    CodeAction.Create(
+            //        Title,
+            //        c => Task.FromResult(Simplify(root, context)),
+            //    context.Diagnostics);
+
+            return Task.CompletedTask;
+        }
+
+        internal static Document Simplify(SyntaxNode root, CodeFixContext context)
+        {
+
+            // if == 0
+            // else if > 0 || >= 1 || != 0
+            throw new NotImplementedException();
+        }
+    }
+}
