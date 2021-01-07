d83e92831e85aca4a2b207506910976523032a85
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-01-07 17:51:29 +0100

    Add S3453 and S1144 FP repros for C# 9

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassNotInstantiatable.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassNotInstantiatable.CSharp9.cs
@@ -3,77 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Tests.Diagnostics
+public record Person(string FirstName, string LastName); // Compliant
+
+public record EmptyRecord { } // Compliant
+
+public record Person1 // FN
 {
-    public record Person(string FirstName, string LastName); // Compliant
+    public string FirstName { get; }
 
-    public record EmptyRecord { } // Compliant
+    private Person1() { }
+}
 
-    public record Person1 // FN
+public record Person2 // FN
+{
+    public string FirstName { get; }
+
+    private Person2() { }
+    private Person2(string first) => FirstName = first;
+}
+
+public sealed record Person3 // Compliant
+{
+    public string FirstName { get; init; }
+
+    private Person3() { }
+    public static Person3 Instance => new Person3();
+}
+
+public record StaticUsage // FN, should use a utility class instead
+{
+    private StaticUsage() { }
+    public static void M() { }
+}
+
+public record OuterRecord // Compliant
+{
+    private OuterRecord() { }
+
+    public record Intermediate
     {
-        public string FirstName { get; }
-
-        private Person1() { }
-    }
-
-    public record Person2 // FN
-    {
-        public string FirstName { get; }
-
-        private Person2() { }
-        private Person2(string first) => FirstName = first;
-    }
-
-    public sealed record Person3 // Compliant
-    {
-        public string FirstName { get; init; }
-
-        private Person3() { }
-        public static Person3 Instance => new Person3();
-    }
-
-    public record StaticUsage // FN, should use a utility class instead
-    {
-        private StaticUsage() { }
-        public static void M() { }
-    }
-
-    public record OuterRecord // Compliant
-    {
-        private OuterRecord() { }
-
-        public record Intermediate
+        public record Nested : OuterRecord // FN
         {
-            public record Nested : OuterRecord // FN
-            {
-                private Nested() { }
-            }
+            private Nested() { }
         }
     }
+}
 
-    public record MyGenericRecord<T>
-    {
-        private MyGenericRecord() { }
-        public record Nested : MyGenericRecord<int> { }
-    }
+public record MyGenericRecord<T>
+{
+    private MyGenericRecord() { }
+    public record Nested : MyGenericRecord<int> { }
+}
 
-    public class MyGenericRecord2<T>
-    {
-        private MyGenericRecord2() { }
-        public object Create() => new MyGenericRecord2<int>();
-    }
+public class MyGenericRecord2<T>
+{
+    private MyGenericRecord2() { }
+    public object Create() => new MyGenericRecord2<int>();
+}
 
-    public class MyAttribute : System.Attribute { }
+public class MyAttribute : System.Attribute { }
 
+[My]
+public record WithAttribute1
+{
+    private WithAttribute1() { }
+}
+
+public record WithAttribute2
+{
     [My]
-    public record WithAttribute1
-    {
-        private WithAttribute1() { }
-    }
+    private WithAttribute2() { }
+}
 
-    public record WithAttribute2
+public class Foo    // Noncompliant FP
+{
+    public static readonly Foo Instance = new();
+
+    public bool IsActive => true;
+
+    private Foo()
     {
-        [My]
-        private WithAttribute2() { }
     }
 }
