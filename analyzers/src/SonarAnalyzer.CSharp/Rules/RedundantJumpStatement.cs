74a5073c81e33c343efdd3dadb9d53ab48705b57
Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
2021-01-08 10:47:42 +0100

    Update analyzers/src/SonarAnalyzer.CSharp/Rules/RedundantJumpStatement.cs
    accept suggestion from pavel
    
    Co-authored-by: Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>

--- analyzers/src/SonarAnalyzer.CSharp/Rules/RedundantJumpStatement.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/RedundantJumpStatement.cs
@@ -148,7 +148,7 @@ namespace SonarAnalyzer.Rules.CSharp
 
         private static bool IsThrow(JumpBlock jumpBlock)
         {
-            return jumpBlock.JumpNode is ThrowStatementSyntax || ThrowExpressionSyntaxWrapper.IsInstance(jumpBlock.JumpNode);
+            return jumpBlock.JumpNode.IsAnyKind(SyntaxKind.ThrowStatement, SyntaxKindEx.ThrowExpression);
         }
 
         private static bool IsReturnWithExpression(JumpBlock jumpBlock)
