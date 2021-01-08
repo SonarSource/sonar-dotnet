a2f960264b8717a486dd8167d9baebc715b21e3b
Corniel Nobel <mail@corniel.nl>
2021-01-08 13:33:59 +0100

    Update due to FF difference.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.Fixed.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-    
+
 namespace Tests.Diagnostics
 {
     public class CollectionEmptinessChecking
