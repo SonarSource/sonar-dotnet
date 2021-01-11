9942d3c8630d0c4791a4a3435afcfd032cd62510
Corniel Nobel <mail@corniel.nl>
2021-01-11 19:39:36 +0100

    Rewrote tests to make behaviour easier to reason about.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.Fixed.cs
@@ -4,67 +4,110 @@ using System.Linq;
 
 namespace Tests.Diagnostics
 {
-    public class CollectionEmptinessChecking
+    public class Extenion_method_for_any
     {
-        private static bool HasContent1(IEnumerable<string> l)
+        public bool Count_greater_then_zero(IEnumerable<string> l)
         {
             return l.Any(); // Fixed
         }
-        private static bool HasContent1b(IEnumerable<string> l)
+        public bool Count_greater_then_or_equal_to_one(IEnumerable<string> l)
         {
             return l.Any(); // Fixed
         }
-        private static bool HasContent2(List<string> l)
-        {
-            return Enumerable.Any(l); // Fixed
-        }
-        private static bool HasContent2b(List<string> l)
-        {
-            return Enumerable.Any(l); // Fixed
-        }
-        private static bool HasContent3(List<string> l)
-        {
-            return l.Any();
-        }
-        private static bool IsNotEmpty1(List<string> l)
+        public bool Count_not_zero(IEnumerable<string> l)
         {
             return l.Any(); // Fixed
         }
-        private static bool IsNotEmpty2(List<string> l)
+        public bool Zero_less_then_count(IEnumerable<string> l)
         {
             return l.Any(); // Fixed
         }
-        private static bool IsEmpty1(List<string> l)
+        public bool One_less_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return l.Any(); // Fixed
+        }
+        public bool Zero_not_count(IEnumerable<string> l)
+        {
+            return l.Any(); // Fixed
+        }
+    }
+    public class Extenion_method_for_empty
+    {
+        public bool Count_equals_zero(IEnumerable<string> l)
         {
             return !l.Any(); // Fixed
         }
-        private static bool IsEmpty2(List<string> l)
+        public bool Count_less_then_one(IEnumerable<string> l)
         {
             return !l.Any(); // Fixed
         }
-        private static bool IsEmpty2b(List<string> l)
+        public bool Count_less_then_or_equal_to_zero(IEnumerable<string> l)
         {
             return !l.Any(); // Fixed
         }
-        private static bool IsEmpty3(List<string> l)
-        {
-            return !l.Any();
-        }
-        private static bool IsEmpty4(List<string> l)
+        public bool Zero_equals_count(IEnumerable<string> l)
         {
             return !l.Any(); // Fixed
         }
-        private static bool IsEmpty4b(List<string> l)
+        public bool One_greater_then_count(IEnumerable<string> l)
         {
             return !l.Any(); // Fixed
         }
-        private static bool HasContentWithCondition(List<int> numbers)
+        public bool Zero_greater_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return !l.Any(); // Fixed
+        }
+    }
+    public class Count_with_condition_specified
+    {
+        public bool Any_for_condition(List<int> numbers)
         {
             return numbers.Any(n => n % 2 == 0); // Fixed
         }
-        private static bool IsEmptyWithCondition(List<int> numbers)
+        public bool Empty_for_condition(List<int> numbers)
         {
             return !numbers.Any(n => n % 2 == 0); // Fixed
+        }
+    }
+    public class Enumerable_count_for_any
+    {
+        public bool Count_greater_then_0(List<string> l)
+        {
+            return Enumerable.Any(l); // Fixed
+        }
+        public bool Zero_less_then_Count(List<string> l)
+        {
+            return Enumerable.Any(l); // Fixed
+        }
+    }
+    public class Alternative_one_constants
+    {
+        public bool Count_greater_then_or_equal_binary_1(IEnumerable<string> l)
+        {
+            return l.Any(); // Fixed
+        }
+        public bool Count_greater_then_or_equal_heximal_1(IEnumerable<string> l)
+        {
+            return l.Any(); // Fixed
+        }
+        public bool UInt64_one_less_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return l.Any(); // Fixed
+        }
+    }
+    public class Local_count_method
+    {
+        public int Count(IEnumerable<string> l) => 3;
+        public bool Count_equals_zero(IEnumerable<string> l)
+        {
+            return Count(l) == 0; // Compliant
+        }
+    }
+    public class Count_for_multiple
+    {
+        public bool Count_more_then_one(IEnumerable<string> l)
+        {
+            return l.Count() > 1; // Compliant
         }
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionEmptinessChecking.cs
@@ -4,69 +4,112 @@ using System.Linq;
 
 namespace Tests.Diagnostics
 {
-    public class CollectionEmptinessChecking
+    public class Extenion_method_for_any
     {
-        private static bool HasContent1(IEnumerable<string> l)
+        public bool Count_greater_then_zero(IEnumerable<string> l)
         {
             return l.Count() > 0; // Noncompliant {{Use '.Any()' to test whether this 'IEnumerable<string>' is empty or not.}}
-//                   ^^^^^
+            //       ^^^^^
         }
-        private static bool HasContent1b(IEnumerable<string> l)
+        public bool Count_greater_then_or_equal_to_one(IEnumerable<string> l)
         {
-            return 0 < l.Count(); // Noncompliant
+            return l.Count() >= 1; // Noncompliant
         }
-        private static bool HasContent2(List<string> l)
-        {
-            return Enumerable.Count(l) >= 0x1; // Noncompliant
-//                            ^^^^^
-        }
-        private static bool HasContent2b(List<string> l)
-        {
-            return 1UL <= Enumerable.Count(l); // Noncompliant // Error [CS0034]
-        }
-        private static bool HasContent3(List<string> l)
-        {
-            return l.Any();
-        }
-        private static bool IsNotEmpty1(List<string> l)
+        public bool Count_not_zero(IEnumerable<string> l)
         {
             return l.Count() != 0; // Noncompliant
         }
-        private static bool IsNotEmpty2(List<string> l)
+        public bool Zero_less_then_count(IEnumerable<string> l)
+        {
+            return 0 < l.Count(); // Noncompliant
+        }
+        public bool One_less_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return 1 <= l.Count(); // Noncompliant
+        }
+        public bool Zero_not_count(IEnumerable<string> l)
         {
             return 0 != l.Count(); // Noncompliant
         }
-        private static bool IsEmpty1(List<string> l)
+    }
+    public class Extenion_method_for_empty
+    {
+        public bool Count_equals_zero(IEnumerable<string> l)
         {
             return l.Count() == 0; // Noncompliant
         }
-        private static bool IsEmpty2(List<string> l)
-        {
-            return l.Count() <= 0; // Noncompliant
-        }
-        private static bool IsEmpty2b(List<string> l)
-        {
-            return 0 >= l.Count(); // Noncompliant
-        }
-        private static bool IsEmpty3(List<string> l)
-        {
-            return !l.Any();
-        }
-        private static bool IsEmpty4(List<string> l)
+        public bool Count_less_then_one(IEnumerable<string> l)
         {
             return l.Count() < 1; // Noncompliant
         }
-        private static bool IsEmpty4b(List<string> l)
+        public bool Count_less_then_or_equal_to_zero(IEnumerable<string> l)
+        {
+            return l.Count() <= 0; // Noncompliant
+        }
+        public bool Zero_equals_count(IEnumerable<string> l)
+        {
+            return 0 == l.Count(); // Noncompliant
+        }
+        public bool One_greater_then_count(IEnumerable<string> l)
         {
             return 1 > l.Count(); // Noncompliant
         }
-        private static bool HasContentWithCondition(List<int> numbers)
+        public bool Zero_greater_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return 0 >= l.Count(); // Noncompliant
+        }
+    }
+    public class Count_with_condition_specified
+    {
+        public bool Any_for_condition(List<int> numbers)
         {
             return numbers.Count(n => n % 2 == 0) > 0; // Noncompliant
         }
-        private static bool IsEmptyWithCondition(List<int> numbers)
+        public bool Empty_for_condition(List<int> numbers)
         {
             return numbers.Count(n => n % 2 == 0) == 0; // Noncompliant
+        }
+    }
+    public class Enumerable_count_for_any
+    {
+        public bool Count_greater_then_0(List<string> l)
+        {
+            return Enumerable.Count(l) > 0; // Noncompliant
+            //                ^^^^^
+        }
+        public bool Zero_less_then_Count(List<string> l)
+        {
+            return 0 < Enumerable.Count(l); // Noncompliant
+        }
+    }
+    public class Alternative_one_constants
+    {
+        public bool Count_greater_then_or_equal_binary_1(IEnumerable<string> l)
+        {
+            return l.Count() >= 0b1; // Noncompliant
+        }
+        public bool Count_greater_then_or_equal_heximal_1(IEnumerable<string> l)
+        {
+            return l.Count() >= 0x1; // Noncompliant
+        }
+        public bool UInt64_one_less_then_or_equal_to_count(IEnumerable<string> l)
+        {
+            return 1UL <= l.Count(); // Noncompliant // Error [CS0034]
+        }
+    }
+    public class Local_count_method
+    {
+        public int Count(IEnumerable<string> l) => 3;
+        public bool Count_equals_zero(IEnumerable<string> l)
+        {
+            return Count(l) == 0; // Compliant
+        }
+    }
+    public class Count_for_multiple
+    {
+        public bool Count_more_then_one(IEnumerable<string> l)
+        {
+            return l.Count() > 1; // Compliant
         }
     }
 }
