4ffa501edf7563dcd11e6a95fb513e0ea4b0421d
Corniel Nobel <mail@corniel.nl>
2021-01-19 14:32:34 +0100

    Update analyzers/src/SonarAnalyzer.Common/Rules/UseArrayEmptyBase.cs
    Co-authored-by: Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>

--- analyzers/src/SonarAnalyzer.Common/Rules/UseArrayEmptyBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/UseArrayEmptyBase.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * SonarAnalyzer for .NET
- * Copyright (C) 2015-2020 SonarSource SA
+ * Copyright (C) 2015-2021 SonarSource SA
  * mailto: contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -75,4 +75,3 @@ namespace SonarAnalyzer.Rules
         protected abstract bool IsEmptyCreation(TCreation creationNode);
     }
 }
-
