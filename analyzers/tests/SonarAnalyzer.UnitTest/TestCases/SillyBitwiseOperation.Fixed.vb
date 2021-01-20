f5f0f3569baf4cb1301fe6a9cef59a7ab7df2c6c
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-01-20 13:56:40 +0100

    Prepare UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyBitwiseOperation.Fixed.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyBitwiseOperation.Fixed.vb
@@ -1,13 +1,41 @@
-Imports System
+﻿Class SillyBitwiseOperation
 
-Namespace Tests.TestCases
+    Public Sub Method()
+        Dim Result As Integer
+        Dim BitMask As Integer = &H10F
 
-    Class Program
+        Result = BitMask ' Fixed
+        Result = BitMask   ' Fixed
+        Result = BitMask  ' Fixed
+        Result = BitMask  ' Fixed {{Remove this silly bit operation.}}
 
-        Public Sub Test()
+        Result = BitMask And 1  ' Compliant
+        Result = BitMask Or 1   ' compliant
+        Result = BitMask Xor 1  ' Compliant
 
-        End Sub
+        Dim BitMaskLong As Long = &H10F
+        Dim ResultLong As Long
+        ResultLong = BitMaskLong ' Fixed
+        ResultLong = BitMaskLong And 0L     ' Compliant
+        ResultLong = BitMaskLong     ' Fixed
+        ResultLong = BitMaskLong    ' Fixed
+        ResultLong = BitMaskLong And ReturnLong() ' Compliant
+        ResultLong = BitMaskLong And &HF   ' Compliant
 
-    End Class
+        Dim ResultULong As ULong = 1UL     ' Fixed
+        ResultULong = 1UL Or 18446744073709551615UL ' Compliant
 
-End Namespace
+        MyMethod(1UL) ' Fixed
+
+        If True AndAlso True Then Return
+        If True OrElse True Then Return
+    End Sub
+
+    Private Shared Function ReturnLong() As Long
+        Return 1L
+    End Function
+
+    Private Shared Sub MyMethod(U As UInt64)
+    End Sub
+
+End Class
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyBitwiseOperation.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyBitwiseOperation.vb
@@ -1,13 +1,43 @@
-Imports System
+﻿Class SillyBitwiseOperation
 
-Namespace Tests.TestCases
+    Public Sub Method()
+        Dim Result As Integer
+        Dim BitMask As Integer = &H10F
 
-    Class Program
+        Result = -1 And BitMask ' Noncompliant
+        '        ^^^^^^
+        Result = BitMask Or 0   ' Noncompliant
+        '                ^^^^
+        Result = BitMask Xor 0  ' Noncompliant
+        Result = BitMask Xor 0  ' Noncompliant {{Remove this silly bit operation.}}
 
-        Public Sub Test()
+        Result = BitMask And 1  ' Compliant
+        Result = BitMask Or 1   ' compliant
+        Result = BitMask Xor 1  ' Compliant
 
-        End Sub
+        Dim BitMaskLong As Long = &H10F
+        Dim ResultLong As Long
+        ResultLong = BitMaskLong And ---+1L ' Noncompliant
+        ResultLong = BitMaskLong And 0L     ' Compliant
+        ResultLong = BitMaskLong Or 0UL     ' Noncompliant
+        ResultLong = BitMaskLong Or &H0L    ' Noncompliant
+        ResultLong = BitMaskLong And ReturnLong() ' Compliant
+        ResultLong = BitMaskLong And &HF   ' Compliant
 
-    End Class
+        Dim ResultULong As ULong = 1UL Or &H0UL     ' Noncompliant
+        ResultULong = 1UL Or 18446744073709551615UL ' Compliant
 
-End Namespace
+        MyMethod(1UL Or &H0UL) ' Noncompliant
+
+        If True AndAlso True Then Return
+        If True OrElse True Then Return
+    End Sub
+
+    Private Shared Function ReturnLong() As Long
+        Return 1L
+    End Function
+
+    Private Shared Sub MyMethod(U As UInt64)
+    End Sub
+
+End Class
