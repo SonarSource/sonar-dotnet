6edd3d9538b208a9e2b36930fa4f8cee89676075
Corniel Nobel <mail@corniel.nl>
2021-01-20 20:09:44 +0100

    Different naming.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseReturnStatement.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseReturnStatement.vb
@@ -1,82 +1,82 @@
-﻿Public Class Implicit_return_statements_are_noncompliant
-    Public Function Assigned_return_value_only() As Integer
-        Assigned_return_value_only = 42 ' Noncompliant ^9#26
+﻿Public Class ImplicitReturnStatementsAreNoncompliant
+    Public Function AssignedReturnValueOnly() As Integer
+        AssignedReturnValueOnly = 42 ' Noncompliant ^9#23
     End Function
 
-    Public Function explictly_return_default_return_value() As Integer
-        explictly_return_default_return_value = 42  ' Noncompliant {{Use a 'Return' statement; assigning returned values to function names is obsolete.}}
-        Return explictly_return_default_return_value ' Noncompliant {{Do not make use of the implicit return value.}}
+    Public Function ExplictlyReturnDefaultReturnValue() As Integer
+        ExplictlyReturnDefaultReturnValue = 42  ' Noncompliant {{Use a 'Return' statement; assigning returned values to function names is obsolete.}}
+        Return ExplictlyReturnDefaultReturnValue ' Noncompliant {{Do not make use of the implicit return value.}}
     End Function
 
-    Public Function case_insensitive() As Integer
-        CASE_INSENSITIVE = 42  ' Noncompliant
+    Public Function CaseInsensitive() As Integer
+        CASEINSENSITIVE = 42  ' Noncompliant
     End Function
 
-    Public Shared Function Static_function() As Integer
-        Static_function = 42  ' Noncompliant
+    Public Shared Function SharedFunction() As Integer
+        SharedFunction = 42  ' Noncompliant
     End Function
 	
-	Public Function read_return_value_only() As Integer
-		dim value = read_return_value_only ' Noncompliant {{Do not make use of the implicit return value.}}
+	Public Function ReadReturnValueOnly() As Integer
+		dim value = ReadReturnValueOnly ' Noncompliant {{Do not make use of the implicit return value.}}
 		return value
 	End Function
 
 End Class
 
-Public Class Does_not_apply_on
-    Public Function function_with_explict_return_only()
+Public Class DoesNotApplyOn
+    Public Function FunctionWithExplictReturnOnly()
         Return 42 'Compliant
     End Function
 
-    Public Sub sub_methods(number As Integer)
-        Dim Function_name = number ' Compliant
+    Public Sub SubMethods(number As Integer)
+        Dim SubMethods = number ' Compliant
     End Sub
 
-    Public Function recursive_function_calls(number As Integer)
+    Public Function RecursiveFunctionCalls(number As Integer)
         If number = 42 Then
             Return 42
         Else
-            Return recursive_function_calls(42) ' Compliant, method call
+            Return RecursiveFunctionCalls(42) ' Compliant, method call
         End If
     End Function
 	
-	Public Function call_to_other_property(other As OtherType) As Integer
+	Public Function CallToOtherProperty(other As OtherType) As Integer
         With other
-            Return .call_to_other_property ' Compliant
+            Return .CallToOtherProperty ' Compliant
         End With
     End Function
 
-    Public Function call_to_other_function(other As OtherType) As Integer
+    Public Function CallToOtherFunction(other As OtherType) As Integer
         With other
-            Return .call_to_other_function() ' Compliant
+            Return .CallToOtherFunction() ' Compliant
         End With
     End Function
 
-    Public Function write_to_other_property() As OtherType
+    Public Function WriteToOtherProperty() As OtherType
         Return New OtherType With
         {
-            .write_to_other_property = 69 ' Compliant
+            .WriteToOtherProperty = 69 ' Compliant
         }
     End Function
 	
-	Public Function write_to_other_function(other As OtherType) As Integer
+	Public Function WriteToOtherFunction(other As OtherType) As Integer
         With other
-            Return .write_to_other_function(42)
+            Return .WriteToOtherFunction(42)
         End With
     End Function
 	
 End Class
 
 Public Class OtherType
-    Public Property call_to_other_property As Integer
+    Public Property CallToOtherProperty As Integer
 	
-	Public Property write_to_other_property As Integer
+	Public Property WriteToOtherProperty As Integer
 
-    Public Function call_to_other_function() As Integer
+    Public Function CallToOtherFunction() As Integer
         Return 42
     End Function
 	
-	Public Function write_to_other_function(number as Integer) As Integer
+	Public Function WriteToOtherFunction(number as Integer) As Integer
         Return number
     End Function
 End Class
