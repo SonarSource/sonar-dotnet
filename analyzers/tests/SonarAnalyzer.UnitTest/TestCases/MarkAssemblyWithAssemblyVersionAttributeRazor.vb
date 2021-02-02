4f9ea8fec128fa5aaf7fedb801d99d218c8f7fba
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-02-02 18:38:19 +0100

    Implement

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MarkAssemblyWithAssemblyVersionAttributeRazor.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Reflection
+Imports Microsoft.AspNetCore.Razor.Hosting
+
+<Assembly: AssemblyCompany("SonarSource")>
+' https://github.com/SonarSource/sonar-dotnet/issues/2921
+' We should not raise on Razor ProjectName.Views.dll auto-generated assemblies
+<Assembly: RazorCompiledItem(GetType(Sample), "mvc.1.0.view", "@/Views/Fake/Index.cshtml")>
+
+Public Class Sample
+
+End Class
