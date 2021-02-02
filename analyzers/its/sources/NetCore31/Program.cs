494e1cc5f84409f6d7d2053d7906351a6fa6b119
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-02-02 18:46:17 +0100

    Analyze generated to have expected ITs

--- analyzers/its/sources/NetCore31/Program.cs
+++ analyzers/its/sources/NetCore31/Program.cs
@@ -6,15 +6,7 @@ namespace NetCore31
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) =>
             Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>()).Build().Run();
-
-            Console.WriteLine("Hello World!");
-            if (args[0] != null)
-            {
-                Console.WriteLine(args[0].ToString());
-            }
-        }
     }
 }
