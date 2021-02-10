c9bae573f53b94accf75135cf46aff48c3d98920
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-02-10 15:53:04 +0100

    New IT project to test configuration of rules

---
+++ analyzers/its/sources/NetCore31WithConfigurableRules/Controllers/MyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore31WithConfigurableRules.Controllers
+{
+    public class MyController : Controller
+    {
+        [HttpPost]
+        [DisableRequestSizeLimit]
+        public ActionResult PostRequestWithNoLimit() =>
+            null;
+
+        [HttpPost]
+        [RequestSizeLimit(17)]
+        public ActionResult PostRequestAboveLimit() =>
+            null;
+
+        [HttpPost]
+        [RequestSizeLimit(15)]
+        public ActionResult PostRequestBelowLimit() =>
+            null;
+
+        [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit = 24)]
+        public ActionResult MultipartFormRequestAboveLimit() =>
+            null;
+
+        [HttpPost]
+        [RequestFormLimits(MultipartBodyLengthLimit = 22)]
+        public ActionResult MultipartFormRequestBelowLimit() =>
+            null;
+    }
+}
