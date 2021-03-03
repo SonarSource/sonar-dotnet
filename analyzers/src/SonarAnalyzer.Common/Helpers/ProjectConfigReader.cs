b4c170d26e9882b77a4beb0daa233a90556c75ab
Andrei Epure <andrei.epure@sonarsource.com>
2021-03-03 19:30:58 +0100

    Add ProjectConfigReader

---
+++ analyzers/src/SonarAnalyzer.Common/Helpers/ProjectConfigReader.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SonarAnalyzer.Helpers
+{
+    /// <summary>
+    /// This class reads and encapsulates <see cref="ProjectConfig"/>, exposing only the configuration our analyzers need.
+    /// </summary>
+    internal class ProjectConfigReader
+    {
+        private readonly Lazy<ProjectConfig> projectConfig;
+
+        public string FilesToAnalyzePath => ProjectConfigValue?.FilesToAnalyzePath;
+
+        private ProjectConfig ProjectConfigValue => projectConfig.Value;
+
+        public ProjectConfigReader(AnalyzerOptions options)
+        {
+            projectConfig = new Lazy<ProjectConfig>(() => ReadContent(options));
+        }
+
+        private static ProjectConfig ReadContent(AnalyzerOptions options)
+        {
+            var sonarProjectConfig = options.AdditionalFiles.FirstOrDefault(IsSonarProjectConfig);
+            if (sonarProjectConfig == null || !File.Exists(sonarProjectConfig.Path))
+            {
+                return null;
+            }
+            try
+            {
+                var ser = new XmlSerializer(typeof(ProjectConfig));
+                using var fs = File.Open(sonarProjectConfig.Path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                return (ProjectConfig)ser.Deserialize(fs);
+            }
+            catch (Exception ex) when (ex is IOException || ex is XmlException || ex is InvalidOperationException)
+            {
+                // cannot log exception
+                return null;
+            }
+        }
+
+        private static bool IsSonarProjectConfig(AdditionalText additionalText) =>
+            new FileInfo(additionalText.Path).Name.Equals("SonarProjectConfig.xml", StringComparison.OrdinalIgnoreCase);
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Helpers/ProjectConfigReaderTests.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Collections.Immutable;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SonarAnalyzer.Helpers;
+
+namespace SonarAnalyzer.UnitTest.Helpers
+{
+    [TestClass]
+
+    public class ProjectConfigReaderTests
+    {
+        [DataTestMethod]
+        [DataRow("Valid_Product_WindowsPaths", @"c:\foo\bar\.sonarqube\conf\0\FilesToAnalyze.txt")]
+        [DataRow("Valid_Test_UnixPaths", @"/home/user/.sonarqube/conf/0/FilesToAnalyze.txt")]
+        public void WhenProjectConfigIsValid_FilesToAnalyzePath_ReturnsCorrectValue(string folder, string expectedFileToAnalyzePath)
+        {
+            var options = CreateOptions($"ResourceTests\\SonarProjectConfig\\{folder}\\SonarProjectConfig.xml");
+
+            var sut = new ProjectConfigReader(options);
+
+            sut.FilesToAnalyzePath.Should().Be(expectedFileToAnalyzePath);
+        }
+
+        [DataTestMethod]
+        [DataRow("Invalid_MissingAnalysisConfigPath")]
+        [DataRow("Invalid_MissingOutPath")]
+        [DataRow("Invalid_MissingProjectPath")]
+        [DataRow("Invalid_MissingProjectType")]
+        [DataRow("Invalid_MissingTargetFramework")]
+
+        public void WhenProjectConfigHasMissingValues_FilesToAnalyzePath_ReturnsCorrectValue(string folder)
+        {
+            var options = CreateOptions($"ResourceTests\\SonarProjectConfig\\{folder}\\SonarProjectConfig.xml");
+
+            var sut = new ProjectConfigReader(options);
+
+            sut.FilesToAnalyzePath.Should().Be(@"c:\foo\bar\.sonarqube\conf\0\FilesToAnalyze.txt");
+        }
+
+        [TestMethod]
+        public void WhenProjectConfigHasMixedSeparators_FilesToAnalyzePath_ReturnsValues()
+        {
+            var options = CreateOptions($"ResourceTests\\SonarProjectConfig\\Invalid_MixedSeparators\\SonarProjectConfig.xml");
+
+            var sut = new ProjectConfigReader(options);
+
+            sut.FilesToAnalyzePath.Should().Be(@"c:\foo\bar\.sonarqube\conf/0/FilesToAnalyze.txt");
+        }
+
+        [DataTestMethod]
+        [DataRow("Invalid_DifferentClassName")]
+        [DataRow("Invalid_DifferentNamespace")]
+        [DataRow("Invalid_WrongProjectTypeValue")]
+        [DataRow("Invalid_Xml")]
+        [DataRow(Constants.NuGetLatestVersion)]
+        public void WhenProjectConfigIsValid_FilesToReturnPath_ReturnNull(string folder)
+        {
+            var options = CreateOptions($"ResourceTests\\SonarProjectConfig\\{folder}\\SonarProjectConfig.xml");
+
+            var sut = new ProjectConfigReader(options);
+
+            sut.FilesToAnalyzePath.Should().BeNull();
+        }
+
+        // FIXME duplicated with ParameterLoaderTests.cs
+        private static AnalyzerOptions CreateOptions(string relativePath)
+        {
+            var additionalText = new Mock<AdditionalText>();
+            additionalText.Setup(x => x.Path).Returns(relativePath);
+            return new AnalyzerOptions(ImmutableArray.Create(additionalText.Object));
+        }
+    }
+}
