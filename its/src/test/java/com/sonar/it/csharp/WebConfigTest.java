81d260be0d8e8837bce48f129213abc008d85cfe
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-03-11 15:42:47 +0100

    Swap expected

--- its/src/test/java/com/sonar/it/csharp/WebConfigTest.java
+++ its/src/test/java/com/sonar/it/csharp/WebConfigTest.java
@@ -45,7 +45,7 @@ public class WebConfigTest extends WebConfigBase {
     List<Hotspot> hotspots = Tests.getHotspots(projectName);
     // One from project directory, one from PathOutsideProjectRoot added with Directory.Build.props
     assertThat(hotspots.size()).isEqualTo(2);
-    assertHotspot(hotspots.get(0), 4, "WebConfig.CSharp\\Web.config");
-    assertHotspot(hotspots.get(1), 6, "PathOutsideProjectRoot\\Web.config");
+    assertHotspot(hotspots.get(0), 6, "PathOutsideProjectRoot\\Web.config");
+    assertHotspot(hotspots.get(1), 4, "WebConfig.CSharp\\Web.config");
   }
 }
