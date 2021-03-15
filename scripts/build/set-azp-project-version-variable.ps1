82edf655d017cc6cc90c31f61da9d53884d5bddc
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-03-15 16:05:59 +0100

    Address code review comments

--- scripts/build/set-azp-project-version-variable.ps1
+++ scripts/build/set-azp-project-version-variable.ps1
@@ -1,8 +1,8 @@
 # Read the project version from the Version.props file
-$versionFilePath = "$env:BUILD_SOURCESDIRECTORY\scripts\version\Version.props"
-Write-Host "Reading the Sonar project version from '${versionFilePath}' ..."
+$version = "$env:BUILD_SOURCESDIRECTORY\scripts\version\Version.props"
+Write-Host "Reading the Sonar project version from '${version}' ..."
 
-[xml]$versionProps = Get-Content "$versionFilePath"
+[xml]$versionProps = Get-Content "$version"
 $sonarProjectVersion = $versionProps.Project.PropertyGroup.MainVersion
 Write-Host "Sonar project version is '${sonarProjectVersion}'"
 
