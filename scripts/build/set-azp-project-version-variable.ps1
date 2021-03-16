b555ff6cdd0716dfe5bcc0718b1a606b2af63f8b
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-03-16 09:29:12 +0100

    Rename sonarProjectVersion to version

--- scripts/build/set-azp-project-version-variable.ps1
+++ scripts/build/set-azp-project-version-variable.ps1
@@ -1,10 +1,10 @@
 # Read the project version from the Version.props file
-$version = "$env:BUILD_SOURCESDIRECTORY\scripts\version\Version.props"
-Write-Host "Reading the Sonar project version from '${version}' ..."
+$versionFilePath = "$env:BUILD_SOURCESDIRECTORY\scripts\version\Version.props"
+Write-Host "Reading the Sonar project version from '${versionFilePath}' ..."
 
-[xml]$versionProps = Get-Content "$version"
-$sonarProjectVersion = $versionProps.Project.PropertyGroup.MainVersion
-Write-Host "Sonar project version is '${sonarProjectVersion}'"
+[xml]$versionProps = Get-Content "$versionFilePath"
+$version = $versionProps.Project.PropertyGroup.MainVersion
+Write-Host "Sonar project version is '${version}'"
 
 # Set the azure build variable (SONAR_PROJECT_VERSION) so it can be used by other tasks
-Write-Host "##vso[task.setvariable variable=SONAR_PROJECT_VERSION;]$sonarProjectVersion"
+Write-Host "##vso[task.setvariable variable=SONAR_PROJECT_VERSION;]$version"
