781349750ac7fea676a081025382d84b467a3a8f
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-03-22 15:43:57 +0100

    Improve comments

--- analyzers/src/SonarAnalyzer.Common/Helpers/ProjectTypeHelper.cs
+++ analyzers/src/SonarAnalyzer.Common/Helpers/ProjectTypeHelper.cs
@@ -27,8 +27,8 @@ namespace SonarAnalyzer.Helpers
 {
     internal static class ProjectTypeHelper
     {
-        // This list is duplicated in sonar-scanner-msbuild and sonar-security and should be synchronized.
-        public /* for testring */ static readonly ISet<string> TestAssemblyNames = new HashSet<string>
+        // This list is duplicated in sonar-scanner-msbuild and sonar-security and should be manually synchronized after each change.
+        public /* for testing */ static readonly ISet<string> TestAssemblyNames = new HashSet<string>
         {
             "DOTMEMORY.UNIT",
             "MICROSOFT.VISUALSTUDIO.TESTPLATFORM.TESTFRAMEWORK",
