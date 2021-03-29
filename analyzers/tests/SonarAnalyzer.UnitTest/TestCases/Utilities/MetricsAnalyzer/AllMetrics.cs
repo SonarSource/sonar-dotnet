8c36603e1d66384a7f3847d9f6e3841f97b9d62c
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-03-29 13:57:43 +0200

    Cover MetricsAnalyzer

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Utilities/MetricsAnalyzer/AllMetrics.cs
@@ -0,0 +1,21 @@
+﻿// Header comment
+
+using System;
+
+namespace Tests
+{
+    public class Program
+    {
+        private int noSonar; // NOSONAR
+        private int field;
+
+        public int Go(bool condition)
+        {
+            if (condition)
+            {
+                return 42;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
