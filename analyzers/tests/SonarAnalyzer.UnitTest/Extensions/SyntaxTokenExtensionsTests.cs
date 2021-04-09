7d8bc81ee544e9982c8462cd53c7db9e87947531
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-04-09 16:57:55 +0200

    Add unit tests

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/SyntaxTokenExtensionsTests.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+extern alias csharp;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static csharp::SonarAnalyzer.Extensions.SyntaxTokenExtensions;
+
+namespace SonarAnalyzer.UnitTest.Extensions
+{
+    [TestClass]
+    public class SyntaxTokenExtensionsTests
+    {
+        [TestMethod]
+        public void GivenEmptyToken_GetBindableParent_ReturnsNull()
+        {
+            SyntaxToken empty = default;
+
+            Assert.IsNull(empty.GetBindableParent());
+        }
+
+        [TestMethod]
+        public void GivenInterpolatedStringTextToken_GetBindableParent_ReturnsInterpolatedStringExpression()
+        {
+            const string code = @"
+using System;
+
+namespace TestCases
+{
+    class Foo
+    {
+        string x = $""a"";
+    }
+}";
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var aToken = syntaxTree.GetRoot().DescendantTokens().First(token => token.IsKind(SyntaxKind.InterpolatedStringTextToken));
+
+            var parent = aToken.GetBindableParent();
+            parent.Kind().Should().Be(SyntaxKind.InterpolatedStringExpression);
+        }
+    }
+}
