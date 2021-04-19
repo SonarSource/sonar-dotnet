a6044d9f0b7ae4cd65dfdb6ddabca73328ed17dd
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-04-19 15:55:20 +0200

    Add ITs

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues/NetFramework48/PermissiveCors.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Microsoft.Net.Http.Headers;
+
+namespace IntentionalFindings
+{
+    public class PermissiveCors : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*", exposedHeaders: "X-Custom-Header")] // Noncompliant (S5122) {{Make sure this permissive CORS policy is safe here.}}
+        public string Get()
+        {
+            var response = HttpContext.Current.Response;
+
+            response.Headers.Add("Access-Control-Allow-Origin", "*"); // Noncompliant
+            response.Headers.Add(HeaderNames.AccessControlAllowOrigin, "*"); // Noncompliant
+            response.AppendHeader(HeaderNames.AccessControlAllowOrigin, "*"); // Noncompliant
+
+            return string.Empty;
+        }
+    }
+}
---
+++ analyzers/its/sources/NetCore31/Controllers/S5122Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCore31.Controllers
+{
+    public class S5122Controller : Controller
+    {
+        public void PermissiveHeader()
+        {
+            Response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
