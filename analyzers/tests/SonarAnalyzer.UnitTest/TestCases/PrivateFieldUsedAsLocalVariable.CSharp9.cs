814183ce3e56cd1eedeecb258c61d43920e0d51b
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-05-03 18:45:32 +0200

    Records support

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PrivateFieldUsedAsLocalVariable.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PrivateFieldUsedAsLocalVariable.CSharp9.cs
@@ -1,12 +1,13 @@
 ﻿record Record
 {
     private int privateUnused = 0; // Compliant - unused
-    private int privateUsed = 0; // Compliant - FN
+    private int privateUsed = 0; // Noncompliant
     private protected int privateProtected = 0; // Compliant
     internal int @internal = 0; // Compliant
     protected internal int protectedInternal = 0; // Compliant
     protected int @protected = 0; // Compliant
     public int @public = 0; // Compliant - Public
+    private int usedInWith = 0; // Noncompliant
 
     void Foo()
     {
@@ -17,6 +18,9 @@
         @protected = 42;
         @public = 42;
         Bar(privateUsed);
+        var zero = new Record();
+        usedInWith = 4;
+        var answer = zero with { @public = usedInWith };
     }
 
     void Bar(int i) { }
@@ -24,7 +28,7 @@
     private int fieldInPropertyCompliant = 0;
     public int P1 { get { return fieldInPropertyCompliant; } }
 
-    private int fieldInPropertyNoncompliant = 0; // Compliant - FN
+    private int fieldInPropertyNoncompliant = 0; // Noncompliant
     public int P2 { get { fieldInPropertyNoncompliant = 42; return fieldInPropertyNoncompliant; } }
 
     private int fieldInPropertyWithInitCompliant = 0;
