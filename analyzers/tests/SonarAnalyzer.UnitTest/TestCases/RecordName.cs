63ccc0784c5d69e4c8b9bc343bf6d609e823ed24
DGolovinov <64070475+dmitry-golovinov-sonarsource@users.noreply.github.com>
2021-05-06 14:22:54 +0200

    Rules S100, S101: Add support for records (#4376)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RecordName.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RecordName.cs
@@ -1,22 +1,34 @@
 ﻿namespace t1
 {
-    record FSM // FN
+    record FSM // Noncompliant {{Rename record 'FSM' to match pascal case naming rules, consider using 'Fsm'.}}
     {
     }
+    record FSM2(string Param); // Noncompliant
 }
 namespace t4
 {
     record AbcDEFgh { } // Compliant
     record Ab4DEFgh { } // Compliant
-    record Ab4DEFGh { } // FN
+    record Ab4DEFGh { } // Noncompliant
 
-    record _AbABaa { } // FN
+    record _AbABaa { }  // Noncompliant
 
-    record 你好 { } // Compliant
+    record 你好 { }      // Compliant
+
+    record AbcDEFgh2(string Param); // Compliant
+    record Ab4DEFgh2(string Param); // Compliant
+    record Ab4DEFGh2(string Param); // Noncompliant
+
+    record _AbABaa2(string Param);  // Noncompliant
+
+    record 你好2(string Param);      // Compliant
 }
 
 namespace TestSuffixes
 {
-    record IEnumerableExtensionsTest { } // FN, records are not test classes
-    record IEnumerableExtensionsTests { } // FN, records are not test classes
+    record IEnumerableExtensionsTest { }              // Noncompliant  {{Rename record 'IEnumerableExtensionsTest' to match pascal case naming rules, consider using 'EnumerableExtensionsTest'.}}
+    record IEnumerableExtensionsTests { }             // Noncompliant
+
+    record IEnumerableExtensionsTest2(string Param);  // Noncompliant
+    record IEnumerableExtensionsTests2(string Param); // Noncompliant
 }
