8cd560f5a3a5fa74641eae8f500af7631386552e
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2021-05-07 16:03:00 +0200

    Rule S4002: add support for records (#4391)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableTypesNeedFinalizers.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableTypesNeedFinalizers.CSharp9.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-public record Foo : IDisposable // FN
+public record Foo : IDisposable // Noncompliant {{Implement a finalizer that calls your 'Dispose' method.}}
 {
     private IntPtr myResource;
     private bool disposed = false;
@@ -32,3 +32,30 @@ public record Bar : IDisposable // Compliant
     }
 }
 
+public record RecordWithParamsNoFinalizer(string X) : IDisposable // Noncompliant {{Implement a finalizer that calls your 'Dispose' method.}}
+{
+    private IntPtr myResource;
+    private bool disposed = false;
+
+    protected virtual void Dispose(bool disposing) { }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+}
+
+public record RecordWithParamsWithFinalizer(string X) : IDisposable // Compliant
+{
+    protected virtual void Dispose(bool disposing) { }
+
+    public void Dispose()
+    {
+        Dispose(true);
+    }
+
+    ~RecordWithParamsWithFinalizer()
+    {
+        Dispose(false);
+    }
+}
