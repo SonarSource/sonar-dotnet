722fe64cb5a88adca6b7eac74f69fd986d2b27ea
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-05-11 17:04:35 +0200

    Add test case

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PropertyWriteOnly.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PropertyWriteOnly.cs
@@ -22,6 +22,29 @@
             }
         }
     }
+
+    public struct PropertyWriteOnlyStruct
+    {
+        public int Foo  //Noncompliant {{Provide a getter for 'Foo' or replace the property with a 'SetFoo' method.}}
+//                 ^^^
+        {
+            set
+            {
+                // ... some code ...
+            }
+        }
+        public int Foo2
+        {
+            get
+            {
+                return 1;
+            }
+            set
+            {
+                // ... some code ...
+            }
+        }
+    }
 }
 
 namespace ReproIssue2390
