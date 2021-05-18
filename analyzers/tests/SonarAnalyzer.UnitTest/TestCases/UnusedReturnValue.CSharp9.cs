cf34e78597d55da29af43932803bc35d9a17a1c4
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-05-18 12:34:30 +0200

    Code cleanup and records test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedReturnValue.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedReturnValue.CSharp9.cs
@@ -12,16 +12,47 @@ var o = SomeGenericMethod2<object>();
 T SomeGenericMethod2<T>() where T : class { return null; }
 T SomeGenericMethod<T>() where T : class { return null; } // Compliant
 
-new R().MyMethod();
-i = new R().MyMethod2();
+new Record().MyMethod();
+i = new Record().MyMethod2();
+
+new PositionalRecord(42).MyMethod();
+i = new PositionalRecord(42).MyMethod2();
 
 int NeverUsed(int neverUsedVal) // FN - the returned value is not used
 {
     return neverUsedVal;
 }
 
-record R
+record Record
 {
     public int MyMethod() { return 42; } // Compliant
     public int MyMethod2() { return 42; }
+
+    public void Test()
+    {
+        SomeGenericMethod<object>();
+
+        SomeGenericMethod2<object>();
+        var o = SomeGenericMethod2<object>();
+    }
+
+    private T SomeGenericMethod<T>() where T : class { return null; } // Noncompliant
+    private T SomeGenericMethod2<T>() where T : class { return null; }
+}
+
+record PositionalRecord(int SomeProperty)
+{
+    public int MyMethod() { return 42; } // Compliant
+    public int MyMethod2() { return 42; }
+
+    public void Test()
+    {
+        SomeGenericMethod<object>();
+
+        SomeGenericMethod2<object>();
+        var o = SomeGenericMethod2<object>();
+    }
+
+    private T SomeGenericMethod<T>() where T : class { return null; } // Noncompliant
+    private T SomeGenericMethod2<T>() where T : class { return null; }
 }
