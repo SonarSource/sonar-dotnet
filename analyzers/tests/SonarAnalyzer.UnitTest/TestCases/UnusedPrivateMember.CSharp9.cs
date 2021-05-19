0d77329f8e104c0877dbed1b254b8a2951e2d4d0
DGolovinov <64070475+dmitry-golovinov-sonarsource@users.noreply.github.com>
2021-05-19 15:31:29 +0200

    Rule S1144: Add support for records (#4444)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp9.cs
@@ -1,79 +1,82 @@
 ﻿using System;
 
-int a = 0;
-int b = 1;
-Console.WriteLine(b);
-
-Product c = new("name", 1);
-Product d = new("name", 2);
-Console.WriteLine(d);
-
-public record Product(string Name, int CategoryId);
-
-public record Record
+namespace Tests.Diagnostics
 {
-    private int a; // Noncompliant {{Remove the unused private field 'a'.}}
-                   // Noncompliant@-1 - duplicate issue reported
-
-    private int b;
-    public int B() => b;
-
-    private nint Value { get; init; }
-    private nint UnusedValue { get; init; } // Noncompliant
-                                            // Noncompliant@-1 - duplicate issue reported
-
-    public Record Create() => new() { Value = 1 };
-
-    private interface IFoo // Noncompliant
-                           // Noncompliant@-1 - duplicate issue reported
+    public record Record
     {
-        public void Bar() { }
-    }
+        private int a; // Noncompliant {{Remove the unused private field 'a'.}}
 
-    private record Nested(string Name, int CategoryId);
+        private int b;
+        public int B() => b;
 
-    public void UseNested()
-    {
-        Nested d = new("name", 2);
-    }
+        private nint Value { get; init; }
 
-    private record Nested2(string Name, int CategoryId);
+        private nint UnusedValue { get; init; } // Noncompliant
 
-    public void UseNested2()
-    {
-        _ = new Nested2("name", 2);
-    }
+        public Record Create() => new() {Value = 1};
 
-    private int usedInPatternMatching = 1;
-
-    public int UseInPatternMatching(int val) =>
-        val switch
+        private interface IFoo // Noncompliant
         {
-            < 0 => usedInPatternMatching,
-            >= 0 => 1
-        };
+            public void Bar() { }
+        }
 
-    private class LocalFunctionAttribute : Attribute { } // Noncompliant - FP
-                                                         // Noncompliant@-1 - duplicate issue reported
+        private record Nested(string Name, int CategoryId);
 
-    public void Foo()
-    {
-        [LocalFunction]
-        static void Bar()
-        { }
+        public void UseNested()
+        {
+            Nested d = new("name", 2);
+        }
+
+        private record Nested2(string Name, int CategoryId);
+
+        public void UseNested2()
+        {
+            _ = new Nested2("name", 2);
+        }
+
+        private record UnusedNested1(string Name, int CategoryId); // Noncompliant
+        internal record UnusedNested2(string Name, int CategoryId); // Noncompliant
+        public record UnusedNested3(string Name, int CategoryId);
+
+        private int usedInPatternMatching = 1;
+
+        public int UseInPatternMatching(int val) =>
+            val switch
+            {
+                < 0 => usedInPatternMatching,
+                >= 0 => 1
+            };
+
+        private class LocalFunctionAttribute : Attribute { } // Noncompliant - FP
+
+        public void Foo()
+        {
+            [LocalFunction]
+            static void Bar()
+            {
+            }
+        }
     }
-}
 
-public class TargetTypedNew
-{
-    private TargetTypedNew(int arg) // Noncompliant FP
-                                    // Noncompliant@-1 - duplicate issue reported
+    public record PositionalRecord(string Value)
     {
-        var x = arg;
+        private int a; // Noncompliant
+        private int b;
+        public int B() => b;
+
+        private record UnusedNested(string Name, int CategoryId) { } // Noncompliant
     }
 
-    public static TargetTypedNew Create()
+    public class TargetTypedNew
     {
-        return new(42);
+        private TargetTypedNew(int arg) // Noncompliant FP
+        {
+            var x = arg;
+        }
+
+        public static TargetTypedNew Create()
+        {
+            return new(42);
+        }
     }
 }
