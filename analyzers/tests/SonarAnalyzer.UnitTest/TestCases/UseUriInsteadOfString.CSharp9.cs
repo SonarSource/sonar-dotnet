7b938c8d4f92d449973446a589e0e8398653e873
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-05-26 16:11:53 +0200

    Rule S4005: Add support for target typed new expressions

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseUriInsteadOfString.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseUriInsteadOfString.CSharp9.cs
@@ -1,15 +1,20 @@
-﻿S3994 p = new("www.sonarsource.com"); // Compliant - FN
+﻿using System;
+
+S3994 p = new("www.sonarsource.com"); // Noncompliant
+
+S3994 q = new("www.sonarsource.com", false); // Compliant
 
 string GetUrl(string url) => ""; // Compliant - FN
 
 public record S3994
 {
-    public S3994(string uri) { } // Noncompliant {{Either change this parameter type to 'System.Uri' or provide an overload which takes a 'System.Uri' parameter.}}
+    public S3994(string uri) { }
 
     public S3994(string uri, bool blah) { } // Noncompliant {{Either change this parameter type to 'System.Uri' or provide an overload which takes a 'System.Uri' parameter.}}
 
     public virtual string Url { get; set; } // Noncompliant {{Change this property type to 'System.Uri'.}}
 //                 ^^^^^^
+    public S3994(Uri uri) { } // Compliant
 }
 
 public record WithParams(string uri) // Noncompliant {{Change this property type to 'System.Uri'.}}
