93ac7443d60906631b7d174da4e2257c8d394255
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-05-26 11:48:15 +0200

    Fix locations in test

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/JwtSigned.CSharp9.cs
@@ -3,16 +3,16 @@
 const string secret = "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk";
 const string invalidToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb28iOiJmYWtlYmFyIiwiaWF0IjoxNTc1NjQ0NTc3fQ.pcX_7snpSGf01uBfaM8XPkbgdhs1gq9JcYRCQvZrJyk";
 
-JwtBuilder decoded1 = new(); // FN {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
-decoded1.WithSecret(secret).Decode(invalidToken);
+JwtBuilder decoded1 = new();
+decoded1.WithSecret(secret).Decode(invalidToken); // FN {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
 
 JwtBuilder decoded2 = new();
 decoded2.WithSecret(secret).MustVerifySignature().Decode(invalidToken); // Compliant
 
 void DecoderMethod()
 {
-    JwtBuilder decoded1 = new(); // Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
-    decoded1.WithSecret(secret).Decode(invalidToken);
+    JwtBuilder decoded1 = new();
+    decoded1.WithSecret(secret).Decode(invalidToken); // Noncompliant {{Use only strong cipher algorithms when verifying the signature of this JWT.}}
 
     JwtBuilder decoded2 = new();
     decoded2.WithSecret(secret).MustVerifySignature().Decode(invalidToken); // Compliant
