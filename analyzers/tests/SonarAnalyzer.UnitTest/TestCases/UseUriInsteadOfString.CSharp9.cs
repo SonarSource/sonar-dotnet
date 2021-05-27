2a758492c7df021bccd3f6a6cd3ac66a1e9a5f8f
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-05-27 14:11:21 +0200

    Preserving original test case

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseUriInsteadOfString.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseUriInsteadOfString.CSharp9.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 
-S3994 p = new("www.sonarsource.com"); // Noncompliant
+Foo p = new("www.sonarsource.com");        // Noncompliant
 
-S3994 q = new("www.sonarsource.com", false); // Compliant
+Foo q = new("www.sonarsource.com", false); // Compliant
 
-string GetUrl(string url) => ""; // Compliant - FN
+string GetUrl(string url) => "";           // Compliant - FN
+
+public record Foo
+{
+    public Foo(string uri) { }
+
+    public Foo(string uri, bool blah) { }   // Noncompliant {{Either change this parameter type to 'System.Uri' or provide an overload which takes a 'System.Uri' parameter.}}
+
+    public Foo(Uri uri) { }                 // Compliant
+}
 
 public record S3994
 {
-    public S3994(string uri) { }
+    public S3994(string uri) { }            // Noncompliant {{Either change this parameter type to 'System.Uri' or provide an overload which takes a 'System.Uri' parameter.}}
 
     public S3994(string uri, bool blah) { } // Noncompliant {{Either change this parameter type to 'System.Uri' or provide an overload which takes a 'System.Uri' parameter.}}
 
     public virtual string Url { get; set; } // Noncompliant {{Change this property type to 'System.Uri'.}}
 //                 ^^^^^^
-    public S3994(Uri uri) { } // Compliant
 }
 
 public record WithParams(string uri) // Noncompliant {{Change this property type to 'System.Uri'.}}
