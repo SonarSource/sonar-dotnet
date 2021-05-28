679911dc734adb1b4a49b90084becbecbe77d444
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-05-28 15:34:32 +0200

    Remove code smells

--- analyzers/src/SonarAnalyzer.CSharp/Rules/InsecureEncryptionAlgorithm.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/InsecureEncryptionAlgorithm.cs
@@ -25,7 +25,6 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.Common;
 using SonarAnalyzer.Helpers;
-using StyleCop.Analyzers.Lightup;
 
 namespace SonarAnalyzer.Rules.CSharp
 {
