1b1ef6b19a16d966a6b167775ffe027ae953e447
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-06-03 15:56:44 +0200

    Implement

--- analyzers/src/SonarAnalyzer.CSharp/Rules/CheckFileLicense.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/CheckFileLicense.cs
@@ -34,25 +34,14 @@ namespace SonarAnalyzer.Rules.CSharp
     public class CheckFileLicense : CheckFileLicenseBase
     {
         internal const string HeaderFormatDefaultValue = @"/*
- * SonarQube, open source software quality management tool.
- * Copyright (C) 2008-2013 SonarSource
- * mailto:contact AT sonarsource DOT com
+ * <Your-Product-Name>
+ * Copyright (c) <Year-From>-<Year-To> <Your-Company-Name>
  *
- * SonarQube is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * SonarQube is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Please configure this header in your SonarCloud/SonarQube quality profile.
+ * You can also set it in SonarLint.xml additional file for SonarLint or standalone NuGet analyzer.
  */
 ";
+        // FIXME: Refactor isEnabledByDefault is not consistent
         private static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager, isEnabledByDefault: false);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
