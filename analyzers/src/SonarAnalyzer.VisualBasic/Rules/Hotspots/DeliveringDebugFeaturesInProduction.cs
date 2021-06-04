1b4c999a02574889a4dd8b62f1beb352d19eba1b
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-06-04 15:39:54 +0200

    Change `is { }` with `is var`

--- analyzers/src/SonarAnalyzer.VisualBasic/Rules/Hotspots/DeliveringDebugFeaturesInProduction.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/Rules/Hotspots/DeliveringDebugFeaturesInProduction.cs
@@ -39,7 +39,7 @@ namespace SonarAnalyzer.Rules.VisualBasic
         internal /*for testing*/ DeliveringDebugFeaturesInProduction(IAnalyzerConfiguration configuration) : base(configuration) { }
 
         protected override bool IsInvokedConditionally(SyntaxNode node, SemanticModel semanticModel) =>
-            node.FirstAncestorOrSelf<StatementSyntax>() is { } invocationStatement
+            node.FirstAncestorOrSelf<StatementSyntax>() is var invocationStatement
             && invocationStatement.Ancestors().Any(x => IsDevelopmentCheck(x, semanticModel));
 
         protected override bool IsInDevelopmentContext(SyntaxNode node) =>
