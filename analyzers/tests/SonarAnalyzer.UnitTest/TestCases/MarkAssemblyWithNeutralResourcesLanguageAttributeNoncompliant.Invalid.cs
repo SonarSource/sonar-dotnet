aee56756b91df2c4020b55a8e7b981e1dae9d939
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-06-16 17:22:37 +0200

    It didn't worked, rule uses compiled version

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MarkAssemblyWithNeutralResourcesLanguageAttributeNoncompliant.Invalid.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MarkAssemblyWithNeutralResourcesLanguageAttributeNoncompliant.Invalid.cs
@@ -1,4 +1,3 @@
 ﻿// Noncompliant {{Provide a 'System.Resources.NeutralResourcesLanguageAttribute' attribute for assembly 'project0'.}}
 
 [assembly: System.Resources.NeutralResourcesLanguage("")]
-[assembly: System.Resources.NeutralResourcesLanguage(42)]   // Error [CS1503] Argument 1: cannot convert from 'int' to 'string'
