9fbc864e8aaf7045ba81cb130d3022080eac8b3d
Andrei Epure <andrei.epure@sonarsource.com>
2021-06-21 14:39:57 +0200

    Fix test

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ConditionEvaluatesToConstantTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ConditionEvaluatesToConstantTest.cs
@@ -74,7 +74,6 @@ namespace SonarAnalyzer.UnitTest.Rules
             Verifier.VerifyAnalyzerFromCSharp9Console(@"TestCases\ConditionEvaluatesToConstant.CSharp9.TopLevelStatements.cs", GetAnalyzer());
 #endif
 
-        // https://github.com/SonarSource/sonar-dotnet/issues/4573
         [TestMethod]
         [TestCategory("Rule")]
         public void ConditionEvaluatesToConstant_UncaughtException()
@@ -106,7 +105,12 @@ public class Reproducer
     public void Bar(string x) { }
 }", GetAnalyzer(), CompilationErrorBehavior.Ignore);
 
-            action.Should().Throw<AssertFailedException>().WithMessage("*AD0001*SymbolicExecutionException*TrySetObjectConstraint");
+            // https://github.com/SonarSource/sonar-dotnet/issues/4573
+            action.Should().Throw<AssertFailedException>().Where(e =>
+                e.Message.Contains("Expected diagnostics {error AD0001: Analyzer 'SonarAnalyzer.Rules.SymbolicExecution.SymbolicExecutionRunner' threw an exception of type " +
+                "'SonarAnalyzer.SymbolicExecution.SymbolicExecutionException' with message 'Error processing method: set_Foo ## Method file: snippet1.cs " +
+                "## Method line: 11,8 ## Inner exception: System.NotSupportedException: Unexpected constraint type: ObjectConstraint.Old constraints: NoValue " +
+                "##    at SonarAnalyzer.SymbolicExecution.SymbolicValue.TrySetObjectConstraint(ObjectConstraint constraint, SymbolicValueConstraints oldConstraints, ProgramState programState)"));
         }
 
         private static SonarDiagnosticAnalyzer GetAnalyzer() =>
