e005b05e1347f4f67c8379a8342b2041808ee146
Andrei Epure <38876598+andrei-epure-sonarsource@users.noreply.github.com>
2021-06-23 10:36:56 +0200

    Add repro for AD0001 for SE (#4574)

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ConditionEvaluatesToConstantTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ConditionEvaluatesToConstantTest.cs
@@ -18,7 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.Linq;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarAnalyzer.Helpers;
 using SonarAnalyzer.Rules.CSharp;
@@ -71,6 +73,45 @@ namespace SonarAnalyzer.UnitTest.Rules
         public void ConditionEvaluatesToConstant_FromCSharp9_TopLevelStatements() =>
             Verifier.VerifyAnalyzerFromCSharp9Console(@"TestCases\ConditionEvaluatesToConstant.CSharp9.TopLevelStatements.cs", GetAnalyzer());
 #endif
+
+        [TestMethod]
+        [TestCategory("Rule")]
+        public void ConditionEvaluatesToConstant_UncaughtException()
+        {
+            Action action = () => Verifier.VerifyCSharpAnalyzer(@"
+using System;
+public class Reproducer
+{
+    private DateTime? foo;
+
+    public virtual DateTime? Foo
+    {
+        get { return foo; }
+
+        set
+        {
+            if (value.HasValue)
+            {
+                value = DateTime.SpecifyKind(value.Value, DateTimeKind.Unspecified);
+            }
+
+            if (foo != value || (foo.HasValue && value.HasValue && foo.Value.Kind != value.Value.Kind))
+            {
+                foo = value;
+                Bar(""x"");
+            }
+        }
+    }
+    public void Bar(string x) { }
+}", GetAnalyzer(), CompilationErrorBehavior.Ignore);
+
+            // https://github.com/SonarSource/sonar-dotnet/issues/4573
+            action.Should().Throw<AssertFailedException>().Where(e =>
+                e.Message.Contains("Expected diagnostics {error AD0001: Analyzer 'SonarAnalyzer.Rules.SymbolicExecution.SymbolicExecutionRunner' threw an exception of type " +
+                "'SonarAnalyzer.SymbolicExecution.SymbolicExecutionException' with message 'Error processing method: set_Foo ## Method file: snippet1.cs " +
+                "## Method line: 11,8 ## Inner exception: System.NotSupportedException: Unexpected constraint type: ObjectConstraint.Old constraints: NoValue " +
+                "##    at SonarAnalyzer.SymbolicExecution.SymbolicValue.TrySetObjectConstraint(ObjectConstraint constraint, SymbolicValueConstraints oldConstraints, ProgramState programState)"));
+        }
 
         private static SonarDiagnosticAnalyzer GetAnalyzer() =>
             new SymbolicExecutionRunner(new ConditionEvaluatesToConstant());
