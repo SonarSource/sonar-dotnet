735e3d27770dcad3381b050e64e1f9fce0d90691
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-06-24 10:39:38 +0200

    Fix typo

--- scripts/build/set-azp-variables.ps1
+++ scripts/build/set-azp-variables.ps1
@@ -1,7 +1,7 @@
 # Read variables shared between stages and tasks
 $Dir = "$env:AGENT_BUILDDIRECTORY\azp-variables"
 
-$Version = Get-Conent "${Dir}\SONAR_PROJECT_VERSION"
+$Version = Get-Content "${Dir}\SONAR_PROJECT_VERSION"
 Write-Host "Sonar project version is '${Version}'"
 
 # Set the azure build variable (SONAR_PROJECT_VERSION) so it can be used by other tasks
