1ca8e5444a9069022cf709070416e6f60dad3eb0
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-06-29 12:43:56 +0200

    More cleanup

--- analyzers/src/SonarAnalyzer.CSharp/Helpers/CSharpEquivalenceChecker.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Helpers/CSharpEquivalenceChecker.cs
@@ -18,50 +18,36 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 
 namespace SonarAnalyzer.Helpers
 {
     internal static class CSharpEquivalenceChecker
     {
-        public static bool AreEquivalent(SyntaxNode node1, SyntaxNode node2)
-        {
-            return Common.EquivalenceChecker.AreEquivalent(node1, node2,
-                (n1, n2) => SyntaxFactory.AreEquivalent(n1, n2));
-        }
+        public static bool AreEquivalent(SyntaxNode node1, SyntaxNode node2) =>
+            Common.EquivalenceChecker.AreEquivalent(node1, node2, (n1, n2) => SyntaxFactory.AreEquivalent(n1, n2));
 
-        public static bool AreEquivalent(SyntaxList<SyntaxNode> nodeList1, SyntaxList<SyntaxNode> nodeList2)
-        {
-            return Common.EquivalenceChecker.AreEquivalent(nodeList1, nodeList2,
-                (n1, n2) => SyntaxFactory.AreEquivalent(n1, n2));
-        }
+        public static bool AreEquivalent(SyntaxList<SyntaxNode> nodeList1, SyntaxList<SyntaxNode> nodeList2) =>
+            Common.EquivalenceChecker.AreEquivalent(nodeList1, nodeList2, (n1, n2) => SyntaxFactory.AreEquivalent(n1, n2));
     }
 
     internal class CSharpSyntaxNodeEqualityComparer<T> : IEqualityComparer<T>, IEqualityComparer<SyntaxList<T>>
         where T : SyntaxNode
     {
-        public bool Equals(T x, T y)
-        {
-            return CSharpEquivalenceChecker.AreEquivalent(x, y);
-        }
+        public bool Equals(T x, T y) =>
+            CSharpEquivalenceChecker.AreEquivalent(x, y);
 
-        public bool Equals(SyntaxList<T> x, SyntaxList<T> y)
-        {
-            return CSharpEquivalenceChecker.AreEquivalent(x, y);
-        }
+        public bool Equals(SyntaxList<T> x, SyntaxList<T> y) =>
+            CSharpEquivalenceChecker.AreEquivalent(x, y);
 
-        public int GetHashCode(T obj)
-        {
-            return obj.GetType().FullName.GetHashCode();
-        }
+        public int GetHashCode(T obj) =>
+            obj.GetType().FullName.GetHashCode();
 
-        public int GetHashCode(SyntaxList<T> obj)
-        {
-            return (obj.Count + string.Join(", ", obj.Select(x => x.GetType().FullName).Distinct())).GetHashCode();
-        }
+        public int GetHashCode(SyntaxList<T> obj) =>
+            (obj.Count + obj.Select(x => x.GetType().FullName).Distinct().JoinStr(", ")).GetHashCode();
     }
 
 }
