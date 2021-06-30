421b5fddc59ac23ca68931f16c8ba2d4444f6e44
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-06-30 14:58:21 +0200

    Review update: Add FP and FNs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExpressionComplexity.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExpressionComplexity.CSharp9.cs
@@ -9,3 +9,28 @@ if (x is true and true and true and true and true) { } // Noncompliant
 if (x is 10 or 20 or 40 or 50 or 60 or 70) { } // Noncompliant
 
 if (x is < 10 or < 20 or < 30 and (40 or 50 or 60)) { } // Noncompliant
+
+switch (x)
+{
+    case true and true and true and true and true and true: // FN
+        break;
+    case false:
+        break;
+    default:
+        break;
+}
+
+var y = x switch // Noncompliant FP
+{
+    true and true and true and true and true and true => 1, // FN
+    false => 2,
+};
+
+y = x switch
+{
+    int => 1,
+    string => 1,
+    decimal => 1,
+    true => 1,
+    false => 0
+};
