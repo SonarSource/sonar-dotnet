1b75e2b59889ca6e09d4dfbf40b2d6ca9af93f7e
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2021-07-05 14:29:57 +0200

    Delete S2278 (deprecated) (#4630)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/InsecureEncryptionAlgorithm.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/InsecureEncryptionAlgorithm.cs
@@ -35,23 +35,20 @@ namespace Tests.Diagnostics
         {
             // Rule will raise an issue for both S2278 and S5547 as they are activated by default in unit tests
 
-                                                                           // Noncompliant@+1 {{Use the recommended AES (Advanced Encryption Standard) instead.}}
             using (var tripleDES = new MyTripleDESCryptoServiceProvider()) // Noncompliant    {{Use a strong cipher algorithm.}}
 //                                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
             {
                 //...
             }
-                                                             // Noncompliant@+1
             using (var des = new DESCryptoServiceProvider()) // Noncompliant
             {
                 //...
             }
-                                                                // Noncompliant@+1
             using (TripleDES TripleDESalg = TripleDES.Create()) // Noncompliant
 //                                          ^^^^^^^^^^^^^^^^^^
             {
             }
-                                                        // Noncompliant@+1
+
             using (var des = DES.Create("fgdsgsdfgsd")) // Noncompliant
             {
             }
@@ -60,37 +57,28 @@ namespace Tests.Diagnostics
             {
                 //...
             }
-                                                              // Noncompliant@+1
+
             using (var rc21 = new RC2CryptoServiceProvider()) // Noncompliant
             {
 
             }
 
-                                            // Noncompliant@+1
             using (var rc22 = RC2.Create()) // Noncompliant
             {
 
             }
-                                                                        // Noncompliant@+1
+
             SymmetricAlgorithm des1 = SymmetricAlgorithm.Create("DES"); // Noncompliant
-
-                                                           // Noncompliant@+1
             des1 = SymmetricAlgorithm.Create("TripleDES"); // Noncompliant
-
-                                                      // Noncompliant@+1
             des1 = SymmetricAlgorithm.Create("3DES"); // Noncompliant
 
-                                                        // Noncompliant@+1
             var rc2 = SymmetricAlgorithm.Create("RC2"); // Noncompliant
 
-                                                             // Noncompliant@+1
             var crypto = CryptoConfig.CreateFromName("DES"); // Noncompliant
 
-                                                     // Noncompliant@+1
             var aesFastEngine = new AesFastEngine(); // Noncompliant
 //                                  ^^^^^^^^^^^^^
 
-                                                                        // Noncompliant@+1
             var blockCipher1 = new GcmBlockCipher(new AesFastEngine()); // Noncompliant
 
             var blockCipher2 = new GcmBlockCipher(new AesEngine());     // Compliant
