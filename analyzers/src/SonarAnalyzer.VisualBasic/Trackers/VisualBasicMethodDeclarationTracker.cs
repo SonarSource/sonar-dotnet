bf4430a98dda59746a95e9216a30b0f69630fcd4
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-07-14 18:03:33 +0200

    Reimplement working part with green UT :trollface:

--- analyzers/src/SonarAnalyzer.VisualBasic/Trackers/VisualBasicMethodDeclarationTracker.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/Trackers/VisualBasicMethodDeclarationTracker.cs
@@ -59,27 +59,14 @@ namespace SonarAnalyzer.Helpers.Trackers
                         && parameterSymbol.Equals(semanticModel.GetSymbolInfo(node).Symbol));
             };
 
-        protected override SyntaxToken? GetMethodIdentifier(SyntaxNode methodDeclaration)
-        {
-            switch (methodDeclaration?.Kind())
+        protected override SyntaxToken? GetMethodIdentifier(SyntaxNode methodDeclaration) =>
+            methodDeclaration switch
             {
-                case SyntaxKind.SubStatement:
-                case SyntaxKind.FunctionStatement:
-                    return ((MethodStatementSyntax)methodDeclaration).Identifier;
-                case SyntaxKind.SubNewStatement:
-                    return ((SubNewStatementSyntax)methodDeclaration).NewKeyword;
-                case SyntaxKind.AddHandlerAccessorBlock:
-                case SyntaxKind.RemoveHandlerAccessorBlock:
-                    return ((EventBlockSyntax)methodDeclaration.Parent.Parent).EventStatement?.Identifier;
-                case SyntaxKind.GetAccessorBlock:
-                case SyntaxKind.SetAccessorBlock:
-                    return ((PropertyBlockSyntax)methodDeclaration.Parent.Parent).PropertyStatement?.Identifier;
-                case SyntaxKind.OperatorStatement:
-                    return ((OperatorStatementSyntax)methodDeclaration).OperatorToken;
-                default:
-                    return null;
-            }
-        }
+                SubNewStatementSyntax constructor => constructor.NewKeyword,
+                MethodStatementSyntax method => method.Identifier,
+                OperatorStatementSyntax op => op.OperatorToken,
+                _ => null
+            };
 
         private static bool HasImplementation(MethodBlockSyntax methodBlock) =>
             methodBlock.Statements.Count > 0;
