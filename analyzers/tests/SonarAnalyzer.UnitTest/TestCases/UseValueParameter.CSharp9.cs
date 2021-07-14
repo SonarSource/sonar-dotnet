08df2c23de60675583347012c0d6e1bfa61cc7a3
DGolovinov <64070475+dmitry-golovinov-sonarsource@users.noreply.github.com>
2021-07-14 19:17:06 +0200

    Rule S3237: introduce support for C# 9 init-only setters (#4669)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseValueParameter.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseValueParameter.CSharp9.cs
@@ -5,17 +5,37 @@
     public int Count
     {
         get { return count; }
-        init { count = 3; } // Compliant FN
+        init { count = 3; } // Noncompliant {{Use the 'value' parameter in this property init accessor declaration.}}
+    }
+
+    public int Count2
+    {
+        get { return count; }
+        set  // Noncompliant
+        {
+            void Foo(int value) {
+                count = value;
+            }
+        }
+    }
+
+    public int Count3
+    {
+        get => 42;
+        set  // Noncompliant
+        {
+            System.Func<int, int> f = value => value;
+        }
     }
 
     public string FirstName { get; init; } = "Foo";
 
-    public string LastName { get => string.Empty; init { } } // Compliant FN
+    public string LastName { get => string.Empty; init { } } // Noncompliant
 
     public int this[int i]
     {
         get => 0;
-        init // Compliant FN
+        init // Noncompliant
         {
             var x = 1;
         }
