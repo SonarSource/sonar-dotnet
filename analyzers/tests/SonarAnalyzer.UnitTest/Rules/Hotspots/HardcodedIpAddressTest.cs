199b5ec045859aa9b8b3bb39d3237808fb74ca10
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-07-22 13:09:33 +0200

    PR suggestion processing

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/Hotspots/HardcodedIpAddressTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/Hotspots/HardcodedIpAddressTest.cs
@@ -18,9 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
-using FluentAssertions;
-using FluentAssertions.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarAnalyzer.Common;
 using SonarAnalyzer.UnitTest.TestFramework;
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCasesForRuleFailure/PerformanceTestCases.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCasesForRuleFailure/PerformanceTestCases.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace SonarAnalyzer.UnitTest.TestCasesForRuleFailure
 {
-    class Foo
+    class PerformanceTestCases
     {
         static void Bar()
         {
