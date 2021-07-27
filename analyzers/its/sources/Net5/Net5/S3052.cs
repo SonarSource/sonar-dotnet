1c53ca7913303079f093fb6b90d72dc6b19fdf33
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-07-27 16:55:36 +0300

    Add ITs

---
+++ analyzers/its/sources/Net5/Net5/S3052.cs
@@ -0,0 +1,10 @@
+﻿namespace Net5
+{
+    public class S3052
+    {
+        public nint Field1 = 0;
+        public nint Field2 = 42;
+        public nuint Field3 = 0;
+        public nuint Field4 = 42;
+    }
+}
