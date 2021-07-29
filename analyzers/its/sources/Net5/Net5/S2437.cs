dacaf77b7ccb55a59c2f81780dbdd911ae4e055b
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-07-29 11:56:21 +0300

    Add IT

---
+++ analyzers/its/sources/Net5/Net5/S2437.cs
@@ -0,0 +1,13 @@
+﻿namespace Net5
+{
+    public class S2437
+    {
+        public void Foo()
+        {
+            nint result;
+            nint bitMask = 0x010F;
+
+            result = bitMask & -1;
+        }
+    }
+}
