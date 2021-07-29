2394b091953fa60d5506b493dd5b5af52c8a04a2
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-07-29 16:37:23 +0300

    Address code smells

--- analyzers/src/SonarAnalyzer.CSharp/Trackers/CSharpConstantValueFinder.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Trackers/CSharpConstantValueFinder.cs
@@ -18,7 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
