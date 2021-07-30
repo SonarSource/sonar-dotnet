9ce199b8d18227fa7f8226d4e30e63fcaf933af7
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2021-07-30 11:27:37 +0300

    Rule S3353: add support for top level methods (#4740)

---
+++ analyzers/its/sources/Net5/Net5/S3353.cs
@@ -0,0 +1,15 @@
+﻿namespace Net5
+{
+    public class S3353
+    {
+        public void Test(nuint val)
+        {
+            nint foo = 42; // Noncompliant
+
+            nuint bar = 31; // Noncompliant
+            if (bar == val)
+            {
+            }
+        }
+    }
+}
