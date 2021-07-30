a80644e02b1a9ffb8916c91ff5af1f9e2b5dfb5d
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-07-30 10:17:34 +0300

    Add more test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp9.cs
@@ -2,11 +2,18 @@
 i = i++; // Noncompliant; i is still zero
 i = ++i; // Compliant
 
-nuint u = 0;
+nuint u = 0, a = 0;
 u = u++; // Noncompliant; u is still zero
 u = ++u; // Compliant
+u += u++;
+a = u++;
+
+int x = (int) u++;
 
 int Compute(nint v) =>
     (int) v++; // Noncompliant
+
+int ComputeArrowBody(int v) =>
+    v--; // Noncompliant
 
 return (int)i++; // Noncompliant
