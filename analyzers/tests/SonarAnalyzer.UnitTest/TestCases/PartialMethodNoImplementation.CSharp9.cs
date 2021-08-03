e107f9021fbd233d59513fcca151c0ff225c5b72
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-08-03 12:21:24 +0200

    C#9 new partial method features support

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PartialMethodNoImplementation.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PartialMethodNoImplementation.CSharp9.cs
@@ -1,18 +1,11 @@
 ﻿public partial record Record
 {
     partial void Method(); //Noncompliant {{Supply an implementation for this partial method.}}
-
     private partial void M2(); // Error CS8795
-                               // Noncompliant@-1 - FP (there is a compiler error already)
     internal partial void M3(); // Error CS8795
-                                // Noncompliant@-1 - FP (there is a compiler error already)
     protected partial void M4(); // Error CS8795
-                                 // Noncompliant@-1 - FP (there is a compiler error already)
     protected internal partial void M5(); // Error CS8795
-                                          // Noncompliant@-1 - FP (there is a compiler error already)
     public partial void M6(); // Error CS8795
-                              // Noncompliant@-1 - FP (there is a compiler error already)
-
     public partial void M7(); // Compliant
 }
 
@@ -21,5 +14,27 @@ public partial record Record
     public partial void M7()
     {
 
+    }
+}
+
+public partial record Record2
+{
+    partial void Method(); //Noncompliant {{Supply an implementation for this partial method.}}
+    private partial void M2(); // Error CS8795
+    internal partial void M3(); // Error CS8795
+    protected partial void M4(); // Error CS8795
+    protected internal partial void M5(); // Error CS8795
+    public partial void M6(); // Error CS8795
+
+    public void SomeMethod()
+    {
+        var record2 = new Record2();
+
+        record2.Method(); // Noncompliant
+        record2.M2();
+        record2.M3();
+        record2.M4();
+        record2.M5();
+        record2.M6();
     }
 }
