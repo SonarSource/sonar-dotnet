4e8c689f289f1cf8887983b8bc887fd3a1cad63f
Corniel Nobel <mail@corniel.nl>
2021-08-16 14:33:36 +0200

    Increase coverage for C#.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExpectedExceptionAttributeShouldNotBeUsed.NUnit.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExpectedExceptionAttributeShouldNotBeUsed.NUnit.cs
@@ -38,5 +38,9 @@ namespace Tests.Diagnostics
                 callFailed = true;
             }
         }
+
+        [Test]
+        [NUnit.Framework.ExpectedException(typeof(ArgumentNullException))]
+        public void NoBody() // Error [CS0501,CS1002]
     }
 }
