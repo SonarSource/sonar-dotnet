3b9280e16381d119fc87a582c99a2a0383ee6365
Dmitry Golovinov <dmitry.golovinov@sonarsource.com>
2021-08-19 14:53:31 +0200

    Address PR suggestion

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/MetadataReferences/NuGetMetadataFactory.Project.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NuGet.Common;
+using NuGet.Configuration;
+using NuGet.Packaging;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
+
+namespace SonarAnalyzer.UnitTest.MetadataReferences
+{
+    public static partial class NuGetMetadataFactory
+    {
+        private class Package
+        {
+            private string Id { get; }
+            private string Version { get; }
+            private string Runtime { get; }
+
+            public Package(string id, string version, string runtime)
+            {
+                Id = id;
+                Version = version;
+                Runtime = runtime;
+            }
+
+            public string PackageDirectory()
+            {
+                var runtimePath = Runtime == null ? string.Empty : $"runtimes\\{Runtime}\\";
+                var combinedPath = Path.Combine(PackagesFolder, $@"{Id}.{Version}", runtimePath, "lib");
+                return Path.GetFullPath(combinedPath);
+            }
+
+            public void EnsurePackageIsInstalled()
+            {
+                // Check to see if the specific package is already installed
+                var packageDir = PackageDirectory();
+                if (!Directory.Exists(packageDir))
+                {
+                    LogMessage($"Package not found at {packageDir}, will attempt to download and install.");
+                    InstallPackageAsync().Wait();
+                    if (!Directory.Exists(packageDir))
+                    {
+                        throw new ApplicationException($"Test setup error: folder for downloaded package does not exist. Folder: {packageDir}");
+                    }
+                }
+            }
+
+            private async Task InstallPackageAsync()
+            {
+                var nugetOrgUrl = Settings.LoadSpecificSettings(NugetConfigFolderRelativePath, "nuget.config")
+                                          .GetSection("packageSources").Items.OfType<AddItem>()
+                                          .Where(ai => ai.Key == "nuget.org")
+                                          .Select(ai => ai.Value)
+                                          .Single();
+                var repository = Repository.Factory.GetCoreV3(nugetOrgUrl);
+                var context = new SourceCacheContext();
+                var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
+
+                NuGetVersion version;
+                if (Version == Constants.NuGetLatestVersion)
+                {
+                    var versions = await resource.GetAllVersionsAsync(
+                        Id,
+                        context,
+                        NullLogger.Instance,
+                        CancellationToken.None);
+                    version = versions.OrderByDescending(x => x.Version).First(x => !x.IsPrerelease);
+                }
+                else
+                {
+                    version = new NuGetVersion(Version);
+                }
+
+                using var packageStream = new MemoryStream();
+                await resource.CopyNupkgToStreamAsync(
+                    Id,
+                    version,
+                    packageStream,
+                    context,
+                    NullLogger.Instance,
+                    CancellationToken.None);
+
+                var packageDirectory = PackageDirectory();
+                using var packageReader = new PackageArchiveReader(packageStream);
+
+                foreach (var dllFile in packageReader.GetFiles().Where(f => f.EndsWith(".dll", StringComparison.OrdinalIgnoreCase)))
+                {
+                    packageReader.ExtractFile(dllFile, $"{packageDirectory}\\{dllFile}", NullLogger.Instance);
+                }
+            }
+
+            private static void LogMessage(string message) =>
+                Console.WriteLine($"[{DateTime.Now}] Test setup: {message}");
+        }
+    }
+}
--- analyzers/tests/SonarAnalyzer.UnitTest/MetadataReferences/NuGetMetadataFactory.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/MetadataReferences/NuGetMetadataFactory.cs
@@ -23,19 +23,11 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using NuGet.Common;
-using NuGet.Configuration;
-using NuGet.Packaging;
-using NuGet.Protocol;
-using NuGet.Protocol.Core.Types;
-using NuGet.Versioning;
 
 namespace SonarAnalyzer.UnitTest.MetadataReferences
 {
-    public static class NuGetMetadataFactory
+    public static partial class NuGetMetadataFactory
     {
         private const string NugetConfigFolderRelativePath = @"..\..\..";
         private const string PackagesFolderRelativePath = @"..\..\..\..\..\packages\";
@@ -67,44 +59,28 @@ namespace SonarAnalyzer.UnitTest.MetadataReferences
         /// <param name="dllDirectory">Name of the directory containing DLL files inside *.nupgk/lib/{dllDirectory}/ or *.nupgk/runtimes/{runtime}/lib/{dllDirectory}/ folder.
         /// This directory name represents target framework in most cases.</param>
         public static IEnumerable<MetadataReference> Create(string packageId, string packageVersion, string runtime, string dllDirectory) =>
-            Create(packageId, packageVersion, runtime, new[] { dllDirectory });
+            Create(new Package(packageId, packageVersion, runtime), new[] { dllDirectory });
 
         public static IEnumerable<MetadataReference> Create(string packageId, string packageVersion, string runtime = null) =>
-            Create(packageId, packageVersion, runtime, SortedAllowedDirectories);
+            Create(new Package(packageId, packageVersion, runtime), SortedAllowedDirectories);
 
         public static IEnumerable<MetadataReference> CreateNetStandard21()
         {
-            var packageDir = Path.Combine(PackagesFolder, @"NETStandard.Library.Ref.2.1.0\lib\ref\netstandard2.1");
-            EnsurePackageIsInstalled("NETStandard.Library.Ref", "2.1.0");
-            if (!Directory.Exists(packageDir))
-            {
-                throw new ApplicationException($"Test setup error: folder for downloaded package does not exist. Folder: {packageDir}");
-            }
+            var package = new Package("NETStandard.Library.Ref", "2.1.0", "netstandard2.1");
+            package.EnsurePackageIsInstalled();
 
-            return Directory.GetFiles(packageDir, "*.dll", SearchOption.AllDirectories)
+            return Directory.GetFiles(package.PackageDirectory(), "*.dll", SearchOption.AllDirectories)
                .Select(x => (MetadataReference)MetadataReference.CreateFromFile(x))
                .ToImmutableArray();
         }
 
-        private static string GetNuGetPackageDirectory(string packageId, string packageVersion, string runtime = null)
-        {
-            var runtimePath = runtime == null ? string.Empty : $"runtimes\\{runtime}\\";
-            var combinedPath = Path.Combine(PackagesFolder, $@"{packageId}.{packageVersion}", runtimePath, "lib");
-            return Path.GetFullPath(combinedPath);
-        }
-
         /// <param name="allowedDirectories">List of allowed directories sorted by preference to search for DLL files.</param>
-        private static IEnumerable<MetadataReference> Create(string packageId, string packageVersion, string runtime, string[] allowedDirectories)
+        private static IEnumerable<MetadataReference> Create(Package package, string[] allowedDirectories)
         {
-            EnsurePackageIsInstalled(packageId, packageVersion);
+            package.EnsurePackageIsInstalled();
 
             var allowedNugetLibDirectoriesByPreference = allowedDirectories.Select((folder, priority) => new { folder, priority });
-            var packageDirectory = GetNuGetPackageDirectory(packageId, packageVersion, runtime);
-            if (!Directory.Exists(packageDirectory))
-            {
-                throw new ApplicationException($"Test setup error: folder for downloaded package does not exist. Folder: {packageDirectory}");
-            }
-
+            var packageDirectory = package.PackageDirectory();
             var matchingDllsGroups = Directory.GetFiles(packageDirectory, "*.dll", SearchOption.AllDirectories)
                                               .Select(path => new FileInfo(path))
                                               .GroupBy(file => file.Directory.Name).ToArray();
@@ -122,63 +98,5 @@ namespace SonarAnalyzer.UnitTest.MetadataReferences
             return selectedGroup.Select(file => (MetadataReference)MetadataReference.CreateFromFile(file.FullName))
                                 .ToImmutableArray();
         }
-
-        private static void EnsurePackageIsInstalled(string packageId, string packageVersion)
-        {
-            // Check to see if the specific package is already installed
-            var packageDir = GetNuGetPackageDirectory(packageId, packageVersion);
-            if (!Directory.Exists(packageDir))
-            {
-                LogMessage($"Package not found at {packageDir}, will attempt to download and install.");
-                InstallPackageAsync(packageId, packageVersion).Wait();
-            }
-        }
-
-        private static async Task InstallPackageAsync(string packageId, string packageVersion)
-        {
-            var nugetOrgUrl = Settings.LoadSpecificSettings(NugetConfigFolderRelativePath, "nuget.config")
-                                      .GetSection("packageSources").Items.OfType<AddItem>()
-                                      .Where(ai => ai.Key == "nuget.org")
-                                      .Select(ai => ai.Value)
-                                      .Single();
-            var repository = Repository.Factory.GetCoreV3(nugetOrgUrl);
-            var context = new SourceCacheContext();
-            var resource = await repository.GetResourceAsync<FindPackageByIdResource>();
-
-            NuGetVersion version;
-            if (packageVersion == Constants.NuGetLatestVersion)
-            {
-                var versions = await resource.GetAllVersionsAsync(
-                    packageId,
-                    context,
-                    NullLogger.Instance,
-                    CancellationToken.None);
-                version = versions.OrderByDescending(x => x.Version).First(x => !x.IsPrerelease);
-            }
-            else
-            {
-                version = new NuGetVersion(packageVersion);
-            }
-
-            using var packageStream = new MemoryStream();
-            await resource.CopyNupkgToStreamAsync(
-                packageId,
-                version,
-                packageStream,
-                context,
-                NullLogger.Instance,
-                CancellationToken.None);
-
-            var packageDirectory = GetNuGetPackageDirectory(packageId, packageVersion);
-            using var packageReader = new PackageArchiveReader(packageStream);
-
-            foreach (var dllFile in packageReader.GetFiles().Where(f => f.EndsWith(".dll", StringComparison.OrdinalIgnoreCase)))
-            {
-                packageReader.ExtractFile(dllFile, $"{packageDirectory}\\{dllFile}", NullLogger.Instance);
-            }
-        }
-
-        private static void LogMessage(string message) =>
-             Console.WriteLine($"[{DateTime.Now}] Test setup: {message}");
     }
 }
