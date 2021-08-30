4545580884aa85ce05b0f0e0267d842efd275226
Andrei Epure <andrei.epure@sonarsource.com>
2021-08-30 14:04:45 +0200

    Add IT w/ xunit and mstest

---
+++ analyzers/its/sources/Net5/Net5Test/S3433_Xunit.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Net5Test
+{
+    public class S3433_Xunit
+    {
+        public void Method()
+        {
+            [Fact]
+            void NestedFact()
+            {
+                Assert.True(true);
+            }
+        }
+    }
+}
