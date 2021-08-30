752a330dd1998f15a83482640a821752c18452e8
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2021-08-30 11:12:31 +0200

    Introduce IOperation wrapper (#4848)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Wrappers/IOperationWrapperSonarTest.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StyleCop.Analyzers.Lightup;
+
+namespace SonarAnalyzer.UnitTest.Wrappers
+{
+    [TestClass]
+    public class IOperationWrapperSonarTest
+    {
+        [TestMethod]
+        public void Null_Throws()
+        {
+            Action a = () => new IOperationWrapperSonar(null).ToString();
+            a.Should().Throw<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void ValidateReflection()
+        {
+            const string code = @"
+public class Sample
+{
+    public void Method()
+    {
+        var value = 42;
+    }
+}";
+            var (tree, semanticModel) = TestHelper.Compile(code);
+            var declaration = tree.GetRoot().DescendantNodes().OfType<EqualsValueClauseSyntax>().Single();
+            var sut = new IOperationWrapperSonar(semanticModel.GetOperation(declaration));
+            sut.Parent.Should().NotBeNull();
+            sut.Parent.Kind.Should().Be(OperationKind.VariableDeclarator);
+            sut.Instance.Should().NotBeNull();
+            sut.Instance.Kind.Should().Be(OperationKind.VariableInitializer);
+            sut.Children.Should().HaveCount(1);
+            sut.Children.Single().Kind.Should().Be(OperationKind.Literal);
+            sut.Language.Should().Be("C#");
+            sut.IsImplicit.Should().Be(false);
+            sut.SemanticModel.Should().Be(semanticModel);
+        }
+    }
+}
