17b164a648b001e427fee6d9a9077580bfcfb987
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-08-31 18:23:48 +0200

    Remove nint and nuint cases from ITs and deocument beehaviour in UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PointersShouldBePrivate.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/PointersShouldBePrivate.CSharp9.cs
@@ -7,6 +7,8 @@ public unsafe record Record
     public RandomNameSpace.UIntPtr p3; // Error [CS0246]
     public (int, string) p4;
     public System.UIntPtr p5; // Noncompliant
+    public nint p5; // Compliant
+    public nuint p5; // Compliant
 }
 
 public unsafe class Class
