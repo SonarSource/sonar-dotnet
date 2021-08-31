df17384c65d3634510cc4f77d1807051915257e0
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-08-31 16:37:14 +0200

    FN repro

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnnecessaryUsingsFNRepro.cs
@@ -0,0 +1,12 @@
+﻿using System; // FN - Using System is not needed for built in types (In this case int)
+
+namespace Repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 42;
+        }
+    }
+}
