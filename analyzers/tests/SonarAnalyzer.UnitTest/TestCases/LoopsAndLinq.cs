1498011679eaa3dd1ad5f5b446c260552dd48833
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-09-01 22:55:02 +0200

    Add UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LoopsAndLinq.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LoopsAndLinq.cs
@@ -1,8 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Tests.Diagnostics
 {
-    class Program
+    public class S3267
     {
+        public List<string> SuggestWhere(ICollection<string> collection, Predicate<string> condition)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection) // Noncompliant
+            {
+                if (condition(element))
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> SuggestSelectAndWhere(ICollection<string> collection)
+        {
+            var result = new List<int>();
+
+            foreach (var element in collection) // Noncompliant
+            {
+                var someValue = element.Length;
+                if (someValue > 0)
+                {
+                    result.Add(someValue);
+                }
+            }
+
+            return result;
+        }
     }
 }
