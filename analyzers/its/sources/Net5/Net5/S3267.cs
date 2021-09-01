b03d5a123cdcd640255a01a44d7c7ed1e584998e
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-09-01 22:52:11 +0200

    Add ITs

---
+++ analyzers/its/sources/Net5/Net5/S3267.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net5
+{
+    public class S3267
+    {
+        public List<string> SuggestWhere(ICollection<string> collection, Predicate<string> condition)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection) // Noncompliant
+            {
+                if (condition(element))
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result;
+        }
+
+        public List<int> SuggestSelectAndWhere(ICollection<string> collection)
+        {
+            var result = new List<int>();
+
+            foreach (var element in collection) // Noncompliant
+            {
+                var someValue = element.Length;
+                if (someValue > 0)
+                {
+                    result.Add(someValue);
+                }
+            }
+
+            return result;
+        }
+    }
+}
