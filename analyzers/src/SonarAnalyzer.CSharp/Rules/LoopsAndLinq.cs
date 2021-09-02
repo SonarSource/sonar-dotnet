39e89207eaf14d085853d1890273ae908d3a9c83
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-09-02 13:36:28 +0200

    Add more test cases

--- analyzers/src/SonarAnalyzer.CSharp/Rules/LoopsAndLinq.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/LoopsAndLinq.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
@@ -25,6 +26,8 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.Common;
+using SonarAnalyzer.ControlFlowGraph;
+using SonarAnalyzer.Extensions;
 using SonarAnalyzer.Helpers;
 
 namespace SonarAnalyzer.Rules.CSharp
@@ -34,7 +37,9 @@ namespace SonarAnalyzer.Rules.CSharp
     public sealed class LoopsAndLinq : SonarDiagnosticAnalyzer
     {
         private const string DiagnosticId = "S3267";
-        private const string MessageFormat = "Loops should be simplified with \"LINQ\" expressions";
+        private const string MessageFormat = "{0}";
+        private const string WhereMessageFormat = @"Loops should be simplified with ""LINQ"" expressions";
+        private const string SelectMessageFormat = "Loop should be simplified by calling Select({0} => {0}.{1}))";
 
         private static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
 
@@ -43,15 +48,96 @@ namespace SonarAnalyzer.Rules.CSharp
         protected override void Initialize(SonarAnalysisContext context) =>
             context.RegisterSyntaxNodeActionInNonGenerated(c =>
                 {
-                    var node = (ForEachStatementSyntax)c.Node;
-
-                    if (node.Statement is BlockSyntax blockSyntax
-                        && blockSyntax.ChildNodes().Count() == 1
-                        && blockSyntax.ChildNodes().Single() is IfStatementSyntax)
+                    var forEachStatementSyntax = (ForEachStatementSyntax)c.Node;
+                    if (CanBeSimplifiedUsingWhere(forEachStatementSyntax.Statement))
                     {
-                        c.ReportDiagnosticWhenActive(Diagnostic.Create(Rule, node.GetLocation()));
+                        c.ReportDiagnosticWhenActive(Diagnostic.Create(Rule, forEachStatementSyntax.Expression.GetLocation(), WhereMessageFormat));
+                    }
+                    else
+                    {
+                        CheckIfCanBeSimplifiedUsingSelect(forEachStatementSyntax, c);
                     }
                 },
                 SyntaxKind.ForEachStatement);
+
+        private static bool CanBeSimplifiedUsingWhere(SyntaxNode statement) =>
+            GetIfStatement(statement) is { } ifStatementSyntax
+            && CanIfStatementBeMoved(ifStatementSyntax);
+
+        private static IfStatementSyntax GetIfStatement(SyntaxNode node) =>
+            node switch
+            {
+                IfStatementSyntax ifStatementSyntax => ifStatementSyntax,
+                BlockSyntax blockSyntax when blockSyntax.ChildNodes().Count() == 1 => GetIfStatement(blockSyntax.ChildNodes().Single()),
+                _ => null
+            };
+
+        private static bool CanIfStatementBeMoved(IfStatementSyntax ifStatementSyntax) =>
+            ifStatementSyntax.Else == null
+            && !ContainsExitPoints(ifStatementSyntax)
+            && ifStatementSyntax.Condition is InvocationExpressionSyntax invocationExpressionSyntax
+            && !invocationExpressionSyntax.DescendantNodes().OfType<ArgumentSyntax>().Any(argument => argument.RefOrOutKeyword.IsKind(SyntaxKind.OutKeyword));
+
+        private static bool ContainsExitPoints(SyntaxNode syntaxNode) =>
+            syntaxNode.DescendantNodes()
+                      .Any(node => node.IsAnyKind(SyntaxKind.ReturnStatement,
+                                                  SyntaxKind.YieldReturnStatement,
+                                                  SyntaxKind.BreakStatement,
+                                                  SyntaxKind.YieldBreakStatement,
+                                                  SyntaxKind.GotoStatement));
+
+        private static void CheckIfCanBeSimplifiedUsingSelect(ForEachStatementSyntax forEachStatementSyntax, SyntaxNodeAnalysisContext c)
+        {
+            // There are multiple scenarios where the code can be simplified using LINQ.
+            // For simplicity, in the first version of the rule, we consider that Select() can be used
+            // only when a single property from the foreach variable is used.
+            // This property needs to be used more than once or if it is the right side of a variable declaration.
+            var declaredSymbol = c.SemanticModel.GetDeclaredSymbol(forEachStatementSyntax);
+            var accessedProperties = new Dictionary<ISymbol, UsageStats>();
+
+            foreach (var identifierSyntax in GetIdentifiers(forEachStatementSyntax))
+            {
+                if (identifierSyntax.Parent is MemberAccessExpressionSyntax { Parent: not InvocationExpressionSyntax and not AssignmentExpressionSyntax } memberAccessExpressionSyntax
+                    && c.SemanticModel.GetSymbolInfo(identifierSyntax).Symbol.Equals(declaredSymbol))
+                {
+                    var symbol = c.SemanticModel.GetSymbolInfo(memberAccessExpressionSyntax.Name).Symbol;
+                    var usageStats = accessedProperties.GetOrAdd(symbol, _ => new UsageStats());
+
+                    if (identifierSyntax.Parent.Parent is EqualsValueClauseSyntax { Parent: VariableDeclaratorSyntax })
+                    {
+                        usageStats.IsInVarDeclarator = true;
+                    }
+
+                    usageStats.Count++;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            if (accessedProperties.Count == 1
+                && accessedProperties.First().Value is var stats
+                && (stats.IsInVarDeclarator || stats.Count > 1))
+            {
+                var diagnostic = Diagnostic.Create(Rule,
+                                                   forEachStatementSyntax.Expression.GetLocation(),
+                                                   string.Format(SelectMessageFormat, forEachStatementSyntax.Identifier.ValueText, accessedProperties.Single().Key.Name));
+                c.ReportDiagnosticWhenActive(diagnostic);
+            }
+
+            static IEnumerable<IdentifierNameSyntax> GetIdentifiers(ForEachStatementSyntax forEachStatementSyntax) =>
+                forEachStatementSyntax.Statement
+                                      .DescendantNodes()
+                                      .OfType<IdentifierNameSyntax>()
+                                      .Where(identifierNameSyntax => identifierNameSyntax.Identifier.ValueText == forEachStatementSyntax.Identifier.ValueText);
+        }
+
+        private class UsageStats
+        {
+            public int Count { get; set; }
+
+            public bool IsInVarDeclarator { get; set; }
+        }
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LoopsAndLinq.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LoopsAndLinq.cs
@@ -1,30 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Tests.Diagnostics
 {
     public class S3267
     {
-        public List<string> SuggestWhere(ICollection<string> collection, Predicate<string> condition)
+        public bool WithOutArg(string p1, out string p2)
+        {
+            p2 = string.Empty;
+            return true;
+        }
+
+        public void ForEachWithBlockWithIfSuggestWhere(ICollection<string> collection, Predicate<string> condition)
         {
             var result = new List<string>();
 
             foreach (var element in collection) // Noncompliant {{Loops should be simplified with "LINQ" expressions}}
+            //                      ^^^^^^^^^^
             {
                 if (condition(element))
                 {
                     result.Add(element);
                 }
             }
-
-            return result;
         }
 
-        public List<int> SuggestSelectAndWhere(ICollection<string> collection)
+        public void ForEach_IfWithSideEffects_Compliant(ICollection<string> collection)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection) // Compliant due to `out var a`
+            {
+                if (WithOutArg(element, out var a))
+                {
+                    result.Add(a);
+                    result.Add(element);
+                }
+            }
+        }
+
+        public void ForEach_NoIf_Compliant(ICollection<string> collection)
+        {
+            var builder = new StringBuilder();
+            foreach (var s in collection)
+            {
+                builder.AppendFormat("\n    {0}", s);
+            }
+        }
+
+        public void ForEach_PropertySet_Compliant(ICollection<Point> collection)
+        {
+            foreach (var point in collection) // Compliant - Selecting `X` and setting it's value will not work in this case.
+            {
+                point.X = point.X + 3;
+            }
+        }
+
+        public void ForEach_IfWithElseBranch_Compliant(ICollection<string> collection, Predicate<string> condition)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection) // Compliant due to else branch
+            {
+                if (condition(element))
+                {
+                    result.Add(element);
+                }
+                else
+                {
+                    result.Add(string.Empty);
+                }
+            }
+        }
+
+        public void ForEachWithIfSuggestWhere(ICollection<string> collection, Predicate<string> condition)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection) // Noncompliant {{Loops should be simplified with "LINQ" expressions}}
+            //                      ^^^^^^^^^^
+                if (condition(element))
+                {
+                    result.Add(element);
+                }
+        }
+
+        public void ForEachWithEarlyExitIsCompliant(ICollection<string> collection, Predicate<string> condition)
+        {
+            var result = new List<string>();
+
+            foreach (var element in collection)
+            {
+                if (condition(element))
+                {
+                    result.Add(element);
+                    return;
+                }
+            }
+        }
+
+        public void ForEachWithPropertyGet_SuggestsSelect(ICollection<string> collection, ICollection<Point> points, int[] values)
         {
             var result = new List<int>();
 
-            foreach (var element in collection)
+            foreach (var element in collection) // Noncompliant {{Loop should be simplified by calling Select(element => element.Length))}}
+            //                      ^^^^^^^^^^
+            {
+                var someValue = element.Length;
+            }
+
+            foreach (var element in collection) // Compliant: property is used only once and not in an assignment
+            {
+                Foo(element.Length);
+            }
+
+            foreach (var element in collection) // Noncompliant {{Loop should be simplified by calling Select(element => element.Length))}}
+            //                      ^^^^^^^^^^
             {
                 var someValue = element.Length;
                 if (someValue > 0)
@@ -33,7 +125,60 @@ namespace Tests.Diagnostics
                 }
             }
 
-            return result;
+            foreach (var element in collection) // Compliant: `element` is used
+            {
+                var someValue = element.Length;
+                if (element == "") { }
+            }
+
+            foreach (var element in collection) // Compliant: IsNormalized is a method
+            {
+                var someValue = element.IsNormalized();
+            }
+
+            foreach (var element in collection) // Compliant: indexer is used
+            {
+                var someValue = element[0];
+            }
+
+            foreach (var point in points) // Compliant (FN): when multiple properties are used we can suggest a tupple.
+            {
+                var x = point.X;
+                var y = point.Y;
+            }
+
+            var sum = 0;
+            for (int i = 0; i < values.Length; i++) // Compliant (FN): we can suggest `sum = values.Sum();`
+                sum += values[i];
+
+            for (int i = 0; i < values.Length; i++) // Compliant (FN): we can suggest `sum = values.Aggregate(sum, (current, t) => current - t);`
+                sum -= values[i];
+
+            var min = 0;
+            foreach (var t in values) // Compliant `min` var is changing during the loop. We should sugest Linq Min() instead.
+            {
+                if (min > t)
+                {
+                    min = t;
+                }
+            }
+
+            var max = 0;
+            foreach (var t in values) // Compliant `min` var is changing during the loop. We should sugest Linq Max() instead.
+            {
+                if (max < t)
+                {
+                    max = t;
+                }
+            }
+        }
+
+        private void Foo(int s) { }
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
         }
     }
 }
