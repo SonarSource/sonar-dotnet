f36d3a575b5a11b2026dfa5244801f09e303d029
mary.georgiou <mary.georgiou@sonarsouce.com>
2021-09-08 15:33:59 +0200

    Add Rule S4061: 'params' should be used instead of 'varargs'

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ParamsInsteadOfVarargs.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public static void TestFunc(__arglist) // Noncompliant {{VarArgs calling convention is not CLS compliant, use the 'params' keyword instead}}
+        {
+        }
+
+        public static void TestFunc(int firstVar, string secondVar, __arglist) // Noncompliant
+        {
+        }
+
+    }
+
+}
