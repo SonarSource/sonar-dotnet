54b401e16f33dd1d91047b59b7e19979d4babd36
DGolovinov <64070475+dmitry-golovinov-sonarsource@users.noreply.github.com>
2021-09-08 11:04:59 +0200

    OperationWalker, BasicBlockWalker, CfgAllPathValidator,supplementary properties for Roslyn CFG (#4869)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/CFG/Roslyn/CfgAllPathValidatorTest.cs
@@ -0,0 +1,98 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarAnalyzer.CFG.Roslyn;
+
+namespace SonarAnalyzer.UnitTest.CFG.Roslyn
+{
+    [TestClass]
+    public class CfgAllPathValidatorTest
+    {
+        [TestMethod]
+        public void ValidateCfgPaths()
+        {
+            const string code = @"
+public class Sample
+{
+    public int Method2(bool condition)
+    {
+        if (condition)
+            return 42;
+        else
+            return 1;
+    }
+}";
+            var cfg = TestHelper.CompileCfg(code);
+            /*
+             *           Entry 0
+             *             |
+             *             |
+             *           Block 1
+             *           /   \
+             *     Else /     \ WhenFalse
+             *         /       \
+             *     Block 2    Block 3
+             *        \        /
+             *         \      /
+             *          \    /
+             *           Exit 4
+             */
+            var validator = new TestCfgValidator(cfg, 0, 1, 2);
+            validator.CheckAllPaths().Should().BeTrue();
+
+            // only entry block is valid
+            validator = new TestCfgValidator(cfg, 0);
+            validator.CheckAllPaths().Should().BeTrue();
+
+            // only exit block is valid
+            validator = new TestCfgValidator(cfg, 4);
+            validator.CheckAllPaths().Should().BeFalse();
+
+            var nonEntryBlockValid = new TestNonEntryBlockValidator(cfg);
+            nonEntryBlockValid.CheckAllPaths().Should().BeTrue();
+        }
+
+        private class TestNonEntryBlockValidator : CfgAllPathValidator
+        {
+            public TestNonEntryBlockValidator(ControlFlowGraph cfg) : base(cfg) { }
+
+            protected override bool IsValid(BasicBlock block) => block.Ordinal > 0;
+
+            protected override bool IsInvalid(BasicBlock block) => false;
+        }
+
+        private class TestCfgValidator : CfgAllPathValidator
+        {
+            private readonly int[] validBlocks;
+
+            public TestCfgValidator(ControlFlowGraph cfg, params int[] validBlocks) : base(cfg) =>
+                this.validBlocks = validBlocks;
+
+            protected override bool IsValid(BasicBlock block) =>
+                validBlocks.Contains(block.Ordinal);
+
+            protected override bool IsInvalid(BasicBlock block) =>
+                !validBlocks.Contains(block.Ordinal);
+        }
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Helpers/OperationFinderTest.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using FluentAssertions;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarAnalyzer.CFG.Helpers;
+using StyleCop.Analyzers.Lightup;
+
+namespace SonarAnalyzer.UnitTest.Helpers
+{
+    [TestClass]
+    public class OperationFinderTest
+    {
+        [TestMethod]
+        public void ValidateFinder()
+        {
+            const string code = @"
+public class Sample
+{
+    int field;
+
+    public void Method(bool condition)
+    {
+        if (condition)
+            field = 42 + 43;
+    }
+}";
+            var cfg = TestHelper.CompileCfg(code);
+            var assign = cfg.Blocks[2];
+            var finder = new FirstNumericLiteralFinder();
+            finder.TryFind(cfg.EntryBlock, out var result).Should().BeFalse();
+            result.Should().Be(default);
+            finder.TryFind(assign, out result).Should().BeTrue();
+            result.Should().Be(42);
+        }
+
+        private class FirstNumericLiteralFinder : OperationFinder<int>
+        {
+            protected override bool TryFindOperation(IOperationWrapperSonar operation, out int result)
+            {
+                if (operation.Instance is ILiteralOperation)
+                {
+                    result = (int)operation.Instance.ConstantValue.Value;
+                    return true;
+                }
+                result = default;
+                return false;
+            }
+        }
+    }
+}
