7946e475eedb454e269ec0085885bb7055ea8740
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-09-17 15:14:56 +0200

    Add logging for language versions used in tests

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestSuiteInitialization.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.VisualBasic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarAnalyzer.UnitTest.TestFramework;
+
+namespace SonarAnalyzer.UnitTest
+{
+    [TestClass]
+    public class TestSuiteInitialization
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            Console.WriteLine(@"Running tests initialization...");
+            var parseOptions = ParseOptionsHelper.GetParseOptionsOrDefault(null).ToList();
+
+            var csVersions = parseOptions.OfType<CSharpParseOptions>().Select(GetVersion);
+            Console.WriteLine(@"C# versions used for analysis: " + string.Join(", ", csVersions));
+
+            var vbVersions = parseOptions.OfType<VisualBasicParseOptions>().Select(GetVersion);
+            Console.WriteLine(@"VB.Net versions used for analysis: " + string.Join(", ", vbVersions));
+        }
+
+        private static string GetVersion(CSharpParseOptions options) =>
+            options.LanguageVersion.ToDisplayString();
+
+        private static string GetVersion(VisualBasicParseOptions options) =>
+            options.LanguageVersion.ToDisplayString();
+    }
+}
