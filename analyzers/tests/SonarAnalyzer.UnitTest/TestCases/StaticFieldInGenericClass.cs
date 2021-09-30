4ee027d410d1da49a5d07cbbea3d6b17273c5bd7
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2021-09-30 13:29:54 +0200

    Rule S2743: document false positives (#4929)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/StaticFieldInGenericClass.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/StaticFieldInGenericClass.cs
@@ -17,14 +17,15 @@ namespace Tests.TestCases
         private static Dictionary<string, List<T>> Dict, Dict4 = new Dictionary<string,List<T>>();
         private static Dictionary<string, T[]> Dict2;
 
-        public static string sProp1 { get; set; } //Noncompliant
+        public static string sProp1 { get; set; } // Noncompliant
 //                           ^^^^^^
-        public /*comment */static string sProp2 { get; set; } //Noncompliant {{A static field in a generic type is not shared among instances of different close constructed types.}}
+        public /*comment */ static string sProp2 { get; set; } // Noncompliant {{A static field in a generic type is not shared among instances of different close constructed types.}}
+
         public string sProp3 { get; set; }
 
         public static T tProp { get; set; }
 
-        internal static string sField; //Noncompliant
+        internal static string sField; // Noncompliant
 
         internal string NestedClassUsage => NestedClass.StringField;
 
@@ -33,5 +34,32 @@ namespace Tests.TestCases
         {
             public static readonly string StringField = "String"; // FN
         }
+
+        private static Dictionary<string, string> _attributes = new Dictionary<string, string>(); // Noncompliant
+
+        public static List<int> Numbers { get; } = new List<int>(); // Noncompliant
+
+        public static string State => nameof(State); // Noncompliant - FP
+
+        public static object New => new object(); // Noncompliant - FP, always a new instance
+
+        public static object Y { get { return new object(); } } // Noncompliant - FP, always a new instance
+
+        public static object Z // Noncompliant
+        {
+            get { return new object(); }
+            set { }
+        }
+
+        protected static Dictionary<string, string> Attributes // Noncompliant - FP? It can use only static fields (for which we already raise)
+        {
+            get {
+                if (_attributes == null)
+                    InitColumns();
+                return _attributes;
+            }
+        }
+
+        private static void InitColumns() { }
     }
 }
