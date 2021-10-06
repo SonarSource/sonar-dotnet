2ddaef693dc024c875c31acf968a9730a4d12f50
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-10-06 16:09:25 +0200

    Add initial Net6 solution

---
+++ analyzers/its/sources/Net6/AdHocExample/Class2.cs
@@ -0,0 +1,8 @@
+﻿namespace Csharp10PlayGround
+{
+    internal class Class2
+    {
+        // Global using of List
+        public List<string> Prop { get; set; }
+    }
+}
---
+++ analyzers/its/sources/Net6/AdHocExample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+int a = 5;
+SomeMethod(a == 42);
+
+// CallerArgumentExpression
+void SomeMethod(object param, [CallerArgumentExpression("param")] string message = null)
+{
+    Console.WriteLine(message);
+}
