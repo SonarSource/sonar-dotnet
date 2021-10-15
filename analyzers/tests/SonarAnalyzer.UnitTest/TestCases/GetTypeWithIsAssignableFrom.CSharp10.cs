399b7eef252b2b94cc62ac229acccca76fdcb4a4
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-10-15 13:25:08 +0200

    Review update 1

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/GetTypeWithIsAssignableFrom.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/GetTypeWithIsAssignableFrom.CSharp10.cs
@@ -4,10 +4,11 @@ Fruit f = apple;
 var b = f as Apple is Apple { Prop.Length: 42 };   // Compliant
 b = f as Apple == null;         // Noncompliant
 
-record Fruit { public int[] Prop { get; } }
+record Fruit
+{
+    public int[] Prop { get; }
+}
+
 sealed record Apple : Fruit
 {
-    public string Taste;
-    public string Color;
-    public void Deconstruct(out string x, out string y) => (x, y) = (Taste, Color);
 }
