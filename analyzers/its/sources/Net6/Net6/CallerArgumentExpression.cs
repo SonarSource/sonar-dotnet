530a57c5cfaac4ce50209266850bd44d26fdaae8
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-10-18 15:24:05 +0200

    Review update 2

--- analyzers/its/sources/Net6/Net6/CallerArgumentExpression.cs
+++ analyzers/its/sources/Net6/Net6/CallerArgumentExpression.cs
@@ -2,7 +2,7 @@
 {
     internal class CallerArgumentExpression
     {
-        // Implicit using of CallerArgumentExpression from System.Runtime.CompilerServices
+        // Implicit using of CallerArgumentExpression from System.Runtime.CompilerServices which is declared in `GlobalUsingDirectives.cs`
         public static void SomeMethod(object param, [CallerArgumentExpression("param")] string message = null) =>
             Console.WriteLine(message);
     }
--- analyzers/its/sources/Net6/Net6/ImplicitUsingOfNamespaces.cs
+++ analyzers/its/sources/Net6/Net6/ImplicitUsingOfNamespaces.cs
@@ -2,7 +2,7 @@
 {
     internal class ImplicitUsingOfNamespaces
     {
-        // Implicit using of List from System.Collections.Generic
+        // Implicit using of List from System.Collections.Generic which is declared in `Net6.csproj` - `<ImplicitUsings>`
         public List<string> Prop { get; set; }
     }
 }
