3aaf9e1a660be82590994dfcc224a565644eab1b
Čaba Šagi <csaba.sagi@sonarsource.com>
2021-10-18 18:32:49 +0200

    C

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionsShouldImplementGenericInterface.CSharp9.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Tests.Diagnostics
+{
+    record TestRecord_01 : CollectionBase { } // FN
+
+    record TestRecord_02 : IList { } // FN
+
+    record TestRecord_03 : IEnumerable { } // FN
+
+    record TestRecord_04 : ICollection { } // FN
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RecordStructName.cs
@@ -0,0 +1,34 @@
+﻿namespace t1
+{
+    record struct FSM // FN
+    {
+    }
+    record struct FSM2(string Param); // FN
+}
+namespace t4
+{
+    record struct AbcDEFgh { } // Compliant
+    record struct Ab4DEFgh { } // Compliant
+    record struct Ab4DEFGh { } // FN
+
+    record struct _AbABaa { }  // FN
+
+    record struct 你好 { }      // Compliant
+
+    record struct AbcDEFgh2(string Param); // Compliant
+    record struct Ab4DEFgh2(string Param); // Compliant
+    record struct Ab4DEFGh2(string Param); // FN
+
+    record struct _AbABaa2(string Param);  // FN
+
+    record struct 你好2(string Param);      // Compliant
+}
+
+namespace TestSuffixes
+{
+    record struct IEnumerableExtensionsTest { }              // FN
+    record struct IEnumerableExtensionsTests { }             // FN
+
+    record struct IEnumerableExtensionsTest2(string Param);  // FN
+    record struct IEnumerableExtensionsTests2(string Param); // FN
+}
