6819d4416df4fbe5f86d202674f876db487ded28
mary-georgiou-sonarsource <mary.georgiou@sonarsouce.com>
2021-10-21 13:18:23 +0200

    add ut

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotShiftByZeroOrIntSize.CSharp10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Foo
+    {
+        public int RightShift()
+        {
+
+            int i = 0;
+            (int a, i) = (0, i >> 0); // Noncompliant {{Remove this useless shift by 0.}}
+
+            return 42;
+        }
+    }
+}
