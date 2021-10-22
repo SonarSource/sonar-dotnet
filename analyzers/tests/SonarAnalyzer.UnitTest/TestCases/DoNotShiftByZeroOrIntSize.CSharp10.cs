6fc5e0223d3671419c6849ad1114348ccc2c6670
unknown <mary.georgiou@sonarsource.com>
2021-10-22 14:03:34 +0200

    fix coding style

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotShiftByZeroOrIntSize.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotShiftByZeroOrIntSize.CSharp10.cs
@@ -4,13 +4,10 @@ namespace Tests.Diagnostics
 {
     class Foo
     {
-        public int RightShift()
+        public void RightShift()
         {
-
             int i = 0;
             (int a, i) = (0, i >> 0); // Noncompliant {{Remove this useless shift by 0.}}
-
-            return 42;
         }
     }
 }
