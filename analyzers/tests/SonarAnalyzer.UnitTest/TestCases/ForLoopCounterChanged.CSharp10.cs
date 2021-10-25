bc9d7e900b5c985c3e8aa05149599c3d121d4c3d
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-10-25 11:22:38 +0200

    Add test for S127

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ForLoopCounterChanged.CSharp10.cs
@@ -0,0 +1,10 @@
+﻿public struct S
+{
+    public void LoopCounterChange((int, int) t)
+    {
+        for (int i = 0; i < 42; i++)
+        {
+            (i, var j) = t; // Compliant - FN {{Do not update the loop counter 'i' within the loop body.}}
+        }
+    }
+}
