2eb0428d5249673c9aa3d3c9111f51f8efa1fd1f
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-10-25 11:26:20 +0200

    Cleanup around ForLoopCounterCondition

--- analyzers/src/SonarAnalyzer.CSharp/Rules/ForLoopCounterCondition.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/ForLoopCounterCondition.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Globalization;
@@ -35,62 +36,54 @@ namespace SonarAnalyzer.Rules.CSharp
     [Rule(DiagnosticId)]
     public sealed class ForLoopCounterCondition : SonarDiagnosticAnalyzer
     {
-        internal const string DiagnosticId = "S1994";
+        private const string DiagnosticId = "S1994";
         private const string MessageFormat = "{0}";
-        internal const string MessageFormatNotEmpty =
-            "This loop's stop condition tests {0} but the incrementer updates {1}.";
-        internal const string MessageFormatEmpty =
-            "This loop's stop incrementer updates {0} but the stop condition doesn't test any variables.";
+        private const string MessageFormatNotEmpty = "This loop's stop condition tests {0} but the incrementer updates {1}.";
+        private const string MessageFormatEmpty = "This loop's stop incrementer updates {0} but the stop condition doesn't test any variables.";
 
-        private static readonly DiagnosticDescriptor rule =
-            DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
+        private static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
 
-        protected override void Initialize(SonarAnalysisContext context)
-        {
+        protected override void Initialize(SonarAnalysisContext context) =>
             context.RegisterSyntaxNodeActionInNonGenerated(
                 c =>
                 {
                     var forNode = (ForStatementSyntax)c.Node;
 
                     var incrementedSymbols = GetIncrementorSymbols(forNode, c.SemanticModel).ToList();
-
                     if (!incrementedSymbols.Any())
                     {
                         return;
                     }
 
                     var conditionSymbols = GetReadSymbolsCondition(forNode, c.SemanticModel).ToList();
-
                     if (conditionSymbols.Intersect(incrementedSymbols).Any())
                     {
                         return;
                     }
 
                     var incrementedVariables = string.Join(",", incrementedSymbols
-                        .Select(s => $"'{s.Name}'")
-                        .OrderBy(s => s));
+                                                                .Select(s => $"'{s.Name}'")
+                                                                .OrderBy(s => s));
                     if (conditionSymbols.Any())
                     {
                         var conditionVariables = string.Join(",", conditionSymbols
-                            .Select(s => $"'{s.Name}'")
-                            .OrderBy(s => s));
-                        c.ReportIssue(Diagnostic.Create(rule, forNode.Condition.GetLocation(),
+                                                                  .Select(s => $"'{s.Name}'")
+                                                                  .OrderBy(s => s));
+                        c.ReportIssue(Diagnostic.Create(Rule, forNode.Condition.GetLocation(),
                             string.Format(CultureInfo.InvariantCulture, MessageFormatNotEmpty, conditionVariables, incrementedVariables)));
                     }
                     else
                     {
-                        c.ReportIssue(Diagnostic.Create(rule, forNode.ForKeyword.GetLocation(),
+                        c.ReportIssue(Diagnostic.Create(Rule, forNode.ForKeyword.GetLocation(),
                             string.Format(CultureInfo.InvariantCulture, MessageFormatEmpty, incrementedVariables)));
                     }
-
                 },
                 SyntaxKind.ForStatement);
-        }
 
         private static IEnumerable<ISymbol> GetIncrementorSymbols(ForStatementSyntax forNode,
-            SemanticModel semanticModel)
+                                                                  SemanticModel semanticModel)
         {
             var accessedSymbols = new List<ISymbol>();
             foreach (var dataFlowAnalysis in forNode.Incrementors
@@ -104,19 +97,18 @@ namespace SonarAnalyzer.Rules.CSharp
             return accessedSymbols.Distinct();
         }
 
-        private static IEnumerable<ISymbol> GetReadSymbolsCondition(ForStatementSyntax forNode,
-            SemanticModel semanticModel)
+        private static IEnumerable<ISymbol> GetReadSymbolsCondition(ForStatementSyntax forNode, SemanticModel semanticModel)
         {
             if (forNode.Condition == null)
             {
-                return new ISymbol[0];
+                return Array.Empty<ISymbol>();
             }
 
             var dataFlowAnalysis = semanticModel.AnalyzeDataFlow(forNode.Condition);
 
             return dataFlowAnalysis.Succeeded
                 ? dataFlowAnalysis.ReadInside.Distinct()
-                : new ISymbol[0];
+                : Array.Empty<ISymbol>();
         }
     }
 }
