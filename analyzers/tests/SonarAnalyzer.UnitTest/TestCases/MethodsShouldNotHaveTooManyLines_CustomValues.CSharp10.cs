19c2c2dd1b6f717b46fa623d4a36f96b3872c71d
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-10-28 14:15:15 +0200

    Add test cases for S138

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodsShouldNotHaveTooManyLines_CustomValues.CSharp10.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class A
+{
+    public A(int i, int j) // Noncompliant
+    {
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(j);
+    }
+}
+
+public record class B
+{
+    public B(int i, int j) // Noncompliant
+    {
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(j);
+    }
+}
+
+public struct C
+{
+    public C(int i, int j) // Noncompliant
+    {
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(j);
+    }
+}
+
+public record struct D
+{
+    public D(int i, int j) // Noncompliant
+    {
+        Console.WriteLine(i);
+        Console.WriteLine(j);
+        Console.WriteLine(j);
+    }
+}
