e38ac8368cbc334cc5ceea2446d797f1f7a19c0c
unknown <mary.georgiou@sonarsource.com>
2021-10-29 16:47:00 +0200

    fix UT

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnchangedLocalVariablesShouldBeConst.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnchangedLocalVariablesShouldBeConst.CSharp10.cs
@@ -5,6 +5,5 @@ int target = 32; // Noncompliant {{Add the 'const' modifier to 'target'.}}
 int usedTarget = 40; // Compliant
 const int alreadyConst = 32;
 
-(int i, int b) = (target, target);
-(usedTarget, i) = (target, target);
-
+(int i, usedTarget) = (target, target);
+(usedTarget, int k) = (alreadyConst, alreadyConst);
