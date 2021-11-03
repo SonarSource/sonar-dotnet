4550560c2e50c18fbb070ffff99e3c32a8dc00ee
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2021-11-03 13:05:22 +0100

    Add C#10 UT for RedundantDeclaration rule. (#5024)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantDeclaration.CSharp10.Fixed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Diagnostics
+{
+    abstract class RedundantDeclaration
+    {
+        public void M()
+        {
+            Test(null, () => true); // Fixed
+        }
+
+        public abstract void Test(object o, Delegate f);
+        public delegate bool BoolDelegate();
+    }
+}
