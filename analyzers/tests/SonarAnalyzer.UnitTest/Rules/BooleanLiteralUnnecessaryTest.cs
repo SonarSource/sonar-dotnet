67849b7004aa901388c63ba4fb57de00509bc849
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2021-11-16 11:49:45 +0100

    Add repro for codefix provider issue S1125 (#5058)
    Co-authored-by: Costin Zaharia <costin.zaharia@sonarsource.com>

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/BooleanLiteralUnnecessaryTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/BooleanLiteralUnnecessaryTest.cs
@@ -34,10 +34,24 @@ namespace SonarAnalyzer.UnitTest.Rules
 
         [TestMethod]
         public void BooleanLiteralUnnecessary_CodeFix_CS() =>
-            Verifier.VerifyCodeFix(@"TestCases\BooleanLiteralUnnecessary.cs",
-                                   @"TestCases\BooleanLiteralUnnecessary.Fixed.cs",
-                                   new CS.BooleanLiteralUnnecessary(),
-                                   new CS.BooleanLiteralUnnecessaryCodeFixProvider());
+            Verifier.VerifyCodeFix(
+                @"TestCases\BooleanLiteralUnnecessary.cs",
+                @"TestCases\BooleanLiteralUnnecessary.Fixed.cs",
+                new CS.BooleanLiteralUnnecessary(),
+                new CS.BooleanLiteralUnnecessaryCodeFixProvider());
+
+        [TestMethod]
+        public void BooleanLiteralUnnecessary_CSharp8() =>
+            Verifier.VerifyAnalyzer(@"TestCases\BooleanLiteralUnnecessary.CSharp8.cs", new CS.BooleanLiteralUnnecessary(), ParseOptionsHelper.FromCSharp8);
+
+        [TestMethod]
+        public void BooleanLiteralUnnecessary_CodeFix_CSharp8() =>
+            Verifier.VerifyCodeFix(
+                @"TestCases\BooleanLiteralUnnecessary.CSharp8.cs",
+                @"TestCases\BooleanLiteralUnnecessary.CSharp8.Fixed.cs",
+                new CS.BooleanLiteralUnnecessary(),
+                new CS.BooleanLiteralUnnecessaryCodeFixProvider(),
+                ParseOptionsHelper.FromCSharp8);
 
         [TestMethod]
         public void BooleanLiteralUnnecessary_VB() =>
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/BooleanLiteralUnnecessary.CSharp8.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Diagnostics
+{
+    // Repro for: https://github.com/SonarSource/sonar-dotnet/issues/5013
+    public class CodeFixProviderRepro
+    {
+        public static bool Foo(string? x, bool y) => x == null ? false : y; // Noncompliant
+    }
+}
