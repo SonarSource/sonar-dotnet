1779be2cb1537c0f54984e76ec30ed30cc084939
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-11-16 14:50:04 +0100

    Implement WhiteSource scan

---
+++ scripts/whitesource/WhiteSource-Scan.ps1
@@ -0,0 +1,41 @@
+param (
+    [Parameter(Mandatory = $true,
+               HelpMessage = "Config file path",
+               Position = 0)]
+    [System.IO.FileInfo]
+    $configPath
+)
+
+function Get-Version {
+    $versionPropsXml = [xml](Get-Content -Path scripts/version/Version.props)
+    return $versionPropsXml.Project.PropertyGroup.MilestoneVersion
+}
+
+# Delete this step after the new image is finished and can be used
+Write-host "Create tools directory"
+$toolsPath = "C:\tools"
+if (-Not [System.IO.Directory]::Exists($toolsPath)){
+  New-Item -Path "C:\" -Name "tools" -ItemType "directory"
+}
+
+# Delete this step after the new image is finished and can be used
+Write-host "Download WhiteSource tool"
+$whiteSourceAgentPath = "$toolsPath\wss-unified-agent.jar"
+Invoke-WebRequest -Uri https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar -OutFile $whiteSourceAgentPath
+
+Write-Host "Validating WhiteSource agent certificate signature..."
+$cert = 'Signed by "CN=whitesource software inc, O=whitesource software inc, STREET=79 Madison Ave, L=New York, ST=New York, OID.2.5.4.17=10016, C=US"'
+if (-Not (& "$env:JAVA\bin\jarsigner.exe" -verify -strict -verbose $whiteSourceAgentPath |  Select-String -Pattern $cert -CaseSensitive -Quiet)){
+  Write-Host "wss-unified-agent.jar signature verification failed."
+  exit 1
+}
+
+# WhiteSource agent needs the following environment variables:
+# - WS_APIKEY
+# - WS_PRODUCTNAME
+# - WS_PROJECTNAME
+
+$env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $(Get-Version)"
+
+Write-Host "Running the WhiteSource unified agent for $env:WS_PROJECTNAME..."
+& "$env:JAVA\bin\java.exe" -jar $env:WHITESOURCE_AGENT_PATH -c $configPath
