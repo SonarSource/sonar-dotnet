322ea641bb340ac9d292e3df660adf082864a7af
Corniel Nobel <mail@corniel.nl>
2021-11-24 16:46:28 +0100

    push

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotCheckZeroSizeCollection.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotCheckZeroSizeCollection.cs
@@ -61,7 +61,7 @@ namespace Tests.Diagnostics
             result = someEnumerable.Count() >= ConstField_NonZero;
             result = someEnumerable.Count() >= ConstField_Zero; // Noncompliant
             result = someEnumerable.Count() >= localConst_NonZero;
-            result = someEnumerable.Count() >= ConstField_Double_Zero; // Compliant, double is ignored
+            result = someEnumerable.Count() >= ConstField_Double_Zero; // Compliant, double is ignored, arguably FN
 
             result = (someEnumerable.Count()) >= (0); // Noncompliant
             result = ((((someEnumerable).Count())) >= ((0))); // Noncompliant
