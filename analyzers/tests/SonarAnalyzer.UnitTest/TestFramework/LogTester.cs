da735286bb41f22151107be68f2153a8298650d5
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-11-29 10:53:46 +0100

    Add UTs for SnippetCompiler

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/LogTester.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2021 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
+using System.IO;
+using FluentAssertions;
+
+namespace SonarAnalyzer.UnitTest.TestFramework
+{
+    internal sealed class LogTester : IDisposable
+    {
+        private readonly TextWriter originalOut;
+        private readonly TextWriter originalError;
+        private readonly StringWriter outWriter = new ();
+        private readonly StringWriter errorWriter = new ();
+
+        public LogTester()
+        {
+            originalOut = Console.Out;
+            Console.SetOut(outWriter);
+            originalError = Console.Error;
+            Console.SetError(errorWriter);
+        }
+
+        public void AssertContain(string value) =>
+            outWriter.ToString().Should().Contain(value);
+
+        public void AssertContainError(string value) =>
+            errorWriter.ToString().Should().Contain(value);
+
+        public void Dispose()
+        {
+            Console.SetOut(originalOut);
+            Console.SetError(originalError);
+        }
+    }
+}
