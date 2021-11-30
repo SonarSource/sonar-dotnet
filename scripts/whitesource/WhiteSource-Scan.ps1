a345937499289e454b81934290d33b130ee1ca06
Costin Zaharia <costin.zaharia@sonarsource.com>
2021-11-30 09:15:09 +0100

    Do not download WhiteSource tool
    - it is already installed on the machine

--- scripts/whitesource/WhiteSource-Scan.ps1
+++ scripts/whitesource/WhiteSource-Scan.ps1
@@ -3,25 +3,6 @@ function Get-Version {
     return $versionPropsXml.Project.PropertyGroup.MilestoneVersion
 }
 
-# Delete this step after the new image is finished and can be used
-Write-host "Create tools directory"
-$toolsPath = "C:\tools"
-if (-Not (Test-Path $toolsPath)){
-  New-Item -Path $toolsPath -ItemType "directory"
-}
-
-# Delete this step after the new image is finished and can be used
-Write-host "Download WhiteSource tool"
-$whiteSourceAgentPath = "$toolsPath\wss-unified-agent.jar"
-Invoke-WebRequest -Uri https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar -OutFile $whiteSourceAgentPath
-
-Write-Host "Validating WhiteSource agent certificate signature..."
-$cert = 'Signed by "CN=whitesource software inc, O=whitesource software inc, STREET=79 Madison Ave, L=New York, ST=New York, OID.2.5.4.17=10016, C=US"'
-if (-Not (& "$env:JAVA\bin\jarsigner.exe" -verify -strict -verbose $whiteSourceAgentPath |  Select-String -Pattern $cert -CaseSensitive -Quiet)){
-  Write-Host "wss-unified-agent.jar signature verification failed."
-  exit 1
-}
-
 # WhiteSource agent needs the following environment variables:
 # - WS_APIKEY
 # - WS_PRODUCTNAME
