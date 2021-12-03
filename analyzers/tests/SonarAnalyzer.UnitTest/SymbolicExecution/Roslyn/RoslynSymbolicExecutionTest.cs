ae9983d61994150cd756fa5c0683610ff0522da2
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-12-03 10:05:02 +0100

    Review update

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.cs
@@ -34,7 +34,7 @@ namespace SonarAnalyzer.UnitTest.SymbolicExecution.Roslyn
         [TestMethod]
         public void Constructor_Throws()
         {
-            var cfg = TestHelper.CompileCfg("public class Sample { public void Main() { } }", AnalyzerLanguage.CSharp);
+            var cfg = TestHelper.CompileCfgCS("public class Sample { public void Main() { } }");
             var check = new Mock<SymbolicExecutionCheck>().Object;
             ((Action)(() => new RoslynSymbolicExecution(null, new[] { check }))).Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("cfg");
             ((Action)(() => new RoslynSymbolicExecution(cfg, null))).Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("checks");
