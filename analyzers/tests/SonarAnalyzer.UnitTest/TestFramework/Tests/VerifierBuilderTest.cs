c86352cdf970c8888d8fe3e73829c03a08ce6136
Pavel Mikula <pavel.mikula@sonarsource.com>
2021-12-20 15:53:50 +0100

    Implement basic Builder

--- analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/Tests/VerifierBuilderTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/Tests/VerifierBuilderTest.cs
@@ -18,14 +18,48 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarAnalyzer.UnitTest.MetadataReferences;
 
 namespace SonarAnalyzer.UnitTest.TestFramework.Tests
 {
     [TestClass]
     public partial class VerifierBuilderTest
     {
+        private static readonly VerifierBuilder<DummyAnalyzer> Empty = new();
+
+        [TestMethod]
+        public void AddPaths_Concatenates_IsImmutable()
+        {
+            var single = Empty.AddPaths("First");
+            var three = single.AddPaths("Second", "Third");
+            Empty.Paths.Should().BeEmpty();
+            single.Paths.Should().ContainSingle().Which.Should().Be("First");
+            three.Paths.Should().BeEquivalentTo("First", "Second", "Third");
+        }
+
+        [TestMethod]
+        public void AddReferences_Concatenates_IsImmutable()
+        {
+            var single = Empty.AddReferences(MetadataReferenceFacade.MsCorLib);
+            var two = single.AddReferences(MetadataReferenceFacade.SystemData);
+            Empty.References.Should().BeEmpty();
+            single.References.Should().BeEquivalentTo(MetadataReferenceFacade.MsCorLib).And.HaveCount(1);
+            two.References.Should().BeEquivalentTo(MetadataReferenceFacade.MsCorLib.Concat(MetadataReferenceFacade.SystemData)).And.HaveCount(2);
+        }
+
+        [TestMethod]
+        public void WithOptions_Overrides_IsImmutable()
+        {
+            var first = Empty.WithOptions(ParseOptionsHelper.OnlyCSharp7);
+            var second = first.WithOptions(ParseOptionsHelper.FromCSharp8);
+            Empty.ParseOptions.Should().BeEmpty();
+            first.ParseOptions.Should().BeEquivalentTo(ParseOptionsHelper.OnlyCSharp7);
+            second.ParseOptions.Should().BeEquivalentTo(ParseOptionsHelper.FromCSharp8);
+        }
+
         [TestMethod]
         public void Build_ReturnsVerifier() =>
             new VerifierBuilder<DummyAnalyzer>().Build().Should().NotBeNull();
--- analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/VerifierBuilder.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/VerifierBuilder.cs
@@ -18,6 +18,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace SonarAnalyzer.UnitTest.TestFramework
@@ -28,7 +32,30 @@ namespace SonarAnalyzer.UnitTest.TestFramework
     internal class VerifierBuilder<TAnalyzer>
         where TAnalyzer : DiagnosticAnalyzer, new()
     {
+        // All properties are (and should be) immutable.
+        public ImmutableArray<string> Paths { get; init; } = ImmutableArray<string>.Empty;
+        public ImmutableArray<MetadataReference> References { get; init; } = ImmutableArray<MetadataReference>.Empty;
+        public ImmutableArray<ParseOptions> ParseOptions { get; init; } = ImmutableArray<ParseOptions>.Empty;
+
+        public VerifierBuilder() { }
+
+        private VerifierBuilder(VerifierBuilder<TAnalyzer> original)
+        {
+            Paths = original.Paths;
+            References = original.References;
+            ParseOptions = original.ParseOptions;
+        }
+
+        public VerifierBuilder<TAnalyzer> AddPaths(params string[] paths) =>
+            new(this) { Paths = Paths.Concat(paths).ToImmutableArray() };
+
+        public VerifierBuilder<TAnalyzer> AddReferences(IEnumerable<MetadataReference> references) =>
+            new(this) { References = References.Concat(references).ToImmutableArray() };
+
+        public VerifierBuilder<TAnalyzer> WithOptions(ImmutableArray<ParseOptions> parseOptions) =>
+            new(this) { ParseOptions = parseOptions };
+
         public Verifier<TAnalyzer> Build() =>
-            new Verifier<TAnalyzer>(this);
+            new(this);
     }
 }
