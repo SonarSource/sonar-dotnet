209a5b292dd7d1807374242cf73f0b4619c934d2
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2022-01-18 14:06:29 +0100

    Fix S2197 FP: False Positive when collection size methods or properties are used (#5268)

--- analyzers/src/SonarAnalyzer.CSharp/Rules/EqualityOnModulus.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/EqualityOnModulus.cs
@@ -18,7 +18,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -34,7 +37,15 @@ namespace SonarAnalyzer.Rules.CSharp
         private const string DiagnosticId = "S2197";
         private const string MessageFormat = "The result of this modulus operation may not be {0}.";
 
-        private static readonly CSharpExpressionNumericConverter ExpressionNumericConverter = new CSharpExpressionNumericConverter();
+        private const string CountName = nameof(Enumerable.Count);
+        private const string LongCountName = nameof(Enumerable.LongCount);
+        private const string LengthName = "Length"; // Represents Array.Length and String.Length
+        private const string LongLengthName = nameof(Array.LongLength);
+        private const string ListCapacityName = nameof(List<object>.Capacity);
+
+        private static readonly string[] CollectionSizePropertyOrMethodNames = { CountName, LongCountName, LengthName, LongLengthName, ListCapacityName };
+
+        private static readonly CSharpExpressionNumericConverter ExpressionNumericConverter = new();
 
         private static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
 
@@ -50,27 +61,57 @@ namespace SonarAnalyzer.Rules.CSharp
             if (CheckExpression(equalsExpression.Left, equalsExpression.Right, c.SemanticModel, out var constantValue)
                 || CheckExpression(equalsExpression.Right, equalsExpression.Left, c.SemanticModel, out constantValue))
             {
-                c.ReportIssue(Diagnostic.Create(Rule,
-                                                               equalsExpression.GetLocation(),
-                                                               constantValue < 0 ? "negative" : "positive"));
+                c.ReportIssue(Diagnostic.Create(Rule, equalsExpression.GetLocation(), constantValue < 0 ? "negative" : "positive"));
             }
         }
 
-        private static bool CheckExpression(SyntaxNode node, ExpressionSyntax modulus, SemanticModel semanticModel, out int constantValue) =>
+        private static bool CheckExpression(SyntaxNode node, ExpressionSyntax expression, SemanticModel semanticModel, out int constantValue) =>
             ExpressionNumericConverter.TryGetConstantIntValue(node, out constantValue)
             && constantValue != 0
-            && IsModulus(modulus)
-            && !IsUnsigned(modulus, semanticModel);
+            && expression.RemoveParentheses() is BinaryExpressionSyntax binary
+            && binary.IsKind(SyntaxKind.ModuloExpression)
+            && !ExpressionIsAlwaysPositive(binary, semanticModel);
 
-        private static bool IsModulus(ExpressionSyntax expression) =>
-            expression.RemoveParentheses() is BinaryExpressionSyntax binary
-            && binary.IsKind(SyntaxKind.ModuloExpression);
-
-        private static bool IsUnsigned(ExpressionSyntax expression, SemanticModel semantic)
+        private static bool ExpressionIsAlwaysPositive(BinaryExpressionSyntax binaryExpression, SemanticModel semantic)
         {
-            var type = semantic.GetTypeInfo(expression).Type;
-            return type.IsAny(KnownType.UnsignedIntegers)
-                   || type.Is(KnownType.System_UIntPtr);
+            var type = semantic.GetTypeInfo(binaryExpression).Type;
+            if (type.IsAny(KnownType.UnsignedIntegers) || type.Is(KnownType.System_UIntPtr))
+            {
+                return true;
+            }
+
+            var leftExpression = binaryExpression.Left;
+            var leftExpressionStringForm = leftExpression.ToString();
+            return CollectionSizePropertyOrMethodNames.Any(x => leftExpressionStringForm.Contains(x))
+                   && semantic.GetSymbolInfo(leftExpression).Symbol is { } symbol
+                   && IsCollectionSize(symbol);
         }
+
+        private static bool IsCollectionSize(ISymbol symbol) =>
+            IsEnumerableCountMethod(symbol)
+            || (symbol is IPropertySymbol propertySymbol
+                && (IsLengthProperty(propertySymbol)
+                    || IsCollectionCountProperty(propertySymbol)
+                    || IsListCapacityProperty(propertySymbol)));
+
+        private static bool IsEnumerableCountMethod(ISymbol symbol) =>
+            (CountName.Equals(symbol.Name) || LongCountName.Equals(symbol.Name))
+            && symbol is IMethodSymbol methodSymbol
+            && methodSymbol.IsExtensionMethod
+            && methodSymbol.ReceiverType != null
+            && methodSymbol.IsExtensionOn(KnownType.System_Collections_Generic_IEnumerable_T);
+
+        private static bool IsLengthProperty(IPropertySymbol propertySymbol) =>
+            (LengthName.Equals(propertySymbol.Name) || LongLengthName.Equals(propertySymbol.Name))
+            && propertySymbol.ContainingType.IsAny(KnownType.System_Array, KnownType.System_String);
+
+        private static bool IsCollectionCountProperty(IPropertySymbol propertySymbol) =>
+            CountName.Equals(propertySymbol.Name)
+            && (propertySymbol.ContainingType.Implements(KnownType.System_Collections_Generic_ICollection_T)
+                || propertySymbol.ContainingType.Implements(KnownType.System_Collections_Generic_IReadOnlyCollection_T));
+
+        private static bool IsListCapacityProperty(IPropertySymbol propertySymbol) =>
+            ListCapacityName.Equals(propertySymbol.Name)
+            && propertySymbol.ContainingType.Implements(KnownType.System_Collections_Generic_IList_T);
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EqualityOnModulus.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EqualityOnModulus.cs
@@ -20,11 +20,36 @@ namespace Tests.Diagnostics
             var xx = unsignedY % 4 == 1; // Compliant
 
             var array = new[] {1};
-            y = array.Length % 2 == 1; // Noncompliant - FP: array Length is > 0;
+            y = array.Length % 2 == 1; // Compliant: array Length is > 0;
+            y = array.LongLength % 2 == 1; // Compliant: array Length is > 0;
 
             IEnumerable<int> enumerable = array;
-            y = enumerable.Count() % 2 == 1; // Noncompliant - FP: IEnumerable Count is > 0;
-            y = enumerable.LongCount() % 2 == 1; // Noncompliant - FP: IEnumerable LongCount is > 0;
+            y = enumerable.Count() % 2 == 1; // Compliant - IEnumerable Count is > 0;
+            y = enumerable.LongCount() % 2 == 1; // Compliant - IEnumerable LongCount is > 0;
+            y = enumerable.Average() % 2 == 1; // Noncompliant
+
+            var list = new List<int>();
+            y = list.Count % 2 == 1; // Compliant - Count property is > 0;
+            y = list.Capacity % 2 == 1; // Compliant - Capacityproperty is > 0;
+
+            var someString = "HelloImAWhale";
+            y = someString.Length % 2 == 1; // Compliant - Length property is > 0;
+
+            y = FakeCount() % 2 == 1; // Noncompliant
+
+            IEnumerable<long> enumerableLong = new List<long>();
+            y = enumerableLong.Count() % 2 == 1; // Compliant - IEnumerable Count is > 0;
+            y = enumerableLong.LongCount() % 2 == 1; // Compliant - IEnumerable LongCount is > 0;
+
+            var fakeList = new FakeList();
+            y = fakeList.Count() % 2 == 1; // Compliant - Count property is inherited from List and is > 0;
+            y = fakeList.Count(1) % 2 == 1; // Noncompliant - Count method is not inherited from List
         }
+
+        public int FakeCount() => -1;
+    }
+
+    public class FakeList : List<int> {
+        public int Count(int i) { return i; }
     }
 }
