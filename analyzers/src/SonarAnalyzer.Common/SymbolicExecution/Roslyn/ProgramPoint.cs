3061c511282cd27708389e223b69974e0f04dd25
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-01-27 15:43:35 +0100

    Implement IEquatable for ExplodedNode

---
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/ProgramPoint.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarAnalyzer.CFG.Roslyn;
+
+namespace SonarAnalyzer.SymbolicExecution.Roslyn
+{
+    public static class ProgramPoint
+    {
+        private const int BlockIndexBits = 10;  // Hash is 10 bits of block-index followed by 22 bits of instruction-index
+        private const int BlockCountMax = 1 << BlockIndexBits;
+        private const int BitShift = 32 - BlockIndexBits;
+
+        public static bool HasSupportedSize(ControlFlowGraph cfg) =>
+            cfg.Blocks.Length <= BlockCountMax;
+
+        public static int Hash(BasicBlock block, int index) =>
+            (int)(((uint)block.Ordinal << BitShift) + (uint)index);
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/ProgramPointTest.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Linq;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarAnalyzer.Helpers;
+using SonarAnalyzer.SymbolicExecution.Roslyn;
+
+namespace SonarAnalyzer.UnitTest.SymbolicExecution.Roslyn
+{
+    [TestClass]
+    public class ProgramPointTest
+    {
+        [TestMethod]
+        public void HasSupportedSize_Supported()
+        {
+            var cfg = TestHelper.CompileCfgBodyCS("var a = true;");
+            cfg.Blocks.Should().HaveCount(3);
+            ProgramPoint.HasSupportedSize(cfg).Should().BeTrue();
+
+            cfg = TestHelper.CompileCfgBodyCS($"var a = true{Enumerable.Repeat(" && true", 1019).JoinStr(null)};");
+            cfg.Blocks.Should().HaveCount(1024);
+            ProgramPoint.HasSupportedSize(cfg).Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void HasSupportedSize_Unsupported()
+        {
+            var cfg = TestHelper.CompileCfgBodyCS($"var a = true{Enumerable.Repeat(" && true", 1020).JoinStr(null)};");
+            cfg.Blocks.Should().HaveCount(1025);
+            ProgramPoint.HasSupportedSize(cfg).Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void Hash()
+        {
+            var cfg = TestHelper.CompileCfgBodyCS("var a = true;");
+            cfg.Blocks.Should().HaveCount(3);
+
+            ProgramPoint.Hash(cfg.Blocks[0], 0).Should().Be(0);
+            ProgramPoint.Hash(cfg.Blocks[0], 1).Should().Be(1);
+            ProgramPoint.Hash(cfg.Blocks[0], 2).Should().Be(2);
+            ProgramPoint.Hash(cfg.Blocks[0], 4).Should().Be(4);
+            ProgramPoint.Hash(cfg.Blocks[0], 42).Should().Be(42);
+            ProgramPoint.Hash(cfg.Blocks[0], 65535).Should().Be(65535);
+            ProgramPoint.Hash(cfg.Blocks[0], 0b1111111111111111111111).Should().Be(4194303);
+
+            ProgramPoint.Hash(cfg.Blocks[1], 0).Should().Be(4194304 + 0);
+            ProgramPoint.Hash(cfg.Blocks[1], 1).Should().Be(4194304 + 1);
+            ProgramPoint.Hash(cfg.Blocks[1], 2).Should().Be(4194304 + 2);
+
+            ProgramPoint.Hash(cfg.Blocks[2], 0).Should().Be(4194304 * 2 + 0);
+            ProgramPoint.Hash(cfg.Blocks[2], 1).Should().Be(4194304 * 2 + 1);
+            ProgramPoint.Hash(cfg.Blocks[2], 2).Should().Be(4194304 * 2 + 2);
+        }
+    }
+}
