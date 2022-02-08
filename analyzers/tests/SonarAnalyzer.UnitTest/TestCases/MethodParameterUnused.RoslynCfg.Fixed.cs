ffe7d70a60115bc82899ed82e24852ad22033810
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-02-08 11:11:32 +0100

    S1172: Add FP repro for #5338 (#5364)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodParameterUnused.RoslynCfg.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodParameterUnused.RoslynCfg.Fixed.cs
@@ -593,4 +593,22 @@ namespace Tests.TestCases
             }
         }
     }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/5338
+    public class Repro_5338
+    {
+        private string[] largerArray;
+
+        // This case is very similar to our MethodParameterUnused.RoslynCfg.Fixed.cs, but it reproduces on .NET build as well.
+        // https://github.com/dotnet/roslyn/issues/56644
+        private bool HasAny(string[] smallerOrEqualArray)       // Fixed
+        {
+            return largerArray.Any(smallerOrEqualArray.Contains);
+        }
+
+        private static double GetDegreeOfOverlap(string[] largerArray, string[] smallerOrEqualArray)    // Fixed
+        {
+            return (double)largerArray.Count(smallerOrEqualArray.Contains) / largerArray.Length;
+        }
+    }
 }
