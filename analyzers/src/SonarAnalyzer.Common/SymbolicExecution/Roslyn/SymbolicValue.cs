0ec732ec06f3b9687ec1bb9dd1507ed4455b667a
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-02-09 17:21:26 +0100

    Rework SymbolicValue immutability

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/SymbolicValue.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/SymbolicValue.cs
@@ -19,57 +19,61 @@
  */
 
 using System;
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using SonarAnalyzer.Helpers;
 
 namespace SonarAnalyzer.SymbolicExecution.Roslyn
 {
-    public class SymbolicValue : IEquatable<SymbolicValue>
+    public record SymbolicValue : IEquatable<SymbolicValue>
     {
+        private readonly SymbolicValueCounter counter;
         private readonly int identifier;    // This is debug information that is intentionally excluded from GetHashCode and Equals
-        private readonly Lazy<Dictionary<Type, SymbolicConstraint>> constraints = new(() => new());  // SymbolicValue can have only one constraint instance of specific type at a time
+        // SymbolicValue can have only one constraint instance of specific type at a time
+        private ImmutableDictionary<Type, SymbolicConstraint> Constraints { get; init; } = ImmutableDictionary<Type, SymbolicConstraint>.Empty;
 
-        public SymbolicValue(SymbolicValueCounter counter) =>
+        public SymbolicValue(SymbolicValueCounter counter)
+        {
+            this.counter = counter;
             identifier = counter.NextIdentifier();
+        }
+
+        protected SymbolicValue(SymbolicValue original) // Custom record copying constructor
+        {
+            counter = original.counter;
+            identifier = counter.NextIdentifier();
+            Constraints = original.Constraints;
+        }
 
         public override string ToString()
         {
             var ret = new StringBuilder();
             ret.Append("SV_").Append(identifier);
-            if (constraints.Value.Any())
+            if (Constraints.Any())
             {
-                ret.Append(": ").Append(constraints.Value.Values.JoinStr(", ", x => x.ToString()));
+                ret.Append(": ").Append(Constraints.Values.JoinStr(", ", x => x.ToString()));
             }
             return ret.ToString();
         }
 
-        public void SetConstraint(SymbolicConstraint constraint) =>
-            constraints.Value[constraint.GetType()] = constraint;
+        public SymbolicValue WithConstraint(SymbolicConstraint constraint) =>
+            this with { Constraints = Constraints.SetItem(constraint.GetType(), constraint) };
 
-        public void RemoveConstraint(SymbolicConstraint constraint)
-        {
-            if (HasConstraint(constraint))
-            {
-                constraints.Value.Remove(constraint.GetType());
-            }
-        }
+        public SymbolicValue WithoutConstraint(SymbolicConstraint constraint) =>
+            HasConstraint(constraint)
+                ? this with { Constraints = Constraints.Remove(constraint.GetType()) }
+                : this;
 
         public bool HasConstraint<T>() where T : SymbolicConstraint =>
-            constraints.Value.ContainsKey(typeof(T));
+            Constraints.ContainsKey(typeof(T));
 
         public bool HasConstraint(SymbolicConstraint constraint) =>
-            constraints.Value.TryGetValue(constraint.GetType(), out var current) && constraint == current;
+            Constraints.TryGetValue(constraint.GetType(), out var current) && constraint == current;
 
         public override int GetHashCode() => 0; // We can't calculate stable hash code. This class is not supposed to be used as a key in sets and dictionaries.
 
-        public override bool Equals(object obj) =>
-            Equals(obj as SymbolicValue);
-
         public virtual bool Equals(SymbolicValue other) =>
-            other is not null
-            && other.constraints.IsValueCreated == constraints.IsValueCreated
-            && (!constraints.IsValueCreated || other.constraints.Value.DictionaryEquals(constraints.Value));
+            other is not null && other.Constraints.DictionaryEquals(Constraints);
     }
 }
