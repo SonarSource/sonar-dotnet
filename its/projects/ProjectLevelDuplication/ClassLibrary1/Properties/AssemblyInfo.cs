1eef25a81f31bc34978e390e74f291aab39d98a5
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2022-02-25 10:35:57 +0100

    Java IT that documents the current de-duplication logic (#5423)

---
+++ its/projects/ProjectLevelDuplication/ClassLibrary1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3bd80bd5-b567-4a88-b921-05bef50eec90")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ its/src/test/java/com/sonar/it/csharp/ProjectLevelDuplicationTest.java
@@ -0,0 +1,59 @@
+/*
+ * SonarSource :: C# :: ITs :: Plugin
+ * Copyright (C) 2011-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonar.it.csharp;
+
+import com.sonar.it.shared.TestUtils;
+import com.sonar.orchestrator.build.BuildResult;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.sonarqube.ws.Issues;
+
+import static com.sonar.it.csharp.Tests.ORCHESTRATOR;
+import static com.sonar.it.csharp.Tests.getComponent;
+import static com.sonar.it.csharp.Tests.getIssues;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProjectLevelDuplicationTest {
+
+  @Rule
+  public TemporaryFolder temp = TestUtils.createTempFolder();
+
+  @Before
+  public void init() {
+    TestUtils.reset(ORCHESTRATOR);
+  }
+
+  @Test
+  public void containsOnlyOneProjectLevelIssue() throws Exception {
+    BuildResult buildResult = Tests.analyzeProject(temp, "ProjectLevelDuplication", null);
+
+    assertThat(getComponent("ProjectLevelDuplication")).isNotNull();
+
+    List<Issues.Issue> issues = getIssues("ProjectLevelDuplication")
+      .stream()
+      .filter(x -> x.getRule().startsWith("csharpsquid:"))
+      .collect(Collectors.toList());
+    assertThat(issues).hasSize(1);
+  }
+}
