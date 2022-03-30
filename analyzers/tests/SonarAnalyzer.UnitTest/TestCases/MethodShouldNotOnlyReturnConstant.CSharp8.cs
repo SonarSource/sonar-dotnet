734e1c4ee04af908e4b19bdbf9f6979415eec1b4
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2022-03-30 15:54:32 +0200

    Rule S3400: Fix FP when using default interface implementation (#5516)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodShouldNotOnlyReturnConstant.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodShouldNotOnlyReturnConstant.CSharp8.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tests.Diagnostics
 {
     interface ISomeInterface {
-        public bool IsCondition() => true; // Noncompliant - FP, see: https://github.com/SonarSource/sonar-dotnet/issues/5498
+        public bool IsCondition() => true; // Compliant, see: https://github.com/SonarSource/sonar-dotnet/issues/5498
     }
 }
