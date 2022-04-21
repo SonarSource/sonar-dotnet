a957b0de5c197c1571fdda84e6163b0e912aa7fb
Corniel Nobel <mail@corniel.nl>
2022-04-21 09:35:40 +0200

    Update analyzers/src/SonarAnalyzer.Common/Facade/ISyntaxKindFacade.cs
    Co-authored-by: Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>

--- analyzers/src/SonarAnalyzer.Common/Facade/ISyntaxKindFacade.cs
+++ analyzers/src/SonarAnalyzer.Common/Facade/ISyntaxKindFacade.cs
@@ -40,6 +40,6 @@ namespace SonarAnalyzer.Helpers.Facade
         abstract TSyntaxKind SimpleMemberAccessExpression { get; }
         abstract TSyntaxKind StringLiteralExpression { get; }
         abstract TSyntaxKind[] TypeDeclaration { get; }
-        abstract TSyntaxKind[] DefaultExpressions{ get; }
+        abstract TSyntaxKind[] DefaultExpressions { get; }
     }
 }
