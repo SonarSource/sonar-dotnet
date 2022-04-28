83b04509de87218c6aad943429ce2d37b5cf3cee
Martin Strecker <martin.strecker@sonarsource.com>
2022-04-28 18:55:05 +0200

    Make tests fail for record structs by removing top-level-statements.

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/StringLiteralShouldNotBeDuplicatedTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/StringLiteralShouldNotBeDuplicatedTest.cs
@@ -40,7 +40,7 @@ namespace SonarAnalyzer.UnitTest.Rules
 
         [TestMethod]
         public void StringLiteralShouldNotBeDuplicated_CSharp10() =>
-            csBuilder.AddPaths(@"StringLiteralShouldNotBeDuplicated.CSharp10.cs").WithOptions(ParseOptionsHelper.FromCSharp10).WithTopLevelStatements().Verify();
+            csBuilder.AddPaths(@"StringLiteralShouldNotBeDuplicated.CSharp10.cs").WithOptions(ParseOptionsHelper.FromCSharp10).Verify();
 #endif
 
         [TestMethod]
