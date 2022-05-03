ac7e015d33f456119bea0a3673da7f80243347fb
Martin Strecker <martin.strecker@sonarsource.com>
2022-05-03 14:19:47 +0200

    Rmove unused usings

--- analyzers/src/SonarAnalyzer.Common/Common/MultiValueDictionary.cs
+++ analyzers/src/SonarAnalyzer.Common/Common/MultiValueDictionary.cs
@@ -19,11 +19,8 @@
  */
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.Serialization;
 
 namespace SonarAnalyzer.Common
