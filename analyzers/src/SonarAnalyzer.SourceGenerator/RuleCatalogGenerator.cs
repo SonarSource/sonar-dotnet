8090e3d3240bc231e6108592b4c010d551dd0677
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-05-10 08:58:58 +0200

    Exclude from coverage

--- analyzers/src/SonarAnalyzer.SourceGenerator/RuleCatalogGenerator.cs
+++ analyzers/src/SonarAnalyzer.SourceGenerator/RuleCatalogGenerator.cs
@@ -20,6 +20,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -29,6 +30,7 @@ using Newtonsoft.Json.Linq;
 namespace SonarAnalyzer.SourceGenerator
 {
     [Generator]
+    [ExcludeFromCodeCoverage]
     public class RuleCatalogGenerator : ISourceGenerator
     {
         private const string SonarWayFileName = "Sonar_way_profile.json";
