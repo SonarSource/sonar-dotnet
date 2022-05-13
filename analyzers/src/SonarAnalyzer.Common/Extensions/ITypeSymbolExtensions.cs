278e801d1beda39646003b8ed55b3d1eadfe0da2
Martin Strecker <martin.strecker@sonarsource.com>
2022-05-13 13:59:29 +0200

    Add GetAccessibleMembersAndBaseMembers extension method.

---
+++ analyzers/src/SonarAnalyzer.Common/Extensions/ITypeSymbolExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace SonarAnalyzer.Extensions
+{
+    public static class ITypeSymbolExtensions
+    {
+        public static ImmutableArray<ISymbol> GetAccessibleMembersAndBaseMembers(this ITypeSymbol typeSymbol, SemanticModel model, int position)
+        {
+            var builder = ImmutableArray.CreateBuilder<ISymbol>();
+            AddAccessibleMembers(typeSymbol);
+            while ((typeSymbol = typeSymbol.BaseType) is not null)
+            {
+                AddAccessibleMembers(typeSymbol);
+            }
+            return builder.ToImmutable();
+
+            void AddAccessibleMembers(ITypeSymbol symbol)
+            {
+                foreach (var member in symbol.GetMembers().Where(x => model.IsAccessible(position, x)))
+                {
+                    builder.Add(member);
+                }
+            }
+        }
+    }
+}
