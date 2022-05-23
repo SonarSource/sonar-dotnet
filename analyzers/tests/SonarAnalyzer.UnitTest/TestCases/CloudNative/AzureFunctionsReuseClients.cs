8f36436e75f7c6c7e3155cc8a85ae0f73ec1ad6b
Martin Strecker <martin.strecker@sonarsource.com>
2022-05-23 08:31:55 +0200

    Add Testcase

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CloudNative/AzureFunctionsReuseClients.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CloudNative/AzureFunctionsReuseClients.cs
@@ -1,5 +1,26 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-public class Program
+namespace FunctionApp1
 {
+    public static class Function1
+    {
+        const string sampleUrl = @"http://example.com";
+
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var client = new HttpClient();
+            var result = await client.GetAsync(sampleUrl);
+            return new OkObjectResult(await result.Content.ReadAsStringAsync());
+        }
+    }
 }
