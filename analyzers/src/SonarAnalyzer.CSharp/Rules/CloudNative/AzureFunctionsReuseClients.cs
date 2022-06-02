12e592af21f8dd17b12b01d22a3853d139974f73
Martin Strecker <martin.strecker@sonarsource.com>
2022-06-02 14:27:10 +0200

    Change to array

--- analyzers/src/SonarAnalyzer.CSharp/Rules/CloudNative/AzureFunctionsReuseClients.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/CloudNative/AzureFunctionsReuseClients.cs
@@ -39,27 +39,29 @@ namespace SonarAnalyzer.Rules.CSharp
         private const string MessageFormat = "Reuse client instances rather than creating new ones with each function invocation.";
 
         private static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorBuilder.GetDescriptor(DiagnosticId, MessageFormat, RspecStrings.ResourceManager);
-        private static readonly ImmutableArray<KnownType> ReusableClients = ImmutableArray.Create(
-            KnownType.System_Net_Http_HttpClient,
-            // ComosDb (DocumentClient is superseded by CosmosClient)
-            KnownType.Microsoft_Azure_Documents_Client_DocumentClient,
-            KnownType.Microsoft_Azure_Cosmos_CosmosClient,
-            // Servicebus V5
-            KnownType.Microsoft_Azure_ServiceBus_Management_ManagementClient,
-            KnownType.Microsoft_Azure_ServiceBus_QueueClient,
-            KnownType.Microsoft_Azure_ServiceBus_SessionClient,
-            KnownType.Microsoft_Azure_ServiceBus_SubscriptionClient,
-            KnownType.Microsoft_Azure_ServiceBus_TopicClient,
-            // Servicebus V7
-            KnownType.Azure_Messaging_ServiceBus_ServiceBusClient,
-            KnownType.Azure_Messaging_ServiceBus_Administration_ServiceBusAdministrationClient,
-            // Storage
-            KnownType.Azure_Storage_Blobs_BlobServiceClient,
-            KnownType.Azure_Storage_Queues_QueueServiceClient,
-            KnownType.Azure_Storage_Files_Shares_ShareServiceClient,
-            KnownType.Azure_Storage_Files_DataLake_DataLakeServiceClient,
-            // Resource manager
-            KnownType.Azure_ResourceManager_ArmClient);
+        private static readonly KnownType[] ReusableClients =
+            {
+                KnownType.System_Net_Http_HttpClient,
+                // ComosDb (DocumentClient is superseded by CosmosClient)
+                KnownType.Microsoft_Azure_Documents_Client_DocumentClient,
+                KnownType.Microsoft_Azure_Cosmos_CosmosClient,
+                // Servicebus V5
+                KnownType.Microsoft_Azure_ServiceBus_Management_ManagementClient,
+                KnownType.Microsoft_Azure_ServiceBus_QueueClient,
+                KnownType.Microsoft_Azure_ServiceBus_SessionClient,
+                KnownType.Microsoft_Azure_ServiceBus_SubscriptionClient,
+                KnownType.Microsoft_Azure_ServiceBus_TopicClient,
+                // Servicebus V7
+                KnownType.Azure_Messaging_ServiceBus_ServiceBusClient,
+                KnownType.Azure_Messaging_ServiceBus_Administration_ServiceBusAdministrationClient,
+                // Storage
+                KnownType.Azure_Storage_Blobs_BlobServiceClient,
+                KnownType.Azure_Storage_Queues_QueueServiceClient,
+                KnownType.Azure_Storage_Files_Shares_ShareServiceClient,
+                KnownType.Azure_Storage_Files_DataLake_DataLakeServiceClient,
+                // Resource manager
+                KnownType.Azure_ResourceManager_ArmClient
+            };
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
