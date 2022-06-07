88002ca24c2fb462e01b9216f9bc1776f16386bd
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-06-07 09:44:46 +0200

    Agrh

--- analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
@@ -54,7 +54,7 @@ namespace SonarAnalyzer.Helpers.Facade
 
         public override bool IsNullLiteral(SyntaxNode node) => node.IsNullLiteral();
 
-        public override IEnumerable<SyntaxNode> ArgumentExpressions(SyntaxNode node) => 
+        public override IEnumerable<SyntaxNode> ArgumentExpressions(SyntaxNode node) =>
             node switch
             {
                 ObjectCreationExpressionSyntax creation => creation.ArgumentList?.Arguments.Select(x => x.Expression) ?? Enumerable.Empty<SyntaxNode>(),
