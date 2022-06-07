81a5a03d798fbaec14a2cb57476d04c4113ea89a
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-06-07 09:44:23 +0200

    Whitespace fix

--- analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
@@ -54,7 +54,8 @@ namespace SonarAnalyzer.Helpers.Facade
 
         public override bool IsNullLiteral(SyntaxNode node) => node.IsNullLiteral();
 
-        public override IEnumerable<SyntaxNode> ArgumentExpressions(SyntaxNode node) => node switch
+        public override IEnumerable<SyntaxNode> ArgumentExpressions(SyntaxNode node) => 
+            node switch
             {
                 ObjectCreationExpressionSyntax creation => creation.ArgumentList?.Arguments.Select(x => x.Expression) ?? Enumerable.Empty<SyntaxNode>(),
                 null => Enumerable.Empty<SyntaxNode>(),
