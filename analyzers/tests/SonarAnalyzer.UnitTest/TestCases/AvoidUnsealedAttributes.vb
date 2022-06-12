99234c29c79afaf211c1d43a78c678337a2e793f
Corniel Nobel <mail@corniel.nl>
2022-06-12 18:55:42 +0200

    Add VB.NET implementation.

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AvoidUnsealedAttributes.vb
@@ -0,0 +1,31 @@
+﻿
+Namespace AvoidUnsealedAttributes
+
+    Public Class PublicAttribute ' Noncompliant {{Seal this attribute or make it abstract.}}
+        Inherits Attribute
+        '        ^^^^^^^^^^^^^^^ @-1
+    End Class
+
+    Public NotInheritable Class SealedAttribute
+        Inherits Attribute ' Compliant
+    End Class
+
+    Public MustInherit Class AbstractAttribute
+        Inherits Attribute ' Compliant
+    End Class
+
+    Public Class Container
+
+        Protected Class ProtectedAttribute ' Noncompliant
+            Inherits Attribute
+        End Class
+
+        Private Class PrivateAttribute ' Compliant
+            Inherits Attribute
+
+            Public Class SubClassAttribute ' Compliant - effective accessibility is private
+                Inherits Attribute
+            End Class
+        End Class
+    End Class
+End Namespace
