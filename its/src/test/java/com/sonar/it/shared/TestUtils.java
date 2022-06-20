dd34fa188aad93b41c3de6d79daaed2f13892c2b
Čaba Šagi <csaba.sagi@sonarsource.com>
2022-06-20 12:46:09 +0200

    Use scanner v5.7.0.49456 in ITs

--- its/src/test/java/com/sonar/it/shared/TestUtils.java
+++ its/src/test/java/com/sonar/it/shared/TestUtils.java
@@ -142,7 +142,7 @@ public class TestUtils {
   private static Build<ScannerForMSBuild> newScanner(Path projectDir) {
     // We need to set the fallback version to run from inside the IDE when the property isn't set
     return ScannerForMSBuild.create(projectDir.toFile())
-      .setScannerVersion(System.getProperty("scannerMsbuild.version", "5.6.0.48455"))
+      .setScannerVersion(System.getProperty("scannerMsbuild.version", "5.7.0.49456"))
 
       // In order to be able to run tests on Azure pipelines, the AGENT_BUILDDIRECTORY environment variable
       // needs to be set to the analyzed project directory.
