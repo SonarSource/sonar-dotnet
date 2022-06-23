882eee34e3e1c61988d67cf5207ad7aa67972279
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-06-23 08:49:34 +0200

    add more UT examples

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ForLoopCounterChanged.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ForLoopCounterChanged.CSharp10.cs
@@ -7,5 +7,17 @@
             (i, var j) = t; // Noncompliant {{Do not update the loop counter 'i' within the loop body.}}
 //          ^^^^^^^^^^
         }
+
+        for (int i = 0; i < 42; i++)
+        {
+            (i, var a, var b, int c) = (1, 2, 3, 4); // Noncompliant {{Do not update the loop counter 'i' within the loop body.}}
+//          ^^^^^^^^^^^^^^^^^^^^^^^^
+        }
+
+        for (int i = 0; i < 42; i++)
+        {
+            int a = 10;
+            (a, var j) = t;
+        }
     }
 }
