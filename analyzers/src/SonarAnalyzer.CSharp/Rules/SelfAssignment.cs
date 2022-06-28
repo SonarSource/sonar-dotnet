0958c5f9fd1db384de4e26e14e1b58451031c139
Martin Strecker <martin.strecker@sonarsource.com>
2022-06-28 15:47:51 +0200

    Fix code smell

--- analyzers/src/SonarAnalyzer.CSharp/Rules/SelfAssignment.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/SelfAssignment.cs
@@ -18,6 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -25,7 +26,6 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.Extensions;
 using SonarAnalyzer.Helpers;
 using StyleCop.Analyzers.Lightup;
-using System.Linq;
 
 namespace SonarAnalyzer.Rules.CSharp
 {
