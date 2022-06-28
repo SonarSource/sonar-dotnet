cdaa6183191fc2e2cd6f2f2e5292ffb16508ad44
Martin Strecker <martin.strecker@sonarsource.com>
2022-06-28 13:23:35 +0200

    Refactoring

--- analyzers/src/SonarAnalyzer.CSharp/Rules/DeadStores.RoslynCfg.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/DeadStores.RoslynCfg.cs
@@ -52,28 +52,28 @@ namespace SonarAnalyzer.Rules.CSharp
 
                 public override void AnalyzeBlock()
                 {
-                    foreach (var operation in block.OperationsAndBranchValue.ToReversedExecutionOrder())
+                    foreach (var operation in block.OperationsAndBranchValue.ToReversedExecutionOrder().Select(x => x.Instance))
                     {
-                        switch (operation.Instance.Kind)
+                        switch (operation.Kind)
                         {
                             case OperationKindEx.LocalReference:
-                                ProcessParameterOrLocalReference(ILocalReferenceOperationWrapper.FromOperation(operation.Instance));
+                                ProcessParameterOrLocalReference(ILocalReferenceOperationWrapper.FromOperation(operation));
                                 break;
                             case OperationKindEx.ParameterReference:
-                                ProcessParameterOrLocalReference(IParameterReferenceOperationWrapper.FromOperation(operation.Instance));
+                                ProcessParameterOrLocalReference(IParameterReferenceOperationWrapper.FromOperation(operation));
                                 break;
                             case OperationKindEx.SimpleAssignment:
-                                ProcessSimpleAssignment(ISimpleAssignmentOperationWrapper.FromOperation(operation.Instance));
+                                ProcessSimpleAssignment(ISimpleAssignmentOperationWrapper.FromOperation(operation));
                                 break;
                             case OperationKindEx.CompoundAssignment:
-                                ProcessCompoundAssignment(ICompoundAssignmentOperationWrapper.FromOperation(operation.Instance));
+                                ProcessCompoundAssignment(ICompoundAssignmentOperationWrapper.FromOperation(operation));
                                 break;
                             case OperationKindEx.DeconstructionAssignment:
-                                ProcessDeconstructionAssignment(IDeconstructionAssignmentOperationWrapper.FromOperation(operation.Instance));
+                                ProcessDeconstructionAssignment(IDeconstructionAssignmentOperationWrapper.FromOperation(operation));
                                 break;
                             case OperationKindEx.Increment:
                             case OperationKindEx.Decrement:
-                                ProcessIncrementOrDecrement(IIncrementOrDecrementOperationWrapper.FromOperation(operation.Instance));
+                                ProcessIncrementOrDecrement(IIncrementOrDecrementOperationWrapper.FromOperation(operation));
                                 break;
                         }
                     }
