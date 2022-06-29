72adb4ead14352a06aaf10090421509b3e58eaa3
Martin Strecker <martin.strecker@sonarsource.com>
2022-06-29 16:10:51 +0200

    Add test cases.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp10.cs
@@ -5,6 +5,8 @@
         int i = 0;
 
         (i, var j) = (i++, 0); // FN
+        (var k, _) = (i++, 0); // Compliant
+        (_, _) = (i++, 0);     // Compliant
     }
 
     public (int, int) M(int i, int j)
