c98a91918b98b451c2358ea8127aa6969137f37c
Martin Strecker <martin.strecker@sonarsource.com>
2022-07-06 12:59:37 +0200

    Add test case for ForEachVariableStatement

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/VariableShadowsField.CSharp10.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/VariableShadowsField.CSharp10.cs
@@ -9,12 +9,14 @@
     private int q = 0;
     private int r = 0;
     private int s = 0;
+    private int t = 0;
+    private int u = 0;
 
     public S()
     {
         (var i, var j) = (0, 0);
-//           ^
-//                  ^ @-1
+        //   ^
+        //          ^ @-1
 
         var (k, l) = (0, 0);     // Noncompliant [issue1,issue2]
         (var m, var n) = (0, 0); // Noncompliant
@@ -23,8 +25,14 @@
         var (c, d) = (0, 0);     // Compliant
 
         var (q, (_, r, _), s) = (1, (2, 3, 4), 5);
-//           ^
-//                  ^ @-1
-//                         ^ @-2
+        //   ^
+        //          ^        @-1
+        //                 ^ @-2
+        foreach ((var t, var u) in new[] { (1, 2) })
+        //            ^
+        //                   ^ @-1
+        {
+
+        }
     }
 }
