21fdfbf62390ef60ffa8524d38cdf47fe5b4ce0a
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-07-13 13:07:23 +0200

    Serialize rule metadata to Json (#5830)

---
+++ analyzers/src/SonarAnalyzer.RuleDescriptorGenerator/Descriptors/Rule.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace SonarAnalyzer.RuleDescriptorGenerator;
+
+[ExcludeFromCodeCoverage]
+public class Rule   // ToDo: Use records after changing target framework
+{
+    public string Id { get; }
+    public RuleParameter[] Parameters { get; }
+
+    public Rule(string id, RuleParameter[] parameters)
+    {
+        Id = id;
+        Parameters = parameters;
+    }
+}
