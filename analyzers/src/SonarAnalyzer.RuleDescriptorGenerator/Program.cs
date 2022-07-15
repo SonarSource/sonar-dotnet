c8cc621cc8b35d60e8fd039351ba2cc0e1c84cdd
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-07-15 10:44:59 +0200

    Package fixes

--- analyzers/src/SonarAnalyzer.RuleDescriptorGenerator/Program.cs
+++ analyzers/src/SonarAnalyzer.RuleDescriptorGenerator/Program.cs
@@ -28,50 +28,49 @@ using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.Common;
 using SonarAnalyzer.Rules;
 
-namespace SonarAnalyzer.RuleDescriptorGenerator
+namespace SonarAnalyzer.RuleDescriptorGenerator;
+
+[ExcludeFromCodeCoverage]
+public static class Program
 {
-    [ExcludeFromCodeCoverage]
-    public static class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        if (args.Length == 2)
         {
-            if (args.Length == 2)
-            {
-                var analyzers = LoadAnalyzerTypes(args[0]);
-                var rules = LoadRules(analyzers);
-                var json = JsonSerializer.Serialize(rules, new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-                File.WriteAllText(args[1], json);
-            }
-            else
-            {
-                Console.WriteLine("Application expects two arguments: <Path-To-Dll> <Path-To-Output-Json>");
-                Console.WriteLine("DiagnosticAnalyzer metadata from <Path-To-Dll> will be serialized to <Path-To-Output-Json>.");
-            }
+            var analyzers = LoadAnalyzerTypes(args[0]);
+            var rules = LoadRules(analyzers);
+            var json = JsonSerializer.Serialize(rules, new JsonSerializerOptions { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+            File.WriteAllText(args[1], json);
         }
-
-        private static Type[] LoadAnalyzerTypes(string path) =>
-            Assembly.LoadFrom(path)  // We don't need 'Load', we have full control over the environment. It would make local build too complicated.
-                .ExportedTypes
-                .Where(x => !x.IsAbstract && typeof(DiagnosticAnalyzer).IsAssignableFrom(x) && !typeof(UtilityAnalyzerBase).IsAssignableFrom(x))
-                .ToArray();
-
-        private static Rule[] LoadRules(Type[] analyzers) =>
-            analyzers
-                .Select(x => new { Type = x, Parameters = RuleParameters(x) })
-                .SelectMany(x => UniqueIds(x.Type).Select(id => new { Id = id, x.Parameters }))
-                .GroupBy(x => x.Id)     // Same id can be in multiple classes (see InvalidCastToInterface)
-                .Select(x => new Rule(x.Key, x.SelectMany(x => x.Parameters).ToArray()))
-                .ToArray();
-
-        private static RuleParameter[] RuleParameters(Type analyzer) =>
-            analyzer.GetProperties()
-                .Select(x => x.GetCustomAttributes<RuleParameterAttribute>().SingleOrDefault())
-                .Where(x => x != null)
-                .Select(x => new RuleParameter(x))
-                .ToArray();
-
-        // One class can have the same ruleId multiple times, see S3240
-        private static string[] UniqueIds(Type analyzer) =>
-            ((DiagnosticAnalyzer)Activator.CreateInstance(analyzer)).SupportedDiagnostics.Select(x => x.Id).Distinct().ToArray();
+        else
+        {
+            Console.WriteLine("Application expects two arguments: <Path-To-Dll> <Path-To-Output-Json>");
+            Console.WriteLine("DiagnosticAnalyzer metadata from <Path-To-Dll> will be serialized to <Path-To-Output-Json>.");
+        }
     }
+
+    private static Type[] LoadAnalyzerTypes(string path) =>
+        Assembly.LoadFrom(path)  // We don't need 'Load', we have full control over the environment. It would make local build too complicated.
+            .ExportedTypes
+            .Where(x => !x.IsAbstract && typeof(DiagnosticAnalyzer).IsAssignableFrom(x) && !typeof(UtilityAnalyzerBase).IsAssignableFrom(x))
+            .ToArray();
+
+    private static Rule[] LoadRules(Type[] analyzers) =>
+        analyzers
+            .Select(x => new { Type = x, Parameters = RuleParameters(x) })
+            .SelectMany(x => UniqueIds(x.Type).Select(id => new { Id = id, x.Parameters }))
+            .GroupBy(x => x.Id)     // Same id can be in multiple classes (see InvalidCastToInterface)
+            .Select(x => new Rule(x.Key, x.SelectMany(x => x.Parameters).ToArray()))
+            .ToArray();
+
+    private static RuleParameter[] RuleParameters(Type analyzer) =>
+        analyzer.GetProperties()
+            .Select(x => x.GetCustomAttributes<RuleParameterAttribute>().SingleOrDefault())
+            .Where(x => x != null)
+            .Select(x => new RuleParameter(x))
+            .ToArray();
+
+    // One class can have the same ruleId multiple times, see S3240
+    private static string[] UniqueIds(Type analyzer) =>
+        ((DiagnosticAnalyzer)Activator.CreateInstance(analyzer)).SupportedDiagnostics.Select(x => x.Id).Distinct().ToArray();
 }
