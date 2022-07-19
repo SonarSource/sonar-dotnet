8de2e54167b0e9e2db9ae4e35989d302b8e83941
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2022-07-19 17:58:32 +0200

    Update analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EmptyMethod.Throw.Fixed.cs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EmptyMethod.Throw.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EmptyMethod.Throw.Fixed.cs
@@ -29,7 +29,7 @@ namespace Tests.Diagnostics
 #endif
         }
 
-        public void ConditionalCompilationEmpty() //FP. We don't check if the conditional contains anything.
+        public void ConditionalCompilationEmpty() // FN. We don't check if the conditional contains anything.
         {
 #if SomeThing
 #endif
