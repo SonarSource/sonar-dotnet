a0f1e3c1b4cb675056ad627fa414f569fa7ef771
Martin Strecker <martin.strecker@sonarsource.com>
2022-07-20 12:19:15 +0200

    Add extension method and test infratructure

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/BlockSyntaxExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SonarAnalyzer.Extensions;
+
+namespace SonarAnalyzer.UnitTest.Extensions
+{
+    [TestClass]
+    public class BlockSyntaxExtensionsTests
+    {
+        [TestMethod]
+        public void IsEmpty_EmptyBlockMethod()
+        {
+            var block = @"
+{
+
+}
+";
+            var declaration = MethodDeclarationSyntax(WrapInClass(block));
+            declaration.Body.IsEmpty().Should().BeTrue();
+        }
+
+        private static string WrapInClass(string methodBlockOrArrow)
+        {
+            return $@"
+public class C
+{{
+    public void M()
+    {methodBlockOrArrow}
+}}
+";
+        }
+
+        private static MethodDeclarationSyntax MethodDeclarationSyntax(string source)
+        {
+            var root = CSharpSyntaxTree.ParseText(source).GetRoot();
+            root.ContainsDiagnostics.Should().BeFalse();
+            return root.DescendantNodes().OfType<MethodDeclarationSyntax>().Single();
+        }
+    }
+}
