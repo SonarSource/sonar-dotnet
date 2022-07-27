35e54aae39a646d713eb182989a0085752e5cea2
Martin Strecker <martin.strecker@sonarsource.com>
2022-07-27 15:35:03 +0200

    Override PostProcessSimple

--- analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/NullPointerDereference.cs
+++ analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/NullPointerDereference.cs
@@ -31,5 +31,10 @@ namespace SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.CSharp
         protected override DiagnosticDescriptor Rule => S2259;
 
         public override bool ShouldExecute() => true;
+
+        protected override ProgramState PostProcessSimple(SymbolicContext context)
+        {
+            return base.PostProcessSimple(context);
+        }
     }
 }
