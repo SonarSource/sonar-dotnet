4d19ac9218f5324a0d0a5d4bb6b808f7050ce9fa
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-07-27 18:13:08 +0200

    Extract RoslynSymbolicExecutionTest.Binary.cs (#5917)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Binary.cs
@@ -0,0 +1,295 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarAnalyzer.SymbolicExecution.Constraints;
+using SonarAnalyzer.UnitTest.TestFramework.SymbolicExecution;
+
+namespace SonarAnalyzer.UnitTest.SymbolicExecution.Roslyn
+{
+    public partial class RoslynSymbolicExecutionTest
+    {
+        [TestMethod]
+        public void Binary_BoolOperands_Equals_CS()
+        {
+            const string code = @"
+var isTrue = true;
+var isFalse = false;
+
+if (isTrue == true)
+    Tag(""True"");
+else
+    Tag(""True Unreachable"");
+
+if (isFalse == false)
+    Tag(""False"");
+else
+    Tag(""False Unreachable"");
+
+if (isTrue == isFalse)
+    Tag(""Variables Unreachable"");
+else
+    Tag(""Variables"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder("True", "False", "Variables");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_Equals_VB()
+        {
+            const string code = @"
+Dim IsTrue = True
+Dim IsFalse = False
+
+If IsTrue = True Then
+    Tag(""True"")
+Else
+    Tag(""True Unreachable"")
+End If
+
+If IsFalse = False Then
+    Tag(""False"")
+Else
+    Tag(""False Unreachable"")
+End If
+
+If IsTrue = IsFalse Then
+    Tag(""Variables Unreachable"")
+Else
+    Tag(""Variables"")
+End If";
+            SETestContext.CreateVB(code).Validator.ValidateTagOrder("True", "False", "Variables");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_NotEquals_CS()
+        {
+            const string code = @"
+var isTrue = true;
+var isFalse = false;
+
+if (isTrue != true)
+    Tag(""True Unreachable"");
+else
+    Tag(""True"");
+
+if (isFalse != false)
+    Tag(""False Unreachable"");
+else
+    Tag(""False"");
+
+if (isTrue != isFalse)
+    Tag(""Variables"");
+else
+    Tag(""Variables Unreachable"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder("True", "False", "Variables");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_NotEquals_VB()
+        {
+            const string code = @"
+Dim IsTrue = True
+Dim IsFalse = False
+
+If IsTrue <> True Then
+    Tag(""True Unreachable"")
+Else
+    Tag(""True"")
+End If
+
+If IsFalse <> False Then
+    Tag(""False Unreachable"")
+Else
+    Tag(""False"")
+End If
+
+If IsTrue <> IsFalse Then
+    Tag(""Variables"")
+Else
+    Tag(""Variables Unreachable"")
+End If";
+            SETestContext.CreateVB(code).Validator.ValidateTagOrder(
+                "True",
+                "False",
+                "Variables");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_And()
+        {
+            const string code = @"
+var isTrue = true;
+var isFalse = false;
+
+if (isTrue & true)
+    Tag(""True & True"");
+else
+    Tag(""True & True Unreachable"");
+
+if (false & isTrue)
+    Tag(""False & True Unreachable"");
+else
+    Tag(""False & True"");
+
+if (false & isFalse)
+    Tag(""False & False Unreachable"");
+else
+    Tag(""False & False"");
+
+if (isTrue && true)
+    Tag(""True && True"");
+else
+    Tag(""True && True Unreachable"");
+
+if (isFalse && true)
+    Tag(""False && True Unreachable"");
+else
+    Tag(""False && True"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder("True & True", "False & True", "False & False", "True && True", "False && True");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_Or()
+        {
+            const string code = @"
+var isTrue = true;
+var isFalse = false;
+
+if (isTrue | true)
+    Tag(""True | True"");
+else
+    Tag(""True | True Unreachable"");
+
+if (false | isTrue)
+    Tag(""False | True"");
+else
+    Tag(""False | True Unreachable"");
+
+if (false | isFalse)
+    Tag(""False | False Unreachable"");
+else
+    Tag(""False | False"");
+
+if (isTrue || true)
+    Tag(""True || True"");
+else
+    Tag(""True || True Unreachable"");
+
+if (isFalse || true)
+    Tag(""False || True"");
+else
+    Tag(""False || True Unreachable"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder("True | True", "False | True", "False | False", "True || True", "False || True");
+        }
+
+        [TestMethod]
+        public void Binary_BoolOperands_Xor()
+        {
+            const string code = @"
+var isTrue = true;
+var isFalse = false;
+
+if (isTrue ^ true)
+    Tag(""True ^ True Unreachable"");
+else
+    Tag(""True ^ True"");
+
+if (false ^ isTrue)
+    Tag(""False ^ True"");
+else
+    Tag(""False ^ True Unreachable"");
+
+if (isTrue ^ false)
+    Tag(""True ^ False"");
+else
+    Tag(""True ^ False Unreachable"");
+
+if (false ^ isFalse)
+    Tag(""False ^ False Unreachable"");
+else
+    Tag(""False ^ False"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder("True ^ True", "False ^ True", "True ^ False", "False ^ False");
+        }
+
+        [DataTestMethod]
+        [DataRow("boolParameter & isTrue")]
+        [DataRow("isTrue & boolParameter")]
+        public void Binary_NoConstraint_VisitsBothBranches(string condition)
+        {
+            var code = $@"
+bool isTrue = true;
+if ({condition})
+{{
+    Tag(""If"");
+}}
+else
+{{
+    Tag(""Else"");
+}}
+Tag(""End"");";
+            SETestContext.CreateCS(code).Validator.ValidateTagOrder(
+                "If",
+                "Else",
+                "End");
+        }
+
+        [DataTestMethod]
+        [DataRow("boolParameter & isTrue")]
+        [DataRow("isTrue & boolParameter")]
+        public void Binary_OtherConstraint_VisitsBothBranches(string condition)
+        {
+            var code = $@"
+bool isTrue = true;
+if ({condition})
+{{
+    Tag(""If"");
+}}
+else
+{{
+    Tag(""Else"");
+}}
+Tag(""End"");";
+            var check = new PostProcessTestCheck(OperationKind.ParameterReference, x => x.SetOperationConstraint(DummyConstraint.Dummy));
+            SETestContext.CreateCS(code, check).Validator.ValidateTagOrder(
+                "If",
+                "Else",
+                "End");
+        }
+
+        [TestMethod]
+        public void Binary_UnexpectedOperator_VisitsBothBranches()
+        {
+            var code = $@"
+if (a > b)      // Both, 'a' and 'b' have bool constraint (weird) and we do not produce bool constraint for '>' binary operator, because it doesn't make sense.
+{{
+    Tag(""If"");
+}}
+else
+{{
+    Tag(""Else"");
+}}
+Tag(""End"");";
+            var check = new PostProcessTestCheck(OperationKind.ParameterReference, x => x.SetOperationConstraint(BoolConstraint.True));
+            SETestContext.CreateCS(code, ", int a, int b", check).Validator.ValidateTagOrder(
+                "If",
+                "Else",
+                "End");
+        }
+    }
+}
