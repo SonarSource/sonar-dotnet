a33313f9f55013075ac89cfdd20070add8bb89c2
Corniel Nobel <mail@corniel.nl>
2022-07-28 10:02:43 +0200

    Drop non-relevant namespace declarations

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AvoidUnsealedAttributes.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AvoidUnsealedAttributes.vb
@@ -1,31 +1,27 @@
-﻿
-Namespace AvoidUnsealedAttributes
+﻿Public Class PublicAttribute ' Noncompliant {{Seal this attribute or make it abstract.}}
+    Inherits Attribute
+    '        ^^^^^^^^^^^^^^^ @-1
+End Class
 
-    Public Class PublicAttribute ' Noncompliant {{Seal this attribute or make it abstract.}}
+Public NotInheritable Class SealedAttribute
+    Inherits Attribute ' Compliant
+End Class
+
+Public MustInherit Class AbstractAttribute
+    Inherits Attribute ' Compliant
+End Class
+
+Public Class Container
+
+    Protected Class ProtectedAttribute ' Noncompliant
         Inherits Attribute
-        '        ^^^^^^^^^^^^^^^ @-1
     End Class
 
-    Public NotInheritable Class SealedAttribute
-        Inherits Attribute ' Compliant
-    End Class
+    Private Class PrivateAttribute ' Compliant
+        Inherits Attribute
 
-    Public MustInherit Class AbstractAttribute
-        Inherits Attribute ' Compliant
-    End Class
-
-    Public Class Container
-
-        Protected Class ProtectedAttribute ' Noncompliant
+        Public Class SubClassAttribute ' Compliant - effective accessibility is private
             Inherits Attribute
         End Class
-
-        Private Class PrivateAttribute ' Compliant
-            Inherits Attribute
-
-            Public Class SubClassAttribute ' Compliant - effective accessibility is private
-                Inherits Attribute
-            End Class
-        End Class
     End Class
-End Namespace
+End Class
