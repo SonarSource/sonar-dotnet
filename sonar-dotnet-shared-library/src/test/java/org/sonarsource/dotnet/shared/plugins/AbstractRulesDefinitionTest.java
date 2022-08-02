b50aa3d050d1d9a526ff6f0152fa92046ee6df01
Costin Zaharia <costin.zaharia@sonarsource.com>
2022-08-02 14:26:49 +0200

    remove duplication

--- sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/AbstractRulesDefinitionTest.java
+++ sonar-dotnet-shared-library/src/test/java/org/sonarsource/dotnet/shared/plugins/AbstractRulesDefinitionTest.java
@@ -56,17 +56,7 @@ public class AbstractRulesDefinitionTest {
 
   @Test
   public void test_before_9_3() {
-    SonarRuntime sonarRuntime = SonarRuntimeImpl.forSonarQube(Version.create(9, 2), SonarQubeSide.SCANNER, SonarEdition.COMMUNITY);
-    AbstractRulesDefinition sut = new TestRulesDefinition(sonarRuntime);
-    RulesDefinition.Context context = new RulesDefinition.Context();
-    sut.define(context);
-
-    RulesDefinition.Repository repository = context.repository("test");
-    assertThat(repository).isNotNull();
-
-    RulesDefinition.Rule rule = repository.rule("S1111");
-    assertThat(rule).isNotNull();
-    assertThat(rule.securityStandards()).containsExactlyInAnyOrder("cwe:117", "cwe:532", "owaspTop10:a10", "owaspTop10:a3");
+    assertThat(getSecurityStandards(Version.create(9, 2), "S1111")).containsExactlyInAnyOrder("cwe:117", "cwe:532", "owaspTop10:a10", "owaspTop10:a3");
   }
 
   @Test
