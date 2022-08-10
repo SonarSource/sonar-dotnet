3b9e5714610ec00e2168978daa3bed23cb89686b
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2022-08-10 12:14:57 +0200

    add deconstruction code sample (#5978)

---
+++ analyzers/its/sources/Net6/Net6/Deconstruction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace Net6
+{
+    public class Sonar
+    {
+        private string? sonar;
+        [Localizable(true)]
+        public string? Source { get; set; }
+        public void SomeTest1()
+        {
+            (var sonar, double _, Source) = ("Sonar", 24 / 7, "Source");
+        }
+    }
+}
