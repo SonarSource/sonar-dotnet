10787e364e85fdb432dbf66f1d65c87315a59e78
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-08-16 15:48:54 +0200

    remove not used usings

--- analyzers/src/SonarAnalyzer.CSharp/Rules/EmptyNamespace.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/EmptyNamespace.cs
@@ -19,10 +19,8 @@
  */
 
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.Helpers;
 using StyleCop.Analyzers.Lightup;
