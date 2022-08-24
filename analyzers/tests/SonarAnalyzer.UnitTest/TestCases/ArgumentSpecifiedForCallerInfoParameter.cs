8e02bd2fed5a8d78a60d1045437a1b3f82aaa3d4
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2022-08-24 11:34:35 +0200

    FP S3236: Passthrough parameters should be compliant (#6021)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArgumentSpecifiedForCallerInfoParameter.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArgumentSpecifiedForCallerInfoParameter.cs
@@ -7,22 +7,33 @@ namespace Tests.Diagnostics
     class ArgumentSpecifiedForCallerInfoParameter
     {
         void TraceMessage(string message,
-          [CallerMemberName] string memberName = "",
-          [CallerFilePath] string filePath = "",
-          [CallerLineNumber] int lineNumber = 0)
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0)
         {
             /* ... */
         }
 
         void MyMethod()
         {
-            TraceMessage("my message", "MyMethod"); // Compliant
-            TraceMessage("my message"); // Compliant
+            TraceMessage("my message", "MyMethod");       // Compliant "memberName" can be specified by the caller (e.g. raising OnPropertyChanged for another property in WPF)
+            TraceMessage("my message");                   // Compliant
             TraceMessage("my message", filePath: "aaaa"); // Noncompliant
-            TraceMessage("my message", lineNumber: 42); // Noncompliant
+            //                         ^^^^^^^^^^^^^^^^
+            TraceMessage("my message", lineNumber: 42);   // Noncompliant
             TraceMessage("my message",
-                filePath: "aaaa",  // Noncompliant
-                lineNumber: 42); // Noncompliant
+                filePath: "aaaa",                         // Noncompliant
+                lineNumber: 42);                          // Noncompliant
+        }
+
+        void PassThrough(string message,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string filePath = "",
+            [CallerLineNumber] int lineNumber = 0)
+        {
+            TraceMessage(message, memberName, filePath, lineNumber); // Compliant
+            TraceMessage(message, filePath: memberName);             // Noncompliant parameters are switched
+            TraceMessage(message, memberName: filePath);             // Compliant "memberName" can be specified
         }
     }
 }
