76754e9dadd33c2bbc67db6e149d6690a1dfb7d9
Martin Strecker <martin.strecker@sonarsource.com>
2022-08-30 15:31:54 +0200

    Move tests to correct file

--- analyzers/tests/SonarAnalyzer.UnitTest/Helpers/EnumerableExtensionsTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Helpers/EnumerableExtensionsTest.cs
@@ -20,6 +20,8 @@
 
 #pragma warning disable SA1122 // Use string.Empty for empty strings
 
+using System.Threading;
+
 namespace SonarAnalyzer.UnitTest.Helpers
 {
     [TestClass]
@@ -172,6 +174,80 @@ namespace SonarAnalyzer.UnitTest.Helpers
             {
                 this.count = count;
             }
+        }
+
+        [TestMethod]
+        public void JoinAndNull()
+        {
+            var result = ((object[])null).JoinAnd();
+            result.Should().Be(string.Empty);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("", null)]
+        [DataRow("", "")]
+        [DataRow("", "", "")]
+        [DataRow("", "", " ", "\t", null)]
+        [DataRow("a", "a")]
+        [DataRow("a and b", "a", "b")]
+        [DataRow("a and b", "a", null, "b")]
+        [DataRow("a, b, and c", "a", "b", "c")]
+        [DataRow("a, b, and c", "a", null, "b", "", "c")]
+        public void JoinAndStrings(string expected, params string[] collection)
+        {
+            var result = collection.JoinAnd();
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("0", 0)]
+        [DataRow("0 and 1", 0, 1)]
+        [DataRow("0, 1, and 2", 0, 1, 2)]
+        [DataRow("1000", 1000)]
+        public void JoinAndInts(string expected, params int[] collection)
+        {
+            var result = collection.JoinAnd();
+            result.Should().Be(expected);
+        }
+
+        [DataTestMethod]
+        [DataRow("")]
+        [DataRow("08/30/2022 12:29:11", "2022-08-30T12:29:11")]
+        [DataRow("08/30/2022 12:29:11 and 12/24/2022 16:00:00", "2022-08-30T12:29:11", "2022-12-24T16:00:00")]
+        [DataRow("08/30/2022 12:29:11, 12/24/2022 16:00:00, and 12/31/2022 00:00:00", "2022-08-30T12:29:11", "2022-12-24T16:00:00", "2022-12-31T00:00:00")]
+        public void JoinAndDateTime(string expected, params string[] collection)
+        {
+            var oldLocale = Thread.CurrentThread.CurrentCulture;
+            var oldUiLocale = Thread.CurrentThread.CurrentUICulture;
+            try
+            {
+                var culture = CultureInfo.InvariantCulture;
+                Thread.CurrentThread.CurrentCulture = culture;
+                Thread.CurrentThread.CurrentUICulture = culture;
+                var result = collection.Select(x => DateTime.Parse(x)).JoinAnd();
+                result.Should().Be(expected);
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentCulture = oldLocale;
+                Thread.CurrentThread.CurrentUICulture = oldUiLocale;
+            }
+        }
+
+        [TestMethod]
+        public void JoinAndMixedClasses()
+        {
+            var collection = new Exception[]
+            {
+                new IndexOutOfRangeException("IndexOutOfRangeMessage"),
+                new InvalidOperationException("OperationMessage"),
+                null,
+                new NotSupportedException("NotSupportedMessage"),
+            };
+            var result = collection.JoinAnd();
+            result.Should().Be("System.IndexOutOfRangeException: IndexOutOfRangeMessage, System.InvalidOperationException: OperationMessage, and System.NotSupportedException: NotSupportedMessage");
         }
     }
 }
