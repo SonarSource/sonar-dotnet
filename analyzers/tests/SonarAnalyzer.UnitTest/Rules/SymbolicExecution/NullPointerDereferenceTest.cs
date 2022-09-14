d1736237e14e483a28122926939a57a8aef245ca
Martin Strecker <martin.strecker@sonarsource.com>
2022-09-14 14:40:29 +0200

    Add tests

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/NullPointerDereferenceTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/NullPointerDereferenceTest.cs
@@ -90,6 +90,10 @@ namespace SonarAnalyzer.UnitTest.Rules
 #if NET
 
         [TestMethod]
+        public void NullPointerDereference_Roslyn_CSharp8_NullableEnable() =>
+            roslynCS.AddPaths("NullPointerDereference.CSharp8_NullableEnable.cs").WithOptions(ParseOptionsHelper.FromCSharp8).WithConcurrentAnalysis(false).Verify();
+
+        [TestMethod]
         public void NullPointerDereference_Sonar_CSharp9() =>
             sonar.AddPaths("NullPointerDereference.CSharp9.cs").WithTopLevelStatements().Verify();
 
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/NullPointerDereference.CSharp8_NullableEnable.cs
@@ -0,0 +1,82 @@
+﻿#nullable enable
+
+using System;
+using System.IO;
+
+namespace Tests.Diagnostics.CSharp8
+{
+    public class NullForgivingOperator
+    {
+        object? field;
+        static object? staticField;
+        event EventHandler<object>? handler;
+
+        public void Local()
+        {
+            object? o = null;
+            o.ToString();                        // Noncompliant
+            o = null;
+            o!.ToString();                       // Compliant
+        }
+
+        public void Parameter(object? p)
+        {
+            if (p == null)
+            {
+                p!.ToString();                   // Compliant
+            }
+        }
+
+        public void Field()
+        {
+            if (field == null)
+            {
+                field!.ToString();               // Compliant
+            }
+        }
+
+        public void StaticField()
+        {
+            if (staticField == null)
+            {
+                staticField!.ToString();         // Compliant
+            }
+        }
+
+        public void EventHandler()
+        {
+            if (handler == null)
+            {
+                handler!.ToString();             // Compliant
+            }
+        }
+
+        public void Array()
+        {
+            object[]? arr = null;
+            arr!.ToString();                     // Compliant
+        }
+
+        public void ArrayElement()
+        {
+            var arr = new object?[] { null };
+            arr[0]!.ToString();                  // Compliant
+        }
+
+        public void Conversion()
+        {
+            object? o = null;
+            ((int?)o)!.ToString();               // Compliant
+        }
+
+        public void Expression(bool condition)
+        {
+            (condition ? new object(): null)!.ToString();  // Compliant
+        }
+
+        public void Parenthesis(bool condition)
+        {
+            (((condition ? new object() : null))!).ToString();  // Compliant
+        }
+    }
+}
