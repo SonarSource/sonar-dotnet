fd6d6528f4d14d53cf6dc8b6fd77d42297c82151
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-09-14 13:16:36 +0200

    Review update: Mention rule ID

--- analyzers/its/sources/Roslyn.1.3.1/OldRoslyn.cs
+++ analyzers/its/sources/Roslyn.1.3.1/OldRoslyn.cs
@@ -5,7 +5,7 @@ using System.Diagnostics;
 namespace Roslyn
 {
     // This class ensures that the old SE engine is run.
-    // FPs and FNs should not get fixed. If they do, there's probably something wrong with rule activation for the old Roslyn.
+    // S2259 FPs and FNs should not get fixed. If they do, there's probably something wrong with rule activation for the old Roslyn.
     public class OldRoslyn
     {
         private object field;
@@ -13,14 +13,14 @@ namespace Roslyn
         public void ThisConditional()
         {
             field = null;
-            this?.field.ToString(); // This is raised only by the old engine. New SE doesn't support this.
+            this?.field.ToString(); // This S2259 is raised only by the old engine. New SE doesn't support this.
         }
 
         public string DebugAssert()
         {
             object asserted = null;
             Debug.Assert(asserted != null);
-            return asserted.ToString();    // This FP should never get fixed, because the old SE engine does not support Debug.Assert
+            return asserted.ToString();    // This S2259 FP should never get fixed, because the old SE engine does not support Debug.Assert
         }
 
         public string TryCatch(object arg) {
@@ -34,7 +34,7 @@ namespace Roslyn
             }
             catch (FormatException) when (arg == null)
             {
-                return arg.ToString();  // This FP should never get fixed, because the old SE engine does not know that FormatException cannot be throw by the arg?. part.
+                return arg.ToString();  // This S2259 FP should never get fixed, because the old SE engine does not know that FormatException cannot be throw by the arg?. part.
             }
             catch
             {
