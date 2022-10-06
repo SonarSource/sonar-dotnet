1f58eaa13bf1ddac3df0b38e2d076900f52b8ffe
Martin Strecker <martin.strecker@sonarsource.com>
2022-10-06 17:23:40 +0200

    Add one compliant example.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries_Dapper.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries_Dapper.cs
@@ -9,12 +9,13 @@ namespace Tests.Diagnostics
         private IDbConnection con = null;
         public void NonCompliant_Concat_Query(string query, string param)
         {
-            con.Query(query + param);                                   // Noncompliant
-            con.Query(typeof(object), query + param);                   // Noncompliant
-            SqlMapper.Query(con, query + param);                        // Noncompliant
-            SqlMapper.Query(con, typeof(object), query + param);        // FN. The string argument is in the third position for this overload invoked in the unreduced form
-            con.Query(query + param, new { Id = 1 });                   // Noncompliant
-            con.Query<DapperTest>(query + param, new { Id = 1 });       // Noncompliant
+            con.Query("Select Name From Person Where Id=@Id", new { Id = param}); // Compliant
+            con.Query(query + param);                                             // Noncompliant
+            con.Query(typeof(object), query + param);                             // Noncompliant
+            SqlMapper.Query(con, query + param);                                  // Noncompliant
+            SqlMapper.Query(con, typeof(object), query + param);                  // FN. The string argument is in the third position for this overload invoked in the unreduced form
+            con.Query(query + param, new { Id = 1 });                             // Noncompliant
+            con.Query<DapperTest>(query + param, new { Id = 1 });                 // Noncompliant
         }
     }
 }
