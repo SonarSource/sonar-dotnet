e80fdecd80d8a08b00161bbc1dabdcd30bb374c9
Corniel Nobel <mail@corniel.nl>
2022-10-09 10:40:20 +0200

    Implemented S2925 for both C# and VB.NET

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/TestsShouldNotUseThreadSleep.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Threading;
+using Xunit;
+
+class Compliant
+{
+    [Test]
+    public void TestWithoutThreadSleep()
+    {
+        Xunit.Assert.Equal("42", "The answer to life, the universe, and everything");
+    }
+
+    [Test]
+    public void CallToOtherSleepMethod()
+    {
+        Sleep(42);
+    }
+
+    void ThreadSleepNotInTest()
+    {
+        Thread.Sleep(42);
+    }
+
+    private void Sleep(int durartion) { }
+}
+
+class NonCompliant
+{
+    [Test]
+    public void ThreadSleepInNUnitTest()
+    {
+        Thread.Sleep(42); // {{Do not use Thread.Sleep() in a test.}}
+//      ^^^^^^^^^^^^^^^^
+    }
+
+    [Fact]
+    public void ThreadSleepInXUnitTest()
+    {
+        Thread.Sleep(42); // Noncompliant
+    }
+
+    [TestMethod]
+    public void ThreadSleepInMSTest()
+    {
+        Thread.Sleep(42); // Noncompliant
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/TestsShouldNotUseThreadSleep.vb
@@ -0,0 +1,41 @@
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports NUnit.Framework
+Imports System.Threading
+Imports Xunit
+
+Class Compliant
+    <Test>
+    Public Sub TestWithoutThreadSleep()
+        Xunit.Assert.Equal("42", "The answer to life, the universe, and everything")
+    End Sub
+
+    <Test>
+    Public Sub CallToOtherSleepMethod()
+        Sleep(42)
+    End Sub
+
+    Private Sub ThreadSleepNotInTest()
+        Thread.Sleep(42)
+    End Sub
+
+    Private Sub Sleep(ByVal durartion As Integer)
+    End Sub
+End Class
+
+Class NonCompliant
+    <Test>
+    Public Sub ThreadSleepInNUnitTest()
+        Thread.Sleep(42) ' {{Do not use Thread.Sleep() in a test.}}
+'       ^^^^^^^^^^^^^^^^
+    End Sub
+
+    <Fact>
+    Public Sub ThreadSleepInXUnitTest()
+        Thread.Sleep(42) ' Noncompliant
+    End Sub
+
+    <TestMethod>
+    Public Sub ThreadSleepInMSTest()
+        Thread.Sleep(42) ' Noncompliant
+    End Sub
+End Class
