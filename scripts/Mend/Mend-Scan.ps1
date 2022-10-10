c4ddc1faf653282f944d621d9f861542086fa003
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2022-10-10 14:13:16 +0200

    Rename WhiteSource to Mend (#6163)

---
+++ scripts/Mend/Mend-Scan.ps1
@@ -0,0 +1,14 @@
+function Get-Version {
+    $versionPropsXml = [xml](Get-Content -Path scripts/version/Version.props)
+    return $versionPropsXml.Project.PropertyGroup.MilestoneVersion
+}
+
+# Mend agent needs the following environment variables:
+# - WS_APIKEY
+# - WS_PRODUCTNAME
+# - WS_PROJECTNAME
+
+$env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $(Get-Version)"
+
+Write-Host "Running the Mend unified agent for $env:WS_PROJECTNAME..."
+& "$env:JAVA\bin\java.exe" -jar $env:MEND_AGENT_PATH -c "$PSScriptRoot\wss-unified-agent.config" -scanComment "buildNumber:$env:BUILD_NUMBER;gitSha:$env:GIT_SHA"
