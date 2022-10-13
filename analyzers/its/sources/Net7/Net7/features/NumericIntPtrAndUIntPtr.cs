cb8b549d3bba039c25dab599d218c62be79e8d60
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2022-10-13 15:46:59 +0200

    Add ITs with C# 11 features on .Net 7 runtime (#6203)

---
+++ analyzers/its/sources/Net7/Net7/features/NumericIntPtrAndUIntPtr.cs
@@ -0,0 +1,14 @@
+﻿namespace Net7.features
+{
+    internal class NumericIntPtrAndUIntPtr
+    {
+        public void Method()
+        {
+            nint intPtr = IntPtr.Zero;
+            nuint uintPtr  = UIntPtr.Zero;
+
+            IntPtr x = intPtr;
+            UIntPtr y = uintPtr;
+        }
+    }   
+}
---
+++ analyzers/its/sources/Net7/Net7/features/RawStringLiterals.cs
@@ -0,0 +1,21 @@
+﻿namespace Net7.features
+{
+    internal class RawStringLiterals
+    {
+        public string Method(decimal longitude, decimal latitude)
+        {
+            string longMessage = """
+    This is a long message.
+    It has several lines.
+        Some are indented
+                more than others.
+    Some should start at the first column.
+    Some have "quoted text" in them.
+    """;
+
+            return $$"""
+   You are at {{{longitude}}, {{latitude}}}
+   """;
+        }
+    }
+}
