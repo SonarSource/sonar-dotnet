1f172f335d85de3a7438e0317bbf72e5bd9a02ef
Costin Zaharia <costin.zaharia@sonarsource.com>
2022-10-13 13:30:35 +0200

    Add missing features

---
+++ analyzers/its/sources/Net7/Net7/features/ExtendedNameofScope.cs
@@ -0,0 +1,12 @@
+﻿namespace Net7.features
+{
+    [Obsolete(nameof(LogMessage))]
+    internal class ExtendedNameofScope
+    {
+        [Obsolete(nameof(message))]
+        public static void LogMessage(string? message)
+        {
+            Console.WriteLine(message.Length);
+        }
+    }
+}
---
+++ analyzers/its/sources/Net7/Net7/features/PatternMatchSpanOrReadOnlySpanOnAConstantString.cs
@@ -0,0 +1,8 @@
+﻿namespace Net7.features
+{
+    internal class PatternMatchSpanOrReadOnlySpanOnAConstantString
+    {
+        public bool Method(Span<char> span, ReadOnlySpan<char> readonlySpan) =>
+            span is "one" || readonlySpan is "two";
+    }
+}
