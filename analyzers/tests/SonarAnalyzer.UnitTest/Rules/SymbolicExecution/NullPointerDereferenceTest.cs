b249b963d8c209bc064e09a7dc894916f235841a
gregory-paidis-sonarsource <gregory.paidis@sonarsource.com>
2022-10-19 17:39:12 +0200

    C#11 Nameof scope: Added a UT for S2259 (NullPointerDereference)

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/NullPointerDereferenceTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/NullPointerDereferenceTest.cs
@@ -116,6 +116,15 @@ namespace SonarAnalyzer.UnitTest.Rules
 #else
 
         [TestMethod]
+        public void NullPointerDereference_Sonar_CSharp11() =>
+            sonar.AddPaths("NullPointerDereference.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
+
+        [TestMethod]
+        public void NullPointerDereference_Roslyn_CSharp11() =>
+            roslynCS.AddPaths("NullPointerDereference.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
+
+
+        [TestMethod]
         public void NullPointerDereference_Roslyn_CS_NetFx() =>
             roslynCS.AddPaths("NullPointerDereference.NetFx.cs").Verify();
 
