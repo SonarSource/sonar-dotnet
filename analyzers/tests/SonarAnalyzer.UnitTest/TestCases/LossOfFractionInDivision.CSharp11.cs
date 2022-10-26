209d6172cb3b871e2fc96afa3caa6cb0dde9f749
gregory-paidis-sonarsource <gregory.paidis@sonarsource.com>
2022-10-26 16:38:56 +0200

    Native int tests: Remove some C#11 redundant testcases for LossOfFractionInDivision (S2184)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LossOfFractionInDivision.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LossOfFractionInDivision.CSharp11.cs
@@ -2,7 +2,7 @@
 
 namespace Testcases
 {
-    class NativeIntChecks 
+    class NativeIntChecks
     {
         decimal result;
         Func<decimal> decimalFunc;
@@ -44,21 +44,9 @@ namespace Testcases
         {
             MethodAcceptingDecimal(intPtr1 / intPtr2); // Noncompliant
             MethodAcceptingDecimal(uIntPtr1 / uIntPtr2); // Noncompliant
-            MethodAcceptingDecimal(nint1 / nint2);  // Noncompliant
-            MethodAcceptingDecimal(nuint1 / nuint2); // Noncompliant
-            MethodAcceptingDecimal(intPtr1 / nint1); // Noncompliant
-            MethodAcceptingDecimal(nint1 / intPtr1); // Noncompliant
-            MethodAcceptingDecimal(uIntPtr1 / nuint1); // Noncompliant
-            MethodAcceptingDecimal(nuint1 / uIntPtr1); // Noncompliant
 
             MethodAcceptingDecimal((decimal)intPtr1 / intPtr2); // Compliant
             MethodAcceptingDecimal((decimal)uIntPtr1 / uIntPtr2); // Compliant
-            MethodAcceptingDecimal((decimal)nint1 / nint2);  // Compliant
-            MethodAcceptingDecimal((decimal)nuint1 / nuint2); // Compliant
-            MethodAcceptingDecimal((decimal)intPtr1 / nint1); // Compliant
-            MethodAcceptingDecimal((decimal)nint1 / intPtr1); // Compliant
-            MethodAcceptingDecimal((decimal)uIntPtr1 / nuint1); // Compliant
-            MethodAcceptingDecimal((decimal)nuint1 / uIntPtr1); // Compliant
 
             decimal MethodAcceptingDecimal(decimal arg) => arg;
         }
@@ -67,42 +55,18 @@ namespace Testcases
         {
             decimalFunc = () => intPtr1 / intPtr2; // Noncompliant
             decimalFunc = () => uIntPtr1 / uIntPtr2; // Noncompliant
-            decimalFunc = () => nint1 / nint2;  // Noncompliant
-            decimalFunc = () => nuint1 / nuint2; // Noncompliant
-            decimalFunc = () => intPtr1 / nint1; // Noncompliant
-            decimalFunc = () => nint1 / intPtr1; // Noncompliant
-            decimalFunc = () => uIntPtr1 / nuint1; // Noncompliant
-            decimalFunc = () => nuint1 / uIntPtr1; // Noncompliant
 
             decimalFunc = () => (decimal)intPtr1 / intPtr2; // Compliant
             decimalFunc = () => (decimal)uIntPtr1 / uIntPtr2; // Compliant
-            decimalFunc = () => (decimal)nint1 / nint2;  // Compliant
-            decimalFunc = () => (decimal)nuint1 / nuint2; // Compliant
-            decimalFunc = () => (decimal)intPtr1 / nint1; // Compliant
-            decimalFunc = () => (decimal)nint1 / intPtr1; // Compliant
-            decimalFunc = () => (decimal)uIntPtr1 / nuint1; // Compliant
-            decimalFunc = () => (decimal)nuint1 / uIntPtr1; // Compliant
         }
 
         decimal MethodReturnChecks()
         {
             return intPtr1 / intPtr2; // Noncompliant
             return uIntPtr1 / uIntPtr2; // Noncompliant
-            return nint1 / nint2;  // Noncompliant
-            return nuint1 / nuint2; // Noncompliant
-            return intPtr1 / nint1; // Noncompliant
-            return nint1 / intPtr1; // Noncompliant
-            return uIntPtr1 / nuint1; // Noncompliant
-            return nuint1 / uIntPtr1; // Noncompliant
 
             return (decimal)intPtr1 / intPtr2; // Compliant
             return (decimal)uIntPtr1 / uIntPtr2; // Compliant
-            return (decimal)nint1 / nint2;  // Compliant
-            return (decimal)nuint1 / nuint2; // Compliant
-            return (decimal)intPtr1 / nint1; // Compliant
-            return (decimal)nint1 / intPtr1; // Compliant
-            return (decimal)uIntPtr1 / nuint1; // Compliant
-            return (decimal)nuint1 / uIntPtr1; // Compliant
         }
     }
 }
