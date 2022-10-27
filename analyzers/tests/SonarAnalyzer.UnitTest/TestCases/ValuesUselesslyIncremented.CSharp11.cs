73f64e4db7297e567d104944f2124a8d1e556283
gregory-paidis-sonarsource <gregory.paidis@sonarsource.com>
2022-10-27 17:15:06 +0200

    Native int tests: Add C#11 testcases for ValuesUselesslyIncremented (S2123)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValuesUselesslyIncremented.CSharp11.cs
@@ -14,3 +14,33 @@ public class WeirdOperatorOverload : IDecrementOperators<WeirdOperatorOverload>
     public static WeirdOperatorOverload operator --(WeirdOperatorOverload value) => throw new NotImplementedException();
 }
 
+public class NativeIntTestcases
+{
+    public nint Method()
+    {
+        IntPtr i = 0;
+        i = i++; // Noncompliant; i is still zero
+        i = ++i; // Compliant
+
+        UIntPtr u = 0, a = 0;
+        u = u++; // Noncompliant; u is still zero
+        u = ++u; // Compliant
+        u += u++;
+        a = u++;
+
+        int x = (int)u++;
+
+        return i++; // Noncompliant
+    }
+
+    IntPtr Compute(IntPtr v)
+    {
+        return v++; // Noncompliant
+//             ^^^
+    }
+
+    IntPtr ComputeArrowBody(IntPtr v) =>
+        v--; // Noncompliant
+//      ^^^
+
+}
