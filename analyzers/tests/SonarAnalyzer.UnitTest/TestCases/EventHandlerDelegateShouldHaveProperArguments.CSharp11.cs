a90f17a4a2687a310af0b28d92c6feacd6b47c38
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2022-10-28 16:59:54 +0200

    Static virtual event test cases for S4220

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EventHandlerDelegateShouldHaveProperArguments.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EventHandlerDelegateShouldHaveProperArguments.CSharp11.cs
@@ -4,47 +4,14 @@ namespace Tests.Diagnostics
 {
     interface IMyInterface
     {
-        static event EventHandler SomeStaticEvent;
+        static virtual event EventHandler VirtualEvent;
 
-        static abstract event EventHandler AbstractEvent;
-
-        static virtual event EventHandler VirtualEvent
+        static virtual void VirtualMethod<T>(object sender, EventArgs e) where T : IMyInterface
         {
-            add => SomeStaticEvent += value;
-            remove => SomeStaticEvent -= value;
-        }
-
-        static virtual void VirtualMethod(object sender, EventArgs e)
-        {
-            SomeStaticEvent?.Invoke(null, e);
-            SomeStaticEvent?.Invoke(sender, e);     // Noncompliant {{Make the sender on this static event invocation null.}}
-            SomeStaticEvent?.Invoke(null, null);    // Noncompliant {{Use 'EventArgs.Empty' instead of null as the event args of this event invocation.}}
-            SomeStaticEvent?.Invoke(sender, null);  // Noncompliant
-                                                    // Noncompliant@-1
-
-            SomeStaticEvent(null, null);            // Noncompliant
-        }
-
-        static abstract void AbstractMethod(EventHandler handler, EventArgs e);
-    }
-
-    class MyClass : IMyInterface
-    {
-        private static event EventHandler PreAbstractEvent;
-        public static event EventHandler AbstractEvent
-        {
-            add => PreAbstractEvent += value;
-            remove => PreAbstractEvent -= value;
-        }
-
-        public static void AbstractMethod(EventHandler handler, EventArgs e)
-        {
-            handler?.Invoke(new object(), e);
-            handler?.Invoke(new object(), null);            // Noncompliant {{Use 'EventArgs.Empty' instead of null as the event args of this event invocation.}}
-
-            PreAbstractEvent?.Invoke(new object(), e);      // Noncompliant {{Make the sender on this static event invocation null.}}
-            PreAbstractEvent?.Invoke(new object(), null);   // Noncompliant {{Use 'EventArgs.Empty' instead of null as the event args of this event invocation.}}
-                                                            // Noncompliant@-1
+            T.VirtualEvent.Invoke(null, null);      // Noncompliant
+            VirtualEvent.Invoke(null, null);        // Noncompliant
+            T.VirtualEvent.Invoke(null, e);         // Compliant
+            VirtualEvent.Invoke(null, e);           // Compliant
         }
     }
 }
