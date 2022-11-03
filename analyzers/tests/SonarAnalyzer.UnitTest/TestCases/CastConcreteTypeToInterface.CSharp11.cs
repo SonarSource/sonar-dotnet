4738955d54849ea010d598f37aaed7cc0658a34b
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-03 12:06:06 +0100

    make examples for c# 11 UT smaller

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CastConcreteTypeToInterface.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CastConcreteTypeToInterface.CSharp11.cs
@@ -7,14 +7,9 @@ namespace Tests.Diagnostics
     {
         void DoStuff();
     }
-    file interface IMyInterface2
-    {
-    }
 
     file class MyClass1 : IMyInterface
     {
-        public int Data { get { return new Random().Next(); } }
-
         public void DoStuff()
         {
             // TODO...
@@ -27,21 +22,9 @@ namespace Tests.Diagnostics
         {
             MyClass1 class1 = (MyClass1)interfaceRef;  // Noncompliant {{Remove this cast and edit the interface to add the missing functionality.}}
 //                            ^^^^^^^^^^^^^^^^^^^^^^
-            int privateData = class1.Data;
 
             class1 = interfaceRef as MyClass1;  // Noncompliant
 //                   ^^^^^^^^^^^^^^^^^^^^^^^^
-            if (class1 != null)
-            {
-                // ...
-            }
-
-            var interf = (IMyInterface2)interfaceRef;
-            interf = (IMyInterface2)class1;
-            var o = (object)interfaceRef;
-
-            IEnumerable<int> list = null;
-            var l = (List<int>)list; // Compliant
         }
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassNotInstantiatable.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassNotInstantiatable.CSharp11.cs
@@ -8,23 +8,12 @@ file record Person(string FirstName, string LastName); // Compliant
 file record Person1 // Noncompliant {{This record can't be instantiated; make its constructor 'public'.}}
 {
     public string FirstName { get; }
-
     private Person1() { }
-}
-
-file record StaticUsage // Noncompliant
-{
-    private StaticUsage() { }
-    public static void M() { }
 }
 
 file class Baz { }
 
 file class Bar  // Noncompliant
 {
-    public static readonly Baz Instance = new();
-
-    public bool IsActive => true;
-
     private Bar() { }
 }
