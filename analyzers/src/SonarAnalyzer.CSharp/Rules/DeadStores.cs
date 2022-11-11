0c4729be9e29ae2dea7acd11372187cfd693cab4
Čaba Šagi <csaba.sagi@sonarsource.com>
2022-11-11 16:56:23 +0100

    Remove unused usings

--- analyzers/src/SonarAnalyzer.CSharp/Rules/DeadStores.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/DeadStores.cs
@@ -18,21 +18,9 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Diagnostics;
 using SonarAnalyzer.CFG.LiveVariableAnalysis;
 using SonarAnalyzer.CFG.Sonar;
-using SonarAnalyzer.Common;
-using SonarAnalyzer.Extensions;
-using SonarAnalyzer.Helpers;
 using SonarAnalyzer.LiveVariableAnalysis.CSharp;
-using StyleCop.Analyzers.Lightup;
 
 namespace SonarAnalyzer.Rules.CSharp
 {
