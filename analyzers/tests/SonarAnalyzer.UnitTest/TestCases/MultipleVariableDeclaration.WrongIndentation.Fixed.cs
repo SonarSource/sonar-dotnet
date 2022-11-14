161fde5e465190b76588e9b1b8aaea528894db3e
Costin Zaharia <costin.zaharia@sonarsource.com>
2022-11-14 12:01:23 +0100

    Remove namespace from test case

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MultipleVariableDeclaration.WrongIndentation.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MultipleVariableDeclaration.WrongIndentation.Fixed.cs
@@ -1,11 +1,10 @@
-﻿namespace App
+﻿class TestCases
 {
-    class TestCases
-    {
-            public void WrongIndendation()
-            {
-                int c;
-            int d;
-        }
+        // Method is indentation is bigger than the default (8 spaces instead of 4).
+        // After codefix, method itself and line "int c" is on expected indent, while "int d" gets missaligned.
+        public void WrongIndendation()
+        {
+            int c;
+        int d;
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MultipleVariableDeclaration.WrongIndentation.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MultipleVariableDeclaration.WrongIndentation.cs
@@ -1,12 +1,9 @@
-﻿namespace App
+﻿class TestCases
 {
-    class TestCases
-    {
-            // Method is indentation is bigger than the default (8 spaces instead of 4).
-            // After codefix, method itself and line "int c" is on expected indent, while "int d" gets missaligned.
-            public void WrongIndendation()
-            {
-                int c, d; // Noncompliant
-            }
-    }
+        // Method is indentation is bigger than the default (8 spaces instead of 4).
+        // After codefix, method itself and line "int c" is on expected indent, while "int d" gets missaligned.
+        public void WrongIndendation()
+        {
+            int c, d; // Noncompliant
+        }
 }
