477702a41fc8ce0512c638c8acff12885689afcd
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-22 17:10:18 +0100

    Add string interpolation and raw string literal test to NameOfShouldBeUsed

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NameOfShouldBeUsed.CSharp11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    class Program
+    {
+        public void Method_With_RawStringLiterals(int arg1)
+        {
+            if (arg1 < 0)
+                throw new Exception("""arg1"""); // FN
+        }
+
+        public void Method_With_NewLinesInStringInterpolation(int arg1)
+        {
+            string argName = "arg1";
+            if (arg1 < 0)
+            {
+                throw new Exception($"{
+                    arg1 switch
+                    {
+                        < 0 => "arg1",  // Noncompliant
+                        _ => "Can't touch this",
+                    }}");
+            }
+            if (arg1 == 0)
+            {
+                string arg1Name = "arg1";
+                throw new Exception($"{arg1}"); // FN
+            }
+        }
+    }
+}
