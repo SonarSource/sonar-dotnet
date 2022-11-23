82fb3624db982533e79e371d532ec38aa04388ca
Martin Strecker <martin.strecker@sonarsource.com>
2022-11-23 12:37:14 +0100

    Add test cases for Half, NFloat and IFloatingPointIeee754

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EqualityOnFloatingPoint.CSharp11.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+public class EqualityOnFloatingPoint
+{
+    bool HalfEqual(Half first, Half second)
+        => first == second; // FN
+
+    bool NFloatEqual(NFloat first, NFloat second)
+        => first == second; // FN
+
+    bool AreEqual<T>(IFloatingPointIeee754<T> first, IFloatingPointIeee754<T> second) where T : IFloatingPointIeee754<T>
+        => first == second; // FN
+}
