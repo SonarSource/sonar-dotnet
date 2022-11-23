97878a0bce78f69f13b4ade66c5b73cebb2591f8
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-23 16:19:21 +0100

    add UT for OperatorsShouldBeOverloadedConsistently

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/OperatorsShouldBeOverloadedConsistently.CSharp11.cs
@@ -0,0 +1,18 @@
+﻿public class Foo : IOperator<Foo> // Compliant - it get + operator from IOperator interface
+{
+    public static object operator -(Foo a, Foo b) => new object();
+    public static object operator *(Foo a, Foo b) => new object();
+    public static object operator /(Foo a, Foo b) => new object();
+    public static object operator %(Foo a, Foo b) => new object();
+    public static object operator ==(Foo a, Foo b) => new object();
+    public static object operator !=(Foo a, Foo b) => new object();
+
+    public override bool Equals(object obj) => false;
+    public override int GetHashCode() => 0;
+}
+
+internal interface IOperator<T> where T : IOperator<T>
+{
+    static virtual T operator +(T a, T b) => a;
+}
+
