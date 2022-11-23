798d2da7767e82f06b1c20ccf1e3386b2e9ec7fe
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-23 17:20:02 +0100

    small UT code cleanup

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/OperatorOverloadsShouldHaveNamedAlternatives.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/OperatorOverloadsShouldHaveNamedAlternatives.CSharp11.cs
@@ -4,6 +4,4 @@ using System;
 internal interface IOperator<T> where T : IOperator<T>
 {
     static virtual T operator ++(T input) => input; // Noncompliant {{Implement alternative method 'Increment' for the operator '++'.}}
-
-    static virtual T Increase(T input) => input;
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/OperatorsShouldBeOverloadedConsistently.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/OperatorsShouldBeOverloadedConsistently.CSharp11.cs
@@ -1,4 +1,4 @@
-﻿public class Foo : IOperator<Foo> // Compliant - it get + operator from IOperator interface
+﻿public class Foo : IOperator<Foo> // Compliant - the '+' operator comes IOperator interface
 {
     public static object operator -(Foo a, Foo b) => new object();
     public static object operator *(Foo a, Foo b) => new object();
@@ -15,4 +15,3 @@ internal interface IOperator<T> where T : IOperator<T>
 {
     static virtual T operator +(T a, T b) => a;
 }
-
