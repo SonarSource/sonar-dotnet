5b243d036fe2aedf20e01c1eed033725226229d1
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2022-11-25 15:55:31 +0100

    S1449: Add repro (#6439) for missing DateTime types. (#6440)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/StringOperationWithoutCulture.CSharp10.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Tests.Diagnostics
+{
+    public class StringOperationWithoutCulture
+    {
+        // Repro for https://github.com/SonarSource/sonar-dotnet/issues/6439
+        void TestDateTimeMethodsNet6()
+        {
+            var time = new TimeOnly().ToString(); // FN
+            var date = new DateOnly().ToString(); // FN
+        }
+    }
+}
