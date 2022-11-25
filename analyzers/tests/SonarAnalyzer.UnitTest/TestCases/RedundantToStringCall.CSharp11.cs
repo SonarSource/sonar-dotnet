9221b4ad74f17bd1cab6c4c7c8e276ca11e0324d
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-25 16:37:34 +0100

    add uts for RedundantToString Calls

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantToStringCall.CSharp11.cs
@@ -0,0 +1,15 @@
+﻿class Foo
+{
+    public void Method()
+    {
+        string rawStringLiteral = """Hello""";
+        string magic = "Abracadabra";
+
+        string interpolationWithNewLine = $"And for my next trick I'll do some {
+            magic.ToString() // Noncompliant {{There's no need to call 'ToString()' on a string.}}
+            }";
+
+        var s = rawStringLiteral.ToString(); // Noncompliant {{There's no need to call 'ToString()' on a string.}}
+        s = interpolationWithNewLine.ToString(); // Noncompliant {{There's no need to call 'ToString()' on a string.}}
+    }
+}
