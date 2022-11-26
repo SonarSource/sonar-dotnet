2afe28a643c80697b976f3a148f8d0d0507316e4
Corniel Nobel <mail@corniel.nl>
2022-11-26 13:57:16 +0100

    Remove global used namespaces.

--- analyzers/src/SonarAnalyzer.Common/Rules/SpecifyTimeoutOnRegexBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/SpecifyTimeoutOnRegexBase.cs
@@ -18,10 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Linq;
 using System.Text.RegularExpressions;
-using Microsoft.CodeAnalysis;
-using SonarAnalyzer.Helpers;
 
 namespace SonarAnalyzer.Rules;
 
