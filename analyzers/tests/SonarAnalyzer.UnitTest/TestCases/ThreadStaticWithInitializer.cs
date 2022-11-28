d88edd3b5559ea5c87f773441340a22b3dc45794
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2022-11-28 14:12:50 +0100

    S2996: Document decision about derived classes and cleanup (#6454)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ThreadStaticWithInitializer.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ThreadStaticWithInitializer.cs
@@ -14,6 +14,23 @@ namespace Tests.Diagnostics
             public static object _perThreadObject;
 
             public static object StaticObject = new object();
+
+            [System.ThreadStatic]
+            public static object PerThreadObject2 = new object(); // Noncompliant
+
+            [ThreadStaticAttribute]
+            public static object PerThreadObject3 = new object(); // Noncompliant
         }
+    }
+
+    public class ThreadStaticWithInitializerDerivedAttribute
+    {
+        public class Foo
+        {
+            [DerivedAttribute]
+            public static object PerThreadObject = new object(); // FN for performance reasons we decided not to handle derived classes
+        }
+
+        public class DerivedAttribute : ThreadStaticAttribute { }
     }
 }
