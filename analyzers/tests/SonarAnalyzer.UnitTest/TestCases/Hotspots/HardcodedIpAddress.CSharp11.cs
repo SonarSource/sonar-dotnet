ccb13a52bb0e7d56da5be69890a842423f7b8cdf
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-11-30 17:09:04 +0100

    update hardcodedIPaddress UTs as FNs for utf-8 were fixed

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/HardcodedIpAddress.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/HardcodedIpAddress.CSharp11.cs
@@ -8,11 +8,11 @@ namespace Tests.Diagnostics
         public void RawStringLiterals(string unknownPart, string knownPart)
         {
             string ip1 = """192.168.0.1"""; // Noncompliant {{Make sure using this hardcoded IP address '192.168.0.1' is safe here.}}
-            var ip2 = "192.168.0.1"u8; // FN
-            var ip3 = """192.168.0.1"""u8; // FN
+            var ip2 = "192.168.0.1"u8; // Noncompliant
+            var ip3 = """192.168.0.1"""u8; // Noncompliant
             var ip4 = """
                 192.168.0.1
-                """u8; // FN
+                """u8; // Noncompliant@-2
         }
 
         public void NewlinesInStringInterpolation()
