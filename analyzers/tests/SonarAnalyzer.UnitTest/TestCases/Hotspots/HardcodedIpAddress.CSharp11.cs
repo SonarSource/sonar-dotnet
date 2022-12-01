45102711977e847bdaa2610c8f03a0e7210585a7
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2022-12-01 09:28:09 +0100

    add example in UTF-8

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/HardcodedIpAddress.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/HardcodedIpAddress.CSharp11.cs
@@ -9,8 +9,9 @@ namespace Tests.Diagnostics
         {
             string ip1 = """192.168.0.1"""; // Noncompliant {{Make sure using this hardcoded IP address '192.168.0.1' is safe here.}}
             var ip2 = "192.168.0.1"u8; // Noncompliant
-            var ip3 = """192.168.0.1"""u8; // Noncompliant
-            var ip4 = """
+            var ip3 = "\x31\x39\x32\x2E\x31\x36\x38\x2E\x30\x2E\x31"u8; // Noncompliant - this is 192.168.0.1 in utf-8
+            var ip4 = """192.168.0.1"""u8; // Noncompliant
+            var ip5 = """
                 192.168.0.1
                 """u8; // Noncompliant@-2
         }
