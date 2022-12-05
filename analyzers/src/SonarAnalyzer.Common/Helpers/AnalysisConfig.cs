8dcea9a2459d811699926fcd65c685a28ded5bde
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-12-05 13:19:14 +0100

    Incremental PR Analysis: Load unchanged changed files

---
+++ analyzers/src/SonarAnalyzer.Common/Helpers/AnalysisConfig.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Xml.Serialization;
+
+namespace SonarAnalyzer.Helpers;
+
+/// <summary>
+/// Data class to describe an analysis configuration.
+/// </summary>
+/// <remarks>
+/// This class is the counterpart of SonarScanner.MSBuild.Common.AnalysisConfig, and is used for easy deserialization.
+/// This class should not be used in this codebase. To get configuration properties, use <see cref="AnalysisConfigReader"/>.
+/// </remarks>
+[XmlRoot(Namespace = "http://www.sonarsource.com/msbuild/integration/2015/1")]
+public class AnalysisConfig
+{
+    public List<ConfigSetting> AdditionalConfig { get; set; }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Helpers/AnalysisConfigReaderTest.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2022 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.IO;
+
+namespace SonarAnalyzer.UnitTest.Helpers
+{
+    [TestClass]
+    public class AnalysisConfigReaderTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void MissingFile_Throws()
+        {
+            var path = Path.GetFullPath("ThisFileDoesNotExist.xml");
+            ((Func<AnalysisConfigReader>)(() => new AnalysisConfigReader(path))).Should().Throw<InvalidOperationException>().WithMessage($"File '{path}' could not be parsed.");
+        }
+
+        [TestMethod]
+        [DataRow(@"<?xml that is invalid")]
+        [DataRow(@"<ValidXml><UnexpectedContent /></ValidXml>")]
+        public void UnexpectedXml_Throws(string xml)
+        {
+            var path = TestHelper.WriteFile(TestContext, "SonarQubeAnalysisConfig.xml", xml);
+            ((Func<AnalysisConfigReader>)(() => new AnalysisConfigReader(path))).Should().Throw<InvalidOperationException>().WithMessage($"File '{path}' could not be parsed.");
+        }
+
+        [TestMethod]
+        public void UnchangedFiles_NotPresent_ReturnsEmpty()
+        {
+            var path = TestHelper.CreateAnalysisConfig(TestContext, "SomeOtherProperty", "This is not UnchangedFilesPath");
+            var sut = new AnalysisConfigReader(path);
+            sut.UnchangedFiles().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void UnchangedFiles_Empty_ReturnsEmpty()
+        {
+            var path = TestHelper.CreateAnalysisConfig(TestContext, Array.Empty<string>());
+            var sut = new AnalysisConfigReader(path);
+            sut.UnchangedFiles().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void UnchangedFiles_Present_ReturnsContent()
+        {
+            var path = TestHelper.CreateAnalysisConfig(TestContext, new[] { @"C:\File1.cs", @"C:\File2.cs", "Any other string" });
+            var sut = new AnalysisConfigReader(path);
+            sut.UnchangedFiles().Should().BeEquivalentTo(@"C:\File1.cs", @"C:\File2.cs", "Any other string");
+        }
+    }
+}
