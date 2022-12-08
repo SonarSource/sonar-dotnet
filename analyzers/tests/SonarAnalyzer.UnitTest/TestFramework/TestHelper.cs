e5688519157f5a2b564291a15132283e947b9b0e
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-12-08 16:57:24 +0100

    Fix UTs on CI

--- analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/TestHelper.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestFramework/TestHelper.cs
@@ -223,11 +223,14 @@ End Class", AnalyzerLanguage.VisualBasic);
         {
             var root = Path.Combine(context.TestDir, context.FullyQualifiedTestClassName.Replace("SonarAnalyzer.UnitTest.", null));
             var directoryName = root.Length + context.TestName.Length + fileName.Length > 250   // 260 can throw PathTooLongException
-                ? $"TooLongTestName.{Directory.GetDirectories(root).Length}"
+                ? $"TooLongTestName.{RootSubdirectoryCount()}"
                 : context.TestName;
             var path = Path.Combine(root, directoryName, fileName);
             Directory.CreateDirectory(Path.GetDirectoryName(path));
             return path;
+
+            int RootSubdirectoryCount() =>
+                Directory.Exists(root) ? Directory.GetDirectories(root).Length : 0
         }
 
         public static string WriteFile(TestContext context, string fileName, string content = null)
