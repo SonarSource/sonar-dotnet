0e2e98069c15142e6e03a67d2486e8890cf54636
Martin Strecker <martin.strecker@sonarsource.com>
2022-12-09 17:21:26 +0100

    Mark test method as non-compliant

--- analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/Net7/S107.cs
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/Net7/S107.cs
@@ -4,7 +4,7 @@ namespace Net7
 {
     public static unsafe partial class S107
     {
-        public static void TooManyArguments(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8)
+        public static void TooManyArguments(int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) // Noncompliant (S107): Method has 8 parameters, which is greater than the 7 authorized.
         {
 
         }
