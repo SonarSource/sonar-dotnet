f1148b3f803f2770af574403a268aa6c89e5d0b5
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2022-12-12 10:36:09 +0100

    Added interpolated test cases for raw string

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NameOfShouldBeUsed.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NameOfShouldBeUsed.CSharp11.cs
@@ -4,16 +4,23 @@ namespace Tests.Diagnostics
 {
     class Program
     {
-        public void Method_With_RawStringLiterals(int arg1)
+        public void Method_With_RawStringLiterals(int arg1, string argument)
         {
             if (arg1 < 0)
                 throw new Exception("""arg1"""); // Noncompliant
-            else if (arg1 < 100)
+            else if (arg1 < 1)
                 throw new ArgumentException("""Bad parameter name""", """arg1"""); // Noncompliant
-            else if (arg1 < 1000)
+            else if (arg1 < 2)
                 throw new ArgumentOutOfRangeException("""
                     arg1
                     """); // Noncompliant@-2
+            else if (arg1 < 3)
+                throw new Exception($"""argument {argument}"""); // Noncompliant
+//                                      ^^^^^^^^^
+            else if (arg1 < 4)
+                throw new Exception($"""arg1 {arg1}"""); // Compliant (parameter length 4, minimum allowed to raise 5)
+            else
+                throw new Exception($"""{nameof(argument)} should not be used with value {argument}"""); // Compliant
         }
 
 
