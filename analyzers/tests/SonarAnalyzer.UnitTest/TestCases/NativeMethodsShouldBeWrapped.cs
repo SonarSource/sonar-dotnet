073fd0de56adabc9cf55019025d7230454f71194
Martin Strecker <martin.strecker@sonarsource.com>
2022-12-13 14:40:45 +0100

    Make test case noncompliant

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.cs
@@ -109,8 +109,8 @@ namespace TestsFromDeprecatedS4214
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
         private static extern bool RemoveDirectory5(string name);
 
-        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] // Error [CS0601] - so do not raise an issue
-        public extern bool RemoveDirectory6(string name);
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] // Error [CS0601]
+        public extern bool RemoveDirectory6(string name);      // Noncompliant
 
         [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] // Error [CS0601]
         public static bool RemoveDirectory7(string name); // Error [CS0501] - so do not raise an issue
