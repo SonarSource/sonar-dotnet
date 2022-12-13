fe0002f5ef3bc31147d101e1a51e3c5b3d79a23f
Martin Strecker <martin.strecker@sonarsource.com>
2022-12-13 14:41:19 +0100

    Formatting

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.CSharp9.cs
@@ -3,9 +3,9 @@
     extern private static void Extern0(); // Compliant
 
     extern private static void Extern1(string s, int x); // Compliant
-    
+
     extern public static void Extern2(string s, int x); // Noncompliant {{Make this native method private and provide a wrapper.}}
-//                            ^^^^^^^
+    //                        ^^^^^^^
 
     public void Method()
     {
