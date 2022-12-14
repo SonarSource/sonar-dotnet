5728cf49a0ff3022406272f9b66434a0f77f78a8
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2022-12-14 12:54:49 +0100

    Tests for S4200: Add testcases from S4214 (#6530)
    * Copy version independent tests
    
    * Fix marker
    
    * Fix message
    
    * Make test case noncompliant
    
    * Formatting
    
    * Copy C#9 tests
    
    * Copy C#11 tests
    
    * Formatting

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.CSharp9.cs
@@ -1,14 +1,31 @@
-﻿public record Record
+﻿using System.Runtime.InteropServices;
+
+public record Record
 {
     extern private static void Extern0(); // Compliant
 
     extern private static void Extern1(string s, int x); // Compliant
-    
+
     extern public static void Extern2(string s, int x); // Noncompliant {{Make this native method private and provide a wrapper.}}
-//                            ^^^^^^^
+    //                        ^^^^^^^
 
     public void Method()
     {
         extern static void Extern(string s, int x); // Compliant - local method
+    }
+}
+
+namespace TestsFromDeprecatedS4214
+{
+    public record Record
+    {
+        public void Method()
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+            static extern bool RemoveDirectory(string name);     // Compliant - Method is not publicly exposed
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool RemoveDirectory(string name);  // Noncompliant
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/NativeMethodsShouldBeWrapped.cs
@@ -88,3 +88,37 @@ namespace Tests.Diagnostics
         }
     }
 }
+
+namespace TestsFromDeprecatedS4214
+{
+    public class Program
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool RemoveDirectory1(string name); // Noncompliant {{Make this native method private and provide a wrapper.}}
+        //                        ^^^^^^^^^^^^^^^^
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        protected static extern bool RemoveDirectory2(string name); // Noncompliant
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal protected static extern bool RemoveDirectory3(string name); // Noncompliant
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        internal static extern bool RemoveDirectory4(string name);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        private static extern bool RemoveDirectory5(string name);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] // Error [CS0601]
+        public extern bool RemoveDirectory6(string name);      // Noncompliant
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)] // Error [CS0601]
+        public static bool RemoveDirectory7(string name); // Error [CS0501] - so do not raise an issue
+    }
+
+    internal class Foo
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool RemoveDirectory1(string name); // Compliant because effective accessibility is not public
+    }
+}
