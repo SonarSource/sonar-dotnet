72d63a9c996c1ad19b7ebd26ecf08405c4d0922e
Martin Strecker <martin.strecker@sonarsource.com>
2022-12-14 17:35:42 +0100

    Remove uncovered syntaxkinds from ArgumentExpressions

--- analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Facade/CSharpSyntaxFacade.cs
@@ -49,7 +49,6 @@ internal sealed class CSharpSyntaxFacade : SyntaxFacade<SyntaxKind>
     public override IEnumerable<SyntaxNode> ArgumentExpressions(SyntaxNode node) =>
         node switch
         {
-            ArgumentListSyntax list => list.Arguments.Select(x => x.Expression),
             ObjectCreationExpressionSyntax creation => creation.ArgumentList?.Arguments.Select(x => x.Expression) ?? Enumerable.Empty<SyntaxNode>(),
             null => Enumerable.Empty<SyntaxNode>(),
             var _ when ImplicitObjectCreationExpressionSyntaxWrapper.IsInstance(node)
