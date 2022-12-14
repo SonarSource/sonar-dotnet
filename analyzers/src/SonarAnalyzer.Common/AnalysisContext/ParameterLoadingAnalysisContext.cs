338b74139db2b7d006ecce22bed421f480a907dc
Pavel Mikula <pavel.mikula@sonarsource.com>
2022-12-14 16:09:31 +0100

    Migrate RegisterCompilationStartAction

--- analyzers/src/SonarAnalyzer.Common/AnalysisContext/ParameterLoadingAnalysisContext.cs
+++ analyzers/src/SonarAnalyzer.Common/AnalysisContext/ParameterLoadingAnalysisContext.cs
@@ -22,7 +22,7 @@ using Microsoft.CodeAnalysis.Text;
 
 namespace SonarAnalyzer.Helpers;
 
-public sealed class ParameterLoadingAnalysisContext : SonarAnalysisContextBase // FIXME: Refactor: Use Sonar* prefix, change contract not to expose the collection, rename everything to "Postponed" to make it clear, add docs
+public sealed class ParameterLoadingAnalysisContext : SonarAnalysisContextBase
 {
     private readonly List<Action<SonarCompilationStartAnalysisContext>> postponedActions = new();
 
