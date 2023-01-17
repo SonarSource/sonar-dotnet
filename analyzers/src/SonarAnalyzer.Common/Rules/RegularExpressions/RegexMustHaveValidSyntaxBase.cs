1c144c5f277644dd91b1f32cb5bb5ae05228e3c7
Corniel Nobel <mail@corniel.nl>
2023-01-17 17:32:13 +0100

    Implement S5856

---
+++ analyzers/src/SonarAnalyzer.Common/Rules/RegularExpressions/RegexMustHaveValidSyntaxBase.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2023 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarAnalyzer.RegularExpressions;
+
+namespace SonarAnalyzer.Rules;
+
+public abstract class RegexMustHaveValidSyntaxBase<TSyntaxKind> : SonarDiagnosticAnalyzer<TSyntaxKind>
+    where TSyntaxKind : struct
+{
+    private const string DiagnosticId = "S101"; //"S5856";
+
+    protected sealed override string MessageFormat => "The pattern contains a syntax error: {0}";
+
+    protected RegexMustHaveValidSyntaxBase() : base(DiagnosticId) { }
+
+    protected override void Initialize(SonarAnalysisContext context)
+    {
+        context.RegisterNodeAction(
+            Language.GeneratedCodeRecognizer,
+            c => Analyze(c, RegexNode.FromCtor(c.Node, c.SemanticModel, Language)),
+            Language.SyntaxKind.ObjectCreationExpressions);
+
+        context.RegisterNodeAction(
+            Language.GeneratedCodeRecognizer,
+            c => Analyze(c, RegexNode.FromMethod(c.Node, c.SemanticModel, Language)),
+            Language.SyntaxKind.InvocationExpression);
+
+        context.RegisterNodeAction(
+            Language.GeneratedCodeRecognizer,
+            c => Analyze(c, RegexNode.FromAttribute(c.Node, c.SemanticModel, Language)),
+            Language.SyntaxKind.Attribute);
+    }
+
+    private void Analyze(SonarSyntaxNodeReportingContext c, RegexNode regex)
+    {
+        if (regex is { }
+            && regex.Pattern.Value is { } pattern
+            && RegexTree.Parse(pattern, regex.Options.Value).ParseError is { } error)
+        {
+            c.ReportIssue(Diagnostic.Create(Rule, regex.Pattern.Node.GetLocation(), error.Message));
+        }
+    }
+}
