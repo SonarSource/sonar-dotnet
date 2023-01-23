06d7ecc23df24a088b0e2d006866af317967f99e
Corniel Nobel <mail@corniel.nl>
2023-01-23 11:31:30 +0100

    Drop not relevant namespaces.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValueTypeShouldImplementIEquatable.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ValueTypeShouldImplementIEquatable.cs
@@ -1,23 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Tests.Diagnostics
+struct MyStruct // Noncompliant {{Implement 'IEquatable<T>' in value type 'MyStruct'.}}
+//     ^^^^^^^^
 {
-    struct MyStruct // Noncompliant {{Implement 'IEquatable<T>' in value type 'MyStruct'.}}
-//         ^^^^^^^^
-    {
-    }
+}
 
-    struct MyCompliantStruct : IEquatable<MyCompliantStruct> // Compliant
+struct MyCompliantStruct : IEquatable<MyCompliantStruct> // Compliant
+{
+    public bool Equals(MyCompliantStruct other)
     {
-        public bool Equals(MyCompliantStruct other)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
     }
+}
 
-    // https://github.com/SonarSource/sonar-dotnet/issues/3157
-    ref struct Repro_3157 // Compliant, ref structs can not implement interfaces
-    {
-    }
+// https://github.com/SonarSource/sonar-dotnet/issues/3157
+ref struct Repro_3157 // Compliant, ref structs can not implement interfaces
+{
 }
