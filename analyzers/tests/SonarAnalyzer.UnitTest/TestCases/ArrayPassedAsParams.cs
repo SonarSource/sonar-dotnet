9acb0386adf87fb37ec4f50da04bf93373c79c3d
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-01-24 09:35:19 +0100

    First batch of test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.cs
@@ -2,4 +2,24 @@
 
 public class Program
 {
+    public void Base(string[] myArray)
+    {
+        Method(new string[] { "s1", "s2" }); // Noncompliant
+
+        Method("s1");           // Compliant
+        Method("s1", "s2");     // Compliant
+        Method(myArray);        // Compliant
+        Method(new string[12]); // Compliant
+
+        Method2(1, new string[] { "s1", "s2" }); // Noncompliant
+
+        Method2(1, "s1");           // Compliant
+        Method2(1, "s1", "s2");     // Compliant
+        Method2(1, myArray);        // Compliant
+        Method2(1, new string[12]); // Compliant
+    }
+
+    public void Method(params string[] args) { }
+
+    public void Method2(int a, params string[] args) { }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.vb
@@ -1,7 +1,23 @@
 ﻿Public Class Program
 
-    Public Sub Test()
+    Public Sub Base(ByVal myArray As String())
+        Method(New String() {"s1", "s2"}) ' Noncompliant: unnecessary
+        Method("s1")                'Compliant
+        Method("s1", "s2")          'Compliant
+        Method(myArray)             'Compliant
+        Method(New String(12) {})   'Compliant
 
+        Method2(1, New String() {"s1", "s2"}) ' Noncompliant: unnecessary
+        Method2(1, "s1")                'Compliant
+        Method2(1, "s1", "s2")          'Compliant
+        Method2(1, myArray)             'Compliant
+        Method2(1, New String(11) {})   'Compliant
+    End Sub
+
+    Public Sub Method(ParamArray args As String())
+    End Sub
+
+    Public Sub Method2(ByVal a As Integer, ParamArray args As String())
     End Sub
 
 End Class
