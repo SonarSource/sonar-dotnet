044fd0da596fa99e0cbd7a76c406a56853902eee
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-01-24 14:21:17 +0100

    Add test case

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.cs
@@ -24,7 +24,9 @@ public class Program
         Method3(new string[] { "s1", "s2" }, new string[12]); // Compliant
         Method3(new string[] { "s1", "s2" }, new string[] { "s1", "s2" }); // Noncompliant
 //                                           ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-        Method3(null, null); // Compliant
+        Method3(null, null);        // Compliant
+        Method4(new [] { "s1" });   // Compliant
+        Method4(new [] { "s1", "s2" }); // Compliant
     }
 
     public void Method(params string[] args) { }
@@ -33,5 +35,7 @@ public class Program
 
     public void Method3(string[] a, params string[] args) { }
 
-    public void Method4(params string[] a, params string[] args) { } // Error [CS0231]
+    public void Method4(object[] a, params object[] args) { }
+
+    public void Method5(params string[] a, params string[] args) { } // Error [CS0231]
 }
