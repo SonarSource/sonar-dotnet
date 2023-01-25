bad5707a6210f6c84378f4cfe52da09c565db844
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-01-25 17:39:14 +0100

    Move a file from Rules/ to Helpers/

--- analyzers/src/SonarAnalyzer.CSharp/Helpers/ParameterValidationInMethodWalker.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Helpers/ParameterValidationInMethodWalker.cs
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.Rules.CSharp
+namespace SonarAnalyzer.Helpers
 {
     internal class ParameterValidationInMethodWalker : SafeCSharpSyntaxWalker
     {
