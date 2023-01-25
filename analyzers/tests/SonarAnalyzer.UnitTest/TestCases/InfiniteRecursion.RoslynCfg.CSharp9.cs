c62cc0c4f4178cb946af51b4b2ea3d1226b2141c
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2023-01-25 10:50:35 +0100

    Fix FN S2190: No issues raised if recursion is inside an init accessor of a property (#6647)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/InfiniteRecursion.RoslynCfg.CSharp9.cs
@@ -0,0 +1,19 @@
+﻿// https://github.com/SonarSource/sonar-dotnet/issues/6646
+namespace Repro_6646
+{
+    public class Repro
+    {
+        public string Name
+        {
+            init // Noncompliant
+            {
+                Name = value;
+            }
+        }
+
+        public string Arrow
+        {
+            init => Arrow = value;   // Noncompliant
+        }
+    }
+}
