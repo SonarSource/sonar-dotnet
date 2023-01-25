044dc9839fc0806426007fd60f9fc8766ec0d8ec
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-01-25 15:11:06 +0100

    Add a lot of testcases for CommentsShouldNotBeEmpty (C#)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CommentsShouldNotBeEmpty.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CommentsShouldNotBeEmpty.cs
@@ -1,38 +1,48 @@
 ﻿using System;
 
 [Obsolete] // Ipsem Lorum
-public class SingleLineComment //
+public class SingleLine //
 // Noncompliant@-1 (inline comment)
 {
-        //
-//               
-// Noncompliant@-2 (a lot of whitespace before)
-// Noncompliant@-2 (a lot of whitespace after)
+             //
+    // Noncompliant@-1 (a lot of whitespace before)
 
-// *
-// Ipsem Lorum
-// \n
-//
-// Noncompliant @-1
+    //               
+    // Noncompliant@-1 (a lot of whitespace after)
 
-//
+    // *
+    // Ipsem Lorum
+    //
+    // Noncompliant @-1
 
-// Noncompliant @-2 
+    //
 
-// /**/
+    // Noncompliant @-2 
 
-// ///
+    // \r
 
-// //
+    // \n
 
-// /** */
+    // \r\n
+
+    // \t
+
+    // z̶̤͚̅̍a̷͈̤̪͌͛̈ļ̷̈͐͝g̸̰̈́͂̆o̴̓̏͜
+
+    // /**/
+
+    // ///
+
+    // //
+
+    // /** */
 }
 
 [Obsolete] /// Ipsem Lorum
-public class SingleLineDocumentationComment ///
+public class SingleLineDocumentation ///
 // Noncompliant@-1 (inline comment)
 {
-    ///
+            ///
     // Noncompliant@-1 (a lot of whitespace before)
 
     ///               
@@ -72,4 +82,200 @@ public class SingleLineDocumentationComment ///
     ///
     ///
     // Noncompliant @-3
+}
+
+[Obsolete] /* Ipsem Lorum */
+[Serializable] /**/
+// Noncompliant @-1 (inline comment)
+public class MultiLine /* */
+// Noncompliant @-1 (inline comment)
+{
+            /* */
+    // Noncompliant@-1 (a lot of whitespace before)
+
+    /*      */
+    // Noncompliant@-1 (a lot of whitespace inside)
+
+    /**/
+    // Noncompliant@-1 (a lot of whitespace after)
+
+    // Noncompliant@+1
+    /*
+     * 
+     */
+
+    // Noncompliant@+1
+    /*
+     * 
+     */
+
+    // Noncompliant@+1
+    /*
+     *
+     *
+     *
+     */
+
+
+    // Noncompliant@+1
+    /*
+
+     */
+
+    // Noncompliant@+1
+    /*
+
+
+
+     */
+
+    /* hey
+     */
+
+    /*
+     there */
+
+    /* hey
+     * 
+     */
+
+    /*
+     * 
+     there */
+
+    /*
+
+    Ipsem Lorum
+    \n
+    */
+
+    /*
+    hey there
+    */
+
+    /*
+     //
+     */
+
+    /*
+     ///
+    */
+
+    /*
+    \r
+    */
+
+    /*
+    \n
+    */
+
+    /*
+    \r\n
+    */
+
+    /*
+    \t
+    */
+
+    /*
+    z̶̤͚̅̍a̷͈̤̪͌͛̈ļ̷̈͐͝g̸̰̈́͂̆o̴̓̏͜
+    */
+}
+
+[Obsolete] /** Ipsem Lorum */
+[Serializable] /***/
+// Noncompliant @-1 (inline comment)
+public class MultiLineDocumentation /** */
+// Noncompliant @-1 (inline comment)
+{
+            /** */
+    // Noncompliant@-1 (a lot of whitespace before)
+
+    /**      */
+    // Noncompliant@-1 (a lot of whitespace inside)
+
+    /***/
+    // Noncompliant@-1 (a lot of whitespace after)
+
+    // Noncompliant@+1
+    /**
+     * 
+     */
+
+    // Noncompliant@+1
+    /**
+     * 
+     */
+
+    // Noncompliant@+1
+    /**
+     *
+     *
+     *
+     */
+
+
+    // Noncompliant@+1
+    /**
+
+     */
+
+    // Noncompliant@+1
+    /**
+
+
+
+     */
+
+    /** hey
+     */
+
+    /**
+     there */
+
+    /** hey
+     * 
+     */
+
+    /**
+     * 
+     there */
+
+    /**
+
+    Ipsem Lorum
+    \n
+    */
+
+    /**
+    hey there
+    */
+
+    /**
+     //
+     */
+
+    /**
+     ///
+    */
+
+    /**
+    \r
+    */
+
+    /**
+    \n
+    */
+
+    /**
+    \r\n
+    */
+
+    /**
+    \t
+    */
+
+    /**
+    z̶̤͚̅̍a̷͈̤̪͌͛̈ļ̷̈͐͝g̸̰̈́͂̆o̴̓̏͜
+    */
 }
