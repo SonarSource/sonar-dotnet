ed808cf2301e8ffe4e7ee6adca23c2fff9c5fd4f
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-01-30 15:55:36 +0100

    Code review

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ImplementSerializationMethodsCorrectly.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ImplementSerializationMethodsCorrectly.CSharp8.cs
@@ -4,44 +4,44 @@ using System.Runtime.Serialization;
 public interface IWithValidSerializationMethods
 {
     [OnSerializing]
-    protected abstract void OnSerializingMethod(StreamingContext context); // Compliant
+    protected abstract void OnSerializingMethod(StreamingContext context);
 
     [OnSerialized]
-    protected abstract void OnSerializedMethod(StreamingContext context); // Compliant
+    protected abstract void OnSerializedMethod(StreamingContext context);
 
     [OnDeserializing]
-    protected void OnDeserializingMethod(StreamingContext context) { } // Compliant
+    protected void OnDeserializingMethod(StreamingContext context) { }
 
     [OnDeserialized]
-    protected virtual void OnDeserializedMethod(StreamingContext context) { } // Compliant
+    protected virtual void OnDeserializedMethod(StreamingContext context) { }
 }
 
 public interface IWithInvalidSerializationMethodsModifiers
 {
     [OnSerializing]
-    public abstract void OnSerializingMethod(StreamingContext context); // Compliant
+    public abstract void OnSerializingMethod(StreamingContext context); // Compliant, despite "public", it is in interface
 
     [OnSerialized]
-    public abstract void OnSerializedMethod(StreamingContext context); // Compliant
+    public abstract void OnSerializedMethod(StreamingContext context);
 
     [OnDeserializing]
-    public void OnDeserializingMethod(StreamingContext context) { } // Compliant
+    public void OnDeserializingMethod(StreamingContext context) { }
 
     [OnDeserialized]
-    public virtual void OnDeserializedMethod(StreamingContext context) { } // Compliant
+    public virtual void OnDeserializedMethod(StreamingContext context) { }
 }
 
 public interface IWithInvalidSerializationMethodsParams
 {
     [OnSerializing]
-    protected abstract void OnSerializingMethod(StreamingContext context, object otherPar); // Compliant
+    protected abstract void OnSerializingMethod(StreamingContext context, object otherPar); // Compliant, despite wrong params, it is in interface
 
     [OnSerialized]
-    protected abstract void OnSerializedMethod(object otherPar); // Compliant
+    protected abstract void OnSerializedMethod(object otherPar);
 
     [OnDeserializing]
-    protected void OnDeserializingMethod(object otherPar, StreamingContext context) { } // Compliant
+    protected void OnDeserializingMethod(object otherPar, StreamingContext context) { }
 
     [OnDeserialized]
-    protected virtual void OnDeserializedMethod() { } // Compliant
+    protected virtual void OnDeserializedMethod() { }
 }
