495e953ff8466c3f1400f82b5dfb74542b729cf1
Corniel Nobel <mail@corniel.nl>
2023-02-03 14:01:02 +0100

    Update message.

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RegularExpressions/RegexMustHaveValidSyntax.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RegularExpressions/RegexMustHaveValidSyntax.CSharp9.cs
@@ -13,6 +13,6 @@ class Noncompliant
     void ImplicitObject()
     {
         Regex patternOnly = new("[A"); // Noncompliant {{Fix the syntax error inside this regex: Invalid pattern '[A' at offset 2. Unterminated [] set.}}
-        Regex differentIssue = new("A???"); // Noncompliant {{Fix the syntax error inside this regex: Invalid pattern '[A' at offset 2. Unterminated [] set.}}
+        Regex differentIssue = new("A???"); // Noncompliant {{Fix the syntax error inside this regex: Invalid pattern 'A???' at offset 4. Nested quantifier '?'.}}
     }
 }
