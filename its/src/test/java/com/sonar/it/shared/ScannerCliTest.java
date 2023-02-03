35e4c8ed13f10fea190872e76c68e5aaaf6d6660
Andrei Epure <andrei.epure@sonarsource.com>
2023-02-03 13:33:11 +0100

    Revert "Relax Java ITs WARN log checks because of SQ regression (#6715)"
    This reverts commit ae8b6565720e246cc415b849248efadb74ebd982.

--- its/src/test/java/com/sonar/it/shared/ScannerCliTest.java
+++ its/src/test/java/com/sonar/it/shared/ScannerCliTest.java
@@ -49,8 +49,7 @@ public class ScannerCliTest {
     BuildResult result = ORCHESTRATOR.executeBuild(scanner);
 
     assertThat(result.getLogsLines(l -> l.contains("WARN")))
-      // ToDo revert back to containsExactlyInAnyOrder https://github.com/SonarSource/sonar-dotnet/issues/6714
-      .contains(
+      .containsExactlyInAnyOrder(
         "WARN: Your project contains C# files which cannot be analyzed with the scanner you are using. To analyze C# or VB.NET, you must use the SonarScanner for .NET 5.x or higher, see https://redirect.sonarsource.com/doc/install-configure-scanner-msbuild.html",
         "WARN: Incremental PR analysis: Could not determine common base path, cache will not be computed. Consider setting 'sonar.projectBaseDir' property.",
         "WARN: Your project contains VB.NET files which cannot be analyzed with the scanner you are using. To analyze C# or VB.NET, you must use the SonarScanner for .NET 5.x or higher, see https://redirect.sonarsource.com/doc/install-configure-scanner-msbuild.html",
@@ -68,8 +67,7 @@ public class ScannerCliTest {
     BuildResult result = ORCHESTRATOR.executeBuild(scanner);
 
     assertThat(result.getLogsLines(l -> l.contains("WARN")))
-      // ToDo revert back to containsExactlyInAnyOrder https://github.com/SonarSource/sonar-dotnet/issues/6714
-      .contains(
+      .containsExactlyInAnyOrder(
         "WARN: Your project contains C# files which cannot be analyzed with the scanner you are using. To analyze C# or VB.NET, you must use the SonarScanner for .NET 5.x or higher, see https://redirect.sonarsource.com/doc/install-configure-scanner-msbuild.html",
         "WARN: Incremental PR analysis: Could not determine common base path, cache will not be computed. Consider setting 'sonar.projectBaseDir' property."
       );
@@ -86,8 +84,7 @@ public class ScannerCliTest {
     BuildResult result = ORCHESTRATOR.executeBuild(scanner);
 
     assertThat(result.getLogsLines(l -> l.contains("WARN")))
-      // ToDo revert back to containsExactlyInAnyOrder https://github.com/SonarSource/sonar-dotnet/issues/6714
-      .contains(
+      .containsExactlyInAnyOrder(
         "WARN: Your project contains C# files which cannot be analyzed with the scanner you are using. To analyze C# or VB.NET, you must use the SonarScanner for .NET 5.x or higher, see https://redirect.sonarsource.com/doc/install-configure-scanner-msbuild.html",
         "WARN: Incremental PR analysis: Could not determine common base path, cache will not be computed. Consider setting 'sonar.projectBaseDir' property."
       );
@@ -102,8 +99,7 @@ public class ScannerCliTest {
       .setProperty("sonar.cs.file.suffixes=", ".no_extension");
     BuildResult result = ORCHESTRATOR.executeBuild(scanner);
 
-    // https://github.com/SonarSource/sonar-dotnet/issues/6714
-    //assertThat(result.getLogsLines(l -> l.contains("WARN"))).isEmpty();
+    assertThat(result.getLogsLines(l -> l.contains("WARN"))).isEmpty();
     TestUtils.verifyNoGuiWarnings(ORCHESTRATOR, result);
   }
 
