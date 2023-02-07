99f3b2778cb4a6fd807b1a7e499bc9ed44242440
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-02-07 11:38:22 +0100

    Apply PR review changes

--- analyzers/src/SonarAnalyzer.CSharp/Rules/SillyMathematicalComparison.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/SillyMathematicalComparison.cs
@@ -18,15 +18,13 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarAnalyzer.Helpers;
-
 namespace SonarAnalyzer.Rules.CSharp
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public sealed class SillyMathematicalComparison : SonarDiagnosticAnalyzer
     {
         private const string DiagnosticId = "S2198";
-        private const string MessageFormat = "Comparison to integral constant is useless; the constant is outside the range of type '{0}'";
+        private const string MessageFormat = "Comparison to this constant is useless; the constant is outside the range of type '{0}'";
 
         private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
 
@@ -38,32 +36,35 @@ namespace SonarAnalyzer.Rules.CSharp
                 SyntaxKind.GreaterThanExpression,
                 SyntaxKind.GreaterThanOrEqualExpression,
                 SyntaxKind.LessThanExpression,
-                SyntaxKind.LessThanOrEqualExpression);
+                SyntaxKind.LessThanOrEqualExpression,
+                SyntaxKind.EqualsExpression,
+                SyntaxKind.NotEqualsExpression);
 
         private static void CheckBinary(SonarSyntaxNodeReportingContext context)
         {
             var binary = (BinaryExpressionSyntax)context.Node;
-            var typeIdentifier = string.Empty;
-            if (ShouldRaise(context.SemanticModel, binary.Left, binary.Right, ref typeIdentifier))
-            {
-                context.ReportIssue(Diagnostic.Create(Rule, binary.GetLocation(), typeIdentifier));
-            }
+            CheckOneSide(context, binary.Left, binary.Right);
+            CheckOneSide(context, binary.Right, binary.Left);
+        }
 
-            if (ShouldRaise(context.SemanticModel, binary.Right, binary.Left, ref typeIdentifier))
+        private static void CheckOneSide(SonarSyntaxNodeReportingContext context, SyntaxNode first, SyntaxNode second)
+        {
+            if (ShouldRaise(context.SemanticModel, first, second))
             {
-                context.ReportIssue(Diagnostic.Create(Rule, binary.GetLocation(), typeIdentifier));
+                var typeName = context.SemanticModel.GetSymbolInfo(second).Symbol.ToMinimalDisplayString(context.SemanticModel, second.GetLocation().SourceSpan.Start);
+                context.ReportIssue(Diagnostic.Create(Rule, first.Parent.GetLocation(), typeName));
             }
         }
 
-        private static bool ShouldRaise(SemanticModel model, SyntaxNode expectedConstant, SyntaxNode other, ref string typeIdentifier) =>
+        private static bool ShouldRaise(SemanticModel model, SyntaxNode expectedConstant, SyntaxNode other) =>
             TryGetConstant(model, expectedConstant, out var constant)
-            && model.GetSymbolInfo(other).Symbol.GetSymbolType() is { } symbol
-            && TryGetRange(symbol, out typeIdentifier, out var min, out var max)
+            && model.GetTypeInfo(other).Type is { } symbol
+            && TryGetRange(symbol, out var min, out var max)
             && (constant < min || constant > max);
 
         private static bool TryGetConstant(SemanticModel model, SyntaxNode expectedConstant, out double constant)
         {
-            if (FindConstant(model, expectedConstant, Convert.ToDouble) is { } constValue)
+            if (FindConstant(model, expectedConstant) is { } constValue)
             {
                 constant = constValue;
                 return true;
@@ -73,36 +74,24 @@ namespace SonarAnalyzer.Rules.CSharp
             return false;
         }
 
-        private static T? FindConstant<T>(SemanticModel semanticModel, SyntaxNode node, Func<object, T> converter) where T : struct
-            => semanticModel.GetSymbolInfo(node).Symbol is var symbol
-                && !IsFieldOrPropertyOutsideSystemNamespace(symbol)
-                && !IsEnum(symbol)
-                && CSharpFacade.Instance.FindConstantValue(semanticModel, node) is { } value
-                && ConversionHelper.TryConvertWith(value, converter, out T typedValue)
+        private static double? FindConstant(SemanticModel semanticModel, SyntaxNode node) =>
+            node.FindConstantValue(semanticModel) is { } value
+            && ConversionHelper.TryConvertWith(value, Convert.ToDouble, out var typedValue)
                 ? typedValue
                 : null;
 
-        private static bool TryGetRange(ITypeSymbol typeSymbol, out string typeIdentifier, out double min, out double max)
+        private static bool TryGetRange(ITypeSymbol typeSymbol, out double min, out double max)
         {
             if (typeSymbol.Is(KnownType.System_Single))
             {
-                typeIdentifier = "float";
                 min = float.MinValue;
                 max = float.MaxValue;
                 return true;
             }
 
-            typeIdentifier = default;
             min = default;
             max = default;
             return false;
         }
-
-        private static bool IsFieldOrPropertyOutsideSystemNamespace(ISymbol symbol) =>
-            symbol is { Kind: SymbolKind.Field or SymbolKind.Property }
-            && symbol.ContainingNamespace.Name != nameof(System);
-
-        private static bool IsEnum(ISymbol symbol) =>
-            symbol.GetSymbolType() is INamedTypeSymbol { EnumUnderlyingType: { } };
     }
 }
