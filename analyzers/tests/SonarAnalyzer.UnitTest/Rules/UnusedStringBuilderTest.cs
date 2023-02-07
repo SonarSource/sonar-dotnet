c207c32036d4b8e6b39cfdbf00de8bde99d392f2
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-02-07 15:25:52 +0100

    Fix UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/UnusedStringBuilderTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/UnusedStringBuilderTest.cs
@@ -38,12 +38,18 @@ public class UnusedStringBuilderTest
     [DataRow("sb.ToString();", true)]
     [DataRow("""var a = sb.Append("").Append("").Append("").Append("").ToString().ToLower();""", true)]
     [DataRow("sb.CopyTo(0, new char[1], 0, 1);", true)]
-    [DataRow("sb.GetChunks();", true)]
     [DataRow("var a = sb[0];", true)]
     [DataRow("""sb?.Append("").ToString().ToLower();""", true)]
     [DataRow("""@sb.Append("").ToString();""", true)]
     [DataRow("sb.Remove(sb.Length - 1, 1);", true)]
     [DataRow("""var a = $"{sb} is ToStringed here";""", true)]
+
+#if NET
+
+    [DataRow("sb.GetChunks();", true)]
+
+#endif
+
     public void UnusedStringBuilder_CSExpressionsTest(string expression, bool compliant)
     {
         var code = $$"""
@@ -114,13 +120,19 @@ public class UnusedStringBuilderTest
     [DataRow("sb.ToString()", true)]
     [DataRow("""Dim a = sb.Append("").Append("").Append("").Append("").ToString().ToLower()""", true)]
     [DataRow("sb.CopyTo(0, New Char(0) {}, 0, 1)", true)]
-    [DataRow("sb.GetChunks()", true)]
     [DataRow("Dim a = sb(0)", true)]
     [DataRow("""sb?.Append("").ToString().ToLower()""", true)]
     [DataRow("""sb.Append("").ToString()""", true)]
     [DataRow("sb.Remove(sb.Length - 1, 1)", true)]
     [DataRow("""Dim a = $"{sb} is ToStringed here" """, true)]
     [DataRow("""SB.ToString()""", true)]
+
+#if NET
+
+    [DataRow("sb.GetChunks()", true)]
+
+#endif
+
     public void UnusedStringBuilder_VBExpressionsTest(string expression, bool compliant)
     {
         var code = $$"""
