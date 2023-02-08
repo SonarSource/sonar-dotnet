07bfc74eb6b94de24f5a9e78947c8e291ab244ff
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-02-08 14:17:47 +0100

    Fix license 2

--- analyzers/src/SonarAnalyzer.Common/Common/RegexConstants.cs
+++ analyzers/src/SonarAnalyzer.Common/Common/RegexConstants.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * SonarAnalyzer for .NET
- * Copyright (C) 2016-2023 SonarSource SA
- * mailto: info AT sonarsource DOT com
+ * Copyright (C) 2015-2023 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
