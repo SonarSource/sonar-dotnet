3a755e49d33e9a71c8c66df508162ad00355125d
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-02-08 17:17:46 +0100

    Remove empty line

--- analyzers/src/SonarAnalyzer.Common/Rules/ClassNamedExceptionBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/ClassNamedExceptionBase.cs
@@ -43,5 +43,4 @@ public abstract class ClassNamedExceptionBase<TSyntaxKind> : SonarDiagnosticAnal
                 }
             },
             Language.SyntaxKind.ClassAndModuleDeclarations);
-
 }
