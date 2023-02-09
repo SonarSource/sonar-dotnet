0980aa48250d2c05286b81fe558c7881d60a38a1
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-02-09 15:55:47 +0100

    Add an is-pattern edgecase in the testcases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyMathematicalComparison.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SillyMathematicalComparison.CSharp9.cs
@@ -16,6 +16,10 @@ namespace SonarAnalyzer.UnitTest.TestCases
             _ = 33 is > 55; // Compliant
             _ = 33 is >= 55; // Compliant
             _ = 33 is not 55; // Compliant
+
+            const double d = double.MaxValue;
+            float x = 42;
+            _ = x is d; // Error [CS0266] - cannot implicitly convert type 'double' to 'float'
         }
     }
 }
