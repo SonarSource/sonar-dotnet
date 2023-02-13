fa0d931fff94ecd41f881413f3692f40d05c236a
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2023-02-13 14:03:14 +0100

    Clean up some code

--- analyzers/src/SonarAnalyzer.CSharp/Rules/RedundantCast.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/RedundantCast.cs
@@ -18,161 +18,155 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.Rules.CSharp
+namespace SonarAnalyzer.Rules.CSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class RedundantCast : SonarDiagnosticAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class RedundantCast : SonarDiagnosticAnalyzer
+    internal const string DiagnosticId = "S1905";
+    private const string MessageFormat = "Remove this unnecessary cast to '{0}'.";
+
+    private static readonly DiagnosticDescriptor Rule =
+        DescriptorFactory.Create(DiagnosticId, MessageFormat);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+    private static readonly ISet<string> CastIEnumerableMethods = new HashSet<string> { "Cast", "OfType" };
+
+    protected override void Initialize(SonarAnalysisContext context)
     {
-        internal const string DiagnosticId = "S1905";
-        private const string MessageFormat = "Remove this unnecessary cast to '{0}'.";
+        context.RegisterNodeAction(
+            c =>
+            {
+                var castExpression = (CastExpressionSyntax)c.Node;
+                CheckCastExpression(c, castExpression.Expression, castExpression.Type, castExpression.Type.GetLocation());
+            },
+            SyntaxKind.CastExpression);
 
-        private static readonly DiagnosticDescriptor rule =
-            DescriptorFactory.Create(DiagnosticId, MessageFormat);
+        context.RegisterNodeAction(
+            c =>
+            {
+                var castExpression = (BinaryExpressionSyntax)c.Node;
+                CheckCastExpression(c, castExpression.Left, castExpression.Right,
+                    castExpression.OperatorToken.CreateLocation(castExpression.Right));
+            },
+            SyntaxKind.AsExpression);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(rule);
+        context.RegisterNodeAction(
+            CheckExtensionMethodInvocation,
+            SyntaxKind.InvocationExpression);
+    }
 
-        private static readonly ISet<string> CastIEnumerableMethods = new HashSet<string> { "Cast", "OfType" };
-
-        protected override void Initialize(SonarAnalysisContext context)
+    private static void CheckCastExpression(SonarSyntaxNodeReportingContext context, ExpressionSyntax expression, ExpressionSyntax type, Location location)
+    {
+        if (expression.IsKind(SyntaxKindEx.DefaultLiteralExpression))
         {
-            context.RegisterNodeAction(
-                c =>
-                {
-                    var castExpression = (CastExpressionSyntax)c.Node;
-                    CheckCastExpression(c, castExpression.Expression, castExpression.Type, castExpression.Type.GetLocation());
-                },
-                SyntaxKind.CastExpression);
-
-            context.RegisterNodeAction(
-                c =>
-                {
-                    var castExpression = (BinaryExpressionSyntax)c.Node;
-                    CheckCastExpression(c, castExpression.Left, castExpression.Right,
-                        castExpression.OperatorToken.CreateLocation(castExpression.Right));
-                },
-                SyntaxKind.AsExpression);
-
-            context.RegisterNodeAction(
-                CheckExtensionMethodInvocation,
-                SyntaxKind.InvocationExpression);
+            return;
         }
 
-        private static void CheckCastExpression(SonarSyntaxNodeReportingContext context, ExpressionSyntax expression, ExpressionSyntax type, Location location)
+        var expressionType = context.SemanticModel.GetTypeInfo(expression).Type;
+        if (expressionType == null)
         {
-            if (expression.IsKind(SyntaxKindEx.DefaultLiteralExpression))
-            {
-                return;
-            }
-
-            var expressionType = context.SemanticModel.GetTypeInfo(expression).Type;
-            if (expressionType == null)
-            {
-                return;
-            }
-
-            var castType = context.SemanticModel.GetTypeInfo(type).Type;
-            if (castType == null)
-            {
-                return;
-            }
-
-            if (expressionType.Equals(castType))
-            {
-                context.ReportIssue(Diagnostic.Create(rule, location,
-                    castType.ToMinimalDisplayString(context.SemanticModel, expression.SpanStart)));
-            }
+            return;
         }
 
-        private static void CheckExtensionMethodInvocation(SonarSyntaxNodeReportingContext context)
+        var castType = context.SemanticModel.GetTypeInfo(type).Type;
+        if (castType == null)
         {
-            var invocation = (InvocationExpressionSyntax)context.Node;
-            if (GetEnumerableExtensionSymbol(invocation, context.SemanticModel) is { } methodSymbol)
-            {
-                var returnType = methodSymbol.ReturnType;
-                if (GetGenericTypeArgument(returnType) is { } castType)
-                {
-                    if (methodSymbol.Name == "OfType" && CanHaveNullValue(castType))
-                    {
-                        // OfType() filters 'null' values from enumerables
-                        return;
-                    }
+            return;
+        }
 
-                    var elementType = GetElementType(invocation, methodSymbol, context.SemanticModel);
-                    // Generic types {T} and {T?} are equal and there is no way to access NullableAnnotation field right now
-                    // See https://github.com/SonarSource/sonar-dotnet/issues/3273
-                    if (elementType != null && elementType.Equals(castType) && string.Equals(elementType.ToString(), castType.ToString(), System.StringComparison.Ordinal))
-                    {
-                        var methodCalledAsStatic = methodSymbol.MethodKind == MethodKind.Ordinary;
-                        context.ReportIssue(Diagnostic.Create(rule, GetReportLocation(invocation, methodCalledAsStatic),
-                            returnType.ToMinimalDisplayString(context.SemanticModel, invocation.SpanStart)));
-                    }
+        if (expressionType.Equals(castType))
+        {
+            ReportIssue(context, expression, location, castType);
+        }
+    }
+
+    private static void CheckExtensionMethodInvocation(SonarSyntaxNodeReportingContext context)
+    {
+        var invocation = (InvocationExpressionSyntax)context.Node;
+        if (GetEnumerableExtensionSymbol(invocation, context.SemanticModel) is { } methodSymbol)
+        {
+            var returnType = methodSymbol.ReturnType;
+            if (GetGenericTypeArgument(returnType) is { } castType)
+            {
+                if (methodSymbol.Name == "OfType" && CanHaveNullValue(castType))
+                {
+                    // OfType() filters 'null' values from enumerables
+                    return;
+                }
+
+                var elementType = GetElementType(invocation, methodSymbol, context.SemanticModel);
+                // Generic types {T} and {T?} are equal and there is no way to access NullableAnnotation field right now
+                // See https://github.com/SonarSource/sonar-dotnet/issues/3273
+                if (elementType != null && elementType.Equals(castType) && string.Equals(elementType.ToString(), castType.ToString(), StringComparison.Ordinal))
+                {
+                    var methodCalledAsStatic = methodSymbol.MethodKind == MethodKind.Ordinary;
+                    ReportIssue(context, invocation, GetReportLocation(invocation, methodCalledAsStatic), returnType);
                 }
             }
         }
+    }
 
-        /// If the invocation one of the <see cref="CastIEnumerableMethods"/> extensions, returns the method symbol.
-        private static IMethodSymbol GetEnumerableExtensionSymbol(InvocationExpressionSyntax invocation, SemanticModel semanticModel) =>
-            invocation.GetMethodCallIdentifier() is { } methodName
-            && CastIEnumerableMethods.Contains(methodName.ValueText)
-            && semanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol
-            && methodSymbol.IsExtensionOn(KnownType.System_Collections_IEnumerable)
-                ? methodSymbol
-                : null;
+    private static void ReportIssue(SonarSyntaxNodeReportingContext context,
+                                    ExpressionSyntax expression,
+                                    Location location,
+                                    ITypeSymbol castType) =>
+        context.ReportIssue(Diagnostic.Create(Rule, location, castType.ToMinimalDisplayString(context.SemanticModel, expression.SpanStart)));
 
-        private static ITypeSymbol GetGenericTypeArgument(ITypeSymbol type) =>
-            type is INamedTypeSymbol returnType && returnType.Is(KnownType.System_Collections_Generic_IEnumerable_T)
-                ? returnType.TypeArguments.Single()
-                : null;
+    /// If the invocation one of the <see cref="CastIEnumerableMethods"/> extensions, returns the method symbol.
+    private static IMethodSymbol GetEnumerableExtensionSymbol(InvocationExpressionSyntax invocation, SemanticModel semanticModel) =>
+        invocation.GetMethodCallIdentifier() is { } methodName
+        && CastIEnumerableMethods.Contains(methodName.ValueText)
+        && semanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol
+        && methodSymbol.IsExtensionOn(KnownType.System_Collections_IEnumerable)
+            ? methodSymbol
+            : null;
 
-        private static bool CanHaveNullValue(ITypeSymbol type) => type.IsReferenceType || type.Name == "Nullable";
+    private static ITypeSymbol GetGenericTypeArgument(ITypeSymbol type) =>
+        type is INamedTypeSymbol returnType && returnType.Is(KnownType.System_Collections_Generic_IEnumerable_T)
+            ? returnType.TypeArguments.Single()
+            : null;
 
-        private static Location GetReportLocation(InvocationExpressionSyntax invocation, bool methodCalledAsStatic)
+    private static bool CanHaveNullValue(ITypeSymbol type) => type.IsReferenceType || type.Name == "Nullable";
+
+    private static Location GetReportLocation(InvocationExpressionSyntax invocation, bool methodCalledAsStatic) =>
+        invocation.Expression switch
         {
-            if (!(invocation.Expression is MemberAccessExpressionSyntax memberAccess))
-            {
-                return invocation.Expression.GetLocation();
-            }
-
-            return methodCalledAsStatic
+            MemberAccessExpressionSyntax memberAccess => methodCalledAsStatic
                 ? memberAccess.GetLocation()
-                : memberAccess.OperatorToken.CreateLocation(invocation);
-        }
+                : memberAccess.OperatorToken.CreateLocation(invocation),
+            _ => invocation.Expression.GetLocation()
+        };
 
-        private static ITypeSymbol GetElementType(InvocationExpressionSyntax invocation, IMethodSymbol methodSymbol,
-            SemanticModel semanticModel)
+    private static ITypeSymbol GetElementType(InvocationExpressionSyntax invocation,
+                                              IMethodSymbol methodSymbol,
+                                              SemanticModel semanticModel)
+    {
+        ExpressionSyntax collection;
+        if (methodSymbol.MethodKind == MethodKind.Ordinary)
         {
-            ExpressionSyntax collection;
-            if (methodSymbol.MethodKind == MethodKind.Ordinary)
+            var firstArgument = invocation.ArgumentList.Arguments.FirstOrDefault();
+            if (firstArgument == default)
             {
-                if (!invocation.ArgumentList.Arguments.Any())
-                {
-                    return null;
-                }
-                collection = invocation.ArgumentList.Arguments.First().Expression;
+                return null;
             }
-            else
-            {
-                if (!(invocation.Expression is MemberAccessExpressionSyntax memberAccess))
-                {
-                    return null;
-                }
-                collection = memberAccess.Expression;
-            }
-
-            var typeInfo = semanticModel.GetTypeInfo(collection);
-            if (typeInfo.Type is INamedTypeSymbol collectionType &&
-                collectionType.TypeArguments.Length == 1)
-            {
-                return collectionType.TypeArguments.First();
-            }
-
-            if (typeInfo.Type is IArrayTypeSymbol arrayType &&
-                arrayType.Rank == 1) // casting is necessary for multidimensional arrays
-            {
-                return arrayType.ElementType;
-            }
-
-            return null;
+            collection = firstArgument.Expression;
         }
+        else
+        {
+            if (invocation.Expression is not MemberAccessExpressionSyntax memberAccess)
+            {
+                return null;
+            }
+            collection = memberAccess.Expression;
+        }
+
+        return semanticModel.GetTypeInfo(collection).Type switch
+        {
+            INamedTypeSymbol { TypeArguments: { Length: 1 } typeArguments } => typeArguments.First(),
+            IArrayTypeSymbol { Rank: 1 } arrayType => arrayType.ElementType,    // casting is necessary for multidimensional arrays
+            _ => null
+        };
     }
 }
