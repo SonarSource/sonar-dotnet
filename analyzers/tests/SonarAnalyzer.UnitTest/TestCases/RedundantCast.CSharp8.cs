56a5ca250ff3bc6021c4bcbaa86a06e713bd1376
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2023-02-13 13:40:44 +0100

    Add FP test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantCast.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantCast.CSharp8.cs
@@ -12,5 +12,19 @@ namespace Tests.Diagnostics
             var nullableStrings = new string?[] { "one", "two", null, "three" };
             return nullableStrings.OfType<string>(); // Compliant - filters out the null
         }
-     }
+    }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/6438
+    public class ArrayOfAnonTypes
+    {
+        public void UsefulCast()
+        {
+            var anonArray = new[] { new { X = (string?)"foo" }, new { X = (string?)null } }; // Compliant
+            var anon = true switch
+            {
+                true => new { X = (string?)"foo" }, // Compliant
+                false => new { X = (string?)null }  // Compliant
+            };
+        }
+    }
 }
