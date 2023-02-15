d7498d4b659acc1228b4032f8f6736abcbb20bba
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-02-15 15:13:15 +0100

    More test cases for records

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassShouldNotBeEmpty.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ClassShouldNotBeEmpty.CSharp9.cs
@@ -1,16 +1,38 @@
-﻿
+﻿using System;
+
 record EmptyRecord1();                                              // Noncompliant {{Remove this empty record, or add members to it.}}
 //     ^^^^^^^^^^^^
 record EmptyRecord2() { };                                          // Noncompliant
 
-record NotEmptyRecord1(int RecordMember);
-record NotEmptyRecord2()
+record RecordWithParameters(int RecordMember);
+
+record RecordWithProperty
 {
-    int RecordMember => 42;
-};
+    int SomeProperty => 42;
+}
+record RecordWithField
+{
+    int SomeField = 42;
+}
+record RecordWithMethod
+{
+    void Method() { }
+}
+record RecordWithIndexer
+{
+    int this[int index] => 42;
+}
+record RecordWithDelegate
+{
+    delegate void MethodDelegate();
+}
+record RecordWithEvent
+{
+    event EventHandler CustomEvent;
+}
 
 record EmptyGenericRecord<T>();                                     // Noncompliant
 //     ^^^^^^^^^^^^^^^^^^
-public record EmptyGenericRecordWithContraint<T>() where T : class; // Noncompliant
+record EmptyGenericRecordWithContraint<T>() where T : class;        // Noncompliant
 record NotEmptyGenericRecord<T>(T RecordMember);
-public record NotEmptyGenericRecordWithContraint<T>(T RecordMember) where T : class;
+record NotEmptyGenericRecordWithContraint<T>(T RecordMember) where T : class;
