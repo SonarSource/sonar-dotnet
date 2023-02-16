6b193b59f13d43b615669d533695dfd616497959
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-02-16 15:48:01 +0100

    Move ShouldAnalyzeFile method outside ShouldAnalyzeTree

--- analyzers/src/SonarAnalyzer.Common/AnalysisContext/SonarAnalysisContextBase.cs
+++ analyzers/src/SonarAnalyzer.Common/AnalysisContext/SonarAnalysisContextBase.cs
@@ -67,8 +67,14 @@ public abstract class SonarAnalysisContextBase<TContext> : SonarAnalysisContextB
     /// <param name="generatedCodeRecognizer">When set, generated trees are analyzed only when language-specific 'analyzeGeneratedCode' configuration property is also set.</param>
     public bool ShouldAnalyzeTree(SyntaxTree tree, GeneratedCodeRecognizer generatedCodeRecognizer) =>
         (generatedCodeRecognizer is null || ShouldAnalyzeGenerated() || !tree.IsGenerated(generatedCodeRecognizer, Compilation))
-        && (tree is null || !IsUnchanged(tree))
-        && ShouldAnalyzeFile(Compilation, Options.ParseSonarLintXmlSettings(), Tree.FilePath);
+        && (tree is null || !IsUnchanged(tree));
+
+    /// <summary>
+    /// Check if the current file path is included or excuded and caches the result.
+    /// </summary>
+    public bool ShouldAnalyzeFile(Compilation compilation, XElement[] sonarLintSettings, string filePath) =>
+        fileCache.GetValue(compilation, x => new()) is { } cache
+        && cache.GetOrAdd(filePath, _ => ShouldAnalyzeFile(sonarLintSettings, filePath));
 
     /// <summary>
     /// Reads configuration from SonarProjectConfig.xml file and caches the result for scope of this analysis.
@@ -123,10 +129,6 @@ public abstract class SonarAnalysisContextBase<TContext> : SonarAnalysisContextB
         Options.SonarLintXml() is { } sonarLintXml
         && AnalysisContext.TryGetValue(sonarLintXml.GetText(), ShouldAnalyzeGeneratedProvider(Compilation.Language), out var shouldAnalyzeGenerated)
         && shouldAnalyzeGenerated;
-
-    private bool ShouldAnalyzeFile(Compilation compilation, XElement[] sonarLintSettings, string filePath) =>
-        fileCache.GetValue(compilation, x => new()) is { } cache
-        && cache.GetOrAdd(filePath, _ => ShouldAnalyzeFile(sonarLintSettings, filePath));
 
     private bool ShouldAnalyzeFile(XElement[] sonarLintSettings, string filePath) =>
         IsIncluded(PropertiesHelper.ReadSourceFileInclusionsProperty(sonarLintSettings), filePath)
