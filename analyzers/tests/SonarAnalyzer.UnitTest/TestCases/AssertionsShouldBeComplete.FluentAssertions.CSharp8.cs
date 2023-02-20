286b13b3a3e4210e5a092c9d56fef58460a6f59f
Martin Strecker <martin.strecker@sonarsource.com>
2023-02-20 17:46:11 +0100

    Demonstrate better overload resolution inC#8 vs C#7

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AssertionsShouldBeComplete.FluentAssertions.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AssertionsShouldBeComplete.FluentAssertions.CSharp8.cs
@@ -10,11 +10,18 @@ public class Program
     public void StringAssertions()
     {
         var s = "Test";
+        s.Should();              // Noncompliant (no "ambiguous calls" compiler error as in C# 7)
         s?.Should();             // Noncompliant
         s?[0].Should();          // Noncompliant
         s.Should().Be("Test");   // Compliant
         s.Should()?.Be("Test");  // Compliant
         s.Should()!?.Be("Test"); // Compliant
+    }
+
+    public void CollectionAssertions()
+    {
+        var collection = new[] { "Test", "Test" };
+        collection.Should(); // Noncompliant (no "ambiguous calls" compiler error as in C# 7)
     }
 
     public void DictAssertions()
