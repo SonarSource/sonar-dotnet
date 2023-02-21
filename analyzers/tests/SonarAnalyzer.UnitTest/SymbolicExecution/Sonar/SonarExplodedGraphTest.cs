15735f92388cb8b25bfb9ca7e4e1c8e7af4ad426
Martin Strecker <martin.strecker@sonarsource.com>
2023-02-21 14:23:04 +0100

    Fix failing test on Release

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Sonar/SonarExplodedGraphTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Sonar/SonarExplodedGraphTest.cs
@@ -942,11 +942,7 @@ namespace Namespace
                     }
                 };
             var walk = () => context.WalkWithInstructions(2);
-#if DEBUG
             walk.Should().Throw<Exception>().Which.Message.Should().Be("Expected NumberOfExitBlockReached to be 1, but found 0.");
-#else
-            walk();
-#endif
         }
 
         [TestMethod]
