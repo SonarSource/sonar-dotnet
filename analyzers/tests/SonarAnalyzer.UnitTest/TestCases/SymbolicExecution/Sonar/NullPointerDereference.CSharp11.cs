2c1dd92370089e9781a25636634fd57b4c706ae9
Martin Strecker <martin.strecker@sonarsource.com>
2023-02-23 16:50:21 +0100

    Add test in S2259 for parenthesized pattern

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Sonar/NullPointerDereference.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Sonar/NullPointerDereference.CSharp11.cs
@@ -19,4 +19,17 @@ namespace SonarAnalyzer.UnitTest.TestCases.SymbolicExecution.Sonar
             }
         }
     }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/6766
+    public class Repo_6766
+    {
+        public void ParenthesizedSwitchPattern(int i)
+        {
+            switch (i)
+            {
+                case (1 or 2):
+                    break;
+            }
+        }
+    }
 }
