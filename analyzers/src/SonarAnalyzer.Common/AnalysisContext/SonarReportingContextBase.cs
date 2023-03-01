da98109cbd12d9710cc8b53023bd808d9ee38a7b
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-03-01 18:28:38 +0100

    Fix missing check in reporting

--- analyzers/src/SonarAnalyzer.Common/AnalysisContext/SonarReportingContextBase.cs
+++ analyzers/src/SonarAnalyzer.Common/AnalysisContext/SonarReportingContextBase.cs
@@ -28,7 +28,7 @@ public abstract class SonarReportingContextBase<TContext> : SonarAnalysisContext
 
     protected void ReportIssueCore(Diagnostic diagnostic)
     {
-        if (HasMatchingScope(diagnostic.Descriptor))
+        if (HasMatchingScope(diagnostic.Descriptor) && SonarAnalysisContext.LegacyIsRegisteredActionEnabled(new[] { diagnostic.Descriptor }, diagnostic.Location?.SourceTree))
         {
             var reportingContext = CreateReportingContext(diagnostic);
             if (reportingContext is { Compilation: { } compilation, Diagnostic.Location: { Kind: LocationKind.SourceFile, SourceTree: { } tree } } && !compilation.ContainsSyntaxTree(tree))
