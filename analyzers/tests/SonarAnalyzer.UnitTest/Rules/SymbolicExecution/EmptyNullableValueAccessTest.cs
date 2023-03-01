b4337f5710d5c07bd3ef323a4618c1a89c0bef87
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-03-01 18:54:10 +0100

    Create a dummy without implementation, register it in SymbolicExecutionRunner, add test methods

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/EmptyNullableValueAccessTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/EmptyNullableValueAccessTest.cs
@@ -18,47 +18,68 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarAnalyzer.Rules.CSharp;
-using SonarAnalyzer.SymbolicExecution.Sonar.Analyzers;
+using SonarAnalyzer.Common;
+using ChecksCS = SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.CSharp;
+using CS = SonarAnalyzer.Rules.CSharp;
 
-namespace SonarAnalyzer.UnitTest.Rules
+namespace SonarAnalyzer.UnitTest.Rules;
+
+[TestClass]
+public class EmptyNullableValueAccessTest
 {
-    [TestClass]
-    public class EmptyNullableValueAccessTest
-    {
-        private readonly VerifierBuilder sonar = new VerifierBuilder<SymbolicExecutionRunner>().WithBasePath(@"SymbolicExecution\Sonar")
-            .WithOnlyDiagnostics(EmptyNullableValueAccess.S3655);
+    private readonly VerifierBuilder sonar = new VerifierBuilder()
+        .AddAnalyzer(() => new CS.SymbolicExecutionRunner(AnalyzerConfiguration.AlwaysEnabledWithSonarCfg))
+        .WithBasePath(@"SymbolicExecution\Sonar")
+        .WithOnlyDiagnostics(ChecksCS.EmptyNullableValueAccess.S3655);
 
-        [DataTestMethod]
-        [DataRow(ProjectType.Product)]
-        [DataRow(ProjectType.Test)]
-        public void EmptyNullableValueAccess_CSharp8(ProjectType projectType) =>
-            sonar.AddPaths("EmptyNullableValueAccess.cs")
-                .AddReferences(TestHelper.ProjectTypeReference(projectType).Concat(MetadataReferenceFacade.NETStandard21))
-                .WithOptions(ParseOptionsHelper.FromCSharp8)
-                .Verify();
+    private readonly VerifierBuilder roslynCS = new VerifierBuilder()
+        .AddAnalyzer(() => new CS.SymbolicExecutionRunner(AnalyzerConfiguration.AlwaysEnabled))
+        .WithBasePath(@"SymbolicExecution\Roslyn")
+        .WithOnlyDiagnostics(ChecksCS.EmptyNullableValueAccess.S3655);
+
+    [DataTestMethod]
+    [DataRow(ProjectType.Product)]
+    [DataRow(ProjectType.Test)]
+    public void EmptyNullableValueAccess_Sonar_CSharp8(ProjectType projectType) =>
+        sonar.AddPaths("EmptyNullableValueAccess.cs")
+            .AddReferences(TestHelper.ProjectTypeReference(projectType).Concat(MetadataReferenceFacade.NETStandard21))
+            .WithOptions(ParseOptionsHelper.FromCSharp8)
+            .Verify();
+
+    [DataTestMethod]
+    [DataRow(ProjectType.Product)]
+    [DataRow(ProjectType.Test)]
+    public void EmptyNullableValueAccess_Roslyn_CSharp8(ProjectType projectType) =>
+        roslynCS.AddPaths("EmptyNullableValueAccess.cs")
+            .AddReferences(TestHelper.ProjectTypeReference(projectType).Concat(MetadataReferenceFacade.NETStandard21))
+            .WithOptions(ParseOptionsHelper.FromCSharp8)
+            .Verify();
 
 #if NET
 
-        [TestMethod]
-        public void EmptyNullableValueAccess_CSharp9() =>
-            sonar.AddPaths("EmptyNullableValueAccess.CSharp9.cs")
-                .WithTopLevelStatements()
-                .Verify();
+    [TestMethod]
+    public void EmptyNullableValueAccess_Sonar_CSharp9() =>
+        sonar.AddPaths("EmptyNullableValueAccess.CSharp9.cs").WithTopLevelStatements().Verify();
 
-        [TestMethod]
-        public void EmptyNullableValueAccess_CSharp10() =>
-            sonar.AddPaths("EmptyNullableValueAccess.CSharp10.cs")
-                .WithOptions(ParseOptionsHelper.FromCSharp10)
-                .Verify();
+    [TestMethod]
+    public void EmptyNullableValueAccess_Roslyn_CSharp9() =>
+        roslynCS.AddPaths("EmptyNullableValueAccess.CSharp9.cs").WithTopLevelStatements().Verify();
+
+    [TestMethod]
+    public void EmptyNullableValueAccess_Sonar_CSharp10() =>
+        sonar.AddPaths("EmptyNullableValueAccess.CSharp10.cs").WithOptions(ParseOptionsHelper.FromCSharp10).Verify();
+
+    [TestMethod]
+    public void EmptyNullableValueAccess_Roslyn_CSharp10() =>
+        roslynCS.AddPaths("EmptyNullableValueAccess.CSharp10.cs").WithOptions(ParseOptionsHelper.FromCSharp10).Verify();
 
 #endif
 
-        [TestMethod]
-        public void EmptyNullableValueAccess_CSharp11() =>
-            sonar
-                .AddPaths("EmptyNullableValueAccess.CSharp11.cs")
-                .WithOptions(ParseOptionsHelper.FromCSharp11)
-                .Verify();
-    }
+    [TestMethod]
+    public void EmptyNullableValueAccess_Sonar_CSharp11() =>
+        sonar.AddPaths("EmptyNullableValueAccess.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
+
+    [TestMethod]
+    public void EmptyNullableValueAccess_Roslyn_CSharp11() =>
+        roslynCS.AddPaths("EmptyNullableValueAccess.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
 }
