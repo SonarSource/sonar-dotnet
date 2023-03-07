84abead3db350ad59f6a74ae2e6072c55ae3e251
Martin Strecker <martin.strecker@sonarsource.com>
2023-03-07 09:33:20 +0100

    Fix Binary handling

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/OperationProcessors/Binary.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/OperationProcessors/Binary.cs
@@ -69,22 +69,24 @@ internal sealed class Binary : BranchingProcessor<IBinaryOperationWrapper>
 
     private static SymbolicConstraint BinaryConstraint(BinaryOperatorKind kind, SymbolicValue left, SymbolicValue right)
     {
-        if (left is null && right is null)
+        var leftBool = left?.Constraint<BoolConstraint>();
+        var rightBool = right?.Constraint<BoolConstraint>();
+        if (leftBool is null && rightBool is null)
         {
             return null;
         }
-        else if (left is null || right is null)
+        else if (leftBool is null || rightBool is null)
         {
             return kind switch
             {
-                BinaryOperatorKind.Or or BinaryOperatorKind.ConditionalOr when (left ?? right).HasConstraint(BoolConstraint.True) => BoolConstraint.True,
-                BinaryOperatorKind.And or BinaryOperatorKind.ConditionalAnd when (left ?? right).HasConstraint(BoolConstraint.False) => BoolConstraint.False,
+                BinaryOperatorKind.Or or BinaryOperatorKind.ConditionalOr when (leftBool ?? rightBool) == BoolConstraint.True => BoolConstraint.True,
+                BinaryOperatorKind.And or BinaryOperatorKind.ConditionalAnd when (leftBool ?? rightBool) == BoolConstraint.False => BoolConstraint.False,
                 _ => null
             };
         }
-        else if (left.HasConstraint<BoolConstraint>() && right.HasConstraint<BoolConstraint>())
+        else if (leftBool is not null && rightBool is not null)
         {
-            return BinaryBoolConstraint(kind, left.HasConstraint(BoolConstraint.True), right.HasConstraint(BoolConstraint.True));
+            return BinaryBoolConstraint(kind, leftBool == BoolConstraint.True, rightBool == BoolConstraint.True);
         }
         else if (left.HasConstraint<ObjectConstraint>() && right.HasConstraint<ObjectConstraint>())
         {
