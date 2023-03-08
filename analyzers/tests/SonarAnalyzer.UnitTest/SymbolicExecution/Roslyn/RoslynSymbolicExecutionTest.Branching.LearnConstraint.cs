de7d0f9eb19d8d1cce18848ffad2c0e917a1dce7
Martin Strecker <martin.strecker@sonarsource.com>
2023-03-08 16:05:01 +0100

    Fix BoolConstraint assertion

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Branching.LearnConstraint.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Branching.LearnConstraint.cs
@@ -138,7 +138,7 @@ if (value = boolParameter)
     Tag(""Value"", value);
 }";
             var validator = SETestContext.CreateCS(code).Validator;
-            validator.ValidateTag("True", x => x.HasConstraint(ObjectConstraint.NotNull).Should().BeTrue());
+            validator.ValidateTag("True", x => x.HasConstraint<BoolConstraint>().Should().BeFalse());
             validator.ValidateTag("False", x => x.HasConstraint(BoolConstraint.False).Should().BeTrue());
             validator.ValidateTag("Value", x => x.HasConstraint(BoolConstraint.True).Should().BeFalse());    // Visited only for "true" condition
         }
