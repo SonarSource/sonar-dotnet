ca6b663b228dba2bbdce65ae4bd4b9f1a3425bfb
Martin Strecker <martin.strecker@sonarsource.com>
2023-03-09 14:39:41 +0100

    Move test case

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Invocation.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Invocation.cs
@@ -906,6 +906,7 @@ f()();";
         [DataRow("new object()", "null", false, ConstraintKind.ObjectNotNull, ConstraintKind.ObjectNull)]
         [DataRow("new int?()", "null", true, ConstraintKind.ObjectNull, ConstraintKind.ObjectNull)]
         [DataRow("new int?(42)", "null", false, ConstraintKind.ObjectNotNull, ConstraintKind.ObjectNull)]
+        [DataRow("new int?()", "42", false, ConstraintKind.ObjectNull, ConstraintKind.ObjectNotNull)]
         public void Invocation_Equals_LearnResult(string left, string right, bool expectedResult, ConstraintKind expectedConstraintLeft, ConstraintKind expectedConstraintRight)
         {
             var code = $@"
@@ -959,21 +960,6 @@ Tag(""End"");";
                     x[result].HasConstraint(BoolConstraint.False).Should().BeTrue();
                     x[arg].HasConstraint(ObjectConstraint.NotNull).Should().BeTrue();
                 });
-        }
-
-        [DataTestMethod]
-        [DataRow("new int?()", "42")]   // ToDo: Should be moved to Invocation_Equals_LearnResult once we build NotNull for int
-        public void Invocation_Equals_SplitsToBothResults_Nullable(string left, string right)
-        {
-            var code = $@"
-object left = {left};
-object right = {right};
-var result = object.Equals(left, right);
-Tag(""End"");";
-            var validator = SETestContext.CreateCS(code).Validator;
-            var result = validator.Symbol("result");
-            validator.TagStates("End").Should().SatisfyRespectively(
-                x => x[result].HasConstraint(BoolConstraint.False).Should().BeTrue());
         }
 
         [TestMethod]
