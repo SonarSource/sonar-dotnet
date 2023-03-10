5a89c74b275e6d43c37b4038888c614a0c72221e
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-03-10 11:10:37 +0100

    Clone test cases

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/PublicMethodArgumentsShouldBeCheckedForNull.CSharp11.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tests.Diagnostics
+{
+    public class Program
+    {
+        private object field = null;
+        private static object staticField = null;
+
+        public void NotCompliantCases(object[] o)
+        {
+            o[1].ToString(); // Noncompliant {{Refactor this method to add validation of parameter 'o' before using it.}}
+        }
+
+        public void Compliant(object[] o)
+        {
+            if (o is [not null, not null])
+            {
+                o.ToString(); // Compliant
+                o[1].ToString(); // Compliant
+            }
+        }
+    }
+
+    public interface ISomeInterface
+    {
+        public static virtual void NotCompliantCases(object o, Exception e)
+        {
+            o.ToString(); // Noncompliant {{Refactor this method to add validation of parameter 'o' before using it.}}
+        }
+    }
+}
