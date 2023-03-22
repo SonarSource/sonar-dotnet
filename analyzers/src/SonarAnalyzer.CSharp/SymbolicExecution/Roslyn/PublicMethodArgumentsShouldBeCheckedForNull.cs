c5de009d28b4da586b826579f78379866be6b18a
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-03-22 14:06:38 +0100

    Review: remove variable capture

--- analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/PublicMethodArgumentsShouldBeCheckedForNull.cs
+++ analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/PublicMethodArgumentsShouldBeCheckedForNull.cs
@@ -36,7 +36,7 @@ public class PublicMethodArgumentsShouldBeCheckedForNull : SymbolicRuleCheck
     protected override ProgramState PreProcessSimple(SymbolicContext context)
     {
         if (context.Operation.Instance.Kind == OperationKindEx.ParameterReference
-            && context.Operation.Instance.ToParameterReference() is { WrappedOperation: var reference, Parameter: var parameterSymbol }
+            && context.Operation.Instance.ToParameterReference() is { Parameter: var parameterSymbol }
             && parameterSymbol.Type.IsValueType is false
             && IsParameterDereferenced(context.Operation)
             && NullableStateIsNotKnownForParameter(parameterSymbol)
@@ -46,7 +46,7 @@ public class PublicMethodArgumentsShouldBeCheckedForNull : SymbolicRuleCheck
             var message = methodSymbol.IsConstructor()
                 ? "Refactor this constructor to avoid using members of parameter '{0}' because it could be null."
                 : "Refactor this method to add validation of parameter '{0}' before using it.";
-            ReportIssue(reference, string.Format(message, context.Operation.Instance.Syntax), context);
+            ReportIssue(context.Operation.Instance, string.Format(message, context.Operation.Instance.Syntax), context);
         }
 
         return context.State;
