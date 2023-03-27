4f63200ee105567127217c0fc15b2faed9b15bdc
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2023-03-27 11:46:39 +0200

    SE: Enumerable ContentHash for ordered and unordered sets (#6979)

--- analyzers/tests/SonarAnalyzer.UnitTest/Helpers/HashCodeTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Helpers/HashCodeTest.cs
@@ -18,6 +18,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#pragma warning disable CA1825 // Avoid zero-length array allocations
+
+using HashCode = SonarAnalyzer.Helpers.HashCode;
+
 namespace SonarAnalyzer.UnitTest.Helpers
 {
     [TestClass]
@@ -28,10 +32,10 @@ namespace SonarAnalyzer.UnitTest.Helpers
         [DataRow("Lorem Ipsum")]
         public void Combine_ProducesDifferentResults(string input)
         {
-            var hash2 = SonarAnalyzer.Helpers.HashCode.Combine(input, input);
-            var hash3 = SonarAnalyzer.Helpers.HashCode.Combine(input, input, input);
-            var hash4 = SonarAnalyzer.Helpers.HashCode.Combine(input, input, input, input);
-            var hash5 = SonarAnalyzer.Helpers.HashCode.Combine(input, input, input, input, input);
+            var hash2 = HashCode.Combine(input, input);
+            var hash3 = HashCode.Combine(input, input, input);
+            var hash4 = HashCode.Combine(input, input, input, input);
+            var hash5 = HashCode.Combine(input, input, input, input, input);
 
             hash2.Should().NotBe(0);
             hash3.Should().NotBe(0).And.NotBe(hash2);
@@ -45,8 +49,8 @@ namespace SonarAnalyzer.UnitTest.Helpers
             var numbers = Enumerable.Range(1, 1000);
             var dict1 = numbers.ToDictionary(x => x, x => x);
             var dict2 = numbers.OrderByDescending(x => x).ToDictionary(x => x, x => x);
-            var hashCode1 = SonarAnalyzer.Helpers.HashCode.DictionaryContentHash(dict1);
-            var hashCode2 = SonarAnalyzer.Helpers.HashCode.DictionaryContentHash(dict2);
+            var hashCode1 = HashCode.DictionaryContentHash(dict1);
+            var hashCode2 = HashCode.DictionaryContentHash(dict2);
             hashCode1.Should().Be(hashCode2);
         }
 
@@ -56,9 +60,73 @@ namespace SonarAnalyzer.UnitTest.Helpers
             var numbers = Enumerable.Range(1, 1000);
             var dict1 = numbers.ToImmutableDictionary(x => x, x => x);
             var dict2 = numbers.OrderByDescending(x => x).ToImmutableDictionary(x => x, x => x);
-            var hashCode1 = SonarAnalyzer.Helpers.HashCode.DictionaryContentHash(dict1);
-            var hashCode2 = SonarAnalyzer.Helpers.HashCode.DictionaryContentHash(dict2);
+            var hashCode1 = HashCode.DictionaryContentHash(dict1);
+            var hashCode2 = HashCode.DictionaryContentHash(dict2);
             hashCode1.Should().Be(hashCode2);
+        }
+
+        [TestMethod]
+        public void EnumerableUnorderedContentHash_Empty()
+        {
+            var ints = new int[0];
+            var strings = new string[0];
+
+            HashCode.EnumerableUnorderedContentHash(ints).Should().Be(HashCode.EnumerableUnorderedContentHash(new int[0]));
+            HashCode.EnumerableUnorderedContentHash(strings).Should().Be(HashCode.EnumerableUnorderedContentHash(strings));
+            HashCode.EnumerableUnorderedContentHash(ints).Should().Be(HashCode.EnumerableUnorderedContentHash(strings));
+        }
+
+        [TestMethod]
+        public void EnumerableUnorderedContentHash_Order()
+        {
+            var ints1 = new[] { 0, 1, 2 };
+            var ints2 = new[] { 2, 1, 0 };
+            var ints3 = new[] { 0, 1, 8 };
+
+            HashCode.EnumerableUnorderedContentHash(ints1).Should().Be(HashCode.EnumerableUnorderedContentHash(ints2)).And.NotBe(0);
+            HashCode.EnumerableUnorderedContentHash(ints1).Should().NotBe(HashCode.EnumerableUnorderedContentHash(ints3));
+        }
+
+        [TestMethod]
+        public void EnumerableUnorderedContentHash_DifferentLength()
+        {
+            var ints1 = new[] { 0, 1, 2 };
+            var ints2 = new[] { 0, 1, 2, 3 };
+
+            HashCode.EnumerableUnorderedContentHash(ints1).Should().NotBe(HashCode.EnumerableUnorderedContentHash(ints2));
+        }
+
+        [TestMethod]
+        public void EnumerableOrderedContentHash_Empty()
+        {
+            var ints = new int[0];
+            var strings = new string[0];
+
+            HashCode.EnumerableOrderedContentHash(ints).Should().Be(HashCode.EnumerableOrderedContentHash(new int[0]));
+            HashCode.EnumerableOrderedContentHash(strings).Should().Be(HashCode.EnumerableOrderedContentHash(strings));
+            HashCode.EnumerableOrderedContentHash(ints).Should().Be(HashCode.EnumerableOrderedContentHash(strings));
+        }
+
+        [TestMethod]
+        public void EnumerableOrderedContentHash_Order()
+        {
+            var ints1 = new[] { 0, 1, 2 };
+            var ints2 = new[] { 0, 1, 2 };
+            var ints3 = new[] { 2, 1, 0 };
+            var ints4 = new[] { 0, 1, 8 };
+
+            HashCode.EnumerableOrderedContentHash(ints1).Should().Be(HashCode.EnumerableOrderedContentHash(ints2)).And.NotBe(0);
+            HashCode.EnumerableOrderedContentHash(ints1).Should().NotBe(HashCode.EnumerableOrderedContentHash(ints3));
+            HashCode.EnumerableOrderedContentHash(ints1).Should().NotBe(HashCode.EnumerableOrderedContentHash(ints4));
+        }
+
+        [TestMethod]
+        public void EnumerableOrderedContentHash_DifferentLength()
+        {
+            var ints1 = new[] { 0, 1, 2 };
+            var ints2 = new[] { 0, 1, 2, 3 };
+
+            HashCode.EnumerableOrderedContentHash(ints1).Should().NotBe(HashCode.EnumerableOrderedContentHash(ints2));
         }
     }
 }
