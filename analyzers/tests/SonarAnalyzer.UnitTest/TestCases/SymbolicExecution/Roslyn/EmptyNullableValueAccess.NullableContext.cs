9249a67c587dae4c7382832d42091ea63712af3e
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-03-28 15:59:43 +0200

    S3655: Deconstruction and null forgiving operator (#6962)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/EmptyNullableValueAccess.NullableContext.cs
@@ -0,0 +1,48 @@
+﻿#nullable enable
+
+using System;
+
+class NullForgivingOperator
+{
+    void Basics(int? i)
+    {
+        _ = i!.Value;               // Compliant, unknown
+        i = SomeMethod();
+        _ = i!.Value;               // Compliant, unknown
+
+        i = null;
+        _ = i!.Value;               // Noncompliant, empty
+        i = new int?();
+        _ = i!.Value;               // Noncompliant, empty
+        i = new Nullable<int>();
+        _ = i!.Value;               // Noncompliant, empty
+
+        i = 42;
+        _ = i!.Value;               // Compliant, non-empty
+    }
+
+    void CastToValueType(int? i)
+    {
+        _ = (int)i!;                // Compliant, unknown
+        i = SomeMethod();
+        _ = (int)i!;                // Compliant, unknown
+
+        i = null;
+        _ = (int)i!;                // Noncompliant, empty
+        i = new int?();
+        _ = (int)i!;                // Noncompliant, empty
+        i = new Nullable<int>();
+        _ = (int)i!;                // Noncompliant, empty
+    }
+
+    void CastToNullableType(int? i)
+    {
+        _ = ((int?)i)!.Value;       // Compliant, unknown
+        _ = (i as int?)!.Value;     // Compliant, unknown
+
+        _ = ((int?)null)!.Value;    // Noncompliant
+        _ = (null as int?)!.Value;  // Noncompliant
+    }
+
+    static int? SomeMethod() => null;
+}
