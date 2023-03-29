2a4472620c19bf3388de3c5d06bdcfe31bd3fdf4
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2023-03-29 16:37:20 +0200

    Changed to file scoped namespace

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/Extensions/IOperationExtensions.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/Extensions/IOperationExtensions.cs
@@ -18,88 +18,87 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.SymbolicExecution.Roslyn
+namespace SonarAnalyzer.SymbolicExecution.Roslyn;
+
+internal static class IOperationExtensions
 {
-    internal static class IOperationExtensions
+    internal static ISymbol TrackedSymbol(this IOperation operation)
     {
-        internal static ISymbol TrackedSymbol(this IOperation operation)
+        return operation?.Kind switch
         {
-            return operation?.Kind switch
-            {
-                OperationKindEx.Conversion when operation.ToConversion() is var conversion && !IsTryDownCast(conversion) => TrackedSymbol(conversion.Operand),
-                OperationKindEx.FieldReference when operation.ToFieldReference() is var fieldReference && IsStaticOrThis(fieldReference) => fieldReference.Field,
-                OperationKindEx.LocalReference => operation.ToLocalReference().Local,
-                OperationKindEx.ParameterReference => operation.ToParameterReference().Parameter,
-                OperationKindEx.Argument => operation.ToArgument().Value.TrackedSymbol(),
-                _ => null
-            };
+            OperationKindEx.Conversion when operation.ToConversion() is var conversion && !IsTryDownCast(conversion) => TrackedSymbol(conversion.Operand),
+            OperationKindEx.FieldReference when operation.ToFieldReference() is var fieldReference && IsStaticOrThis(fieldReference) => fieldReference.Field,
+            OperationKindEx.LocalReference => operation.ToLocalReference().Local,
+            OperationKindEx.ParameterReference => operation.ToParameterReference().Parameter,
+            OperationKindEx.Argument => operation.ToArgument().Value.TrackedSymbol(),
+            _ => null
+        };
 
-            static bool IsTryDownCast(IConversionOperationWrapper conversion) =>
-                conversion.IsTryCast && !conversion.Operand.Type.DerivesOrImplements(conversion.Type);
-        }
-
-        internal static IInvocationOperationWrapper? AsInvocation(this IOperation operation) =>
-            operation.As(OperationKindEx.Invocation, IInvocationOperationWrapper.FromOperation);
-
-        internal static IIsPatternOperationWrapper? AsIsPattern(this IOperation operation) =>
-            operation.As(OperationKindEx.IsPattern, IIsPatternOperationWrapper.FromOperation);
-
-        internal static IObjectCreationOperationWrapper? AsObjectCreation(this IOperation operation) =>
-            operation.As(OperationKindEx.ObjectCreation, IObjectCreationOperationWrapper.FromOperation);
-
-        internal static IAssignmentOperationWrapper? AsAssignment(this IOperation operation) =>
-            operation.As(OperationKindEx.SimpleAssignment, IAssignmentOperationWrapper.FromOperation);
-
-        internal static IPropertyReferenceOperationWrapper? AsPropertyReference(this IOperation operation) =>
-            operation.As(OperationKindEx.PropertyReference, IPropertyReferenceOperationWrapper.FromOperation);
-
-        internal static IAwaitOperationWrapper ToAwait(this IOperation operation) =>
-            IAwaitOperationWrapper.FromOperation(operation);
-
-        internal static IArgumentOperationWrapper ToArgument(this IOperation operation) =>
-            IArgumentOperationWrapper.FromOperation(operation);
-
-        internal static IArrayElementReferenceOperationWrapper ToArrayElementReference(this IOperation operation) =>
-            IArrayElementReferenceOperationWrapper.FromOperation(operation);
-
-        internal static IConversionOperationWrapper ToConversion(this IOperation operation) =>
-            IConversionOperationWrapper.FromOperation(operation);
-
-        internal static IInvocationOperationWrapper ToInvocation(this IOperation operation) =>
-            IInvocationOperationWrapper.FromOperation(operation);
-
-        internal static IFieldReferenceOperationWrapper ToFieldReference(this IOperation operation) =>
-            IFieldReferenceOperationWrapper.FromOperation(operation);
-
-        internal static ILocalReferenceOperationWrapper ToLocalReference(this IOperation operation) =>
-            ILocalReferenceOperationWrapper.FromOperation(operation);
-
-        internal static IPropertyReferenceOperationWrapper ToPropertyReference(this IOperation operation) =>
-            IPropertyReferenceOperationWrapper.FromOperation(operation);
-
-        internal static IParameterReferenceOperationWrapper ToParameterReference(this IOperation operation) =>
-            IParameterReferenceOperationWrapper.FromOperation(operation);
-
-        internal static IEventReferenceOperationWrapper ToEventReference(this IOperation operation) =>
-            IEventReferenceOperationWrapper.FromOperation(operation);
-
-        internal static IUnaryOperationWrapper ToUnary(this IOperation operation) =>
-            IUnaryOperationWrapper.FromOperation(operation);
-
-        public static bool IsStaticOrThis(this IMemberReferenceOperationWrapper reference) =>
-            reference.Instance == null // static fields
-            || reference.Instance.Kind == OperationKindEx.InstanceReference;
-
-        public static IOperation UnwrapConversion(this IOperation operation)
-        {
-            while (operation?.Kind == OperationKindEx.Conversion)
-            {
-                operation = operation.ToConversion().Operand;
-            }
-            return operation;
-        }
-
-        private static T? As<T>(this IOperation operation, OperationKind kind, Func<IOperation, T> fromOperation) where T : struct =>
-            operation.Kind == kind ? fromOperation(operation) : null;
+        static bool IsTryDownCast(IConversionOperationWrapper conversion) =>
+            conversion.IsTryCast && !conversion.Operand.Type.DerivesOrImplements(conversion.Type);
     }
+
+    internal static IInvocationOperationWrapper? AsInvocation(this IOperation operation) =>
+        operation.As(OperationKindEx.Invocation, IInvocationOperationWrapper.FromOperation);
+
+    internal static IIsPatternOperationWrapper? AsIsPattern(this IOperation operation) =>
+        operation.As(OperationKindEx.IsPattern, IIsPatternOperationWrapper.FromOperation);
+
+    internal static IObjectCreationOperationWrapper? AsObjectCreation(this IOperation operation) =>
+        operation.As(OperationKindEx.ObjectCreation, IObjectCreationOperationWrapper.FromOperation);
+
+    internal static IAssignmentOperationWrapper? AsAssignment(this IOperation operation) =>
+        operation.As(OperationKindEx.SimpleAssignment, IAssignmentOperationWrapper.FromOperation);
+
+    internal static IPropertyReferenceOperationWrapper? AsPropertyReference(this IOperation operation) =>
+        operation.As(OperationKindEx.PropertyReference, IPropertyReferenceOperationWrapper.FromOperation);
+
+    internal static IAwaitOperationWrapper ToAwait(this IOperation operation) =>
+        IAwaitOperationWrapper.FromOperation(operation);
+
+    internal static IArgumentOperationWrapper ToArgument(this IOperation operation) =>
+        IArgumentOperationWrapper.FromOperation(operation);
+
+    internal static IArrayElementReferenceOperationWrapper ToArrayElementReference(this IOperation operation) =>
+        IArrayElementReferenceOperationWrapper.FromOperation(operation);
+
+    internal static IConversionOperationWrapper ToConversion(this IOperation operation) =>
+        IConversionOperationWrapper.FromOperation(operation);
+
+    internal static IInvocationOperationWrapper ToInvocation(this IOperation operation) =>
+        IInvocationOperationWrapper.FromOperation(operation);
+
+    internal static IFieldReferenceOperationWrapper ToFieldReference(this IOperation operation) =>
+        IFieldReferenceOperationWrapper.FromOperation(operation);
+
+    internal static ILocalReferenceOperationWrapper ToLocalReference(this IOperation operation) =>
+        ILocalReferenceOperationWrapper.FromOperation(operation);
+
+    internal static IPropertyReferenceOperationWrapper ToPropertyReference(this IOperation operation) =>
+        IPropertyReferenceOperationWrapper.FromOperation(operation);
+
+    internal static IParameterReferenceOperationWrapper ToParameterReference(this IOperation operation) =>
+        IParameterReferenceOperationWrapper.FromOperation(operation);
+
+    internal static IEventReferenceOperationWrapper ToEventReference(this IOperation operation) =>
+        IEventReferenceOperationWrapper.FromOperation(operation);
+
+    internal static IUnaryOperationWrapper ToUnary(this IOperation operation) =>
+        IUnaryOperationWrapper.FromOperation(operation);
+
+    public static bool IsStaticOrThis(this IMemberReferenceOperationWrapper reference) =>
+        reference.Instance == null // static fields
+        || reference.Instance.Kind == OperationKindEx.InstanceReference;
+
+    public static IOperation UnwrapConversion(this IOperation operation)
+    {
+        while (operation?.Kind == OperationKindEx.Conversion)
+        {
+            operation = operation.ToConversion().Operand;
+        }
+        return operation;
+    }
+
+    private static T? As<T>(this IOperation operation, OperationKind kind, Func<IOperation, T> fromOperation) where T : struct =>
+        operation.Kind == kind ? fromOperation(operation) : null;
 }
