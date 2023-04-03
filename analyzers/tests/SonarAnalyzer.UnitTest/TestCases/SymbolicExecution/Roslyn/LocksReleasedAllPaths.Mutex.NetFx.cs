6a2a61dd44f58ddb6c7d7561c84d97882fd84879
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2023-04-03 10:59:59 +0200

    SE - Non-nullable value types: Add support for Enum constraint (#6993)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/LocksReleasedAllPaths.Mutex.NetFx.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/LocksReleasedAllPaths.Mutex.NetFx.cs
@@ -61,7 +61,7 @@ public class MutexTest
         if (doesNotExist)
         {
             // The mutex does not exist, so create it.
-            m = new Mutex(true, mutexName, out mutexWasCreated, mutexSecurity); // Noncompliant FP, because the rule is not aware that the mutex didn't exist before and doesn't support out mutexWasCreated. Only awareness of both can fix it.
+            m = new Mutex(true, mutexName, out mutexWasCreated, mutexSecurity);
             if (!mutexWasCreated)
             {
                 // unable to create the mutex
@@ -94,7 +94,7 @@ public class MutexTest
             // Enter the mutex, and hold it until the program exits.
             try
             {
-                m.WaitOne();
+                m.WaitOne(); // Noncompliant FP
             }
             catch (UnauthorizedAccessException ex)
             {
