fee8155c0580859bdf6f31ba08f2c416338d9086
Martin Strecker <martin.strecker@sonarsource.com>
2023-04-04 16:56:48 +0200

    Make NullableAnnotation accessible and add tests

---
+++ analyzers/src/SonarAnalyzer.CFG/ShimLayer/ISymbolNullableExtensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace StyleCop.Analyzers.Lightup
+{
+    public static class ISymbolNullableExtensions
+    {
+        private static readonly Func<ITypeSymbol, NullableAnnotation> TypeSymbolAccessor = CreateSymbolNullableAnnotationAccessor<ITypeSymbol>();
+        private static readonly Func<IArrayTypeSymbol, NullableAnnotation> ArrayTypeSymbolElementAccessor = CreateSymbolNullableAnnotationAccessor<IArrayTypeSymbol>("ElementNullableAnnotation");
+        private static readonly Func<IParameterSymbol, NullableAnnotation> ParameterSymbolAccessor = CreateSymbolNullableAnnotationAccessor<IParameterSymbol>();
+        private static readonly Func<ILocalSymbol, NullableAnnotation> LocalSymbolAccessor = CreateSymbolNullableAnnotationAccessor<ILocalSymbol>();
+        private static readonly Func<IPropertySymbol, NullableAnnotation> PropertySymbolAccessor = CreateSymbolNullableAnnotationAccessor<IPropertySymbol>();
+        private static readonly Func<IFieldSymbol, NullableAnnotation> FieldSymbolAccessor = CreateSymbolNullableAnnotationAccessor<IFieldSymbol>();
+        private static readonly Func<IEventSymbol, NullableAnnotation> EventSymbolAccessor = CreateSymbolNullableAnnotationAccessor<IEventSymbol>();
+        private static readonly Func<ITypeParameterSymbol, NullableAnnotation> TypeParameterSymbolReferenceTypeConstraintAccessor =
+            CreateSymbolNullableAnnotationAccessor<ITypeParameterSymbol>("ReferenceTypeConstraintNullableAnnotation"); // ConstraintNullableAnnotations is not yet supported
+        private static readonly Func<IMethodSymbol, NullableAnnotation> MethodSymbolReceiverAccessor =
+            CreateSymbolNullableAnnotationAccessor<IMethodSymbol>("ReceiverNullableAnnotation");
+        private static readonly Func<IMethodSymbol, NullableAnnotation> MethodSymbolReturnAccessor =
+            CreateSymbolNullableAnnotationAccessor<IMethodSymbol>("ReturnNullableAnnotation"); // TypeArgumentNullableAnnotations is not yet supported
+
+        public static NullableAnnotation NullableAnnotation(this ITypeSymbol type) => TypeSymbolAccessor(type);
+        public static NullableAnnotation NullableAnnotation(this IParameterSymbol parameter) => ParameterSymbolAccessor(parameter);
+        public static NullableAnnotation NullableAnnotation(this ILocalSymbol local) => LocalSymbolAccessor(local);
+        public static NullableAnnotation NullableAnnotation(this IPropertySymbol property) => PropertySymbolAccessor(property);
+        public static NullableAnnotation NullableAnnotation(this IFieldSymbol field) => FieldSymbolAccessor(field);
+        public static NullableAnnotation NullableAnnotation(this IEventSymbol eventSymbol) => EventSymbolAccessor(eventSymbol);
+        public static NullableAnnotation ElementNullableAnnotation(this IArrayTypeSymbol arrayType) => ArrayTypeSymbolElementAccessor(arrayType);
+        public static NullableAnnotation ReferenceTypeConstraintNullableAnnotation(this ITypeParameterSymbol eventSymbol) => TypeParameterSymbolReferenceTypeConstraintAccessor(eventSymbol);
+        public static NullableAnnotation ReceiverNullableAnnotation(this IMethodSymbol method) => MethodSymbolReceiverAccessor(method);
+        public static NullableAnnotation ReturnNullableAnnotation(this IMethodSymbol method) => MethodSymbolReturnAccessor(method);
+
+        private static Func<T, NullableAnnotation> CreateSymbolNullableAnnotationAccessor<T>(string propertyName = nameof(NullableAnnotation)) where T : ISymbol
+            => LightupHelpers.CreateSyntaxPropertyAccessor<T, NullableAnnotation>(typeof(T), propertyName);
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Wrappers/ISymbolNullableExtensionsTest.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2023 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using StyleCop.Analyzers.Lightup;
+using NullableAnnotation = StyleCop.Analyzers.Lightup.NullableAnnotation;
+
+namespace SonarAnalyzer.UnitTest.Wrappers;
+
+[TestClass]
+public class ISymbolNullableExtensionsTest
+{
+    [DataTestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void NullableAnnotation_SimpleAnnitations(bool nullable)
+    {
+        var nullableAnnotation = nullable ? "?" : string.Empty;
+        var (tree, model) = TestHelper.CompileCS($$"""
+            #nullable enable
+            using System;
+            class C
+            {
+                object{{nullableAnnotation}} ObjectField;
+                object{{nullableAnnotation}}[] ArrayField;
+                object{{nullableAnnotation}} Property { get; set; }
+                event EventHandler{{nullableAnnotation}} MyEvent;
+
+                object{{nullableAnnotation}} Method(object{{nullableAnnotation}} parameter)
+                {
+                    object{{nullableAnnotation}} local;
+                    return null;
+                }
+            }
+            """);
+        var expected = nullable ? NullableAnnotation.Annotated : NullableAnnotation.NotAnnotated;
+
+        GetSymbol<IFieldSymbol>("ObjectField").NullableAnnotation().Should().Be(expected);
+        GetSymbol<IPropertySymbol>("Property").NullableAnnotation().Should().Be(expected);
+        GetSymbol<IEventSymbol>("MyEvent").NullableAnnotation().Should().Be(expected);
+        GetSymbol<IParameterSymbol>("parameter").NullableAnnotation().Should().Be(expected);
+        GetSymbol<ILocalSymbol>("local").NullableAnnotation().Should().Be(expected);
+        GetSymbol<IFieldSymbol>("ArrayField").Type.Should().BeAssignableTo<IArrayTypeSymbol>().Which.ElementNullableAnnotation().Should().Be(expected);
+        GetSymbol<IMethodSymbol>("Method").ReturnNullableAnnotation().Should().Be(expected);
+
+        T GetSymbol<T>(string name) where T : ISymbol
+        {
+            var node = tree.GetRoot().DescendantTokens().Single(x => x.ValueText == name).Parent;
+            node.Should().NotBeNull();
+            return model.GetDeclaredSymbol(node).Should().BeAssignableTo<T>().Which;
+        }
+    }
+
+    [DataTestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void NullableAnnotation_Receiver(bool nullable)
+    {
+        var nullableAnnotation = nullable ? "?" : string.Empty;
+        var (tree, model) = TestHelper.CompileCS($$"""
+        #nullable enable
+        using System;
+        static class C
+        {
+            static object ExtensionMethod(this object{{nullableAnnotation}} receiver)
+            {
+                return receiver.ExtensionMethod();
+            }
+        }
+        """);
+        var invocation = tree.GetRoot().DescendantNodesAndSelf().OfType<InvocationExpressionSyntax>().First();
+        var symbol = model.GetSymbolInfo(invocation).Symbol.Should().BeAssignableTo<IMethodSymbol>().Which;
+
+        var expected = nullable ? NullableAnnotation.Annotated : NullableAnnotation.NotAnnotated;
+        symbol.ReceiverNullableAnnotation().Should().Be(expected);
+    }
+
+    [DataTestMethod]
+    [DataRow(true)]
+    [DataRow(false)]
+    public void NullableAnnotation_ReferenceTypeConstrain(bool nullable)
+    {
+        var nullableAnnotation = nullable ? "?" : string.Empty;
+        var (tree, model) = TestHelper.CompileCS($$"""
+        #nullable enable
+        using System;
+        class C<T> where T: class
+        {
+            T{{nullableAnnotation}} field;
+        }
+        """);
+        var fieldDeclaration = tree.GetRoot().DescendantNodesAndSelf().OfType<FieldDeclarationSyntax>().First();
+        var symbol = model.GetDeclaredSymbol(fieldDeclaration.Declaration.Variables[0]).Should().BeAssignableTo<IFieldSymbol>().Which.Type.Should().BeAssignableTo<ITypeParameterSymbol>().Which;
+
+        var expected = nullable ? NullableAnnotation.Annotated : NullableAnnotation.NotAnnotated;
+        symbol.NullableAnnotation().Should().Be(expected);
+        symbol.ReferenceTypeConstraintNullableAnnotation().Should().Be(NullableAnnotation.NotAnnotated);
+
+    }
+}
