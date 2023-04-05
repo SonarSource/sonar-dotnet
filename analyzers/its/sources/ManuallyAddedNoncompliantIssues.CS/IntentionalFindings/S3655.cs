8b872cfe89d441454c6330676fa766b14867960e
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-04-05 16:21:04 +0200

    S3655: Add ITs in intentional findings (#7030)

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S3655.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * <Your-Product-Name>
+ * Copyright (c) <Year-From>-<Year-To> <Your-Company-Name>
+ *
+ * Please configure this header in your SonarCloud/SonarQube quality profile.
+ * You can also set it in SonarLint.xml additional file for SonarLint or standalone NuGet analyzer.
+ */
+
+namespace IntentionalFindings
+{
+    public class S3655
+    {
+        public void ValueAccessOnEmptyNullable()
+        {
+            int? i = null;
+            _ = i.Value; // Noncompliant (S3655) {{'i' is null on at least one execution path.}}
+        }
+    }
+}
