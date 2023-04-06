12ff96b70c5facf81a5024a66fb9f307ae59d6f5
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-04-06 16:03:36 +0200

    More UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Operations.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/RoslynSymbolicExecutionTest.Operations.cs
@@ -761,13 +761,15 @@ public static class Guard
 
                 Public Class Sample
 
-                    Public Sub Main(o1 As Object, o2 As Object, o3 As Object, o4 As Object, o5 As Object, o6 As Object, o7 As Object, o8 As Object, s1 As String)
+                    Public Sub Main(o1 As Object, o2 As Object, o3 As Object, o4 As Object, o5 As Object, o6 As Object, o7 As Object, o8 As Object, s1 As String, s2 As String)
+                        Dim SomeString As String = "Lorem"
                         Guard.NotNullExt(o1)
                         o2.NotNullExt()
                         NotNullInst(o3)
                         NotNullInst(o4, o5)
                         NotNullInst(value2:=o6, value1:=o7, value3:=o8)  ' value2 is not annotated
                         NotNullRef(s1)
+                        SomeString.NotNullExtOtherArgument(s2)
                         Tag("AfterGuard_o1", o1)
                         Tag("AfterGuard_o2", o2)
                         Tag("AfterGuard_o3", o3)
@@ -777,6 +779,7 @@ public static class Guard
                         Tag("AfterGuard_o7", o7)
                         Tag("AfterGuard_o8", o8)
                         Tag("AfterGuard_s1", s1)
+                        Tag("AfterGuard_s2", s2)
                     End Sub
 
                     Private Sub NotNullInst({notNullAttribute} value As Object)
@@ -811,6 +814,10 @@ public static class Guard
                     Public Sub NotNullExt(Of T As Class)({notNullAttribute} Value As T)
                     End Sub
 
+                    <Extension>
+                    Public Sub NotNullExtOtherArgument(Of T As Class)(Value As String, {notNullAttribute} NotNull As T)
+                    End Sub
+
                 End Module
                 """;
             var validator = new SETestContext(code, AnalyzerLanguage.VisualBasic, Array.Empty<SymbolicCheck>()).Validator;
@@ -823,6 +830,7 @@ public static class Guard
             validator.ValidateTag("AfterGuard_o7", x => x.Should().HaveOnlyConstraint(ObjectConstraint.NotNull));
             validator.ValidateTag("AfterGuard_o8", x => x.Should().HaveOnlyConstraint(ObjectConstraint.NotNull));
             validator.ValidateTag("AfterGuard_s1", x => x.Should().HaveOnlyConstraint(ObjectConstraint.NotNull));
+            validator.ValidateTag("AfterGuard_s2", x => x.Should().HaveOnlyConstraint(ObjectConstraint.NotNull));
         }
 
         [TestMethod]
