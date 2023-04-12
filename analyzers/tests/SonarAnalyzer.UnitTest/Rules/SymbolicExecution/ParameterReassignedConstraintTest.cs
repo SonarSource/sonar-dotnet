67e13d974ee1439719c67f0823953af1a5d72f58
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-04-12 13:14:37 +0200

    Use file scoped namespaces in unit tests

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ParameterReassignedConstraintTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ParameterReassignedConstraintTest.cs
@@ -22,34 +22,34 @@ using SonarAnalyzer.SymbolicExecution.Constraints;
 using SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.CSharp;
 using SonarAnalyzer.UnitTest.TestFramework.SymbolicExecution;
 
-namespace SonarAnalyzer.UnitTest.Rules
-{
-    [TestClass]
-    public class ParameterReassignedConstraintTest
-    {
-        [DataTestMethod]
-        [DataRow(@"arg = Unknown<object>();", "object")]
-        [DataRow(@"arg = Unknown<List<object>>();", "List<object>")]
-        [DataRow(@"arg = Unknown<int>();", "int")]
-        [DataRow(@"arg = Unknown<int?>();", "int?")]
-        [DataRow(@"arg = Unknown<object>();", "ref object")]
-        [DataRow(@"arg = Unknown<object>();", "out object")]
-        [DataRow(@"arg = Unknown<object[]>();", "params object[]")]
-        [DataRow(@"arg = Unknown<object>(); arg.ToString();", "object")]
-        [DataRow(@"arg = Unknown<object>(); arg.ToString();", "object")]
-        [DataRow(@"arg = Unknown<object>(); arg = Unknown<object>();", "object")]
-        public void ParameterReassignedConstraint_AfterAssignment(string methodSnippet, string argumentType)
-        {
-            var methodBody = $@"{methodSnippet} Tag(""AfterAssignment"", arg);";
-            var argumentSnippet = $", {argumentType} arg";
-            var validator = SETestContext.CreateCS(methodBody, argumentSnippet, new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
-            validator.ValidateTag("AfterAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
-        }
+namespace SonarAnalyzer.UnitTest.Rules;
 
-        [TestMethod]
-        public void ParameterReassignedConstraint_InsideCondition()
-        {
-            var snippet = @"if (arg == null)
+[TestClass]
+public class ParameterReassignedConstraintTest
+{
+    [DataTestMethod]
+    [DataRow(@"arg = Unknown<object>();", "object")]
+    [DataRow(@"arg = Unknown<List<object>>();", "List<object>")]
+    [DataRow(@"arg = Unknown<int>();", "int")]
+    [DataRow(@"arg = Unknown<int?>();", "int?")]
+    [DataRow(@"arg = Unknown<object>();", "ref object")]
+    [DataRow(@"arg = Unknown<object>();", "out object")]
+    [DataRow(@"arg = Unknown<object[]>();", "params object[]")]
+    [DataRow(@"arg = Unknown<object>(); arg.ToString();", "object")]
+    [DataRow(@"arg = Unknown<object>(); arg.ToString();", "object")]
+    [DataRow(@"arg = Unknown<object>(); arg = Unknown<object>();", "object")]
+    public void ParameterReassignedConstraint_AfterAssignment(string methodSnippet, string argumentType)
+    {
+        var methodBody = $@"{methodSnippet} Tag(""AfterAssignment"", arg);";
+        var argumentSnippet = $", {argumentType} arg";
+        var validator = SETestContext.CreateCS(methodBody, argumentSnippet, new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
+        validator.ValidateTag("AfterAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
+    }
+
+    [TestMethod]
+    public void ParameterReassignedConstraint_InsideCondition()
+    {
+        var snippet = @"if (arg == null)
                             {
                                 arg = Unknown<object>();
                                 Tag(""AfterAssignment"", arg);
@@ -58,30 +58,29 @@ namespace SonarAnalyzer.UnitTest.Rules
                             {
                                 Tag(""NoAssignment"", arg);
                             }";
-            var validator = SETestContext.CreateCS(snippet, ", object arg", new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
-            validator.ValidateTag("AfterAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
-            validator.ValidateTag("NoAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeFalse());
-        }
+        var validator = SETestContext.CreateCS(snippet, ", object arg", new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
+        validator.ValidateTag("AfterAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
+        validator.ValidateTag("NoAssignment", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeFalse());
+    }
 
-        [TestMethod]
-        public void ParameterReassignedConstraint_WithMultipleParameters()
-        {
-            var snippet = @"arg1 = Unknown<object>();
+    [TestMethod]
+    public void ParameterReassignedConstraint_WithMultipleParameters()
+    {
+        var snippet = @"arg1 = Unknown<object>();
                             Tag(""AfterAssignmentArg1"", arg1);
                             Tag(""AfterAssignmentArg2"", arg2);";
-            var validator = SETestContext.CreateCS(snippet, ", object arg1, object arg2", new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
-            validator.ValidateTag("AfterAssignmentArg1", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
-            validator.ValidateTag("AfterAssignmentArg2", x => x.Should().Match(c => c == null || !c.HasConstraint<ParameterReassignedConstraint>()));
-        }
+        var validator = SETestContext.CreateCS(snippet, ", object arg1, object arg2", new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
+        validator.ValidateTag("AfterAssignmentArg1", x => x.HasConstraint<ParameterReassignedConstraint>().Should().BeTrue());
+        validator.ValidateTag("AfterAssignmentArg2", x => x.Should().Match(c => c == null || !c.HasConstraint<ParameterReassignedConstraint>()));
+    }
 
-        [DataTestMethod]
-        [DataRow(@"var local = Unknown<object>(); Tag(""AfterAssignment"", local);")]
-        [DataRow(@"ObjectField = Unknown<object>(); Tag(""AfterAssignment"", ObjectField);")]
-        [DataRow(@"FullProperty = Unknown<object>(); Tag(""AfterAssignment"", FullProperty);")]
-        public void PublicMethodArgumentsShouldBeCheckedForNull_IgnoredAssignments_State_Roslyn_CS(string snippet)
-        {
-            var validator = SETestContext.CreateCS(snippet, new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
-            validator.ValidateTag("AfterAssignment", x => x.Should().Match(c => c == null || !c.HasConstraint<ParameterReassignedConstraint>()));
-        }
+    [DataTestMethod]
+    [DataRow(@"var local = Unknown<object>(); Tag(""AfterAssignment"", local);")]
+    [DataRow(@"ObjectField = Unknown<object>(); Tag(""AfterAssignment"", ObjectField);")]
+    [DataRow(@"FullProperty = Unknown<object>(); Tag(""AfterAssignment"", FullProperty);")]
+    public void PublicMethodArgumentsShouldBeCheckedForNull_IgnoredAssignments_State_Roslyn_CS(string snippet)
+    {
+        var validator = SETestContext.CreateCS(snippet, new PublicMethodArgumentsShouldBeCheckedForNull()).Validator;
+        validator.ValidateTag("AfterAssignment", x => x.Should().Match(c => c == null || !c.HasConstraint<ParameterReassignedConstraint>()));
     }
 }
