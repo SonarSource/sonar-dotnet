ed981552a083d7677b00b96bff455a4775739773
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-05-01 15:06:04 +0200

    S103: Fix exception when extern local functions are defined

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/MethodsShouldNotHaveTooManyLines.LocalFunctions.CSharp9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class WithExternLocalFunctions
+{
+    void ShortWithSingleExtern() // Compliant, we do not count extern local functions
+    {
+        int i = 0;
+        i++;
+        i++;
+        i++;
+        i++;
+
+        [DllImport("libc")]
+        static extern int chmod(string pathname, int mode); // Compliant, we do not count lines of an externally defined function
+    }
+
+    void ShortWithMultipleExtern() // Compliant, we do not count extern local functions
+    {
+        int i = 0;
+        i++;
+        i++;
+
+        [DllImport("libc")] static extern int chmod1(string pathname, int mode); // Compliant
+        [DllImport("libc")] static extern int chmod2(string pathname, int mode); // Compliant
+        [DllImport("libc")] static extern int chmod3(string pathname, int mode); // Compliant
+    }
+
+    void Long() // Noncompliant {{This method 'Long' has 6 lines, which is greater than the 5 lines authorized. Split it into smaller methods.}}
+    {
+        int i = 0;
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
+
+        [DllImport("libc")]
+        static extern int chmod(string pathname, int mode); // Compliant, we do not count lines of an externally defined function
+    }
+}
