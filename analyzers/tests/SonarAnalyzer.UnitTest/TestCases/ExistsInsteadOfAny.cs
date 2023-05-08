2496dc560937ff4cc48c02b901c23fe1072ef9c5
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-05-08 15:17:04 +0200

    Add UTs based on review comments

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExistsInsteadOfAny.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExistsInsteadOfAny.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -30,13 +31,61 @@ public class TestClass
         list?.Any(x => x > 0).ToString(); // Noncompliant
         classB?.Any(x => x); // Compliant
 
+        var enumList = new EnumList<int>();
+        enumList.Any(x => x > 0); // Compliant
+
+        var goodList = new GoodList<int>();
+        goodList.Any(x => x > 0); // Noncompliant
+
+        var ternary = (true ? list : goodList).Any(x => x > 0); // Noncompliant
+        var nullCoalesce = (list ?? goodList).Any(x => x > 0); // Noncompliant
+        var ternaryNullCoalesce = (list ?? (true ? list : goodList)).Any(x => x > 0); // Noncompliant
+
+        goodList.GetList().Any(x => true); // Noncompliant
+
+        Any<int>(x => x > 0); // Compliant
+        AcceptMethod<int>(goodList.Any); // Compliant
+
+        goodList.GetList().GetList().GetList().GetList().Any(x => x > 0);     //Noncompliant
+        goodList.GetList().GetList().GetList().GetList()?.Any(x => x > 0);    //Noncompliant
+        goodList.GetList().GetList().GetList()?.GetList().Any(x => x > 0);    //Noncompliant
+        goodList.GetList().GetList().GetList()?.GetList()?.Any(x => x > 0);   //Noncompliant
+        goodList.GetList().GetList()?.GetList().GetList().Any(x => x > 0);    //Noncompliant
+        goodList.GetList().GetList()?.GetList().GetList()?.Any(x => x > 0);   //Noncompliant
+        goodList.GetList().GetList()?.GetList()?.GetList().Any(x => x > 0);   //Noncompliant
+        goodList.GetList().GetList()?.GetList()?.GetList()?.Any(x => x > 0);  //Noncompliant
+        goodList.GetList()?.GetList().GetList().GetList().Any(x => x > 0);    //Noncompliant
+        goodList.GetList()?.GetList().GetList().GetList()?.Any(x => x > 0);   //Noncompliant
+        goodList.GetList()?.GetList().GetList()?.GetList().Any(x => x > 0);   //Noncompliant
+        goodList.GetList()?.GetList().GetList()?.GetList()?.Any(x => x > 0);  //Noncompliant
+        goodList.GetList()?.GetList()?.GetList().GetList().Any(x => x > 0);   //Noncompliant
+        goodList.GetList()?.GetList()?.GetList().GetList()?.Any(x => x > 0);  //Noncompliant
+        goodList.GetList()?.GetList()?.GetList()?.GetList().Any(x => x > 0);  //Noncompliant
+        goodList.GetList()?.GetList()?.GetList()?.GetList()?.Any(x => x > 0); //Noncompliant
+
         return list.Any(x => x % 2 == 0); // Noncompliant
     }
 
     bool ContainsEvenExpression(List<int> data) =>
         data.Any(x => x % 2 == 0); // Noncompliant
 
-    public class ClassA
+    bool Any<T>(Func<T, bool> predicate) => true;
+
+    void AcceptMethod<T>(Func<Func<T, bool>, bool> methodThatLooksLikeAny) { }
+
+    class GoodList<T> : List<T>
+    {
+        public GoodList<T> GetList() => this;
+        void CallAny() => this.Any(x => true); // Noncompliant
+    }
+
+    class EnumList<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator() => null;
+        IEnumerator IEnumerable.GetEnumerator() => null;
+    }
+
+    class ClassA
     {
         public List<int> myListField = new List<int>();
 
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExistsInsteadOfAny.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ExistsInsteadOfAny.vb
@@ -1,4 +1,5 @@
 ﻿Imports System
+Imports System.Collections
 Imports System.Collections.Generic
 Imports System.Linq
 
@@ -27,6 +28,29 @@ Public Class TestClass
         data?.Any(Function(x) x > 0).ToString() ' Noncompliant
         classB?.Any(Function(x) x) ' Compliant
 
+        Dim goodList = New GoodList(Of Integer)()
+
+        goodList.GetList().Any(Function(x) x > 0) ' Noncompliant
+
+        Any(Of Integer)(Function(x) x > 0) ' Compliant
+
+        goodList.GetList().GetList().GetList().GetList().Any(Function(x) x > 0)     ' Noncompliant
+        goodList.GetList().GetList().GetList().GetList()?.Any(Function(x) x > 0)    ' Noncompliant
+        goodList.GetList().GetList().GetList()?.GetList().Any(Function(x) x > 0)    ' Noncompliant
+        goodList.GetList().GetList().GetList()?.GetList()?.Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList().GetList()?.GetList().GetList().Any(Function(x) x > 0)    ' Noncompliant
+        goodList.GetList().GetList()?.GetList().GetList()?.Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList().GetList()?.GetList()?.GetList().Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList().GetList()?.GetList()?.GetList()?.Any(Function(x) x > 0)  ' Noncompliant
+        goodList.GetList()?.GetList().GetList().GetList().Any(Function(x) x > 0)    ' Noncompliant
+        goodList.GetList()?.GetList().GetList().GetList()?.Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList()?.GetList().GetList()?.GetList().Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList()?.GetList().GetList()?.GetList()?.Any(Function(x) x > 0)  ' Noncompliant
+        goodList.GetList()?.GetList()?.GetList().GetList().Any(Function(x) x > 0)   ' Noncompliant
+        goodList.GetList()?.GetList()?.GetList().GetList()?.Any(Function(x) x > 0)  ' Noncompliant
+        goodList.GetList()?.GetList()?.GetList()?.GetList().Any(Function(x) x > 0)  ' Noncompliant
+        goodList.GetList()?.GetList()?.GetList()?.GetList()?.Any(Function(x) x > 0) ' Noncompliant
+
         Return data.Any(Function(x) x Mod 2 = 0) ' Noncompliant
     End Function
 
@@ -34,28 +58,55 @@ Public Class TestClass
         Return data.Any(Function(x) x Mod 2 = 0) ' Noncompliant
     End Function
 
-    Public Class ClassA
-        Public myListField As List(Of Integer) = New List(Of Integer)()
-
-        Public Property myListProperty As List(Of Integer)
-            Get
-                Return myListField
-            End Get
-            Set(ByVal value As List(Of Integer))
-                myListField.AddRange(value)
-                Dim b = myListField.Any(Function(x) x > 0) ' Noncompliant
-                Dim c = myListField.Exists(Function(x) x > 0) ' Compliant
-            End Set
-        End Property
-
-        Public classB As ClassB = New ClassB()
-    End Class
-End Class
-
-Public Class ClassB
-    Public myListField As List(Of Integer) = New List(Of Integer)()
-
-    Public Function Any(ByVal predicate As Func(Of Boolean, Boolean)) As Boolean
-        Return False
+    Private Function Any(Of T)(ByVal predicate As Func(Of T, Boolean)) As Boolean
+        Return True
     End Function
+
+    Private Sub AcceptMethod(Of T)(ByVal methodThatLooksLikeAny As Func(Of Func(Of T, Boolean), Boolean))
+    End Sub
+
+    Private Class GoodList(Of T)
+        Inherits List(Of T)
+
+        Public Function GetList() As GoodList(Of T)
+            Return Me
+        End Function
+
+        Private Sub CallAny()
+            Me.Any(Function(x) True) ' Noncompliant
+        End Sub
+    End Class
+
+    Private Class EnumList(Of T)
+        Implements IEnumerable(Of T)
+
+        Public Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+            Return Nothing
+        End Function
+
+        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Return Nothing
+        End Function
+    End Class
+
+    Public Class ClassA
+            Public myListField As List(Of Integer) = New List(Of Integer)()
+            Public Property myListProperty As List(Of Integer)
+                Get
+                    Return myListField
+                End Get
+                Set(ByVal value As List(Of Integer))
+                    myListField.AddRange(value)
+                    Dim b = myListField.Any(Function(x) x > 0) ' Noncompliant
+                    Dim c = myListField.Exists(Function(x) x > 0) ' Compliant
+                End Set
+            End Property
+            Public classB As ClassB = New ClassB()
+        End Class
+    End Class
+    Public Class ClassB
+        Public myListField As List(Of Integer) = New List(Of Integer)()
+        Public Function Any(ByVal predicate As Func(Of Boolean, Boolean)) As Boolean
+            Return False
+        End Function
 End Class
