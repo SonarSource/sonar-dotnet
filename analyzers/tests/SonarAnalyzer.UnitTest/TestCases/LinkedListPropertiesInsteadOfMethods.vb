e50c1414a88bbaa1c304b729fe4efb1c226b617b
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-05-09 10:42:08 +0200

    Add If operator tests

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LinkedListPropertiesInsteadOfMethods.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/LinkedListPropertiesInsteadOfMethods.vb
@@ -10,13 +10,16 @@ Class [MyClass]
         '          ^^^^^
         Enumerable.Last(data)  ' Noncompliant {{'Last' property of 'LinkedList' should be used instead of the 'Last()' extension method.}}
         '          ^^^^
+
         Dim a = Enumerable.Any(data)    ' Compliant
-        Dim b1 = data.First()            ' Compliant
+        Dim b1 = data.First()           ' Compliant
         Dim b2 = data.Last()            ' Compliant
-        Dim c1 = data.First.Value        ' Compliant
+        Dim c1 = data.First.Value       ' Compliant
         Dim c2 = data.Last.Value        ' Compliant
         Dim d = data.Count()            ' Compliant
         Dim e1 = Enumerable.First(data, Function(x) x > 0) ' Compliant
         Dim e2 = Enumerable.Last(data, Function(x) x > 0)  ' Compliant
+
+        Enumerable.First(If(True = True, data, data)) ' Noncompliant
     End Sub
 End Class
