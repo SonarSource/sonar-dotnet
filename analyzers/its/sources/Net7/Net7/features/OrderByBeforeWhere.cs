ce4cb1c502603a36dfbb13e5ceaf400e4cbdce75
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-05-10 13:39:15 +0200

    Update ITs

---
+++ analyzers/its/sources/Net7/Net7/features/OrderByBeforeWhere.cs
@@ -0,0 +1,11 @@
+﻿namespace Net7.features
+{
+    public static class Foo
+    {
+        public static List<int> Bar()
+        {
+            var list = new List<int>();
+            return list.OrderBy(x => x).Where(x => true).ToList();
+        }
+    }
+}
