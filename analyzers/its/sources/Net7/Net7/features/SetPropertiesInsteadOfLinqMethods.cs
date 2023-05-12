7aadf3a2a29e85c1731467ec290dd1c67299554a
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-05-12 14:02:29 +0200

    Update ITs

---
+++ analyzers/its/sources/Net7/Net7/features/SetPropertiesInsteadOfLinqMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+namespace Net7.features
+{
+    public static class SetPropertiesInsteadOfLinqMethods
+    {
+        static int Method()
+        {
+            var set = new SortedSet<int>();
+            return set.Min();
+        }
+    }
+}
