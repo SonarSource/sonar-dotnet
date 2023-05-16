50b5b4ae715dd3ec97443189cb27131bd420c334
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-05-16 15:08:21 +0200

    Update ITs

---
+++ analyzers/its/sources/Net7/Net7/features/UseStringCreate.cs
@@ -0,0 +1,8 @@
+﻿namespace Net7.features
+{
+    public static class UseStringCreate
+    {
+        public static string Interpolate(string value) =>
+            FormattableString.CurrentCulture($"Value: {value}");
+    }
+}
