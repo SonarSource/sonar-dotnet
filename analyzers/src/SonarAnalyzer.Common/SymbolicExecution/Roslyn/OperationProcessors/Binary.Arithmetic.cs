9ded47cbe1c40ad293b68e421a6d173f06c197ac
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-05-24 16:09:51 +0200

    SE: Add BitOrOperation (#7232)

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/OperationProcessors/Binary.Arithmetic.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/OperationProcessors/Binary.Arithmetic.cs
@@ -31,7 +31,9 @@ internal sealed partial class Binary : BranchingProcessor<IBinaryOperationWrappe
         BinaryOperatorKind.Subtract => NumberConstraint.From(left.Min - right.Max, left.Max - right.Min),
         BinaryOperatorKind.Multiply => CalculateMultiply(left, right),
         BinaryOperatorKind.And when left.IsSingleValue && right.IsSingleValue => NumberConstraint.From(left.Min.Value & right.Min.Value),
-        BinaryOperatorKind.And => CalculateAnd(left, right),
+        BinaryOperatorKind.And => NumberConstraint.From(CalculateAndMin(left, right), CalculateAndMax(left, right)),
+        BinaryOperatorKind.Or when left.IsSingleValue && right.IsSingleValue => NumberConstraint.From(left.Min.Value | right.Min.Value),
+        BinaryOperatorKind.Or => NumberConstraint.From(CalculateOrMin(left, right), CalculateOrMax(left, right)),
         _ => null
     };
 
@@ -55,58 +57,120 @@ internal sealed partial class Binary : BranchingProcessor<IBinaryOperationWrappe
         return NumberConstraint.From(min, max);
     }
 
-    private static NumberConstraint CalculateAnd(NumberConstraint left, NumberConstraint right)
+    private static BigInteger? CalculateAndMin(NumberConstraint left, NumberConstraint right)
     {
         // The result can only be negative if both operands are negative => The result must be >= 0 unless both ranges include negative numbers.
-        BigInteger? min = 0;
         if (left.CanBeNegative && right.CanBeNegative)
         {
-            min = left.Min.HasValue && right.Min.HasValue
-                ? NegativeMagnitude(BigInteger.Min(left.Min.Value, right.Min.Value))
-                : null;
+            return left.Min.HasValue && right.Min.HasValue ? NegativeMagnitude(BigInteger.Min(left.Min.Value, right.Min.Value)) : null;
         }
-        return NumberConstraint.From(min, CalculateAndMax(left, right));
+        else
+        {
+            return 0;
+        }
     }
 
     private static BigInteger? CalculateAndMax(NumberConstraint left, NumberConstraint right)
     {
         // BitAnd can only turn 1s into 0s, not the other way around => If both operands have the same sign, the result cannot be bigger than the smaller of the two.
-        if ((left.IsNegative && right.IsNegative)
-            || (left.IsPositive && right.IsPositive))
+        if ((left.IsNegative && right.IsNegative) || (left.IsPositive && right.IsPositive))
         {
             return SmallestMaximum(left, right);
         }
-        // -1 == 0b11111111 => a & -1 == a =>
-        // If one operand can be -1 and one is positive, the result will be positive, but not bigger than the positive operand.
         else if (left.IsPositive)
         {
+            // At this point, right range R can be:
+            // - negative => it can contain -1 or other negative numbers, which can at most make the result equal to max of L.
+            // - mixed => it's the union of a negative sub-range Rn and a positive one Rp.
+            //   - L & Rn can go as high as max of L, because Rn contains -1 (all ones).
+            //   - L & Rp, because both are positive, cannot be bigger than the smaller of max of L and Rp, so smaller of max of L and R.
+            // Therefore, L & R <= max(max of L, smaller of max of L and R) = max of L.
             return left.Max;
         }
         else if (right.IsPositive)
         {
+            // Same reasoning as above
             return right.Max;
         }
         else
         {
-            // If both operands can be -1 and both can have positive values => The result cannot be bigger than the bigger of the two maxima.
-            // If one operand is negative and one can have positive values => The result cannot be bigger than the positive maximum.
+            // For all other cases we exploit the fact that & cannot make the result bigger than any of its inputs
             return BiggestMaximum(left, right);
+        }
+    }
+
+    private static BigInteger? CalculateOrMin(NumberConstraint left, NumberConstraint right)
+    {
+        // BitOr can only turn 0s into 1s, not the other way around => If both operands have the same sign, the result cannot be smaller than the bigger of the two.
+        if ((left.IsNegative && right.IsNegative) || (left.IsPositive && right.IsPositive))
+        {
+            return BiggestMinimum(left, right);
+        }
+        else if (left.IsNegative)
+        {
+            // At this point, right range R can be:
+            // - positive => it can contain 0 or other positive numbers, which can at most make the result as small as min of L.
+            // - mixed => it's the union of a negative sub-range Rn and a positive one Rp.
+            //   - L & Rn, because both are negative, cannot be smaller than the bigger of min of L and Rn, so bigger of min of L and R.
+            //   - L & Rp can go as small as min of L, because Rp contains 0 (all zeros).
+            // Therefore, L & R >= min(bigger of min of L and R, min of L) = min of L.
+            return left.Min;
+        }
+        else if (right.IsNegative)
+        {
+            // Same reasoning as above
+            return right.Min;
+        }
+        else
+        {
+            // For all other cases we exploit the fact that | cannot make the result smaller than any of its inputs
+            return SmallestMinimum(left, right);
+        }
+    }
+
+    private static BigInteger? CalculateOrMax(NumberConstraint left, NumberConstraint right)
+    {
+        // The result can only be positive if both operands are positive => The result must be < 0 unless both ranges include positive numbers.
+        if (left.CanBePositive && right.CanBePositive)
+        {
+            return left.Max.HasValue && right.Max.HasValue ? PositiveMagnitude(BigInteger.Max(left.Max.Value, right.Max.Value)) : null;
+        }
+        else
+        {
+            return -1;
         }
     }
 
     private static BigInteger? NegativeMagnitude(BigInteger value)
     {
-        // For various inputs, we're looking for the longest chain of 1 from the MSB side
-        // 11000000 - 64
-        // 11000001 - 63
-        // 11000010 - 62
-        // ------------ -
-        // 11000000 - 64 expected result
-        // ^^
+        // For increasing powers of 2 with negative sign, we're looking for the longest chain of 1 from the MSB side
+        // For example, given value = 0b1101000:
+        // 0b11111111 > 0b1101000 -> continue
+        // 0b11111110 > 0b1101000 -> continue
+        // ...
+        // 0b11000000 <= 0b1101000 -> stop!
+        //   ^^            ^^
         BigInteger magnitude = -1;
-        while (value < magnitude)
+        while (magnitude > value)
         {
-            magnitude *= 2;
+            magnitude <<= 1;
+        }
+        return magnitude;
+    }
+
+    private static BigInteger? PositiveMagnitude(BigInteger value)
+    {
+        // For increasing precendents of powers of 2, we're looking for the longest chain of 0 from the MSB side
+        // For example, given value = 0b0011010:
+        // 0b00000001 < 0b0011010 -> continue
+        // 0b00000011 < 0b0011010 -> continue
+        // ...
+        // 0b00111111 >= 0b0011010 -> stop!
+        //   ^^            ^^
+        BigInteger magnitude = 1;
+        while (magnitude < value)
+        {
+            magnitude = (magnitude << 1) | 1;
         }
         return magnitude;
     }
@@ -136,6 +200,18 @@ internal sealed partial class Binary : BranchingProcessor<IBinaryOperationWrappe
         else
         {
             return BigInteger.Max(left.Max.Value, right.Max.Value);
+        }
+    }
+
+    private static BigInteger? SmallestMinimum(NumberConstraint left, NumberConstraint right)
+    {
+        if (left.Min is null || right.Min is null)
+        {
+            return null;
+        }
+        else
+        {
+            return BigInteger.Min(left.Min.Value, right.Min.Value);
         }
     }
 
