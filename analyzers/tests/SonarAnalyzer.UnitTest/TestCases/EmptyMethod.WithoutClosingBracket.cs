2bd0eb197f574d7758622a729e2aaeecad4088a1
Čaba Šagi <75226367+csaba-sagi-sonarsource@users.noreply.github.com>
2023-05-26 08:15:35 +0200

    Fix AD0001 on S1186: NullReferenceException for LocalFunctionStatements (#7264)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/EmptyMethod.WithoutClosingBracket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Tests.Diagnostics
+{
+    public class EmptyMethod
+    {
+        void F1() // Noncompliant
+        {
+        }
+
+        void F2() // Noncompliant
+        {
