d69bff211bd647be18a5a40bb56a28e7db2a7070
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-05-31 14:52:04 +0200

    copy UTs and transform FN and NonCompliant tags to FIXMe

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.cs
@@ -1,154 +1,154 @@
 ﻿using System;
 using System.IO;
 
-public interface IInterface1 : IDisposable { }
+    public interface IInterface1 : IDisposable { }
 
-class Program
-{
-    public void DisposedTwice()
+    class Program
     {
-        var d = new Disposable();
-        d.Dispose();
-        d.Dispose(); // FIXME Non-compliant
-    }
-
-    public void DisposedTwice_Conditional()
-    {
-        IDisposable d = null;
-        d = new Disposable();
-        if (d != null)
+        public void DisposedTwice()
         {
+            var d = new Disposable();
             d.Dispose();
+            d.Dispose(); // FIXME Non-compliant
         }
-        d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
-//          ^
-    }
 
-    public void DisposedTwice_Try()
-    {
-        IDisposable d = null;
-        try
+        public void DisposedTwice_Conditional()
         {
+            IDisposable d = null;
             d = new Disposable();
-            var x = d;
-            x.Dispose();
-        }
-        finally
-        {
+            if (d != null)
+            {
+                d.Dispose();
+            }
             d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
+//          ^
         }
-    }
 
-    public void DisposedTwice_Array()
-    {
-        var a = new[] { new Disposable() };
-        a[0].Dispose();
-        a[0].Dispose(); // Compliant, we don't handle arrays
-    }
-
-    public void Dispose_Stream_LeaveOpenFalse()
-    {
-        using (MemoryStream memoryStream = new MemoryStream()) // Compliant
-        using (StreamWriter writer = new StreamWriter(memoryStream, new System.Text.UTF8Encoding(false), 1024, leaveOpen: false))
+        public void DisposedTwice_Try()
         {
-        }
-    }
-
-    public void Dispose_Stream_LeaveOpenTrue()
-    {
-        using (MemoryStream memoryStream = new MemoryStream()) // Compliant
-        using (StreamWriter writer = new StreamWriter(memoryStream, new System.Text.UTF8Encoding(false), 1024, leaveOpen: true))
-        {
-        }
-    }
-
-    public void Disposed_Using_WithDeclaration()
-    {
-        using (var d = new Disposable()) // FIXME Non-compliant
-        {
-            d.Dispose();
-        }
-    }
-
-    public void Disposed_Using_WithExpressions()
-    {
-        var d = new Disposable();
-        using (d) // FIXME Non-compliant
-        {
-            d.Dispose();
-        }
-    }
-
-    public void Disposed_Using_Parameters(IDisposable param1)
-    {
-        param1.Dispose();
-        param1.Dispose(); // FIXME Non-compliant
-    }
-
-    public void Close_ParametersOfDifferentTypes(IInterface1 interface1, IDisposable interface2)
-    {
-        // Regression test for https://github.com/SonarSource/sonar-dotnet/issues/1038
-        interface1.Dispose(); // ok, only called once on each parameter
-        interface2.Dispose();
-    }
-
-    public void Close_ParametersOfSameType(IInterface1 instance1, IInterface1 instance2)
-    {
-        // Regression test for https://github.com/SonarSource/sonar-dotnet/issues/1038
-        instance1.Dispose();
-        instance2.Dispose();
-    }
-
-    public void Close_OneParameterDisposedTwice(IInterface1 instance1, IInterface1 instance2)
-    {
-        instance1.Dispose();
-        instance1.Dispose(); // FIXME Non-compliant
-        instance1.Dispose(); // FIXME Non-compliant
-
-        instance2.Dispose(); // ok - only disposed once
-    }
-}
-
-public class Disposable : IDisposable
-{
-    public void Dispose() { }
-}
-
-public class MyClass : IDisposable
-{
-    public void Dispose() { }
-
-    public void DisposeMultipleTimes()
-    {
-        Dispose();
-        this.Dispose(); // FIXME Non-compliant
-        Dispose(); // FIXME Non-compliant
-    }
-
-    public void DoSomething()
-    {
-        Dispose();
-    }
-}
-
-class TestLoopWithBreak
-{
-    public static void LoopWithBreak(System.Collections.Generic.IEnumerable<string> list, bool condition, IInterface1 instance1)
-    {
-        foreach (string x in list)
-        {
+            IDisposable d = null;
             try
             {
-                if (condition)
-                {
-                    instance1.Dispose(); // FIXME Non-compliant
-                }
-                break;
+                d = new Disposable();
+                var x = d;
+                x.Dispose();
             }
-            catch (Exception)
+            finally
             {
-                continue;
+                d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
+            }
+        }
+
+        public void DisposedTwice_Array()
+        {
+            var a = new[] { new Disposable() };
+            a[0].Dispose();
+            a[0].Dispose(); // Compliant, we don't handle arrays
+        }
+
+        public void Dispose_Stream_LeaveOpenFalse()
+        {
+            using (MemoryStream memoryStream = new MemoryStream()) // Compliant
+            using (StreamWriter writer = new StreamWriter(memoryStream, new System.Text.UTF8Encoding(false), 1024, leaveOpen: false))
+            {
+            }
+        }
+
+        public void Dispose_Stream_LeaveOpenTrue()
+        {
+            using (MemoryStream memoryStream = new MemoryStream()) // Compliant
+            using (StreamWriter writer = new StreamWriter(memoryStream, new System.Text.UTF8Encoding(false), 1024, leaveOpen: true))
+            {
+            }
+        }
+
+        public void Disposed_Using_WithDeclaration()
+        {
+            using (var d = new Disposable()) // FIXME Non-compliant
+            {
+                d.Dispose();
+            }
+        }
+
+        public void Disposed_Using_WithExpressions()
+        {
+            var d = new Disposable();
+            using (d) // FIXME Non-compliant
+            {
+                d.Dispose();
+            }
+        }
+
+        public void Disposed_Using_Parameters(IDisposable param1)
+        {
+            param1.Dispose();
+            param1.Dispose(); // FIXME Non-compliant
+        }
+
+        public void Close_ParametersOfDifferentTypes(IInterface1 interface1, IDisposable interface2)
+        {
+            // Regression test for https://github.com/SonarSource/sonar-dotnet/issues/1038
+            interface1.Dispose(); // ok, only called once on each parameter
+            interface2.Dispose();
+        }
+
+        public void Close_ParametersOfSameType(IInterface1 instance1, IInterface1 instance2)
+        {
+            // Regression test for https://github.com/SonarSource/sonar-dotnet/issues/1038
+            instance1.Dispose();
+            instance2.Dispose();
+        }
+
+        public void Close_OneParameterDisposedTwice(IInterface1 instance1, IInterface1 instance2)
+        {
+            instance1.Dispose();
+            instance1.Dispose(); // FIXME Non-compliant
+            instance1.Dispose(); // FIXME Non-compliant
+
+            instance2.Dispose(); // ok - only disposed once
+        }
+    }
+
+    public class Disposable : IDisposable
+    {
+        public void Dispose() { }
+    }
+
+    public class MyClass : IDisposable
+    {
+        public void Dispose() { }
+
+        public void DisposeMultipleTimes()
+        {
+            Dispose();
+            this.Dispose(); // FIXME Non-compliant
+            Dispose(); // FIXME Non-compliant
+        }
+
+        public void DoSomething()
+        {
+            Dispose();
+        }
+    }
+
+    class TestLoopWithBreak
+    {
+        public static void LoopWithBreak(System.Collections.Generic.IEnumerable<string> list, bool condition, IInterface1 instance1)
+        {
+            foreach (string x in list)
+            {
+                try
+                {
+                    if (condition)
+                    {
+                        instance1.Dispose(); // FIXME Non-compliant
+                    }
+                    break;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
             }
         }
     }
-}
