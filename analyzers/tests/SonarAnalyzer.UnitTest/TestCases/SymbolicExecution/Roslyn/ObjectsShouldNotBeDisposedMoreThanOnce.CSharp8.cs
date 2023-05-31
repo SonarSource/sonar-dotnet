76f3a028e8b9becd67a6c660f0efb5eaf4370fda
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-05-31 15:40:32 +0200

    split to csharp8 file

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.CSharp8.cs
@@ -6,83 +6,83 @@ public class Disposable : IDisposable
     public void Dispose() { }
 }
 
-class UsingDeclaration
-{
-    public void Disposed_UsingDeclaration()
-    {
-        using var d = new Disposable();
-        d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
-    }
-}
-
-public class NullCoalescenceAssignment
-{
-    public void NullCoalescenceAssignment_Compliant(IDisposable s)
-    {
-        s ??= new Disposable();
-        s.Dispose();
-    }
-
-    public void NullCoalescenceAssignment_NonCompliant(IDisposable s)
-    {
-        using (s ??= new Disposable()) // FIXME Non-compliant
+        class UsingDeclaration
         {
-            s.Dispose();
-        }
-    }
-}
-
-public interface IWithDefaultMembers
-{
-    void DoDispose()
-    {
-        var d = new Disposable();
-        d.Dispose();
-        d.Dispose(); // FIXME Non-compliant
-    }
-}
-
-public class LocalStaticFunctions
-{
-    public void Method(object arg)
-    {
-        void LocalFunction()
-        {
-            var d = new Disposable();
-            d.Dispose();
-            d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+            public void Disposed_UsingDeclaration()
+            {
+                using var d = new Disposable();
+                d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
+            }
         }
 
-        static void LocalStaticFunction()
+        public class NullCoalescenceAssignment
         {
-            var d = new Disposable();
-            d.Dispose();
-            d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+            public void NullCoalescenceAssignment_Compliant(IDisposable s)
+            {
+                s ??= new Disposable();
+                s.Dispose();
+            }
+
+            public void NullCoalescenceAssignment_NonCompliant(IDisposable s)
+            {
+                using (s ??= new Disposable()) // FIXME Non-compliant
+                {
+                    s.Dispose();
+                }
+            }
         }
-    }
-}
 
-public ref struct Struct
-{
-    public void Dispose()
-    {
-    }
-}
+        public interface IWithDefaultMembers
+        {
+            void DoDispose()
+            {
+                var d = new Disposable();
+                d.Dispose();
+                d.Dispose(); // FIXME Non-compliant
+            }
+        }
 
-public class Consumer
-{
-    public void M1()
-    {
-        var s = new Struct();
+        public class LocalStaticFunctions
+        {
+            public void Method(object arg)
+            {
+                void LocalFunction()
+                {
+                    var d = new Disposable();
+                    d.Dispose();
+                    d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+                }
 
-        s.Dispose();
-        s.Dispose(); // FIXME Non-compliant
-    }
+                static void LocalStaticFunction()
+                {
+                    var d = new Disposable();
+                    d.Dispose();
+                    d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+                }
+        }
+        }
 
-    public void M2()
-    {
-        using var s = new Struct();
+        public ref struct Struct
+        {
+            public void Dispose()
+            {
+            }
+        }
 
-        s.Dispose(); // FIXME Non-compliant
-    }
-}
+        public class Consumer
+        {
+            public void M1()
+            {
+                var s = new Struct();
+
+                s.Dispose();
+                s.Dispose(); // FIXME Non-compliant
+            }
+
+            public void M2()
+            {
+                using var s = new Struct();
+
+                s.Dispose(); // FIXME Non-compliant
+            }
+        }
