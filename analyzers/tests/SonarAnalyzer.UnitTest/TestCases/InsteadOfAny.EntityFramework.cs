07c37bde57b7077dbb22f566bcb62d296fe17431
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-06-01 11:28:56 +0200

    S6605: Add FP repro for #7286

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/InsteadOfAny.EntityFramework.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+// https://github.com/SonarSource/sonar-dotnet/issues/7286
+public class EntityFrameworkReproGH7286
+{
+    public class MyEntity
+    {
+        public int Id { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        public DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    public List<MyEntity> GetEntities(MyDbContext dbContext, List<int> ids) =>
+        dbContext
+            .MyEntities
+            .Where(e => ids.Any(i => e.Id == i)) // Noncompliant - FP, should not raise in context of EntityFramework queries. Exist cannot be translated to SQL query
+            .ToList();
+}
