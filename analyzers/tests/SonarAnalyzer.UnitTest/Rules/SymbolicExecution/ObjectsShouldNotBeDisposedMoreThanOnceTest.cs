f3c1d8c154ccf84dd73e583731217e83412332e4
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2023-06-07 09:03:07 +0200

    Migrate S3966: Scaffold the rule for the new SE (#7315)

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ObjectsShouldNotBeDisposedMoreThanOnceTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SymbolicExecution/ObjectsShouldNotBeDisposedMoreThanOnceTest.cs
@@ -18,31 +18,64 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarAnalyzer.Rules.CSharp;
+using SonarAnalyzer.Common;
 using SonarAnalyzer.SymbolicExecution.Sonar.Analyzers;
+using ChecksCS = SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.CSharp;
+using CS = SonarAnalyzer.Rules.CSharp;
 
 namespace SonarAnalyzer.UnitTest.Rules
 {
     [TestClass]
     public class ObjectsShouldNotBeDisposedMoreThanOnceTest
     {
-        private readonly VerifierBuilder sonar = new VerifierBuilder<SymbolicExecutionRunner>().WithBasePath(@"SymbolicExecution\Sonar")
+        private readonly VerifierBuilder sonar = new VerifierBuilder()
+            .AddAnalyzer(() => new CS.SymbolicExecutionRunner(AnalyzerConfiguration.AlwaysEnabledWithSonarCfg))
+            .WithBasePath(@"SymbolicExecution\Sonar")
             .WithOnlyDiagnostics(ObjectsShouldNotBeDisposedMoreThanOnce.S3966);
+
+        private readonly VerifierBuilder roslynCS = new VerifierBuilder()
+            .AddAnalyzer(() => new CS.SymbolicExecutionRunner(AnalyzerConfiguration.AlwaysEnabled))
+            .WithBasePath(@"SymbolicExecution\Roslyn")
+            .WithOnlyDiagnostics(ChecksCS.ObjectsShouldNotBeDisposedMoreThanOnce.S3966);
 
         [DataTestMethod]
         [DataRow(ProjectType.Product)]
         [DataRow(ProjectType.Test)]
-        public void ObjectsShouldNotBeDisposedMoreThanOnce_CSharp8(ProjectType projectType) =>
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_Sonar_CSharp8(ProjectType projectType) =>
             sonar.AddPaths("ObjectsShouldNotBeDisposedMoreThanOnce.cs")
                 .WithOptions(ParseOptionsHelper.FromCSharp8)
-                .AddReferences(TestHelper.ProjectTypeReference(projectType).Concat(MetadataReferenceFacade.NETStandard21))
+                .AddReferences(TestHelper.ProjectTypeReference(projectType))
+                .AddReferences(MetadataReferenceFacade.NETStandard21)
+                .Verify();
+
+        [Ignore] // ToDo: Remove after S3966 implementation
+        [DataTestMethod]
+        [DataRow(ProjectType.Product)]
+        [DataRow(ProjectType.Test)]
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_Roslyn_CS(ProjectType projectType) =>
+            roslynCS.AddPaths("ObjectsShouldNotBeDisposedMoreThanOnce.cs")
+                .AddReferences(TestHelper.ProjectTypeReference(projectType))
                 .Verify();
 
 #if NET
 
+        [Ignore] // ToDo: Remove after S3966 implementation
         [TestMethod]
-        public void ObjectsShouldNotBeDisposedMoreThanOnce_CSharp9() =>
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_Roslyn_CSharp8() =>
+            roslynCS.AddPaths("ObjectsShouldNotBeDisposedMoreThanOnce.CSharp8.cs")
+                .WithOptions(ParseOptionsHelper.FromCSharp8)
+                .Verify();
+
+        [TestMethod]
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_Sonar_CSharp9() =>
             sonar.AddPaths("ObjectsShouldNotBeDisposedMoreThanOnce.CSharp9.cs")
+                .WithTopLevelStatements()
+                .Verify();
+
+        [Ignore] // ToDo: Remove after S3966 implementation
+        [TestMethod]
+        public void ObjectsShouldNotBeDisposedMoreThanOnce_Roslyn_CSharp9() =>
+            roslynCS.AddPaths("ObjectsShouldNotBeDisposedMoreThanOnce.CSharp9.cs")
                 .WithTopLevelStatements()
                 .Verify();
 
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.CSharp8.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+public class Disposable : IDisposable
+{
+    public void Dispose() { }
+}
+
+class UsingDeclaration
+{
+    public void Disposed_UsingDeclaration()
+    {
+        using var d = new Disposable();
+        d.Dispose(); // FIXME Non-compliant {{Refactor this code to make sure 'd' is disposed only once.}}
+    }
+}
+
+public class NullCoalescenceAssignment
+{
+    public void NullCoalescenceAssignment_Compliant(IDisposable s)
+    {
+        s ??= new Disposable();
+        s.Dispose();
+    }
+
+    public void NullCoalescenceAssignment_NonCompliant(IDisposable s)
+    {
+        using (s ??= new Disposable()) // FIXME Non-compliant
+        {
+            s.Dispose();
+        }
+    }
+}
+
+public interface IWithDefaultMembers
+{
+    void DoDispose()
+    {
+        var d = new Disposable();
+        d.Dispose();
+        d.Dispose(); // FIXME Non-compliant
+    }
+}
+
+public class LocalStaticFunctions
+{
+    public void Method(object arg)
+    {
+        void LocalFunction()
+        {
+            var d = new Disposable();
+            d.Dispose();
+            d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+        }
+
+        static void LocalStaticFunction()
+        {
+            var d = new Disposable();
+            d.Dispose();
+            d.Dispose(); // FIXME Non-compliant - FN: local functions are not supported by the CFG
+        }
+    }
+}
+
+public ref struct Struct
+{
+    public void Dispose()
+    {
+    }
+}
+
+public class Consumer
+{
+    public void M1()
+    {
+        var s = new Struct();
+
+        s.Dispose();
+        s.Dispose(); // FIXME Non-compliant
+    }
+
+    public void M2()
+    {
+        using var s = new Struct();
+
+        s.Dispose(); // FIXME Non-compliant
+    }
+}
