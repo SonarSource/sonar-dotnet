29080c4b8f883a9e31dff9997f34a6585a77ca4c
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2023-06-13 13:02:03 +0200

    Delete empty line

--- analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.cs
+++ analyzers/src/SonarAnalyzer.CSharp/SymbolicExecution/Roslyn/ObjectsShouldNotBeDisposedMoreThanOnce.cs
@@ -25,7 +25,6 @@ public sealed class ObjectsShouldNotBeDisposedMoreThanOnce : ObjectsShouldNotBeD
     public static readonly DiagnosticDescriptor S3966 = DescriptorFactory.Create(DiagnosticId, MessageFormat);
     protected override DiagnosticDescriptor Rule => S3966;
 
-
     public override bool ShouldExecute()
     {
         var walker = new Walker();
