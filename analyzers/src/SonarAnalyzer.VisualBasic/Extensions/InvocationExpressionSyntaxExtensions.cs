31c77564375881c3672513f4b718dae39bf2a855
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-06-16 15:32:43 +0200

    when arguments are null in invocation consider it n=0

--- analyzers/src/SonarAnalyzer.VisualBasic/Extensions/InvocationExpressionSyntaxExtensions.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/Extensions/InvocationExpressionSyntaxExtensions.cs
@@ -30,7 +30,9 @@ namespace SonarAnalyzer.Extensions
             invocation.ArgumentList.Arguments.GetSymbolsOfKnownType(knownType, semanticModel);
 
         internal static bool HasExactlyNArguments(this InvocationExpressionSyntax invocation, int count) =>
-            invocation?.ArgumentList != null && invocation.ArgumentList.Arguments.Count == count;
+            count == 0
+                ? invocation?.ArgumentList == null || invocation.ArgumentList.Arguments.Count == 0
+                : invocation?.ArgumentList != null && invocation.ArgumentList.Arguments.Count == count;
 
         internal static bool TryGetOperands(this InvocationExpressionSyntax invocation, out SyntaxNode left, out SyntaxNode right)
         {
