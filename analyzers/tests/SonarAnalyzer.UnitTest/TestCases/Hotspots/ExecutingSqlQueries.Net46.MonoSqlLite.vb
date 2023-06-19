482ae6f0500cb9aaaeec095fbe5431369f53f594
Martin Strecker <martin.strecker@sonarsource.com>
2023-06-19 13:32:25 +0200

    Fix vbh test case

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.vb
@@ -7,15 +7,15 @@ Namespace Test
         Private ConstQuery As String = ""
 
         Private Sub Compliant(ByVal connection As SqliteConnection)
-            Dim command = New SqliteCommand()
-            command = New SqliteCommand(connection)
-            Dim adapter = New SqliteDataAdapter()
+            Dim command = New SqliteCommand()       ' Compliant
+            command = New SqliteCommand(connection) ' Compliant
+            Dim adapter = New SqliteDataAdapter()   ' Compliant
         End Sub
 
         Private Sub Foo(ByVal connection As SqliteConnection, ByVal query As String, ParamArray parameters As Object())
-            Dim command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection)
-            command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}")
-            Dim adapter = New SqliteDataAdapter(String.Concat(query, parameters), connection)
+            Dim command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection) ' Noncompliant
+            command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}")                 ' Noncompliant
+            Dim adapter = New SqliteDataAdapter(String.Concat(query, parameters), connection)         ' Noncompliant
         End Sub
     End Class
 End Namespace
