b07fde510f17cb302f726fcd414989068c81197f
Martin Strecker <martin.strecker@sonarsource.com>
2023-06-19 13:42:33 +0200

    Add reproducer

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.cs
@@ -21,5 +21,20 @@ namespace Test
             command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}");                  // Noncompliant
             var adapter = new SqliteDataAdapter(string.Concat(query, parameters), connection);          // Noncompliant
         }
+
+        // https://github.com/SonarSource/sonar-dotnet/issues/7261
+        void Reproduce_7261(string connectionString, string query)
+        {
+            //get data
+            string sql = "select * from table where query = '" + query + "';"; // Secondary [adapter, command]
+
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                SqliteDataAdapter da = new SqliteDataAdapter(sql, connection); // Noncompliant [adapter]
+                SqliteCommand command = new SqliteCommand(sql, connection);    // Noncompliant [command]
+            }
+        }
     }
 }
