2197ff3c45160f42ce0656e27a2b125a558c05a5
Martin Strecker <martin.strecker@sonarsource.com>
2023-06-20 09:16:46 +0200

    Review

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.cs
@@ -2,38 +2,37 @@
 using System.Linq;
 using Mono.Data.Sqlite;
 
-namespace Test
+public class Sample
 {
-    public class C
+    string ConstQuery = "";
+
+    void Compliant(SqliteConnection connection)
     {
-        string ConstQuery = "";
+        var command = new SqliteCommand();       // Compliant
+        command = new SqliteCommand(connection); // Compliant
+        var adapter = new SqliteDataAdapter();   // Compliant
+    }
 
-        void Compliant(SqliteConnection connection)
+    void Foo(SqliteConnection connection, string query, SqliteTransaction transaction, params object[] parameters)
+    {
+        var command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection);          // Noncompliant
+        command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}");                          // Noncompliant
+        command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection, transaction); // Noncompliant
+        var adapter = new SqliteDataAdapter(string.Concat(query, parameters), connection);                  // Noncompliant
+        adapter = new SqliteDataAdapter(string.Concat(query, parameters), "connection");                    // Noncompliant
+    }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/7261
+    void Reproduce_7261(string connectionString, string query)
+    {
+        string sql = "select * from table where query = '" + query + "';"; // Secondary [adapter, command]
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
         {
-            var command = new SqliteCommand();       // Compliant
-            command = new SqliteCommand(connection); // Compliant
-            var adapter = new SqliteDataAdapter();   // Compliant
-        }
+            connection.Open();
 
-        void Foo(SqliteConnection connection, string query, params object[] parameters)
-        {
-            var command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection);  // Noncompliant
-            command = new SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}");                  // Noncompliant
-            var adapter = new SqliteDataAdapter(string.Concat(query, parameters), connection);          // Noncompliant
-        }
-
-        // https://github.com/SonarSource/sonar-dotnet/issues/7261
-        void Reproduce_7261(string connectionString, string query)
-        {
-            string sql = "select * from table where query = '" + query + "';"; // Secondary [adapter, command]
-
-            using (SqliteConnection connection = new SqliteConnection(connectionString))
-            {
-                connection.Open();
-
-                var adapter = new SqliteDataAdapter(sql, connection); // Noncompliant [adapter]
-                var command = new SqliteCommand(sql, connection);     // Noncompliant [command]
-            }
+            var adapter = new SqliteDataAdapter(sql, connection); // Noncompliant [adapter]
+            var command = new SqliteCommand(sql, connection);     // Noncompliant [command]
         }
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.Net46.MonoSqlLite.vb
@@ -2,20 +2,20 @@
 Imports System.Linq
 Imports Mono.Data.Sqlite
 
-Namespace Test
-    Public Class C
-        Private ConstQuery As String = ""
+Public Class Sample
+    Private ConstQuery As String = ""
 
-        Private Sub Compliant(ByVal connection As SqliteConnection)
-            Dim command = New SqliteCommand()       ' Compliant
-            command = New SqliteCommand(connection) ' Compliant
-            Dim adapter = New SqliteDataAdapter()   ' Compliant
-        End Sub
+    Private Sub Compliant(ByVal connection As SqliteConnection)
+        Dim command = New SqliteCommand()       ' Compliant
+        command = New SqliteCommand(connection) ' Compliant
+        Dim adapter = New SqliteDataAdapter()   ' Compliant
+    End Sub
 
-        Private Sub Foo(ByVal connection As SqliteConnection, ByVal query As String, ParamArray parameters As Object())
-            Dim command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection) ' Noncompliant
-            command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}")                 ' Noncompliant
-            Dim adapter = New SqliteDataAdapter(String.Concat(query, parameters), connection)         ' Noncompliant
-        End Sub
-    End Class
-End Namespace
+    Private Sub Foo(ByVal connection As SqliteConnection, transaction As SqliteTransaction, ByVal query As String, ParamArray parameters As Object())
+        Dim command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection)          ' Noncompliant
+        command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}")                          ' Noncompliant
+        command = New SqliteCommand($"SELECT * FROM mytable WHERE mycol={query}", connection, transaction) ' Noncompliant
+        Dim adapter = New SqliteDataAdapter(String.Concat(query, parameters), connection)                  ' Noncompliant
+        adapter = New SqliteDataAdapter(String.Concat(query, parameters), "connection")                    ' Noncompliant
+    End Sub
+End Class
