ba7e93bc0700986f53c89d96f3678f983afe444c
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-06-21 17:06:06 +0200

    Add IT issue

---
+++ analyzers/its/sources/Net7/Net7/features/AvoidDateTimeNowForBenchmarking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Net7.features
+{
+    internal class AvoidDateTimeNowForBenchmarking
+    {
+        public void S6561()
+        {
+            var start = DateTime.Now;
+            MethodToBeBenchmarked();
+            Console.WriteLine($"{(DateTime.Now - start).TotalMilliseconds} ms");
+        }
+
+        void MethodToBeBenchmarked() { }
+    }
+}
