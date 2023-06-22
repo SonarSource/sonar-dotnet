3a624fda36e20319e26b43a908ca6eaf2d9d9416
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-06-22 16:59:32 +0200

    Add IT test for VB

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.VB/IntentionalFindings/AvoidDateTimeNowForBenchmarking.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+
+Friend Class AvoidDateTimeNowForBenchmarking
+    Public Sub S6561()
+        Dim start = Date.Now
+        MethodToBeBenchmarked()
+        Console.WriteLine($"{(Date.Now - start).TotalMilliseconds} ms")
+    End Sub
+    Private Sub MethodToBeBenchmarked()
+    End Sub
+End Class
