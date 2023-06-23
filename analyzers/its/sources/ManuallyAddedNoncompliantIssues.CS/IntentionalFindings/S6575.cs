9f6fee7c9037a0421e614061d9bbeaf6d7dc6357
Čaba Šagi <csaba.sagi@sonarsource.com>
2023-06-23 14:54:15 +0200

    Update ITs

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6575.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * <Your-Product-Name>
+ * Copyright (c) <Year-From>-<Year-To> <Your-Company-Name>
+ *
+ * Please configure this header in your SonarCloud/SonarQube quality profile.
+ * You can also set it in SonarLint.xml additional file for SonarLint or standalone NuGet analyzer.
+ */
+
+using TimeZoneConverter;
+
+namespace IntentionalFindings
+{
+    public class S6575
+    {
+        public void ConvertTimeZone() =>
+            TZConvert.IanaToWindows("Asia/Tokyo"); // Noncompliant (S6575)
+    }
+}
