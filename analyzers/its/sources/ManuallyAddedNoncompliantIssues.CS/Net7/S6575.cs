d8da3560a66231f2311d75e8f6320d7544cdf611
Čaba Šagi <csaba.sagi@sonarsource.com>
2023-06-23 15:42:15 +0200

    Move ITs to Net7 project

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/Net7/S6575.cs
@@ -0,0 +1,10 @@
+﻿using TimeZoneConverter;
+
+namespace IntentionalFindings
+{
+    public class S6575
+    {
+        public void ConvertTimeZone() =>
+            TZConvert.IanaToWindows("Asia/Tokyo"); // Noncompliant (S6575)
+    }
+}
---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.VB/Net7/S6575.vb
@@ -0,0 +1,9 @@
+﻿Imports TimeZoneConverter
+
+Namespace IntentionalFindings
+    Public Class S6575
+        Public Sub ConvertTimeZone()
+            TZConvert.IanaToWindows("Asia/Tokyo") ' Noncompliant (S6575)
+        End Sub
+    End Class
+End Namespace
