1d14c3ad1c6d2b1943b82c40e232254d740d9af4
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-06-29 13:56:24 +0200

    Add test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ParametersCorrectOrder.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ParametersCorrectOrder.cs
@@ -1,284 +1,280 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace Tests.Diagnostics
+public class Params
 {
-    public class Params
+    public void method(int i, int k = 5, params int[] rest)
     {
-        public void method(int i, int k = 5, params int[] rest)
+    }
+
+    public void call()
+    {
+        int i = 0, j = 5, k = 6, l = 7;
+        method(i, j, k, l);
+    }
+    public void call2()
+    {
+        int i = 0, j = 5, rest = 6, l = 7;
+        var k = new[] { i, l };
+        method(i, k: rest, rest: k); // Compliant, code will not compile if suggestion is applied
+    }
+}
+
+public static class Other
+{
+    public static void call()
+    {
+        var a = "1";
+        var b = "2";
+
+        Comparer.Default.Compare(b, a); // Noncompliant
+    }
+}
+
+public static class Extensions
+{
+    public static void Ex(this string self, string v1, string v2)
+    //                 ^^ Secondary [6]
+    {
+    }
+    public static void Ex(this string self, string v1, string v2, int x)
+    {
+        Ex(self, v1, v2);
+        self.Ex(v1, v2);
+        Extensions.Ex(self, v1, v2);
+        Tests.Diagnostics.Extensions.Ex(self, v1, v2);
+    }
+}
+
+public partial class ParametersCorrectOrder
+{
+    partial void divide(int divisor, int someOther, int dividend, int p = 10, int some = 5, int other2 = 7);
+    //           ^^^^^^ Secondary [1,2,3,4]
+}
+
+public partial class ParametersCorrectOrder
+{
+    partial void divide(int a, int b, int c, int p, int other, int other2)
+    {
+        var x = a / b;
+    }
+
+    public void m(int a, int b) // Secondary [5,7]
+    {
+    }
+
+    public void n(int a, int b, int c)
+    {
+    }
+
+    public void doTheThing()
+    {
+        int divisor = 15;
+        int dividend = 5;
+        var something = 6;
+        var someOther = 6;
+        var other2 = 6;
+        var some = 6;
+
+        divide(dividend, 1 + 1, divisor, other2: 6);  // Noncompliant [1] operation succeeds, but result is unexpected
+
+        divide(divisor, other2, dividend);
+        divide(divisor, other2, dividend, other2: someOther); // Noncompliant [2] {{Parameters to 'divide' have the same names but not the same order as the method arguments.}}
+
+        divide(divisor, someOther, dividend, other2: some, some: other2); // Noncompliant [3]
+
+        divide(1, 1, 1, other2: some, some: other2); // Noncompliant [4]
+        divide(1, 1, 1, other2: 1, some: other2);
+
+        int a = 5, b = 6;
+
+        m(1, a); // Noncompliant [7] Argument "a" may be first
+        m(1, b);
+        m(a, a);
+        m(divisor, dividend);
+
+        m(a, b);
+        m(b, b); // Compliant
+        m(b, a); // Noncompliant [5]
+
+        var v1 = "";
+        var v2 = "";
+
+        "aaaaa".Ex(v1, v2);
+        "aaaaa".Ex(v2, v1); // Noncompliant [6]
+
+        int c = 1;
+        n(a, a, a);
+        n(b, b, b);
+        n(c, c, c);
+    }
+}
+
+public class A
+{
+    public class B
+    {
+        public class C
         {
+            public C(string left, string right) { } // Secondary [C]
+        }
+    }
+}
+
+public class Foo
+{
+    public Foo(string left, string right) { } // Secondary [B]
+
+    public void Method(string left, string right) { } // Secondary [A]
+
+    public void Bar()
+    {
+        var left = "valueLeft";
+        var right = "valueRight";
+
+        Method(left, right);
+        Method(right, left); // Noncompliant [A]
+
+        var foo1 = new Foo(left, right);
+        var foo2 = new Foo(right, left); // Noncompliant [B]
+
+        var c1 = new A.B.C(left, right);
+        var c2 = new A.B.C(right, left); // Noncompliant [C]
+    }
+}
+
+class Program
+{
+    void Struct(DateTime a, string b)
+    {
+        Bar1(a, b); // Compliant
+    }
+
+    void ClassAndInterface(Boo a, string b)
+    {
+        Bar2(a, b); // Compliant
+        Bar3(a, b); // Compliant
+        Bar3(b: a, a: b); // Compliant
+    }
+    void Bar1(DateTime b, string a) { }
+    void Bar2(Boo b, string a) { }
+    void Bar3(IBoo b, string a) { }
+}
+interface IBoo { }
+class Boo : IBoo { }
+
+class WithLocalFunctions
+{
+    public void M1()
+    {
+        static double divide(int divisor, int dividend)
+        {
+            return divisor / dividend;
         }
 
-        public void call()
+        static void doTheThing(int divisor, int dividend)
         {
-            int i = 0, j = 5, k = 6, l=7;
-            method(i, j, k, l);
-        }
-        public void call2()
-        {
-            int i = 0, j = 5, rest = 6, l = 7;
-            var k = new[] { i, l };
-            method(i, k : rest, rest : k); // Compliant, code will not compile if suggestion is applied
+            double result = divide(dividend, divisor);  // Noncompliant
         }
     }
 
-    public static class Other
+    public void M2()
     {
-        public static void call()
+        double divide(int divisor, int dividend)
         {
-            var a = "1";
-            var b = "2";
+            return divisor / dividend;
+        }
 
-            Comparer.Default.Compare(b, a); // Noncompliant
+        void doTheThing(int divisor, int dividend)
+        {
+            double result = divide(dividend, divisor);  // Noncompliant
         }
     }
 
-    public static class Extensions
+    public void M3()
     {
-        public static void Ex(this string self, string v1, string v2)
-//                         ^^ Secondary [6]
+        double divide(int divisor, int dividend) => divisor / dividend;
+
+        double doTheThing(int divisor, int dividend) => divide(dividend, divisor);  // Noncompliant
+    }
+}
+
+// See https://github.com/SonarSource/sonar-dotnet/issues/3879
+class NotOnlyNullableParam
+{
+    internal class A
+    {
+        public C Something { get; set; }
+
+        public WithValueProperty b { get; set; }
+    }
+
+    internal class C
+    {
+        public int b { get; set; }
+    }
+
+    internal class WithValueProperty
+    {
+        public int Value { get; set; }
+    }
+
+    void NotNullableParamVoid(int a, int b) // Secondary [D,E,F,G,H,I]
+    {
+        // Do nothing
+    }
+
+    void NullableParamValueVoid(int q, Nullable<int> b)
+    {
+        if (b.HasValue)
         {
-        }
-        public static void Ex(this string self, string v1, string v2, int x)
-        {
-            Ex(self, v1, v2);
-            self.Ex(v1, v2);
-            Extensions.Ex(self, v1, v2);
-            Tests.Diagnostics.Extensions.Ex(self, v1, v2);
+            NotNullableParamVoid(b.Value, q); // Noncompliant [D]
+            NotNullableParamVoid(q, b.Value); // Compliant
         }
     }
 
-    public partial class ParametersCorrectOrder
+    void NullableParamCastVoid(int q, int? b)
     {
-        partial void divide(int divisor, int someOther, int dividend, int p = 10, int some = 5, int other2 = 7);
-//                   ^^^^^^ Secondary [1,2,3,4]
-    }
-
-    public partial class ParametersCorrectOrder
-    {
-        partial void divide(int a, int b, int c, int p, int other, int other2)
+        if (b.HasValue)
         {
-            var x = a / b;
-        }
-
-        public void m(int a, int b) // Secondary [5,7]
-        {
-        }
-
-        public void n(int a, int b, int c)
-        {
-        }
-
-        public void doTheThing()
-        {
-            int divisor = 15;
-            int dividend = 5;
-            var something = 6;
-            var someOther = 6;
-            var other2 = 6;
-            var some = 6;
-
-            divide(dividend, 1 + 1, divisor, other2: 6);  // Noncompliant [1] operation succeeds, but result is unexpected
-
-            divide(divisor, other2, dividend);
-            divide(divisor, other2, dividend, other2: someOther); // Noncompliant [2] {{Parameters to 'divide' have the same names but not the same order as the method arguments.}}
-
-            divide(divisor, someOther, dividend, other2: some, some: other2); // Noncompliant [3]
-
-            divide(1, 1, 1, other2: some, some: other2); // Noncompliant [4]
-            divide(1, 1, 1, other2: 1, some: other2);
-
-            int a=5, b=6;
-
-            m(1, a); // Noncompliant [7] Argument "a" may be first
-            m(1, b);
-            m(a, a);
-            m(divisor, dividend);
-
-            m(a, b);
-            m(b, b); // Compliant
-            m(b, a); // Noncompliant [5]
-
-            var v1 = "";
-            var v2 = "";
-
-            "aaaaa".Ex(v1, v2);
-            "aaaaa".Ex(v2, v1); // Noncompliant [6]
-
-            int c = 1;
-            n(a, a, a);
-            n(b, b, b);
-            n(c, c, c);
-        }
-    }
-
-    public class A
-    {
-        public class B
-        {
-            public class C
-            {
-                public C(string left, string right) { } // Secondary [C]
-            }
-        }
-    }
-
-    public class Foo
-    {
-        public Foo(string left, string right) { } // Secondary [B]
-
-        public void Method(string left, string right) { } // Secondary [A]
-
-        public void Bar()
-        {
-            var left = "valueLeft";
-            var right = "valueRight";
-
-            Method(left, right);
-            Method(right, left); // Noncompliant [A]
-
-            var foo1 = new Foo(left, right);
-            var foo2 = new Foo(right, left); // Noncompliant [B]
-
-            var c1 = new A.B.C(left, right);
-            var c2 = new A.B.C(right, left); // Noncompliant [C]
-        }
-    }
-
-    class Program
-    {
-        void Struct(DateTime a, string b)
-        {
-            Bar1(a, b); // Compliant
-        }
-
-        void ClassAndInterface(Boo a, string b)
-        {
-            Bar2(a, b); // Compliant
-            Bar3(a, b); // Compliant
-            Bar3(b: a, a: b); // Compliant
-        }
-        void Bar1(DateTime b, string a) { }
-        void Bar2(Boo b, string a) { }
-        void Bar3(IBoo b, string a) { }
-    }
-    interface IBoo { }
-    class Boo : IBoo { }
-
-    class WithLocalFunctions
-    {
-        public void M1()
-        {
-            static double divide(int divisor, int dividend)
-            {
-                return divisor / dividend;
-            }
-
-            static void doTheThing(int divisor, int dividend)
-            {
-                double result = divide(dividend, divisor);  // Noncompliant
-            }
-        }
-
-        public void M2()
-        {
-            double divide(int divisor, int dividend)
-            {
-                return divisor / dividend;
-            }
-
-            void doTheThing(int divisor, int dividend)
-            {
-                double result = divide(dividend, divisor);  // Noncompliant
-            }
-        }
-
-        public void M3()
-        {
-            double divide(int divisor, int dividend) => divisor / dividend;
-
-            double doTheThing(int divisor, int dividend) => divide(dividend, divisor);  // Noncompliant
-        }
-    }
-
-    // See https://github.com/SonarSource/sonar-dotnet/issues/3879
-    class NotOnlyNullableParam
-    {
-        internal class A
-        {
-            public C Something { get; set; }
-
-            public B b { get; set; }
-        }
-
-        internal class C
-        {
-            public int b { get; set; }
-        }
-
-        internal class B
-        {
-            public int Value { get; set; }
-        }
-
-        void NotNullableParamVoid(int a, int b) // Secondary [D,E,F,G,H,I]
-        {
-            // Do nothing
-        }
-
-        void NullableParamValueVoid(int q, Nullable<int> b)
-        {
-            if (b.HasValue)
-            {
-                NotNullableParamVoid(b.Value, q); // Noncompliant [D]
-                NotNullableParamVoid(q, b.Value); // Compliant
-            }
-        }
-
-        void NullableParamCastVoid(int q, int? b)
-        {
-            if (b.HasValue)
-            {
-                NotNullableParamVoid((int)b, q); // Noncompliant [E]
-                NotNullableParamVoid(q, (int)b); // Compliant
-            }
-        }
-
-        void InnerPropertyParamVoid(int q, A c)
-        {
-            NotNullableParamVoid(c.Something.b, q); // Noncompliant [F]
-            NotNullableParamVoid(q, c.Something.b); // Compliant
-        }
-
-        void InnerPropertyValueParamVoid(int q, A c)
-        {
-            NotNullableParamVoid(c.b.Value, q); // Compliant Value is real propery
-        }
-
-        void InnerPropertyValueParamVoid(int c, B b)
-        {
-            NotNullableParamVoid(b.Value, c); // Compliant Value is real propery
-            NotNullableParamVoid(c, b.Value); // Compliant Value is real propery
-        }
-
-        void ObjectParamCastVoid(int q, object b)
-        {
-            NotNullableParamVoid((int)b, q); // Noncompliant [G]
+            NotNullableParamVoid((int)b, q); // Noncompliant [E]
             NotNullableParamVoid(q, (int)b); // Compliant
+            NotNullableParamVoid(b as int, q);   
         }
+    }
 
-        void ObjectParamNullableCastVoid(int q, object b)
-        {
-            NotNullableParamVoid(((int?)b).Value, q); // Noncompliant [H]
-            NotNullableParamVoid(q, ((int?)b).Value); // Compliant
-        }
+    void InnerPropertyParamVoid(int q, A c)
+    {
+        NotNullableParamVoid(c.Something.b, q); // Noncompliant [F]
+        NotNullableParamVoid(q, c.Something.b); // Compliant
+    }
 
-        void DifferentCaseParamsVoid(int a, int B)
-        {
-            NotNullableParamVoid(B, a); // Noncompliant [I]
-            NotNullableParamVoid(a, B); // Compliant
-        }
+    void InnerPropertyValueParamVoid(int q, A c)
+    {
+        NotNullableParamVoid(c.b.Value, q); // Compliant Value is real propery
+    }
+
+    void InnerPropertyValueParamVoid(int c, WithValueProperty b)
+    {
+        NotNullableParamVoid(b.Value, c); // Compliant Value is real propery
+        NotNullableParamVoid(c, b.Value); // Compliant Value is real propery
+    }
+
+    void ObjectParamCastVoid(int q, object b)
+    {
+        NotNullableParamVoid((int)b, q); // Noncompliant [G]
+        NotNullableParamVoid(q, (int)b); // Compliant
+    }
+
+    void ObjectParamNullableCastVoid(int q, object b)
+    {
+        NotNullableParamVoid(((int?)b).Value, q); // Noncompliant [H]
+        NotNullableParamVoid(q, ((int?)b).Value); // Compliant
+    }
+
+    void DifferentCaseParamsVoid(int a, int B)
+    {
+        NotNullableParamVoid(B, a); // Noncompliant [I]
+        NotNullableParamVoid(a, B); // Compliant
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ParametersCorrectOrder.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ParametersCorrectOrder.vb
@@ -1,134 +1,130 @@
-﻿Imports System
+﻿Public Class Foo
+    Public Shared Property ValProp As Integer
 
-Namespace Tests.TestCases
-    Public Class Foo
-        Public Shared Property ValProp As Integer
+    Sub New(ByVal left As Integer, ByVal right As Integer)
 
+    End Sub
+
+    Sub New()
+    End Sub
+
+    Public Sub Invocations()
+        Dim divisor = 15
+        Dim dividend = 5
+
+        Divide(divisor, dividend)
+        Divide(dividend, dividend)
+        Divide(divisor, divisor)
+        Foo.GlobalDivide(divisor, dividend)
+
+        Divide(dividend, divisor) ' Noncompliant [1] {{Parameters to 'Divide' have the same names but not the same order as the method arguments.}}
+        Foo.GlobalDivide(dividend, divisor) ' Noncompliant
+
+        Divide(dividend:=dividend, divisor:=divisor)
+
+        Divide(ValProp, Foo.ValProp)
+
+        FooBar(1)
+        FooBar(1, "a", "b")
+        FooBar(value:=1)
+    End Sub
+
+    Public Sub ObjectCreations()
+        Dim left = 1
+        Dim right = 2
+
+        Dim x = New Foo(left, right)
+        x = New Foo(left, left)
+        x = New Foo(right, right)
+        x = New Foo(right:=right, left:=left)
+        x = New Foo
+
+        x = New Foo(right, left) ' Noncompliant
+        x = New FooFoo(right, left) ' Noncompliant
+        x = New Foo.FooFoo(right, left) ' Noncompliant
+
+        x = New Foo(ValProp, Foo.ValProp)
+    End Sub
+
+    Sub Bar(ByVal name As String, Optional ByVal age As Short = 0)
+    End Sub
+
+    Sub FooBar(ByVal value As Integer, ByVal ParamArray args() As String)
+    End Sub
+
+    Public Function Divide(ByVal divisor As Integer, ByVal dividend As Integer) As Double ' Should be secondary-location for [1], but does not work
+        Return divisor / dividend
+    End Function
+
+    Public Shared Function GlobalDivide(ByVal divisor As Integer, ByVal dividend As Integer) As Double
+        Return divisor / dividend
+    End Function
+
+    Public Class FooFoo
+        Inherits Foo
         Sub New(ByVal left As Integer, ByVal right As Integer)
-
+            MyBase.New(right, left) ' Noncompliant
         End Sub
+    End Class
+End Class
 
-        Sub New()
-        End Sub
+' See https://github.com/SonarSource/sonar-dotnet/issues/3879
+Public Class NotOnlyNullableParam
 
-        Public Sub Invocations()
-            Dim divisor = 15
-            Dim dividend = 5
-
-            Divide(divisor, dividend)
-            Divide(dividend, dividend)
-            Divide(divisor, divisor)
-            Foo.GlobalDivide(divisor, dividend)
-
-            Divide(dividend, divisor) ' Noncompliant [1] {{Parameters to 'Divide' have the same names but not the same order as the method arguments.}}
-            Foo.GlobalDivide(dividend, divisor) ' Noncompliant
-
-            Divide(dividend:=dividend, divisor:=divisor)
-
-            Divide(ValProp, Foo.ValProp)
-
-            FooBar(1)
-            FooBar(1, "a", "b")
-            FooBar(value:=1)
-        End Sub
-
-        Public Sub ObjectCreations()
-            Dim left = 1
-            Dim right = 2
-
-            Dim x = New Foo(left, right)
-            x = New Foo(left, left)
-            x = New Foo(right, right)
-            x = New Foo(right:=right, left:=left)
-            x = New Foo
-
-            x = New Foo(right, left) ' Noncompliant
-            x = New FooFoo(right, left) ' Noncompliant
-            x = New Foo.FooFoo(right, left) ' Noncompliant
-
-            x = New Foo(ValProp, Foo.ValProp)
-        End Sub
-
-        Sub Bar(ByVal name As String, Optional ByVal age As Short = 0)
-        End Sub
-
-        Sub FooBar(ByVal value As Integer, ByVal ParamArray args() As String)
-        End Sub
-
-        Public Function Divide(ByVal divisor As Integer, ByVal dividend As Integer) As Double ' Should be secondary-location for [1], but does not work
-            Return divisor / dividend
-        End Function
-
-        Public Shared Function GlobalDivide(ByVal divisor As Integer, ByVal dividend As Integer) As Double
-            Return divisor / dividend
-        End Function
-
-        Public Class FooFoo
-            Inherits Foo
-            Sub New(ByVal left As Integer, ByVal right As Integer)
-                MyBase.New(right, left) ' Noncompliant
-            End Sub
-        End Class
+    Public Class A
+        Public Property Something As C
     End Class
 
-    ' See https://github.com/SonarSource/sonar-dotnet/issues/3879
-    Public Class NotOnlyNullableParam
+    Public Class C
+        Public Property b As Integer
+    End Class
 
-        Public Class A
-            Public Property Something As C
-        End Class
+    Public Class WithValueProperty
+        Public Property Value As Integer
+    End Class
 
-        Public Class C
-            Public Property b As Integer
-        End Class
+    Sub NotNullableParamVoid(ByVal a As Integer, ByVal b As Integer)
+        ' Do nothing
+    End Sub
 
-        Public Class B
-            Public Property Value As Integer
-        End Class
+    Sub NullableParamValueVoid(ByVal a As Integer, ByVal b As Nullable(Of Integer))
 
-        Sub NotNullableParamVoid(ByVal a As Integer, ByVal b As Integer)
-            ' Do nothing
-        End Sub
-
-        Sub NullableParamValueVoid(ByVal a As Integer, ByVal b As Nullable(Of Integer))
-
-            If b.HasValue Then
-                NotNullableParamVoid(b.Value, a) ' Noncompliant
-                NotNullableParamVoid(a, b.Value) ' Compliant
-            End If
-        End Sub
+        If b.HasValue Then
+            NotNullableParamVoid(b.Value, a) ' Noncompliant
+            NotNullableParamVoid(a, b.Value) ' Compliant
+        End If
+    End Sub
 
 
-        Sub NullableParamCastVoid(ByVal a As Integer, ByVal b As Integer?)
-            If b.HasValue Then
-                NotNullableParamVoid(CInt(b), a) ' Noncompliant
-                NotNullableParamVoid(a, CInt(b)) ' Compliant
-            End If
-        End Sub
-
-        Sub InnerPropertyParamVoid(ByVal a As Integer, ByRef c As A)
-            NotNullableParamVoid(c.Something.b, a) ' Noncompliant
-            NotNullableParamVoid(a, c.Something.b) ' Compliant
-        End Sub
-
-        Sub InnerPropertyParamVoid(ByVal c As Integer, ByRef b As B)
-            NotNullableParamVoid(b.Value, c) ' Compliant
-            NotNullableParamVoid(c, b.Value) ' Compliant
-        End Sub
-
-        Sub ObjectParamCastVoid(ByVal a As Integer, ByVal b As Object)
+    Sub NullableParamCastVoid(ByVal a As Integer, ByVal b As Integer?)
+        If b.HasValue Then
             NotNullableParamVoid(CInt(b), a) ' Noncompliant
             NotNullableParamVoid(a, CInt(b)) ' Compliant
-        End Sub
+        End If
+    End Sub
 
-        Sub ObjectParamNullableCastVoid(ByVal a As Integer, ByVal b As Object)
-            NotNullableParamVoid(DirectCast(b, Integer?).Value, a) ' Noncompliant
-            NotNullableParamVoid(a, DirectCast(b, Integer?).Value) ' Compliant
-        End Sub
+    Sub InnerPropertyParamVoid(ByVal a As Integer, ByRef c As A)
+        NotNullableParamVoid(c.Something.b, a) ' Noncompliant
+        NotNullableParamVoid(a, c.Something.b) ' Compliant
+    End Sub
 
-        Sub DifferentCaseParamsVoid(ByVal a As Integer, ByVal B As Integer)
-            NotNullableParamVoid(B, a) ' Noncompliant
-            NotNullableParamVoid(a, B) ' Compliant
-        End Sub
-    End Class
-End Namespace
+    Sub InnerPropertyParamVoid(ByVal c As Integer, ByRef b As WithValueProperty)
+        NotNullableParamVoid(b.Value, c) ' Compliant
+        NotNullableParamVoid(c, b.Value) ' Compliant
+    End Sub
+
+    Sub ObjectParamCastVoid(ByVal a As Integer, ByVal b As Object)
+        NotNullableParamVoid(CInt(b), a) ' Noncompliant
+        NotNullableParamVoid(a, CInt(b)) ' Compliant
+    End Sub
+
+    Sub ObjectParamNullableCastVoid(ByVal a As Integer, ByVal b As Object)
+        NotNullableParamVoid(DirectCast(b, Integer?).Value, a) ' Noncompliant
+        NotNullableParamVoid(a, DirectCast(b, Integer?).Value) ' Compliant
+    End Sub
+
+    Sub DifferentCaseParamsVoid(ByVal a As Integer, ByVal B As Integer)
+        NotNullableParamVoid(B, a) ' Noncompliant
+        NotNullableParamVoid(a, B) ' Compliant
+    End Sub
+End Class
