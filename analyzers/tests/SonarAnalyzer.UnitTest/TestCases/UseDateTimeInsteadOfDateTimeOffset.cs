5567a55a2c493836b3610dcda2a37280c878f863
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-06-30 16:37:39 +0200

    Add UTs

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseDateTimeInsteadOfDateTimeOffset.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseDateTimeInsteadOfDateTimeOffset.cs
@@ -1,5 +1,100 @@
 ﻿using System;
+using System.Globalization;
 
 public class Program
 {
+    void Constructors()
+    {
+        _ = new DateTime(1);                                                                 // Noncompliant
+        _ = new DateTime(1, 1, 1);                                                           // Noncompliant
+        _ = new DateTime(1, 1, 1, new GregorianCalendar());                                  // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1);                                                  // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, new GregorianCalendar());                         // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, DateTimeKind.Utc);                                // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1);                                               // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, new GregorianCalendar());                      // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, DateTimeKind.Utc);                             // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, new GregorianCalendar(), DateTimeKind.Utc);    // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, 1, new GregorianCalendar());                   // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, 1, DateTimeKind.Utc);                          // Noncompliant
+        _ = new DateTime(1, 1, 1, 1, 1, 1, 1, 1, new GregorianCalendar(), DateTimeKind.Utc); // Noncompliant
+    }
+
+    void Fields()
+    {
+        _ = DateTime.MaxValue;  // Noncompliant
+        _ = DateTime.MinValue;  // Noncompliant
+        _ = DateTime.UnixEpoch; // Noncompliant
+    }
+
+    void Properties(DateTime date)
+    {
+        _ = date.Date;
+        _ = date.Day;
+        _ = date.DayOfWeek;
+        _ = date.DayOfYear;
+        _ = date.Hour;
+        _ = date.Kind;
+        _ = date.Microsecond;
+        _ = date.Millisecond;
+        _ = date.Minute;
+        _ = date.Month;
+        _ = date.Nanosecond;
+        _ = date.Second;
+        _ = date.Ticks;
+        _ = date.TimeOfDay;
+        _ = date.Year;
+    }
+
+    void StaticProperties()
+    {
+        _ = DateTime.Now; // Noncompliant
+        _ = DateTime.Today; // Noncompliant
+        _ = DateTime.UtcNow; // Noncompliant
+    }
+
+    void Methods(DateTime date, Span<char> span)
+    {
+        date.Add(TimeSpan.Zero);
+        date.AddDays(0);
+        date.AddHours(0);
+        date.AddMicroseconds(0);
+        date.AddMilliseconds(0);
+        date.AddMinutes(0);
+        date.AddMonths(0);
+        date.AddSeconds(0);
+        date.AddTicks(0);
+        date.AddYears(0);
+        date.CompareTo(date);
+        DateTime.Compare(date, date);
+        DateTime.DaysInMonth(1, 1);
+        DateTime.Equals(date, date);
+        date.Equals(date);
+        DateTime.FromBinary(1);
+        DateTime.FromFileTime(1);
+        DateTime.FromFileTimeUtc(1);
+        DateTime.FromOADate(1);
+        date.GetDateTimeFormats('a');
+        date.GetHashCode();
+        date.GetTypeCode();
+        date.IsDaylightSavingTime();
+        DateTime.IsLeapYear(1);
+        DateTime.Parse("06/01/1993");                          // Noncompliant
+        DateTime.ParseExact("06/01/1993", "dd/MM/yyyy", null); // Noncompliant
+        DateTime.SpecifyKind(date, DateTimeKind.Local);        // Noncompliant
+        date.Subtract(date);
+        date.ToBinary();
+        date.ToFileTime();
+        date.ToFileTimeUtc();
+        date.ToLocalTime();
+        date.ToLongDateString();
+        date.ToLongTimeString();
+        date.ToShortDateString();
+        date.ToShortTimeString();
+        date.ToString();
+        date.ToUniversalTime();
+        date.TryFormat(span, out int myInt);
+        DateTime.TryParse("06/01/1993", out date);                                                                                    // Noncompliant
+        DateTime.TryParseExact("06/01/1993", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, out date); // Noncompliant
+    }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseDateTimeInsteadOfDateTimeOffset.vb
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseDateTimeInsteadOfDateTimeOffset.vb
@@ -1,7 +1,95 @@
-﻿Public Class Program
+﻿Imports System
+Imports System.Globalization
 
-    Public Sub Test()
-
+Public Class Program
+    Private Sub Constructors()
+        __ = New DateTime(1)                                                                 ' Noncompliant
+        __ = New DateTime(1, 1, 1)                                                           ' Noncompliant
+        __ = New DateTime(1, 1, 1, New GregorianCalendar())                                  ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1)                                                  ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, New GregorianCalendar())                         ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, DateTimeKind.Utc)                                ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1)                                               ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, New GregorianCalendar())                      ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, DateTimeKind.Utc)                             ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, New GregorianCalendar(), DateTimeKind.Utc)    ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, 1, New GregorianCalendar())                   ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, 1, DateTimeKind.Utc)                          ' Noncompliant
+        __ = New DateTime(1, 1, 1, 1, 1, 1, 1, 1, New GregorianCalendar(), DateTimeKind.Utc) ' Noncompliant
     End Sub
 
+    Private Sub Fields()
+        __ = Date.MaxValue  ' Noncompliant
+        __ = Date.MinValue  ' Noncompliant
+        __ = Date.UnixEpoch ' Noncompliant
+    End Sub
+
+    Private Sub Properties(ByVal [date] As Date)
+        __ = [date].Date
+        __ = [date].Day
+        __ = [date].DayOfWeek
+        __ = [date].DayOfYear
+        __ = [date].Hour
+        __ = [date].Kind
+        __ = [date].Microsecond
+        __ = [date].Millisecond
+        __ = [date].Minute
+        __ = [date].Month
+        __ = [date].Nanosecond
+        __ = [date].Second
+        __ = [date].Ticks
+        __ = [date].TimeOfDay
+        __ = [date].Year
+    End Sub
+
+    Private Sub StaticProperties()
+        __ = Date.Now ' Noncompliant
+        __ = Date.Today ' Noncompliant
+        __ = Date.UtcNow ' Noncompliant
+    End Sub
+
+    Private Sub Methods(ByVal [date] As Date, ByVal span As Span(Of Char))
+        [date].Add(TimeSpan.Zero)
+        [date].AddDays(0)
+        [date].AddHours(0)
+        [date].AddMicroseconds(0)
+        [date].AddMilliseconds(0)
+        [date].AddMinutes(0)
+        [date].AddMonths(0)
+        [date].AddSeconds(0)
+        [date].AddTicks(0)
+        [date].AddYears(0)
+        [date].CompareTo([date])
+        Date.Compare([date], [date])
+        Date.DaysInMonth(1, 1)
+        Date.Equals([date], [date])
+        [date].Equals([date])
+        Date.FromBinary(1)
+        Date.FromFileTime(1)
+        Date.FromFileTimeUtc(1)
+        Date.FromOADate(1)
+        [date].GetDateTimeFormats("a"c)
+        [date].GetHashCode()
+        [date].GetTypeCode()
+        [date].IsDaylightSavingTime()
+        Date.IsLeapYear(1)
+        Date.Parse("06/01/1993")                          ' Noncompliant
+        Date.ParseExact("06/01/1993", "dd/MM/yyyy", Nothing) ' Noncompliant
+        Date.SpecifyKind([date], DateTimeKind.Local)        ' Noncompliant
+        [date].Subtract([date])
+        [date].ToBinary()
+        [date].ToFileTime()
+        [date].ToFileTimeUtc()
+        [date].ToLocalTime()
+        [date].ToLongDateString()
+        [date].ToLongTimeString()
+        [date].ToShortDateString()
+        [date].ToShortTimeString()
+        [date].ToString()
+        [date].ToUniversalTime()
+        Dim myInt As Integer = Nothing
+        [date].TryFormat(span, myInt)
+        Date.TryParse("06/01/1993", [date])                                                                                    ' Noncompliant
+        Date.TryParseExact("06/01/1993", "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.AdjustToUniversal, [date]) ' Noncompliant
+    End Sub
 End Class
