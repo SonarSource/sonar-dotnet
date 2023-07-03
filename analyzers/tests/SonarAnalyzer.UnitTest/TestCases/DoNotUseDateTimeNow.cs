0dabd1e2bf9d99e8fafe6f04956e957c5bbb9654
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2023-07-03 09:19:51 +0200

    Basic test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotUseDateTimeNow.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DoNotUseDateTimeNow.cs
@@ -1,5 +1,28 @@
 ﻿using System;
+using static System.DateTime;
+using AliasedDateTime = System.DateTime;
 
-public class Program
+class Test
 {
+    void TestCases()
+    {
+        var currentTime = DateTime.Now; // Noncompliant {{Do not use 'DateTime.Now' for recording DateTime instants}}
+        //                ^^^^^^^^^^^^
+        currentTime = System.DateTime.Now; // Noncompliant
+        currentTime = Now; // Noncompliant
+        currentTime = AliasedDateTime.Now; // Noncompliant
+    }
+}
+
+class CustomTypeCalledDateTime
+{
+    public struct DateTime
+    {
+        public static DateTime Now => new DateTime();
+    }
+
+    CustomTypeCalledDateTime()
+    {
+        _ = DateTime.Now;                               // Compliant - this is not System.DateTime
+    }
 }
