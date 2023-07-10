5cb7f164c94d016e26c23e15fc72823c5c39df79
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-07-10 13:44:56 +0200

    just copy test files

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/InitializationVectorShouldBeRandom.CSharp11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+public class Sample
+{
+    public void SymetricAlgorithmCreateEncryptor()
+    {
+        var initializationVectorConstant = new byte[16];
+
+        using (SymmetricAlgorithm sa = SymmetricAlgorithm.Create("AES"))
+        {
+            sa.GenerateKey();
+            var generateIVNotCalled = sa.CreateEncryptor(sa.Key, sa.IV);
+            var constantVector2 = sa.CreateEncryptor(sa.Key, "1234567890123456"u8.ToArray()); // FN
+        }
+    }
+}
