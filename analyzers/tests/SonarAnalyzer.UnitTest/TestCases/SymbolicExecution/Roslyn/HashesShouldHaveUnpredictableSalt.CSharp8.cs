cffaa9cfed6a37cc907354bb9bfae8ced3f78733
Zsolt Kolbay <121798625+zsolt-kolbay-sonarsource@users.noreply.github.com>
2023-07-11 10:49:02 +0200

    Migrate S2053: Scaffold the rule for the new SE (#7567)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SymbolicExecution/Roslyn/HashesShouldHaveUnpredictableSalt.CSharp8.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+public class Sample
+{
+    public void Conditional(int arg, string password, byte[] passwordBytes)
+    {
+        var salt = new byte[16];
+
+        DeriveBytes e = arg switch
+        {
+            1 => new Rfc2898DeriveBytes(password, salt),        // FIXME Non-compliant
+            2 => new PasswordDeriveBytes(passwordBytes, salt),  // FIXME Non-compliant
+            _ => null
+        };
+    }
+}
+
+public interface InterfaceWithMethodImplementation
+{
+    public void Method()
+    {
+        var salt = new byte[16];
+        new Rfc2898DeriveBytes(password, salt),                 // FIXME Non-compliant
+    }
+}
