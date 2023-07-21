d7c59816e81b6838cbe28e7ebd5d189ae9e7b3d7
Čaba Šagi <csaba.sagi@sonarsource.com>
2023-07-21 13:42:39 +0200

    Move InterpolatedStringExpressionSyntaxExtensionsTests

--- analyzers/tests/SonarAnalyzer.UnitTest/Extensions/InterpolatedStringExpressionSyntaxExtensionsTests.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/InterpolatedStringExpressionSyntaxExtensionsTests.cs
@@ -18,16 +18,21 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using SonarAnalyzer.Extensions;
+extern alias csharp;
+extern alias vbnet;
+
+using CS = Microsoft.CodeAnalysis.CSharp;
+using CSSyntax = Microsoft.CodeAnalysis.CSharp.Syntax;
+using SyntaxNodeExtensionsCS = csharp::SonarAnalyzer.Extensions.InterpolatedStringExpressionSyntaxExtensions;
+using SyntaxNodeExtensionsVB = vbnet::SonarAnalyzer.Extensions.InterpolatedStringExpressionSyntaxExtensions;
+using VBSyntax = Microsoft.CodeAnalysis.VisualBasic.Syntax;
 
 namespace SonarAnalyzer.UnitTest.Extensions
 {
     [TestClass]
     public class InterpolatedStringExpressionSyntaxExtensionsTests
     {
-        private const string CodeSnipet = @"
+        private const string CodeSnipetCS = @"
 public class C
 {{
     public void M(int notConstant, string notConstantString)
@@ -38,6 +43,17 @@ public class C
     string Foo() => ""x"";
 }}";
 
+        private const string CodeSnipetVB = @"
+Public Class C
+        Public Sub M(ByVal notConstant As Integer, ByVal notConstantString As String)
+            {0}
+        End Sub
+
+        Private Function Foo() As String
+            Return ""x""
+        End Function
+End Class";
+
         [DataTestMethod]
         [DataRow(@"var methodCall = $""{Foo()}"";")]
         [DataRow(@"var nestedMethodCall = $""{$""{$""{Foo()}""}""}"";")]
@@ -47,11 +63,27 @@ public class C
                  var mixConstantAndLiteral = $""TextValue {constant}"";")]
         [DataRow(@"const int constant = 1;
                  var mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}"";")]
-        public void TryGetGetInterpolatedTextValue_UnsupportedSyntaxKinds_ReturnsFalse(string code)
+        public void TryGetGetInterpolatedTextValue_UnsupportedSyntaxKinds_ReturnsFalse_CS(string code)
         {
-            var codeSnipet = string.Format(CodeSnipet, code);
-            var (expression, semanticModel) = Compile(codeSnipet);
-            expression.TryGetInterpolatedTextValue(semanticModel, out var interpolatedValue).Should().Be(false);
+            var codeSnipet = string.Format(CodeSnipetCS, code);
+            var (expression, semanticModel) = CompileCS(codeSnipet);
+            SyntaxNodeExtensionsCS.TryGetInterpolatedTextValue(expression, semanticModel, out var interpolatedValue).Should().Be(false);
+            interpolatedValue.Should().BeNull();
+        }
+
+        [DataTestMethod]
+        [DataRow(@"Dim methodCall = $""{Foo()}""")]
+        [DataRow(@"Dim nestedMethodCall = $""{$""{$""{Foo()}""}""}""")]
+        [DataRow(@"Const constant As Integer = 1
+                   Dim mixConstantNonConstant = $""{notConstant}{constant}""")]
+        [DataRow(@"Const constant As Integer = 1
+                   Dim mixConstantAndLiteral = $""TextValue {constant}""")]
+        [DataRow(@"Const constant As Integer = 1
+                   Dim mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}""")]
+        public void TryGetGetInterpolatedTextValue_UnsupportedSyntaxKinds_ReturnsFalse_VB(string methodBody)
+        {
+            var (expression, semanticModel) = CompileVB(methodBody);
+            SyntaxNodeExtensionsVB.TryGetInterpolatedTextValue(expression, semanticModel, out var interpolatedValue).Should().Be(false);
             interpolatedValue.Should().BeNull();
         }
 
@@ -75,24 +107,56 @@ public class C
                  string interpolatedString = $""{notConstantString}"";
                  ",
                  "SomeValue")]
-        public void TryGetGetInterpolatedTextValue_SupportedSyntaxKinds_ReturnsTrue(string code, string expectedTextValue)
+        public void TryGetGetInterpolatedTextValue_SupportedSyntaxKinds_ReturnsTrue_CS(string code, string expectedTextValue)
         {
-            var codeSnipet = string.Format(CodeSnipet, code);
-            var (expression, semanticModel) = Compile(codeSnipet);
-            expression.TryGetInterpolatedTextValue(semanticModel, out var interpolatedValue).Should().Be(true);
+            var codeSnipet = string.Format(CodeSnipetCS, code);
+            var (expression, semanticModel) = CompileCS(codeSnipet);
+            SyntaxNodeExtensionsCS.TryGetInterpolatedTextValue(expression, semanticModel, out var interpolatedValue).Should().Be(true);
             interpolatedValue.Should().Be(expectedTextValue);
         }
 
-        private static (InterpolatedStringExpressionSyntax InterpolatedStringExpression, SemanticModel SemanticModel) Compile(string code)
+        [DataTestMethod]
+        [DataRow(@"
+                   Dim textOnly = $""TextOnly""
+                 ",
+         "TextOnly")]
+        [DataRow(@"
+                    Const constantString As String = ""Foo""
+                    Dim constantInterpolation As String = $""{constantString} with text.""
+                 ",
+         "Foo with text.")]
+        [DataRow(@"
+                    Const constantString As String = ""Foo""
+                    Dim constantInterpolation As String = $""{$""Nested {constantString}""} with text.""",
+         "Nested Foo with text.")]
+        [DataRow(@"
+                    notConstantString = ""SomeValue""
+                    Dim interpolatedString As String = $""{notConstantString}""
+                 ",
+         "SomeValue")]
+        public void TryGetGetInterpolatedTextValue_SupportedSyntaxKinds_ReturnsTrue_VB(string methodBody, string expectedTextValue)
         {
-            var tree = CSharpSyntaxTree.ParseText(code);
-            var compilation = CSharpCompilation.Create("TempAssembly.dll")
+            var (expression, semanticModel) = CompileVB(methodBody);
+            SyntaxNodeExtensionsVB.TryGetInterpolatedTextValue(expression, semanticModel, out var interpolatedValue).Should().Be(true);
+            interpolatedValue.Should().Be(expectedTextValue);
+        }
+
+        private static (CSSyntax.InterpolatedStringExpressionSyntax InterpolatedStringExpression, SemanticModel SemanticModel) CompileCS(string code)
+        {
+            var tree = CS.CSharpSyntaxTree.ParseText(code);
+            var compilation = CS.CSharpCompilation.Create("TempAssembly.dll")
                                                .AddSyntaxTrees(tree)
                                                .AddReferences(MetadataReferenceFacade.ProjectDefaultReferences);
 
             var semanticModel = compilation.GetSemanticModel(tree);
 
-            return (tree.First<InterpolatedStringExpressionSyntax>(), semanticModel);
+            return (tree.First<CSSyntax.InterpolatedStringExpressionSyntax>(), semanticModel);
+        }
+
+        private static (VBSyntax.InterpolatedStringExpressionSyntax InterpolatedStringExpression, SemanticModel SemanticModel) CompileVB(string methodBody)
+        {
+            var (tree, model) = TestHelper.CompileVB(string.Format(CodeSnipetVB, methodBody));
+            return (tree.First<VBSyntax.InterpolatedStringExpressionSyntax>(), model);
         }
     }
 }
