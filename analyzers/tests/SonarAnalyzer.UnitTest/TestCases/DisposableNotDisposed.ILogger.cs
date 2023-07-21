120210b5edfaeaa8dc6e61e83caa7c9bb8de09ee
Martin Strecker <martin.strecker@sonarsource.com>
2023-07-21 09:53:18 +0200

    Add reproducer

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableNotDisposed.ILogger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Tests.Diagnostics
+{
+    public class DisposableNotDisposed_ILogger
+    {
+        public void Tests(ILogger logger)
+        {
+            var scope1 = logger.BeginScope("Test");        // FN. Non-compliant {{Dispose 'scope1' when it is no longer needed.}} 
+            using (var scope2 = logger.BeginScope("Test")) // Compliant
+            { };
+        }
+    }
+}
