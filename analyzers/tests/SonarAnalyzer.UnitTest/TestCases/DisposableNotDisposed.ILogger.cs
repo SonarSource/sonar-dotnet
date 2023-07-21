fc492d65652884ffe394ad0b0542770958b43c05
Martin Strecker <martin.strecker@sonarsource.com>
2023-07-21 09:59:41 +0200

    More test cases including extension methods

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableNotDisposed.ILogger.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableNotDisposed.ILogger.cs
@@ -7,9 +7,12 @@ namespace Tests.Diagnostics
     {
         public void Tests(ILogger logger)
         {
-            var scope1 = logger.BeginScope("Test");        // FN. Non-compliant {{Dispose 'scope1' when it is no longer needed.}} 
-            using (var scope2 = logger.BeginScope("Test")) // Compliant
+            var scope1 = logger.BeginScope("Test");                                // FN. Non-compliant {{Dispose 'scope1' when it is no longer needed.}} 
+            using (var scope2 = logger.BeginScope("Test"))                         // Compliant
             { };
+            var scope3 = logger.BeginScope<string>(null);                          // FN
+            var scope4 = logger.BeginScope("messageFormat", 1);                    // FN. extension method
+            var scope5 = LoggerExtensions.BeginScope(logger, "messageFormat", 1);  // FN. extension method
         }
     }
 }
