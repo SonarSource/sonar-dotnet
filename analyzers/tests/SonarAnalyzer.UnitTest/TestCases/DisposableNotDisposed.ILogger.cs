dfa0564a779a24d8877b7787591403e45f209116
Martin Strecker <martin.strecker@sonarsource.com>
2023-07-24 12:59:26 +0200

    Add more test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableNotDisposed.ILogger.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/DisposableNotDisposed.ILogger.cs
@@ -1,18 +1,33 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
 
-namespace Tests.Diagnostics
+public class DisposableNotDisposed_ILogger
 {
-    public class DisposableNotDisposed_ILogger
+    public void ILoggerTests(ILogger logger)
     {
-        public void Tests(ILogger logger)
+        var scope1 = logger.BeginScope("Test");                                // FN. Non-compliant {{Dispose 'scope1' when it is no longer needed.}} 
+        using (var scope2 = logger.BeginScope("Test"))                         // Compliant
+        { };
+        var scope3 = logger.BeginScope<string>(null);                          // FN
+        var scope4 = logger.BeginScope("messageFormat", 1);                    // FN. extension method
+        var scope5 = LoggerExtensions.BeginScope(logger, "messageFormat", 1);  // FN. extension method
+    }
+
+    public void ILoggerTCategoryNameTests(ILogger<DisposableNotDisposed_ILogger> logger)
+    {
+        var scope1 = logger.BeginScope("Test"); // FN
+    }
+
+    public void DisposedInFinally(ILogger logger)
+    {
+        IDisposable loggerScope = null;
+        try
         {
-            var scope1 = logger.BeginScope("Test");                                // FN. Non-compliant {{Dispose 'scope1' when it is no longer needed.}} 
-            using (var scope2 = logger.BeginScope("Test"))                         // Compliant
-            { };
-            var scope3 = logger.BeginScope<string>(null);                          // FN
-            var scope4 = logger.BeginScope("messageFormat", 1);                    // FN. extension method
-            var scope5 = LoggerExtensions.BeginScope(logger, "messageFormat", 1);  // FN. extension method
+            loggerScope = logger.BeginScope("Test"); // Compliant
+        }
+        finally
+        {
+            loggerScope.Dispose();
         }
     }
 }
