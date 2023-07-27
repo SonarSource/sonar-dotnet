59ba61894ffb78b5c8f09fde2064ff105c368181
Čaba Šagi <csaba.sagi@sonarsource.com>
2023-07-27 10:21:02 +0200

    Make test cases single line

--- analyzers/tests/SonarAnalyzer.UnitTest/Extensions/InterpolatedStringExpressionSyntaxExtensionsTests.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/InterpolatedStringExpressionSyntaxExtensionsTests.cs
@@ -57,12 +57,9 @@ End Class";
     [DataTestMethod]
     [DataRow(@"var methodCall = $""{Foo()}"";")]
     [DataRow(@"var nestedMethodCall = $""{$""{$""{Foo()}""}""}"";")]
-    [DataRow(@"const int constant = 1;
-                 var mixConstantNonConstant = $""{notConstant}{constant}"";")]
-    [DataRow(@"const int constant = 1;
-                 var mixConstantAndLiteral = $""TextValue {constant}"";")]
-    [DataRow(@"const int constant = 1;
-                 var mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}"";")]
+    [DataRow(@"const int constant = 1; var mixConstantNonConstant = $""{notConstant}{constant}"";")]
+    [DataRow(@"const int constant = 1; var mixConstantAndLiteral = $""TextValue {constant}"";")]
+    [DataRow(@"const int constant = 1; var mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}"";")]
     public void TryGetGetInterpolatedTextValue_UnsupportedSyntaxKinds_ReturnsFalse_CS(string code)
     {
         var codeSnipet = string.Format(CodeSnipetCS, code);
@@ -74,12 +71,9 @@ End Class";
     [DataTestMethod]
     [DataRow(@"Dim methodCall = $""{Foo()}""")]
     [DataRow(@"Dim nestedMethodCall = $""{$""{$""{Foo()}""}""}""")]
-    [DataRow(@"Const constant As Integer = 1
-                   Dim mixConstantNonConstant = $""{notConstant}{constant}""")]
-    [DataRow(@"Const constant As Integer = 1
-                   Dim mixConstantAndLiteral = $""TextValue {constant}""")]
-    [DataRow(@"Const constant As Integer = 1
-                   Dim mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}""")]
+    [DataRow(@"Const constant As Integer = 1 : Dim mixConstantNonConstant = $""{notConstant}{constant}""")]
+    [DataRow(@"Const constant As Integer = 1 : Dim mixConstantAndLiteral = $""TextValue {constant}""")]
+    [DataRow(@"Const constant As Integer = 1 : Dim mix = $""{constant}{$""{Foo()}""}{""{notConstant}""}""")]
     public void TryGetGetInterpolatedTextValue_UnsupportedSyntaxKinds_ReturnsFalse_VB(string methodBody)
     {
         var (expression, semanticModel) = CompileVB(methodBody);
@@ -88,25 +82,10 @@ End Class";
     }
 
     [DataTestMethod]
-    [DataRow(@"
-                   var textOnly = $""TextOnly"";
-                 ",
-             "TextOnly")]
-    [DataRow(@"
-                    const string constantString = ""Foo"";
-                    const string constantInterpolation = $""{constantString} with text."";
-                 ",
-             "Foo with text.")]
-    [DataRow(@"
-                    const string constantString = ""Foo"";
-                    const string constantInterpolation = $""{$""Nested {constantString}""} with text."";
-                 ",
-             "Nested Foo with text.")]
-    [DataRow(@"
-                    notConstantString = ""SomeValue"";
-                 string interpolatedString = $""{notConstantString}"";
-                 ",
-             "SomeValue")]
+    [DataRow(@"var textOnly = $""TextOnly"";", "TextOnly")]
+    [DataRow(@"const string constantString = ""Foo""; const string constantInterpolation = $""{constantString} with text."";", "Foo with text.")]
+    [DataRow(@"const string constantString = ""Foo""; const string constantInterpolation = $""{$""Nested {constantString}""} with text."";", "Nested Foo with text.")]
+    [DataRow(@"notConstantString = ""SomeValue""; string interpolatedString = $""{notConstantString}"";", "SomeValue")]
     public void TryGetGetInterpolatedTextValue_SupportedSyntaxKinds_ReturnsTrue_CS(string code, string expectedTextValue)
     {
         var codeSnipet = string.Format(CodeSnipetCS, code);
@@ -116,24 +95,10 @@ End Class";
     }
 
     [DataTestMethod]
-    [DataRow(@"
-                   Dim textOnly = $""TextOnly""
-                 ",
-     "TextOnly")]
-    [DataRow(@"
-                    Const constantString As String = ""Foo""
-                    Dim constantInterpolation As String = $""{constantString} with text.""
-                 ",
-     "Foo with text.")]
-    [DataRow(@"
-                    Const constantString As String = ""Foo""
-                    Dim constantInterpolation As String = $""{$""Nested {constantString}""} with text.""",
-     "Nested Foo with text.")]
-    [DataRow(@"
-                    notConstantString = ""SomeValue""
-                    Dim interpolatedString As String = $""{notConstantString}""
-                 ",
-     "SomeValue")]
+    [DataRow(@"Dim textOnly = $""TextOnly""", "TextOnly")]
+    [DataRow(@"Const constantString As String = ""Foo"" : Dim constantInterpolation As String = $""{constantString} with text.""", "Foo with text.")]
+    [DataRow(@"Const constantString As String = ""Foo"" : Dim constantInterpolation As String = $""{$""Nested {constantString}""} with text.""", "Nested Foo with text.")]
+    [DataRow(@"notConstantString = ""SomeValue"" : Dim interpolatedString As String = $""{notConstantString}""", "SomeValue")]
     public void TryGetGetInterpolatedTextValue_SupportedSyntaxKinds_ReturnsTrue_VB(string methodBody, string expectedTextValue)
     {
         var (expression, semanticModel) = CompileVB(methodBody);
