021764b3eb0f08ceb2ca9f746377fb709a86df3a
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-08-02 10:14:24 +0200

    Remove unused using

--- analyzers/src/SonarAnalyzer.Common/Rules/StringConcatenationInLoopBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/StringConcatenationInLoopBase.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Linq.Expressions;
-
 namespace SonarAnalyzer.Rules
 {
     public abstract class StringConcatenationInLoopBase<TSyntaxKind, TAssignmentExpression, TBinaryExpression> : SonarDiagnosticAnalyzer<TSyntaxKind>
