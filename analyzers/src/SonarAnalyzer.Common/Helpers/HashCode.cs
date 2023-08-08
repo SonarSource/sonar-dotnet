2555792f01df86b876d5bd6810dd6c43f53632f3
Martin Strecker <martin.strecker@sonarsource.com>
2023-08-08 08:50:44 +0200

    Add PerformanceSensitive to DictionaryContentHash

--- analyzers/src/SonarAnalyzer.Common/Helpers/HashCode.cs
+++ analyzers/src/SonarAnalyzer.Common/Helpers/HashCode.cs
@@ -19,6 +19,7 @@
  */
 
 using System.Runtime.CompilerServices;
+using Roslyn.Utilities;
 
 namespace SonarAnalyzer.Helpers
 {
@@ -30,6 +31,10 @@ namespace SonarAnalyzer.Helpers
         private const int RotateOffset = 17;
         private const int IntSeed = 393241;
 
+        [PerformanceSensitive("https://github.com/SonarSource/sonar-dotnet/pull/7012",
+            AllowCaptures = false,
+            AllowGenericEnumeration = false,
+            AllowImplicitBoxing = false)]
         public static int DictionaryContentHash<TKey, TValue>(ImmutableDictionary<TKey, TValue> dictionary)
         {
             // Performance: Make sure this method is allocation free:
