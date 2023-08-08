8b72630e85c2ab25d72fc00edbc73ff64e7403e3
Martin Strecker <martin.strecker@sonarsource.com>
2023-08-08 13:31:03 +0200

    Add test file for MS

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/Utilities/TokenTypeAnalyzerTest.Classifier.MS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarAnalyzer.UnitTest.Rules;
+
+public partial class TokenTypeAnalyzerTest
+{
+    [DataTestMethod]
+    [DataRow("[t:Exception] ex;")]        // -model
+    [DataRow("[u:System].Exception ex;")] // +model
+    [DataRow("[t:List]<[t:Exception]> ex;")] // -model
+    [DataRow("List<[u:System].Exception> ex;")] // +model
+    public void IdentifierToken_LocalDeclaration(string declaration, bool allowSemanticModel = true) =>
+        ClassifierTestHarness.AssertTokenTypes(
+        $$"""
+        using System;
+        using System.Collections.Generic;
+        public class Test
+        {
+            public void M()
+            {
+                {{declaration}}
+            }
+        }
+        """, allowSemanticModel);
+
+    [DataTestMethod]
+    [DataRow("ex is [t:ArgumentException]")]
+    public void IdentifierToken_Expressions(string expression, bool allowSemanticModel = true) =>
+        ClassifierTestHarness.AssertTokenTypes(
+        $$"""
+        using System;
+        using System.Collections.Generic;
+        public class Test
+        {
+            public void M(Exception ex)
+            {
+                var x = {{expression}};
+            }
+        }
+        """, allowSemanticModel);
+}
