0b67aa5e468e3b770a4d870b2184a93aab25635f
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-08-10 17:06:40 +0200

    remove usused using

--- analyzers/src/SonarAnalyzer.VisualBasic/SymbolicExecution/Roslyn/ConditionEvaluatesToConstant.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/SymbolicExecution/Roslyn/ConditionEvaluatesToConstant.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using StyleCop.Analyzers.Lightup;
-
 namespace SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.VisualBasic;
 
 public class ConditionEvaluatesToConstant : ConditionEvaluatesToConstantBase
