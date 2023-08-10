dacc8c0be41e135e1b29037c2f8b0b58960881e8
Cristian Ambrosini <114916336+cristian-ambrosini-sonarsource@users.noreply.github.com>
2023-08-10 17:23:16 +0200

    Add ITs with .razor and .cshtml (#7771)

--- analyzers/its/update-expected.ps1
+++ analyzers/its/update-expected.ps1
@@ -2,7 +2,7 @@
 param
 (
     [Parameter(HelpMessage = "The name of single project to update issues for. If ommited, issues for all projects will be updated.")]
-    [ValidateSet("AnalyzeGenerated", "AnalyzeGeneratedVb", "akka.net", "Automapper", "Ember-MM", "Nancy", "NetCore31", "Net5", "Net6", "Net7", "NetCore31WithConfigurableRules", "ManuallyAddedNoncompliantIssues", "ManuallyAddedNoncompliantIssuesVB", "SkipGenerated", "SkipGeneratedVb", "WebConfig")]
+    [ValidateSet("AnalyzeGenerated", "AnalyzeGeneratedVb", "akka.net", "Automapper", "BlazorSample", "Ember-MM", "Nancy", "NetCore31", "Net5", "Net6", "Net7", "NetCore31WithConfigurableRules", "ManuallyAddedNoncompliantIssues", "ManuallyAddedNoncompliantIssuesVB", "RazorSample", "SkipGenerated", "SkipGeneratedVb", "WebConfig")]
     [string]
     $project
 )
