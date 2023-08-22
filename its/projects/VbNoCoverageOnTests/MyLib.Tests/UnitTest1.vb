6ebc0747d52a936aca002987156d101c588b63fd
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-08-22 11:05:23 +0200

    Migrate project to the SDK format

--- its/projects/VbNoCoverageOnTests/MyLib.Tests/UnitTest1.vb
+++ its/projects/VbNoCoverageOnTests/MyLib.Tests/UnitTest1.vb
@@ -1,6 +1,8 @@
-﻿<TestClass()> Public Class UnitTest1
+﻿Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
-    <TestMethod()> Public Sub TestMethod1()
+<TestClass> Public Class UnitTest1
+
+    <TestMethod> Public Sub TestMethod1()
     End Sub
 
 End Class
