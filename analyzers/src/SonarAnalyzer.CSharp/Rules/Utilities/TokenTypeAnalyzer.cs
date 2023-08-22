9fbc478d4183c6d501b719e8ca06aa50a5315359
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-08-22 14:51:05 +0200

    Fix classification for global namespace parts in method invocation (#7837)

--- analyzers/src/SonarAnalyzer.CSharp/Rules/Utilities/TokenTypeAnalyzer.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/Utilities/TokenTypeAnalyzer.cs
@@ -202,7 +202,7 @@ namespace SonarAnalyzer.Rules.CSharp
             private static bool AnyMemberAccessLeftIsNotASimpleName(MemberAccessExpressionSyntax memberAccess) =>
                 memberAccess switch
                 {
-                    { Expression: not SimpleNameSyntax and not MemberAccessExpressionSyntax } => true,
+                    { Expression: not SimpleNameSyntax and not MemberAccessExpressionSyntax and not AliasQualifiedNameSyntax } => true,
                     { Expression: MemberAccessExpressionSyntax left } => AnyMemberAccessLeftIsNotASimpleName(left),
                     _ => false,
                 };
@@ -262,7 +262,7 @@ namespace SonarAnalyzer.Rules.CSharp
                         // This is a type, except on the right side of "is" where it might also be a constant like Int32.MaxValue
                         && !NameIsRightOfIsExpression(topMostQualifiedName, parentOfTopMostQualifiedName) => TokenType.TypeName,
                     // Name is directly after alias global::SomeType
-                    { Parent: AliasQualifiedNameSyntax { Name: { } x, Parent: not QualifiedNameSyntax } } when name == x => TokenType.TypeName,
+                    { Parent: AliasQualifiedNameSyntax { Name: { } x, Parent: not (QualifiedNameSyntax or MemberAccessExpressionSyntax) } } when name == x => TokenType.TypeName,
                     // We are somewhere in a qualified name. It probably is a namespace but could also be the outer type of a nested type.
                     _ => ClassifyIdentifierByModel(name),
                 };
