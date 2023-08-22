3771c8ea11aff3540d67f8de4925d23cc4d3977b
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-08-22 10:49:25 +0200

    Update the ms testing framework in IT projects to be compatible with VS2022

--- its/projects/VbNoCoverageOnTests/MyLib.Tests/UnitTest1.vb
+++ its/projects/VbNoCoverageOnTests/MyLib.Tests/UnitTest1.vb
@@ -1,7 +1,4 @@
-﻿Imports System.Text
-Imports Microsoft.VisualStudio.TestTools.UnitTesting
-
-<TestClass()> Public Class UnitTest1
+﻿<TestClass()> Public Class UnitTest1
 
     <TestMethod()> Public Sub TestMethod1()
     End Sub
