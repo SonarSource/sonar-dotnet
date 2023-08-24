58f7166dbb51c03bcd8d2ceef4a20115574b871b
Tim Pohlmann <tim.pohlmann@sonarsource.com>
2023-08-24 14:44:26 +0200

    Move file

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/CollectionConstraintTests.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Roslyn/CollectionConstraintTests.cs
@@ -21,7 +21,7 @@
 using ChecksCS = SonarAnalyzer.SymbolicExecution.Roslyn.RuleChecks.CSharp;
 using CS = SonarAnalyzer.Rules.CSharp;
 
-namespace SonarAnalyzer.UnitTest.Rules;
+namespace SonarAnalyzer.UnitTest.SymbolicExecution.Roslyn;
 
 [TestClass]
 public class CollectionConstraintTests
