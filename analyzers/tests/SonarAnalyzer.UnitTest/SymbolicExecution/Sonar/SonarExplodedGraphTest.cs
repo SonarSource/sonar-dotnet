9c52665cc9700be263428f38fdff7824d6768614
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-08-30 10:59:15 +0200

    Try to fix UT

--- analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Sonar/SonarExplodedGraphTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/SymbolicExecution/Sonar/SonarExplodedGraphTest.cs
@@ -917,8 +917,7 @@ namespace Namespace
                 }
                 """;
             var context = new ExplodedGraphContext(testInput);
-            var walk = () => context.WalkWithInstructions(2);
-            walk.Should().Throw<Exception>().WithMessage("Expected NumberOfExitBlockReached to be 1, but found 0 (difference of -1).");
+            context.WalkWithExitBlocks(2, 0);
         }
 
         [TestMethod]
