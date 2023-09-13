047b870638d0c701d830a1ecfb6f06b1e0b7296a
Costin Zaharia <costin.zaharia@sonarsource.com>
2023-09-13 17:29:29 +0200

    Document FP on razor components

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CollectionPropertiesShouldBeReadOnly.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestSamples;
+
+public partial class Sample
+{
+    [Parameter]
+    public Dictionary<string, object> Attributes { get; set; } = new (); // Noncompliant, FP
+}
