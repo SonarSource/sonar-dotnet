d95d36bc8d15c886580c275a5bd22b69e3ab330e
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-09-20 12:19:05 +0200

    Add ITs with C# 12 features on .Net 8 runtime

---
+++ analyzers/its/sources/Net8/Net8/features/PrimaryConstructor.cs
@@ -0,0 +1,18 @@
+﻿namespace Net8.features
+{
+    internal class PrimaryConstructor
+    {
+        public class BankAccount(string accountID, string owner)
+        {
+            public string AccountID { get; } = accountID;
+            public string Owner { get; } = owner;
+            public decimal CreditLimit { get; } = 0; 
+            public BankAccount(string accountID, string owner, decimal creditLimit) : this(accountID, owner)
+            {
+                CreditLimit = creditLimit;
+            }
+            
+            public override string ToString() => $"Account ID: {AccountID}, Owner: {Owner}";
+        }
+    }
+}
