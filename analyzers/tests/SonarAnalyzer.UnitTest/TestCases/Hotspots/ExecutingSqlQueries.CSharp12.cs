fc704aef0d13d042046e58671dcbcc308813fb40
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-09-27 16:34:21 +0200

    S2077

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ExecutingSqlQueries.CSharp12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.Sqlite;
+
+namespace Tests.Diagnostics
+{
+    class MyClass(DbContext context, SqliteConnection connection, string someUserInput, params object[] parameters)
+    {
+        void MyMethod()
+        {
+            context.Query<object>().FromSql(someUserInput, parameters); // Compliant FN
+            SqliteCommand command = new($"""SELECT * FROM mytable WHERE mycol={someUserInput}""", connection); // Noncompliant
+        }
+    }
+}
