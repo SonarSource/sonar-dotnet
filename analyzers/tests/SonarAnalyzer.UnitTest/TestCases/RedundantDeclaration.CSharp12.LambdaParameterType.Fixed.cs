1f2290ec9491e3354988ca0a7be0ca41da373823
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-09-27 15:54:32 +0200

    S3257: Add UTs for Alias any type

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantDeclaration.CSharp12.LambdaParameterType.Fixed.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AliasAnyType
+{
+    using Int = int;
+    using IntArray = int[];
+    using IntNullable = int?;
+    using Point = (int x, int y);
+    using ThreeInts = int[3];                   // Error [CS0270]: Array size cannot be specified in a variable declaration
+    using IntToIntFunc = Func<int, int>;
+
+    class AClass
+    {
+        void AliasWithArraySize()
+        {
+            IntArray a1 = new Int[3] { 1, 2, 3 };  // Fixed
+        }
+
+        void AliasWithUnnecessaryType()
+        {
+            IntToIntFunc f = (i) => i;      // Fixed
+        }
+
+        void AliasWithInitializer()
+        {
+            IntArray a1 = new IntArray { };     // Error [CS8386]: Invalid object creation
+            var a2 = new IntArray { };          // Error [CS8386]: Invalid object creation
+            int[] a3 = new IntArray();          // Error [CS8386]: Invalid object creation
+        }
+
+        void AliasWithEmptyParamsList()
+        {
+            IntArray a1 = new IntArray();       // Error [CS8386]: Invalid object creation
+            var a2 = new IntArray();            // Error [CS8386]: Invalid object creation
+            int[] a3 = new IntArray();          // Error [CS8386]: Invalid object creation
+        }
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantDeclaration.CSharp12.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AliasAnyType
+{
+    using Int = int;
+    using IntArray = int[];
+    using IntNullable = int?;
+    using Point = (int x, int y);
+    using ThreeInts = int[3];                   // Error [CS0270]: Array size cannot be specified in a variable declaration
+    using IntToIntFunc = Func<int, int>;
+
+    class AClass
+    {
+        void AliasWithArraySize()
+        {
+            IntArray a1 = new Int[3] { 1, 2, 3 };  // Noncompliant
+        }
+
+        void AliasWithUnnecessaryType()
+        {
+            IntToIntFunc f = (Int i) => i;      // Noncompliant {{Remove the type specification; it is redundant.}}
+        }
+
+        void AliasWithInitializer()
+        {
+            IntArray a1 = new IntArray { };     // Error [CS8386]: Invalid object creation
+            var a2 = new IntArray { };          // Error [CS8386]: Invalid object creation
+            int[] a3 = new IntArray();          // Error [CS8386]: Invalid object creation
+        }
+
+        void AliasWithEmptyParamsList()
+        {
+            IntArray a1 = new IntArray();       // Error [CS8386]: Invalid object creation
+            var a2 = new IntArray();            // Error [CS8386]: Invalid object creation
+            int[] a3 = new IntArray();          // Error [CS8386]: Invalid object creation
+        }
+    }
+}
