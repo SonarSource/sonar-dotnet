38f1ea612d52d110dd3737e1bc51829b4ffb7439
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-09-27 16:33:43 +0200

    S4061

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UseParamsForVariableArguments.CSharp12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Tests.Diagnostics
+{
+    public class BaseClass(__arglist)
+    {
+        public void Foo()
+        {
+            ArgIterator argumentIterator = new ArgIterator(__arglist); // Error CS0190
+            for (int i = 0; i < argumentIterator.GetRemainingCount(); i++)
+            {
+                Console.WriteLine(__refvalue(argumentIterator.GetNextArg(), string));
+            }
+        }
+    }
+}
