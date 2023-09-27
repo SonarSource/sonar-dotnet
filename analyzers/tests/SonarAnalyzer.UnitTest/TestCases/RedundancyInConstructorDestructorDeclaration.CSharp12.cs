53ad667f210bb4a6c464a1fe00af662ffbe01b4b
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-09-27 11:21:34 +0200

    S3253: add repro for #8092

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundancyInConstructorDestructorDeclaration.CSharp12.cs
@@ -0,0 +1,15 @@
+﻿// https://github.com/SonarSource/sonar-dotnet/issues/8092
+namespace Repro_8092
+{
+    namespace PrimaryParameterlessConstructor
+    {
+        class AClassWithBody() { }                // FN
+        class AClassWithoutBody();                // FN
+        struct AStructWithBody() { }              // FN
+        struct AStructWithoutBody();              // FN
+        record ARecordWithBody() { }              // FN
+        record ARecordWithoutBody();              // FN
+        record struct ARecordStructWithBody() { } // FN
+        record struct ARecordStructWithoutBody(); // FN
+    }
+}
