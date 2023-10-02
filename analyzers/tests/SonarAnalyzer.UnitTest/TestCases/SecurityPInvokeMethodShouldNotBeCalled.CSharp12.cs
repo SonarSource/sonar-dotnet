dfce6405a97016e36ae1896a24051484f68d3d27
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-10-02 16:16:19 +0200

    S3884: Add UTs for Alias any type

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SecurityPInvokeMethodShouldNotBeCalled.CSharp12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using IntPtrAliasFromKeywork = nint;
+
+class AliasAnyType
+{
+    [DllImport("OlE32.dll")]
+    static extern int CoSetProxyBlanket(
+        [MarshalAs(UnmanagedType.IUnknown)] object pProxy,
+        uint dwAuthnSvc,
+        uint dwAuthzSvc,
+        [MarshalAs(UnmanagedType.LPWStr)] string pServerPrincName,
+        uint dwAuthnLevel,
+        IntPtrAliasFromKeywork dwImpLevel,
+        IntPtrAliasFromKeywork pAuthInfo,
+        uint dwCapabilities);
+
+    void Test()
+    {
+        _ = CoSetProxyBlanket(null, 0, 0, null, 0, 0, 0, 0); // Noncompliant
+        //  ^^^^^^^^^^^^^^^^^
+    }
+}
