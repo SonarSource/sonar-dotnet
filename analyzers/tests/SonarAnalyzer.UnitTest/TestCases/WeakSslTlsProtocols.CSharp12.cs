ff7dac2b6c4e928e081a5baf9e84460efdf51ecb
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-02 09:16:42 +0200

    S4423

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/WeakSslTlsProtocols.CSharp12.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+public class WeakSslTlsProtocols(SecurityProtocolType classParam = SecurityProtocolType.Ssl3) // Noncompliant
+{
+    public void SecurityProtocolTypeNonComplaint()
+    {
+        var lambda = (SecurityProtocolType lambdaParam = SecurityProtocolType.Ssl3) => lambdaParam; // Noncompliant
+    }
+}
+
