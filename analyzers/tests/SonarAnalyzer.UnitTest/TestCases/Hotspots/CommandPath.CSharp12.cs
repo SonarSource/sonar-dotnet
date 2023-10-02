05f5045b1e23416d364473fb8dde0c4b25242b64
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-02 09:44:56 +0200

    Fix S4036 UTs (Noncompliant -> FN)

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/CommandPath.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/CommandPath.CSharp12.cs
@@ -4,8 +4,8 @@ class PrimaryConstructor(string ctorParam = "file.exe")
 {
     void Method(string methodParam = "file.exe")
     {
-        Process.Start(ctorParam); // Noncompliant
-        Process.Start(methodParam); // Noncompliant
-        var lambda = (string lambdaParam = "file.exe") => Process.Start(lambdaParam); // Noncompliant
+        Process.Start(ctorParam); // FN
+        Process.Start(methodParam); // FN
+        var lambda = (string lambdaParam = "file.exe") => Process.Start(lambdaParam); // FN
     }
 }
