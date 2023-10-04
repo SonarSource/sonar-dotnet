e484c7b475862e1cf5009b5cfd6b947016d3294b
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-04 14:20:36 +0200

    S107

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/TooManyParameters_CustomValues.CSharp12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class MyWrongClass(int p1, int p2, int p3, int p4) { } // FN {{Constructor has 4 parameters, which is greater than the 3 authorized.}}
+
+public class SubClass(int p1, int p2, int p3, int p4) : MyWrongClass(p1, p2, p3, p4) { } // Compliant: base class requires them
+
+public class SubClass2() : MyWrongClass(1, 2, 3, 4) // Compliant
+{
+    public SubClass2(int p1, int p2, int p3, int p4, int p5) : this() { } // Noncompliant
+
+    void Method()
+    {
+        var a = (int p1 = 1, int p2 = 2, int p3 = 3, int p4 = 4) => true; // Noncompliant {{Lambda has 4 parameters, which is greater than the 3 authorized.}}
+    }
+}
