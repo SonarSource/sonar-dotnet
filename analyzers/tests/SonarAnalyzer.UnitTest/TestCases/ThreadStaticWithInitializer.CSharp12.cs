dc4cd2ab799ec086fe9187b29465a336179c2615
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-04 14:21:01 +0200

    S2996

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ThreadStaticWithInitializer.CSharp12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Point2D = (int, int);
+
+namespace Tests.Diagnostics
+{
+    public class ThreadStaticWithInitializer
+    {
+        public class Foo
+        {
+            [ThreadStatic]
+            public static int[] PerThreadArray = [1, 2, 3]; // Noncompliant
+
+            [ThreadStatic]
+            public static Point2D PerThreadPoint = new Point2D(); // Noncompliant
+        }
+    }
+}
