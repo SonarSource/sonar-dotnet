d1c8f22e50ce7b2e1c915cfac2b6a29b6c2894e4
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-10-04 18:38:49 +0200

    Revert "S3884: Add UTs for Alias any type"
    This reverts commit b88b691fbfeb54d1eb78f665a4252875d4d59a85.

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/SecurityPInvokeMethodShouldNotBeCalledTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/SecurityPInvokeMethodShouldNotBeCalledTest.cs
@@ -38,10 +38,6 @@ namespace SonarAnalyzer.UnitTest.Rules
         public void SecurityPInvokeMethodShouldNotBeCalled_CSharp11() =>
             builderCS.AddPaths("SecurityPInvokeMethodShouldNotBeCalled.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
 
-        [TestMethod]
-        public void SecurityPInvokeMethodShouldNotBeCalled_CSharp12() =>
-            builderCS.AddPaths("SecurityPInvokeMethodShouldNotBeCalled.CSharp12.cs").WithOptions(ParseOptionsHelper.FromCSharp12).Verify();
-
 #endif
 
         [TestMethod]
