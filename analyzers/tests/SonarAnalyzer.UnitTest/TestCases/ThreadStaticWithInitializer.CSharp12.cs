af4a1642c1cfb6360e83c238d507ce1ec37f7e50
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-05 11:54:19 +0200

    Address comments

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ThreadStaticWithInitializer.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ThreadStaticWithInitializer.CSharp12.cs
@@ -1,17 +1,14 @@
 ﻿using System;
 using Point2D = (int, int);
 
-namespace Tests.Diagnostics
+public class ThreadStaticWithInitializer
 {
-    public class ThreadStaticWithInitializer
+    public class Foo
     {
-        public class Foo
-        {
-            [ThreadStatic]
-            public static int[] PerThreadArray = [1, 2, 3]; // Noncompliant
+        [ThreadStatic]
+        public static int[] PerThreadArray = [1, 2, 3]; // Noncompliant
 
-            [ThreadStatic]
-            public static Point2D PerThreadPoint = new Point2D(); // Noncompliant
-        }
+        [ThreadStatic]
+        public static Point2D PerThreadPoint = new Point2D(); // Noncompliant
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/TypeExaminationOnSystemType.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/TypeExaminationOnSystemType.CSharp12.cs
@@ -3,5 +3,8 @@ using Point2D = (int, int);
 
 Point2D p = new(1, 2);
 
-if (p.GetType().IsInstanceOfType(typeof(Point2D))) //Noncompliant
+if (p.GetType().IsInstanceOfType(typeof(Point2D))) // Noncompliant
 { /* ... */ }
+
+typeof(Point2D).IsInstanceOfType("42"); // Compliant
+typeof(Point2D).IsInstanceOfType(typeof(Point2D)); // Noncompliant
