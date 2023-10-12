5a7d5b5ff7aa4b190957c40d2c06231a35508448
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-12 11:07:46 +0200

    Address comments

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/ArrayPassedAsParamsTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/ArrayPassedAsParamsTest.cs
@@ -44,13 +44,16 @@ public class ArrayPassedAsParamsTest
     [TestMethod]
     public void ArrayPassedAsParams_CSharp12() =>
         builderCS.AddSnippet("""
-            MyClass s0 = new(1, [1, 2, 3]);     // Noncompliant
-            MyClass s1 = new(1, []);            // Noncompliant
-            MyClass2 s2 = new([1], [1, 2, 3]); // Noncompliant
-            MyClass2 s3 = new([1, 2, 3], 1);
+            _ = new MyClass(1, [1, 2, 3]);     // Noncompliant
+            _ = new MyClass(1, []);            // Noncompliant
+            _ = new MyClass2([1], [1, 2, 3]);  // Noncompliant
+            _ = new MyClass2([1, 2, 3], 1);
+            _ = new MyClass3([1, 2, 3], [4, 5, 6]); // Noncompliant FP
+            _ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
 
-            public class MyClass(int a, params int[] args) { }
-            public class MyClass2(int[] a, params int[] args) { }
+            class MyClass(int a, params int[] args);
+            class MyClass2(int[] a, params int[] args);
+            class MyClass3(params int[][] args);
             """)
             .WithOptions(ParseOptionsHelper.FromCSharp12)
             .WithTopLevelStatements()
