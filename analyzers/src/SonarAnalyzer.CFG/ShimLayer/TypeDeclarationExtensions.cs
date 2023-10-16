1a1262deb2fa9abdc1445b1fcd7fe38eea8782dc
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-16 15:26:35 +0200

    Update TypeDeclarationExtensions

--- analyzers/src/SonarAnalyzer.CFG/ShimLayer/TypeDeclarationExtensions.cs
+++ analyzers/src/SonarAnalyzer.CFG/ShimLayer/TypeDeclarationExtensions.cs
@@ -4,17 +4,28 @@
 namespace StyleCop.Analyzers.Lightup
 {
     using Microsoft.CodeAnalysis.CSharp.Syntax;
+
     public static class TypeDeclarationExtensions
     {
         private static readonly Func<TypeDeclarationSyntax, ParameterListSyntax> ParameterListAccessor;
+        private static readonly Func<TypeDeclarationSyntax, ParameterListSyntax> RecordDeclarationParameterListAccessor;
 
         static TypeDeclarationExtensions()
         {
             ParameterListAccessor = LightupHelpers.CreateSyntaxPropertyAccessor<TypeDeclarationSyntax, ParameterListSyntax>(typeof(TypeDeclarationSyntax), nameof(ParameterList));
+            var recordDeclaration = typeof(ClassDeclarationSyntax).Assembly.GetType("Microsoft.CodeAnalysis.CSharp.Syntax.RecordDeclarationSyntax", throwOnError: false);
+            if (recordDeclaration is not null)
+            {
+                RecordDeclarationParameterListAccessor = LightupHelpers.CreateSyntaxPropertyAccessor<TypeDeclarationSyntax, ParameterListSyntax>(recordDeclaration, nameof(ParameterList));
+            }
         }
 
         public static ParameterListSyntax ParameterList(this TypeDeclarationSyntax syntax)
         {
+            if (syntax.Kind() is SyntaxKindEx.RecordClassDeclaration or SyntaxKindEx.RecordStructDeclaration)
+            {
+                return RecordDeclarationParameterListAccessor(syntax);
+            }
             return ParameterListAccessor(syntax);
         }
     }
