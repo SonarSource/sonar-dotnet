702ec77ff52b7bb8380f72c4adf055800cd79d75
Martin Strecker <martin.strecker@sonarsource.com>
2023-10-18 18:13:06 +0200

    Update code fixes

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantArgument.Named.Fixed.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/RedundantArgument.Named.Fixed.cs
@@ -16,30 +16,31 @@ namespace Tests.Diagnostics
         {
             var x = "".ToString();
             M(1, 5); //Fixed
+            M(1, z: 0); //Fixed
             M(1, z: 7); //Fixed
             M(1, 5, // Fixed
                 7); // Fixed
             M(1);
             M(1, 2, 4);
             M2(1, 1, 1);
-            5.Ext(5); //Fixed
+            5.Ext(5);       //Fixed
+            5.Ext(parameters: new[] { 0, 0 }); //Fixed
             5.Ext2(5);
 
             RedundantArgument.Ext(5, parameters: new[] { 4, 4, 5, 6 }); //Fixed
             RedundantArgument.Ext(5, y: 5, parameters: new int[] { 4, 4, 5, 6 }); //Fixed
             RedundantArgument.Ext(5, 5); //Fixed
 
-            M3(1,//Fixed
-                y: 5,//Fixed
-                z: 7);//Fixed
+            M3(1,      //Fixed
+                y: 5,  //Fixed
+                z: 7); //Fixed
 
-            M3(1,//Fixed
+            M3(1,      //Fixed
                 y: 4);
-            M3(x: 1,//Fixed
+            M3(x: 1,   //Fixed
                 y: 4);
-            M3(y: 4);//Fixed
-
-            M3(1, 4);
+            M3(y: 4);    //Fixed
+            M3(y: 4);
         }
     }
 
