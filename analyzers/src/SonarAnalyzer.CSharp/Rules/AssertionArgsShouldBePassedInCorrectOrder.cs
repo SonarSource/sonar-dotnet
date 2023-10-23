5568b57465c8d99a192d6f968598868ef5b3f85f
Martin Strecker <martin.strecker@sonarsource.com>
2023-10-23 22:20:54 +0200

    Fix AssertionArgsShouldBePassedInCorrectOrder tests

--- analyzers/src/SonarAnalyzer.CSharp/Rules/AssertionArgsShouldBePassedInCorrectOrder.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/AssertionArgsShouldBePassedInCorrectOrder.cs
@@ -37,7 +37,7 @@ public sealed class AssertionArgsShouldBePassedInCorrectOrder : SonarDiagnosticA
                 && c.SemanticModel.GetSymbolInfo(invocation).AllSymbols()
                     .SelectMany(symbol =>
                         symbol is IMethodSymbol { IsStatic: true, ContainingSymbol: INamedTypeSymbol container } methodSymbol
-                            ? knownAssertParameters.Select(knownParameters => FindWrongArguments(c.SemanticModel, container, methodSymbol, argumentList, knownParameters))
+                            ? knownAssertParameters.Select(knownParameters => FindWrongArguments(c.SemanticModel, container, methodSymbol, invocation, knownParameters))
                             : Enumerable.Empty<WrongArguments?>())
                     .FirstOrDefault(x => x is not null) is (Expected: var expected, Actual: var actual))
             {
@@ -79,10 +79,10 @@ public sealed class AssertionArgsShouldBePassedInCorrectOrder : SonarDiagnosticA
     private static WrongArguments? FindWrongArguments(SemanticModel semanticModel,
                                                       INamedTypeSymbol container,
                                                       IMethodSymbol symbol,
-                                                      ArgumentListSyntax argumentList,
+                                                      InvocationExpressionSyntax invocation,
                                                       KnownAssertParameters knownParameters) =>
         container.Is(knownParameters.AssertClass)
-        && CSharpFacade.Instance.MethodParameterLookup(argumentList, symbol) is var parameterLookup
+        && CSharpFacade.Instance.MethodParameterLookup(invocation, symbol) is var parameterLookup
         && parameterLookup.TryGetSyntax(knownParameters.ExpectedParameterName, out var expectedArguments)
         && expectedArguments.FirstOrDefault() is { } expected
         && semanticModel.GetConstantValue(expected).HasValue is false
