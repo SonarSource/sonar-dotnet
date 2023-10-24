e65f78dcb7e6b9e37098f4d4e3ca478ad724913f
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-24 17:33:22 +0200

    First part: redesign and initial support

--- analyzers/src/SonarAnalyzer.CSharp/Rules/VariableShadowsField.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/VariableShadowsField.cs
@@ -28,75 +28,97 @@ namespace SonarAnalyzer.Rules.CSharp
 
         private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
 
+        private static readonly SyntaxKind[] TypesWithPrimaryConstructorDeclarations =
+        {
+            SyntaxKind.ClassDeclaration,
+            SyntaxKind.StructDeclaration,
+            SyntaxKindEx.RecordClassDeclaration,
+            SyntaxKindEx.RecordStructDeclaration
+        };
+
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
 
         protected override void Initialize(SonarAnalysisContext context)
         {
-            context.RegisterNodeAction(c =>
-                {
-                    var declaration = (ForEachStatementSyntax)c.Node;
-
-                    var variableSymbol = c.SemanticModel.GetDeclaredSymbol(declaration);
-                    if (variableSymbol == null)
-                    {
-                        return;
-                    }
-
-                    var members = GetMembers(variableSymbol.ContainingType);
-
-                    ReportOnVariableMatchingField(c, members, declaration.Identifier);
-                },
+            context.RegisterNodeAction(c => Process(c, GetDeclarationOrDesignation(c.Node)),
+                SyntaxKind.LocalDeclarationStatement,
+                SyntaxKind.ForStatement,
+                SyntaxKind.UsingStatement,
+                SyntaxKind.FixedStatement,
+                SyntaxKindEx.DeclarationExpression,
+                SyntaxKindEx.RecursivePattern,
+                SyntaxKindEx.VarPattern,
+                SyntaxKindEx.DeclarationPattern,
+                SyntaxKindEx.ListPattern);
+            context.RegisterNodeAction(c => Process(c, c.Node),
                 SyntaxKind.ForEachStatement);
-
-            context.RegisterNodeAction(c => ProcessVariableDeclaration(c, ((LocalDeclarationStatementSyntax)c.Node).Declaration), SyntaxKind.LocalDeclarationStatement);
-            context.RegisterNodeAction(c => ProcessVariableDeclaration(c, ((ForStatementSyntax)c.Node).Declaration), SyntaxKind.ForStatement);
-            context.RegisterNodeAction(c => ProcessVariableDeclaration(c, ((UsingStatementSyntax)c.Node).Declaration), SyntaxKind.UsingStatement);
-            context.RegisterNodeAction(c => ProcessVariableDeclaration(c, ((FixedStatementSyntax)c.Node).Declaration), SyntaxKind.FixedStatement);
-
-            context.RegisterNodeAction(c => ProcessVariableDesignation(c, ((DeclarationExpressionSyntaxWrapper)c.Node).Designation), SyntaxKindEx.DeclarationExpression);
-            context.RegisterNodeAction(c => ProcessVariableDesignation(c, ((RecursivePatternSyntaxWrapper)c.Node).Designation), SyntaxKindEx.RecursivePattern);
-            context.RegisterNodeAction(c => ProcessVariableDesignation(c, ((VarPatternSyntaxWrapper)c.Node).Designation), SyntaxKindEx.VarPattern);
-            context.RegisterNodeAction(c => ProcessVariableDesignation(c, ((DeclarationPatternSyntaxWrapper)c.Node).Designation), SyntaxKindEx.DeclarationPattern);
-            context.RegisterNodeAction(c => ProcessVariableDesignation(c, ((ListPatternSyntaxWrapper)c.Node).Designation), SyntaxKindEx.ListPattern);
         }
 
-        private static void ProcessVariableDesignation(SonarSyntaxNodeReportingContext context, VariableDesignationSyntaxWrapper variableDesignation)
-        {
-            if (variableDesignation.AllVariables() is { Length: > 0 } variables
-                && context.ContainingSymbol.ContainingType is { } containingType
-                && GetMembers(containingType) is var members)
+        private static SyntaxNode GetDeclarationOrDesignation(SyntaxNode node) =>
+            node switch
             {
-                foreach (var variable in variables)
+                LocalDeclarationStatementSyntax localDeclaration => localDeclaration.Declaration,
+                ForStatementSyntax forStatement => forStatement.Declaration,
+                UsingStatementSyntax usingStatement => usingStatement.Declaration,
+                FixedStatementSyntax fixedStatement => fixedStatement.Declaration,
+                _ when DeclarationExpressionSyntaxWrapper.IsInstance(node) => ((DeclarationExpressionSyntaxWrapper)node).Designation,
+                _ when RecursivePatternSyntaxWrapper.IsInstance(node) => ((RecursivePatternSyntaxWrapper)node).Designation,
+                _ when VarPatternSyntaxWrapper.IsInstance(node) => ((VarPatternSyntaxWrapper)node).Designation,
+                _ when DeclarationPatternSyntaxWrapper.IsInstance(node) => ((DeclarationPatternSyntaxWrapper)node).Designation,
+                _ when ListPatternSyntaxWrapper.IsInstance(node) => ((ListPatternSyntaxWrapper)node).Designation,
+                _ => null
+            };
+
+        private static void Process(SonarSyntaxNodeReportingContext context, SyntaxNode node)
+        {
+            if (ExtractIdentifiers(node) is { Count: > 0 } identifiers
+                && GetContextSymbols(context) is var members)
+            {
+                foreach (var identifier in identifiers)
                 {
-                    ReportOnVariableMatchingField(context, members, variable.Identifier);
+                    ReportOnVariableMatchingField(context, members, identifier);
                 }
             }
         }
 
-        private static void ProcessVariableDeclaration(SonarSyntaxNodeReportingContext context, VariableDeclarationSyntax variableDeclaration)
-        {
-            if (variableDeclaration is { Variables: { Count: > 0 } variables }
-                && context.ContainingSymbol.ContainingType is { } containingType
-                && GetMembers(containingType) is var members)
+        private static List<SyntaxToken> ExtractIdentifiers(SyntaxNode node) =>
+            node switch
             {
-                foreach (var variable in variables)
-                {
-                    ReportOnVariableMatchingField(context, members, variable.Identifier);
-                }
-            }
+                VariableDeclarationSyntax variableDeclaration => variableDeclaration.Variables.Select(x => x.Identifier).ToList(),
+                ForEachStatementSyntax foreachStatement => new() { foreachStatement.Identifier },
+                _ when VariableDesignationSyntaxWrapper.IsInstance(node) => ((VariableDesignationSyntaxWrapper)node).AllVariables().Select(x => x.Identifier).ToList(),
+                _ => new()
+            };
+
+        private static ImmutableArray<ISymbol> GetContextSymbols(SonarSyntaxNodeReportingContext context)
+        {
+            var symbols = context.ContainingSymbol.ContainingSymbol.GetSymbolType().GetMembers();
+            var relevantSymbols = symbols.Where(x => x is IPropertySymbol or IFieldSymbol).ToImmutableArray();
+            var primaryCtor = symbols.FirstOrDefault(x => x is IMethodSymbol && IsPrimaryCtor(x));
+            var primaryCtorParameters = primaryCtor?.GetParameters();
+            return primaryCtor is null ? relevantSymbols : relevantSymbols.AddRange(primaryCtorParameters);
         }
+
+        private static bool IsPrimaryCtor(ISymbol methodSymbol) =>
+            methodSymbol.IsConstructor()
+            && methodSymbol.DeclaringSyntaxReferences.FirstOrDefault()?.GetSyntax() is { } syntax
+            && TypesWithPrimaryConstructorDeclarations.Contains(syntax.Kind());
 
         private static void ReportOnVariableMatchingField(SonarSyntaxNodeReportingContext context, IEnumerable<ISymbol> members, SyntaxToken identifier)
         {
-            if (members.FirstOrDefault(m => m.Name == identifier.ValueText) is { } matchingMember)
+            if (members.FirstOrDefault(x => x.Name == identifier.ValueText) is { } matchingMember)
             {
-                context.ReportIssue(Diagnostic.Create(Rule, identifier.GetLocation(), identifier.Text, matchingMember is IFieldSymbol ? "field" : "property"));
+                context.ReportIssue(Diagnostic.Create(Rule, identifier.GetLocation(), identifier.Text, GetSymbolName(matchingMember)));
             }
         }
 
-        private static List<ISymbol> GetMembers(INamespaceOrTypeSymbol classSymbol) =>
-            classSymbol.GetMembers()
-                       .Where(member => member is IFieldSymbol or IPropertySymbol)
-                       .ToList();
+        private static string GetSymbolName(ISymbol symbol) =>
+            symbol switch
+            {
+                IFieldSymbol => "field",
+                IPropertySymbol => "property",
+                IParameterSymbol => "primary constructor parameter",
+                _ => string.Empty
+            };
     }
 }
--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/VariableShadowsField.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/VariableShadowsField.CSharp12.cs
@@ -1,11 +1,29 @@
-﻿class MyClass(int first, int second, int third, int fourth)
-{
-    private int first = first; // Compliant
-    private int second; // FN
+﻿using System;
 
-    void MyMethod()
+class MyParentClass(int parentClassParam)
+{
+    class MyClass(int a, int b, int c, int d, int e, int f, int g)
     {
-        int third = 1; // FN
-        _ = first is object fourth; // FN
+        private int a = a; // Compliant
+        private int b; // FN
+        private int c { get; set; } = c; // Compliant
+        private int d { get; set; } // FN
+
+        class MyChildClass(int childClassParam) { }
+
+        MyClass(int a, int b) : this(a, b, 1, 1, 1, 1, 1) { } // Compliant
+
+        void MyMethod()
+        {
+            int parentClassParam = 42; // Compliant
+            int childClassParam = 42; // Compliant
+            int ctorParam = 42; // Compliant
+
+            int e = 42; // Noncompliant {{Rename 'e' which hides the primary constructor parameter with the same name.}}
+            _ = a is object f; // Noncompliant
+            for (int g = 0; g < 10; g++) { } // Noncompliant
+            for (int a = 0; a < 10; a++) { } // Noncompliant FP
+            var lambda = (int g = 1) => g; // Compliant
+        }
     }
 }
