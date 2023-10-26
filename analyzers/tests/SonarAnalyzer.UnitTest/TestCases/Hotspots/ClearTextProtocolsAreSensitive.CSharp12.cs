43ba550e61f37ca8883ea8dadd3a5a7b26a5bcc1
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-10-26 10:40:46 +0200

    Address comment

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ClearTextProtocolsAreSensitive.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/Hotspots/ClearTextProtocolsAreSensitive.CSharp12.cs
@@ -7,3 +7,12 @@ class PrimaryConstructor(string ctorParam = "http://foo.com") // Noncompliant
         var lambda = (string lambdaParam = "http://foo.com") => lambdaParam; // Noncompliant
     }
 }
+
+public class Telnet { }
+
+public sealed class Repro
+{
+    public Telnet[] GetTelnet() => CreateTelnets();
+
+    private static Telnet[] CreateTelnets() => [new()]; // Compliant - FN
+}
