3d33211650f7888eb0eacea71c60d07d83181661
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-10-26 14:55:31 +0200

    S3247: Add repro for #223

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CastShouldNotBeDuplicated.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/CastShouldNotBeDuplicated.cs
@@ -1,87 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Tests.Diagnostics
+class Fruit { }
+
+class Program
 {
-    class Fruit { }
+    private object someField;
 
-    class Program
+    public void Foo(Object x)
     {
-        private object someField;
-
-        public void Foo(Object x)
+        if (x is Fruit)  // Noncompliant
         {
-            if (x is Fruit)  // Noncompliant
-            {
-                var f1 = (Fruit)x;
-//                       ^^^^^^^^ Secondary
-                var f2 = (Fruit)x;
-//                       ^^^^^^^^ Secondary
-            }
-
-            var f = x as Fruit;
-            if (x != null) // Compliant
-            {
-
-            }
+            var f1 = (Fruit)x;
+//                   ^^^^^^^^ Secondary
+            var f2 = (Fruit)x;
+//                   ^^^^^^^^ Secondary
         }
 
-        public void Bar(object x)
+        var f = x as Fruit;
+        if (x != null) // Compliant
         {
-            if (!(x is Fruit))
-            {
-                var f1 = (Fruit)x; // Compliant - but will throw
-            }
-            else
-            {
-                var f2 = (Fruit)x; // Compliant - should be non compliant
-            }
 
-        }
-
-        public void FooBar(object x)
-        {
-            if (x is int)
-            {
-                var res = (int)x; // Compliant because we are casting to a ValueType
-            }
-        }
-
-        // See https://github.com/SonarSource/sonar-dotnet/issues/2314
-        public void TakeIdentifierIntoAccount(object x)
-        {
-            if (x is Fruit)
-            {
-                var f = new Fruit();
-                var c = (Fruit)f;
-            }
-
-            if (someField is Fruit) // Noncompliant
-            {
-                var fruit = (Fruit)this.someField;
-//                          ^^^^^^^^^^^^^^^^^^^^^ Secondary
-            }
-        }
-
-        public void UnknownFoo(object x)
-        {
-            if (x is Car)  // Compliant because we ignore what the type is // Error [CS0246]
-            {
-                var c = (Car)x; // Error [CS0246]
-            }
         }
     }
 
-    public class Bar<T> { }
-
-    public class Foo<T>
+    public void Bar(object x)
     {
-        public void Process(object message)
+        if (!(x is Fruit))
         {
-            if (message is Bar<T>/*comment*/)     // Noncompliant
-            {
-                var sub = (Bar<T>/**/) message;   // Secondary
-            }
+            var f1 = (Fruit)x; // Compliant - but will throw
+        }
+        else
+        {
+            var f2 = (Fruit)x; // Compliant - should be non compliant
+        }
+
+    }
+
+    public void FooBar(object x)
+    {
+        if (x is int)
+        {
+            var res = (int)x; // Compliant because we are casting to a ValueType
+        }
+    }
+
+    // See https://github.com/SonarSource/sonar-dotnet/issues/2314
+    public void TakeIdentifierIntoAccount(object x)
+    {
+        if (x is Fruit)
+        {
+            var f = new Fruit();
+            var c = (Fruit)f;
+        }
+
+        if (someField is Fruit) // Noncompliant
+        {
+            var fruit = (Fruit)this.someField;
+//                      ^^^^^^^^^^^^^^^^^^^^^ Secondary
+        }
+    }
+
+    public void UnknownFoo(object x)
+    {
+        if (x is Car)  // Compliant because we ignore what the type is // Error [CS0246]
+        {
+            var c = (Car)x; // Error [CS0246]
+        }
+    }
+}
+
+public class Bar<T> { }
+
+public class Foo<T>
+{
+    public void Process(object message)
+    {
+        if (message is Bar<T>/*comment*/)     // Noncompliant
+        {
+            var sub = (Bar<T>/**/) message;   // Secondary
         }
     }
 }
