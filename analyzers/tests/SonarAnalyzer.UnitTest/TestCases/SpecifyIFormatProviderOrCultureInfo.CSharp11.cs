d981f98f6aae3332afe2ebdfd31db051bac11218
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-10-26 16:27:02 +0200

    Add more test cases

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SpecifyIFormatProviderOrCultureInfo.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/SpecifyIFormatProviderOrCultureInfo.CSharp11.cs
@@ -20,6 +20,10 @@ namespace Tests.Diagnostics
         {
             // Need more example of type implementating IParseable that ignore culture
             _ = Guid.Parse(""); // Noncompliant - FP
+            // This should be noncompliant see: https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Boolean.cs#L410
+            // Need some investigation on why it does not raise.
+            _ = Boolean.Parse(""); // Compliant - FN (as of today's implementation)
+            _ = int.Parse("-1"); // Noncompliant - FP Controversial: There are edge cases like culture "fa-AF" where the 0x2212 (MINUS SIGN) is used instead of the usual 0x002D (HYPHEN-MINUS)
         }
     }
 }
