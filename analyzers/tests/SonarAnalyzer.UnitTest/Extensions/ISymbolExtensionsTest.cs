97f2cfea3be44a155f142256cc8cfcc863d03fcf
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-27 11:41:55 +0200

    Add tests to cover ISymbolExtensions.IsPrimaryConstructor

--- analyzers/tests/SonarAnalyzer.UnitTest/Extensions/ISymbolExtensionsTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/ISymbolExtensionsTest.cs
@@ -19,11 +19,16 @@
  */
 
 extern alias vbnet;
+extern alias csharp;
 
 using Microsoft.CodeAnalysis.VisualBasic;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using ISymbolExtensionsCommon = SonarAnalyzer.Helpers.ISymbolExtensions;
 using ISymbolExtensionsVB = vbnet::SonarAnalyzer.Extensions.ISymbolExtensions;
+using ISymbolExtensionsCS = csharp::SonarAnalyzer.Extensions.ISymbolExtensions;
+using SonarAnalyzer.CFG.Helpers;
+using SonarAnalyzer.Json.Parsing;
+using Microsoft.CodeAnalysis;
 
 namespace SonarAnalyzer.UnitTest.Extensions;
 
@@ -146,6 +151,56 @@ Public Class Sample
 
 End Class";
         ISymbolExtensionsCommon.IsAutoProperty(CreateSymbol(code, AnalyzerLanguage.VisualBasic)).Should().BeFalse();
+    }
+
+    [DataTestMethod]
+    [DataRow("class MyClass();", 1)]
+    [DataRow("class MyClass() { }", 1)]
+    [DataRow("class MyClass(int a) { }", 1)]
+    [DataRow("class MyClass { }", 0)]
+    [DataRow("class MyClass(int a) { MyClass() : this(1) { } };", 1)]
+    [DataRow("class MyClass { MyClass() { } };", 0)]
+    [DataRow("struct MyStruct();", 1)]
+    [DataRow("struct MyStruct() { }", 1)]
+    [DataRow("struct MyStruct(int a) { }", 1)]
+    [DataRow("struct MyStruct { }", 0)]
+    [DataRow("struct MyStruct(int a) { MyStruct() : this(1) { } };", 1)]
+    [DataRow("struct MyStruct { public MyStruct() { } };", 0)]
+    [DataRow("record MyRecord();", 1)]
+    [DataRow("record MyRecord() { }", 1)]
+    [DataRow("record MyRecord(int a) { }", 1)]
+    [DataRow("record MyRecord { }", 0)]
+    [DataRow("record MyRecord(int a) { MyRecord() : this(1) { } };", 1)]
+    [DataRow("record MyRecord { MyRecord() { } };", 0)]
+    [DataRow("record struct MyRecordStruct();", 1)]
+    [DataRow("record struct MyRecordStruct() { }", 1)]
+    [DataRow("record struct MyRecordStruct(int a) { }", 1)]
+    [DataRow("record struct MyRecordStruct { }", 0)]
+    [DataRow("record struct MyRecordStruct(int a) { MyRecordStruct() : this(1) { } };", 1)]
+    [DataRow("record struct MyRecordStruct { MyRecordStruct() { } };", 0)]
+    [DataRow("record class MyRecordClass();", 1)]
+    [DataRow("record class MyRecordClass() { }", 1)]
+    [DataRow("record class MyRecordClass(int a) { }", 1)]
+    [DataRow("record class MyRecordClass { }", 0)]
+    [DataRow("record class MyRecordClass(int a) { MyRecordClass() : this(1) { } };", 1)]
+    [DataRow("record class MyRecordClass { MyRecordClass() { } };", 0)]
+    [DataRow("readonly struct MyReadonlyStruct();", 1)]
+    [DataRow("readonly struct MyReadonlyStruct() { }", 1)]
+    [DataRow("readonly struct MyReadonlyStruct(int a) { }", 1)]
+    [DataRow("readonly struct MyReadonlyStruct { }", 0)]
+    [DataRow("readonly struct MyReadonlyStruct(int a) { MyReadonlyStruct() : this(1) { } };", 1)]
+    [DataRow("readonly struct MyReadonlyStruct { public MyReadonlyStruct() { } };", 0)]
+    public void IsPrimaryConstructor_CS(string code, int primaryConstructors)
+    {
+        var compilation = SolutionBuilder.Create()
+                                         .AddProject(AnalyzerLanguage.CSharp, createExtraEmptyFile: false)
+                                         .AddSnippet(code)
+                                         .GetCompilation(ParseOptionsHelper.CSharpLatest.First());
+
+        var semanticModel = compilation.GetSemanticModel(compilation.SyntaxTrees.First());
+        var typeSymbol = (INamedTypeSymbol)semanticModel.GetDeclaredSymbol(compilation.SyntaxTrees.First().GetRoot().DescendantNodes().First());
+        var methodSymbols = typeSymbol.GetMembers().OfType<IMethodSymbol>();
+        methodSymbols.Count(ISymbolExtensionsCS.IsPrimaryConstructor).Should().Be(primaryConstructors);
     }
 
     private static ISymbol CreateSymbol(string snippet, AnalyzerLanguage language)
