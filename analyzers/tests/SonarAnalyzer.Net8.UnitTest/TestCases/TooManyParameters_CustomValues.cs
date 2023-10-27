bac89608a5438c1f6a98c97e2842419d7cad20b1
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-10-27 08:58:43 +0200

    Change test case files

--- analyzers/tests/SonarAnalyzer.Net8.UnitTest/TestCases/TooManyParameters_CustomValues.cs
+++ analyzers/tests/SonarAnalyzer.Net8.UnitTest/TestCases/TooManyParameters_CustomValues.cs
@@ -9,13 +9,13 @@ namespace Repro_8156
     class ZeroOverheadMemberAccess
     {
         [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
-        extern static UserData CallConstructor(int x1, int x2, int x3, int x4);                 // Noncompliant, FP: signature has to match target
+        extern static UserData CallConstructor(int x1, int x2, int x3, int x4);                 // Compliant: signature has to match target
 
-        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Method")]
-        extern static void CallMethod(UserData userData, int x1, int x2, int x3, int x4);       // Noncompliant, FP: signature has to match target
+        [UnsafeAccessorAttribute(UnsafeAccessorKind.Method, Name = "Method")]
+        extern static void CallMethod(UserData userData, int x1, int x2, int x3, int x4);       // Compliant: signature has to match target
 
         [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "StaticMethod")]
-        extern static void CallStaticMethod(UserData userData, int x1, int x2, int x3, int x4); // Noncompliant, FP: signature has to match target
+        extern static void CallStaticMethod(UserData userData, int x1, int x2, int x3, int x4); // Compliant: signature has to match target
     }
 
     class UserData
