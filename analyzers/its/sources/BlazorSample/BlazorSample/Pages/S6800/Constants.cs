16fe9fdacd2e0499a37d43844282735f4403f934
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-11-02 09:18:22 +0100

    BlazorSample .NET ITs result

---
+++ analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6800/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorSample.Pages.S6800;
+
+public static class Constants
+{
+    public const string ROUTE_UNMATCHED = "/S6800/constant/{UnmatchedFromConstant:bool}";
+    public const string ROUTE_MATCHED = "/S6800/constant/{MatchedFromConstant:string}";
+}
