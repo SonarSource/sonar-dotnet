650e380c52decf64e8792f1d4dac2c0d190dd32f
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-11-03 14:08:33 +0100

    Rename classes

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/AvoidLambdaExpressionInLoopsInBlazor.cs
@@ -0,0 +1,10 @@
+﻿class LambdaInLoopInMethod
+{
+    void Method()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var a = () => { }; // Compliant - Not in blazor
+        }
+    }
+}
