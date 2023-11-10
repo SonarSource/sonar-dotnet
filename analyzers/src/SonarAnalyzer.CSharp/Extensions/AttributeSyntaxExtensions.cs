c262a235667eba99ea74dc09bdc5a910d4318930
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-11-10 14:30:01 +0100

    Revert unused changes

--- analyzers/src/SonarAnalyzer.CSharp/Extensions/AttributeSyntaxExtensions.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Extensions/AttributeSyntaxExtensions.cs
@@ -24,9 +24,6 @@ internal static class AttributeSyntaxExtensions
 {
     private const int AttributeLength = 9;
 
-    public static bool IsSameShortName(this AttributeSyntax attribute, KnownType knownType) =>
-        attribute.Name.GetName().Contains(GetShortNameWithoutAttributeSuffix(knownType));
-
     public static bool IsKnownType(this AttributeSyntax attribute, ImmutableArray<KnownType> attributeKnownTypes, SemanticModel semanticModel)
     {
         for (var i = 0; i < attributeKnownTypes.Length; i++)
@@ -40,7 +37,8 @@ internal static class AttributeSyntaxExtensions
     }
 
     public static bool IsKnownType(this AttributeSyntax attribute, KnownType knownType, SemanticModel semanticModel) =>
-        attribute.IsSameShortName(knownType) && SymbolHelper.IsKnownType(attribute, knownType, semanticModel);
+        attribute.Name.GetName().Contains(GetShortNameWithoutAttributeSuffix(knownType))
+        && SymbolHelper.IsKnownType(attribute, knownType, semanticModel);
 
     private static string GetShortNameWithoutAttributeSuffix(KnownType knownType) =>
         knownType.TypeName == nameof(Attribute)
