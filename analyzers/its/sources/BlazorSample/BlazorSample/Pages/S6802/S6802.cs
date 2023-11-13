5192dc3d7d95ade7c2dd7ea2859133bef0fc14ed
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2023-11-13 15:38:14 +0100

    Simplify .NET ITs

--- analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6802/S6802.cs
+++ analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6802/S6802.cs
@@ -10,3 +10,28 @@ public class LambdaInLoopInMethod
         }
     }
 }
+
+public class LambdaComponent : ComponentBase
+{
+    private List<Button> Buttons { get; } = new();
+
+    private class Button
+    {
+        public string? Id { get; } = Guid.NewGuid().ToString();
+        public Action<MouseEventArgs> Action { get; set; } = e => { };
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        foreach (var button in Buttons)
+        {
+            builder.OpenElement(12, "button");
+            builder.AddMultipleAttributes(13, new Dictionary<string, object>()
+            {
+                { "onclick", (MouseEventArgs e) => button.Action(e) }
+            });
+            builder.AddMarkupContent(14, "\r\n        Button");
+            builder.CloseElement();
+        }
+    }
+}
