7a92f3cd9a48ae45003c13ac8aa9db8d9528bd88
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2023-11-14 15:20:39 +0100

    Update ITs

--- its/src/test/java/com/sonar/it/csharp/RazorClassLibProjectTest.java
+++ its/src/test/java/com/sonar/it/csharp/RazorClassLibProjectTest.java
@@ -78,7 +78,7 @@ public class RazorClassLibProjectTest {
   
   @Test
   void issuesOfS6801AreRaised() {
-    var issuesS6801 = Tests.getIssues(PROJECT).stream().filter(x -> x.getRule().startsWith(SONAR_RULE_S6801)).collect(Collectors.toList());
-    assertThat(issuesS6801.stream().filter(issue -> issue.getComponent().equals(S6801_CONSUMER_COMPONENT_FILE))).hasSize(1);
+    var issues = Tests.getIssues(PROJECT).stream().filter(x -> x.getRule().startsWith(SONAR_RULE_S6801)).collect(Collectors.toList());
+    assertThat(issues.stream().filter(issue -> issue.getComponent().equals("RazorClassLib:S6801/Consumer.razor"))).hasSize(1);
   }
 }
