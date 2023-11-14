e829d3e50c2004944a775fc2163923a9bf60180b
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-11-14 15:04:25 +0100

    S6798: Add ITs (#8353)

--- analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6800/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorSample.Pages.S6800;
-
-public static class Constants
-{
-    public const string ROUTE_UNMATCHED = "/S6800/constant/{UnmatchedFromConstant:bool}";
-    public const string ROUTE_MATCHED = "/S6800/constant/{MatchedFromConstant:string}";
-}
--- analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6800/S6800CsharpOnly.cs
+++ analyzers/its/sources/BlazorSample/BlazorSample/Pages/S6800/S6800CsharpOnly.cs
@@ -4,8 +4,6 @@ namespace BlazorSample.Pages.S6800;
 
 [Route("/S6800/csharp-only/{Unmatched:bool}")]
 [Route("/S6800/csharp-only/{Matched:datetime}")]
-[Route(Constants.ROUTE_UNMATCHED)]
-[Route(Constants.ROUTE_MATCHED)]
 public class S6800CsharpOnly
 {
     [Parameter]
@@ -13,10 +11,4 @@ public class S6800CsharpOnly
 
     [Parameter]
     public DateTime Matched { get; set; }
-
-    [Parameter]
-    public string UnmatchedFromConstant { get; set; }
-
-    [Parameter]
-    public string MatchedFromConstant { get; set; }
 }
