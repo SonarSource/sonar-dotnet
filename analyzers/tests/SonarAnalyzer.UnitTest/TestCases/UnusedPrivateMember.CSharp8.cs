36de33bb31e942b1dc2445549c2c8cf555585194
Antonio Aversa <antonio.aversa@sonarsource.com>
2023-11-15 16:05:44 +0100

    Code review 1

--- analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/UnusedPrivateMember.CSharp8.cs
@@ -153,6 +153,11 @@ namespace Repro6653
 public class Repro_8342
 {
     [Private1] private protected void APrivateProtectedMethod() { }
+    [Public1, Private2] public void APublicMethodWithMultipleAttributes1() { }
+    [Public1][Private2] public void APublicMethodWithMultipleAttributes2() { }
 
     private class Private1Attribute : Attribute { }  // Noncompliant: FP: attribute used on a private protected method
+    private class Private2Attribute : Attribute { }  // Noncompliant: FP: attribute used on a public method
+    private class Private3Attribute : Attribute { }  // Noncompliant: FP: attribute used on a public method
+    public class Public1Attribute : Attribute { }    // Compliant: public
 }
