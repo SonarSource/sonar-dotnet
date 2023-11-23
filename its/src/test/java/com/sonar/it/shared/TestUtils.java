8bc57b2290254181d0c307e0b09728ba04c30d74
Cristian Ambrosini <114916336+cristian-ambrosini-sonarsource@users.noreply.github.com>
2023-11-23 16:31:18 +0100

    Bump S4NET to 5.15.0.80890 in ITs (#8381)

--- its/src/test/java/com/sonar/it/shared/TestUtils.java
+++ its/src/test/java/com/sonar/it/shared/TestUtils.java
@@ -156,7 +156,7 @@ public class TestUtils {
   private static Build<ScannerForMSBuild> newScanner(Path projectDir) {
     // We need to set the fallback version to run from inside the IDE when the property isn't set
     return ScannerForMSBuild.create(projectDir.toFile())
-      .setScannerVersion(System.getProperty("scannerMsbuild.version", "5.14.0.78575"))
+      .setScannerVersion(System.getProperty("scannerMsbuild.version", "5.15.0.80890"))
 
       // In order to be able to run tests on Azure pipelines, the AGENT_BUILDDIRECTORY environment variable
       // needs to be set to the analyzed project directory.
