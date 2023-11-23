ee7cd80ac9d381b19c1449b750ffc470a98aa528
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2023-11-23 19:59:42 +0100

    Extract common code in SyntaxNodeExtensionsTest (#8355)

--- analyzers/tests/SonarAnalyzer.UnitTest/Extensions/SyntaxNodeExtensionsTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Extensions/SyntaxNodeExtensionsTest.cs
@@ -25,6 +25,7 @@ using FluentAssertions.Extensions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Operations;
 using Microsoft.CodeAnalysis.Text;
+using SonarAnalyzer.CFG.Roslyn;
 using StyleCop.Analyzers.Lightup;
 using static csharp::SonarAnalyzer.Extensions.SyntaxTokenExtensions;
 using ExtensionsCS = csharp::SonarAnalyzer.Extensions.SyntaxNodeExtensions;
@@ -183,10 +184,7 @@ namespace SonarAnalyzer.UnitTest.Extensions
                     }
                 }
                 """;
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var node = tree.Single<SyntaxCS.MethodDeclarationSyntax>();
-
-            ExtensionsCS.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            CreateCfgCS<SyntaxCS.MethodDeclarationSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
@@ -198,10 +196,7 @@ namespace SonarAnalyzer.UnitTest.Extensions
                     public int Property => 42;
                 }
                 """;
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var node = tree.Single<SyntaxCS.PropertyDeclarationSyntax>();
-
-            ExtensionsCS.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            CreateCfgCS<SyntaxCS.PropertyDeclarationSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
@@ -213,10 +208,7 @@ namespace SonarAnalyzer.UnitTest.Extensions
                     public int Property {get; set;}
                 }
                 """;
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var node = tree.Single<SyntaxCS.PropertyDeclarationSyntax>();
-
-            ExtensionsCS.CreateCfg(node, semanticModel, default).Should().BeNull();
+            CreateCfgCS<SyntaxCS.PropertyDeclarationSyntax>(code).Should().BeNull();
         }
 
         [TestMethod]
@@ -229,92 +221,78 @@ namespace SonarAnalyzer.UnitTest.Extensions
                     public string this[int index] => field = null;
                 }
                 """;
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var node = tree.Single<SyntaxCS.IndexerDeclarationSyntax>();
-
-            ExtensionsCS.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            CreateCfgCS<SyntaxCS.IndexerDeclarationSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
         public void CreateCfg_MethodBlock_ReturnsCfg_VB()
         {
-            const string code = @"
-Public Class Sample
-    Public Sub Main()
-        Dim X As Integer = 42
-    End Sub
-End Class";
-            var (tree, semanticModel) = TestHelper.CompileVB(code);
-            var node = tree.Single<SyntaxVB.MethodBlockSyntax>();
-
-            ExtensionsVB.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            const string code = """
+                Public Class Sample
+                    Public Sub Main()
+                        Dim X As Integer = 42
+                    End Sub
+                End Class
+                """;
+            CreateCfgVB<SyntaxVB.MethodBlockSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
         public void CreateCfg_AnyNode_ReturnsCfg_CS()
         {
-            const string code = @"
-public class Sample
-{
-    public void Main()
-    {
-        Main();
-    }
-}";
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var node = tree.Single<SyntaxCS.InvocationExpressionSyntax>();
-
-            ExtensionsCS.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            const string code = """
+                public class Sample
+                {
+                    public void Main()
+                    {
+                        Main();
+                    }
+                }
+                """;
+            CreateCfgCS<SyntaxCS.InvocationExpressionSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
         public void CreateCfg_AnyNode_ReturnsCfg_VB()
         {
-            const string code = @"
-Public Class Sample
-    Public Sub Main()
-        Main()
-    End Sub
-End Class";
-            var (tree, semanticModel) = TestHelper.CompileVB(code);
-            var node = tree.Single<SyntaxVB.InvocationExpressionSyntax>();
-
-            ExtensionsVB.CreateCfg(node, semanticModel, default).Should().NotBeNull();
+            const string code = """
+                Public Class Sample
+                    Public Sub Main()
+                        Main()
+                    End Sub
+                End Class
+                """;
+            CreateCfgVB<SyntaxVB.InvocationExpressionSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
         public void CreateCfg_LambdaInsideQuery_CS()
         {
-            const string code = @"
-using System;
-using System.Linq;
-public class Sample
-{
-    public void Main(int[] values)
-    {
-        var result = from value in values select new Lazy<int>(() => value);
-    }
-}";
-            var (tree, semanticModel) = TestHelper.CompileCS(code);
-            var lambda = tree.Single<SyntaxCS.ParenthesizedLambdaExpressionSyntax>();
-
-            ExtensionsCS.CreateCfg(lambda, semanticModel, default).Should().NotBeNull();
+            const string code = """
+                using System;
+                using System.Linq;
+                public class Sample
+                {
+                    public void Main(int[] values)
+                    {
+                        var result = from value in values select new Lazy<int>(() => value);
+                    }
+                }
+                """;
+            CreateCfgCS<SyntaxCS.ParenthesizedLambdaExpressionSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
         public void CreateCfg_LambdaInsideQuery_VB()
         {
-            const string code = @"
-Public Class Sample
-    Public Sub Main(Values() As Integer)
-        Dim Result As IEnumerable(Of Lazy(Of Integer)) = From Value In Values Select New Lazy(Of Integer)(Function() Value)
-    End Sub
-End Class
-";
-            var (tree, semanticModel) = TestHelper.CompileVB(code);
-            var lambda = tree.Single<SyntaxVB.SingleLineLambdaExpressionSyntax>();
-
-            ExtensionsVB.CreateCfg(lambda, semanticModel, default).Should().NotBeNull();
+            const string code = """
+                Public Class Sample
+                    Public Sub Main(Values() As Integer)
+                        Dim Result As IEnumerable(Of Lazy(Of Integer)) = From Value In Values Select New Lazy(Of Integer)(Function() Value)
+                    End Sub
+                End Class
+                """;
+            CreateCfgVB<SyntaxVB.SingleLineLambdaExpressionSyntax>(code).Should().NotBeNull();
         }
 
         [TestMethod]
@@ -408,14 +386,15 @@ End Class";
         [TestMethod]
         public void CreateCfg_UndefinedSymbol_ReturnsCfg_CS()
         {
-            const string code = @"
-public class Sample
-{
-    public void Main()
-    {
-        Undefined(() => 45);
-    }
-}";
+            const string code = """
+                public class Sample
+                {
+                    public void Main()
+                    {
+                        Undefined(() => 45);
+                    }
+                }
+                """;
             var (tree, model) = TestHelper.CompileIgnoreErrorsCS(code);
             var lambda = tree.Single<SyntaxCS.ParenthesizedLambdaExpressionSyntax>();
             ExtensionsCS.CreateCfg(lambda, model, default).Should().NotBeNull();
@@ -444,7 +423,6 @@ End Class";
         {
             var (tree, model) = TestHelper.CompileIgnoreErrorsCS(code);
             var lambda = tree.Single<SyntaxCS.ParenthesizedLambdaExpressionSyntax>();
-
             ExtensionsCS.CreateCfg(lambda, model, default).Should().NotBeNull();
         }
 
@@ -1232,5 +1210,17 @@ public class X
                 .GetCompilation()
                 .SyntaxTrees
                 .First();
+
+        private static ControlFlowGraph CreateCfgCS<T>(string code) where T : CSharpSyntaxNode
+        {
+            var (tree, model) = TestHelper.CompileCS(code);
+            return ExtensionsCS.CreateCfg(tree.Single<T>(), model, default);
+        }
+
+        private static ControlFlowGraph CreateCfgVB<T>(string code) where T : Microsoft.CodeAnalysis.VisualBasic.VisualBasicSyntaxNode
+        {
+            var (tree, model) = TestHelper.CompileVB(code);
+            return ExtensionsVB.CreateCfg(tree.Single<T>(), model, default);
+        }
     }
 }
