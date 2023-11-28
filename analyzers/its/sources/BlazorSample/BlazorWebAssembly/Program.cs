b09fff93c4fbf5dc10328ede86f6021e15473d9f
Martin Strecker <martin.strecker@sonarsource.com>
2023-11-28 14:59:10 +0100

    Revert IT project

--- analyzers/its/sources/BlazorSample/BlazorWebAssembly/Program.cs
+++
@@ -1,6 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-var host = builder.Build();
-await host.RunAsync().ConfigureAwait(false);
