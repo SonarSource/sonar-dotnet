39ba679943cd1d36f295bddf0596eaae9f3c9fd5
Pavel Mikula <pavel.mikula@sonarsource.com>
2023-12-20 17:13:07 +0100

    Parameter deduplication

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/RoslynSymbolicExecution.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/RoslynSymbolicExecution.cs
@@ -230,17 +230,17 @@ internal class RoslynSymbolicExecution
         }
     }
 
-    private IEnumerable<ExplodedNode> ExceptionSuccessors(ExplodedNode node, ExceptionState exception, ControlFlowRegion tryRegion)
+    private IEnumerable<ExplodedNode> ExceptionSuccessors(ExplodedNode node, ExceptionState exception, ControlFlowRegion nearestTryRegion)
     {
         var state = node.State.ResetOperations();   // We're jumping out of the outer statement => We need to reset operation states.
         state = node.Block.EnclosingRegion(ControlFlowRegionKind.Catch) is not null
             ? state.PushException(exception)        // If we're nested inside catch, we need to preserve the exception chain
             : state.SetException(exception);        // Otherwise we track only the current exception to avoid explosion of states after each statement
 
-        foreach (var handler in ReachableHandlers(tryRegion, exception))
+        foreach (var handler in ReachableHandlers(nearestTryRegion))
         {
             yield return new(cfg.Blocks[handler.FirstBlockOrdinal], state, null);
-            if (IsExhaustive(handler, exception))
+            if (IsExhaustive(handler))
             {
                 yield break;
             }
@@ -248,13 +248,13 @@ internal class RoslynSymbolicExecution
 
         yield return new(cfg.ExitBlock, node.State.SetException(exception), null);  // catch without finally or uncaught exception type
 
-        static IEnumerable<ControlFlowRegion> ReachableHandlers(ControlFlowRegion tryRegion, ExceptionState exception) =>
+        IEnumerable<ControlFlowRegion> ReachableHandlers(ControlFlowRegion tryRegion) =>
             tryRegion is null
                 ? Enumerable.Empty<ControlFlowRegion>()
                 : tryRegion.EnclosingRegion.NestedRegions.Where(x => x.Kind != ControlFlowRegionKind.Try && IsReachable(x, exception))
-                    .Concat(ReachableHandlers(tryRegion.EnclosingRegion.EnclosingRegionOrSelf(ControlFlowRegionKind.Try), exception));
+                    .Concat(ReachableHandlers(tryRegion.EnclosingRegion.EnclosingRegionOrSelf(ControlFlowRegionKind.Try)));  // Use also outer candidates for nested try/catch.
 
-        static bool IsExhaustive(ControlFlowRegion handler, ExceptionState exception) =>
+        bool IsExhaustive(ControlFlowRegion handler) =>
             handler.Kind switch
             {
                 ControlFlowRegionKind.Finally => true,
