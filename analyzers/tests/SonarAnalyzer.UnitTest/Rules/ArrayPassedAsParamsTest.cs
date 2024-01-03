a83bc3660e92a2b7f588120581ff1cc347b9b39f
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-01-03 15:48:47 +0100

    add repro

--- analyzers/tests/SonarAnalyzer.UnitTest/Rules/ArrayPassedAsParamsTest.cs
+++ analyzers/tests/SonarAnalyzer.UnitTest/Rules/ArrayPassedAsParamsTest.cs
@@ -43,18 +43,7 @@ public class ArrayPassedAsParamsTest
 
     [TestMethod]
     public void ArrayPassedAsParams_CSharp12() =>
-        builderCS.AddSnippet("""
-            _ = new MyClass(1, [1, 2, 3]);     // Noncompliant
-            _ = new MyClass(1, []);            // Noncompliant
-            _ = new MyClass2([1], [1, 2, 3]);  // Noncompliant
-            _ = new MyClass2([1, 2, 3], 1);
-            _ = new MyClass3([1, 2, 3], [4, 5, 6]); // Noncompliant FP
-            _ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
-
-            class MyClass(int a, params int[] args);
-            class MyClass2(int[] a, params int[] args);
-            class MyClass3(params int[][] args);
-            """)
+        builderCS.AddPaths("ArrayPassedAsParams.CSharp12.cs")
             .WithOptions(ParseOptionsHelper.FromCSharp12)
             .WithTopLevelStatements()
             .Verify();
---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.CSharp12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+const int a = 1;
+int[] bs = [2, 3];
+
+_ = new MyClass(1, [1, 2, 3]);     // Noncompliant
+_ = new MyClass(1, []);            // Noncompliant
+_ = new MyClass(1, [a, .. bs]);    // Noncompliant FP
+_ = new MyClass2([1], [1, 2, 3]);  // Noncompliant
+_ = new MyClass2([1, 2, 3], 1);
+_ = new MyClass3([1, 2, 3], [4, 5, 6]); // Noncompliant FP
+_ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
+_ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
+
+class MyClass(int a, params int[] args);
+class MyClass2(int[] a, params int[] args);
+class MyClass3(params int[][] args);
