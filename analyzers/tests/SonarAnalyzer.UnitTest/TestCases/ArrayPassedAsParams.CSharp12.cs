2fb41a48b66f41899bddae55093394073a101512
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2024-01-03 16:36:40 +0100

    S3878: Add FP repro for #8510 (#8511)

---
+++ analyzers/tests/SonarAnalyzer.UnitTest/TestCases/ArrayPassedAsParams.CSharp12.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+const int a = 1;
+int[] array = [2, 3];
+
+_ = new MyClass(1, [1, 2, 3]);     // Noncompliant
+_ = new MyClass(1, []);            // Noncompliant
+
+// repro for https://github.com/SonarSource/sonar-dotnet/issues/8510
+_ = new MyClass(1, [a, .. array]);    // Noncompliant FP
+
+_ = new MyClass2([1], [1, 2, 3]);  // Noncompliant
+_ = new MyClass2([1, 2, 3], 1);
+_ = new MyClass3([1, 2, 3], [4, 5, 6]); // Noncompliant FP
+_ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
+_ = new MyClass3([[1, 2, 3], [4, 5, 6]]); // Noncompliant
+
+class MyClass(int a, params int[] args);
+class MyClass2(int[] a, params int[] args);
+class MyClass3(params int[][] args);
