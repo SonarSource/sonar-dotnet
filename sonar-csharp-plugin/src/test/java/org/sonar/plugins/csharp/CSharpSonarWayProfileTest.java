9bdfd39ff50a2f95f38ce387b4b71acb641a96cb
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-01-15 11:14:47 +0100

    Update Java files

--- sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarWayProfileTest.java
+++ sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarWayProfileTest.java
@@ -1,6 +1,6 @@
 /*
  * SonarC#
- * Copyright (C) 2014-2023 SonarSource SA
+ * Copyright (C) 2014-2024 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
