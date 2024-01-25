75029491445695bc1c0486ac83b8c8fc25b1f9d6
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-01-25 17:43:38 +0100

    Add UTs for S6444

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/Hotspots/SpecifyTimeoutOnRegex.DefaultMatchTimeout.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+using System;
+
+AppDomain.CurrentDomain.SetData("REGEX_DEFAULT_MATCH_TIMEOUT", TimeSpan.FromMilliseconds(100));
+
+void RegexPattern(string input)
+{
+    _ = new Regex(".+@.+", RegexOptions.None);  // Noncompliant, FP REGEX_DEFAULT_MATCH_TIMEOUT is set in the AppDomain
+    _ = Regex.IsMatch(input, "[0-9]+");         // Noncompliant, FP REGEX_DEFAULT_MATCH_TIMEOUT is set in the AppDomain
+}
