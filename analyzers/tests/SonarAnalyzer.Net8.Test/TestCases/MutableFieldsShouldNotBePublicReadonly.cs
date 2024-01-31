bc5a399efd9e756863b4847bf4f7a8baa505b6bc
Martin Strecker <martin.strecker@sonarsource.com>
2024-01-31 14:19:57 +0100

    Add tests for FrozenSet

--- analyzers/tests/SonarAnalyzer.Net8.Test/TestCases/MutableFieldsShouldNotBePublicReadonly.cs
+++ analyzers/tests/SonarAnalyzer.Net8.Test/TestCases/MutableFieldsShouldNotBePublicReadonly.cs
@@ -7,5 +7,6 @@ namespace Repro_8638
     public class FrozenCollections
     {
         public readonly FrozenDictionary<string, string> FrozenDictionaryTest = new Dictionary<string, string>().ToFrozenDictionary(); // Compilant
+        public readonly FrozenSet<string> FrozenSetTest = new HashSet<string>().ToFrozenSet(); // Compilant
     }
 }
--- analyzers/tests/SonarAnalyzer.Net8.Test/TestCases/MutableFieldsShouldNotBePublicStatic.cs
+++ analyzers/tests/SonarAnalyzer.Net8.Test/TestCases/MutableFieldsShouldNotBePublicStatic.cs
@@ -7,5 +7,6 @@ namespace Repro_8638
     public class FrozenCollections
     {
         public static FrozenDictionary<string, string> FrozenDictionaryTest = new Dictionary<string, string>().ToFrozenDictionary(); // Compilant
+        public static FrozenSet<string> FrozenSetTest = new HashSet<string>().ToFrozenSet(); // Compilant
     }
 }
