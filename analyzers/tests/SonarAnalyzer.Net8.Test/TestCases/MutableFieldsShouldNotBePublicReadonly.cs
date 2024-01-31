4b933c246e56fdd4061da6c4aea50937c82f404a
Martin Strecker <martin.strecker@sonarsource.com>
2024-01-31 14:13:19 +0100

    Add reproducer for S3887

---
+++ analyzers/tests/SonarAnalyzer.Net8.Test/TestCases/MutableFieldsShouldNotBePublicReadonly.cs
@@ -0,0 +1,11 @@
+﻿//https://github.com/SonarSource/sonar-dotnet/issues/8638
+using System.Collections.Frozen;
+using System.Collections.Generic;
+
+namespace Repro_8638
+{
+    public class FrozenCollections
+    {
+        public readonly FrozenDictionary<string, string> FrozenDictionaryTest = new Dictionary<string, string>().ToFrozenDictionary(); // Compilant
+    }
+}
