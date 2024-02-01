ae45497b3dff9602dc6e5e985bab0d1177bc1b4c
Martin Strecker <martin.strecker@sonarsource.com>
2024-02-01 19:17:17 +0100

    Update header

--- analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/States.cs
+++ analyzers/src/SonarAnalyzer.Common/SymbolicExecution/Roslyn/States.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * SonarAnalyzer for .NET
- * Copyright (C) 2015-2023 SonarSource SA
+ * Copyright (C) 2015-2024 SonarSource SA
  * mailto: contact AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
