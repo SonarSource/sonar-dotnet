d7402d3ac78d5b7dea910794241f50d99fd3c93c
Gregory Paidis <115458417+gregory-paidis-sonarsource@users.noreply.github.com>
2024-02-02 13:53:06 +0100

    ITs: Scaffold JSON Parser for ITs (#8646)

---
+++ analyzers/src/ITs.JsonParser/Program.cs
@@ -0,0 +1,40 @@
+﻿using CommandLine;
+using ITs.JsonParser;
+
+Parser.Default.ParseArguments<CommandLineOptions>(args).MapResult(Execute, errs => 1);
+
+int Execute(CommandLineOptions options)
+{
+    WriteLineColor("Processing analyzer results", ConsoleColor.Yellow);
+    NewIssueReports();
+    ShowDiffResults(options.Project, options.RuleId);
+    return 0;
+}
+
+// TODO: Reimplement this functionality from the powershell function.
+void NewIssueReports()
+{
+    Console.WriteLine("Normalizing the SARIF reports");
+}
+
+// TODO: Reimplement this functionality from the powershell function.
+void ShowDiffResults(string project, string ruleId)
+{
+    // Placeholder, to be deleted
+    if (project is not null)
+    {
+        Console.WriteLine($"Target Project: {project}");
+    }
+    if (ruleId is not null)
+    {
+        Console.WriteLine($"Target Rule: {ruleId}");
+    }
+}
+
+void WriteLineColor(string value, ConsoleColor color)
+{
+    var before = Console.ForegroundColor;
+    Console.ForegroundColor = color;
+    Console.WriteLine(value);
+    Console.ForegroundColor = before;
+}
