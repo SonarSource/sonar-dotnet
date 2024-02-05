5a80560a527074ca70139f9db0969f2404bcc1fd
Gregory Paidis <gregory.paidis@sonarsource.com>
2024-02-05 14:43:14 +0100

    Add license headers and exclude Program.cs from coverage

--- analyzers/src/ITs.JsonParser/Program.cs
+++ analyzers/src/ITs.JsonParser/Program.cs
@@ -1,37 +1,64 @@
-﻿using CommandLine;
-using ITs.JsonParser;
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
-Parser.Default.ParseArguments<CommandLineOptions>(args).MapResult(Execute, errs => 1);
+using System.Diagnostics.CodeAnalysis;
+using CommandLine;
 
-int Execute(CommandLineOptions options)
+namespace ITs.JsonParser;
+
+[ExcludeFromCodeCoverage]
+public static class Program
 {
-    ConsoleHelper.WriteLineColor("Processing analyzer results", ConsoleColor.Yellow);
-    ParseIssues();
-    ShowDiff(options.Project, options.RuleId);
-    return 0;
-}
+    public static void Main(string[] args) =>
+        Parser.Default.ParseArguments<CommandLineOptions>(args).MapResult(Execute, errs => 1);
 
-void ParseIssues()
-{
-    var sw = Stopwatch.StartNew();
-    ConsoleHelper.WriteLineColor("Splitting the SARIF reports to actual folder", ConsoleColor.Yellow);
-    var here = Directory.GetCurrentDirectory();
-    var inputRoot = Path.Combine(here, "output", "Issues");
-    var outputRoot = Path.Combine(here, "actual");
-    IssueParser.Execute(inputRoot, outputRoot);
-    ConsoleHelper.WriteLineColor($"Normalized the SARIF reports in '{sw.Elapsed}'", ConsoleColor.Yellow);
-}
-
-// TODO: Reimplement this functionality from the powershell function.
-void ShowDiff(string project, string ruleId)
-{
-    // Placeholder, to be deleted
-    if (project is not null)
+    public static int Execute(CommandLineOptions options)
     {
-        Console.WriteLine($"Target Project: {project}");
+        ConsoleHelper.WriteLineColor("Processing analyzer results", ConsoleColor.Yellow);
+        ParseIssues();
+        ShowDiff(options.Project, options.RuleId);
+        return 0;
     }
-    if (ruleId is not null)
+
+    public static void ParseIssues()
     {
-        Console.WriteLine($"Target Rule: {ruleId}");
+        var sw = Stopwatch.StartNew();
+        ConsoleHelper.WriteLineColor("Splitting the SARIF reports to actual folder", ConsoleColor.Yellow);
+        var here = Directory.GetCurrentDirectory();
+        var inputRoot = Path.Combine(here, "output", "Issues");
+        var outputRoot = Path.Combine(here, "actual");
+        IssueParser.Execute(inputRoot, outputRoot);
+        ConsoleHelper.WriteLineColor($"Normalized the SARIF reports in '{sw.Elapsed}'", ConsoleColor.Yellow);
+    }
+
+    // TODO: Reimplement this functionality from the powershell function.
+    public static void ShowDiff(string project, string ruleId)
+    {
+        // Placeholder, to be deleted
+        if (project is not null)
+        {
+            Console.WriteLine($"Target Project: {project}");
+        }
+        if (ruleId is not null)
+        {
+            Console.WriteLine($"Target Rule: {ruleId}");
+        }
     }
 }
