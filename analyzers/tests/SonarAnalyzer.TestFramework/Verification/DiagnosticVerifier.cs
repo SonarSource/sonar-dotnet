9be506e7d3d1d1e2b1671fd15b014b2a7bb53889
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-02-08 16:38:23 +0100

    Fix code smells

--- analyzers/tests/SonarAnalyzer.TestFramework/Verification/DiagnosticVerifier.cs
+++ analyzers/tests/SonarAnalyzer.TestFramework/Verification/DiagnosticVerifier.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Text;
-using FluentAssertions.Execution;
 using SonarAnalyzer.TestFramework.Verification.IssueValidation;
 
 namespace SonarAnalyzer.Test.TestFramework
