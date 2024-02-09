fa9efe5724ddd0bded44d97b4a5d6a8a6c2ef115
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-02-09 15:31:37 +0100

    ITs: Cleanup and update

--- analyzers/tests/ITs.JsonParser.Test/IssueParserTest.cs
+++ analyzers/tests/ITs.JsonParser.Test/IssueParserTest.cs
@@ -18,8 +18,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using SonarAnalyzer.Test;
-using SonarAnalyzer.Test.Helpers;
+using SonarAnalyzer.TestFramework.Common;
+using SonarAnalyzer.TestFramework.Extensions;
 
 namespace ITs.JsonParser.Test;
 
