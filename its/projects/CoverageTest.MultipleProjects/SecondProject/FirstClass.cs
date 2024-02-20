9c7c2ae3bd2b7c61397264bf48e4b17277da61d2
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-02-20 16:45:44 +0100

    Java ITs: Fix flaky CoverageTest (#8777)

---
+++ its/projects/CoverageTest.MultipleProjects/SecondProject/FirstClass.cs
@@ -0,0 +1,27 @@
+﻿
+namespace SecondProject
+{
+    public class FirstClass
+    {
+        public string SwitchExpression(int number) =>
+            number switch
+            {
+                10 => "ten",
+                20 => "twenty",
+                _ => "unknown"
+            };
+
+        public string IfElse(bool condition)
+        {
+            if (condition) // S3923 (conditional structure with same code on both branches)
+            {
+                return SwitchExpression(10);
+            }
+            else
+            {
+                return SwitchExpression(10);
+            }
+        }
+
+    }
+}
