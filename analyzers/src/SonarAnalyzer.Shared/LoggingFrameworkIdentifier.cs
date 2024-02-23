9bc5cc6f016c723d8fbad586b0b3e549e9058a89
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-02-23 14:47:11 +0100

    Keep only the properties

--- analyzers/src/SonarAnalyzer.Shared/LoggingFrameworkIdentifier.cs
+++ analyzers/src/SonarAnalyzer.Shared/LoggingFrameworkIdentifier.cs
@@ -60,23 +60,4 @@ public static class LoggingFrameworkIdentifier
     ];
 
     public static readonly HashSet<string> NLogILoggerBaseLoggingMethods = ["Log"];
-
-    public static bool IsLoggingInvocation(InvocationExpressionSyntax invocation, SemanticModel model) =>
-        IsLoggingInvocation(invocation, model, MicrosoftExtensionsLoggingMethods, KnownType.Microsoft_Extensions_Logging_LoggerExtensions, true)
-        || IsLoggingInvocation(invocation, model, CastleCoreOrCommonCoreLoggingMethods, KnownType.Castle_Core_Logging_ILogger, false)
-        || IsLoggingInvocation(invocation, model, CastleCoreOrCommonCoreLoggingMethods, KnownType.Common_Logging_ILog, false)
-        || IsLoggingInvocation(invocation, model, Log4NetLoggingMethods, KnownType.log4net_ILog, false)
-        || IsLoggingInvocation(invocation, model, Log4NetLoggingExtensionMethods, KnownType.log4net_Util_ILogExtensions, true)
-        || IsLoggingInvocation(invocation, model, NLogLoggingMethods, KnownType.NLog_ILogger, false)
-        || IsLoggingInvocation(invocation, model, NLogILoggerBaseLoggingMethods, KnownType.NLog_ILoggerBase, false);
-
-    public static bool IsLoggingInvocation(InvocationExpressionSyntax invocation, SemanticModel model, ICollection<string> methodNames, KnownType containingType, bool isExtensionMethod) =>
-        methodNames.Contains(invocation.GetIdentifier().ToString())
-        && model.GetSymbolInfo(invocation).Symbol is { } invocationSymbol
-        && HasMatchingContainingType(invocationSymbol, containingType, isExtensionMethod);
-
-    private static bool HasMatchingContainingType(ISymbol symbol, KnownType containingType, bool isExtensionMethod) =>
-        isExtensionMethod
-            ? symbol.ContainingType.Is(containingType)
-            : symbol.ContainingType.DerivesOrImplements(containingType);
 }
