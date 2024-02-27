019b3aa9897f132b90e4bfcaf7daa773a98db151
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-02-27 14:31:46 +0100

    Add manual finding for S6668

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6668.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace IntentionalFindings
+{
+    public class S6668
+    {
+        private void Log(ILogger logger, Exception e)
+        {
+            logger.LogCritical("Expected exception.", e); // Noncompliant (S6668) {{Logging arguments should be passed to the correct parameter.}}
+        }
+    }
+}
