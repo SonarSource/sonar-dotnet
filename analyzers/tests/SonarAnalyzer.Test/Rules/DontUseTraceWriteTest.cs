17ed4a20aa5327c1d2c1b28fb5219b756ebcebae
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-03-05 00:32:12 +0100

    Add UTs

---
+++ analyzers/tests/SonarAnalyzer.Test/Rules/DontUseTraceWriteTest.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using SonarAnalyzer.Rules.CSharp;
+
+namespace SonarAnalyzer.Test.Rules;
+
+[TestClass]
+public class DontUseTraceWriteTest
+{
+    private readonly VerifierBuilder builder = new VerifierBuilder<DontUseTraceWrite>();
+
+    [TestMethod]
+    public void DontUseTraceWrite_CS() =>
+        builder.AddPaths("DontUseTraceWrite.cs").Verify();
+
+    [TestMethod]
+    public void DontUseTraceWrite_CustomTraceClass_CS() =>
+        builder.AddSnippet("""
+            public class Program
+            {
+                public void Method(string arg)
+                {
+                    Trace.Write("Message");                 // Compliant - the method is not from the System.Diagnostics.Trace class
+                    Trace.Write("Message: {0}", arg);
+                    Trace.WriteLine("Message");
+                    Trace.WriteLine("Message: {0}", arg);
+                }
+            }
+
+            public class Trace
+            {
+                public static void Write(string message) { }
+                public static void Write(string message, params object[] args) { }
+                public static void WriteLine(string message) { }
+                public static void WriteLine(string message, params object[] args) { }
+            }
+            """).Verify();
+}
---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/DontUseTraceWrite.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using AliasedTrace = System.Diagnostics.Trace;
+
+public class Program
+{
+    public void Noncompliant_TraceMethods()
+    {
+        Trace.Write("Message");             // Noncompliant {{Avoid using Trace.Write, use instead methods that specify the trace event type.}}
+//            ^^^^^
+        Trace.WriteLine("Message");         // Noncompliant {{Avoid using Trace.WriteLine, use instead methods that specify the trace event type.}}
+    }
+
+    public void Compliant_TraceMethods(string arg)
+    {
+        Trace.WriteIf(true, "Message");
+        Trace.WriteLineIf(true, "Message");
+
+        Trace.TraceError("Message");
+        Trace.TraceError("Message: {0}", arg);
+
+        Trace.TraceInformation("Message");
+        Trace.TraceInformation("Message: {0}", arg);
+
+        Trace.TraceWarning("Message");
+        Trace.TraceWarning("Message: {0}", arg);
+    }
+
+    public void Write_Overloads()
+    {
+        Trace.Write("Message");                         // Noncompliant
+        Trace.Write(42);                                // Noncompliant
+        Trace.Write("Message", "Category");             // Noncompliant
+        Trace.Write(42, "Category");                    // Noncompliant
+
+        Trace.WriteLine("Message");                     // Noncompliant
+        Trace.WriteLine(42);                            // Noncompliant
+        Trace.WriteLine("Message", "Category");         // Noncompliant
+        Trace.WriteLine(42, "Category");                // Noncompliant
+    }
+
+    public void Not_TraceClass(string arg)
+    {
+        Console.Write("Message");
+        Console.Write("Message: {0}", arg);
+        Console.WriteLine("Message");
+        Console.WriteLine("Message: {0}", arg);
+
+        Debug.Write("Message");
+        Debug.Write("Message: {0}", arg);
+        Debug.WriteLine("Message");
+        Debug.WriteLine("Message: {0}", arg);
+    }
+
+    public void Aliased_Trace()
+    {
+        AliasedTrace.Write("Message");                  // Noncompliant
+        AliasedTrace.WriteLine("Message");              // Noncompliant
+    }
+}
