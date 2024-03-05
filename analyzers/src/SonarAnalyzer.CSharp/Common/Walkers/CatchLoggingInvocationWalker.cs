e923f10fee8feec389e800f0d50e180bc4811421
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-03-05 16:34:18 +0100

    Rule S2139: Exceptions should be either logged or rethrown but not both

---
+++ analyzers/src/SonarAnalyzer.CSharp/Common/Walkers/CatchLoggingInvocationWalker.cs
@@ -0,0 +1,119 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using static Roslyn.Utilities.SonarAnalyzer.Shared.LoggingFrameworkMethods;
+
+namespace SonarAnalyzer.Common.Walkers;
+
+// The walker is used to:
+// - visit the catch clause (all the nested catch clauses are skipped; they will be visited independently)
+// - save the declared exception
+// - find all the logging invocations and check if the exception is logged
+// - if the exception is logged, it will stop looking for the other invocations and set IsExceptionLogged to true
+// - if the exception is not logged, it will visit all the invocations
+public sealed class CatchLoggingInvocationWalker : SafeCSharpSyntaxWalker
+{
+    private readonly SemanticModel model;
+    private bool isFirstCatchClauseVisited;
+    private bool hasWhenFilterWithDeclarations;
+
+    public ISymbol CaughtException { get; private set; }
+    public bool IsExceptionLogged { get; private set; }
+    public InvocationExpressionSyntax LoggingInvocationWithException { get; private set; }
+    public ThrowStatementSyntax ThrowStatementSyntax { get; private set; }
+    public ImmutableArray<InvocationExpressionSyntax> LoggingInvocationsWithoutException { get; private set; } = ImmutableArray<InvocationExpressionSyntax>.Empty;
+
+    private static readonly ImmutableArray<LoggingInvocationDescriptor> LoggingInvocationIdentifiers = ImmutableArray.Create(
+        new LoggingInvocationDescriptor(MicrosoftExtensionsLogging, KnownType.Microsoft_Extensions_Logging_LoggerExtensions, false),
+        new LoggingInvocationDescriptor(CastleCoreOrCommonCore, KnownType.Castle_Core_Logging_ILogger, true),
+        new LoggingInvocationDescriptor(CastleCoreOrCommonCore, KnownType.Common_Logging_ILog, true),
+        new LoggingInvocationDescriptor(Log4NetILog, KnownType.log4net_ILog, true),
+        new LoggingInvocationDescriptor(Log4NetILogExtensions, KnownType.log4net_Util_ILogExtensions, false),
+        new LoggingInvocationDescriptor(NLogLoggingMethods, KnownType.NLog_ILogger, true),
+        new LoggingInvocationDescriptor(NLogLoggingMethods, KnownType.NLog_ILoggerExtensions, false),
+        new LoggingInvocationDescriptor(NLogILoggerBase, KnownType.NLog_ILoggerBase, true),
+        new LoggingInvocationDescriptor(Serilog, KnownType.Serilog_ILogger, true),
+        new LoggingInvocationDescriptor(Serilog, KnownType.Serilog_Log, false));
+
+    public CatchLoggingInvocationWalker(SemanticModel model)
+    {
+        this.model = model;
+    }
+
+    public override void VisitCatchClause(CatchClauseSyntax node)
+    {
+        // We want to look for logging invocations only in the main catch clause.
+        if (isFirstCatchClauseVisited)
+        {
+            return;
+        }
+
+        isFirstCatchClauseVisited = true;
+        hasWhenFilterWithDeclarations = node.Filter != null && node.Filter.DescendantNodes().Any(DeclarationPatternSyntaxWrapper.IsInstance);
+        if (node.Declaration != null && !node.Declaration.Identifier.IsKind(SyntaxKind.None))
+        {
+            CaughtException = model.GetDeclaredSymbol(node.Declaration);
+        }
+        base.VisitCatchClause(node);
+    }
+
+    public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+    {
+        if (!IsExceptionLogged && IsLoggingInvocation(node, model))
+        {
+            if (GetArgumentSymbolDerivedFromException(node, model) is { } currentException
+                && (hasWhenFilterWithDeclarations || currentException.Equals(CaughtException)))
+            {
+                IsExceptionLogged = true;
+                LoggingInvocationWithException = node;
+                return;
+            }
+            else
+            {
+                LoggingInvocationsWithoutException = LoggingInvocationsWithoutException.Add(node);
+            }
+        }
+        base.VisitInvocationExpression(node);
+    }
+
+    public override void VisitThrowStatement(ThrowStatementSyntax node)
+    {
+        ThrowStatementSyntax = node;
+        base.VisitThrowStatement(node);
+    }
+
+    private static ISymbol GetArgumentSymbolDerivedFromException(InvocationExpressionSyntax invocation, SemanticModel semanticModel) =>
+        invocation.ArgumentList.Arguments
+            .Where(x => semanticModel.GetTypeInfo(x.Expression).Type.DerivesFrom(KnownType.System_Exception))
+            .Select(x => x.Expression is MemberAccessExpressionSyntax memberAccess && memberAccess.NameIs("InnerException")
+                ? semanticModel.GetSymbolInfo(memberAccess.Expression).Symbol
+                : semanticModel.GetSymbolInfo(x.Expression).Symbol)
+            .FirstOrDefault();
+
+    private static bool IsLoggingInvocation(InvocationExpressionSyntax invocation, SemanticModel model) =>
+        LoggingInvocationIdentifiers.Any(x => IsLoggingInvocation(invocation, model, x.MethodNames, x.ContainingType, x.CheckDerivedTypes));
+
+    private static bool IsLoggingInvocation(InvocationExpressionSyntax invocation, SemanticModel model, ICollection<string> methodNames, KnownType containingType, bool checkDerivedTypes) =>
+        methodNames.Contains(invocation.GetIdentifier().ToString())
+        && model.GetSymbolInfo(invocation).Symbol is { } invocationSymbol
+        && invocationSymbol.HasContainingType(containingType, checkDerivedTypes);
+
+    private record struct LoggingInvocationDescriptor(HashSet<string> MethodNames, KnownType ContainingType, bool CheckDerivedTypes);
+}
---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/ExceptionsShouldBeLoggedOrThrown.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+public class ExceptionsShouldBeEitherLoggedOrThrown
+{
+    public void Generic(ILogger<ExceptionsShouldBeEitherLoggedOrThrown> logger)
+    {
+        try
+        {
+        }
+        catch (DivideByZeroException divideByZeroException) // The exception is only logged.
+        {
+            logger.LogError(divideByZeroException, "Message!");
+        }
+        catch (InvalidOperationException loggedMultipleTimes)           // Noncompliant {{Either log this exception and handle it, or rethrow it with some contextual information.}}
+        {
+            logger.LogError(loggedMultipleTimes, "Message!");
+//          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Secondary {{Logging statement.}}
+            logger.LogInformation(loggedMultipleTimes, "Message!");
+            throw;
+//          ^^^^^^ Secondary {{Thrown exception.}}
+        }
+        catch (ArgumentException misplaced)                             // Noncompliant
+        {
+            logger.LogError("Message!", misplaced);                     // Secondary
+            throw;                                                      // Secondary
+        }
+        catch (InvalidCastException invalidCastException)               // Noncompliant
+        {
+            logger.LogError(invalidCastException, "Message!");          // Secondary
+            throw invalidCastException;                                 // Secondary
+        }
+        catch (Exception ex)                                            // Noncompliant
+        {
+            logger.LogError(ex, "Message!");                            // Secondary
+            throw;                                                      // Secondary
+        }
+    }
+
+    public void NonGeneric(ILogger logger)
+    {
+        try {}
+        catch (Exception ex)                                            // Noncompliant
+        {
+            logger.LogError(ex, "Message!");                            // Secondary
+            throw;                                                      // Secondary
+        }
+    }
+
+    public void Nested(ILogger logger)
+    {
+        try {}
+        catch (Exception outer)                                         // FN - Only the main catch body is visited
+        {
+            try {}
+            catch (Exception inner)
+            {
+                logger.LogError(outer, "Message");
+            }
+
+            try {}
+            catch (Exception inner)                                     // Noncompliant
+            {
+                logger.LogError(inner, "Message");                      // Secondary
+                throw;                                                  // Secondary
+            }
+
+            throw;
+        }
+
+        try {}
+        catch (Exception outer)
+        {
+            logger.LogError(outer, "Message!");
+            try {}
+            catch (Exception inner)
+            {
+                throw;
+            }
+        }
+
+        try {}
+        catch (Exception outer)                                         // FN - Only the main catch body is visited
+        {
+            logger.LogError(outer, "Message!");
+            try {}
+            catch (Exception inner)
+            {
+                throw outer;
+            }
+        }
+    }
+
+    public void NoCatch(ILogger logger, Exception exception)
+    {
+        logger.LogError(exception, "Message!");                         // Compliant - there is not catch block
+        throw exception;
+    }
+
+    public void Filtering(ILogger logger)
+    {
+        try {}
+        catch (Exception ex) when (ex is DivideByZeroException d)       // Noncompliant
+        {
+            logger.LogError(ex, "Message!");                            // Secondary
+            throw;                                                      // Secondary
+        }
+
+        try {}
+        catch (Exception ex) when (ex is DivideByZeroException d)       // FN
+        {
+            logger.LogError(ex, "Message!");
+            throw d;
+        }
+    }
+
+    private void With(CustomLogger logger)
+    {
+        try { }
+        catch (Exception exception)                                     // The exception is only logged.
+        {
+            logger.LogError(exception, "Message!");
+        }
+    }
+
+    public void LoggingFromAnotherMethod(ILogger logger)
+    {
+        try { }
+        catch (Exception e)                                             // FN
+        {
+            Log(logger, e);
+            throw;
+        }
+    }
+
+    private void Log(ILogger logger, Exception e)
+    {
+        logger.LogCritical("Message!");                                 // Compliant - we do not check this
+        throw e;
+    }
+
+    public void LogFromLambda(ILogger logger)
+    {
+        try { }
+        catch (Exception e)                                             // Noncompliant
+        {
+            Call(() => logger.LogCritical(e, "Message!"));              // Secondary
+            throw;                                                      // Secondary
+        }
+
+        try { }
+        catch (Exception e)
+        {
+            Action<Exception, string> LogCritical = (Exception ex, string message) => { };
+            LogCritical(e, "Message");                                 // Compliant
+            throw;
+        }
+    }
+
+    private void ReAssignment(ILogger logger)
+    {
+        try { }
+        catch (InvalidOperationException e)                             // FN
+        {
+            var other = e;
+            logger.LogWarning(other, "Message!");
+            throw;
+        }
+        catch (Exception e)                                             // FN
+        {
+            var other = e;
+            logger.LogWarning(other, "Message!");
+            throw e;
+        }
+    }
+
+    public void LogPartial_Compliant(ILogger logger)
+    {
+        try { }
+        catch (Exception e)
+        {
+            logger.LogWarning("Message!" + e.Message);
+            logger.LogWarning(e.Message);
+            logger.LogWarning(e?.InnerException, "Message!");
+            logger.LogWarning(e.InnerException.InnerException, "Message!");
+            logger.LogWarning((Exception)e.InnerException, "Message!");
+
+            throw;
+        }
+    }
+    public void LogPartial_Notcompliant(ILogger logger)
+    {
+        try { }
+        catch (InvalidCastException e)                                  // Noncompliant
+        {
+            logger.LogWarning(e.InnerException, "Message!");            // Secondary
+            throw;                                                      // Secondary
+        }
+    }
+
+    public void LogFromCatchWithoutException(ILogger logger)
+    {
+        try { }
+        catch
+        {
+            logger.LogWarning("Message! Stack trace: {StackTrace}", new System.Diagnostics.StackTrace());
+            throw;
+        }
+    }
+
+    private void Call(Action action)
+    {
+        action();
+    }
+
+    private class CustomLogger : ILogger
+    {
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter) { }
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+    }
+}
