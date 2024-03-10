cab7a50bd934b74f97f29faf5f2d4312d46a8623
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-03-10 23:08:07 +0100

    Add ITs

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6675.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace IntentionalFindings
+{
+    public class S6675
+    {
+        public void Method(TraceSwitch traceSwitch)
+        {
+            Trace.WriteLineIf(traceSwitch.TraceError, "Message"); // Noncompliant (S6675) {{Don't use Trace.WriteLineIf with TraceSwitch level checks.}}
+        }
+    }
+}
