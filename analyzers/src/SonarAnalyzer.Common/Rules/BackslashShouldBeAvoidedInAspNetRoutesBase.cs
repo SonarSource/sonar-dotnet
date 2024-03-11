1b7ec0ca2bffb4e4b31d627440e0d1d36095c184
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-03-11 14:33:44 +0100

    Fix code smell - unnecessary using

--- analyzers/src/SonarAnalyzer.Common/Rules/BackslashShouldBeAvoidedInAspNetRoutesBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/BackslashShouldBeAvoidedInAspNetRoutesBase.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Xml.Linq;
-
 namespace SonarAnalyzer.Rules;
 
 public abstract class BackslashShouldBeAvoidedInAspNetRoutesBase<TSyntaxKind> : SonarDiagnosticAnalyzer<TSyntaxKind>
