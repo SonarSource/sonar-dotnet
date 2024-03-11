0d253050add6009c4461069fc92ad24eb95974e5
Zsolt Kolbay <121798625+zsolt-kolbay-sonarsource@users.noreply.github.com>
2024-03-11 17:37:00 +0100

    New rule S6675: Trace.WriteLineIf should not be used with TraceSwitch levels (#8881)

---
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/DontUseTraceSwitchLevels.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarAnalyzer.Rules.CSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class DontUseTraceSwitchLevels : SonarDiagnosticAnalyzer
+{
+    private const string DiagnosticId = "S6675";
+    private const string MessageFormat = "'Trace.{0}' should not be used with 'TraceSwitch' levels.";
+
+    private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
+
+    private static readonly ImmutableArray<string> TraceSwitchProperties = ImmutableArray.Create(
+        "Level",
+        "TraceError",
+        "TraceInfo",
+        "TraceVerbose",
+        "TraceWarning");
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
+            {
+                var invocation = (InvocationExpressionSyntax)c.Node;
+                if (invocation.GetName() is "WriteIf" or "WriteLineIf"
+                    && invocation.ArgumentList.Arguments.Count > 1
+                    && c.SemanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol
+                    && methodSymbol.ContainingType.Is(KnownType.System_Diagnostics_Trace)
+                    && UsesTraceSwitchAsCondition(c.SemanticModel, methodSymbol, invocation) is { } traceSwitchProperty)
+                {
+                    c.ReportIssue(Diagnostic.Create(Rule, traceSwitchProperty.GetLocation(), invocation.GetName()));
+                }
+            },
+            SyntaxKind.InvocationExpression);
+
+    private static SyntaxNode UsesTraceSwitchAsCondition(SemanticModel model, IMethodSymbol methodSymbol, InvocationExpressionSyntax invocation)
+    {
+        var lookup = new CSharpMethodParameterLookup(invocation.ArgumentList, methodSymbol);
+        lookup.TryGetSyntax("condition", out var expressions);
+        var conditionArgument = expressions[0];
+        return conditionArgument.DescendantNodesAndSelf().FirstOrDefault(x => x is MemberAccessExpressionSyntax memberAccess
+            && TraceSwitchProperties.Contains(memberAccess.GetName())
+            && model.GetTypeInfo(memberAccess.Expression).Type.DerivesFrom(KnownType.System_Diagnostics_TraceSwitch));
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/DontUseTraceSwitchLevels.cs
@@ -0,0 +1,121 @@
+﻿using System.Diagnostics;
+using AliasedTrace = System.Diagnostics.Trace;
+using AliasedTraceSwitch = System.Diagnostics.TraceSwitch;
+
+public class Program
+{
+    private TraceSwitch _traceSwitch;
+
+    public void Noncompliant_TraceIfMethods(bool condition)
+    {
+        Trace.WriteIf(condition, "Message");                    // Compliant
+        Trace.WriteLineIf(condition, "Message");
+
+        Trace.WriteIf(_traceSwitch.TraceError, "Message");      // Noncompliant {{'Trace.WriteIf' should not be used with 'TraceSwitch' levels.}}
+        //            ^^^^^^^^^^^^^^^^^^^^^^^
+        Trace.WriteLineIf(_traceSwitch.TraceError, "Message");  // Noncompliant {{'Trace.WriteLineIf' should not be used with 'TraceSwitch' levels.}}
+    }
+
+    public void Compliant_TraceMethods(string arg)
+    {
+        Trace.Write("Message");
+        Trace.Write("Message", "Category");
+
+        Trace.WriteLine("Message");
+        Trace.WriteLine("Message", "Category");
+
+        Trace.TraceError("Message");
+        Trace.TraceError("Message: {0}", arg);
+
+        Trace.TraceInformation("Message");
+        Trace.TraceInformation("Message: {0}", arg);
+
+        Trace.TraceWarning("Message");
+        Trace.TraceWarning("Message: {0}", arg);
+    }
+
+    public void WriteIf_Overloads(bool condition)
+    {
+        Trace.WriteIf(_traceSwitch.TraceError, "Message");                  // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceError, 42);                         // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceError, "Message", "Category");      // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceError, 42, "Category");             // Noncompliant
+
+        Trace.WriteLineIf(_traceSwitch.TraceError, "Message");              // Noncompliant
+        Trace.WriteLineIf(_traceSwitch.TraceError, 42);                     // Noncompliant
+        Trace.WriteLineIf(_traceSwitch.TraceError, "Message", "Category");  // Noncompliant
+        Trace.WriteLineIf(_traceSwitch.TraceError, 42, "Category");         // Noncompliant
+    }
+
+    public void TraceSwitch_Properties(bool condition)
+    {
+        Trace.WriteIf(_traceSwitch.TraceError, "Message");                  // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceInfo, "Message");                   // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceVerbose, "Message");                // Noncompliant
+        Trace.WriteIf(_traceSwitch.TraceWarning, "Message");                // Noncompliant
+        Trace.WriteIf(_traceSwitch.Level == TraceLevel.Error, "Message");   // Noncompliant
+        Trace.WriteIf(_traceSwitch.Level != TraceLevel.Off, "Message");     // Noncompliant
+    }
+
+    public void Not_TraceClass(string arg)
+    {
+        Debug.WriteIf(_traceSwitch.TraceError, "Message");                  // Compliant - the method is not from the System.Diagnostics.Trace class
+        Debug.WriteIf(_traceSwitch.TraceError, "Message: {0}", arg);
+        Debug.WriteLineIf(_traceSwitch.TraceError, "Message");
+        Debug.WriteLineIf(_traceSwitch.TraceError, "Message: {0}", arg);
+    }
+
+    public void Aliased_Trace(AliasedTraceSwitch aliasedTraceSwitch)
+    {
+        AliasedTrace.WriteIf(aliasedTraceSwitch.TraceError, "Message");     // Noncompliant
+        AliasedTrace.WriteLineIf(aliasedTraceSwitch.TraceError, "Message"); // Noncompliant
+    }
+
+    public void Custom_TraceSwitch(CustomTraceSwitch customTraceSwitch)
+    {
+        AliasedTrace.WriteIf(customTraceSwitch.TraceError, "Message");      // Noncompliant
+        AliasedTrace.WriteLineIf(customTraceSwitch.TraceError, "Message");  // Noncompliant
+    }
+
+    public class CustomTraceSwitch : TraceSwitch
+    {
+        public CustomTraceSwitch(string displayName, string description) : base(displayName, description)
+        {
+        }
+    }
+}
+
+namespace MyNamespace
+{
+    public class Test
+    {
+        private System.Diagnostics.TraceSwitch _regularTraceSwitch;
+        private TraceSwitch _fakeTraceSwitch;
+
+        public void Using_CustomClass(string arg)
+        {
+            Trace.WriteIf(_regularTraceSwitch.TraceError, "Message");                   // Compliant - the method is not from the System.Diagnostics.Trace class
+            Trace.WriteIf(_regularTraceSwitch.TraceError, "Message: {0}", arg);
+            Trace.WriteLineIf(_regularTraceSwitch.TraceError, "Message");
+            Trace.WriteLineIf(_regularTraceSwitch.TraceError, "Message: {0}", arg);
+
+            System.Diagnostics.Trace.WriteIf(_fakeTraceSwitch.TraceError, "Message");   // Compliant - the TraceSwitch object in the if statement is not an instance of the System.Diagnostics.TraceSwitch class
+            System.Diagnostics.Trace.WriteIf(_fakeTraceSwitch.TraceError, "Message: {0}", arg);
+            System.Diagnostics.Trace.WriteLineIf(_fakeTraceSwitch.TraceError, "Message");
+            System.Diagnostics.Trace.WriteLineIf(_fakeTraceSwitch.TraceError, "Message: {0}", arg);
+        }
+    }
+
+    public static class Trace
+    {
+        public static void WriteIf(bool condition, string message) { }
+        public static void WriteIf(bool condition, string message, params object[] args) { }
+        public static void WriteLineIf(bool condition, string message) { }
+        public static void WriteLineIf(bool condition, string message, params object[] args) { }
+    }
+
+    public class TraceSwitch
+    {
+        public bool TraceError { get; }
+    }
+}
