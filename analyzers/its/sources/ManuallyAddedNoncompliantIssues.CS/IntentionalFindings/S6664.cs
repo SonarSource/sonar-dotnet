2d61a03b6dbec9d8b344e3d5fa3db434ea2089eb
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-03-13 18:02:40 +0100

    Add ITs

---
+++ analyzers/its/sources/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6664.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace IntentionalFindings
+{
+    public class S6664
+    {
+        public void Method(ILogger logger)
+        {
+            logger.LogError("Error 1");     // Noncompliant (S6664)
+            logger.LogError("Error 2");
+        }
+    }
+}
