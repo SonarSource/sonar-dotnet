ae757708d0eb5a4bd954e2bcac827e4bf509f862
Martin Strecker <martin.strecker@sonarsource.com>
2024-03-14 15:55:57 +0100

    Move Compliant() and NoncompliantKeyVariations to parameterized tests.

--- analyzers/tests/SonarAnalyzer.Test/Rules/UseModelBindingTest.cs
+++ analyzers/tests/SonarAnalyzer.Test/Rules/UseModelBindingTest.cs
@@ -40,5 +40,71 @@ public class UseModelBindingTest
     [TestMethod]
     public void UseModelBinding_AspNetCore_CS() =>
         builderAspNetCore.AddPaths("UseModelBinding_AspNetCore.cs").Verify();
+
+    [DataTestMethod]
+    [DataRow("Form")]
+    [DataRow("Query")]
+    [DataRow("RouteValues")]
+    [DataRow("Headers")]
+    public void NonCompliantAccess(string property) =>
+        builderAspNetCore.AddSnippet($$""""
+            using Microsoft.AspNetCore.Http;
+            using Microsoft.AspNetCore.Mvc;
+            using Microsoft.AspNetCore.Mvc.Filters;
+            using System;
+            using System.Linq;
+            using System.Threading.Tasks;
+
+            public class TestController : Controller
+            {
+                async Task NoncompliantKeyVariations()
+                {
+                    _ = Request.{{property}}[@"key"];                                 // Noncompliant
+                    _ = Request.{{property}}.TryGetValue(@"key", out _);              // Noncompliant
+                    _ = Request.{{property}}["""key"""];                              // Noncompliant
+                    _ = Request.{{property}}.TryGetValue("""key""", out _);           // Noncompliant
+            
+                    const string key = "id";
+                    _ = Request.{{property}}[key];                                    // Noncompliant
+                    _ = Request.{{property}}.TryGetValue(key, out _);                 // Noncompliant
+                    _ = Request.{{property}}[$"prefix.{key}"];                        // Noncompliant
+                    _ = Request.{{property}}.TryGetValue($"prefix.{key}", out _);     // Noncompliant
+                    _ = Request.{{property}}[$"""prefix.{key}"""];                    // Noncompliant
+                    _ = Request.{{property}}.TryGetValue($"""prefix.{key}""", out _); // Noncompliant
+            
+                    _ = Request.{{property}}[key: "id"];                              // Noncompliant
+                    _ = Request.{{property}}.TryGetValue(value: out _, key: "id");    // Noncompliant
+                }
+            }
+            """").Verify();
+
+    [TestMethod]
+    [CombinatorialData]
+    public void CompliantAccess(
+        [DataValues(
+            "_ = {0}.Keys",
+            "_ = {0}.Count",
+            "foreach (var kvp in {0}) {{ }}",
+            "_ = {0}.Select(x => x);",
+            "_ = {0}[key];                    // Compliant: The accessed key is not a compile time constant")] string statementFormat,
+        [DataValues("Request", "this.Request", "ControllerContext.HttpContext.Request", "request")] string request,
+        [DataValues("Form", "Headers", "Query", "RouteValues")] string property,
+        [DataValues("[FromForm]", "[FromQuery]", "[FromRoute]", "[FromHeader]")] string attribute) =>
+        builderAspNetCore.AddSnippet($$"""
+            using Microsoft.AspNetCore.Http;
+            using Microsoft.AspNetCore.Mvc;
+            using Microsoft.AspNetCore.Mvc.Filters;
+            using System;
+            using System.Linq;
+            using System.Threading.Tasks;
+
+            public class TestController : Controller
+            {
+                async Task Compliant({{attribute}} string key, HttpRequest request)
+                {
+                    {{string.Format(statementFormat, $"{request}.{property}")}};
+                }
+            }
+            """).Verify();
 #endif
 }
