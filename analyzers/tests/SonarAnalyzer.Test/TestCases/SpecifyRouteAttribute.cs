42f16a996e22e2e64381ca70e5ac218127ed98f1
Martin Strecker <martin.strecker@sonarsource.com>
2024-03-18 20:03:46 +0100

    Scaffolding

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/SpecifyRouteAttribute.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+public class NoActionHasHttpAttributeWithRouteController : Controller
+{
+    public IActionResult Index() => View();   // Compliant
+
+    [HttpGet]
+    public IActionResult Index2() => View();  // Compliant, default behavior if not route template is defined
+
+    public IActionResult Error() => View();  // Compliant
+}
+
+public class ActionHasHttpAttributeWithRouteController : Controller
+{
+    [HttpGet("GetObject")]          // Noncompliant
+    public IActionResult Get() => View();
+
+    [HttpPost("CreateObject")]     // Noncompliant
+    public IActionResult Post() => View();
+
+    [HttpPut("UpdateObject")]      // Noncompliant
+    public IActionResult Put() => View();
+
+    [HttpDelete("DeleteObject")]  // Noncompliant
+    public IActionResult Delete() => View();
+
+    [HttpPatch("PatchObject")]    // Noncompliant
+    public IActionResult Patch() => View();
+
+    [HttpHead("Head")]           // Noncompliant
+    public IActionResult HttpHead() => View();
+
+    [HttpOptions("Options")]     // Noncompliant
+    public IActionResult HttpOptions() => View();
+}
+
+public class WithUserDefinedAttribute : Controller
+{
+    [MyHttpMethod("Test")]       // Compliant, behavior is user defined
+    public IActionResult Index() => View();
+
+    private sealed class MyHttpMethodAttribute(string template) : HttpMethodAttribute([template]) { }
+}
