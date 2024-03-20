3b34194965f5dfd51e75379dead29cd3c7955a65
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-03-20 16:02:33 +0100

    Add projects to ManuallyAddedNoncompliantIssues.CS

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/AspNet4x/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNet4x.MvcApplication" Language="C#" %>
---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/AspNetCore8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
