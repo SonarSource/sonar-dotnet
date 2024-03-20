372b35ba144d78d3de6296c78ab6c3749291324e
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-03-20 16:11:18 +0100

    Add S6930 to .NET ITs

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/AspNet4x/Controllers/S6930Controller.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AspNet4x.Controllers
+{
+    [Route(@"A\[controller]")]    // Noncompliant (S6930)
+    //     ^^^^^^^^^^^^^^^^^
+    public class S6930Controller : Controller
+    {
+        [Route(@"A\[action]")]    // Noncompliant (S6930)
+        //     ^^^^^^^^^^^^^
+        public ActionResult Index() => View();
+    }
+}
