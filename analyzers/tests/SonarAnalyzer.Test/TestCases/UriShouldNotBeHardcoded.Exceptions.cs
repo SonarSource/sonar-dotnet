bace93ecf7d0533525fc75fbe76186024a69e472
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-03-21 14:49:54 +0100

    repro

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UriShouldNotBeHardcoded.Exceptions.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+
+public class RuleExceptions
+{
+    // Repro for https://github.com/SonarSource/sonar-dotnet/issues/8967
+    static XmlNamespaceManager CreateNamespaceManager()
+    {
+        var namespaceManager = new XmlNamespaceManager(new NameTable());
+        namespaceManager.AddNamespace("ffc", "http://www.redacted.com/namespaces/ffc"); // Noncompliant FP
+        namespaceManager.AddNamespace("dex", "http://www.redacted.com/namespaces/dex"); // Noncompliant FP
+        return namespaceManager;
+    }
+}
