5ea316942be398397760e7072e92bb663bc982af
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-03-21 14:37:24 +0100

    S6930: Add .NET ITs (#8963)

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/AspNetCore6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
