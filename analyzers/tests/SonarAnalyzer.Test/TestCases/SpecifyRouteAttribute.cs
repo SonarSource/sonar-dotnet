69ef0d23ffed6d408c9e5d5bb0a1a6175dce5f90
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-03-21 10:53:57 +0100

    Add more test cases

--- analyzers/tests/SonarAnalyzer.Test/TestCases/SpecifyRouteAttribute.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/SpecifyRouteAttribute.cs
@@ -1,35 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Routing;
 
 using MA = Microsoft.AspNetCore;
 
-public class RouteTemplateIsNotSpecified : Controller
+public class RouteTemplateIsNotSpecifiedController : Controller
 {
-    public IActionResult Index() => View();                 // Compliant
+    public IActionResult Index() => View();                                     // Compliant
 
     [HttpGet]
-    public IActionResult Index2() => View();                // Compliant
+    public IActionResult Index2() => View();                                    // Compliant
 
     [HttpGet()]
-    public IActionResult Index3() => View();                // Compliant
+    public IActionResult Index3() => View();                                    // Compliant
 
     [HttpGetAttribute]
-    public IActionResult Index4() => View();                // Compliant
+    public IActionResult Index4() => View();                                    // Compliant
 
     [Microsoft.AspNetCore.Mvc.HttpGet]
-    public IActionResult Index5() => View();                // Compliant
+    public IActionResult Index5() => View();                                    // Compliant
 
     [MA.Mvc.HttpGet]
-    public IActionResult Index6() => View();                // Compliant
+    public IActionResult Index6() => View();                                    // Compliant
 
     [method: HttpGet]
-    public IActionResult Index7() => View();                // Compliant
+    public IActionResult Index7() => View();                                    // Compliant
 
-    public IActionResult Error() => View();                 // Compliant
+    public IActionResult Error() => View();                                     // Compliant
 }
 
-public class RouteTemplatesAreSpecified : Controller        // Noncompliant [controller] {{Specify the RouteAttribute when an HttpMethodAttribute is specified at an action level.}}
-//           ^^^^^^^^^^^^^^^^^^^^^^^^^^
+public class RouteTemplatesAreSpecifiedController : Controller                  // Noncompliant [controller] {{Specify the RouteAttribute when an HttpMethodAttribute is specified at an action level.}}
+//           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 {
     private const string ConstantRoute = "ConstantRoute";
 
@@ -39,73 +40,118 @@ public class RouteTemplatesAreSpecified : Controller        // Noncompliant [con
 
     [HttpGet("GetFirst")]
     [HttpGet("GetSecond")]
-    public IActionResult GetMultipleTemplates() => View();  // Secondary [controller]
+    public IActionResult GetMultipleTemplates() => View();                      // Secondary [controller]
 
     [HttpGet("GetFirst")]
-    [HttpPut("GetSecond")]
-    public IActionResult Mix() => View();                   // Secondary [controller]
+    [HttpPut("PutFirst")]
+    public IActionResult Mix() => View();                                       // Secondary [controller]
 
     [HttpPost("CreateObject")]
-    public IActionResult Post() => View();                  // Secondary [controller]
+    public IActionResult Post() => View();                                      // Secondary [controller]
 
     [HttpPut("UpdateObject")]
-    public IActionResult Put() => View();                   // Secondary [controller]
+    public IActionResult Put() => View();                                       // Secondary [controller]
 
     [HttpDelete("DeleteObject")]
-    public IActionResult Delete() => View();                // Secondary [controller]
+    public IActionResult Delete() => View();                                    // Secondary [controller]
 
     [HttpPatch("PatchObject")]
-    public IActionResult Patch() => View();                 // Secondary [controller]
+    public IActionResult Patch() => View();                                     // Secondary [controller]
 
     [HttpHead("Head")]
-    public IActionResult HttpHead() => View();              // Secondary [controller]
+    public IActionResult HttpHead() => View();                                  // Secondary [controller]
 
     [HttpOptions("Options")]
-    public IActionResult HttpOptions() => View();           // Secondary [controller]
+    public IActionResult HttpOptions() => View();                               // Secondary [controller]
 
     [Route("details")]
-    public IActionResult GetDetails() => View();            // FN
+    public IActionResult GetDetails() => View();                                // FN
+
+    [Route("[controller]/List/{sortBy}/{direction}")]
+    [HttpGet("[controller]/Search/{sortBy}/{direction}")]
+    public IActionResult RouteAndMethodMix(string sortBy) => View();            // Secondary [controller]
 
     [HttpGet("details", Order = 1)]
-    public IActionResult GetDetails2() => View();           // Secondary [controller]
+    public IActionResult MultipleProperties1() => View();                       // Secondary [controller]
 
     [HttpGet("details", Order = 1, Name = "Details")]
-    public IActionResult GetDetails3() => View();           // Secondary [controller]
+    public IActionResult MultipleProperties2() => View();                       // Secondary [controller]
+
+    [HttpGet("details", Name = "Details", Order = 1)]
+    public IActionResult MultipleProperties3() => View();                       // Secondary [controller]
 
     [HttpGet(ConstantRoute)]
-    public IActionResult GetDetails4() => View();           // Secondary [controller]
+    public IActionResult Constant() => View();                                  // Secondary [controller]
 
     [HttpGet("""
              ConstantRoute
              """)]
-    public IActionResult GetDetails5() => View();           // Secondary [controller]
+    public IActionResult Constant2() => View();                                 // Secondary [controller]
 
     [HttpGet($"Route {ConstantRoute}")]
-    public IActionResult GetDetails6() => View();           // Secondary [controller]
+    public IActionResult Interpolation1() => View();                            // Secondary [controller]
 
     [HttpGet($"""
              {ConstantRoute}
              """)]
-    public IActionResult GetDetails7() => View();           // Secondary [controller]
+    public IActionResult Interpolation2() => View();                            // Secondary [controller]
 
     [HttpGet("GetObject")]
-    public ActionResult GetDetails8() => View();            // Secondary [controller]
+    public ActionResult WithActionResult() => View();                           // Secondary [controller]
 
     // [HttpPost("Comment")]
     public IActionResult Comment() => View();
 }
 
-public class WithUserDefinedAttribute : Controller          // Noncompliant [customAttribute]
+public class WithUserDefinedAttributeController : Controller                    // Noncompliant [customAttribute]
 {
     [MyHttpMethod("Test")]
-    public IActionResult Index() => View();                 // Secondary [customAttribute]
+    public IActionResult Index() => View();                                     // Secondary [customAttribute]
 
     private sealed class MyHttpMethodAttribute(string template) : HttpMethodAttribute([template]) { }
 }
 
-public class WithCustomGetAttribute : Controller
+public class WithCustomGetAttributeController : Controller                      // Noncompliant [custom-get-attribute]
 {
-    [HttpGet("Test")]                                       // Compliant, behavior is user-defined
-    public IActionResult Index() => View();
+    [HttpGet("Test")]
+    public IActionResult Index() => View();                                     // Secondary [custom-get-attribute]
+
     private sealed class HttpGetAttribute(string template) : HttpMethodAttribute([template]) { }
 }
+
+public class WithCustomController : DerivedController                           // Noncompliant [derivedController]
+{
+    [HttpGet("Test")]
+    public IActionResult Index() => View();                                     // Secondary [derivedController]
+}
+
+[Controller]
+public class WithAttributeController                                            // Noncompliant [attribute-controller]
+{
+    [HttpGet("Test")]
+    public string Index() => "Hi!";                                             // Secondary [attribute-controller]
+}
+
+public class WithAttributeControllerUsingInheritanceController : Endpoint       // FN
+{
+    [HttpGet("Test")]
+    public string Index() => "Hi!";                                             // FN
+}
+
+[Route("api/[controller]")]
+public class NamedController                                                    // FN
+{
+    [HttpGet("Test")]
+    public string Index() => "Hi!";                                             // FN
+}
+
+[NonController]
+public class NonController
+{
+    [HttpGet("Test")]
+    public string Index() => "Hi!";
+}
+public class DerivedController : Controller { }
+
+[Controller]
+public class Endpoint {}
