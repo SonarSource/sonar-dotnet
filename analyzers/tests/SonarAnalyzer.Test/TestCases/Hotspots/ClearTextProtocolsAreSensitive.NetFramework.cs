bf1af89adf3268044cc90c03877b094912fc0e9d
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-04-09 10:00:41 +0200

    Address comments

--- analyzers/tests/SonarAnalyzer.Test/TestCases/Hotspots/ClearTextProtocolsAreSensitive.NetFramework.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/Hotspots/ClearTextProtocolsAreSensitive.NetFramework.cs
@@ -1,14 +1,11 @@
 ﻿using System.Web.Services;
 using System.Web.Services.Protocols;
 
-namespace Tests.Diagnostics
+// https://github.com/SonarSource/sonar-dotnet/issues/9017
+public class MyWebService : WebService
 {
-    // https://github.com/SonarSource/sonar-dotnet/issues/9017
-    public class MyWebService : WebService
-    {
-        [SoapDocumentMethod(Action = "http://www.contoso.com/GetUserName")] // Noncompliant - FP
-        public string GetUserName() {
-            return User.Identity.Name;
-        }
+    [SoapDocumentMethod(Action = "http://www.contoso.com/GetUserName")] // Noncompliant - FP
+    public string GetUserName() {
+        return User.Identity.Name;
     }
 }
