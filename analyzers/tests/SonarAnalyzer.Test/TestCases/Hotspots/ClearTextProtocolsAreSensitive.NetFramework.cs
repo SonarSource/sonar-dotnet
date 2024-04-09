47f3e974844f0aa28c2e6217cf5be242a6286722
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-04-09 10:18:35 +0200

    Add reproducer for #9017 (#9025)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/Hotspots/ClearTextProtocolsAreSensitive.NetFramework.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Services;
+using System.Web.Services.Protocols;
+
+// https://github.com/SonarSource/sonar-dotnet/issues/9017
+public class MyWebService : WebService
+{
+    [SoapDocumentMethod(Action = "http://www.contoso.com/GetUserName")] // Noncompliant - FP
+    public string GetUserName() {
+        return User.Identity.Name;
+    }
+}
