f725f1a9eef39c1e3fca33fbe74aa16d1987fe34
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-04-10 17:27:52 +0200

    New Rule T0002: Place => correctly

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/ArrowLocation.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarAnalyzer.Rules.CSharp.Styling;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class ArrowLocation : StylingAnalyzer
+{
+    public ArrowLocation() : base("T0002", "Place the arrow at the end of the previous line.") { }
+
+    protected override void Initialize(SonarAnalysisContext context)
+    {
+        context.RegisterNodeAction(c => Verify(c, ((LambdaExpressionSyntax)c.Node).ArrowToken), SyntaxKind.SimpleLambdaExpression);
+        context.RegisterNodeAction(c => Verify(c, ((LambdaExpressionSyntax)c.Node).ArrowToken), SyntaxKind.ParenthesizedLambdaExpression);
+        context.RegisterNodeAction(c => Verify(c, ((ArrowExpressionClauseSyntax)c.Node).ArrowToken), SyntaxKind.ArrowExpressionClause);
+        context.RegisterNodeAction(c => Verify(c, ((SwitchExpressionArmSyntax)c.Node).EqualsGreaterThanToken), SyntaxKind.SwitchExpressionArm);
+    }
+
+    private void Verify(SonarSyntaxNodeReportingContext context, SyntaxToken arrowToken)
+    {
+        if (!arrowToken.IsMissing && Line(arrowToken) > Line(arrowToken.GetPreviousToken()))
+        {
+            context.ReportIssue(Rule, arrowToken);
+        }
+    }
+
+    private static int Line(SyntaxToken token) =>
+        token.GetLocation().GetLineSpan().StartLinePosition.Line;
+}
