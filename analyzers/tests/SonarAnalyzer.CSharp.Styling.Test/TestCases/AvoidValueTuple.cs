e8639a91ba863481f7442bcf6ac801d0592cc531
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-04-12 09:25:03 +0200

    New Rule T0003: Don't use ValueTuple in production code (#9066)

---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/AvoidValueTuple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Sample
+{
+    public (string, int) ReturnedType() // Noncompliant {{Do not use ValueTuple in the production code due to missing System.ValueTuple.dll.}}
+    //     ^^^^^^^^^^^^^
+    {
+        return ("Lorem", 42);   // Noncompliant
+        //     ^^^^^^^^^^^^^
+    }
+
+    public (string Name, int Count) NamedReturnedType() =>  // Noncompliant
+        ("Lorem", 42);                                      // Noncompliant
+
+    public void Use()
+    {
+        (string, int) noName;                   // Noncompliant
+        (string Name, int Count) withName;      // Noncompliant
+
+        noName = ("Lorem", 42);     // Noncompliant
+        withName = ("Lorem", 42);   // Noncompliant
+    }
+
+    public System.Tuple<string, int> ExplicitType() // Complaint
+    {
+        return new Tuple<string, int>("Lorem", 42);
+    }
+}
