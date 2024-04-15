8be97b8071dc4b585b0f22efa7e97c0bab49ad95
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-04-15 08:34:10 +0200

    New Rule T0007: Use "is (not) null" for null checks (#9086)

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/NullPatternMatching.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarAnalyzer.Rules.CSharp.Styling;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class NullPatternMatching : StylingAnalyzer
+{
+    public NullPatternMatching() : base("T0007", "Use 'is {0}null' pattern matching.") { }
+
+    protected override void Initialize(SonarAnalysisContext context)
+    {
+        context.RegisterNodeAction(c => Validate(c, null), SyntaxKind.EqualsExpression);
+        context.RegisterNodeAction(c => Validate(c, "not "), SyntaxKind.NotEqualsExpression);
+    }
+
+    private void Validate(SonarSyntaxNodeReportingContext context, string messageInfix)
+    {
+        var binary = (BinaryExpressionSyntax)context.Node;
+        if (binary.Left.IsKind(SyntaxKind.NullLiteralExpression) || binary.Right.IsKind(SyntaxKind.NullLiteralExpression))
+        {
+            context.ReportIssue(Rule, binary, messageInfix);
+        }
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/NullPatternMatching.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom;
+
+public class Sample
+{
+    public void Method(object o, int a, int b, object another)
+    {
+        if (o == null)  // Noncompliant {{Use 'is null' pattern matching.}}
+        //  ^^^^^^^^^
+        { }
+        if (a > b || (a < b && o == null))  // Noncompliant
+        //                     ^^^^^^^^^
+        { }
+
+        if (o != null)  // Noncompliant {{Use 'is not null' pattern matching.}}
+        //  ^^^^^^^^^
+        { }
+
+        _ = !(o == null);   // Noncompliant
+        //    ^^^^^^^^^
+        _ = !(o != null);   // Noncompliant
+        //    ^^^^^^^^^
+        _ = null == o;      // Noncompliant
+        _ = null != o;      // Noncompliant
+
+        _ = o is null;
+        _ = o is not null;
+        _ = o == "null";
+        _ = o != "null";
+        _ = o == this;
+        _ = o != this;
+        _ = o == another;
+        _ = o != another;
+        _ = o == Invocation();
+        _ = o != Invocation();
+
+        Use(o == null);     // Noncompliant
+        while (o == null)   // Noncompliant
+        { }
+
+        Use(o ==);  // Error [CS1525] Invalid expression term ';'
+        Use(== o);  // Error [CS1525] Invalid expression term '=='
+    }
+
+    private object Invocation() => null;
+
+    private void Use(bool b) { }
+
+    public void CustomOperator()
+    {
+        var s = new Sample();
+        if(s == null)   // Noncompliant
+        {
+            // This is always visited due to overriden operator => we don't care, it's a bad idea anyway
+        }
+    }
+
+    public static bool operator ==(Sample a, Sample b) =>
+        b is null;
+
+    public static bool operator !=(Sample a, Sample b) =>
+        b is not null;
+}
