66c99a1f536e14eca0adac66110a42871b9a24c1
Martin Strecker <martin.strecker@sonarsource.com>
2024-04-15 14:16:13 +0200

    Try annotate VB extension fo CI

--- analyzers/src/SonarAnalyzer.VisualBasic/Extensions/ExpressionSyntaxExtensions.Roslyn.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/Extensions/ExpressionSyntaxExtensions.Roslyn.cs
@@ -2,15 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.CodeDom.Compiler;
-
 namespace Microsoft.CodeAnalysis.VisualBasic.Extensions;
 
-[GeneratedCode("Copied and converted from Roslyn", "5a1cc5f83e4baba57f0355a685a5d1f487bfac66")]
+[CodeCopiedFrom(TrustedCodeSource.Roslyn,
+    "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb")]
 internal static class ExpressionSyntaxExtensions
 {
-    // Copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L362
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L362")]
     public static bool IsWrittenTo(this ExpressionSyntax expression, SemanticModel semanticModel, CancellationToken cancellationToken)
     {
         if (expression == null)
@@ -58,8 +57,8 @@ internal static class ExpressionSyntaxExtensions
         return false;
     }
 
-    // copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L325
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L325")]
     public static bool IsOnlyWrittenTo(this ExpressionSyntax expression)
     {
         if (expression.IsRightSideOfDot())
@@ -90,29 +89,29 @@ internal static class ExpressionSyntaxExtensions
         return false;
     }
 
-    // Copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L73
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L73")]
     public static bool IsRightSideOfDot(this ExpressionSyntax expression)
     {
         return expression.IsSimpleMemberAccessExpressionName() || expression.IsRightSideOfQualifiedName();
     }
 
-    // Copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L56
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L56")]
     public static bool IsSimpleMemberAccessExpressionName(this ExpressionSyntax expression)
     {
         return expression.IsParentKind(SyntaxKind.SimpleMemberAccessExpression) && ((MemberAccessExpressionSyntax)expression.Parent).Name == expression;
     }
 
-    // Copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L78
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L78")]
     public static bool IsRightSideOfQualifiedName(this ExpressionSyntax expression)
     {
         return expression.IsParentKind(SyntaxKind.QualifiedName) && ((QualifiedNameSyntax)expression.Parent).Right == expression;
     }
 
-    // Copied and converted from
-    // https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L277
+    [CodeCopiedFrom(TrustedCodeSource.Roslyn,
+        "https://github.com/dotnet/roslyn/blob/5a1cc5f83e4baba57f0355a685a5d1f487bfac66/src/Workspaces/SharedUtilitiesAndExtensions/Compiler/VisualBasic/Extensions/ExpressionSyntaxExtensions.vb#L277")]
     public static bool IsInRefContext(this ExpressionSyntax expression, SemanticModel semanticModel, CancellationToken cancellationToken)
     {
         var simpleArgument = expression?.Parent as SimpleArgumentSyntax;
