e7872d18c1b92b0d61076d607c83d0a834836173
Martin Strecker <martin.strecker@sonarsource.com>
2024-04-18 12:46:53 +0200

    Simplify test case

--- analyzers/tests/SonarAnalyzer.Test/TestCases/UseAwaitableMethod_CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UseAwaitableMethod_CSharp9.cs
@@ -2,6 +2,5 @@
 using System.Threading.Tasks;
 using System;
 
-    var ms = new MemoryStream();
-    ms.Dispose(); // Noncompliant {{Await DisposeAsync instead.}}
-//  ^^^^^^^^^^^^
+var ms = new MemoryStream();
+ms.Dispose(); // Noncompliant {{Await DisposeAsync instead.}}
