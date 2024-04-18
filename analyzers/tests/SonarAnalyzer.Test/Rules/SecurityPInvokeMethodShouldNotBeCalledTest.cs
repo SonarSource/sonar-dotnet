6030db75d8e93ad82c413cb5ddd335836e8ad73c
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-04-18 17:27:09 +0200

    Move SecurityPInvokeMethodShouldNotBeCalled

--- analyzers/tests/SonarAnalyzer.Test/Rules/SecurityPInvokeMethodShouldNotBeCalledTest.cs
+++ analyzers/tests/SonarAnalyzer.Test/Rules/SecurityPInvokeMethodShouldNotBeCalledTest.cs
@@ -38,6 +38,10 @@ namespace SonarAnalyzer.Test.Rules
         public void SecurityPInvokeMethodShouldNotBeCalled_CSharp11() =>
             builderCS.AddPaths("SecurityPInvokeMethodShouldNotBeCalled.CSharp11.cs").WithOptions(ParseOptionsHelper.FromCSharp11).Verify();
 
+        [TestMethod]
+        public void SecurityPInvokeMethodShouldNotBeCalled_CSharp12() =>
+            builderCS.AddPaths("SecurityPInvokeMethodShouldNotBeCalled.CSharp12.cs").WithOptions(ParseOptionsHelper.FromCSharp12).Verify();
+
 #endif
 
         [TestMethod]
