cb3934b9c56e19a5609ef1a1b9d3c73be9be646f
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-04-18 14:04:13 +0200

    Add scaffolding

--- analyzers/tests/SonarAnalyzer.Test/PackagingTests/RuleTypeMappingCS.cs
+++ analyzers/tests/SonarAnalyzer.Test/PackagingTests/RuleTypeMappingCS.cs
@@ -6892,7 +6892,7 @@ internal static class RuleTypeMappingCS
         ["S6965"] = "CODE_SMELL",
         ["S6966"] = "CODE_SMELL",
         // ["S6967"],
-        // ["S6968"],
+        ["S6968"] = "CODE_SMELL",
         // ["S6969"],
         // ["S6970"],
         // ["S6971"],
