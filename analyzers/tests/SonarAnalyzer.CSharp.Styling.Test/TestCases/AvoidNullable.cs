df20546d694d17d8e293cfabe2393e76cbea9851
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-04-19 18:22:33 +0200

    New Rule T0036: Do not activate a nullable context

---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/AvoidNullable.cs
@@ -0,0 +1,10 @@
+﻿
+// This is not expected to raise when the whole project is configured with nullable context.
+
+// Noncompliant@+1 {{Do not use nullable context.}}
+  #nullable enable
+//^^^^^^^^^^^^^^^^
+
+#nullable disable // Compliant
+
+#nullable restore // Compliant
