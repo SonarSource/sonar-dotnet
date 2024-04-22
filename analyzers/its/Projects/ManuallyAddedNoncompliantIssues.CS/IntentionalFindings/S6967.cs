8307180c626eb41dd8114b3ec4692778069c9efc
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-04-22 19:41:00 +0200

    Update ITs

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6967.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace IntentionalFindings
+{
+    public class S6967: Controller
+    {
+        [HttpPost] public IActionResult Create(Model model) => View(model); // Noncompliant
+
+        public class Model
+        {
+            [Required] public int Id { get; set; }
+        }
+    }
+}
