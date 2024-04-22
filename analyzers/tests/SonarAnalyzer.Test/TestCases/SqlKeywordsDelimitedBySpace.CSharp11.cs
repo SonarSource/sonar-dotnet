2fd1b40eb13a518769152c7228e719e86fb5ea7d
Martin Strecker <martin.strecker@sonarsource.com>
2024-04-22 16:26:24 +0200

    Add repro to C#11 and add raw string

--- analyzers/tests/SonarAnalyzer.Test/TestCases/SqlKeywordsDelimitedBySpace.CSharp11.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/SqlKeywordsDelimitedBySpace.CSharp11.cs
@@ -29,4 +29,27 @@ namespace Tests.Diagnostics
                 s2}}TABLE HumanResources.JobCandidate;"""; // Noncompliant
         }
     }
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/9177
+    public class Repro_9177
+    {
+        record ArtifactDto(string Id, string TagIdentifier);
+
+        string sqlQuery1 = $@"
+	        SELECT
+		        [Artifact].[Id] AS [{nameof(ArtifactDto.Id)}],
+		        [Artifact].[TagIdentifier] AS [{nameof(ArtifactDto.TagIdentifier)}]
+	        FROM
+		        [Artifacts] AS [Artifact]"; // Noncompliant@-3 [Id, bracket_Id] FPs
+                                            // Noncompliant@-3 [TagIdentifier, bracket_TagIdentifier] FPs
+
+        string sqlQuery2 = $"""
+            SELECT
+                [Artifact].[Id] AS [{nameof(ArtifactDto.Id)}],
+                [Artifact].[TagIdentifier] AS [{nameof(ArtifactDto.TagIdentifier)}]
+            FROM
+                [Artifacts] AS [Artifact]
+            """; // Noncompliant@-4 [Id2, bracket_Id2] FPs
+                 // Noncompliant@-4 [TagIdentifier2, bracket_TagIdentifier2] FPs
+    }
 }
