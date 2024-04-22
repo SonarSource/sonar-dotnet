1a11e50de1e3f077b2d7cbba9e70992c692c168c
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-04-22 12:36:00 +0200

    Add ITs

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6964.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntentionalFindings
+{
+    public class S6964
+    {
+        public class Model
+        {
+            public int ValueProperty { get; set; }  // Noncompliant
+        }
+    }
+    
+    public class ControllerClass : Controller
+    {
+        [HttpPost] public IActionResult Create(S6964.Model model) => View(model);
+    }
+}
