571aeceffaf677a9935e9f3c9298de46063072de
Antonio Aversa <antonio.aversa@sonarsource.com>
2024-04-23 16:14:33 +0200

    Code review 6

--- analyzers/tests/SonarAnalyzer.Test/Common/DisjointSetsTest.cs
+++ analyzers/tests/SonarAnalyzer.Test/Common/DisjointSetsTest.cs
@@ -3,12 +3,13 @@
 [TestClass]
 public class DisjointSetsTest
 {
+    private static readonly string[] FirstSixPositiveInts = Enumerable.Range(1, 6).Select(x => x.ToString()).ToArray();
+
     [TestMethod]
     public void FindRootAndUnion_AreConsistent()
     {
-        var elements = Enumerable.Range(1, 5).Select(x => x.ToString());
-        var sets = new DisjointSets(elements);
-        foreach (var element in elements)
+        var sets = new DisjointSets(FirstSixPositiveInts);
+        foreach (var element in FirstSixPositiveInts)
         {
             sets.FindRoot(element).Should().Be(element);
         }
@@ -35,28 +36,28 @@ public class DisjointSetsTest
     [TestMethod]
     public void GetAllSetsAndUnion_AreConsistent()
     {
-        var sets = new DisjointSets(Enumerable.Range(1, 5).Select(x => x.ToString()));
-        AssertSets([["1"], ["2"], ["3"], ["4"], ["5"]], sets); // Initial state
+        var sets = new DisjointSets(FirstSixPositiveInts);
+        AssertSets([["1"], ["2"], ["3"], ["4"], ["5"], ["6"]], sets); // Initial state
         sets.Union("1", "2");
-        AssertSets([["1", "2"], ["3"], ["4"], ["5"]], sets);   // Correctness
+        AssertSets([["1", "2"], ["3"], ["4"], ["5"], ["6"]], sets);   // Correctness
         sets.Union("1", "2");
-        AssertSets([["1", "2"], ["3"], ["4"], ["5"]], sets);   // Idempotency
+        AssertSets([["1", "2"], ["3"], ["4"], ["5"], ["6"]], sets);   // Idempotency
 
         sets.Union("2", "3");
-        AssertSets([["1", "2", "3"], ["4"], ["5"]], sets);     // Transitivity
+        AssertSets([["1", "2", "3"], ["4"], ["5"], ["6"]], sets);     // Transitivity
         sets.Union("1", "3");
-        AssertSets([["1", "2", "3"], ["4"], ["5"]], sets);     // Idempotency after transitivity
+        AssertSets([["1", "2", "3"], ["4"], ["5"], ["6"]], sets);     // Idempotency after transitivity
 
         sets.Union("4", "5");
-        AssertSets([["1", "2", "3"], ["4", "5"]], sets);       // Separated trees
+        AssertSets([["1", "2", "3"], ["4", "5"], ["6"]], sets);       // Separated trees
         sets.Union("3", "4");
-        AssertSets([["1", "2", "3", "4", "5"]], sets);         // Merged trees
+        AssertSets([["1", "2", "3", "4", "5"], ["6"]], sets);         // Merged trees
     }
 
     [TestMethod]
     public void GetAllSetsAndUnion_OfNestedTrees()
     {
-        var sets = new DisjointSets(Enumerable.Range(1, 6).Select(x => x.ToString()));
+        var sets = new DisjointSets(FirstSixPositiveInts);
         sets.Union("1", "2");
         sets.Union("3", "4");
         sets.Union("5", "6");
@@ -77,5 +78,5 @@ public class DisjointSetsTest
     }
 
     private static void AssertSets(List<List<string>> expected, DisjointSets sets) =>
-      sets.GetAllSets().Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
+        sets.GetAllSets().Should().BeEquivalentTo(expected, options => options.WithStrictOrdering());
 }
