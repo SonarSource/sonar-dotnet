ce87e617c2ceca141cf312afa5fcdb052c3054b7
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-05-15 16:47:12 +0200

    Whitespace fix

--- analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/SeparateDeclarations.cs
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/SeparateDeclarations.cs
@@ -67,7 +67,6 @@ public sealed class SeparateDeclarations : StylingAnalyzer
 
         static bool IsStandaloneCloseBrace(SyntaxToken token) =>
             token.IsKind(SyntaxKind.CloseBraceToken) && token.Line() != token.GetPreviousToken().Line();
-
     }
 
     private void ValidateSeparatedMember(SonarSyntaxNodeReportingContext context)
