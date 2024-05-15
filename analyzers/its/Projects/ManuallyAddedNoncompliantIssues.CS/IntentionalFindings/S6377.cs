9dcdadda380b670274bf171b8151bd6c16a49526
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-05-15 11:13:44 +0200

    Add IT

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6377.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+using System.Xml;
+
+namespace IntentionalFindings;
+
+public class S6377
+{
+    public void CheckSignature(XmlDocument xmlDoc, RSACryptoServiceProvider rsaCryptoServiceProvider)
+    {
+        var signedXml = new SignedXml(xmlDoc);
+        signedXml.LoadXml((XmlElement)xmlDoc.GetElementsByTagName("Signature").Item(0));
+
+        _ = signedXml.CheckSignature(rsaCryptoServiceProvider);
+        _ = signedXml.CheckSignature(); // The key is missing.
+    }
+}
