eccb51d862d2b352d69346dc2995158dcafd5baa
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-05-22 11:15:31 +0200

    don't raise for reference types in nullable context

--- analyzers/src/SonarAnalyzer.CSharp/Rules/AspNet/AvoidUnderPosting.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/AspNet/AvoidUnderPosting.cs
@@ -60,7 +60,8 @@ public sealed class AvoidUnderPosting : SonarDiagnosticAnalyzer
 
     private static void ProcessControllerMethods(SonarSyntaxNodeReportingContext context, ConcurrentDictionary<ITypeSymbol, bool> examinedTypes)
     {
-        if (context.SemanticModel.GetDeclaredSymbol(context.Node) is IMethodSymbol method && method.IsControllerActionMethod())
+        if (context.SemanticModel.GetDeclaredSymbol(context.Node) is IMethodSymbol method
+            && method.IsControllerActionMethod())
         {
             var modelParameterTypes = method.Parameters
                 .Where(x => !HasValidateNeverAttribute(x))
@@ -97,11 +98,12 @@ public sealed class AvoidUnderPosting : SonarDiagnosticAnalyzer
         && (type.Constructors.Any(x => x.Parameters.Length == 0)        // The type must have a parameterless constructor, unless
             || type.IsValueType                                         // - it's a value type
             || type.IsRecord()                                          // - it's a record type
-            || type.IsInterface());                                     // - it's an interface (although the type that implements will be actually used)
+            || type.IsInterface()                                       // - it's an interface (although the type that implements will be actually used)
+            || type.Is(KnownType.System_String));                       // - it has a custom Model Binder (e.g. System.String has one)
 
     private static bool CanBeNull(ITypeSymbol type) =>
         type is ITypeParameterSymbol { HasValueTypeConstraint: false }
-        || (type.IsReferenceType && type.NullableAnnotation() != NullableAnnotation.NotAnnotated)
+        || type.IsReferenceType
         || type.IsNullableValueType();
 
     private static void GetAllDeclaredProperties(ITypeSymbol type, ConcurrentDictionary<ITypeSymbol, bool> examinedTypes, List<IPropertySymbol> declaredProperties)
--- analyzers/tests/SonarAnalyzer.Test/TestCases/AspNet/AvoidUnderPosting.CSharp8.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/AspNet/AvoidUnderPosting.CSharp8.cs
@@ -10,10 +10,15 @@ namespace NullableReferences
         // Otherwise, strings behave always as nullable reference types.
 #nullable enable
         public string NonNullableReferenceProperty { get; set; }
+        public object AnotherNonNullableReferenceProperty { get; set; }
         [Required] public string RequiredNonNullableReferenceProperty { get; set; }
         public string? NullableReferenceProperty { get; set; }
+        public int ValueProperty { get; set; }                // Noncompliant
+        public int? NullableValueProperty { get; set; }
 #nullable disable
         public string ReferenceProperty { get; set; }
+        public object AnotherReferenceProperty { get; set; }
+        public int? AnotherNullableValueProperty { get; set; }
     }
 
     public class DerivedFromController : Controller
@@ -23,26 +28,5 @@ namespace NullableReferences
         {
             return View(model);
         }
-    }
-}
-
-namespace CustomGenerics
-{
-    public class GenericType<TNoContstraint, TClass, TStruct, TNotNull>
-        where TClass : class
-        where TStruct : struct
-        where TNotNull : notnull
-    {
-        public TNoContstraint NoConstraintProperty { get; set; }
-        public TClass ClassProperty { get; set; }
-        public TStruct StructProperty { get; set; }                             // Noncompliant
-        [Required] public TStruct RequiredStructProperty { get; set; }
-        public TStruct? NullableStructProperty { get; set; }
-        public TNotNull NotNullProperty { get; set; }                           // Noncompliant
-    }
-
-    public class ControllerClass : Controller
-    {
-        [HttpPost] public IActionResult Create(GenericType<object, string, int, DateTime> model) => View(model);
     }
 }
