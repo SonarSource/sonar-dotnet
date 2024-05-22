63f99276dc2239b4d7350d3088a9266102e59f8d
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2024-05-22 15:05:47 +0200

    Bump version to 9.25.1 on the hotfix branch (#9311)

--- analyzers/src/AssemblyInfo.Shared.cs
+++ analyzers/src/AssemblyInfo.Shared.cs
@@ -23,10 +23,10 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("9.25.0")]
-[assembly: AssemblyFileVersion("9.25.0.0")]
+[assembly: AssemblyVersion("9.25.1")]
+[assembly: AssemblyFileVersion("9.25.1.0")]
 // The value should look like "Version:X.X.X.X Branch:not-set Sha1:not-set"
-[assembly: AssemblyInformationalVersion("Version:9.25.0.0 Branch: Sha1:")]
+[assembly: AssemblyInformationalVersion("Version:9.25.1.0 Branch: Sha1:")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SonarSource")]
 [assembly: AssemblyCopyright("Copyright © 2015-2024 SonarSource SA")]
