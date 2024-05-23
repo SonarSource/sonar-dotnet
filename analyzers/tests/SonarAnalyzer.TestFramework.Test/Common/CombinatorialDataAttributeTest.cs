c707a053c69a195b49171c697ffccab9ef97589a
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2024-05-23 09:42:31 +0200

    Add combinatorial data attribute (#8949)

---
+++ analyzers/tests/SonarAnalyzer.TestFramework.Test/Common/CombinatorialDataAttributeTest.cs
@@ -0,0 +1,196 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Reflection;
+using NSubstitute;
+
+namespace SonarAnalyzer.Test.TestFramework.Tests.Common;
+
+[TestClass]
+
+public class CombinatorialDataAttributeTest_TwoDimensions
+{
+    private static List<(int X, int Y)> combinations;
+
+    [ClassInitialize]
+    public static void Initialize(TestContext context)
+    {
+        combinations = new();
+    }
+
+    [TestMethod]
+    [CombinatorialData]
+#pragma warning disable S2699 // Tests should include assertions. Assertion happens in cleanup
+    public void Combinatorial([DataValues(1, 2, 3)] int x, [DataValues(-1, -2, -3)] int y)
+#pragma warning restore S2699
+    {
+        combinations.Add((x, y));
+    }
+
+    [ClassCleanup]
+    public static void Cleanup()
+    {
+        combinations.Should().BeEquivalentTo([
+            (1, -1),
+            (1, -2),
+            (1, -3),
+            (2, -1),
+            (2, -2),
+            (2, -3),
+            (3, -1),
+            (3, -2),
+            (3, -3),
+        ]);
+    }
+}
+
+[TestClass]
+public class CombinatorialDataAttributeTest_ThreeDimensions
+{
+    private static List<(int X, string Y, bool Z)> combinations;
+
+    [ClassInitialize]
+    public static void Initialize(TestContext context)
+    {
+        combinations = new();
+    }
+
+    [TestMethod]
+    [CombinatorialData]
+#pragma warning disable S2699 // Tests should include assertions. Assertion happens in cleanup
+    public void Combinatorial([DataValues(1, 2, 3)] int x, [DataValues("A", "B")] string y, [DataValues(true, false)] bool z)
+#pragma warning restore S2699
+    {
+        combinations.Add((x, y, z));
+    }
+
+    [ClassCleanup]
+    public static void Cleanup()
+    {
+        combinations.Should().BeEquivalentTo([
+            (1, "A", true),
+            (1, "B", true),
+            (1, "A", false),
+            (1, "B", false),
+            (2, "A", true),
+            (2, "B", true),
+            (2, "A", false),
+            (2, "B", false),
+            (3, "A", true),
+            (3, "B", true),
+            (3, "A", false),
+            (3, "B", false),
+        ]);
+    }
+}
+
+[TestClass]
+public class CombinatorialDataAttributeTest_AttributeTest
+{
+    [TestMethod]
+    public void Combinatorial_Test()
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var data = attribute.GetData(typeof(CombinatorialDataAttributeTest_AttributeTest).GetMethod(nameof(Combinatorial)));
+        data.Should().BeEquivalentTo<object[]>([
+            [1, "A", true],
+            [1, "B", true],
+            [1, "A", false],
+            [1, "B", false],
+            [2, "A", true],
+            [2, "B", true],
+            [2, "A", false],
+            [2, "B", false],
+            [3, "A", true],
+            [3, "B", true],
+            [3, "A", false],
+            [3, "B", false],
+            ]);
+    }
+
+    [TestMethod]
+    public void MissingAttribute_Test()
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var data = () => attribute.GetData(typeof(CombinatorialDataAttributeTest_AttributeTest).GetMethod(nameof(MissingAttribute))).ToList();
+        data.Should().Throw<InvalidOperationException>().WithMessage("Combinatorial test requires all parameters to have the [DataValues] attribute set");
+    }
+
+    [TestMethod]
+    public void EmptyDataValuesAttribute_Test()
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var data = () => attribute.GetData(typeof(CombinatorialDataAttributeTest_AttributeTest).GetMethod(nameof(EmptyDataValuesAttribute))).ToList();
+        data.Should().Throw<InvalidOperationException>().WithMessage("[DataValues] attribute must have values set for all parameters");
+    }
+
+    [TestMethod]
+    public void NoParameters_Test()
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var data = () => attribute.GetData(typeof(CombinatorialDataAttributeTest_AttributeTest).GetMethod(nameof(NoParameters))).ToList();
+        data.Should().Throw<InvalidOperationException>().WithMessage("Combinatorial test must specify parameters with [DataValues] attributes");
+    }
+
+    [DataTestMethod]
+    [DataRow("Test ()")]
+    [DataRow("Test ()", null)]
+    [DataRow("Test (a)", "a")]
+    [DataRow("Test (a,b)", "a", "b")]
+    [DataRow("Test (a,b,1)", "a", "b", 1)]
+    public void GetDisplayName_Test(string expected, params object[] arguments)
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var methodInfo = Substitute.For<MethodInfo>();
+        methodInfo.Name.Returns("Test");
+        var actual = attribute.GetDisplayName(methodInfo, arguments);
+        actual.Should().Be(expected);
+    }
+
+    [TestMethod]
+    public void GetDisplayName_Null()
+    {
+        var attribute = new CombinatorialDataAttribute();
+        var methodInfo = Substitute.For<MethodInfo>();
+        methodInfo.Name.Returns("Test");
+        var actual = attribute.GetDisplayName(methodInfo, null);
+        actual.Should().Be("Test ()");
+    }
+
+    public static void Combinatorial([DataValues(1, 2, 3)] int x, [DataValues("A", "B")] string y, [DataValues(true, false)] bool z)
+    {
+        // Used for reflection only.
+    }
+
+    public static void MissingAttribute([DataValues(1, 2, 3)] int x, string y, [DataValues(true, false)] bool z)
+    {
+        // Used for reflection only.
+    }
+
+    public static void EmptyDataValuesAttribute([DataValues] int x)
+    {
+        // Used for reflection only.
+    }
+
+    public static void NoParameters()
+    {
+        // Used for reflection only.
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.TestFramework/Common/CombinatorialDataAttribute.cs
@@ -0,0 +1,79 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System.Reflection;
+
+namespace SonarAnalyzer.TestFramework.Common;
+
+// Based on https://stackoverflow.com/a/75531690
+[AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+public sealed class DataValuesAttribute : Attribute
+{
+    public object[] Values { get; }
+
+    public DataValuesAttribute(params object[] values)
+    {
+        Values = values;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class CombinatorialDataAttribute : Attribute, ITestDataSource
+{
+    public IEnumerable<object[]> GetData(MethodInfo methodInfo)
+    {
+        var valuesPerParameter = methodInfo.GetParameters().Select(x => x.GetCustomAttribute<DataValuesAttribute>()?.Values
+            ?? throw new InvalidOperationException("Combinatorial test requires all parameters to have the [DataValues] attribute set")).ToArray();
+        if (valuesPerParameter.Length == 0)
+        {
+            throw new InvalidOperationException("Combinatorial test must specify parameters with [DataValues] attributes");
+        }
+        var arrayLengths = valuesPerParameter.Select(x => x.Length == 0
+            ? throw new InvalidOperationException($"[DataValues] attribute must have values set for all parameters")
+            : x.Length).ToArray();
+        var totalCombinations = arrayLengths.Aggregate(1, (x, y) => x * y);
+        var parameterIndices = new int[arrayLengths.Length];
+
+        for (var i = 0; i < totalCombinations; i++)
+        {
+            var temp = i;
+            for (var j = arrayLengths.Length - 1; j >= 0; j--)
+            {
+                parameterIndices[j] = temp % arrayLengths[j];
+                temp /= arrayLengths[j];
+            }
+            yield return CreateArguments(valuesPerParameter, parameterIndices);
+        }
+    }
+
+    private static object[] CreateArguments(object[][] valuesPerParameter, int[] parameterIndices)
+    {
+        var arg = new object[parameterIndices.Length];
+        for (var i = 0; i < parameterIndices.Length; i++)
+        {
+            arg[i] = valuesPerParameter[i][parameterIndices[i]];
+        }
+
+        return arg;
+    }
+
+    public string GetDisplayName(MethodInfo methodInfo, object[] data) =>
+        $"{methodInfo.Name} ({(data is not null ? string.Join(",", data) : null)})";
+}
