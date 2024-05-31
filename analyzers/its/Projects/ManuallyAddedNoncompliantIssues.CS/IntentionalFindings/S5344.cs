e1faf98e3f092349eed143f8d27dc4b555561eb6
Costin Zaharia <costin.zaharia@sonarsource.com>
2024-05-31 10:25:38 +0200

    ITs:  add tests for S4347, S5344, and S6781

---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S5344.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IntentionalFindings;
+
+public class S5344
+{
+    public void TestCases()
+    {
+        _ = new PasswordHasherOptions
+            {
+                IterationCount = 1 // Noncompliant {{Use at least 100,000 iterations here.}}
+            };
+    }
+}
---
+++ analyzers/its/Projects/ManuallyAddedNoncompliantIssues.CS/IntentionalFindings/S6781.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace IntentionalFindings;
+
+public class S6781
+{
+    private const string HardCodedKey = "SecretSecretSecretSecretSecretSecretSecretSecret";
+
+    public void TestCases()
+    {
+        _ = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(HardCodedKey)); // Noncompliant
+    }
+}
