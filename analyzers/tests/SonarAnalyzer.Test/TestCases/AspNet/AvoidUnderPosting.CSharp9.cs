cf7fb8bdf0c752ee1a2ae5e421e5426ef782f1a1
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-06-03 15:19:39 +0200

    repro

--- analyzers/tests/SonarAnalyzer.Test/TestCases/AspNet/AvoidUnderPosting.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/AspNet/AvoidUnderPosting.CSharp9.cs
@@ -6,8 +6,9 @@ namespace CSharp9
 {
     // https://learn.microsoft.com/en-us/aspnet/core/mvc/models/model-binding#constructor-binding-and-record-types
     public record RecordModel(
-        int ValueProperty,                                                          // Noncompliant
-        [property: JsonRequired] int RequiredValueProperty,                         // without the property prefix the attribute would have been applied to the constructor parameter instead
+        int ValueProperty,                                                         // Noncompliant
+        [property: JsonRequired] int RequiredValueProperty,                        // Without the property prefix the attribute would have been applied to the constructor parameter instead
+        [Range(0, 2)] int RequiredValuePropertyWithoutPropertyPrefix,              // Noncompliant, FP see: https://github.com/SonarSource/sonar-dotnet/issues/9363
         int? NullableValueProperty,
         int PropertyWithDefaultValue = 42);
 
