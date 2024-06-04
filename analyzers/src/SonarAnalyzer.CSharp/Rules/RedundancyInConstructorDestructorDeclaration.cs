992abce87a21a96899e0fd742eebfcf98164d050
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-06-04 18:00:02 +0200

    file scoped namespace

--- analyzers/src/SonarAnalyzer.CSharp/Rules/RedundancyInConstructorDestructorDeclaration.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/RedundancyInConstructorDestructorDeclaration.cs
@@ -18,109 +18,108 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.Rules.CSharp
+namespace SonarAnalyzer.Rules.CSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class RedundancyInConstructorDestructorDeclaration : SonarDiagnosticAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class RedundancyInConstructorDestructorDeclaration : SonarDiagnosticAnalyzer
+    internal const string DiagnosticId = "S3253";
+    private const string MessageFormat = "Remove this redundant {0}.";
+
+    private static readonly SyntaxKind[] TypesWithPrimaryConstructorDeclarations =
+    [
+        SyntaxKind.ClassDeclaration,
+        SyntaxKind.StructDeclaration,
+        SyntaxKindEx.RecordClassDeclaration,
+        SyntaxKindEx.RecordStructDeclaration
+    ];
+
+    private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+    protected override void Initialize(SonarAnalysisContext context)
     {
-        internal const string DiagnosticId = "S3253";
-        private const string MessageFormat = "Remove this redundant {0}.";
-
-        private static readonly SyntaxKind[] TypesWithPrimaryConstructorDeclarations =
-        {
-            SyntaxKind.ClassDeclaration,
-            SyntaxKind.StructDeclaration,
-            SyntaxKindEx.RecordClassDeclaration,
-            SyntaxKindEx.RecordStructDeclaration
-        };
-
-        private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
-
-        protected override void Initialize(SonarAnalysisContext context)
-        {
-            context.RegisterNodeAction(CheckConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
-            context.RegisterNodeAction(CheckDestructorDeclaration, SyntaxKind.DestructorDeclaration);
-            context.RegisterNodeAction(CheckTypesWithPrimaryConstructor, TypesWithPrimaryConstructorDeclarations);
-        }
-
-        private static void CheckTypesWithPrimaryConstructor(SonarSyntaxNodeReportingContext context)
-        {
-            var typeDeclaration = (TypeDeclarationSyntax)context.Node;
-            if (!Excluded(typeDeclaration)
-                && typeDeclaration.ParameterList() is { Parameters.Count: 0 } parameterList
-                && !IsStructWithInitializedFieldOrProperty(typeDeclaration, context.SemanticModel))
-            {
-                context.ReportIssue(Rule, parameterList, "primary constructor");
-            }
-
-            static bool Excluded(TypeDeclarationSyntax node) =>
-                RecordDeclarationSyntaxWrapper.IsInstance(node)
-                && ((RecordDeclarationSyntaxWrapper)node).BaseList is { } baseList
-                && baseList.DescendantNodes().OfType<ArgumentSyntax>().Any();
-        }
-
-        private static void CheckDestructorDeclaration(SonarSyntaxNodeReportingContext context)
-        {
-            var destructorDeclaration = (DestructorDeclarationSyntax)context.Node;
-
-            if (destructorDeclaration.Body is { Statements.Count: 0 })
-            {
-                context.ReportIssue(Rule, destructorDeclaration, "destructor");
-            }
-        }
-
-        private static void CheckConstructorDeclaration(SonarSyntaxNodeReportingContext context)
-        {
-            var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
-
-            if (IsConstructorRedundant(constructorDeclaration, context.SemanticModel))
-            {
-                context.ReportIssue(Rule, constructorDeclaration, "constructor");
-                return;
-            }
-
-            var initializer = constructorDeclaration.Initializer;
-            if (initializer != null
-                && IsInitializerRedundant(initializer))
-            {
-                context.ReportIssue(Rule, initializer, "'base()' call");
-            }
-        }
-
-        private static bool IsInitializerRedundant(ConstructorInitializerSyntax initializer) =>
-            initializer.IsKind(SyntaxKind.BaseConstructorInitializer)
-            && initializer.ArgumentList != null
-            && !initializer.ArgumentList.Arguments.Any();
-
-        private static bool IsConstructorRedundant(ConstructorDeclarationSyntax constructorDeclaration, SemanticModel semanticModel) =>
-            constructorDeclaration is { ParameterList.Parameters.Count: 0, Body.Statements.Count: 0, Parent: BaseTypeDeclarationSyntax typeDeclaration }
-            && !IsStructWithInitializedFieldOrProperty(typeDeclaration, semanticModel)
-            && (IsSinglePublicConstructor(constructorDeclaration, semanticModel)
-                || constructorDeclaration.Modifiers.Any(SyntaxKind.StaticKeyword));
-
-        private static bool IsStructWithInitializedFieldOrProperty(BaseTypeDeclarationSyntax typeDeclaration, SemanticModel semanticModel) =>
-            typeDeclaration.Kind() is SyntaxKind.StructDeclaration or SyntaxKindEx.RecordStructDeclaration
-            && semanticModel.GetDeclaredSymbol(typeDeclaration) is { } typeSymbol
-            && typeSymbol.GetMembers().Any(f => f.Kind is SymbolKind.Field or SymbolKind.Property && f.DeclaringSyntaxReferences.Any(d => d.GetSyntax().GetInitializer() is not null));
-
-        private static bool IsSinglePublicConstructor(ConstructorDeclarationSyntax constructorDeclaration, SemanticModel semanticModel) =>
-            constructorDeclaration.Modifiers.Any(SyntaxKind.PublicKeyword)
-            && IsInitializerEmptyOrRedundant(constructorDeclaration.Initializer)
-            && constructorDeclaration is { Parent: BaseTypeDeclarationSyntax typeDeclaration }
-            && TypeHasExactlyOneConstructor(typeDeclaration, semanticModel);
-
-        private static bool IsInitializerEmptyOrRedundant(ConstructorInitializerSyntax initializer) =>
-            initializer is null
-            || (initializer.ArgumentList.Arguments.Count == 0
-                && initializer.ThisOrBaseKeyword.IsKind(SyntaxKind.BaseKeyword));
-
-        private static bool TypeHasExactlyOneConstructor(BaseTypeDeclarationSyntax containingTypeDeclaration, SemanticModel semanticModel) =>
-            semanticModel.GetDeclaredSymbol(containingTypeDeclaration) is { } typeSymbol
-            && typeSymbol
-                .GetMembers(".ctor")
-                .OfType<IMethodSymbol>()
-                .Count(m => m is { MethodKind: MethodKind.Constructor, IsImplicitlyDeclared: false, IsStatic: false }) == 1;
+        context.RegisterNodeAction(CheckConstructorDeclaration, SyntaxKind.ConstructorDeclaration);
+        context.RegisterNodeAction(CheckDestructorDeclaration, SyntaxKind.DestructorDeclaration);
+        context.RegisterNodeAction(CheckTypesWithPrimaryConstructor, TypesWithPrimaryConstructorDeclarations);
     }
+
+    private static void CheckTypesWithPrimaryConstructor(SonarSyntaxNodeReportingContext context)
+    {
+        var typeDeclaration = (TypeDeclarationSyntax)context.Node;
+        if (!Excluded(typeDeclaration)
+            && typeDeclaration.ParameterList() is { Parameters.Count: 0 } parameterList
+            && !IsStructWithInitializedFieldOrProperty(typeDeclaration, context.SemanticModel))
+        {
+            context.ReportIssue(Rule, parameterList, "primary constructor");
+        }
+
+        static bool Excluded(TypeDeclarationSyntax node) =>
+            RecordDeclarationSyntaxWrapper.IsInstance(node)
+            && ((RecordDeclarationSyntaxWrapper)node).BaseList is { } baseList
+            && baseList.DescendantNodes().OfType<ArgumentSyntax>().Any();
+    }
+
+    private static void CheckDestructorDeclaration(SonarSyntaxNodeReportingContext context)
+    {
+        var destructorDeclaration = (DestructorDeclarationSyntax)context.Node;
+
+        if (destructorDeclaration.Body is { Statements.Count: 0 })
+        {
+            context.ReportIssue(Rule, destructorDeclaration, "destructor");
+        }
+    }
+
+    private static void CheckConstructorDeclaration(SonarSyntaxNodeReportingContext context)
+    {
+        var constructorDeclaration = (ConstructorDeclarationSyntax)context.Node;
+
+        if (IsConstructorRedundant(constructorDeclaration, context.SemanticModel))
+        {
+            context.ReportIssue(Rule, constructorDeclaration, "constructor");
+            return;
+        }
+
+        var initializer = constructorDeclaration.Initializer;
+        if (initializer != null
+            && IsInitializerRedundant(initializer))
+        {
+            context.ReportIssue(Rule, initializer, "'base()' call");
+        }
+    }
+
+    private static bool IsInitializerRedundant(ConstructorInitializerSyntax initializer) =>
+        initializer.IsKind(SyntaxKind.BaseConstructorInitializer)
+        && initializer.ArgumentList != null
+        && !initializer.ArgumentList.Arguments.Any();
+
+    private static bool IsConstructorRedundant(ConstructorDeclarationSyntax constructorDeclaration, SemanticModel semanticModel) =>
+        constructorDeclaration is { ParameterList.Parameters.Count: 0, Body.Statements.Count: 0, Parent: BaseTypeDeclarationSyntax typeDeclaration }
+        && !IsStructWithInitializedFieldOrProperty(typeDeclaration, semanticModel)
+        && (IsSinglePublicConstructor(constructorDeclaration, semanticModel)
+            || constructorDeclaration.Modifiers.Any(SyntaxKind.StaticKeyword));
+
+    private static bool IsStructWithInitializedFieldOrProperty(BaseTypeDeclarationSyntax typeDeclaration, SemanticModel semanticModel) =>
+        typeDeclaration.Kind() is SyntaxKind.StructDeclaration or SyntaxKindEx.RecordStructDeclaration
+        && semanticModel.GetDeclaredSymbol(typeDeclaration) is { } typeSymbol
+        && typeSymbol.GetMembers().Any(f => f.Kind is SymbolKind.Field or SymbolKind.Property && f.DeclaringSyntaxReferences.Any(d => d.GetSyntax().GetInitializer() is not null));
+
+    private static bool IsSinglePublicConstructor(ConstructorDeclarationSyntax constructorDeclaration, SemanticModel semanticModel) =>
+        constructorDeclaration.Modifiers.Any(SyntaxKind.PublicKeyword)
+        && IsInitializerEmptyOrRedundant(constructorDeclaration.Initializer)
+        && constructorDeclaration is { Parent: BaseTypeDeclarationSyntax typeDeclaration }
+        && TypeHasExactlyOneConstructor(typeDeclaration, semanticModel);
+
+    private static bool IsInitializerEmptyOrRedundant(ConstructorInitializerSyntax initializer) =>
+        initializer is null
+        || (initializer.ArgumentList.Arguments.Count == 0
+            && initializer.ThisOrBaseKeyword.IsKind(SyntaxKind.BaseKeyword));
+
+    private static bool TypeHasExactlyOneConstructor(BaseTypeDeclarationSyntax containingTypeDeclaration, SemanticModel semanticModel) =>
+        semanticModel.GetDeclaredSymbol(containingTypeDeclaration) is { } typeSymbol
+        && typeSymbol
+            .GetMembers(".ctor")
+            .OfType<IMethodSymbol>()
+            .Count(m => m is { MethodKind: MethodKind.Constructor, IsImplicitlyDeclared: false, IsStatic: false }) == 1;
 }
