cbda1be60b3ee887bf45f39b48d9a59ae2389d36
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-06-04 13:56:15 +0200

    Codesmell fix

--- analyzers/src/SonarAnalyzer.VisualBasic/Rules/Naming/ParameterName.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic/Rules/Naming/ParameterName.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using System.Reflection;
-
 namespace SonarAnalyzer.Rules.VisualBasic;
 
 [DiagnosticAnalyzer(LanguageNames.VisualBasic)]
