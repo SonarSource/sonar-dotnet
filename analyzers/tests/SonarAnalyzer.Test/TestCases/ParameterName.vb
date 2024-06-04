7708e9e3ba72f2a3b07c6c2ee878c4c4210918ab
Pavel Mikula <pavel.mikula@sonarsource.com>
2024-06-04 11:33:54 +0200

    Add repro

--- analyzers/tests/SonarAnalyzer.Test/TestCases/ParameterName.vb
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/ParameterName.vb
@@ -1,5 +1,7 @@
 ﻿Module Module1
 
+    Private WithEvents fSource As Source
+
     Sub GetSomething(ByVal ID As Integer) ' Noncompliant
         '                  ^^
     End Sub
@@ -17,4 +19,49 @@
         End Get
     End Property
 
+    Private Sub fSource_SomeEvent(Sender As Object) Handles fSource.SomeEvent ' Noncompliant FP
+    End Sub
+
 End Module
+
+Public Class Source
+
+    Public Event SomeEvent(Sender As Object) ' Noncompliant
+
+End Class
+
+Public Interface ISomething
+
+    Sub DoSomething(Name As String)             ' Noncompliant
+    ReadOnly Property Value(Index As Integer)   ' Noncompliant
+
+End Interface
+
+Public Class Something
+    Implements ISomething
+
+    Public ReadOnly Property Value(Index As Integer) As System.Object Implements ISomething.Value  ' Noncompliant FP
+        Get
+            Return 42
+        End Get
+    End Property
+
+    Public Sub DoSomething(Name As String) Implements ISomething.DoSomething     ' Noncompliant FP
+    End Sub
+
+End Class
+
+Public Class Base
+
+    Protected Overridable Sub DoSomething(Name As String) ' Noncompliant
+    End Sub
+
+End Class
+
+Public Class Derived
+    Inherits Base
+
+    Protected Overrides Sub DoSomething(Name As String) ' Noncompliant FP
+    End Sub
+
+End Class
