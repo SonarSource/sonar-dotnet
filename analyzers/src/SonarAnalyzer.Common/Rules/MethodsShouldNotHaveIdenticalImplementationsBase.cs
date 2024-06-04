3b3d68f7c941745af1f0af0e6d2ba2254fe57468
Martin Strecker <martin.strecker@sonarsource.com>
2024-06-04 17:29:25 +0200

    Unsued using

--- analyzers/src/SonarAnalyzer.Common/Rules/MethodsShouldNotHaveIdenticalImplementationsBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/MethodsShouldNotHaveIdenticalImplementationsBase.cs
@@ -18,8 +18,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-using Google.Protobuf.WellKnownTypes;
-
 namespace SonarAnalyzer.Rules
 {
     public abstract class MethodsShouldNotHaveIdenticalImplementationsBase<TSyntaxKind, TMethodDeclarationSyntax> : SonarDiagnosticAnalyzer<TSyntaxKind>
