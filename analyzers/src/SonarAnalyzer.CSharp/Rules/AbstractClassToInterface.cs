1f8e7905b730418743c377c729ff22d5ad6b4fba
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-06-06 12:14:28 +0200

    Improve S1694: Remove part about protected constructor (#9387)

---
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/AbstractClassToInterface.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2015-2024 SonarSource SA
+ * mailto: contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace SonarAnalyzer.Rules.CSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class AbstractClassToInterface : SonarDiagnosticAnalyzer<SyntaxKind>
+{
+    private const string DiagnosticId = "S1694";
+
+    private static readonly MethodKind[] ConstructorKinds = [MethodKind.Constructor, MethodKind.SharedConstructor];
+
+    protected override string MessageFormat => "Convert this 'abstract' {0} to an interface.";
+    protected override ILanguageFacade<SyntaxKind> Language => CSharpFacade.Instance;
+
+    public AbstractClassToInterface() : base(DiagnosticId) { }
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterSymbolAction(
+            c =>
+            {
+                var symbol = (INamedTypeSymbol)c.Symbol;
+                if (symbol.IsClass()
+                    && symbol.IsAbstract
+                    && symbol.BaseType.Is(KnownType.System_Object)
+                    && !IsRecordWithParameters(symbol)
+                    && AllMethodsAreAbstract(symbol))
+                {
+                    foreach (var declaringSyntaxReference in symbol.DeclaringSyntaxReferences)
+                    {
+                        var node = declaringSyntaxReference.GetSyntax();
+                        if (node is ClassDeclarationSyntax classDeclaration)
+                        {
+                            c.ReportIssue(Rule, classDeclaration.Identifier, "class");
+                        }
+
+                        if (RecordDeclarationSyntaxWrapper.IsInstance(node))
+                        {
+                            var wrapper = (RecordDeclarationSyntaxWrapper)node;
+                            c.ReportIssue(Rule, wrapper.Identifier, "record");
+                        }
+                    }
+                }
+            },
+            SymbolKind.NamedType);
+
+    private static bool IsRecordWithParameters(ISymbol symbol) =>
+        symbol.DeclaringSyntaxReferences.Any(x => x.GetSyntax() is { } node
+                                                  && RecordDeclarationSyntaxWrapper.IsInstance(node)
+                                                  && ((RecordDeclarationSyntaxWrapper)node).ParameterList is { } parameterList
+                                                  && parameterList.Parameters.Count > 0);
+
+    private static bool AllMethodsAreAbstract(INamedTypeSymbol symbol)
+    {
+        var methods = GetAllMethods(symbol);
+        return methods.Any() && methods.All(x => x.IsAbstract);
+    }
+
+    private static IMethodSymbol[] GetAllMethods(INamedTypeSymbol symbol) =>
+        symbol.GetMembers().OfType<IMethodSymbol>().Where(x => !x.IsImplicitlyDeclared && !ConstructorKinds.Contains(x.MethodKind)).ToArray();
+}
---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/AbstractClassToInterface.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+
+public abstract partial class PartialMixed
+{
+    public abstract void X();
+}
+public abstract partial class PartialMixed
+{
+    public void Y() { }
+}
+
+public abstract partial class PartialAbstract // Noncompliant
+{
+    public abstract void X();
+}
+public abstract partial class PartialAbstract // Noncompliant
+{
+    public abstract void Y();
+}
+
+public abstract class Empty
+{
+}
+
+public abstract class Animal // Noncompliant {{Convert this 'abstract' class to an interface.}}
+//                    ^^^^^^
+{
+    protected abstract void move();
+    protected abstract void feed();
+}
+
+public class SomeBaseClass { }
+
+public abstract class Animal2 : SomeBaseClass //Compliant
+{
+    protected abstract void move();
+    protected abstract void feed();
+
+}
+
+public abstract class Color
+{
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
+
+    public int getRed()
+    {
+        return red;
+    }
+}
+
+public interface AnimalCompliant
+{
+
+    void move();
+    void feed();
+
+}
+
+public class ColorCompliant
+{
+    private int red = 0;
+    private int green = 0;
+    private int blue = 0;
+
+    private ColorCompliant()
+    { }
+
+    public int getRed()
+    {
+        return red;
+    }
+}
+
+public abstract class LampCompliant
+{
+    private bool switchLamp = false;
+
+    public abstract void glow();
+
+    public void flipSwitch()
+    {
+        switchLamp = !switchLamp;
+        if (switchLamp)
+        {
+            glow();
+        }
+    }
+}
+
+public abstract class View // Noncompliant, should be an interface
+{
+    public abstract string Content { get; }
+}
+
+public abstract class View2 // Compliant, has abstract and non abstract members
+{
+    public abstract string Content { get; }
+    public abstract string Content1 { get; }
+    public string Content2 { get; }
+}
+
+public abstract class View2Derived : View2 // Compliant, still has abstract parts
+{
+    public string Content3 { get; }
+    public override string Content1 { get { return ""; } }
+}
+
+public abstract class View3Derived : SomeUnknownType // Error [CS0246]
+{
+    public string Content3 { get; }
+    public override int Content1 { get { return 1; } }
+}
+
+public abstract class WithConstructor   // Noncompliant
+{
+    public abstract void ToOverride();
+
+    static WithConstructor()
+    {
+        // Do something here
+    }
+
+    public WithConstructor()
+    {
+        // Do something here
+    }
+}
