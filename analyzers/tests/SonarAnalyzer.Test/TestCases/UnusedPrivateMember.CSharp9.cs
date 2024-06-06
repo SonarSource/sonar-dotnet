b7463c902ed6cc0906298e53804bd7e304ac6d7d
Cristian Ambrosini <cristian.ambrosini@sonarsource.com>
2024-06-06 16:48:39 +0200

    Review 2

--- analyzers/tests/SonarAnalyzer.Test/TestCases/UnusedPrivateMember.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UnusedPrivateMember.CSharp9.cs
@@ -187,11 +187,14 @@ namespace Repro_9379
         public static T CreateInstance<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>() =>
             (T)Activator.CreateInstance(typeof(T), 42);
 
+        public static object CreateInstance([DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] Type type) =>
+            Activator.CreateInstance(type, 42);
+
         public static void InitValue<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(T value) { }
 
         private class A
         {
-            private bool a = true; // Noncompliant FP: type argument is inferred and ArgumentList is not present on syntax level
+            private bool a = true; // Noncompliant FP: type argument is inferred and ArgumentList is not present on syntax level (see line 183)
         }
 
         class C<T>
