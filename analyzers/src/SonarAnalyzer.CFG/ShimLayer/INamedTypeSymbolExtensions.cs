38187f358a2173bebafb003aa00d9907c19482c6
Martin Strecker <martin.strecker@sonarsource.com>
2024-06-11 11:37:25 +0200

    NewLine

--- analyzers/src/SonarAnalyzer.CFG/ShimLayer/INamedTypeSymbolExtensions.cs
+++ analyzers/src/SonarAnalyzer.CFG/ShimLayer/INamedTypeSymbolExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-
 namespace StyleCop.Analyzers.Lightup
 {
     using System.Reflection;
