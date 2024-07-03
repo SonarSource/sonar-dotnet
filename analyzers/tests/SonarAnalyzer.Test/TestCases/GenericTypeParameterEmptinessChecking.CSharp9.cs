0b551489faf3a21c25089c9be270bf0ffdb5ef2c
Zsolt Kolbay <zsolt.kolbay@sonarsource.com>
2024-07-03 21:10:15 +0200

    Add repro

--- analyzers/tests/SonarAnalyzer.Test/TestCases/GenericTypeParameterEmptinessChecking.CSharp9.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/GenericTypeParameterEmptinessChecking.CSharp9.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 bool IsDefault<T>(T value)
@@ -9,6 +10,13 @@ bool IsDefault<T>(T value)
 bool IsDefault2<T>(T value)
 {
     return object.Equals(value, default(T)); // Compliant
+}
+
+// https://github.com/SonarSource/sonar-dotnet/issues/9493
+bool NotNullWithNullableType<T>(Func<T?> arg) where T : notnull
+{
+    T? t = arg();
+    return t == null; // Noncompliant - FP
 }
 
 public record Record<T>
