605228e9071b5f5ca9622ec70792780c6d8ffcdd
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-07-11 16:04:07 +0200

    Fix S1104 FP: Do not report in Unity serializable classes (#9514)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/FieldsShouldBeEncapsulatedInProperties.Unity3D.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// https://github.com/SonarSource/sonar-dotnet/issues/7522
+public class UnityMonoBehaviour : UnityEngine.MonoBehaviour
+{
+    public int Field1; // Compliant
+}
+
+public class UnityScriptableObject : UnityEngine.ScriptableObject
+{
+    public int Field1; // Compliant
+}
+
+public class InvalidCustomSerializableClass1 : UnityEngine.Object
+{
+    public int Field1; // Noncompliant
+}
+
+[Serializable]
+public class ValidCustomSerializableClass : UnityEngine.Object
+{
+    public int Field1; // Compliant
+}
+
+// Unity3D does not seem to be available as a nuget package and we cannot use the original classes
+// Cannot run this test case in Concurrent mode because of the Concurrent namespace
+namespace UnityEngine
+{
+    public class MonoBehaviour { }
+    public class ScriptableObject { }
+    public class Object { }
+}
