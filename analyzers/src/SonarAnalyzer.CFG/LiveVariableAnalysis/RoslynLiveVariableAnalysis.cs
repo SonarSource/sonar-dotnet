99887db99fad6bba6a930cfda47f240ce7182d8e
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2024-07-19 09:42:11 +0200

    LVA: Resolve FlowCapture and FlowCaptureReference operations to symbols instead of operations (#9533)

--- analyzers/src/SonarAnalyzer.CFG/LiveVariableAnalysis/RoslynLiveVariableAnalysis.cs
+++ analyzers/src/SonarAnalyzer.CFG/LiveVariableAnalysis/RoslynLiveVariableAnalysis.cs
@@ -24,7 +24,7 @@ namespace SonarAnalyzer.CFG.LiveVariableAnalysis;
 
 public sealed class RoslynLiveVariableAnalysis : LiveVariableAnalysisBase<ControlFlowGraph, BasicBlock>
 {
-    private readonly Dictionary<CaptureId, List<IOperation>> flowCaptureOperations = [];
+    private readonly Dictionary<CaptureId, List<ISymbol>> flowCaptures = [];
     private readonly Dictionary<int, List<BasicBlock>> blockPredecessors = [];
     private readonly Dictionary<int, List<BasicBlock>> blockSuccessors = [];
 
@@ -74,7 +74,7 @@ public sealed class RoslynLiveVariableAnalysis : LiveVariableAnalysisBase<Contro
     protected override State ProcessBlock(BasicBlock block)
     {
         var ret = new RoslynState(this);
-        ret.ProcessBlock(Cfg, block, flowCaptureOperations);
+        ret.ProcessBlock(Cfg, block, flowCaptures);
         return ret;
     }
 
@@ -88,24 +88,24 @@ public sealed class RoslynLiveVariableAnalysis : LiveVariableAnalysisBase<Contro
                                     .Select(x => x.Instance.ToFlowCapture()))
         {
             if (flowCapture.Value.AsFlowCaptureReference() is { } captureReference
-                && flowCaptureOperations.TryGetValue(captureReference.Id, out var capturedOperations))
+                && flowCaptures.TryGetValue(captureReference.Id, out var symbols))
             {
-                AppendFlowCaptureReference(flowCapture.Id, capturedOperations.ToArray());
+                AppendFlowCaptureReference(flowCapture.Id, symbols.ToArray());
             }
-            else
+            else if (ParameterOrLocalSymbol(flowCapture.Value) is { } symbol)
             {
-                AppendFlowCaptureReference(flowCapture.Id, flowCapture.Value);
+                AppendFlowCaptureReference(flowCapture.Id, symbol);
             }
         }
 
-        void AppendFlowCaptureReference(CaptureId id, params IOperation[] operations)
+        void AppendFlowCaptureReference(CaptureId id, params ISymbol[] symbols)
         {
-            if (!flowCaptureOperations.TryGetValue(id, out var list))
+            if (!flowCaptures.TryGetValue(id, out var list))
             {
                 list = [];
-                flowCaptureOperations.Add(id, list);
+                flowCaptures.Add(id, list);
             }
-            list.AddRange(operations);
+            list.AddRange(symbols);
         }
     }
 
@@ -238,17 +238,13 @@ public sealed class RoslynLiveVariableAnalysis : LiveVariableAnalysisBase<Contro
         public RoslynState(RoslynLiveVariableAnalysis owner) =>
             this.owner = owner;
 
-        public void ProcessBlock(ControlFlowGraph cfg, BasicBlock block, Dictionary<CaptureId, List<IOperation>> flowCaptureOperations)
+        public void ProcessBlock(ControlFlowGraph cfg, BasicBlock block, Dictionary<CaptureId, List<ISymbol>> flowCaptureOperations)
         {
             foreach (var operation in block.OperationsAndBranchValue.ToReversedExecutionOrder().Select(x => x.Instance))
             {
-                if ((operation.AsFlowCapture() is { } flowCapture && flowCaptureOperations.TryGetValue(flowCapture.Id, out var captured))
-                    || (operation.AsFlowCaptureReference() is { } flowCaptureReference  && flowCaptureOperations.TryGetValue(flowCaptureReference.Id, out captured)))
+                if (operation.AsFlowCaptureReference() is { } flowCaptureReference  && flowCaptureOperations.TryGetValue(flowCaptureReference.Id, out var symbols))
                 {
-                    foreach (var capturedOperation in captured)
-                    {
-                        ProcessOperation(cfg, capturedOperation);
-                    }
+                    UsedBeforeAssigned.UnionWith(symbols);
                 }
                 else
                 {
