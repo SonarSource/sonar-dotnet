88f910575aaca308c52f63e41f5a08914d23ddf6
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-07-24 11:14:05 +0200

    More test cases

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UsePascalCaseForNamedPlaceHolders.Latest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+// https://github.com/SonarSource/sonar-dotnet/issues/9545
+public class Repro_9545
+{
+    public void Method(ILogger logger, int number)
+    {
+        logger.LogDebug($@"{nameof(Repro_9545)} filter: {{number}}", number);       // Compliant - FN
+        logger.LogDebug(@$"{nameof(Repro_9545)} filter: {{number}}", number);       // Compliant - FN
+        logger.LogDebug($$"""{{nameof(Repro_9545)}} filter: {number}""", number);   // Compliant - FN
+        logger.LogDebug($$$"""
+                           {{{nameof(Repro_9545)}}} filter: {number}
+                           """, number);                                            // Compliant - FN
+    }
+}
