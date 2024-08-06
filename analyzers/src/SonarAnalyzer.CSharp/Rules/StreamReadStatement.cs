84b8cc0e196029bab350d1e0ff54df9d6992b52d
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-08-06 17:16:28 +0200

    S2674 Cleanup for SonarWay promotion

--- analyzers/src/SonarAnalyzer.CSharp/Rules/StreamReadStatement.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Rules/StreamReadStatement.cs
@@ -20,45 +20,45 @@
 
 using System.IO;
 
-namespace SonarAnalyzer.Rules.CSharp
+namespace SonarAnalyzer.Rules.CSharp;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class StreamReadStatement : SonarDiagnosticAnalyzer
 {
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class StreamReadStatement : SonarDiagnosticAnalyzer
+    private const string DiagnosticId = "S2674";
+    private const string MessageFormat = "Check the return value of the '{0}' call to see how many bytes were read.";
+
+    private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
+
+    private static readonly ISet<string> ReadMethodNames = new HashSet<string>(StringComparer.Ordinal)
     {
-        private const string DiagnosticId = "S2674";
-        private const string MessageFormat = "Check the return value of the '{0}' call to see how many bytes were read.";
+        nameof(Stream.Read),
+        nameof(Stream.ReadAsync),
+        "ReadAtLeast", // Net7: https://learn.microsoft.com/dotnet/api/system.io.stream.readatleast#applies-to
+        "ReadAtLeastAsync",
+    };
 
-        private static readonly DiagnosticDescriptor Rule = DescriptorFactory.Create(DiagnosticId, MessageFormat);
-        private static readonly ISet<string> ReadMethodNames = new HashSet<string>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
             {
-                nameof(Stream.Read),
-                nameof(Stream.ReadAsync),
-                "ReadAtLeast", // Net7: https://learn.microsoft.com/dotnet/api/system.io.stream.readatleast#applies-to
-                "ReadAtLeastAsync",
-            };
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
-
-        protected override void Initialize(SonarAnalysisContext context) =>
-            context.RegisterNodeAction(c =>
+                var statement = (ExpressionStatementSyntax)c.Node;
+                var expression = statement.Expression switch
                 {
-                    var statement = (ExpressionStatementSyntax)c.Node;
-                    var expression = statement.Expression switch
-                    {
-                        AwaitExpressionSyntax awaitExpression => awaitExpression.AwaitedExpressionWithoutConfigureAwait(),
-                        var x => x,
-                    };
-                    expression = expression.RemoveConditionalAccess();
-                    if (expression is InvocationExpressionSyntax invocation
-                        && invocation.GetMethodCallIdentifier() is { } methodIdentifier
-                        && ReadMethodNames.Contains(methodIdentifier.Text, StringComparer.Ordinal)
-                        && c.SemanticModel.GetSymbolInfo(expression).Symbol is IMethodSymbol method
-                        && (method.ContainingType.Is(KnownType.System_IO_Stream)
-                            || (method.IsOverride && method.ContainingType.DerivesOrImplements(KnownType.System_IO_Stream))))
-                    {
-                        c.ReportIssue(Rule, expression, method.Name);
-                    }
-                },
-                SyntaxKind.ExpressionStatement);
-    }
+                    AwaitExpressionSyntax awaitExpression => awaitExpression.AwaitedExpressionWithoutConfigureAwait(),
+                    var x => x,
+                };
+                expression = expression.RemoveConditionalAccess();
+                if (expression is InvocationExpressionSyntax invocation
+                    && invocation.GetMethodCallIdentifier() is { } methodIdentifier
+                    && ReadMethodNames.Contains(methodIdentifier.Text)
+                    && c.SemanticModel.GetSymbolInfo(expression).Symbol is IMethodSymbol method
+                    && (method.ContainingType.Is(KnownType.System_IO_Stream)
+                        || (method.IsOverride && method.ContainingType.DerivesOrImplements(KnownType.System_IO_Stream))))
+                {
+                    c.ReportIssue(Rule, expression, method.Name);
+                }
+            },
+            SyntaxKind.ExpressionStatement);
 }
