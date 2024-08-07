39b591d0a8d2124e72c490beae7b8ef4e3d23552
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-08-07 11:01:34 +0200

    Deprecate rule S2387 (#9603)

--- analyzers/src/SonarAnalyzer.Common/Rules/FieldShadowsParentFieldBase.cs
+++ analyzers/src/SonarAnalyzer.Common/Rules/FieldShadowsParentFieldBase.cs
@@ -18,54 +18,53 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.Rules
+namespace SonarAnalyzer.Rules;
+
+[Obsolete("This rule has been deprecated since 9.32")]
+public abstract class FieldShadowsParentFieldBase<TSyntaxKind, TVariableDeclaratorSyntax> : SonarDiagnosticAnalyzer
+    where TSyntaxKind : struct
+    where TVariableDeclaratorSyntax : SyntaxNode
 {
-    public abstract class FieldShadowsParentFieldBase<TSyntaxKind, TVariableDeclaratorSyntax> : SonarDiagnosticAnalyzer
-        where TSyntaxKind : struct
-        where TVariableDeclaratorSyntax : SyntaxNode
+    private const string S2387DiagnosticId = "S2387";
+    private const string S2387MessageFormat = "'{0}' is the name of a field in '{1}'.";
+    private const string S4025DiagnosticId = "S4025";
+    private const string S4025MessageFormat = "Rename this field; it may be confused with '{0}' in '{1}'.";
+
+    private readonly DiagnosticDescriptor s2387;
+    private readonly DiagnosticDescriptor s4025;
+
+    protected abstract ILanguageFacade<TSyntaxKind> Language { get; }
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s2387, s4025);
+
+    protected FieldShadowsParentFieldBase()
     {
-        protected const string S2387DiagnosticId = "S2387";
-        private const string S2387MessageFormat = "'{0}' is the name of a field in '{1}'.";
+        s2387 = Language.CreateDescriptor(S2387DiagnosticId, S2387MessageFormat);
+        s4025 = Language.CreateDescriptor(S4025DiagnosticId, S4025MessageFormat);
+    }
 
-        protected const string S4025DiagnosticId = "S4025";
-        private const string S4025MessageFormat = "Rename this field; it may be confused with '{0}' in '{1}'.";
-
-        private readonly DiagnosticDescriptor s2387;
-        private readonly DiagnosticDescriptor s4025;
-
-        protected abstract ILanguageFacade<TSyntaxKind> Language { get; }
-
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s2387, s4025);
-
-        protected FieldShadowsParentFieldBase()
+    protected IEnumerable<Diagnostic> CheckFields(SemanticModel model, TVariableDeclaratorSyntax variableDeclarator)
+    {
+        if (model.GetDeclaredSymbol(variableDeclarator) is IFieldSymbol fieldSymbol)
         {
-            s2387 = Language.CreateDescriptor(S2387DiagnosticId, S2387MessageFormat);
-            s4025 = Language.CreateDescriptor(S4025DiagnosticId, S4025MessageFormat);
-        }
-
-        protected IEnumerable<Diagnostic> CheckFields(SemanticModel semanticModel, TVariableDeclaratorSyntax variableDeclarator)
-        {
-            if (semanticModel.GetDeclaredSymbol(variableDeclarator) is IFieldSymbol fieldSymbol)
+            var fieldName = fieldSymbol.Name;
+            foreach (var baseType in fieldSymbol.ContainingType.BaseType.GetSelfAndBaseTypes())
             {
-                var fieldName = fieldSymbol.Name;
-                foreach (var baseType in fieldSymbol.ContainingType.BaseType.GetSelfAndBaseTypes())
+                var similarFields = baseType.GetMembers().OfType<IFieldSymbol>().Where(IsMatch).ToList();
+                if (similarFields.Any(x => x.Name == fieldName))
                 {
-                    var similarFields = baseType.GetMembers().OfType<IFieldSymbol>().Where(IsMatch).ToList();
-                    if (similarFields.Any(field => field.Name == fieldName))
-                    {
-                        yield return Diagnostic.Create(s2387, Language.Syntax.NodeIdentifier(variableDeclarator).Value.GetLocation(), fieldName, baseType.Name);
-                    }
-                    else if (similarFields.Any())
-                    {
-                        yield return Diagnostic.Create(s4025, Language.Syntax.NodeIdentifier(variableDeclarator).Value.GetLocation(), similarFields.First().Name, baseType.Name);
-                    }
+                    yield return Diagnostic.Create(s2387, Language.Syntax.NodeIdentifier(variableDeclarator).Value.GetLocation(), fieldName, baseType.Name);
                 }
-
-                bool IsMatch(IFieldSymbol field) =>
-                    field.DeclaredAccessibility != Accessibility.Private
-                    && !field.IsStatic
-                    && field.Name.Equals(fieldName, StringComparison.OrdinalIgnoreCase);
+                else if (similarFields.Any())
+                {
+                    yield return Diagnostic.Create(s4025, Language.Syntax.NodeIdentifier(variableDeclarator).Value.GetLocation(), similarFields.First().Name, baseType.Name);
+                }
             }
+
+            bool IsMatch(IFieldSymbol field) =>
+                field.DeclaredAccessibility != Accessibility.Private
+                && !field.IsStatic
+                && field.Name.Equals(fieldName, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
