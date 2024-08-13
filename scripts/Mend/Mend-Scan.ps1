9e61fead52e9e769660e82a0d2c51dc351880966
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2024-08-13 16:10:12 +0200

    run only mend

--- scripts/Mend/Mend-Scan.ps1
+++ scripts/Mend/Mend-Scan.ps1
@@ -11,4 +11,4 @@ function Get-Version {
 $env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $(Get-Version)"
 
 Write-Host "Running the Mend unified agent for $env:WS_PROJECTNAME..."
-& "$env:JAVA_HOME\bin\java.exe" -jar $env:MEND_AGENT_PATH -c "$PSScriptRoot\wss-unified-agent.config" -scanComment "buildNumber:$env:BUILD_NUMBER;gitSha:$env:GIT_SHA"
+& "$env:JAVA_HOME\bin\java.exe" -jar $env:MEND_AGENT_PATH -c "$PSScriptRoot\wss-unified-agent.config" -scanComment "buildNumber:97113;gitSha:cc0ba76c09677fe094621d35ab0c408bd1215aaa"
