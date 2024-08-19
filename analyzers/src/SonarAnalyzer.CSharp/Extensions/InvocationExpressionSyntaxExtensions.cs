05aff85273eaa119c84d0f259c3177e37b6e4a1c
Zsolt Kolbay <121798625+zsolt-kolbay-sonarsource@users.noreply.github.com>
2024-08-19 14:52:24 +0200

    Fix AD0001: NullReferenceException is thrown in SonarAnalyzer.Rules.CSharp.ConditionalStructureSameImplementation

--- analyzers/src/SonarAnalyzer.CSharp/Extensions/InvocationExpressionSyntaxExtensions.cs
+++ analyzers/src/SonarAnalyzer.CSharp/Extensions/InvocationExpressionSyntaxExtensions.cs
@@ -18,43 +18,48 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-namespace SonarAnalyzer.Extensions
+namespace SonarAnalyzer.Extensions;
+
+internal static class InvocationExpressionSyntaxExtensions
 {
-    internal static class InvocationExpressionSyntaxExtensions
-    {
-        internal static bool IsMemberAccessOnKnownType(this InvocationExpressionSyntax invocation, string identifierName, KnownType knownType, SemanticModel semanticModel) =>
-            invocation.Expression is MemberAccessExpressionSyntax memberAccess
-            && memberAccess.IsMemberAccessOnKnownType(identifierName, knownType, semanticModel);
+    internal static bool IsMemberAccessOnKnownType(this InvocationExpressionSyntax invocation, string identifierName, KnownType knownType, SemanticModel model) =>
+        invocation.Expression is MemberAccessExpressionSyntax memberAccess
+        && memberAccess.IsMemberAccessOnKnownType(identifierName, knownType, model);
 
-        internal static IEnumerable<ISymbol> GetArgumentSymbolsOfKnownType(this InvocationExpressionSyntax invocation, KnownType knownType, SemanticModel semanticModel) =>
-            invocation.ArgumentList.Arguments.GetSymbolsOfKnownType(knownType, semanticModel);
+    internal static IEnumerable<ISymbol> GetArgumentSymbolsOfKnownType(this InvocationExpressionSyntax invocation, KnownType knownType, SemanticModel model) =>
+        invocation.ArgumentList.Arguments.GetSymbolsOfKnownType(knownType, model);
 
-        internal static bool HasExactlyNArguments(this InvocationExpressionSyntax invocation, int count) =>
-            invocation?.ArgumentList != null && invocation.ArgumentList.Arguments.Count == count;
+    internal static bool HasExactlyNArguments(this InvocationExpressionSyntax invocation, int count) =>
+        invocation?.ArgumentList is not null && invocation.ArgumentList.Arguments.Count == count;
 
-        internal static bool IsGetTypeCall(this InvocationExpressionSyntax invocation, SemanticModel semanticModel) =>
-            invocation != null
-            && (semanticModel.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol && methodSymbol.IsGetTypeCall());
+    internal static bool IsGetTypeCall(this InvocationExpressionSyntax invocation, SemanticModel model) =>
+        invocation is not null
+        && model.GetSymbolInfo(invocation).Symbol is IMethodSymbol methodSymbol && methodSymbol.IsGetTypeCall();
 
-        internal static bool IsOnBase(this InvocationExpressionSyntax invocation) =>
-            (invocation.Expression as MemberAccessExpressionSyntax)?.Expression is BaseExpressionSyntax;
+    internal static bool IsOnBase(this InvocationExpressionSyntax invocation) =>
+        (invocation.Expression as MemberAccessExpressionSyntax)?.Expression is BaseExpressionSyntax;
 
-        internal static bool IsEqualTo(this InvocationExpressionSyntax first, InvocationExpressionSyntax second, SemanticModel model) =>
-            model.GetSymbolInfo(first).Symbol.Equals(model.GetSymbolInfo(second).Symbol);
-
-        internal static bool TryGetOperands(this InvocationExpressionSyntax invocation, out SyntaxNode left, out SyntaxNode right)
+    internal static bool IsEqualTo(this InvocationExpressionSyntax first, InvocationExpressionSyntax second, SemanticModel model) =>
+        (model.GetSymbolInfo(first).Symbol, model.GetSymbolInfo(second).Symbol) switch
         {
-            (left, right) = invocation switch
-            {
-                { Expression: MemberAccessExpressionSyntax access } => (access.Expression, access.Name),
-                { Expression: MemberBindingExpressionSyntax binding } => (invocation.GetParentConditionalAccessExpression()?.Expression, binding.Name),
-                _ => (null, null)
-            };
+            // the nameof(someVariable) is considered an Invocation Expression, but it is not a method call, and GetSymbolInfo returns null for it.
+            (null, null) when first.GetName() == "nameof" && second.GetName() == "nameof" => model.GetConstantValue(first).Equals(model.GetConstantValue(second)),
+            ({ } firstSymbol, { } secondSymbol) => firstSymbol.Equals(secondSymbol),
+            _ => false
+        };
 
-            return left is not null && right is not null;
-        }
+    internal static bool TryGetOperands(this InvocationExpressionSyntax invocation, out SyntaxNode left, out SyntaxNode right)
+    {
+        (left, right) = invocation switch
+        {
+            { Expression: MemberAccessExpressionSyntax access } => (access.Expression, access.Name),
+            { Expression: MemberBindingExpressionSyntax binding } => (invocation.GetParentConditionalAccessExpression()?.Expression, binding.Name),
+            _ => (null, null)
+        };
 
-        internal static SyntaxToken? GetMethodCallIdentifier(this InvocationExpressionSyntax invocation) =>
-            invocation?.Expression.GetIdentifier();
+        return left is not null && right is not null;
     }
+
+    internal static SyntaxToken? GetMethodCallIdentifier(this InvocationExpressionSyntax invocation) =>
+        invocation?.Expression.GetIdentifier();
 }
--- analyzers/tests/SonarAnalyzer.Test/Rules/ConditionalStructureSameImplementationTest.cs
+++ analyzers/tests/SonarAnalyzer.Test/Rules/ConditionalStructureSameImplementationTest.cs
@@ -21,64 +21,136 @@
 using CS = SonarAnalyzer.Rules.CSharp;
 using VB = SonarAnalyzer.Rules.VisualBasic;
 
-namespace SonarAnalyzer.Test.Rules
+namespace SonarAnalyzer.Test.Rules;
+
+[TestClass]
+public class ConditionalStructureSameImplementationTest
 {
-    [TestClass]
-    public class ConditionalStructureSameImplementationTest
+    private readonly VerifierBuilder builderCS = new VerifierBuilder<CS.ConditionalStructureSameImplementation>();
+    private readonly VerifierBuilder builderVB = new VerifierBuilder<VB.ConditionalStructureSameImplementation>();
+
+    public TestContext TestContext { get; set; }
+
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_If_CSharp() =>
+        builderCS.AddPaths("ConditionalStructureSameImplementation_If.cs").Verify();
+
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_Switch_CSharp() =>
+        builderCS.AddPaths("ConditionalStructureSameImplementation_Switch.cs").Verify();
+
+    // https://github.com/SonarSource/sonar-dotnet/issues/9637
+    [DataTestMethod]
+    [DataRow("nameof(first)", "nameof(first)", false)]
+    [DataRow("nameof ( first ) ", "nameof(first)", false)]
+    [DataRow("nameof(first)", "nameof(second)", true)]
+    [DataRow("first.ToLower()", "nameof(second)", true)]
+    [DataRow("nameof(first)", "first.ToLower()", true)]
+    public void ConditionalStructureSameImplementation_NameOf_CSharp(string firstExpression, string secondExpression, bool isCompliant)
     {
-        private readonly VerifierBuilder builderCS = new VerifierBuilder<CS.ConditionalStructureSameImplementation>();
-        private readonly VerifierBuilder builderVB = new VerifierBuilder<VB.ConditionalStructureSameImplementation>();
+        var compliantComment = isCompliant ? string.Empty : "// Noncompliant";
+        var secondaryComment = isCompliant ? string.Empty : "// Secondary";
+        var builder = builderCS.AddSnippet($$"""
+            public class NameOfExpressions
+            {
+                public string Method(string first, string second)
+                {
+                    if (first.Length == 42)
+                    {                                   {{secondaryComment}}
+                        var ret = {{firstExpression}};
+                        return ret;
+                    }
+                    else if (second.Length == 42)
+                    {                                   {{compliantComment}}
+                        var ret = {{secondExpression}};
+                        return ret;
+                    }
+                    return "";
+                }
+            }
+            """);
+        if (isCompliant)
+        {
+            builder.VerifyNoIssues();
+        }
+        else
+        {
+            builder.Verify();
+        }
+    }
 
-        public TestContext TestContext { get; set; }
+    [DataTestMethod]
+    [DataRow("NameOf(first)", "NameOf(first)", false)]
+    [DataRow("NameOf ( first ) ", "NameOf(first)", false)]
+    [DataRow("NameOf(first)", "NameOf(second)", true)]
+    [DataRow("first.ToLower()", "NameOf(second)", true)]
+    [DataRow("NameOf(first)", "first.ToLower()", true)]
+    public void ConditionalStructureSameImplementation_SymbolCannotBeResolved_VB(string firstExpression, string secondExpression, bool isCompliant)
+    {
+        var compliantComment = isCompliant ? "' Compliant" : "' Noncompliant";
+        var builder = builderVB.AddSnippet($$"""
+            Public Class NameOfExpressions
+                Public Function Method(first as String, second as String)
+                    If first.Length = 42 Then
+                        Dim ret = {{firstExpression}}
+                        Return ret
+                    ElseIf second.Length = 42 Then
+                        Dim ret = {{secondExpression}}  {{compliantComment}}
+                        Return ret
+                    End If
+                    Return ""
+                End Function
+            End Class
+            """);
+        if (isCompliant)
+        {
+            builder.VerifyNoIssues();
+        }
+        else
+        {
+            builder.Verify();
+        }
+    }
 
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_If_CSharp() =>
-            builderCS.AddPaths("ConditionalStructureSameImplementation_If.cs").Verify();
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_If_VisualBasic() =>
+        builderVB.AddPaths("ConditionalStructureSameImplementation_If.vb").Verify();
 
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_Switch_CSharp() =>
-            builderCS.AddPaths("ConditionalStructureSameImplementation_Switch.cs").Verify();
-
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_If_VisualBasic() =>
-            builderVB.AddPaths("ConditionalStructureSameImplementation_If.vb").Verify();
-
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_Switch_VisualBasic() =>
-            builderVB.AddPaths("ConditionalStructureSameImplementation_Switch.vb").Verify();
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_Switch_VisualBasic() =>
+        builderVB.AddPaths("ConditionalStructureSameImplementation_Switch.vb").Verify();
 
 #if NET
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_Switch_CSharp_Latest() =>
-            builderCS.AddPaths("ConditionalStructureSameImplementation_Switch.Latest.cs").WithOptions(ParseOptionsHelper.CSharpLatest).VerifyNoIssues();
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_Switch_CSharp_Latest() =>
+        builderCS.AddPaths("ConditionalStructureSameImplementation_Switch.Latest.cs").WithOptions(ParseOptionsHelper.CSharpLatest).VerifyNoIssues();
 
-        [TestMethod]
-        public void ConditionalStructureSameImplementation_RazorFile_CorrectMessage() =>
-            builderCS.AddSnippet(
-                """
-                @code
+    [TestMethod]
+    public void ConditionalStructureSameImplementation_RazorFile_CorrectMessage() =>
+        builderCS.AddSnippet(
+            """
+            @code
+            {
+                public bool someCondition1 { get; set; }
+                public void DoSomething1() { }
+
+                public void Method()
                 {
-                    public bool someCondition1 { get; set; }
-                    public void DoSomething1() { }
-
-                    public void Method()
-                    {
-                        if (someCondition1)
-                        { // Secondary
-                            DoSomething1();
-                            DoSomething1();
-                        }
-                        else
-                        { // Noncompliant {{Either merge this branch with the identical one on line 9 or change one of the implementations.}}
-                            DoSomething1();
-                            DoSomething1();
-                        }
+                    if (someCondition1)
+                    { // Secondary
+                        DoSomething1();
+                        DoSomething1();
+                    }
+                    else
+                    { // Noncompliant {{Either merge this branch with the identical one on line 9 or change one of the implementations.}}
+                        DoSomething1();
+                        DoSomething1();
                     }
                 }
-                """,
-                "SomeRazorFile.razor")
-            .WithAdditionalFilePath(AnalysisScaffolding.CreateSonarProjectConfig(TestContext, ProjectType.Product))
-            .Verify();
+            }
+            """,
+            "SomeRazorFile.razor")
+        .WithAdditionalFilePath(AnalysisScaffolding.CreateSonarProjectConfig(TestContext, ProjectType.Product))
+        .Verify();
 #endif
-    }
 }
