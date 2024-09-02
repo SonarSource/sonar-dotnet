ed256694c701eed8fa79cf3b23d10716c86f59d6
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2024-09-02 17:07:14 +0200

    NET-65 Repro for S1144 (#32)

--- analyzers/tests/SonarAnalyzer.Test/TestCases/UnusedPrivateMember.CSharp12.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UnusedPrivateMember.CSharp12.cs
@@ -7,3 +7,37 @@ public sealed class Repro
 
     private static Line[] CreateLines() => [new(0)];
 }
+
+// https://github.com/SonarSource/sonar-dotnet/issues/9652
+class Repro_9652
+{
+    class Inner
+    {
+        public object? this[int test] => 0; // Noncompliant - FP
+        public int this[int x, int y] => 0; // Noncompliant - FP
+        private int this[string test] => 0;
+        public int Method() => this[""];
+    }
+
+    private int this[int test] => 0;
+
+    public Repro_9652()
+    {
+        Inner inner = new Inner();
+        _ = inner[0];
+        _ = inner?[0];
+        _ = inner![0];
+        _ = inner!?[0];
+        _ = inner[0].ToString();
+        _ = inner?[0].ToString();
+        _ = inner[0]?.ToString();
+        _ = inner?[0]?.ToString();
+        _ = inner![0].ToString();
+        _ = inner[0]!.ToString();
+        _ = inner![0]!.ToString();
+        _ = inner!?[0]!?.ToString();
+        _ = inner[0, 0];
+        _ = this[0];
+        _ = inner.Method();
+    }
+}
