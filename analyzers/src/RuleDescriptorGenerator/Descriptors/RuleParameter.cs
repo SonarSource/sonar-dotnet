d1d1e8c0cdc0065aefa8ecbfde7cf476891e6ef3
Zsolt Kolbay <121798625+zsolt-kolbay-sonarsource@users.noreply.github.com>
2024-09-19 17:47:59 +0200

    NET-281 Prepare RuleDescriptorGenerator for merged assemblies (#162)

--- analyzers/src/RuleDescriptorGenerator/Descriptors/RuleParameter.cs
+++ analyzers/src/RuleDescriptorGenerator/Descriptors/RuleParameter.cs
@@ -19,6 +19,7 @@
  */
 
 using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 using SonarAnalyzer.Common;
 
 namespace RuleDescriptorGenerator;
@@ -26,6 +27,11 @@ namespace RuleDescriptorGenerator;
 [ExcludeFromCodeCoverage]
 public class RuleParameter
 {
+    // To speed up the use of reflection in the PropertyValue() method for the 4 properties, we cache the PropertyInfo instances.
+    // It's only used in a single-threaded Console application.
+    // The RuleParameter constructor is called hundreds of times, so it's worth caching the PropertyInfo instances.
+    private static readonly Dictionary<string, PropertyInfo> PropertyInfoCache = [];
+
     public string Key { get; set; }
     public string Description { get; set; }
     public string Type { get; set; }
@@ -33,12 +39,12 @@ public class RuleParameter
 
     public RuleParameter() { }
 
-    public RuleParameter(RuleParameterAttribute attribute)
+    public RuleParameter(object attribute)
     {
-        Key = attribute.Key;
-        Description = attribute.Description;
-        Type = ToServerApi(attribute.Type);
-        DefaultValue = attribute.DefaultValue;
+        Key = PropertyValue<string>(attribute, nameof(RuleParameterAttribute.Key));
+        Description = PropertyValue<string>(attribute, nameof(RuleParameterAttribute.Description));
+        Type = ToServerApi(PropertyValue<int>(attribute, nameof(RuleParameterAttribute.Type)));
+        DefaultValue = PropertyValue<string>(attribute, nameof(RuleParameterAttribute.DefaultValue));
     }
 
     /// <summary>
@@ -46,15 +52,27 @@ public class RuleParameter
     /// SINGLE_SELECT_LIST,multiple=false,values="First,Second,Third"
     /// SINGLE_SELECT_LIST,multiple=true,values="First,Second,Third"
     /// </summary>
-    private static string ToServerApi(PropertyType type) =>
+    private static string ToServerApi(int type) =>
         type switch
         {
-            PropertyType.String => "STRING",
-            PropertyType.Text => "TEXT",
-            PropertyType.Boolean => "BOOLEAN",
-            PropertyType.Integer => "INTEGER",
-            PropertyType.Float => "FLOAT",
-            PropertyType.RegularExpression => "REGULAR_EXPRESSION",  // This will be translated to STRING by RuleParamType.parse() on the sonar-plugin-api side
+            (int)PropertyType.String => "STRING",
+            (int)PropertyType.Text => "TEXT",
+            (int)PropertyType.Boolean => "BOOLEAN",
+            (int)PropertyType.Integer => "INTEGER",
+            (int)PropertyType.Float => "FLOAT",
+            (int)PropertyType.RegularExpression => "REGULAR_EXPRESSION",  // This will be translated to STRING by RuleParamType.parse() on the sonar-plugin-api side
             _ => throw new UnexpectedValueException(nameof(type), type.ToString())
         };
+
+    // Reflection must be used because of the merged assemblies in the pipeline.
+    // RuleDescriptionGenerator will not recognize the RuleParameterAttribute type because it's no longer in the SonarAnalyzer.Common module, as it was during compilation.
+    private static T PropertyValue<T>(object target, string propertyName)
+    {
+        if (!PropertyInfoCache.TryGetValue(propertyName, out var propertyInfo))
+        {
+            propertyInfo = target.GetType().GetProperty(propertyName);
+            PropertyInfoCache[propertyName] = propertyInfo;
+        }
+        return (T)propertyInfo.GetValue(target, null);
+    }
 }
