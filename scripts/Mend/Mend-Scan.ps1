26fd874f6c3156e44b27ecd613fc2a3541d10028
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-10-01 13:02:22 +0200

    NET-41 Rework version management (#214)

--- scripts/Mend/Mend-Scan.ps1
+++ scripts/Mend/Mend-Scan.ps1
@@ -1,14 +1,11 @@
-function Get-Version {
-    $versionPropsXml = [xml](Get-Content -Path scripts/version/Version.props)
-    return $versionPropsXml.Project.PropertyGroup.MilestoneVersion
-}
-
 # Mend agent needs the following environment variables:
 # - WS_APIKEY
 # - WS_PRODUCTNAME
 # - WS_PROJECTNAME
 
-$env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $(Get-Version)"
+[xml]$Xml = [xml](Get-Content -Path analyzers/Version.targets)
+$Version = $Xml.Project.PropertyGroup.ShortVersion
+$env:WS_PROJECTNAME = "$env:WS_PRODUCTNAME $Version"
 
 Write-Host "Running the Mend unified agent for $env:WS_PROJECTNAME..."
 & "$env:JAVA_HOME\bin\java.exe" -jar $env:MEND_AGENT_PATH -c "$PSScriptRoot\wss-unified-agent.config" -scanComment "buildNumber:$env:BUILD_NUMBER;gitSha:$env:GIT_SHA"
