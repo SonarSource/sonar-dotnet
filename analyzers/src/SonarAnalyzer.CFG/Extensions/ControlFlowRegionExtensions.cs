765652e0d5a86116873c9564852ac04f20e44314
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2024-10-02 12:16:25 +0200

    NET-366 Use file-scoped namespaces in SonarAnalyzer.Core.Extensions

--- analyzers/src/SonarAnalyzer.CFG/Extensions/ControlFlowRegionExtensions.cs
+++ analyzers/src/SonarAnalyzer.CFG/Extensions/ControlFlowRegionExtensions.cs
@@ -20,48 +20,47 @@
 
 using SonarAnalyzer.CFG.Roslyn;
 
-namespace SonarAnalyzer.Extensions
+namespace SonarAnalyzer.Extensions;
+
+public static class ControlFlowRegionExtensions
 {
-    public static class ControlFlowRegionExtensions
+    public static IEnumerable<BasicBlock> Blocks(this ControlFlowRegion region, ControlFlowGraph cfg) =>
+        cfg.Blocks.Where((_, i) => region.FirstBlockOrdinal <= i && i <= region.LastBlockOrdinal);
+
+    public static ControlFlowRegion EnclosingNonLocalLifetimeRegion(this ControlFlowRegion region)
     {
-        public static IEnumerable<BasicBlock> Blocks(this ControlFlowRegion region, ControlFlowGraph cfg) =>
-            cfg.Blocks.Where((_, i) => region.FirstBlockOrdinal <= i && i <= region.LastBlockOrdinal);
-
-        public static ControlFlowRegion EnclosingNonLocalLifetimeRegion(this ControlFlowRegion region)
+        while (region.EnclosingRegion is not null && region.Kind == ControlFlowRegionKind.LocalLifetime)
         {
-            while (region.EnclosingRegion is not null && region.Kind == ControlFlowRegionKind.LocalLifetime)
-            {
-                region = region.EnclosingRegion;
-            }
-            return region;
+            region = region.EnclosingRegion;
         }
-
-        public static ControlFlowRegion EnclosingRegionOrSelf(this ControlFlowRegion region, ControlFlowRegionKind kind)
-        {
-            while (region is not null && region.Kind != kind)
-            {
-                if (region.Kind == ControlFlowRegionKind.Root)
-                {
-                    return null;    // Do not traverse from inner lambda CFG to the outer method CFG
-                }
-                region = region.EnclosingRegion;
-            }
-            return region;
-        }
-
-        public static ControlFlowRegion EnclosingRegion(this ControlFlowRegion region, ControlFlowRegionKind kind) =>
-            region.EnclosingRegion.EnclosingRegionOrSelf(kind);
-
-        public static ControlFlowRegion NestedRegion(this ControlFlowRegion region, ControlFlowRegionKind kind) =>
-            region.NestedRegions.Single(x => x.Kind == kind);
-
-        /// <summary>
-        /// Returns all Catch, FilterAndHandler, and Finally regions that are reachable from the given try region.
-        /// </summary>
-        public static IEnumerable<ControlFlowRegion> ReachableHandlers(this ControlFlowRegion tryRegion) =>
-            tryRegion is null
-                ? Enumerable.Empty<ControlFlowRegion>()
-                : tryRegion.EnclosingRegion.NestedRegions.Where(x => x.Kind != ControlFlowRegionKind.Try)
-                    .Concat(ReachableHandlers(tryRegion.EnclosingRegion(ControlFlowRegionKind.Try)));   // Use also all outer candidates for nested try/catch.
+        return region;
     }
+
+    public static ControlFlowRegion EnclosingRegionOrSelf(this ControlFlowRegion region, ControlFlowRegionKind kind)
+    {
+        while (region is not null && region.Kind != kind)
+        {
+            if (region.Kind == ControlFlowRegionKind.Root)
+            {
+                return null;    // Do not traverse from inner lambda CFG to the outer method CFG
+            }
+            region = region.EnclosingRegion;
+        }
+        return region;
+    }
+
+    public static ControlFlowRegion EnclosingRegion(this ControlFlowRegion region, ControlFlowRegionKind kind) =>
+        region.EnclosingRegion.EnclosingRegionOrSelf(kind);
+
+    public static ControlFlowRegion NestedRegion(this ControlFlowRegion region, ControlFlowRegionKind kind) =>
+        region.NestedRegions.Single(x => x.Kind == kind);
+
+    /// <summary>
+    /// Returns all Catch, FilterAndHandler, and Finally regions that are reachable from the given try region.
+    /// </summary>
+    public static IEnumerable<ControlFlowRegion> ReachableHandlers(this ControlFlowRegion tryRegion) =>
+        tryRegion is null
+            ? Enumerable.Empty<ControlFlowRegion>()
+            : tryRegion.EnclosingRegion.NestedRegions.Where(x => x.Kind != ControlFlowRegionKind.Try)
+                .Concat(ReachableHandlers(tryRegion.EnclosingRegion(ControlFlowRegionKind.Try)));   // Use also all outer candidates for nested try/catch.
 }
