75f1ab36167d1c5128abfe8faf7dc303441e8ea2
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2024-10-08 13:47:19 +0200

    NET-405 Fix S3459 FP: Support @ref bindings in razor files / partial Blazor components (#249)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/NotAssignedPrivateMember.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Razor
+{
+    public partial class NotAssignedPrivateMember
+    {
+        private ElementReference pageTitle; // Compliant - Assigned in the generated code for the razor file.
+
+        public void Test()
+        {
+            Console.WriteLine(pageTitle.ToString());
+        }
+    }
+}
