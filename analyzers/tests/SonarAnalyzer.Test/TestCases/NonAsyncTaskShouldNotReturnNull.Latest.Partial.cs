dd9de6135eef97e26305963193b89b9c623b2913
Gregory Paidis <115458417+gregory-paidis-sonarsource@users.noreply.github.com>
2024-10-08 13:30:12 +0200

    NET-420 Greg/uts non async task should not return null

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/NonAsyncTaskShouldNotReturnNull.Latest.Partial.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+public partial class PartialProperties
+{
+    public partial Task<object> Prop1 => null; // Noncompliant
+
+    public partial Task<object> Prop2
+    {
+        get
+        {
+            return null; // Noncompliant
+        }
+    }
+}
