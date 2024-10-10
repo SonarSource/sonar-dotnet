525fb552a9eafe2f2d7f7e01bc21ce2b4e9ce9cc
Gregory Paidis <115458417+gregory-paidis-sonarsource@users.noreply.github.com>
2024-10-10 16:38:13 +0200

    NET-452 NET9: UTs PropertyGetterWithThrow

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/PropertyGetterWithThrow.Latest.Partial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp13
+{
+    public partial class PartialProperties
+    {
+        public partial int MyProperty { get; }
+        public partial int MyProperty2 { get; }
+        public partial int MyProperty3 { get; }
+        public partial int MyProperty4 { get; set; }
+    }
+}
