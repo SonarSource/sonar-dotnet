281e7a73f05b6ab5925259affff07d322efb1600
Gregory Paidis <115458417+gregory-paidis-sonarsource@users.noreply.github.com>
2024-10-10 08:58:08 +0200

    NET-454 NET9: UTs PropertyWriteOnly (#282)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/PropertyWriteOnly.Latest.Partial.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharp13
+{
+    public partial class PartialProperties
+    {
+        public partial int Foo { set; } // Noncompliant
+        public partial int Foo2 { get; set; }
+    }
+}
