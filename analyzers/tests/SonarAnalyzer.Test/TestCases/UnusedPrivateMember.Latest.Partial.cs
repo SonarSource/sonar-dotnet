f19f856636a224df6df48c4ad16ddcd178186147
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-10 09:28:15 +0200

    NET-453 NET 9: UTS UnusedPrivateMember

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UnusedPrivateMember.Latest.Partial.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp9
+{
+    public partial class PartialMethods
+    {
+        partial void UnusedMethod(); // Noncompliant
+
+    }
+}
+
+namespace CSharp13
+{
+    public partial class PartialProperty
+    {
+        partial int PartialProp { get => 42; } // Noncompliant
+    }
+}
