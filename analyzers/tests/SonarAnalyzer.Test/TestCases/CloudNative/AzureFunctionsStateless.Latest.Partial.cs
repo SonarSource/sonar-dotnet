087c0f299ccf1fad3f1fc244072da13ddff2de1c
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-16 12:44:54 +0200

    NET-511 NET9: UTs S6419 (#321)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/CloudNative/AzureFunctionsStateless.Latest.Partial.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+public static partial class StaticPartialClass
+{
+    public static partial int PartialProperty { get { return 42; } set { } }
+
+    public static partial void Update(int value) =>
+        PartialProperty = value; //Noncompliant
+}
+
