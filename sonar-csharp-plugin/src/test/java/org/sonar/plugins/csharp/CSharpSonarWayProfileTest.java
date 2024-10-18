82a3cb13181ab25a7847039dad92363d6b6eaa4f
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2024-10-18 14:20:16 +0200

    NET-394  Java UTs: Add Enterprise version of SonarWayProfileTest for SE rules
    Co-authored-by: Sebastien Marichal <sebastien.marichal@sonarsource.com>

--- sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarWayProfileTest.java
+++ sonar-csharp-plugin/src/test/java/org/sonar/plugins/csharp/CSharpSonarWayProfileTest.java
@@ -19,56 +19,60 @@
  */
 package org.sonar.plugins.csharp;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.slf4j.event.Level;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition.BuiltInQualityProfile;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition.Context;
-import org.sonar.api.testfixtures.log.LogTesterJUnit5;
 import org.sonarsource.csharp.core.CSharpSonarWayProfile;
 import org.sonarsource.dotnet.shared.plugins.RoslynRules;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CSharpSonarWayProfileTest {
-  @RegisterExtension
-  public LogTesterJUnit5 logTester = new LogTesterJUnit5().setLevel(Level.DEBUG);
+
+  private static final RoslynRules ROSLYN_RULES = new RoslynRules(CSharpPlugin.METADATA);
+  private static final String REPOSITORY_KEY = CSharpPlugin.METADATA.repositoryKey();
+  private static BuiltInQualityProfile profile;
+
+  @BeforeAll
+  static void setup() {
+    Context context = new Context();
+    CSharpSonarWayProfile profileDef = new CSharpSonarWayProfile(CSharpPlugin.METADATA, ROSLYN_RULES);
+    profileDef.define(context);
+    profile = context.profile("cs", "Sonar way");
+  }
+
+  @Test
+  void expected_rules_in_sonar_way() {
+    // SonarWay rules
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2198"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2223"))).isNotNull();
+
+    // Non Sonarway rules
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2330"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2952"))).isNull();
+  }
 
   @Test
   void hotspots_in_sonar_way() {
-    Context context = new Context();
-    String repositoryKey = CSharpPlugin.METADATA.repositoryKey();
-
-    CSharpSonarWayProfile profileDef = new CSharpSonarWayProfile(CSharpPlugin.METADATA, new RoslynRules(CSharpPlugin.METADATA));
-    profileDef.define(context);
-
-    BuiltInQualityProfile profile = context.profile("cs", "Sonar way");
-
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S1313"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2068"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2092"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2245"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S3330"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S4507"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S4790"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S5042"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2077"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S5766"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S1313"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2068"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2092"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2245"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S3330"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S4507"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S4790"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S5042"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2077"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S5766"))).isNotNull();
   }
 
   @Test
   void symbolic_execution_not_in_sonar_way() {
-    Context context = new Context();
-    String repositoryKey = CSharpPlugin.METADATA.repositoryKey();
-
-    CSharpSonarWayProfile profileDef = new CSharpSonarWayProfile(CSharpPlugin.METADATA, new RoslynRules(CSharpPlugin.METADATA));
-    profileDef.define(context);
-    BuiltInQualityProfile profile = context.profile("cs", "Sonar way");
-
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2222"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2259"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2583"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2589"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2222"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2259"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2583"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2589"))).isNull();
   }
 }
