fe26cd5d0d42a2508ac0c1533a5dfb4bf43019ae
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-18 13:15:46 +0200

    NET-522 NET9: UTs S2156 (#331)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/StaticSealedClassProtectedMembers.Latest.Partial.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp13
+{
+    sealed partial class PartialPropertyClass
+    {
+        protected partial int PartialProperty // Noncompliant
+        {
+            get;
+            set;
+        }
+        public partial int OtherPartialProperty
+        {
+            get;
+            set;
+        }
+        public partial int this[int index] { get { return 42; } }
+        protected partial int this[string index] { get { return 42; } } // Noncompliant
+    }
+}
