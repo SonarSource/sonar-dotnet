6f691e8fa84ba083848366889726fb2fefa9bba8
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-18 11:19:50 +0200

    NET-523 NET9: UTs S3441 (#332)

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/RedundantPropertyNamesInAnonymousClass.Latest.Partial.cs
@@ -0,0 +1,17 @@
+﻿
+namespace CSharp13
+{
+    public partial class PartialPropertyClass
+    {
+        public partial int PartialProperty  
+        {
+            get;
+            set;
+        }
+        public partial int OtherPartialProperty
+        {
+            get => 42;
+            set { }
+        }
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/RedundantPropertyNamesInAnonymousClass.Latest.cs
@@ -0,0 +1,31 @@
+﻿namespace CSharp13
+{
+    public partial class PartialPropertyClass
+    {
+        public partial int PartialProperty
+        {
+            get => 42;
+            set { }
+        }
+
+        public partial int OtherPartialProperty
+        {
+            get;
+            set;
+        }
+
+        public void M()
+        {
+            var anon = new
+            {
+                PartialProperty = PartialProperty, //Noncompliant
+//              ^^^^^^^^^^^^^^^^^
+            };
+
+            var anon2 = new
+            {
+                OtherPartialProperty = OtherPartialProperty, //Noncompliant
+            };
+        }
+    }
+}
