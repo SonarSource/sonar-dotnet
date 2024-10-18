8d75c0f57283b847d23dc822748f9de892a92e97
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2024-10-18 14:20:16 +0200

    NET-394  Java UTs: Add Enterprise version of SonarWayProfileTest for SE rules (#330)
    Co-authored-by: Sebastien Marichal <sebastien.marichal@sonarsource.com>

--- sonar-vbnet-plugin/src/test/java/org/sonar/plugins/vbnet/VbNetSonarWayProfileTest.java
+++ sonar-vbnet-plugin/src/test/java/org/sonar/plugins/vbnet/VbNetSonarWayProfileTest.java
@@ -19,54 +19,57 @@
  */
 package org.sonar.plugins.vbnet;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.slf4j.event.Level;
 import org.sonar.api.rule.RuleKey;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition.BuiltInQualityProfile;
 import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition.Context;
-import org.sonar.api.testfixtures.log.LogTesterJUnit5;
 import org.sonarsource.dotnet.shared.plugins.RoslynRules;
 import org.sonarsource.vbnet.core.VbNetSonarWayProfile;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VbNetSonarWayProfileTest {
-  @RegisterExtension
-  public LogTesterJUnit5 logTester = new LogTesterJUnit5().setLevel(Level.DEBUG);
 
   private static final RoslynRules ROSLYN_RULES = new RoslynRules(VbNetPlugin.METADATA);
+  private static final String REPOSITORY_KEY = VbNetPlugin.METADATA.repositoryKey();
+  private static BuiltInQualityProfile profile;
+
+  @BeforeAll
+  static void setup() {
+    Context context = new Context();
+    VbNetSonarWayProfile profileDef = new VbNetSonarWayProfile(VbNetPlugin.METADATA, ROSLYN_RULES);
+    profileDef.define(context);
+    profile = context.profile("vbnet", "Sonar way");
+  }
+
+  @Test
+  void expected_rules_in_sonar_way() {
+    // SonarWay rules
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2347"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2349"))).isNotNull();
+
+    // Non Sonarway rules
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2348"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2354"))).isNull();
+  }
 
   @Test
   void hotspots_in_sonar_way() {
-    Context context = new Context();
-    String repositoryKey = VbNetPlugin.METADATA.repositoryKey();
-
-    VbNetSonarWayProfile profileDef = new VbNetSonarWayProfile(VbNetPlugin.METADATA, ROSLYN_RULES);
-    profileDef.define(context);
-
-    BuiltInQualityProfile profile = context.profile("vbnet", "Sonar way");
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S4507"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S5042"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2077"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2068"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S1313"))).isNotNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S4790"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S4507"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S5042"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2077"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2068"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S1313"))).isNotNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S4790"))).isNotNull();
   }
 
 
   @Test
   void symbolic_execution_not_in_sonar_way() {
-    Context context = new Context();
-    String repositoryKey = VbNetPlugin.METADATA.repositoryKey();
-
-    VbNetSonarWayProfile profileDef = new VbNetSonarWayProfile(VbNetPlugin.METADATA, ROSLYN_RULES);
-    profileDef.define(context);
-    BuiltInQualityProfile profile = context.profile("vbnet", "Sonar way");
-
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2222"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2259"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2583"))).isNull();
-    assertThat(profile.rule(RuleKey.of(repositoryKey, "S2589"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2222"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2259"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2583"))).isNull();
+    assertThat(profile.rule(RuleKey.of(REPOSITORY_KEY, "S2589"))).isNull();
   }
 }
