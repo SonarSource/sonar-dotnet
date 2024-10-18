115725c4cf44534a0ad826f3695f1cd2eddcdf7d
Gregory Paidis <115458417+gregory-paidis-sonarsource@users.noreply.github.com>
2024-10-18 16:42:14 +0200

    NET-539 NET9: UTs PreferJaggedArraysOverMultidimensional

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/PreferJaggedArraysOverMultidimensional.Latest.cs
@@ -0,0 +1,11 @@
+﻿partial class PartialProperties
+{
+    public partial int[,] MultiDimArrayProperty { get; }            // Noncompliant
+    public partial int[,] this[int index] { get; }                  // Noncompliant
+}
+
+partial class PartialProperties
+{
+    public partial int[,] MultiDimArrayProperty { get => null; }    // Noncompliant
+    public partial int[,] this[int index] { get => null; }          // Noncompliant
+}
