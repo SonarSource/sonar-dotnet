bfa143ea26e2137f24a47639e2e2191597ade362
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-18 14:49:09 +0200

    NET-538 NET9: Fix UTs S3353

--- analyzers/tests/SonarAnalyzer.Test/Rules/UnchangedLocalVariablesShouldBeConstTest.cs
+++ analyzers/tests/SonarAnalyzer.Test/Rules/UnchangedLocalVariablesShouldBeConstTest.cs
@@ -31,6 +31,23 @@ public class UnchangedLocalVariablesShouldBeConstTest
     public void UnchangedLocalVariablesShouldBeConst() =>
         verifier.AddPaths("UnchangedLocalVariablesShouldBeConst.cs").Verify();
 
+    [TestMethod]
+    public void UnchangedLocalVariablesShouldBeConst_CSharp7() =>
+        verifier.AddSnippet("""
+                            public class Test{
+
+                                public void Message()
+                                {
+                                    var s1 = "Test";                              // Noncompliant {{Add the 'const' modifier to 's1', and replace 'var' with 'string'.}}
+                                    string s2 = $"This is a {nameof(Message)}";   // Compliant - constant string interpolation is only valid in C# 10 and above
+                                    var s3 = $"This is a {nameof(Message)}";      // Compliant - constant string interpolation is only valid in C# 10 and above
+                                    var s4 = "This is a" + $" {nameof(Message)}"; // Compliant - constant string interpolation is only valid in C# 10 and above
+                                    var s5 = $@"This is a {nameof(Message)}";     // Compliant - constant string interpolation is only valid in C# 10 and above
+                                }
+                            }
+                            """)
+        .WithOptions(ParseOptionsHelper.OnlyCSharp7).Verify();
+
 #if NET
 
     [TestMethod]
