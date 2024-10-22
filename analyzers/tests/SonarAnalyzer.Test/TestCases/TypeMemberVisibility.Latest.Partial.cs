60544af6c7d48d31aa3da1a5909e39a583b5271e
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2024-10-22 17:27:06 +0200

    NET-551 NET9: UTs S3059

---
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/TypeMemberVisibility.Latest.Partial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp13
+{
+    internal partial class PartialPropertyClass //Noncompliant
+    {
+        public partial int PropertyA { get { return 42; } } // Secondary
+        private partial int PropertyE { get  { return 42; } }
+    }
+
+    //https://sonarsource.atlassian.net/browse/NET-560
+    partial class OtherPartialPropertyClass // CompliantFN
+    {
+        public partial int PropertyA { get { return 42; } } // Compliant FN
+        private partial int PropertyE { get { return 42; } }
+
+        public partial void VoidMethod();  // Compliant FN
+        partial void AnotherAnotherVoidMethod();
+
+        public partial void DoSomething() { } // Compliant FN
+        private partial void DoSomethingElse() { }
+    }
+}
