a3303184df22f33f3452b970a6886860be900ddb
Mary Georgiou <89914005+mary-georgiou-sonarsource@users.noreply.github.com>
2024-10-28 11:10:35 +0100

    NET-579 S1215: Add reproducer for FN - GC.GetTotalMemory(true)

--- analyzers/tests/SonarAnalyzer.Test/TestCases/DoNotCallGCCollectMethod.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/DoNotCallGCCollectMethod.cs
@@ -13,6 +13,10 @@ namespace Tests.Diagnostics
             GC.Collect(2, GCCollectionMode.Optimized); // Noncompliant
 
             Collect(); // Noncompliant
+
+            // Repro for: https://github.com/SonarSource/sonar-dotnet/issues/9687
+            GC.GetTotalMemory(true);  // FN, when forceFullCollection is true the method calls GC.Collect() see: https://github.com/dotnet/runtime/blob/fe3e5437c0a4c51ae1f9c5e9ebe142dc41c8feba/src/coreclr/System.Private.CoreLib/src/System/GC.CoreCLR.cs#L391
+            GC.GetTotalMemory(false); // Compliant
         }
     }
 }
