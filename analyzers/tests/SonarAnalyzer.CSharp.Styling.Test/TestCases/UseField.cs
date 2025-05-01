de29066fac8440f15dd9189704f44ebb8ad40f03
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-05-01 11:18:45 +0200

    NET-1354 New Rule T0038: Use fields instead of auto-implemented private or protected properties

---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/UseField.cs
@@ -0,0 +1,35 @@
+﻿public class Sample
+{
+    private int field;
+
+    private int Private { get; set; }                       // Noncompliant {{Use field instead of this private auto-property.}}
+//  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    protected int Protected { get; set; }                   // Noncompliant {{Use field instead of this protected auto-property.}}
+    protected static int ProtectedStatic { get; set; }      // Noncompliant {{Use field instead of this protected auto-property.}}
+    private protected int PrivateProtected { get; set; }    // Noncompliant  {{Use field instead of this private protected auto-property.}}
+    private int PrivateReadOnly { get; }                    // Noncompliant
+    protected int ProtectedReadOnly { get; }                // Noncompliant
+
+    // Compliant
+    public int Public { get; set; }
+    internal int Internal { get; set; }
+    internal protected int InternalProtected { get; set; }
+
+    private int WithArrow
+    {
+        get => field;
+        set => field = value;
+    }
+
+    private int WithBody
+    {
+        get
+        {
+            return field;
+        }
+        set
+        {
+            field = value;
+        }
+    }
+}
