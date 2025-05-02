808e7a33f61c05e321d4e65dfa17927aee924921
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-05-02 13:23:57 +0200

    NET-1328 New Rule T0018: Move the operator to the beginning of the next line

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/OperatorLocation.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class OperatorLocation : StylingAnalyzer
+{
+    public OperatorLocation() : base("T0018", "The '{0}' operator should not be at the end of the line.") { }
+
+    protected override void Initialize(SonarAnalysisContext context)
+    {
+        context.RegisterNodeAction(c => Validate(c, ((BinaryExpressionSyntax)c.Node).OperatorToken),
+            SyntaxKind.LogicalAndExpression,
+            SyntaxKind.LogicalOrExpression,
+            SyntaxKind.BitwiseAndExpression,
+            SyntaxKind.BitwiseOrExpression,
+            SyntaxKind.ExclusiveOrExpression,
+            SyntaxKind.CoalesceExpression,
+            SyntaxKind.AddExpression,
+            SyntaxKind.LeftShiftExpression,
+            SyntaxKind.RightShiftExpression,
+            SyntaxKind.UnsignedRightShiftExpression,
+            SyntaxKind.SubtractExpression,
+            SyntaxKind.MultiplyExpression,
+            SyntaxKind.DivideExpression,
+            SyntaxKind.ModuloExpression,
+            SyntaxKind.EqualsExpression,
+            SyntaxKind.NotEqualsExpression,
+            SyntaxKind.GreaterThanExpression,
+            SyntaxKind.GreaterThanOrEqualExpression,
+            SyntaxKind.LessThanExpression,
+            SyntaxKind.LessThanOrEqualExpression,
+            SyntaxKind.AsExpression,
+            SyntaxKind.IsExpression);
+        context.RegisterNodeAction(c => Validate(c, ((BinaryPatternSyntax)c.Node).OperatorToken),
+            SyntaxKind.AndPattern,
+            SyntaxKind.OrPattern);
+        context.RegisterNodeAction(c => Validate(c, ((IsPatternExpressionSyntax)c.Node).IsKeyword),
+            SyntaxKind.IsPatternExpression);
+        context.RegisterNodeAction(c => Validate(c, ((RangeExpressionSyntax)c.Node).OperatorToken),
+            SyntaxKind.RangeExpression);
+        context.RegisterNodeAction(c =>
+            {
+                var conditional = (ConditionalExpressionSyntax)c.Node;
+                Validate(c, conditional.QuestionToken);
+                Validate(c, conditional.ColonToken);
+            },
+            SyntaxKind.ConditionalExpression);
+    }
+
+    private void Validate(SonarSyntaxNodeReportingContext context, SyntaxToken token)
+    {
+        if (token.GetLocation().StartLine() != token.GetNextToken().GetLocation().StartLine())
+        {
+            context.ReportIssue(Rule, token, token.ToString());
+        }
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/OperatorLocation.cs
@@ -0,0 +1,292 @@
+﻿using System.Collections.Generic;
+
+class MyClass
+{
+    void Compliant(int a, int b, int c, object o, List<int> list)
+    {
+        _ = a + b;
+        _ =
+            a + b;
+        c +=
+            a - b;
+        c -=
+            a - b;
+        c *=
+            a - b;
+        c /=
+            a - b;
+        c %=
+            a - b;
+        o ??=
+            new
+                object();
+        c <<=
+            a - b;
+        c >>=
+            a - b;
+        c >>>=
+            a - b;
+        c |=
+            a - b;
+        c &=
+            a - b;
+        c ^=
+            a - b;
+        o?
+            .
+            ToString();
+        o!
+            .
+            ToString();
+
+        o.ToString
+            ();
+        _ = list?
+            [0];
+        _ = list!
+            [0];
+    }
+
+    void LogicalOperator(bool a, bool b)
+    {
+        _ = a && b;
+        _ = a &&    // Noncompliant {{The '&&' operator should not be at the end of the line.}}
+        //    ^^
+            b;
+        _ = a ||    // Noncompliant
+            b;
+        _ = a &     // Noncompliant
+            b;
+        _ = a |     // Noncompliant
+            b;
+        _ = a ^     // Noncompliant
+            b;
+
+        _ = a
+            &&      // Noncompliant
+            b;
+
+        _ = a &&    // Noncompliant
+            (b ||   // Noncompliant
+            a) &&   // Noncompliant
+            b;
+    }
+
+    void Ternary(bool a, bool b)
+    {
+        _ = a ? 1 : 2;
+        _ = a ? 1 :     // Noncompliant
+            2;
+        _ = a ?         // Noncompliant
+            1 :         // Noncompliant
+            2;
+
+        _ = a
+            && // Noncompliant
+            b
+            ? // Noncompliant {{The '?' operator should not be at the end of the line.}}
+            1
+            : // Noncompliant {{The ':' operator should not be at the end of the line.}}
+            2;
+    }
+
+    void Coelesce(object o1, object o2)
+    {
+        _ = o1 ?? o2;
+        _ = o1 ??   // Noncompliant
+            o2;
+        _ = o1 ??   // Noncompliant
+            o2;
+        _ = o1
+            ??      // Noncompliant
+            o2;
+
+        _ = o1?
+            .ToString();
+    }
+
+    void AsIs(object o)
+    {
+        _ = o as string;
+        _ = o as // Noncompliant
+            string;
+        _ = o
+            as // Noncompliant
+            string;
+        _ = o is string;
+        _ = o is // Noncompliant
+            string;
+        _ = o
+            is // Noncompliant
+            string;
+    }
+
+    void Pattern(object o1, object o2)
+    {
+        _ = o1 is int or float;
+        _ = o1 is // Noncompliant
+            int or float;
+        _ = o1 is int or // Noncompliant
+            float;
+
+        _ = o1 is string { Length: > 5 } and // Noncompliant {{The 'and' operator should not be at the end of the line.}}
+            { Length: < 10 };
+
+        _ = o2 is string { Length: > 5 }
+            and // Noncompliant
+            { Length: < 10 };
+    }
+
+    void Arithmetic(int a, int b, string str)
+    {
+        _ = a + b;
+        _ = a + // Noncompliant
+            b;
+        _ = a
+            + // Noncompliant
+            b;
+        _ = a - b;
+        _ = a - // Noncompliant
+            b;
+        _ = a
+            - // Noncompliant
+            b;
+        _ = a * b;
+        _ = a * // Noncompliant
+            b;
+        _ = a
+            * // Noncompliant
+            b;
+        _ = a / b;
+        _ = a / // Noncompliant
+            b;
+        _ = a
+            / // Noncompliant
+            b;
+        _ = a % b;
+        _ = a % // Noncompliant
+            b;
+        _ = a
+            % // Noncompliant
+            b;
+
+        _ = str + " text " + str;
+        _ = str + // Noncompliant
+            " text " + str;
+        _ = str
+            + " text " + // Noncompliant
+            str;
+    }
+
+    void Comparison(int a, int b)
+    {
+        _ = a == b;
+        _ = a == // Noncompliant
+            b;
+        _ = a
+            == // Noncompliant
+            b;
+        _ = a != b;
+        _ = a != // Noncompliant
+            b;
+        _ = a
+            != // Noncompliant
+            b;
+        _ = a < b;
+        _ = a < // Noncompliant
+            b;
+        _ = a
+            < // Noncompliant
+            b;
+        _ = a > b;
+        _ = a > // Noncompliant
+            b;
+        _ = a
+            > // Noncompliant
+            b;
+        _ = a <= b;
+        _ = a <= // Noncompliant
+            b;
+        _ = a
+            <= // Noncompliant
+            b;
+        _ = a >= b;
+        _ = a >= // Noncompliant
+            b;
+        _ = a
+            >= // Noncompliant
+            b;
+    }
+
+    void Shift(int a, int b)
+    {
+        _ = a << b;
+        _ = a << // Noncompliant
+            b;
+        _ = a
+            << // Noncompliant
+            b;
+        _ = a >> b;
+        _ = a >> // Noncompliant
+            b;
+        _ = a
+            >> // Noncompliant
+            b;
+        _ = a >>> b;
+        _ = a >>> // Noncompliant
+            b;
+        _ = a
+            >>> // Noncompliant
+            b;
+    }
+
+    // Covered by SA1003
+    void Unary(int a, bool b)
+    {
+        _ = a++
+            + 1;
+        _ = +a;
+        _ = +
+            a;
+        _ = -a;
+        _ = -
+            a;
+        _ = ~a;
+        _ = ~
+            a;
+        _ = ++a;
+        _ = ++
+            a;
+        _ = --a;
+        _ = --
+            a;
+        _ = a++;
+        _ = !b;
+        _ = !
+            b;
+        _ = ^a;
+        _ = ^
+            a;
+
+        unsafe
+        {
+            _ = &a;
+            _ = &
+                a;
+            int *p = &a;
+            _ = *p;
+            _ = *
+                p;
+        }
+    }
+
+    void Range()
+    {
+        _ = 1..2;
+        _ = 1.. // Noncompliant
+            2;
+        _ = 1
+            .. // Noncompliant
+            2;
+    }
+}
