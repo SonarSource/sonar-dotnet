a82e3c3f355e62285443e374246d914999afb55a
Sebastien Marichal <sebastien.marichal@sonarsource.com>
2025-05-02 14:08:01 +0200

    NET-1359 New Rule T0043: Avoid primary constructors on normal classes and structs

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/AvoidPrimaryConstructor.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AvoidPrimaryConstructor : StylingAnalyzer
+{
+    public AvoidPrimaryConstructor() : base("T0043", "Do not use primary constructors.") { }
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
+        {
+            if (c.Node.Parent is ClassDeclarationSyntax or StructDeclarationSyntax)
+            {
+                c.ReportIssue(Rule, c.Node);
+            }
+        }, SyntaxKind.ParameterList);
+}
