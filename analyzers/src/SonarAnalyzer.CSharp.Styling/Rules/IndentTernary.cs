453b53622a23d059e42bc87bffa481c167a5d08e
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-05-02 14:48:15 +0200

    NET-1334 New Rule T0025: Indent ‘?’ and ‘:’ +4 further than the condition line

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/IndentTernary.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+using Microsoft.CodeAnalysis.Text;
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class IndentTernary : StylingAnalyzer
+{
+    public IndentTernary() : base("T0025", "Indent this ternary at line position {0}.") { }
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
+            {
+                var ternary = (ConditionalExpressionSyntax)c.Node;
+                var location = ternary.GetLocation();
+                if (location.StartLine() != location.EndLine() && StatementRoot(ternary) is { } root)
+                {
+                    var expected = (root.GetLocation().GetLineSpan().StartLinePosition.Character + 4) / 4 * 4; // Nearest next tab distance
+                    Verify(c, expected, ternary.QuestionToken, ternary.WhenTrue);
+                    Verify(c, expected, ternary.ColonToken, ternary.WhenFalse);
+                }
+            },
+            SyntaxKind.ConditionalExpression);
+
+    private void Verify(SonarSyntaxNodeReportingContext context, int expected, SyntaxToken token, ExpressionSyntax expression)
+    {
+        if (token.Line() != token.GetPreviousToken().Line() // Don't raise in T0024 cases
+            && token.GetLocation().GetLineSpan().StartLinePosition.Character != expected)
+        {
+            context.ReportIssue(Rule, Location.Create(expression.SyntaxTree, TextSpan.FromBounds(token.SpanStart, expression.Span.End)), (expected + 1).ToString());
+        }
+    }
+
+    private static SyntaxNode StatementRoot(SyntaxNode node)
+    {
+        var current = node;
+        while (current is not null)
+        {
+            if (current is ForStatementSyntax)
+            {
+                return node;    // Root from the ternary condition itself
+            }
+            else if (current is StatementSyntax
+                || (current is ExpressionSyntax && current.Parent is IfStatementSyntax or WhileStatementSyntax)
+                || current.Parent is ArrowExpressionClauseSyntax or ArgumentSyntax or LambdaExpressionSyntax)
+            {
+                return current;
+            }
+            else
+            {
+                current = current.Parent;
+            }
+        }
+        return null;
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/IndentTernary.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Linq;
+
+public class Sample
+{
+    private bool condition;
+
+    public void Method()
+    {
+        bool longVariableName;
+        longVariableName = condition    // 0
+        ? true          // Noncompliant {{Indent this ternary at line position 13.}}
+        : false;        // Noncompliant {{Indent this ternary at line position 13.}}
+
+        longVariableName = condition   // 1
+         ? true         // Noncompliant
+         : false;       // Noncompliant
+
+        longVariableName = condition   // 2
+          ? true        // Noncompliant
+          : false;      // Noncompliant
+
+        longVariableName = condition   // 3
+           ? true       // Noncompliant
+           : false;     // Noncompliant
+
+        longVariableName = condition   // 4
+            ? true
+            : false;
+
+        longVariableName = condition   // 5
+             ? true     // Noncompliant
+        //   ^^^^^^
+             : false;   // Noncompliant
+        //   ^^^^^^^
+
+        longVariableName = condition   // One of the branches is too far
+             ? true     // Noncompliant
+            : false;
+
+        longVariableName = condition   // One of the branches is too far
+            ? true
+             : false;   // Noncompliant
+
+        longVariableName = condition ? true : false;
+        longVariableName = condition
+            ? true : false;     // Compliant, this is T0024
+        longVariableName = condition ? true
+            : false;            // Compliant, this is T0024
+    }
+
+    public bool ArrowNoncompliant() =>
+        condition
+        ? true          // Noncompliant
+        : false;        // Noncompliant
+
+    public bool ArrowCompliant() =>
+        condition
+            ? true
+            : false;
+
+    public bool ArrowNotInScope() =>
+        condition ? true : false;
+
+    public bool ReturnNoncompliant()
+    {
+        return condition
+        ? true  // Noncompliant, too little
+                : false; // Noncompliant, too much
+    }
+
+    public bool ReturnCompliant()
+    {
+        return condition
+            ? true
+            : false;
+    }
+
+    public void Invocations()
+    {
+        Something(condition // This is bad already
+        ? true              // Noncompliant {{Indent this ternary at line position 21.}}
+            : false,        // Noncompliant, too close as well
+            "Some other argument");
+        Something(condition // This is bad already
+                    ? true
+                    : false,
+            "Some other argument");
+        global::Sample.Something(condition  // This is bad already
+                ? true      // Noncompliant {{Indent this ternary at line position 37.}}
+                : false,    // Noncompliant
+            "Some other argument");
+        global::Sample.Something(condition  // This is bad already
+                                    ? true
+                                    : false,
+            "Some other argument");
+        Something(Something(Something(condition // This is bad already
+                ? true      // Noncompliant {{Indent this ternary at line position 41.}}
+                : false,    // Noncompliant
+            "Some other argument")));
+        Something(Something(Something(condition // This is bad already
+                                        ? true
+                                        : false,
+            "Some other argument")));
+
+        Something(
+            condition
+            ? true          // Noncompliant {{Indent this ternary at line position 17.}}
+            : false,        // Noncompliant
+            "Some other argument");
+        Something(
+            condition
+                ? true
+                : false,
+            "Some other argument");
+
+        global::Sample.Something(       // Longer invocation name does not matter
+            condition
+                ? true
+                : false,
+            "Some other argument");
+
+        Something(Something(Something(  // This is bad already for other reasons
+            condition
+                ? true
+                : false,
+            "Some other argument")));
+    }
+
+    public void Lambdas(int[] list, int[] longer)
+    {
+        list.Where(x => condition
+            ? true                  // Noncompliant, too close
+                        : false);   // Noncompliant, too close
+        list.Where(x => condition
+                        ? true      // Noncompliant, too close
+                        : false);   // Noncompliant, too close
+        list.Where(x => condition
+                                ? true  // Noncompliant, too far
+                             : false);  // Noncompliant, too far
+        list.Where(x => condition       // Simple lambda
+                            ? true
+                            : false);
+        list.Where((x) => condition     // Parenthesized lambda
+                            ? true
+                            : false);
+        longer.Where(x => condition         // Simple lambda, longer name
+                            ? true          // Compliant, as long as it's after the condition and aligned to the grid of 4
+                            : false);
+        longer.Where((x) => condition       // Parenthesized lambda, longer name
+                                ? true      // Compliant, as long as it's after the condition and aligned to the grid of 4
+                                : false);
+        list.Where(x =>
+        {
+            return condition
+                ? true
+                : false;
+        });
+    }
+
+    public void If()
+    {
+        if (condition
+        ? true                  // Noncompliant {{Indent this ternary at line position 17.}}
+                    : false)    // Noncompliant
+        {
+        }
+        if (condition
+                ? true
+                : false)
+        {
+        }
+    }
+
+    public void While()
+    {
+        while (condition
+        ? true                  // Noncompliant {{Indent this ternary at line position 17.}}
+                    : false)    // Noncompliant
+        {
+        }
+        while (condition
+                ? true
+                : false)
+        {
+        }
+    }
+
+    public void For()
+    {
+        for (var i = 0; condition
+                        ? true                  // Noncompliant {{Indent this ternary at line position 29.}}
+                                : false; i++)   // Noncompliant
+        {
+        }
+        for (int i = 0; condition
+                            ? true
+                            : false; i++)
+        {
+        }
+    }
+
+    [Obsolete(true  // Not supported, used for coverage
+    ? "true"
+    : "false")]
+    public static bool Something(bool arg, object another = null) => true;
+}
