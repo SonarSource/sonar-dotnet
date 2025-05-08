a26f1bda02b0d202d5ac86f816a1fb1b703cb3e6
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-05-08 17:08:23 +0200

    NET-1357 New Rule T0041: Use raw string literals for multiline strings

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/UseRawString.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class UseRawString : StylingAnalyzer
+{
+    public UseRawString() : base("T0041", "Use raw string literal for multiline strings.") { }
+
+    protected override void Initialize(SonarAnalysisContext context)
+    {
+        context.RegisterNodeAction(c =>
+            {
+                var location = c.Node.GetLocation();
+                if (location.StartLine() != location.EndLine()
+                    && c.Node.GetFirstToken().Kind() is not SyntaxKind.MultiLineRawStringLiteralToken and not SyntaxKind.InterpolatedMultiLineRawStringStartToken)
+                {
+                    c.ReportIssue(Rule, c.Node);
+                }
+            },
+            SyntaxKind.StringLiteralExpression,
+            SyntaxKind.InterpolatedStringExpression);
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/UseRawString.cs
@@ -0,0 +1,67 @@
+﻿public class Sample
+{
+    public void Method()
+    {
+        // Noncompliant@+1 {{Use raw string literal for multiline strings.}}
+        _ = @"
+Verbatim
+Multiline";
+        // Noncompliant@+1
+        _ = @"""
+Still Verbatim
+But mutiline
+""";
+        // Noncompliant@+1
+        _ = @"Smallest just to get the EOL
+";
+        // Noncompliant@+1
+        _ = @$"Verbatim
+{42}
+Interpolated";
+        // Noncompliant@+1
+        _ = $@"Interpolated
+{42}
+Verbatim";
+
+        _ = """
+            This is fine
+            """;
+        _ = $"""
+            Interpolated
+            {42}
+            Raw
+            """;
+        _ = $$$"""
+            Interpolated
+            {{{42}}}
+            Raw
+            """;
+    }
+
+    public void SingleLine()
+    {
+        _ = "";
+        _ = "Normal";
+        _ = @"Verbatim";
+        _ = @"""Still Verbatim""";
+        _ = """Raw""";
+        _ = $"Interpolated {42}";
+        _ = @$"Verbatim {42} Interpolated";
+        _ = $@"Interpolated {42} Verbatim";
+        _ = $"""Interpolated {42} Raw""";
+        _ = $$$"""Interpolated {{{42}}} Raw""";
+    }
+
+    public void Binary()
+    {
+        // We don't raise on these
+        _ = "FirstLine" +
+            "Also first Line";
+        _ = "FirstLine\n" +
+            "Second Line";
+        _ = "FirstLine\r" +
+            "Second Line";
+        _ = "FirstLine\n\r" +
+            "Second Line";
+    }
+}
