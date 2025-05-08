31292ee7e50a2a4a529a985c1311d5fc8594a912
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-05-08 10:49:06 +0200

    NET-1336 New Rule T0027: Move subsequent expressions on separate lines

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/MemberAccessLine.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+using Microsoft.CodeAnalysis.Text;
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class MemberAccessLine : StylingAnalyzer
+{
+    public MemberAccessLine() : base("T0027", "Move this member access to the next line.") { }
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
+            {
+                var memberAccess = (MemberAccessExpressionSyntax)c.Node;
+                var nameLine = memberAccess.Name.GetLocation().StartLine();
+                if (memberAccess.GetLocation().StartLine() != nameLine   // ToDo: Use HasSameLineAs
+                    && memberAccess.OperatorToken.GetPreviousToken().GetLocation().EndLine() == nameLine
+                    && !IsIgnored(memberAccess))
+                {
+                    c.ReportIssue(Rule, Location.Create(c.Tree, TextSpan.FromBounds(memberAccess.OperatorToken.SpanStart, memberAccess.Span.End)));
+                }
+            },
+            SyntaxKind.SimpleMemberAccessExpression);
+
+    private static bool IsIgnored(MemberAccessExpressionSyntax memberAccess) =>
+        memberAccess.Expression is MemberAccessExpressionSyntax
+        {
+            Name: IdentifierNameSyntax { Identifier.ValueText: "And" }
+        };
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/MemberAccessLine.cs
@@ -0,0 +1,105 @@
+﻿public class Sample
+{
+    Builder builder;
+
+    public void Method()
+    {
+        builder.Build().Build()
+            .Build();
+        _ = builder
+            .Build().Build()    // Noncompliant {{Move this member access to the next line.}}
+        //          ^^^^^^
+            .Build().Variable;  // Noncompliant
+        //          ^^^^^^^^^
+        builder
+            .Build().Build();   // Noncompliant
+        //          ^^^^^^
+        builder.Build().Variable.Build()
+            .Build()
+            .Build().Build()    // Noncompliant
+            .Build().Variable   // Noncompliant
+            .Build().Property   // Noncompliant
+            .Build()[42]
+            .Build();
+
+        builder.Build()
+            .Build().Build().Build().Build();
+        //          ^^^^^^                  // Noncompliant@-1
+        //                   ^^^^^^         // Noncompliant@-2
+        //                          ^^^^^^  // Noncompliant@-3
+
+        builder.Build()
+            .Variable
+            .Build()
+            .Property
+            .Build()
+            .Indexed[42]
+            .Build()
+            .Property[42]
+            .Variable[42]
+            .Build()[42]
+            .Build();
+    }
+
+    public void FluentAssertions()
+    {
+        builder.Should().BeSomething()
+            .And.BeSomething().And.BeSomething();   // Noncompliant
+        //                    ^^^^
+        builder.Should().BeSomething()
+            .And.BeSomething()
+            .And            // Allowed
+            .BeSomething();
+    }
+
+    public void ConditionalAccess()
+    {
+        builder?
+            .Build()?.Build();              // FN, ConditionalAccessExpression and MemberBindingExpression are too different from MemberAccessExpression to deal with
+        builder?.Build()?.Build();
+    }
+
+    public void Global()
+    {
+        global::Builder
+                .StaticMethod().Build();    // Noncompliant
+        //                     ^^^^^^
+        global::Builder
+            .StaticMethod()
+            .Build();
+    }
+
+    public void Nested()
+    {
+        Builder.NestedOnce.NestedTwice
+            .StaticMethod().Build();        // Noncompliant
+        Builder.NestedOnce.NestedTwice
+            .StaticMethod()
+            .Build();
+    }
+}
+
+public class Builder
+{
+    public Builder Variable;
+    public Builder Property => null;
+    public Builder[] Indexed;
+    public Builder this[int index] => null;
+
+    public Builder Build() => this;
+    public static Builder StaticMethod() => null;
+    public bool IsTrue() => true;
+
+    // FluentAssertions-like methods
+    public Builder And => null; // Special case to simulate FluentAssertions API
+    public Builder Should() => null;
+    public Builder BeSomething() => null;
+
+    public class NestedOnce
+    {
+        public class NestedTwice
+        {
+            public static Builder StaticMethod() => null;
+        }
+    }
+}
