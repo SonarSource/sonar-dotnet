d7bfb6fda67e97c7e354d5fb6d517e8d0731a3bd
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2025-05-22 16:05:54 +0200

    NET-1474 Repro for Fix S6966 FP: should not raise when inside lock

--- analyzers/tests/SonarAnalyzer.Test/TestCases/UseAwaitableMethod.cs
+++ analyzers/tests/SonarAnalyzer.Test/TestCases/UseAwaitableMethod.cs
@@ -296,3 +296,17 @@ public class XmlReaderException
         }
     }
 }
+
+// Repro for https://sonarsource.atlassian.net/browse/NET-1468
+public class TestLockClass
+{
+    private readonly object locktarget;
+    private readonly SemaphoreSlim sm;
+    public async Task DoWorkAsync(CancellationToken cancellationToken)
+    {
+        lock (locktarget)
+        {
+            sm.Wait(); // Noncompliant FP
+        }
+    }
+}
