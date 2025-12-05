b923f4855e6238c7afe7251a0b4f7719e3abaa06
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-12-05 09:18:52 +0100

    NET-2720 Create ShimLayer projects
    GitOrigin-RevId: 9485d54915f738b5bf828bb826b34bb7d7bc0d52

---
+++ analyzers/src/SonarAnalyzer.ShimLayer.Generator/ShimLayerGenerator.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace SonarAnalyzer.ShimLayer.Generator;
+
+[Generator]
+[ExcludeFromCodeCoverage]
+public class ShimLayerGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context) =>
+        context.RegisterSourceOutput(
+            context.ParseOptionsProvider.Select((_, _) => context.GetType().Assembly.FullName), // Any simple provider, return Roslyn version
+            (context, _) =>
+            {
+                foreach (var file in Factory.CreateAllFiles())
+                {
+                    context.AddSource(file.Name, file.Content);
+                }
+            });
+}
