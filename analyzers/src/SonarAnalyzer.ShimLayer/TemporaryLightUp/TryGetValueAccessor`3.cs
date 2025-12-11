d4a07eefe3def0bdc34c758baa0878bfbae9958d
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2025-12-11 10:23:04 +0100

    NET-2835 ShimLayer: Add temporary Lightup helpers
    GitOrigin-RevId: 95035337a439259de5250ad3e752bbf345c08d2f

---
+++ analyzers/src/SonarAnalyzer.ShimLayer/TemporaryLightUp/TryGetValueAccessor`3.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Tunnel Vision Laboratories, LLC. All Rights Reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#nullable disable
+
+namespace SonarAnalyzer.ShimLayer
+{
+    internal delegate bool TryGetValueAccessor<T, TKey, TValue>(T instance, TKey key, out TValue value);
+    internal delegate bool TryGetValueAccessor<T, TFirst, TSecond, TValue>(T instance, TFirst first, TSecond second, out TValue value); // Sonar
+    internal delegate bool TryGetValueAccessor<T, TFirst, TSecond, TThird, TValue>(T instance, TFirst first, TSecond second, TThird third, out TValue value); // Sonar
+}
