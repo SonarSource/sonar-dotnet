d074dc081adcd92b25e835368daba9d9ceec7f6f
Martin Strecker <103252490+martin-strecker-sonarsource@users.noreply.github.com>
2025-12-17 10:36:01 +0100

    NET-2893 Simplify SyntaxNodeTypes.LatestType
    Co-authored-by: Pavel Mikula <pavel.mikula@sonarsource.com>
    GitOrigin-RevId: 3d5d33a27a7baff9184b9fbef26ae3f8c82cdaa4

--- analyzers/src/SonarAnalyzer.ShimLayer/Common/SyntaxNodeTypes.cs
+++ analyzers/src/SonarAnalyzer.ShimLayer/Common/SyntaxNodeTypes.cs
@@ -14,31 +14,20 @@
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 
+using System.Reflection;
 using Microsoft.CodeAnalysis.CSharp;
 
 namespace SonarAnalyzer.ShimLayer;
 
 internal static class SyntaxNodeTypes
 {
-    private static readonly ImmutableDictionary<Type, Type> Map;    // Wrapper type => Latest Roslyn type
-
-    static SyntaxNodeTypes()
+    public static Type LatestType(Type wrapper)
     {
-        var builder = ImmutableDictionary.CreateBuilder<Type, Type>();
-        var assembly = typeof(CSharpSyntaxNode).Assembly;
-        foreach (var wrapper in typeof(SyntaxNodeTypes).Assembly.ExportedTypes)
-        {
-            if ((Load(wrapper, nameof(BaseNamespaceDeclarationSyntaxWrapper.WrappedTypeName)) ?? Load(wrapper, nameof(BaseNamespaceDeclarationSyntaxWrapper.FallbackWrappedTypeName))) is { } type)
-            {
-                builder.Add(wrapper, type);
-            }
-        }
-        Map = builder.ToImmutable();
+        return Load(wrapper, nameof(BaseNamespaceDeclarationSyntaxWrapper.WrappedTypeName)) ?? Load(wrapper, nameof(BaseNamespaceDeclarationSyntaxWrapper.FallbackWrappedTypeName));
 
-        Type Load(Type wrapper, string fieldName) =>
-            wrapper.GetField(fieldName) is { } field && field.GetValue(null) is string name ? assembly.GetType(name) : null;
+        static Type Load(Type wrapper, string fieldName) =>
+            wrapper.GetField(fieldName, BindingFlags.Static | BindingFlags.Public) is { } field && field.GetValue(null) is string name
+                ? typeof(CSharpSyntaxNode).Assembly.GetType(name) // This may need to be extended to other assemblies if needed. See TypeLoader.LoadBaseline and .LoadLatest
+                : null;
     }
-
-    public static Type LatestType(Type wrapper) =>
-        Map.TryGetValue(wrapper, out var latest) ? latest : null;
 }
