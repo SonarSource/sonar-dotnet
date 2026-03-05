c95a3025eae531196a19ea081c9b183669ffdcb4
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2026-03-05 16:44:29 +0100

    NET-3326 Implement JsonSerializer
    GitOrigin-RevId: 9b161e23bf3a3c10e0da8e2dd574e528bd202921

---
+++ analyzers/src/SonarAnalyzer.Core/Json/JsonSerializer.cs
@@ -0,0 +1,75 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+using System.Text;
+
+namespace SonarAnalyzer.Core.Json;
+
+public static class JsonSerializer
+{
+    public static string Serialize(object value) =>
+        SerializeLines("{", "}", value.GetType().GetProperties().Where(x => x.GetIndexParameters().Length == 0).Select(x => $"""  "{SerializeKey(x.Name)}": {SerializeValue(x.GetValue(value))}"""));
+
+    private static string SerializeKey(string key) =>
+        char.ToLowerInvariant(key[0]) + key.Substring(1);
+
+    private static string SerializeValue(object original) =>
+        original switch
+        {
+            null => "null",
+            bool value => value.ToString().ToLower(),
+            int value => value.ToString(),
+            string value => SerializeValue(value),
+            Enum => SerializeValue(original.ToString()),
+            IEnumerable<string> values => $"[{values.JoinStr(", ", SerializeValue)}]",
+            IEnumerable<KeyValuePair<string, object>> values => SerializeValue(values),
+            _ => throw new NotSupportedException($"Unexpected type: {original.GetType().Name}")
+        };
+
+    private static string SerializeValue(string value)
+    {
+        var sb = new StringBuilder();
+        sb.Append(@"""");
+        foreach (var ch in value)
+        {
+            sb.Append(ch switch
+            {
+                '\\' => @"\\",
+                '\"' => @"\""",
+                '\n' => @"\n",
+                '\r' => @"\r",
+                '\t' => @"\t",
+                '\b' => @"\b",
+                '\f' => @"\f",
+                _ => ch
+            });
+        }
+        sb.Append(@"""");
+        return sb.ToString();
+    }
+
+    private static string SerializeValue(IEnumerable<KeyValuePair<string, object>> pairs) =>
+        SerializeLines("[", "  ]", pairs.Select(x => $$"""    { "key": {{SerializeValue(x.Key)}}, "value": {{SerializeValue(x.Value)}} }"""));
+
+    private static string SerializeLines(string prefix, string suffix, IEnumerable<string> lines)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine(prefix);
+        sb.AppendLine(lines.JoinStr($",{Environment.NewLine}"));
+        sb.AppendLine(suffix);
+        return sb.ToString();
+    }
+}
---
+++ analyzers/tests/SonarAnalyzer.Core.Test/Json/JsonSerializerTest.cs
@@ -0,0 +1,111 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) 2014-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SonarAnalyzer.Core.Json.Test;
+
+[TestClass]
+public class JsonSerializerTest
+{
+    [TestMethod]
+    public void Serialize_UnsupportedType() =>
+        FluentActions.Invoking(() => JsonSerializer.Serialize(new { Value = new StringBuilder() })).Should().Throw<NotSupportedException>().WithMessage("Unexpected type: StringBuilder");
+
+    [TestMethod]
+    public void Serialize_BasicTypes()
+    {
+        var dictionary = new Dictionary<string, object>
+        {
+            { "StringKey", "String value"},
+            { "BoolKey", true},
+            { "IntKey", 42}
+        };
+        var value = new TestData("Name", true, false, null, ["a", "b", "c"], StringComparison.CurrentCulture, dictionary.ToArray());
+
+        var result = JsonSerializer.Serialize(value);
+        result.ToUnixLineEndings().Should().Be("""
+            {
+              "stringValue": "Name",
+              "trueValue": true,
+              "falseValue": false,
+              "nullString": null,
+              "stringArray": ["a", "b", "c"],
+              "enumValue": "CurrentCulture",
+              "dictionary": [
+                { "key": "StringKey", "value": "String value" },
+                { "key": "BoolKey", "value": true },
+                { "key": "IntKey", "value": 42 }
+              ]
+
+            }
+
+            """);
+        // And it also deserializes correctly
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters = { new JsonStringEnumConverter(), new PrimitiveObjectConverter() } };
+        System.Text.Json.JsonSerializer.Deserialize<TestData>(result, options).Should().BeEquivalentTo(value);
+    }
+
+    [TestMethod]
+    public void Serialize_Encoding() =>
+        JsonSerializer.Serialize(new { Value = "Start \\ \" \n \r \t \b \f End" }).ToUnixLineEndings().Should().Be("""
+            {
+              "value": "Start \\ \" \n \r \t \b \f End"
+            }
+
+            """);
+
+    [TestMethod]
+    public void Serialize_ObjectWithIndexer() =>
+        JsonSerializer.Serialize(new List<int> { 42, 43 }).ToUnixLineEndings().Should().Be("""
+            {
+              "capacity": 4,
+              "count": 2
+            }
+
+            """);
+
+    private sealed record TestData(string StringValue,
+                                   bool TrueValue,
+                                   bool FalseValue,
+                                   string NullString,
+                                   string[] StringArray,
+                                   StringComparison EnumValue,
+                                   KeyValuePair<string, object>[] Dictionary)
+    {
+        public TestData() : this(null, true, false, null, null, StringComparison.Ordinal, null) { }
+    }
+
+    private sealed class PrimitiveObjectConverter : JsonConverter<object>
+    {
+        // This makes KeyValuePair<string, object> to deserialize to actual value. The default behavior is that object holds general JsonElement instead.
+        public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) =>
+            reader.TokenType switch
+            {
+                JsonTokenType.True => true,
+                JsonTokenType.False => false,
+                JsonTokenType.Null => null,
+                JsonTokenType.String => reader.GetString(),
+                JsonTokenType.Number => reader.GetDouble(),
+                _ => throw new InvalidOperationException("Unexpected token type: " + reader.TokenType)
+            };
+
+        public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options) =>
+            throw new NotSupportedException();
+    }
+}
