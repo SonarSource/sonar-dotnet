3f663b888a40a3eb8334fd42f239e5f96db7999d
Pavel Mikula <57188685+pavel-mikula-sonarsource@users.noreply.github.com>
2026-04-10 08:44:12 +0200

    NET-3565 Happy New Year! Ultimate Edition - .NET
    GitOrigin-RevId: 205da2613f8714b407afe00e5f544dd634333dcd

--- analyzers/src/SonarAnalyzer.VisualBasic.Core/Syntax/Utilities/VisualBasicSyntaxClassifier.cs
+++ analyzers/src/SonarAnalyzer.VisualBasic.Core/Syntax/Utilities/VisualBasicSyntaxClassifier.cs
@@ -1,7 +1,18 @@
 ﻿/*
- * Copyright (C) 2015-2025 SonarSource Sàrl
- * All rights reserved
+ * SonarAnalyzer for .NET
+ * Copyright (C) SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 
 using SonarAnalyzer.CFG.Syntax.Utilities;
