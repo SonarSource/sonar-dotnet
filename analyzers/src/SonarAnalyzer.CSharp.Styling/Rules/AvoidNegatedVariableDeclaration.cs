4776e1ba7e9f2dcfea6d744ae4c37b05334b6140
Alex Meseldzija <alexander.meseldzija@sonarsource.com>
2026-07-01 14:46:58 +0200

    NET-2280 New Rule T0048: Avoid `is not { }`
    GitOrigin-RevId: 92a4780069c86bb13751345acf3bf45fa7ce04b7

---
+++ analyzers/src/SonarAnalyzer.CSharp.Styling/Rules/AvoidNegatedVariableDeclaration.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * SonarAnalyzer for .NET
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+namespace SonarAnalyzer.CSharp.Styling.Rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class AvoidNegatedVariableDeclaration : StylingAnalyzer
+{
+    public AvoidNegatedVariableDeclaration() : base("T0048", "Avoid declaring a variable in a negated pattern.") { }
+
+    protected override void Initialize(SonarAnalysisContext context) =>
+        context.RegisterNodeAction(c =>
+            {
+                if (c.Node.DescendantNodes().Any(x => x is SingleVariableDesignationSyntax))
+                {
+                    c.ReportIssue(Rule, c.Node);
+                }
+            },
+            SyntaxKind.NotPattern);
+}
---
+++ analyzers/tests/SonarAnalyzer.CSharp.Styling.Test/TestCases/AvoidNegatedVariableDeclaration.cs
@@ -0,0 +1,47 @@
+﻿public class Sample
+{
+    public void Method(object o, string s, int[] arr)
+    {
+        _ = o is not { } a;                 // Noncompliant {{Avoid declaring a variable in a negated pattern.}}
+        //       ^^^^^^^^^
+        _ = o is not int b;                 // Noncompliant
+        _ = o is not string c;              // Noncompliant
+        _ = o is not Other { } d;           // Noncompliant
+        _ = s is not { Length: 0 } e;       // Noncompliant
+        _ = arr is not [] g;                // Noncompliant
+        if (o is not not { } h) { }         // Noncompliant
+                                            // Noncompliant@-1
+        _ = o is not ({ } j);               // Noncompliant
+        _ = s is not { Length: var k };     // Noncompliant
+        _ = s is not { Length: { } l };     // Noncompliant
+        while (o is not { } i) { }          // Noncompliant
+
+        // Compliant - no variable is declared
+        _ = o is not { };
+        _ = o is not int;
+        _ = o is not Other;
+        _ = o is not null;
+        _ = o is not int _;
+
+        // Compliant - positive patterns
+        _ = o is { } positive;
+        _ = o is int alsoPositive;
+        _ = o is null;
+
+        // Compliant - the 'not' constrains a value inside a subpattern; the capture itself is positive
+        _ = s is { Length: not 0 } nonEmpty;
+        _ = o is Other { Length: not 0 } typed;
+
+        // Compliant - declaration sits next to a 'not', not under the 'not'
+        _ = o is not int and { } chainedAnd;
+        _ = s is { Length: not 0 and var lengthCapture };
+        _ = o is int x and not 0;
+        _ = o is not int && s is { } chainedSeparate;
+        if (o is not int && s is { } chainedInIf) { }
+    }
+}
+
+public class Other
+{
+    public int Length { get; }
+}
